7dee2a55d008aa576c91958f98b9fae9b26ea81b
gkuznets <gkuznets@4525493e-7705-40b1-a816-d608a930855b>
2007-05-17 11:09:54 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@4388 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/DB/ProductionRepositoryDB.py
+++ LHCbDIRAC/ProductionManagementSystem/DB/ProductionRepositoryDB.py
@@ -1,14 +1,18 @@
-# $Id: ProductionRepositoryDB.py,v 1.1 2007/05/16 13:30:21 gkuznets Exp $
+# $Id: ProductionRepositoryDB.py,v 1.2 2007/05/17 11:09:16 gkuznets Exp $
 """
     DIRAC ProductionRepositoryDB class is a front-end to the pepository database containing
     Workflow (templates) Productions and vectors to create jobs.
 
     The following methods are provided for public usage:
 
-    publishWF()
+    publishWorkflow()
+    getWorkflow()
+    getWorkglowsList()
+    getWorkflowInfo()
+    updateWorkflow()
 
 """
-__RCSID__ = "$Revision: 1.1 $"
+__RCSID__ = "$Revision: 1.2 $"
 
 from DIRAC.Core.Base.DB import DB
 from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
@@ -19,25 +23,63 @@ class ProductionRepositoryDB(BaseDB):
     """ Constructor
     """
 
-    BaseDB.__init__(self,'ProductionRepositoryDB', 'ProductionManagement/ProductionrepositoryDB', maxQueueSize)
+    BaseDB.__init__(self,'ProductionRepositoryDB', 'ProductionManagement/ProductionRepositoryDB', maxQueueSize)
 
-  def publishWF(self, wf, userDN):
-    wfName = wf.getName()
-    wfType = wf.getType()
+  def publishWorkflow(self, wf_body, publisherDN):
+    wf = Workflow(wf_body)
+    wf_name = wf.getName()
+    wf_type = wf.getType()
     cmd = 'INSERT INTO Workflows ( WFName, WFType, PublisherDN, PublishingTime, Body ) VALUES ' \
-            '(\'%s\', \'%s\', \'%s\', NOW(), \'%s\')' % (wfName, wfType, publisherDN, wf.toXMLString())
+            '(\'%s\', \'%s\', \'%s\', NOW(), \'%s\')' % (wf_name, wf_type, publisherDN, wf_body)
     result = self._update( cmd )
     if result['OK']:
-      self.gLogger.info( 'Workflow "%s" Type "%s" published by DN="%s"' % (wfName, wfType, publisherDN) )
+      self.log.info( 'Workflow "%s" Type "%s" published by DN="%s"' % (wf_name, wf_type, publisherDN) )
     else:
-      self.gLogger.error( 'Workflow "%s" Type "%s" FAILED to be published by DN="%s"' % (wfName, wfType, publisherDN) )
-      return S_ERROR('Failed to publish Workflow')
+      error = 'Workflow "%s" Type "%s" FAILED to be published by DN="%s"' % (wf_name, wf_type, publisherDN)
+      self.log.error( error )
+      return S_ERROR( error )
     return S_OK()
 
-  def getWorkflow(self, wfName):
-    cmd = "SELECT body from Workflows WHERE WFName='%s'" % wfName
+  def getWorkflow(self, wf_name):
+    cmd = "SELECT WFName, WFType, PublisherDN, PublishingTime Body from Workflows WHERE WFName='%s'" % wf_name
     result = self._query(cmd)
     if result['OK']:
       return S_OK(result['Value'])
     else:
-      return S_ERROR('Failed to retrive Workflow with the name '+wfName)
+      return S_ERROR('Failed to retrive Workflow with the name '+wf_name)
+
+  def getWorkflowsList(self):
+    #KGG we need to adjust code for the empty list!!!!
+    cmd = "SELECT  WFName, WFType, PublisherDN, PublishingTime from Workflows"
+    result = self._query(cmd)
+    if not result['OK']:
+      return result
+    try:
+      wf_list = result['Value']
+      result_list = [ (x[0],x[1],x[2],x[3]) for x in wf_list]
+      return S_OK(result_list)
+    except:
+      return S_ERROR('Failed to get Workflow list from the Production Repository')
+
+  def getWorkflowInfo(self, wf_name):
+    cmd = "SELECT  WFName, WFType, PublisherDN, PublishingTime from Workflows WHERE WFName='%s'" % wf_name
+    result = self._query(cmd)
+    if result['OK']:
+      return S_OK(result['Value'])
+    else:
+      return S_ERROR('Failed to retrive Workflow with the name '+wf_name) #KGG need to check logic
+
+  def updateWorkflow(self, wf_body, publisherDN):
+    wf = Workflow(wf_body)
+    wf_name = wf.getName()
+    wf_type = wf.getType()
+    cmd = "UPDATE Workflows set WFType='%s', PublisherDN='%s', PublishingTime=NOW(), Body='%s' WHERE WFName='%s'" \
+            % (wf_type, publisherDN, wf_body, wf_name)
+    result = self._update( cmd )
+    if result['OK']:
+      self.log.info( 'Workflow "%s" Type "%s" updated by DN="%s"' % (wf_name, wf_type, publisherDN) )
+    else:
+      error = 'Workflow "%s" Type "%s" FAILED on update by DN="%s"' % (wf_name, wf_type, publisherDN)
+      self.log.error( error )
+      return S_ERROR( error )
+    return S_OK()
---
+++ LHCbDIRAC/ProductionManagementSystem/Service/ProductionRepositoryHandler.py
@@ -0,0 +1,64 @@
+# $Id: ProductionRepositoryHandler.py,v 1.1 2007/05/17 11:09:54 gkuznets Exp $
+"""
+ProductionRepositoryHandler is the implementation of the ProductionRepository service
+    in the DISET framework
+
+    The following methods are available in the Service interface
+
+    publishWF()
+    getWorkflow()
+
+"""
+__RCSID__ = "$Revision: 1.1 $"
+
+from DIRAC.Core.DISET.RequestHandler import RequestHandler
+from DIRAC import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.ProductionManagementSystem.DB.ProductionRepositoryDB import ProductionRepositoryDB
+
+# This is a global instance of the ProductionRepositoryDB class
+ProductionRepositoryDB = False
+
+def initializeProductionRepositoryHandler( serviceInfo ):
+  global ProductionRepositoryDB
+  ProductionRepositoryDB = ProductionRepositoryDB()
+  return S_OK()
+
+class ProductionRepositoryHandler( RequestHandler ):
+
+  types_publishWorkflow = [ StringType ]
+  def export_publishWorkflow( self, wf_body ):
+    result = ProductionRepositoryDB.publishWorkflow(wf_body, self.sDN)
+    if not result['OK']:
+        return result
+    gLogger.info('Workflow '+wf_name+' of type '+ wf_type+' added to the Production Repository by the '+self.sDN )
+    return S_OK()
+
+  types_getWorkflow = [ StringType ]
+  def export_getWorkflow( self, wf_name ):
+    result = ProductionRepositoryDB.getWorkflow(wf_name)
+    if not result['OK']:
+        error = 'Failed to read Workflow with the name %s from the repository' % wf_name
+        gLogger.error(error)
+        return S_ERROR(error)
+    gLogger.info('Workflow %s sucessfully read from the Production Repository' % wf_name)
+    return S_OK(result['Value'])
+
+  types_getWorkflowsList = [ ]
+  def export_getWorkflowsList(self):
+    result = ProductionRepositoryDB.getWorkflowsList()
+    if not result['OK']:
+      error = 'Failed to read List of Workflows from the repository'
+      gLogger.error(error)
+      return S_ERROR(error)
+    gLogger.info('List of Workflows requested from the Production Repository')
+    return S_OK(result['Value'])
+
+  types_getWorkflow = [ StringType ]
+  def export_getWorkflowInfo( self, wf_name ):
+    result = ProductionRepositoryDB.getWorkflowInfo(wf_name)
+    if not result['OK']:
+        error = 'Failed to read Workflow Info with the name %s from the repository' % wf_name
+        gLogger.error(error)
+        return S_ERROR(error)
+    gLogger.info('Workflow Info %s sucessfully read from the Production Repository' % wf_name)
+    return S_OK(result['Value'])
