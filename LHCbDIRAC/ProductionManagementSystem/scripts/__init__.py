2327f85922020ab52801c370d9a5c9fe4756b0cd
gkuznets <gkuznets@4525493e-7705-40b1-a816-d608a930855b>
2007-05-30 11:24:27 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@4516 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/ProductionManagementSystem/scripts/__init__.py
@@ -0,0 +1,2 @@
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/scripts/__init__.py,v 1.1 2007/05/30 11:24:27 gkuznets Exp $
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/05/30 11:24:27 gkuznets Exp $"
