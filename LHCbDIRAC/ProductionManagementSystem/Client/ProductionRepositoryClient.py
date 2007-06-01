22d5b927bef3dce143aed8e0072c923bb2752460
gkuznets <gkuznets@4525493e-7705-40b1-a816-d608a930855b>
2007-06-01 13:08:54 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@4525 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Client/ProductionRepositoryClient.py
+++ LHCbDIRAC/ProductionManagementSystem/Client/ProductionRepositoryClient.py
@@ -1,15 +1,16 @@
-# $Id: ProductionRepositoryClient.py,v 1.5 2007/06/01 12:49:02 gkuznets Exp $
-__RCSID__ = "$Revision: 1.5 $"
+# $Id: ProductionRepositoryClient.py,v 1.6 2007/06/01 13:08:54 gkuznets Exp $
+__RCSID__ = "$Revision: 1.6 $"
 
 from DIRAC.Core.DISET.RPCClient import RPCClient
 from DIRAC.ConfigurationSystem.Client.ConfigurationData import gConfigurationData
 from DIRAC import gLogger, S_OK, S_ERROR
 
-class ProductionRepositoryClient(RPCClient):
+class ProductionRepositoryClient():
 
   def __init__(self, *args, **kwargs):
     print "KGG"
-    RPCClient.__init__(self, "dips://volhcb03.cern.ch:9131/ProductionManagement/ProductionRepository")
+    client=RPCClient("dips://volhcb03.cern.ch:9131/ProductionManagement/ProductionRepository")
+    print "KGG2"
 
   def publishWorkflow(self, path):
     print path
