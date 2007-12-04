5068e97ae0671e1e5c0f135463a80186cf3fc453
gkuznets <gkuznets@4525493e-7705-40b1-a816-d608a930855b>
2007-12-04 19:20:54 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@5551 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/WorkflowLib/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/WorkflowLib/__init__.py,v 1.1 2007/12/04 19:20:54 gkuznets Exp $
+############################################################
+
+"""
+   DIRAC.WorkflowLib package
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/12/04 19:20:54 gkuznets Exp $"
