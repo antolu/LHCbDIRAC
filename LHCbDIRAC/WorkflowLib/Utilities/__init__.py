022f9c9cd7e5f325a9412950b7fb931b6878b5b3
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2007-12-13 19:41:11 +0000

    WorkflowLib Utilities package.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@5716 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/WorkflowLib/Utilities/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/WorkflowLib/Utilities/__init__.py,v 1.1 2007/12/13 19:41:11 paterson Exp $
+############################################################
+
+"""
+   WorkflowLib.Utilities package
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2007/12/13 19:41:11 paterson Exp $"
