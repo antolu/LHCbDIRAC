b0a9b8a99c176f7431dc79e9b6da6f7eb741ca6d
gkuznets <gkuznets@4525493e-7705-40b1-a816-d608a930855b>
2008-01-20 13:49:33 +0000

    Structure changed
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@5992 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/DB/ProductionRepositoryDB.sql
+++ LHCbDIRAC/ProductionManagementSystem/DB/ProductionRepositoryDB.sql
@@ -21,7 +21,14 @@ FLUSH PRIVILEGES;
 
 -------------------------------------------------------------------------------
 USE ProductionRepositoryDB;
-
+- This table keeps Workflow for the purpose of creating Productions
+- WFName - name of the WF taken from the xml field "name"
+- WFParent - name of the parent Workflow used to create the current one.
+-            taken from the XML field "type"
+- Description - short description of the workflow taken from the field "descr_short" of XML
+- PublisherDN - last persone to update WF
+- PublishingTime - time stamp
+- Body - XML body of the Workflow
 --------------------------------------------------------------------------------
 DROP TABLE IF EXISTS Workflows;
 CREATE TABLE Workflows (
@@ -34,30 +41,71 @@ CREATE TABLE Workflows (
     PRIMARY KEY(WFName)
 );
 
+
+--------------------------------------------------------------------------------
+- This table store Productions
+- ProductionID - production index
+- PRName - name of the Production taken from the xml field "name"
+- PRParent - name of the parent Workflow used to create the current one.
+-            taken from the XML field "type"
+- Description - short description of the workflow taken from the field "descr_short" of XML
+- PublisherDN - last persone to update Production
+- PublishingTime - time stamp
+- Type - type of the workflow. At this point is not clear what to put in there
+-   SIMULATION - Montecarlo production, no input data required
+-   PROCESSING - Processing production, input files required
+-   REPLICATION - data replication production, no body required
+- Status - information about current status of the production
+-   NEW - newly created, equivalent to STOPED
+-   ACTIVE - can submit
+-   STOPPED - stopped by manager
+-   DONE - job limits reached, extension is possible
+-   ERROR - Production with error, equivalent to STOPPED
+-   TERMINATED - stopped, extension impossible
+- Mode - information about submission mode of the production for the submission agent
+-   MANUAL
+-   AUTOMATIC
+- Body - XML body of the Production if required
+- removed from table
+--    JobsTotal INTEGER NOT NULL DEFAULT 0,
+--    JobsSubmitted INTEGER NOT NULL DEFAULT 0,
+--    LastSubmittedJob INTEGER NOT NULL DEFAULT 0,
 --------------------------------------------------------------------------------
 DROP TABLE IF EXISTS Productions;
 CREATE TABLE Productions (
     ProductionID INTEGER NOT NULL AUTO_INCREMENT,
     PRName VARCHAR(255) NOT NULL,
-    PRParent VARCHAR(255) NOT NULL,
+    PRParent VARCHAR(255),
     Description  VARCHAR(255),
     PublisherDN VARCHAR(255) NOT NULL,
     PublishingTime TIMESTAMP,
-    JobsTotal INTEGER NOT NULL DEFAULT 0,
-    JobsSubmitted INTEGER NOT NULL DEFAULT 0,
-    LastSubmittedJob INTEGER NOT NULL DEFAULT 0,
-    Status  VARCHAR(255) NOT NULL,
-    Body BLOB NOT NULL,
+    Type CHAR(16) DEFAULT 'SIMULATION'
+    Status  CHAR(16) DEFAULT 'NEW',
+    Progress FLOAT(6,2) DEFAULT 0.0,
+    Mode CHAR(16) DEFAULT 'MANUAL',
+    LFNMask VARCHAR(255),
+    GroupBySiteFlag ENUM ( "True", "False" ) DEFAULT "True" NOT NULL,
+    GroupSize INTEGER NOT NULL DEFAULT 1,
+    Body BLOB,
     PRIMARY KEY(ProductionID)
 );
 
 --------------------------------------------------------------------------------
---- this will be a dynamic table one per Production
-DROP TABLE IF EXISTS InputViectors_<ProductionID>;
-CREATE TABLE InputViectors (
-    vectorID INTEGER NOT NULL AUTO_INCREMENT,
-    vector_body BLOB NOT NULL,
-    PRIMARY KEY (vectorID)
+- For each row in the Productions table we going to have associated Job table
+- JobID - job index within production
+- WwsStatus - job status in the WMS, for example:
+-   CREATED - newly created job
+-   SUBMITTED - job submitted to WMS
+-   DONE - job finished
+- JobWmsID - index of this job in the WMS
+--------------------------------------------------------------------------------
+DROP TABLE IF EXISTS Jobs_<ProductionID>;
+CREATE TABLE  Jobs_<ProductionID>(
+  JobID INTEGER NOT NULL AUTO_INCREMENT,
+  WmsStatus char(16) DEFAULT 'CREATED',
+  JobWmsID char(16),
+  InputVector BLOB DEFAULT 0,
+  PRIMARY KEY(JobID)
 );
 
 
