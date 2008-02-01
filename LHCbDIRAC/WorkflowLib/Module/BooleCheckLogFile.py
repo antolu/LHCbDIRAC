9bfe8b19bd819bc83b6236b0fcb0dea139b857b2
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2008-02-01 08:12:03 +0000

    BooleCheckLogFile for DIRAC3
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@6208 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/WorkflowLib/Module/BooleCheckLogFile.py
@@ -0,0 +1,44 @@
+import os,string
+
+from WorkflowLib.Module.LHCbCheckLogFile import LHCbCheckLogFile
+from WorkflowLib.Module.CheckLogFile import CheckLogFile
+from DIRAC import                                        S_OK, S_ERROR, gLogger, gConfig
+
+class BooleCheckLogFile(LHCbCheckLogFile):
+
+   def __init__(self):
+      self.argv0     = 'BooleCheckLogFile'
+      self.log = gLogger.getSubLogger("BooleCheckLogFile")
+      self.iClient   = None
+      self.info      = 1
+
+   def checkApplicationLog(self,error):
+      if self.info:
+         print self.argv0 + '.BooleCheckLogFile: analyze %s '%(self.logfile)
+
+      mailto = self.appName.upper()+'_EMAIL'
+
+      last_line = self.grep(self.logfile,'dum','-l')
+      if string.find(last_line,'User defined signal') == -1:
+         mailto = 'DIRAC_EMAIL'
+         result = S_ERROR(mailto + 'USER signal')
+      else:
+         result = S_ERROR(error)
+
+      if result['Message'].find('EMAIL') != -1:
+         self.sendErrorMail(result['Message'])
+
+      return result
+
+#
+#------------------------------------------------------------------------------------------
+#
+if __name__ == '__main__':
+
+   Chk = BooleCheckLogFile()
+
+   rc = Chk.execute()
+
+   print 'main-Boole - rc= ',rc
+
+
