1e3035aec70c2bb46d963839831a17452001104f
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2008-02-01 15:17:28 +0000

    first import
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@6231 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/WorkflowLib/Module/BrunelCheckLogFile.py
@@ -0,0 +1,49 @@
+
+__RCSID__ = "$Id: BrunelCheckLogFile.py,v 1.1 2008/02/01 15:17:28 joel Exp $"
+
+
+import os,string
+
+from DIRAC.Core.Base.Script import parseCommandLine
+parseCommandLine()
+
+from WorkflowLib.Module.LHCbCheckLogFile import LHCbCheckLogFile
+from WorkflowLib.Module.CheckLogFile import CheckLogFile
+from DIRAC import                                        S_OK, S_ERROR, gLogger, gConfig
+
+class BrunelCheckLogFile(LHCbCheckLogFile):
+
+  def __init__(self):
+    self.argv0     = 'BrunelCheckLogFile'
+    self.log = gLogger.getSubLogger("BrunelheckLogFile")
+    self.iClient   = None
+    self.info      = 1
+
+  def checkApplicationLog(self,error):
+    self.log.info(self.argv0 + '.BrunelCheckLogFile: analyze %s '%(self.logfile))
+
+    mailto = self.appName.upper()+'_EMAIL'
+
+    last_line = self.grep(self.logfile,'dum','-l')
+    if string.find(last_line,'User defined signal') != -1:
+      mailto = 'DIRAC_EMAIL'
+      result = S_ERROR(mailto + ' USER signal')
+    else:
+      result = S_ERROR(error)
+
+    if result['Message'].find('EMAIL') != -1:
+      self.sendErrorMail(result['Message'])
+
+    return result
+#
+#------------------------------------------------------------------------------------------
+#
+if __name__ == '__main__':
+
+   Chk = BrunelCheckLogFile()
+
+   rc = Chk.execute()
+
+   print 'main-Boole - rc= ',rc
+
+
