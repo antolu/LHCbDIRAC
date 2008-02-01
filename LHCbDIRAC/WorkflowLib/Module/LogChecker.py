a08b20360dbfe67f6427634aa245776347aabbdb
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2008-02-01 15:00:10 +0000

    minor modification..
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@6227 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/WorkflowLib/Module/LogChecker.py
+++ LHCbDIRAC/WorkflowLib/Module/LogChecker.py
@@ -1,22 +1,16 @@
 ########################################################################
-# $Id: LogChecker.py,v 1.2 2008/02/01 11:41:58 joel Exp $
+# $Id: LogChecker.py,v 1.3 2008/02/01 15:00:10 joel Exp $
 ########################################################################
 """ Script Base Class """
 
-__RCSID__ = "$Id: LogChecker.py,v 1.2 2008/02/01 11:41:58 joel Exp $"
+__RCSID__ = "$Id: LogChecker.py,v 1.3 2008/02/01 15:00:10 joel Exp $"
 
-from DIRAC.Core.Workflow.Parameter import *
-from DIRAC.Core.Workflow.Module import *
-from DIRAC.Core.Workflow.Step import *
-from DIRAC.Core.Workflow.Workflow import *
-from DIRAC.Core.Workflow.WorkflowReader import *
 from WorkflowLib.Module.BooleCheckLogFile import *
 from DIRAC.Core.Utilities.Subprocess                     import shellCall
 from DIRAC.DataManagementSystem.Client.PoolXMLCatalog    import PoolXMLCatalog
 from DIRAC                                               import S_OK, S_ERROR, gLogger, gConfig
 
 import commands, os
-
 
 class LogChecker(object):
 
@@ -45,11 +39,12 @@ class LogChecker(object):
     self.logChecker.logfile = self.logfile
     self.logChecker.appName = self.appName
     self.logChecker.appVersion = self.appVersion
-    self.logChecker.job_id = self.job_id
-    self.logChecker.prod_id = self.prod_id
+    self.logChecker.job_id = self.JOB_ID
+    self.logChecker.prod_id = self.PRODUCTION_ID
     self.logChecker.inputData = self.inputData
     self.logChecker.EMAIL = self.EMAIL
     self.logChecker.OUTPUT_MAX = self.OUTPUT_MAX
     rc = self.logChecker.execute()
     if not rc['OK']:
       self.logChecker.checkApplicationLog(rc['Message'])
+
