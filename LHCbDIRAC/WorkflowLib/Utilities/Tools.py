1efe0edcc333d74c284d8ce436ec90024bbc79d9
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2008-02-14 07:26:34 +0000

    add more tools.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@6546 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/WorkflowLib/Utilities/Tools.py
+++ LHCbDIRAC/WorkflowLib/Utilities/Tools.py
@@ -1,7 +1,9 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/WorkflowLib/Utilities/Tools.py,v 1.1 2008/02/13 14:17:52 joel Exp $
-__RCSID__ = "$Id: Tools.py,v 1.1 2008/02/13 14:17:52 joel Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/WorkflowLib/Utilities/Tools.py,v 1.2 2008/02/14 07:26:34 joel Exp $
+__RCSID__ = "$Id: Tools.py,v 1.2 2008/02/14 07:26:34 joel Exp $"
 
-import re, string
+import os, re, string
+from DIRAC.Core.Utilities.Subprocess                     import shellCall
+
 
 def makeProductionLfn(self,filetuple,mode,prodstring):
     """ Constructs the logical file name according to LHCb conventions.
@@ -31,7 +33,6 @@ def makeProductionPath(self,typeName,mode,prodstring,log=False):
 #  result = '/lhcb/'+mode+'/'+self.CONFIG_NAME+'/'+self.CONFIG_VERSION+'/'+prodstring+'/'
 #  result = '/lhcb/'+self.DataType+'/'+self.YEAR+'/'+self.appType.upper()+'/'+self.CONFIG_NAME+'/'+prodstring+'/'
   result = self.LFN_ROOT+'/'+typeName+'/'+self.CONFIG_NAME+'/'+prodstring+'/'
-
   if log:
     try:
       jobid = int(self.JOB_ID)
@@ -41,3 +42,88 @@ def makeProductionPath(self,typeName,mode,prodstring,log=False):
     result += 'LOG/'+jobindex
 
   return result
+
+def gzip(fname):
+  "interface to the system gzip command"
+  if not os.path.exists(fname):
+    print "gzip failed for file ",fname,": no such file"
+    return 1
+
+  result = shellCall(0,'gzip -f '+fname)
+  status = result['Value'][0]
+  output = result['Value'][1]
+  if status > 0 :
+    print "gzip failed for file ",fname
+    print output
+    return status
+
+  return 0
+
+def gunzip(fname):
+  "interface to the system gunzip command"
+  if not os.path.exists(fname):
+    fname_gz = fname+".gz"
+    if not os.path.exists(fname_gz):
+      print "gunzip failed for file ",fname,": no such file"
+      return 1
+    else:
+      fname = fname_gz
+
+  result = shellCall(0,'gunzip '+fname)
+  status = result['Value'][0]
+  output = result['Value'][1]
+  if status > 0 :
+    print "gunzip failed for file ",fname
+    print output
+    return status
+
+  return 0
+
+def makeIndex():
+  """ Function to make an index of log files in the current run directory"""
+
+  jobname = None
+  files = os.listdir('.')
+  for f in files:
+    if f == 'job.info':
+      jobfile = open(f)
+      jobname = jobfile.readline()
+      jobname = string.strip(jobname)
+
+  if jobname is None:
+    cwd = os.path.basename(os.getcwd())
+    try:
+      run = int(cwd)
+      jobname = str(run)
+    except:
+      print "Wrong current directory",cwd,", index failed"
+      return
+
+  if os.path.exists("index.html"):
+    os.remove("index.html")
+
+  flist = os.listdir(".")
+  index = open("index.html","w")
+  index.write( """
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>\n""")
+  index.write("<title>Logs for Run %s</title>\n" % jobname)
+  index.write( """</head>
+<body text="#000000" bgcolor="#33ffff" link="#000099" vlink="#990099"
+ alink="#000099"> \n
+""")
+  index.write("<h3>Log files for Run %s</h3> \n" % jobname)
+
+  for f in flist:
+    if f != "index.html":
+      index.write( '<a href="%s">%s</a><br> \n' % (f,f))
+
+  index.write( '<a href="job.output">job.output</a><br> \n')
+
+  index.write("""</body>
+</html>""" )
+
+  index.close()
+
+  return 0
