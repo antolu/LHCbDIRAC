49e3263e70ec89297c11e627c83debf73d6bf8bb
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2008-02-14 12:06:31 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@6550 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/BookkeepingSystem/__init__.py
@@ -0,0 +1,5 @@
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/BookkeepingSystem/__init__.py,v 1.1 2008/02/14 12:06:21 joel Exp $
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/02/14 12:06:21 joel Exp $"
+"""
+   DIRAC.BookkeepingSystem package
+"""
