e24c5d480d242479e537d6807d561619a6f4f8a1
gkuznets <gkuznets@4525493e-7705-40b1-a816-d608a930855b>
2008-02-21 11:08:38 +0000

    Method added to copy all attributes from container class into embedded class replacing inheritance mechanism which can not be used in this case
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@6784 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/WorkflowLib/Module/LogChecker.py
+++ LHCbDIRAC/WorkflowLib/Module/LogChecker.py
@@ -1,9 +1,9 @@
 ########################################################################
-# $Id: LogChecker.py,v 1.4 2008/02/07 07:23:13 joel Exp $
+# $Id: LogChecker.py,v 1.5 2008/02/21 11:08:38 gkuznets Exp $
 ########################################################################
 """ Script Base Class """
 
-__RCSID__ = "$Id: LogChecker.py,v 1.4 2008/02/07 07:23:13 joel Exp $"
+__RCSID__ = "$Id: LogChecker.py,v 1.5 2008/02/21 11:08:38 gkuznets Exp $"
 
 from WorkflowLib.Module.BooleCheckLogFile import *
 from WorkflowLib.Module.BrunelCheckLogFile import *
@@ -12,6 +12,7 @@ from WorkflowLib.Module.BrunelCheckLogFile import *
 from DIRAC.Core.Utilities.Subprocess                     import shellCall
 from DIRAC.DataManagementSystem.Client.PoolXMLCatalog    import PoolXMLCatalog
 from DIRAC                                               import S_OK, S_ERROR, gLogger, gConfig
+from WorkflowLib.Utilities.Tools                         import *
 
 import commands, os
 
@@ -39,14 +40,19 @@ class LogChecker(object):
         self.log.error('appName is not defined or known %s' %(self.appName))
         return S_ERROR('appName is not defined or known %s' %(self.appName))
 
-    self.logChecker.logfile = self.appLog
-    self.logChecker.appName = self.appName
-    self.logChecker.appVersion = self.appVersion
-    self.logChecker.job_id = self.JOB_ID
-    self.logChecker.prod_id = self.PRODUCTION_ID
-    self.logChecker.inputData = self.inputData
-    self.logChecker.EMAIL = self.EMAIL
-    self.logChecker.OUTPUT_MAX = self.OUTPUT_MAX
+    #self.logChecker.logfile = self.appLog
+    #self.logChecker.appName = self.appName
+    #self.logChecker.appVersion = self.appVersion
+    #self.logChecker.job_id = self.JOB_ID
+    #self.logChecker.prod_id = self.PRODUCTION_ID
+    #self.logChecker.inputData = self.inputData
+    #self.logChecker.EMAIL = self.EMAIL
+    #self.logChecker.OUTPUT_MAX = self.OUTPUT_MAX
+    
+    # Copy all attributes from container class into embedded class replacing 
+    # inheritance mechanism which can not be used in this case    
+    copyClassAttributes(self, self.logChecker)
+    
     rc = self.logChecker.execute()
     if not rc['OK']:
       self.logChecker.checkApplicationLog(rc['Message'])
