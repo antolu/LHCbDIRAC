0ddba12b8fe2958941fa7585910c3b4e6f15dc77
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2008-02-22 17:20:50 +0000

    Package added
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@6840 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/BookkeepingSystem/Agent/__init__.py
@@ -0,0 +1,5 @@
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/BookkeepingSystem/Agent/__init__.py,v 1.1 2008/02/22 17:20:50 zmathe Exp $
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/02/22 17:20:50 zmathe Exp $"
+"""
+   DIRAC.BookkeepingSystem.Agent package
+"""
