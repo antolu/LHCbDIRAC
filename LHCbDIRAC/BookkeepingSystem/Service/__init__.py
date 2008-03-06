5893d357092b110bd4e14b03fc296758cd07b5d7
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2008-03-06 12:03:17 +0000

    added __ini__ file this package
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@7156 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/BookkeepingSystem/Service/__init__.py
@@ -0,0 +1,5 @@
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/BookkeepingSystem/Service/__init__.py,v 1.1 2008/03/06 12:03:17 zmathe Exp $
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/03/06 12:03:17 zmathe Exp $"
+"""
+   DIRAC.BookkeepingSystem.Agent package
+"""
