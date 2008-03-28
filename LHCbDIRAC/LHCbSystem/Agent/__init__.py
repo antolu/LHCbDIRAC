51267efcc2f44b6e0fc2e60c34dae0188a4ed2a0
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2008-03-28 14:41:14 +0000

    LHCbSystem Agent package.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@7327 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/Agent/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Agent/__init__.py,v 1.1 2008/03/28 14:41:14 paterson Exp $
+############################################################
+
+"""
+   LHCbSystem.Agent package
+
+   LHCb specific  agents for any system (e.g. WMS, DMS ...) can be added here.
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/03/28 14:41:14 paterson Exp $"
