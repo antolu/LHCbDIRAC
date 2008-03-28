c8952da6c40edf41da9ade42f7cec16a700e69ae
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2008-03-28 14:40:55 +0000

    LHCb System
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@7326 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/__init__.py,v 1.1 2008/03/28 14:40:55 paterson Exp $
+############################################################
+
+"""
+   LHCbSystem package
+
+   This contains the LHCb specific plugins for DIRAC.
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/03/28 14:40:55 paterson Exp $"
