2dc2899a58834243b2e4f79b1e361ceb094a9fe9
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2008-04-09 16:13:26 +0000

    Import the new module for checking the LOG file
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@7459 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/WorkflowLib/Module/LogChecker.py
+++ LHCbDIRAC/WorkflowLib/Module/LogChecker.py
@@ -1,12 +1,13 @@
 ########################################################################
-# $Id: LogChecker.py,v 1.6 2008/02/22 15:40:44 joel Exp $
+# $Id: LogChecker.py,v 1.7 2008/04/09 16:13:26 joel Exp $
 ########################################################################
 """ Script Base Class """
 
-__RCSID__ = "$Id: LogChecker.py,v 1.6 2008/02/22 15:40:44 joel Exp $"
+__RCSID__ = "$Id: LogChecker.py,v 1.7 2008/04/09 16:13:26 joel Exp $"
 
 from WorkflowLib.Module.BooleCheckLogFile import *
 from WorkflowLib.Module.BrunelCheckLogFile import *
+from WorkflowLib.Module.AnalyseLogFile import *
 #from WorkflowLib.Module.GaussCheckLogFile import *
 from WorkflowLib.Module.DaVinciCheckLogFile import *
 from DIRAC.Core.Utilities.Subprocess                     import shellCall
@@ -24,6 +25,9 @@ class LogChecker(object):
     self.log = gLogger.getSubLogger("CheckLogFile")
     self.logChecker = None
     self.appName = None
+    self.NUMBER_OF_EVENTS = None
+    self.NUMBER_OF_EVENTS_INPUT = None
+    self.NUMBER_OF_EVENTS_OUTPUT = None
     self.OUTPUT_MAX = 'None'
 
   def execute(self):
@@ -31,11 +35,13 @@ class LogChecker(object):
     if self.appName == 'Boole':
         self.logChecker = BooleCheckLogFile()
     elif  self.appName == 'Brunel':
-        self.logChecker = BrunelCheckLogFile()
+#        self.logChecker = BrunelCheckLogFile()
+        self.logChecker = AnalyseLogFile()
     elif  self.appName == 'Gauss':
         self.logChecker = GaussCheckLogFile()
     elif  self.appName == 'DaVinci':
-        self.logChecker = DaVinciCheckLogFile()
+#        self.logChecker = DaVinciCheckLogFile()
+        self.logChecker = AnalyseLogFile()
     else:
         self.log.error('appName is not defined or known %s' %(self.appName))
         return S_ERROR('appName is not defined or known %s' %(self.appName))
@@ -54,6 +60,8 @@ class LogChecker(object):
     copyClassAttributes(self, self.logChecker, 'logChecker')
 
     rc = self.logChecker.execute()
+    self.NUMBER_OF_EVENTS_INPUT = self.logChecker.NUMBER_OF_EVENTS_INPUT
+    self.NUMBER_OF_EVENTS_OUTPUT = self.logChecker.NUMBER_OF_EVENTS_OUTPUT
     if not rc['OK']:
       self.logChecker.checkApplicationLog(rc['Message'])
 
