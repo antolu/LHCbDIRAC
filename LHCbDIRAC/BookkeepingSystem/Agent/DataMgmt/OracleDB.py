a7edc033f4aef71272d80acc6d2604ccd109bc8a
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2008-04-18 14:39:18 +0000

    stored procedure call implementation
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@7682 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/Agent/DataMgmt/OracleDB.py
+++ LHCbDIRAC/BookkeepingSystem/Agent/DataMgmt/OracleDB.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Id: OracleDB.py,v 1.2 2008/04/18 12:56:04 zmathe Exp $
+# $Id: OracleDB.py,v 1.3 2008/04/18 14:39:18 zmathe Exp $
 ########################################################################
 
 """
@@ -9,7 +9,7 @@
 from DIRAC                 import gLogger, S_OK, S_ERROR
 import cx_Oracle
 
-__RCSID__ = "$Id: OracleDB.py,v 1.2 2008/04/18 12:56:04 zmathe Exp $"
+__RCSID__ = "$Id: OracleDB.py,v 1.3 2008/04/18 14:39:18 zmathe Exp $"
 
 class OracleDB:
   
@@ -18,26 +18,40 @@ class OracleDB:
     self.userName_ = userName
     self.password_ = password
     self.tnsEntry_ = tnsEntry
-    self.db_ = None
-    self.cursor_ = None
-    
-    try:
-      self.db_ = cx_Oracle.Connection(self.userName_, self.password_, self.tnsEntry_)
-      self.cursor_ = cx_Oracle.Cursor(self.db_)
-    except Exception, ex:
-      gLogger.error(ex)    
-  
+
   #############################################################################
-  def getDataBase(self):
-    return self.db_
+  def _createConnection(self):
+    try:
+      return cx_Oracle.Connection(self.userName_, self.password_, self.tnsEntry_)
+    except Exception, ex:
+      gLogger.error(ex)
+    return None    
   
   #############################################################################
   def execute(self, sql, *params):
     results = None
     try:
-      self.cursor_.execute(sql,*params)
-      results = self.cursor_.fetchall()
+      connection = self._createConnection()
+      cursor = cx_Oracle.Cursor(connection)
+      cursor.execute(sql,*params)
+      results = cursor.fetchall()
+      connection.close()
     except Exception, ex:
       gLogger.error(ex)
     return results
+  
+  #############################################################################
+  def executeCursor(self, name, parameters):
+    results = None
+    try:
+      connection = self._createConnection()
+      result = cx_Oracle.Cursor(connection)
+      cursor = cx_Oracle.Cursor(connection)
+      cursor.callproc(name, parameters)
+      results = result.fetchall()
+      connection.close()
+    except Exception, ex:
+      gLogger.error(ex)    
+    return results;
+
   
