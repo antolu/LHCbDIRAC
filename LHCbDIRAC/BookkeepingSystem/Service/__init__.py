97b1fad6d9e0ab1d9e6d8f6b68bf17a74f5dc69d
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2008-04-22 13:58:53 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@7745 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/Service/__init__.py
+++ LHCbDIRAC/BookkeepingSystem/Service/__init__.py
@@ -1,5 +1,6 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/BookkeepingSystem/Service/__init__.py,v 1.1 2008/03/06 12:03:17 zmathe Exp $
-__RCSID__ = "$Id: __init__.py,v 1.1 2008/03/06 12:03:17 zmathe Exp $"
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/BookkeepingSystem/Service/__init__.py,v 1.2 2008/04/22 13:58:53 zmathe Exp $
+__RCSID__ = "$Id: __init__.py,v 1.2 2008/04/22 13:58:53 zmathe Exp $"
 """
+
    DIRAC.BookkeepingSystem.Agent package
 """
