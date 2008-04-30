36bd4716c58c5ae7767505f6e37727e97774042f
atsareg <atsareg@4525493e-7705-40b1-a816-d608a930855b>
2008-04-30 07:26:50 +0000

    Included new Staging job status
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@8734 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/ProductionUpdateAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/ProductionUpdateAgent.py
@@ -1,12 +1,12 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/ProductionUpdateAgent.py,v 1.4 2008/03/02 12:57:20 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/ProductionUpdateAgent.py,v 1.5 2008/04/30 07:26:50 atsareg Exp $
 ########################################################################
 
 """  The Transformation Agent prepares production jobs for processing data
      according to transformation definitions in the Production database.
 """
 
-__RCSID__ = "$Id: ProductionUpdateAgent.py,v 1.4 2008/03/02 12:57:20 atsareg Exp $"
+__RCSID__ = "$Id: ProductionUpdateAgent.py,v 1.5 2008/04/30 07:26:50 atsareg Exp $"
 
 from DIRAC.Core.Base.Agent    import Agent
 from DIRAC                    import S_OK, S_ERROR, gConfig, gLogger, gMonitor
@@ -16,8 +16,8 @@ import os, time
 
 
 AGENT_NAME = 'ProductionManagement/ProductionUpdateAgent'
-UPDATE_STATUS = ['Created','Submitted','Received','Checking','Waiting','Matched','Running','Stalled']
-WAITING_STATUS = ['Submitted','Received','Checking','Waiting']
+UPDATE_STATUS = ['Created','Submitted','Received','Checking','Staging','Waiting','Matched','Running','Stalled','Completed']
+WAITING_STATUS = ['Submitted','Received','Checking','Staging','Waiting']
 RUNNING_STATUS = ['Running','Completed']
 FINAL_STATUS = ['Done','Failed']
 # Update job statuses no more frequently than NEWER minutes
