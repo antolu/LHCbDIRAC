545f3f172e55d26ea50ecb209cf38c22ee910163
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2008-05-08 06:15:45 +0000

    recover missing file
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@8897 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/WorkflowLib/Module/BooleCheckLogFile.py
@@ -0,0 +1,62 @@
+########################################################################
+# $Id: BooleCheckLogFile.py,v 1.9 2008/05/08 06:15:45 rgracian Exp $
+########################################################################
+""" Script Base Class """
+
+
+__RCSID__ = "$Id: BooleCheckLogFile.py,v 1.9 2008/05/08 06:15:45 rgracian Exp $"
+
+
+import os,string
+
+from DIRAC.Core.Base.Script import parseCommandLine
+parseCommandLine()
+
+from WorkflowLib.Module.LHCbCheckLogFile import LHCbCheckLogFile
+from DIRAC.Core.DISET.RPCClient                          import RPCClient
+from WorkflowLib.Module.CheckLogFile import CheckLogFile
+from DIRAC import                                        S_OK, S_ERROR, gLogger, gConfig
+
+class BooleCheckLogFile(LHCbCheckLogFile):
+
+  def __init__(self):
+    self.argv0     = 'BooleCheckLogFile'
+    self.log = gLogger.getSubLogger("BooleCheckLogFile")
+    self.jobID = None
+    if os.environ.has_key('JOBID'):
+      self.jobID = os.environ['JOBID']
+    self.iClient   = None
+    self.jobReport  = RPCClient('WorkloadManagement/JobStateUpdate')
+    self.info      = 1
+
+  def checkApplicationLog(self,error):
+    if not os.path.exists(self.appLog):
+      return S_ERROR(error)
+
+    self.log.info('analyze %s ' % (self.appLog))
+
+    mailto = self.appName.upper()+'_EMAIL'
+
+    last_line = self.grep(self.logfile,'dum','-l')
+    if string.find(last_line,'User defined signal') != -1:
+      mailto = 'DIRAC_EMAIL'
+      result = S_ERROR(mailto + ' USER signal')
+    else:
+      result = S_ERROR(error)
+
+    if result['Message'].find('EMAIL') != -1:
+      self.sendErrorMail(result['Message'])
+
+    return result
+#
+#------------------------------------------------------------------------------------------
+#
+if __name__ == '__main__':
+
+   Chk = BooleCheckLogFile()
+
+   rc = Chk.execute()
+
+   print 'main-Boole - rc= ',rc
+
+
---
+++ LHCbDIRAC/WorkflowLib/Module/BrunelCheckLogFile.py
@@ -0,0 +1,63 @@
+########################################################################
+# $Id: BrunelCheckLogFile.py,v 1.10 2008/05/08 06:15:45 rgracian Exp $
+########################################################################
+""" Script Base Class """
+
+__RCSID__ = "$Id: BrunelCheckLogFile.py,v 1.10 2008/05/08 06:15:45 rgracian Exp $"
+
+
+import os,string
+
+from DIRAC.Core.Base.Script import parseCommandLine
+parseCommandLine()
+
+from WorkflowLib.Module.LHCbCheckLogFile import LHCbCheckLogFile
+from DIRAC.Core.DISET.RPCClient                          import RPCClient
+from WorkflowLib.Module.CheckLogFile import CheckLogFile
+from DIRAC import                                        S_OK, S_ERROR, gLogger, gConfig
+
+class BrunelCheckLogFile(LHCbCheckLogFile):
+
+  def __init__(self):
+    self.argv0     = 'BrunelCheckLogFile'
+    self.log = gLogger.getSubLogger("BrunelheckLogFile")
+    self.jobID = None
+    if os.environ.has_key('JOBID'):
+      self.jobID = os.environ['JOBID']
+    self.jobReport  = RPCClient('WorkloadManagement/JobStateUpdate')
+    self.iClient   = None
+    self.info      = 1
+
+  def checkApplicationLog(self,error):
+
+    if not os.path.exists(self.appLog):
+      return S_ERROR(error)
+
+    self.log.info(' analyze %s '%(self.appLog))
+
+    mailto = self.appName.upper()+'_EMAIL'
+
+    last_line = self.grep(self.appLog,'dum','-l')
+    if string.find(last_line,'User defined signal') != -1:
+      mailto = 'DIRAC_EMAIL'
+      result = S_ERROR(mailto + ' USER signal')
+    else:
+      result = S_ERROR(error)
+
+    if result['Message'].find('EMAIL') != -1:
+      self.sendErrorMail(result['Message'])
+
+    return result
+
+#
+#------------------------------------------------------------------------------------------
+#
+if __name__ == '__main__':
+
+   Chk = BrunelCheckLogFile()
+
+   rc = Chk.execute()
+
+   print 'main-Boole - rc= ',rc
+
+
