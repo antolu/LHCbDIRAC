9c0eee51d6fb4eabb833fec0744e1454664b139d
atsareg <atsareg@4525493e-7705-40b1-a816-d608a930855b>
2008-06-03 14:10:15 +0000

    added setFileStatus() method
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@9471 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/WorkflowLib/Module/ModuleBase.py
+++ LHCbDIRAC/WorkflowLib/Module/ModuleBase.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/WorkflowLib/Module/ModuleBase.py,v 1.2 2008/05/27 13:13:05 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/WorkflowLib/Module/ModuleBase.py,v 1.3 2008/06/03 14:10:15 atsareg Exp $
 ########################################################################
 
 """ ModuleBase - base class for LHCb workflow modules. Defines several
@@ -7,13 +7,13 @@
 
 """
 
-__RCSID__ = "$Id: ModuleBase.py,v 1.2 2008/05/27 13:13:05 atsareg Exp $"
+__RCSID__ = "$Id: ModuleBase.py,v 1.3 2008/06/03 14:10:15 atsareg Exp $"
 
 from DIRAC  import S_OK, S_ERROR, gLogger
 
 class ModuleBase(object):
 
-    
+
   #############################################################################
   def setApplicationStatus(self,status):
     """Wraps around setJobApplicationStatus of state update client
@@ -25,7 +25,7 @@ class ModuleBase(object):
 
     if self.workflow_commons.has_key('JobReport'):
       self.jobReport  = self.workflow_commons['JobReport']
-      
+
     if not self.jobReport:
       return S_OK('No reporting tool given')
     jobStatus = self.jobReport.setApplicationStatus(status)
@@ -42,10 +42,10 @@ class ModuleBase(object):
       return S_OK('JobID not defined') # e.g. running locally prior to submission
 
     self.log.verbose('setJobParameter(%s,%s,%s)' %(self.jobID,name,value))
-    
+
     if self.workflow_commons.has_key('JobReport'):
       self.jobReport  = self.workflow_commons['JobReport']
-      
+
     if not self.jobReport:
       return S_OK('No reporting tool given')
     jobParam = self.jobReport.setJobParameter(str(name),str(value))
@@ -53,3 +53,24 @@ class ModuleBase(object):
       self.log.warn(jobParam['Message'])
 
     return jobParam
+
+  #############################################################################
+  def setFileStatus(self,production,lfn,status):
+    """ set the file status for the given production in the Production Database
+    """
+    self.log.verbose('setFileStatus(%s,%s,%s)' %(production,lfn,status))
+
+    if self.workflow_commons.has_key('FileReport'):
+      self.fileReport = self.workflow_commons['FileReport']
+    else:
+      self.fileReport = None
+
+    if not self.fileReport:
+      return S_OK('No reporting tool given')
+    result = self.fileReport.setFileStatus(production,lfn,status)
+    if not result['OK']:
+      self.log.warn(result['Message'])
+
+    return result
+
+
