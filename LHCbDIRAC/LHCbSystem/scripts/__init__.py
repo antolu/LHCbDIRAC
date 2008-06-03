98f37a3dd6f7142a33904ebe1f1fe946653a6062
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2008-06-03 14:33:25 +0000

    LHCbSystem.scripts package.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@9476 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/scripts/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/scripts/__init__.py,v 1.1 2008/06/03 14:33:25 paterson Exp $
+############################################################
+
+"""
+   LHCbSystem.scripts package
+
+   This contains the LHCb specific DIRAC scripts.
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/06/03 14:33:25 paterson Exp $"
