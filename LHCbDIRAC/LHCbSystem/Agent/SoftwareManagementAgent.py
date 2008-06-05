44286668cdbb655bba759dc7587d4097d7fec435
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2008-06-05 13:55:20 +0000

    Simple agent to manage the software shared areas.  Can also force the deletion of all files in a given shared area via the PurgeFlag config option.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@9521 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/Agent/SoftwareManagementAgent.py
@@ -0,0 +1,138 @@
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Agent/SoftwareManagementAgent.py,v 1.1 2008/06/05 13:55:20 paterson Exp $
+# File :   SoftwareManagementAgent.py
+# Author : Stuart Paterson
+########################################################################
+
+"""  The LHCb SoftwareManagementAgent is designed to manage software caches
+     for DIRAC or Grid sites.  This could be integrated as part of e.g. a
+     SAM framework or can simply be used 'by hand' to install all VO software.
+
+     This depends on a VO software installation plugin module and expects the
+     list of software to be in the DIRAC CS.  A detailed report of any errors is
+     given after all software is attempted to be installed.
+
+"""
+
+__RCSID__ = "$Id: SoftwareManagementAgent.py,v 1.1 2008/06/05 13:55:20 paterson Exp $"
+
+from DIRAC.Core.Base.Agent                                      import Agent
+from DIRAC.Core.Utilities.ModuleFactory                         import ModuleFactory
+from DIRAC                                                      import S_OK, S_ERROR, gConfig, gLogger
+
+import os, sys, re, string, time, shutil
+
+AGENT_NAME = 'LHCb/SoftwareManagementAgent'
+
+class SoftwareManagementAgent(Agent):
+
+  #############################################################################
+  def __init__(self):
+    """ Standard constructor for Agent
+    """
+    Agent.__init__(self,AGENT_NAME)
+
+  #############################################################################
+  def initialize(self):
+    """Sets defaults
+    """
+    result = Agent.initialize(self)
+    self.pollingTime = gConfig.getValue(self.section+'/PollingTime',10)
+    #DEBUGGING OPTIONS
+    self.pollingTime = 5
+    self.maxcount = 1 #AGent.py base class must be fixed for run_once use-case
+    return result
+
+  #############################################################################
+  def execute(self):
+    """The SoftwareManagementAgent execution method.
+    """
+    softwareModule = gConfig.getValue(self.section+'/ModulePath','LHCbSystem.Utilities.CombinedSoftwareInstallation')
+    self.log.info('LHCb Software Distribution module: %s' %(softwareModule))
+    try:
+      exec 'from %s import InstallApplication,RemoveApplication' %softwareModule
+    except Exception,x:
+      return self.__finish('Could not import InstallApplication,RemoveApplication from %s' %(softwareModule))
+
+    activeSoftware = gConfig.getValue(self.section+'/ActiveSoftwareSection','/Operations/SoftwareDistribution/Active')
+    installList = gConfig.getValue(activeSoftware)
+    if not installList:
+      return self.__finish('The active list of software could not be retreived from %s or is null' %(activeSoftware))
+
+    if type(installList)==type(" "):
+      installList = [installList]
+
+    deprecatedSoftware = gConfig.getValue(self.section+'/DeprecatedSoftwareSection','/Operations/SoftwareDistribution/Deprecated')
+    removeList = gConfig.getValue(deprecatedSoftware)
+    if not removeList:
+      removeList=[]
+    if type(removeList)==type(" "):
+      removeList = [removeList]
+
+    localPlatforms = gConfig.getValue('/LocalSite/Architecture')
+    if not localPlatforms:
+      return self.__finish('/LocalSite/Architecture is not defined in the local configuration')
+
+    sharedArea = gConfig.getValue('/LocalSite/SharedArea')
+    if not sharedArea:
+      return self.__finish('/LocalSite/SharedArea is not found, exiting')
+
+    purgeFlag =  gConfig.getValue(self.section+'/PurgeFlag','Disabled')
+    if purgeFlag.lower()=='enabled':
+      self.log.info('Purge flag is ENABLED, removing all software from shared area')
+      for item in os.listdir(sharedArea):
+        self.log.verbose('Removing %s from shared area %s' %(item,sharedArea))
+        if os.path.isdir('%s/%s' %(sharedArea,item)):
+          shutil.rmtree('%s/%s' %(sharedArea,item))
+        else:
+          os.remove('%s/%s' %(sharedArea,item))
+
+    if not os.path.exists(sharedArea):
+      try:
+        os.mkdir(sharedArea)
+      except Exception,x:
+        return self.__finish('Could not create proposed shared area directory:\n%s' %(sharedArea))
+
+    if not type(localPlatforms)==type([]):
+      localPlatforms=[localPlatforms]
+
+    for systemConfig in localPlatforms:
+      self.log.info('The following software packages will be installed by %s:\n%s\nfor system configuration %s' %(AGENT_NAME,string.join(installList,'\n'),systemConfig))
+      for installPackage in installList:
+        appNameVersion = string.split(installPackage,'.')
+        if not len(appNameVersion)==2:
+          return self.__finish('Could not determine name and version of package: %s' %installPackage)
+        self.log.info('Attempting to install %s %s for system configuration %s' %(appNameVersion[0],appNameVersion[1],systemConfig))
+        result = InstallApplication(appNameVersion, systemConfig, sharedArea )
+        #result = True
+        if not result: #or not result['OK']:
+          return self.__finish('Problem during execution:\n %s\n agent is stopped.' %(result))
+        else:
+          self.log.info('Installation of %s %s for %s successful' %(appNameVersion[0],appNameVersion[1],systemConfig))
+
+      for removePackage in removeList:
+        appNameVersion = string.split(removePackage,'.')
+        if not len(appNameVersion)==2:
+          return self.__finish('Could not determine name and version of package: %s' %installPackage)
+        self.log.info('Attempting to remove %s %s for system configuration %s' %(appNameVersion[0],appNameVersion[1],systemConfig))
+        result = RemoveApplication(appNameVersion, systemConfig, sharedArea )
+        result = True
+        if not result: # or not result['OK']:
+          return self.__finish('Problem during execution:\n %s\n agent is stopped.' %(result))
+        else:
+          self.log.info('Removal of %s %s for %s successful' %(appNameVersion[0],appNameVersion[1],systemConfig))
+
+    return S_OK()
+
+  #############################################################################
+  def __finish(self,message):
+    """Force the agent to complete gracefully.
+    """
+    self.log.info('%s will stop with message: \n%s' %(AGENT_NAME,message))
+    fd = open(self.controlDir+'/stop_agent','w')
+    fd.write('%s Stopped at %s [UTC]' % (AGENT_NAME,time.asctime(time.gmtime())))
+    fd.close()
+    self.log.info('%s Stopped at %s [UTC] using %s/stop_agent control flag.' %(AGENT_NAME,time.asctime(time.gmtime()),self.controlDir))
+    return S_OK(message)
+
+  #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
