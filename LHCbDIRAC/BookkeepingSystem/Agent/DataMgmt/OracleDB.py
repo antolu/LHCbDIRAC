a25de124789b1623cd1a9fb32fcccb70cdf8856c
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2008-06-05 13:31:10 +0000

    correction
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@9519 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/Agent/DataMgmt/OracleDB.py
+++ LHCbDIRAC/BookkeepingSystem/Agent/DataMgmt/OracleDB.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Id: OracleDB.py,v 1.21 2008/06/05 13:26:57 zmathe Exp $
+# $Id: OracleDB.py,v 1.22 2008/06/05 13:31:10 zmathe Exp $
 ########################################################################
 
 """
@@ -9,7 +9,7 @@
 from DIRAC                 import gLogger, S_OK, S_ERROR
 import cx_Oracle
 
-__RCSID__ = "$Id: OracleDB.py,v 1.21 2008/06/05 13:26:57 zmathe Exp $"
+__RCSID__ = "$Id: OracleDB.py,v 1.22 2008/06/05 13:31:10 zmathe Exp $"
 
 class OracleDB:
   
@@ -51,128 +51,6 @@ class OracleDB:
     except Exception, ex:
       gLogger.error(ex)    
     return results
-  #############################################################################
-  """
-  def executeAviableEventNbCursor(self):
-    results = None
-    try:
-      result = cx_Oracle.Cursor(self.connection_)
-      cursor = cx_Oracle.Cursor(self.connection_)
-      cursor.callproc('BKK_ORACLE.getAviableEventTypes', [result])
-      results = result.fetchall()
-    except Exception, ex:
-      gLogger.error(ex)    
-    return results;
   
   #############################################################################
-  def executeFullEventTypeAndNumberCursor(self, configName, configVersion, eventTypeId):
-    results = None
-    try:
-      result = cx_Oracle.Cursor(self.connection_)
-      cursor = cx_Oracle.Cursor(self.connection_)
-      cursor.callproc('BKK_ORACLE.getEventTypeAndNumberAll', [configName, configVersion, eventTypeId, result])
-      #cursor.callproc('BKK_ORACLE.getFullEventTypeAndNumber', [configName, configVersion, eventTypeId, result])
-      results = result.fetchall()
-    except Exception, ex:
-      gLogger.error(ex)    
-    return results;
-  
-  #############################################################################
-  def executeFullEventTypeAndNumberCursor1(self, configName, configVersion, fileType, eventTypeId):
-    results = None
-    try:
-      result = cx_Oracle.Cursor(self.connection_)
-      cursor = cx_Oracle.Cursor(self.connection_)
-      cursor.callproc('BKK_ORACLE.getFullEventTypeAndNumber1', [configName, configVersion, fileType, eventTypeId, result])
-      results = result.fetchall()
-    except Exception, ex:
-      gLogger.error(ex)    
-    return results;
-  
-  #############################################################################
-  def executeGetFiles(self, configName, configVersion, fileType, eventTypeId, production):
-    results = None
-    try:
-      result = cx_Oracle.Cursor(self.connection_)
-      cursor = cx_Oracle.Cursor(self.connection_)
-      cursor.callproc('BKK_ORACLE.getFiles', [configName, configVersion, fileType, eventTypeId, production, result])
-      results = result.fetchall()
-    except Exception, ex:
-      gLogger.error(ex)    
-    return results;
-  
-  def executeGetSpecificFiles(self, configName, configVersion, programName, programVersion, fileType, eventTypeId, production):
-    results = None
-    try:
-      result = cx_Oracle.Cursor(self.connection_)
-      cursor = cx_Oracle.Cursor(self.connection_)
-      cursor.callproc('BKK_ORACLE.getSpecificFiles', [configName, configVersion,  programName, programVersion, fileType, eventTypeId, production, result])
-      results = result.fetchall()
-    except Exception, ex:
-      gLogger.error(ex)    
-    return results;
-  
-  #############################################################################
-  def executegetEventTypeAndNumber(self, fileType, eventTypeId):
-    results = None
-    try:
-      result = cx_Oracle.Cursor(self.connection_)
-      cursor = cx_Oracle.Cursor(self.connection_)
-      cursor.callproc('BKK_ORACLE.getEventTypeAndNumber', [fileType, eventTypeId, result])
-      results = result.fetchall()
-    except Exception, ex:
-      gLogger.error(ex)    
-    return results;
-  
-  #############################################################################
-  def executegetAviableEventTypesAndNumberOfEvents(self, configName, configVersion, eventTypeId):
-    results = None
-    try:
-      result = cx_Oracle.Cursor(self.connection_)
-      cursor = cx_Oracle.Cursor(self.connection_)
-      cursor.callproc('BKK_ORACLE.getEventTypeAndNumberAll', [configName, configVersion, eventTypeId, result])
-      results = result.fetchall()
-    except Exception, ex:
-      gLogger.error(ex)    
-    return results;
-  
-  #############################################################################
-  def executegetProcessingPass(self):
-    results = None
-    try:
-      result = cx_Oracle.Cursor(self.connection_)
-      cursor = cx_Oracle.Cursor(self.connection_)
-      cursor.callproc('BKK_ORACLE.getProcessingPass', [result])
-      results = result.fetchall()
-    except Exception, ex:
-      gLogger.error(ex)    
-    return results;
-    
-  #############################################################################  
-  
-  def executegetProductions(self, processingPass):
-    results = None
-    try:
-      result = cx_Oracle.Cursor(self.connection_)
-      cursor = cx_Oracle.Cursor(self.connection_)
-      cursor.callproc('BKK_ORACLE.getProductions', [processingPass, result])
-      results = result.fetchall()
-    except Exception, ex:
-      gLogger.error(ex)    
-    return results;
-  """
-  """
-  #############################################################################  
-  def executegetFilesByProduction(self, production):
-    results = None
-    try:
-      result = cx_Oracle.Cursor(self.connection_)
-      cursor = cx_Oracle.Cursor(self.connection_)
-      cursor.callproc('BKK_ORACLE.getFilesByProduction', [production, result])
-      results = result.fetchall()
-    except Exception, ex:
-      gLogger.error(ex)    
-    return results;
-  """
-  #############################################################################  
   
