1f5c1dabb51faa7b67b583990f4e99494c359bda
atsareg <atsareg@4525493e-7705-40b1-a816-d608a930855b>
2008-06-16 16:33:44 +0000

    added setReplicaProblematic() method
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@9764 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/WorkflowLib/Module/ModuleBase.py
+++ LHCbDIRAC/WorkflowLib/Module/ModuleBase.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/WorkflowLib/Module/ModuleBase.py,v 1.3 2008/06/03 14:10:15 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/WorkflowLib/Module/ModuleBase.py,v 1.4 2008/06/16 16:33:44 atsareg Exp $
 ########################################################################
 
 """ ModuleBase - base class for LHCb workflow modules. Defines several
@@ -7,9 +7,12 @@
 
 """
 
-__RCSID__ = "$Id: ModuleBase.py,v 1.3 2008/06/03 14:10:15 atsareg Exp $"
+__RCSID__ = "$Id: ModuleBase.py,v 1.4 2008/06/16 16:33:44 atsareg Exp $"
 
-from DIRAC  import S_OK, S_ERROR, gLogger
+from DIRAC  import S_OK, S_ERROR, gLogger, gConfig
+from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.RequestManagementSystem.Client.DISETSubRequest import DISETSubRequest
 
 class ModuleBase(object):
 
@@ -72,5 +75,28 @@ class ModuleBase(object):
       self.log.warn(result['Message'])
 
     return result
+
+  #############################################################################
+  def setReplicaProblematic(self,lfn,se,pfn='',reason='Access failure'):
+    """ Set replica status to Problematic in the File Catalog
+    """
+
+    rm = ReplicaManager()
+    site = gConfig.getValue('/LocalSite/Site','Unknown')
+    source = "Job %d at %s" % (self.jobID,site)
+    result = rm.setReplicaProblematic((lfn,pfn,se,reason),source)
+    if not result['OK'] or result['Value']['Failed']:
+      # We have failed the report, let's attempt the Integrity DB faiover
+      integrityDB = RPCClient('DataManagement/DataIntegrity')
+      fileMetadata = {'Prognosis':reason,'LFN':lfn,'PFN':pfn,'StorageElement':se}
+      result = integrityDB.insertProblematic(source,fileMetadata)
+      if not result['OK']:
+        # Add it to the request
+        if self.workflow_commons.has_key('Request'):
+          request  = self.workflow_commons['Request']
+          subrequest = DISETSubRequest(result['rpcStub']).getDictionary()
+          request.addSubRequest(subrequest,'integrity')
+
+    return S_OK()
 
 
