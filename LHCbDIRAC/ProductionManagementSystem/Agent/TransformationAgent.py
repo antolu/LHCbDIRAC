c451c2fda00d83ff59481148af1dbefa803cacc5
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2008-06-20 05:26:54 +0000

    bugfix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10043 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
@@ -1,12 +1,12 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/TransformationAgent.py,v 1.11 2008/06/19 16:12:03 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/TransformationAgent.py,v 1.12 2008/06/20 05:26:54 rgracian Exp $
 ########################################################################
 
 """  The Transformation Agent prepares production jobs for processing data
      according to transformation definitions in the Production database.
 """
 
-__RCSID__ = "$Id: TransformationAgent.py,v 1.11 2008/06/19 16:12:03 atsareg Exp $"
+__RCSID__ = "$Id: TransformationAgent.py,v 1.12 2008/06/20 05:26:54 rgracian Exp $"
 
 from DIRAC.Core.Base.Agent      import Agent
 from DIRAC                      import S_OK, S_ERROR, gConfig, gLogger, gMonitor
@@ -57,7 +57,7 @@ class TransformationAgent(Agent):
     result = self.server.getAllProductions()
     activeTransforms = []
     if not result['OK']:
-      gLogger.error("TransformationAgent.execute: Failed to get transformations.", res['Message'])
+      gLogger.error("TransformationAgent.execute: Failed to get transformations.", result['Message'])
 
     for transDict in result['Value']:
       transID = long(transDict['TransID'])
