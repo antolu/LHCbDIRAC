65622cabc292aaac9f32e675a03fa375847a602d
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2008-07-03 13:08:40 +0000

    bugfix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10299 4525493e-7705-40b1-a816-d608a930855b

--- scripts/install_agent.sh
+++ scripts/install_agent.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/scripts/install_agent.sh,v 1.8 2008/07/02 10:39:45 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/scripts/install_agent.sh,v 1.9 2008/07/03 13:08:40 rgracian Exp $
 # File :   install_agent.sh
 # Author : Ricardo Graciani
 ########################################################################
 #
 DESTDIR=`dirname $0`
 DESTDIR=`cd $DESTDIR/../..; pwd`
+
 #
 source $DESTDIR/bashrc
 System=$1
@@ -24,25 +25,25 @@ elif [ ! -d $AgentDir ] ; then
 fi
 mkdir -p $AgentDir/log
 #
-cat > $AgentDir/log/config << EOF
+cat >> $AgentDir/log/config << EOF
 s10000000
 n100
 EOF
-cat > $AgentDir/log/run << EOF
+cat >> $AgentDir/log/run << EOF
 #!/bin/bash
 #
 source $DESTDIR/bashrc
 #
 exec svlogd .
 EOF
-cat > $AgentDir/run << EOF
+cat >> $AgentDir/run << EOF
 #!/bin/bash
 #
 source $DESTDIR/bashrc
 #
 exec 2>&1
 #
-exec dirac-agent -o LogLevel=VERBOSE $System/$Agent \$DIRAC/etc/${System}_${Agent}.cfg
+exec dirac-agent $System/$Agent \$DIRAC/etc/${System}_${Agent}.cfg -o LogLevel=$LOGLEVEL
 EOF
 chmod +x $AgentDir/log/run $AgentDir/run
 
