0940760e451360ee98df213b04adb3cfb7c21008
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2008-07-04 08:20:31 +0000

    check InputData
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10318 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/WorkflowLib/Module/GaudiApplication.py
+++ LHCbDIRAC/WorkflowLib/Module/GaudiApplication.py
@@ -1,9 +1,9 @@
 ########################################################################
-# $Id: GaudiApplication.py,v 1.55 2008/07/04 05:39:59 joel Exp $
+# $Id: GaudiApplication.py,v 1.56 2008/07/04 08:20:31 joel Exp $
 ########################################################################
 """ Gaudi Application Class """
 
-__RCSID__ = "$Id: GaudiApplication.py,v 1.55 2008/07/04 05:39:59 joel Exp $"
+__RCSID__ = "$Id: GaudiApplication.py,v 1.56 2008/07/04 08:20:31 joel Exp $"
 
 from DIRAC.Core.Utilities.Subprocess                     import shellCall
 from DIRAC.DataManagementSystem.Client.PoolXMLCatalog    import PoolXMLCatalog
@@ -237,11 +237,12 @@ class GaudiApplication(ModuleBase):
     if self.step_commons.has_key('inputDataType'):
        self.inputDataType = self.step_commons['inputDataType']
 
+    if self.workflow_commons.has_key('InputData'):
+       self.inputData = self.workflow_commons['InputData']
+
     if self.step_commons.has_key('inputData'):
        self.inputData = self.step_commons['inputData']
 
-    if self.workflow_commons.has_key('InputData'):
-       self.InputData = self.workflow_commons['InputData']
 
   #############################################################################
   def execute(self):
