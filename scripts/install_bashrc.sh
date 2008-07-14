ccf27085ace3a44200cd91005d28d9cc12202e1d
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2008-07-14 21:24:46 +0000

    keep full Grid Environment, and make sure dirac bin and scripts directory are fisrt in the path
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10640 4525493e-7705-40b1-a816-d608a930855b

--- scripts/install_bashrc.sh
+++ scripts/install_bashrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/scripts/install_bashrc.sh,v 1.1 2008/04/19 10:45:33 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/scripts/install_bashrc.sh,v 1.2 2008/07/14 21:24:46 rgracian Exp $
 # File :   install_bashrc.sh
 # Author : Ricardo Graciani
 ########################################################################
@@ -22,26 +22,30 @@ if [ -e /afs/cern.ch/lhcb/scripts/GridEnv.sh ] ; then
   source /afs/cern.ch/lhcb/scripts/GridEnv.sh
   #
   # Forces their python first in the path, remove it.
-  LCGPYTHON=\`which python\`
-  LCGPATH=\`dirname \$LCGPYTHON\`
-  PATH=\`echo \$PATH | awk -F: '{\$1="";OFS=":";print \$0}'\`
+  # LCGPYTHON=\`which python\`
+  # LCGPATH=\`dirname \$LCGPYTHON\`
+  # PATH=\`echo \$PATH | awk -F: '{\$1="";OFS=":";print \$0}'\`
 fi
 
 DIRAC=$DESTDIR/pro
 DIRACBIN=$DESTDIR/pro/$ARCH/bin
 DIRACSCRIPTS=$DESTDIR/pro/scripts
 DIRACLIB=$DESTDIR/pro/$ARCH/lib
+dir=`echo $DESTDIR/pro/$DIRACARCH/bin | sed 's/\//\\\\\//g'`
+PATH=`echo $PATH | sed "s/$dir://"`
+dir=`echo $DESTDIR/pro/scripts | sed 's/\//\\\\\//g'`
+PATH=`echo $PATH | sed "s/$dir://"`
 ( echo \$PATH | grep -q \$DIRACBIN ) || export PATH=\$DIRACBIN:\$PATH
 ( echo \$PATH | grep -q \$DIRACSCRIPTS ) || export PATH=\$DIRACSCRIPTS:\$PATH
 
 [ -z "\$LD_LIBRARY_PATH" ] && export LD_LIBRARY_PATH=\$DIRACLIB:\$DIRACLIB/mysql
 ( echo \$LD_LIBRARY_PATH | grep -q \$DIRACLIB ) || export LD_LIBRARY_PATH=\$DIRACLIB:\$DIRACLIB/mysql:\$LD_LIBRARY_PATH
 
-export PYTHONPATH=\$DIRAC:
-( echo \$PYTHONPATH | grep -q \$DIRAC: ) || export PYTHONPATH=\$DIRAC:\$PYTHONPATH
-( echo \$PYTHONPATH | grep -q \$DIRACSCRIPTS ) || export PYTHONPATH=\$DIRACSCRIPTS:\$PYTHONPATH
-
-( echo \$PATH | grep -q /usr/local/bin ) || export PATH=\$PATH:/usr/local/bin
+# export PYTHONPATH=\$DIRAC:
+# ( echo \$PYTHONPATH | grep -q \$DIRAC: ) || export PYTHONPATH=\$DIRAC:\$PYTHONPATH
+# ( echo \$PYTHONPATH | grep -q \$DIRACSCRIPTS ) || export PYTHONPATH=\$DIRACSCRIPTS:\$PYTHONPATH
+# 
+# ( echo \$PATH | grep -q /usr/local/bin ) || export PATH=\$PATH:/usr/local/bin
 
 EOF
 
