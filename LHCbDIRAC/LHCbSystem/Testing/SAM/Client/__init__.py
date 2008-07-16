b5433eb50f61aa733528ee0e53e60e945c80ec69
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2008-07-16 21:05:45 +0000

    LHCbSystem.Testing.SAM.client package
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10707 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/Testing/SAM/Client/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Testing/SAM/Client/__init__.py,v 1.1 2008/07/16 21:05:45 paterson Exp $
+############################################################
+
+"""
+   LHCbSystem.Testing.SAM.client package
+
+   This contains the LHCb specific SAM job modules.
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/07/16 21:05:45 paterson Exp $"
