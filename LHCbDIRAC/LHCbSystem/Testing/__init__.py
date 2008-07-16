3c8da072baaa5d8167522d2b43bfb0654dd45c85
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2008-07-16 21:04:04 +0000

    LHCbSystem.Testing package
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10704 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/Testing/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Testing/__init__.py,v 1.1 2008/07/16 21:04:04 paterson Exp $
+############################################################
+
+"""
+   LHCbSystem.Testing package
+
+   This contains the LHCb testing suite.
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/07/16 21:04:04 paterson Exp $"
