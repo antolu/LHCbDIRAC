5d69dacd67424501b5f6579a5235d1ea17519914
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2008-07-16 21:04:47 +0000

    LHCbSystem.Testing.SAM package
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10705 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/Testing/SAM/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Testing/SAM/__init__.py,v 1.1 2008/07/16 21:04:47 paterson Exp $
+############################################################
+
+"""
+   LHCbSystem.Testing.SAM package
+
+   This contains the LHCb specific SAM suite responsible for site functional tests.
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/07/16 21:04:47 paterson Exp $"
