37cde4b170b140706989a26eb2b279c6abbfc7d3
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2008-07-17 14:15:23 +0000

    remove extra echo
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10737 4525493e-7705-40b1-a816-d608a930855b

--- scripts/install_bashrc.sh
+++ scripts/install_bashrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/scripts/install_bashrc.sh,v 1.4 2008/07/17 13:58:06 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/scripts/install_bashrc.sh,v 1.5 2008/07/17 14:15:23 rgracian Exp $
 # File :   install_bashrc.sh
 # Author : Ricardo Graciani
 ########################################################################
@@ -39,9 +39,7 @@ DIRACLIB=$DESTDIR/pro/$ARCH/lib
 sDIRACBIN="`echo $DESTDIR/pro/$ARCH/bin | sed 's/\//\\\\\//g'`"
 sDIRACSCRIPTS="`echo $DESTDIR/pro/scripts | sed 's/\//\\\\\//g'`"
 PATH=\`echo \$PATH | sed "s/\$sDIRACBIN://g"\`
-echo \$PATH
 PATH=\`echo \$PATH | sed "s/\$sDIRACSCRIPTS://g"\`
-echo \$PATH
 
 ( echo \$PATH | grep -q \$DIRACBIN ) || export PATH=\$DIRACBIN:\$PATH
 ( echo \$PATH | grep -q \$DIRACSCRIPTS ) || export PATH=\$DIRACSCRIPTS:\$PATH
