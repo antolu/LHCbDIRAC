64a9cbd5b2f6d6434a0d0dbf3d0cf8d26f978263
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2008-07-17 13:43:53 +0000

    update
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10734 4525493e-7705-40b1-a816-d608a930855b

--- scripts/install_bashrc.sh
+++ scripts/install_bashrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/scripts/install_bashrc.sh,v 1.2 2008/07/14 21:24:46 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/scripts/install_bashrc.sh,v 1.3 2008/07/17 13:43:53 rgracian Exp $
 # File :   install_bashrc.sh
 # Author : Ricardo Graciani
 ########################################################################
@@ -18,6 +18,9 @@ rm -f bashrc
 cat > bashrc << EOF
 export PYTHONUNBUFFERED=yes
 export PYTHONOPTIMIZE=x
+export HOME=$HOME
+PYTHONPATH=""
+LD_LIBRARY_PATH=""
 if [ -e /afs/cern.ch/lhcb/scripts/GridEnv.sh ] ; then
   source /afs/cern.ch/lhcb/scripts/GridEnv.sh
   #
@@ -25,27 +28,29 @@ if [ -e /afs/cern.ch/lhcb/scripts/GridEnv.sh ] ; then
   # LCGPYTHON=\`which python\`
   # LCGPATH=\`dirname \$LCGPYTHON\`
   # PATH=\`echo \$PATH | awk -F: '{\$1="";OFS=":";print \$0}'\`
+  PYTHONPATH=\`echo \$PYTHONPATH | awk -F: '{\$1="" ; \$2="" ; OFS=":";print \$0}'\`
 fi
 
 DIRAC=$DESTDIR/pro
 DIRACBIN=$DESTDIR/pro/$ARCH/bin
 DIRACSCRIPTS=$DESTDIR/pro/scripts
 DIRACLIB=$DESTDIR/pro/$ARCH/lib
-dir=`echo $DESTDIR/pro/$DIRACARCH/bin | sed 's/\//\\\\\//g'`
-PATH=`echo $PATH | sed "s/$dir://"`
-dir=`echo $DESTDIR/pro/scripts | sed 's/\//\\\\\//g'`
-PATH=`echo $PATH | sed "s/$dir://"`
+
+sDIRACBIN=`echo $DESTDIR/pro/$ARCH/bin | sed 's/\//\\\\\//g'`
+sDIRACSCRIPTS=`echo $DESTDIR/pro/scripts | sed 's/\//\\\\\//g'`
+PATH=\`echo \$PATH | sed "s/${sDIRACBIN}://g"\`
+PATH=\`echo \$PATH | sed "s/${sDIRACSCRIPTS}://g"\`
+
 ( echo \$PATH | grep -q \$DIRACBIN ) || export PATH=\$DIRACBIN:\$PATH
 ( echo \$PATH | grep -q \$DIRACSCRIPTS ) || export PATH=\$DIRACSCRIPTS:\$PATH
 
 [ -z "\$LD_LIBRARY_PATH" ] && export LD_LIBRARY_PATH=\$DIRACLIB:\$DIRACLIB/mysql
 ( echo \$LD_LIBRARY_PATH | grep -q \$DIRACLIB ) || export LD_LIBRARY_PATH=\$DIRACLIB:\$DIRACLIB/mysql:\$LD_LIBRARY_PATH
 
-# export PYTHONPATH=\$DIRAC:
-# ( echo \$PYTHONPATH | grep -q \$DIRAC: ) || export PYTHONPATH=\$DIRAC:\$PYTHONPATH
-# ( echo \$PYTHONPATH | grep -q \$DIRACSCRIPTS ) || export PYTHONPATH=\$DIRACSCRIPTS:\$PYTHONPATH
-# 
-# ( echo \$PATH | grep -q /usr/local/bin ) || export PATH=\$PATH:/usr/local/bin
+( echo \$PYTHONPATH | grep -q \$DIRAC: ) || export PYTHONPATH=\$DIRAC:\$PYTHONPATH
+( echo \$PYTHONPATH | grep -q \$DIRACSCRIPTS ) || export PYTHONPATH=\$DIRACSCRIPTS:\$PYTHONPATH
+
+( echo \$PATH | grep -q /usr/local/bin ) || export PATH=\$PATH:/usr/local/bin
 
 EOF
 
