c014f39a50e74fe71296fed04c3d913c02e4686c
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2008-07-18 13:41:54 +0000

    Added calls for future modules.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10782 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/LHCbSystem/Testing/SAM/Client/LHCbSAMJob.py
+++ LHCbDIRAC/LHCbSystem/Testing/SAM/Client/LHCbSAMJob.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Testing/SAM/Client/LHCbSAMJob.py,v 1.1 2008/07/16 21:13:15 paterson Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Testing/SAM/Client/LHCbSAMJob.py,v 1.2 2008/07/18 13:41:54 paterson Exp $
 # File :   LHCbSAMJob.py
 # Author : Stuart Paterson
 ########################################################################
@@ -16,22 +16,24 @@
 
    An example SAM Test script would be::
 
-     from DIRAC.Interfaces.API.Dirac import Dirac
-     from DIRAC.LHCbSystem.Testing.SAM.Client.LHCbSAMJob import LHCbSAMJob
+    from DIRAC.Interfaces.API.Dirac import Dirac
+    from DIRAC.LHCbSystem.Testing.SAM.Client.LHCbSAMJob import LHCbSAMJob
 
-     j = LHCbSAMJob()
-     j.setCPUTime(5000)
-    #j.setDestination('LCG.CERN.ch')
-     j.setSharedAreaLock(forceDeletion=False,enableFlag=False)
-     j.checkSystemConfiguration(False)
-     j.checkSiteQueues(False)
-
-     dirac = Dirac()
-     jobID = dirac.submit(j)
-     print 'Submission Result: ',jobID
+    j = LHCbSAMJob()
+    j.setDestination('LCG.PIC.es')
+    j.setSharedAreaLock(forceDeletion=False,enableFlag=False)
+    j.checkSystemConfiguration(enableFlag=False)
+    j.checkSiteQueues(enableFlag=False)
+    j.installSoftware(forceDeletion=False,enableFlag=False)
+    j.testApplications(enableFlag=False)
+    j.finalizeAndPublish(logUpload=False,publishResults=False,enableFlag=False)
+    dirac = Dirac()
+    print j._toJDL()
+    jobID = dirac.submit(j,mode='Local')
+    print 'Submission Result: ',jobID
 """
 
-__RCSID__ = "$Id: LHCbSAMJob.py,v 1.1 2008/07/16 21:13:15 paterson Exp $"
+__RCSID__ = "$Id: LHCbSAMJob.py,v 1.2 2008/07/18 13:41:54 paterson Exp $"
 
 import string, re, os, time, shutil, types, copy
 
@@ -60,6 +62,7 @@ class LHCbSAMJob(Job):
     self.samDefaultCPUTime = gConfig.getValue('/Operations/SAM/CPUTime',50000)
     self.samPlatform = gConfig.getValue('/Operations/SAM/Platform','gLite')
     self.samOutputFiles = gConfig.getValue('/Operations/SAM/OutputSandbox',['*.log'])
+    self.appTestPath = '/Operations/SAM/TestApplications'
     self.__setDefaults()
 
   #############################################################################
@@ -102,6 +105,9 @@ class LHCbSAMJob(Job):
     self.workflow.addStep(step)
     stepPrefix = '%s_' % stepName
     self.currentStepPrefix = stepPrefix
+
+    if forceDeletion:
+      self._addJDLParameter('LockRemovalFlag','True')
 
     # Define Step and its variables
     stepInstance = self.workflow.createStepInstance(stepDefn,stepName)
@@ -239,5 +245,187 @@ class LHCbSAMJob(Job):
     step.addParameter(Parameter("enable","","bool","","",False, False, "enable flag"))
     return step
 
+  #############################################################################
+  def installSoftware(self,forceDeletion=False,enableFlag=True):
+    """Helper function.
+
+       Add the SoftwareInstallation test.
+
+       Example usage:
+
+       >>> job = LHCbSAMJob()
+       >>> job.installSoftware(enableFlag='True')
+
+       @param enableFlag: Flag to enable / disable calls for testing purposes
+       @type enableFlag: boolean
+       @param forceDeletion: Flag to force shared area deletion e.g. rm -rf *
+       @type forceDeletion: boolean
+    """
+    if not enableFlag in [True,False] or not forceDeletion in [True,False]:
+      raise TypeError,'Expected boolean value for SAM lock test flags'
+
+    self.gaudiStepCount +=1
+    stepNumber = self.gaudiStepCount
+    stepDefn = '%sStep%s' %('SAM',stepNumber)
+    step =  self.__getSoftwareInstallationStep(stepDefn)
+
+    self._addJDLParameter('SoftwareInstallationTest',str(enableFlag))
+    stepName = 'Run%sStep%s' %('SAM',stepNumber)
+    self.addToOutputSandbox.append('*.log')
+    self.workflow.addStep(step)
+    stepPrefix = '%s_' % stepName
+    self.currentStepPrefix = stepPrefix
+
+    # Define Step and its variables
+    stepInstance = self.workflow.createStepInstance(stepDefn,stepName)
+    stepInstance.setValue("enable",enableFlag)
+    stepInstance.setValue("purgeSharedAreaFlag",forceDeletion)
+
+
+  #############################################################################
+  def __getSoftwareInstallationStep(self,name='SoftwareInstallation'):
+    """Internal function.
+
+        This method controls the definition for a SoftwareInstallation step.
+    """
+    # Create the SoftwareInstallation module first
+    moduleName = 'SoftwareInstallation'
+    module = ModuleDefinition(moduleName)
+    module.setDescription('A module to install LHCb software')
+    body = 'from DIRAC.LHCbSystem.Testing.SAM.Modules.SoftwareInstallation import SoftwareInstallation\n'
+    module.setBody(body)
+    # Create Step definition
+    step = StepDefinition(name)
+    step.addModule(module)
+    moduleInstance = step.createModuleInstance('SoftwareInstallation',name)
+    # Define step parameters
+    step.addParameter(Parameter("enable","","bool","","",False, False, "enable flag"))
+    step.addParameter(Parameter("purgeSharedAreaFlag","","bool","","",False, False, "Remove all software in shared area"))
+    return step
+
+  #############################################################################
+  def testApplications(self,enableFlag=True):
+    """Helper function.
+
+       Add the TestApplications step.
+
+       Example usage:
+
+       >>> job = LHCbSAMJob()
+       >>> job.testApplications('True')
+
+       @param enableFlag: Flag to enable / disable calls for testing purposes
+       @type enableFlag: boolean
+
+    """
+    if not enableFlag in [True,False]:
+      raise TypeError,'Expected boolean value for enableFlag'
+
+    result = gConfig.getOptionsDict(self.appTestPath)
+    if not result['OK']:
+      raise TypeError,'Section %s is not defined or could not be retrieved' %self.appTestPath
+
+    self.log.verbose('Will generate tests for: %s' %(result['Value']))
+    for testName,appNameVersion in result['Value'].items():
+      self.gaudiStepCount +=1
+      stepNumber = self.gaudiStepCount
+      stepDefn = '%sStep%s' %('SAM',stepNumber)
+      step =  self.__getTestApplicationsStep(stepDefn)
+
+      self._addJDLParameter('TestApplication%s' %(appNameVersion.replace('.','')),str(enableFlag))
+      stepName = 'Run%sStep%s' %('SAM',stepNumber)
+      self.addToOutputSandbox.append('*.log')
+      self.workflow.addStep(step)
+      stepPrefix = '%s_' % stepName
+      self.currentStepPrefix = stepPrefix
+
+      # Define Step and its variables
+      stepInstance = self.workflow.createStepInstance(stepDefn,stepName)
+      stepInstance.setValue("enable",enableFlag)
+      stepInstance.setValue("samTestName",testName)
+      stepInstance.setValue("appNameVersion",appNameVersion)
+
+  #############################################################################
+  def __getTestApplicationsStep(self,name='TestApplications'):
+    """Internal function.
+
+        This method controls the definition for a TestApplications step.
+    """
+    # Create the GaudiApplication module first
+    moduleName = 'TestApplications'
+    module = ModuleDefinition(moduleName)
+    module.setDescription('A module to check the LHCb queues for the given CE')
+    body = 'from DIRAC.LHCbSystem.Testing.SAM.Modules.TestApplications import TestApplications\n'
+    module.setBody(body)
+    # Create Step definition
+    step = StepDefinition(name)
+    step.addModule(module)
+    moduleInstance = step.createModuleInstance('TestApplications',name)
+    # Define step parameters
+    step.addParameter(Parameter("enable","","bool","","",False, False, "enable flag"))
+    step.addParameter(Parameter("samTestName","","string","","",False, False, "TestApplication SAM Test Name"))
+    step.addParameter(Parameter("appNameVersion","","string","","",False, False, "Appliciation name and version"))
+    return step
+
+  #############################################################################
+  def finalizeAndPublish(self,logUpload=True,publishResults=True,enableFlag=True):
+    """Helper function.
+
+       Add the SAM Finalization module step.
+
+       Example usage:
+
+       >>> job = LHCbSAMJob()
+       >>> job.finalizeAndPublish(enableFlag='True')
+
+       @param enableFlag: Flag to enable / disable calls for testing purposes
+       @type enableFlag: boolean
+       @param logUpload: Flag to enable / disable log files upload
+       @type logUpload: boolean
+       @param publishResults: Flag to enable / disable publishing of results to SAM DB
+       @type publishResults: boolean
+    """
+    if not enableFlag in [True,False] or not logUpload in [True,False] or not publishResults in [True,False]:
+      raise TypeError,'Expected boolean value for SAM lock test flags'
+
+    self.gaudiStepCount +=1
+    stepNumber = self.gaudiStepCount
+    stepDefn = '%sStep%s' %('SAM',stepNumber)
+    step =  self.__getSAMFinalizationStep(stepDefn)
+
+    self._addJDLParameter('FinalizeAndPublishTest',str(enableFlag))
+    stepName = 'Run%sStep%s' %('SAM',stepNumber)
+    self.addToOutputSandbox.append('*.log')
+    self.workflow.addStep(step)
+    stepPrefix = '%s_' % stepName
+    self.currentStepPrefix = stepPrefix
+
+    # Define Step and its variables
+    stepInstance = self.workflow.createStepInstance(stepDefn,stepName)
+    stepInstance.setValue("enable",enableFlag)
+    stepInstance.setValue("publishResultsFlag",publishResults)
+    stepInstance.setValue("uploadLogsFlag",logUpload)
+
+  #############################################################################
+  def __getSAMFinalizationStep(self,name='SAMFinalization'):
+    """Internal function.
+
+        This method controls the definition for a SAMFinalization step.
+    """
+    # Create the SAMFinalization module first
+    moduleName = 'SAMFinalization'
+    module = ModuleDefinition(moduleName)
+    module.setDescription('A module for LHCb SAM job finalization, reports to SAM DB')
+    body = 'from DIRAC.LHCbSystem.Testing.SAM.Modules.SAMFinalization import SAMFinalization\n'
+    module.setBody(body)
+    # Create Step definition
+    step = StepDefinition(name)
+    step.addModule(module)
+    moduleInstance = step.createModuleInstance('SAMFinalization',name)
+    # Define step parameters
+    step.addParameter(Parameter("enable","","bool","","",False, False, "enable flag"))
+    step.addParameter(Parameter("publishResultsFlag","","bool","","",False, False, "Flag to trigger publishing of results to SAM DB"))
+    step.addParameter(Parameter("uploadLogsFlag","","bool","","",False, False, "Flag to trigger upload of SAM logs to LogSE"))
+    return step
 
   #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
