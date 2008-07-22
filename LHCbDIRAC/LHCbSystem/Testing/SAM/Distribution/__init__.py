e23e865f197410b4f4b0ddb3144616a33f9e7bd4
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2008-07-22 10:55:20 +0000

    LHCbSystem.Testing.SAM.Distribution package.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@10843 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/Testing/SAM/Distribution/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Testing/SAM/Distribution/__init__.py,v 1.1 2008/07/22 10:55:20 paterson Exp $
+############################################################
+
+"""
+   LHCbSystem.Testing.SAM.Distribution package
+
+   This contains the stable SAM client tarball.
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2008/07/22 10:55:20 paterson Exp $"
