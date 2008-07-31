dd2f699dd8b19197d4fb265dac20ef0f1d7b9af0
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2008-07-31 10:30:47 +0000

    Environment scripts
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@11190 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/Environments/AdminEnv.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Environments/AdminEnv.sh,v 1.1 2008/07/31 10:30:47 rgracian Exp $
+# File :   AdminEnv.sh
+# Author : Ricardo Graciani
+########################################################################
+__RCSID__="$Id: AdminEnv.sh,v 1.1 2008/07/31 10:30:47 rgracian Exp $"
+__VERSION__="$Revision: 1.1 $"
+
+export DIRACROOT=`dirname $0`
+export DIRACROOT=`cd $DIRACROOT ; pwd`
+
+echo
+echo Running DIRAC AdminEnv.sh version $__VERSION__ with DIRACROOT=$DIRACROOT
+echo
+
+hostname | grep -q ".cern.ch" && source /afs/cern.ch/lhcb/scripts/GridEnv.sh
+
+if [ ! -e ~/.lcgpasswd ] ; then
+  echo -n "Certificate password: "
+  stty -echo
+  read userPasswd
+  stty echo
+else
+  userPasswd=`cat ~/.lcgpasswd`
+fi
+
+group=diracAdmin
+
+export DPLAT=`$DIRACROOT/scripts/platform.py`
+export PATH=$DIRACROOT/$DPLAT/bin:$DIRACROOT/scripts:$PATH
+export LD_LIBRARY_PATH=$DIRACROOT/$DPLAT/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$DIRACROOT
+
+if ! echo $userPasswd | lhcb-proxy-init -d -g $group --pwstdin --valid 240:0 ; then
+  echo "You aren't allowed in the DIRAC $group group!"
+  exit 1
+fi
+
+export PS1="(\[\e[1;34m\]DIRAC3-User\[\e[0m\])[\u@\h \w]\$ "
+bash -p
---
+++ LHCbDIRAC/LHCbSystem/Environments/UserEnv.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Environments/UserEnv.sh,v 1.1 2008/07/31 10:30:47 rgracian Exp $
+# File :   UserEnv.sh
+# Author : Ricardo Graciani
+########################################################################
+__RCSID__="$Id: UserEnv.sh,v 1.1 2008/07/31 10:30:47 rgracian Exp $"
+__VERSION__="$Revision: 1.1 $"
+
+export DIRACROOT=`dirname $0`
+export DIRACROOT=`cd $DIRACROOT ; pwd`
+
+echo
+echo Running DIRAC UserEnv.sh version $__VERSION__ with DIRACROOT=$DIRACROOT
+echo
+
+hostname | grep -q ".cern.ch" && source /afs/cern.ch/lhcb/scripts/GridEnv.sh
+
+if [ ! -e ~/.lcgpasswd ] ; then
+  echo -n "Certificate password: "
+  stty -echo
+  read userPasswd
+  stty echo
+else
+  userPasswd=`cat ~/.lcgpasswd`
+fi
+
+group=lhcb_user
+
+export DPLAT=`$DIRACROOT/scripts/platform.py`
+export PATH=$DIRACROOT/$DPLAT/bin:$DIRACROOT/scripts:$PATH
+export LD_LIBRARY_PATH=$DIRACROOT/$DPLAT/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$DIRACROOT
+
+if ! echo $userPasswd | lhcb-proxy-init -d -g $group --pwstdin; then
+  echo "You aren't allowed in the DIRAC $group group!"
+  exit 1
+fi
+
+export PS1="(\[\e[1;32m\]DIRAC3-User\[\e[0m\])[\u@\h \w]\$ "
+bash -p
