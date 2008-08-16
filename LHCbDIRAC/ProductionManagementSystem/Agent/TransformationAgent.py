702f17be1efc16510390783b5ca33dfdc9195034
atsareg <atsareg@4525493e-7705-40b1-a816-d608a930855b>
2008-08-16 14:31:47 +0000

    added ancestor checking facility
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@11565 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
@@ -1,17 +1,18 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/TransformationAgent.py,v 1.17 2008/08/14 22:43:15 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/TransformationAgent.py,v 1.18 2008/08/16 14:31:47 atsareg Exp $
 ########################################################################
 
 """  The Transformation Agent prepares production jobs for processing data
      according to transformation definitions in the Production database.
 """
 
-__RCSID__ = "$Id: TransformationAgent.py,v 1.17 2008/08/14 22:43:15 atsareg Exp $"
+__RCSID__ = "$Id: TransformationAgent.py,v 1.18 2008/08/16 14:31:47 atsareg Exp $"
 
 from DIRAC.Core.Base.Agent      import Agent
 from DIRAC                      import S_OK, S_ERROR, gConfig, gLogger, gMonitor
 from DIRAC.Core.DISET.RPCClient import RPCClient
 from DIRAC.DataManagementSystem.Client.Catalog.LcgFileCatalogCombinedClient import LcgFileCatalogCombinedClient
+from DIRAC.LHCbSystem.Utilities.AncestorFiles import getAncestorFiles
 import os, time, random
 
 AGENT_NAME = 'ProductionManagement/TransformationAgent'
@@ -123,9 +124,9 @@ class TransformationAgent(Agent):
       plugin = 'Standard'
     server = RPCClient('ProductionManagement/ProductionManager')
     result = server.getInputData(prodName,'')
-    sflag = True #WARNING KGG this is possibly an error
+
     if result['OK']:
-      data = result['Value']      
+      data = result['Value']
     else:
       print "Failed to get data for transformation", prodID, result['Message']
       return S_ERROR("Failed to get data for transformation %d %s"%(prodID,result['Message']))
@@ -133,13 +134,13 @@ class TransformationAgent(Agent):
     if self.checkLFC == 'yes' and data:
       result = self.checkDataWithLFC(prodID,data)
       if result['OK']:
-        data = result['Value']  
+        data = result['Value']
 
     nJobs = 0
     if flush:
       while len(data) >0:
         ldata = len(data)
-        data = eval('self.generateJob_'+plugin+'(data,prodID,sflag,group_size, flush)')
+        data = eval('self.generateJob_'+plugin+'(data,prodID,transDict,flush)')
         if ldata == len(data):
           break
         else:
@@ -147,7 +148,7 @@ class TransformationAgent(Agent):
     else:
       while len(data) >= group_size:
         ldata = len(data)
-        data = eval('self.generateJob_'+plugin+'(data,prodID,sflag,group_size, flush)')
+        data = eval('self.generateJob_'+plugin+'(data,prodID,transDictflush)')
         if ldata == len(data):
           break
         else:
@@ -157,9 +158,11 @@ class TransformationAgent(Agent):
     return S_OK(nJobs)
 
   #####################################################################################
-  def generateJob_CCRC_RAW(self,data,production,sflag,group_size,flush=False):
+  def generateJob_CCRC_RAW(self,data,production,transDict,flush=False):
     """ Generate a job according to the CCRC 2008 site shares
     """
+
+    group_size = int(transDict['GroupSize'])
     dataLog = RPCClient('DataManagement/DataLogging')
     server = RPCClient('ProductionManagement/ProductionManager')
     # Sort files by LFN
@@ -243,82 +246,99 @@ class TransformationAgent(Agent):
     return data_m
 
   #####################################################################################
-  def generateJob_Standard(self,data,production,sflag,group_size,flush=False):
+  def generateJob_Standard(self,data,production,transDict,flush=False):
     """ Generates a job based on the input data, adds job to the repository
         and returns a reduced list of the lfns that rest to be processed
         If flush is true, the group_size is not taken into account
     """
+
+    group_size = int(transDict['GroupSize'])
+    ancestorDepth = 0
+    if transDict.has_key('Additional'):
+      if transDict['Additional'].has_key('AncestorDepth'):
+        ancestorDepth = int(transDict['Additional']['AncestorDepth'])
     dataLog = RPCClient('DataManagement/DataLogging')
     server = RPCClient('ProductionManagement/ProductionManager')
     # Sort files by SE
     datadict = {}
+    ancestorSEDict = {}
     for lfn,se in data:
-      #if not se in self.seBlackList:
-        if not datadict.has_key(se):
-          datadict[se] = []
+      #if se in self.seBlackList:
+      #  continue
+
+      if not datadict.has_key(se):
+        datadict[se] = []
+
+      if ancestorDepth > 0:
+        # Find SEs allowed by the ancestor presence
+        if not ancestorSEDict.has_key(lfn):
+          result = self.checkAncestors(lfn,ancestorDepth)
+          if result['OK']:
+            ancestorSEs = result['Value']
+          else:
+            ancestorSEs = []
+        if se in ancestorSEs:
+          datadict[se].append(lfn)
+      else:
         datadict[se].append(lfn)
 
     data_m = data
 
-    if sflag:
-      # Group files by SE
+    # Group files by SE
 
-      if flush: # flush mode
-        # Find the SE with maximum sufficient amount of data
-        lmax = 0
-        lse = ''
-        for se in datadict.keys():
-          ldata = len(datadict[se])
-          if ldata > lmax:
-            lmax = ldata
-            lse = se
-        if lmax < group_size:
-          group_size = lmax
+    if flush: # flush mode
+      # Find the SE with maximum sufficient amount of data
+      lmax = 0
+      lse = ''
+      for se in datadict.keys():
+        ldata = len(datadict[se])
+        if ldata > lmax:
+          lmax = ldata
+          lse = se
+      if lmax < group_size:
+        group_size = lmax
 
-      else: # normal  mode
-        # Find the SE with the minimally sufficient amount of data
-        lmin = len(data)+1
-        lse = ''
-        for se in datadict.keys():
-          ldata = len(datadict[se])
-          if ldata < lmin and ldata >= group_size:
-            lmin = ldata
-            lse = se
+    else: # normal  mode
+      # Find the SE with the minimally sufficient amount of data
+      lmin = len(data)+1
+      lse = ''
+      for se in datadict.keys():
+        ldata = len(datadict[se])
+        if ldata < lmin and ldata >= group_size:
+          lmin = ldata
+          lse = se
 
-      if lse:
-        # We have found a SE with minimally(or max in case of flush) sufficient amount of data
-        lfns = datadict[lse][:group_size]
-        result = self.addJobToProduction(production,lfns,lse)
-        if result['OK']:
-          jobID = long(result['Value'])
-          if jobID:
-            result = server.setFileStatusForTransformation(production,[('Assigned',lfns)])
-            if not result['OK']:
-              gLogger.error("Failed to update file status for production %d"%production)
+    if lse:
+      # We have found a SE with minimally(or max in case of flush) sufficient amount of data
+      lfns = datadict[lse][:group_size]
+      result = self.addJobToProduction(production,lfns,lse)
+      if result['OK']:
+        jobID = long(result['Value'])
+        if jobID:
+          result = server.setFileStatusForTransformation(production,[('Assigned',lfns)])
+          if not result['OK']:
+            gLogger.error("Failed to update file status for production %d"%production)
 
-            result = server.setFileJobID(production,jobID,lfns)
-            if not result['OK']:
-              gLogger.error("Failed to set file job ID for production %d"%production)
+          result = server.setFileJobID(production,jobID,lfns)
+          if not result['OK']:
+            gLogger.error("Failed to set file job ID for production %d"%production)
 
-            result = server.setFileSEForTransformation(production,lse,lfns)
-            if not result['OK']:
-              gLogger.error("Failed to set SE for production %d"%production)
-            for lfn in lfns:
-              result = dataLog.addFileRecord(lfn,'Job created','ProdID: %s JobID: %s' % (production,jobID),'','TransformationAgent')
+          result = server.setFileSEForTransformation(production,lse,lfns)
+          if not result['OK']:
+            gLogger.error("Failed to set SE for production %d"%production)
+          for lfn in lfns:
+            result = dataLog.addFileRecord(lfn,'Job created','ProdID: %s JobID: %s' % (production,jobID),'','TransformationAgent')
 
-          # Remove used files from the initial list
-          data_m = []
-          for lfn,se in data:
-            if lfn not in lfns:
-              data_m.append((lfn,se))
-        else:
-          gLogger.warn("Failed to add a new job to repository: "+result['Message'])
-
+        # Remove used files from the initial list
+        data_m = []
+        for lfn,se in data:
+          if lfn not in lfns:
+            data_m.append((lfn,se))
       else:
-        gLogger.verbose("Neither SE has enough input data")
+        gLogger.warn("Failed to add a new job to repository: "+result['Message'])
+
     else:
-      # Do not discriminate sites
-      pass
+      gLogger.verbose("Neither SE has enough input data")
 
     return data_m
 
@@ -361,7 +381,7 @@ class TransformationAgent(Agent):
     lfc_data = []
     if not result['OK']:
       return result
-      
+
     replicas = result['Value']['Successful']
     for lfn, replicaDict in replicas.items():
       lfc_datadict[lfn] = []
@@ -375,11 +395,49 @@ class TransformationAgent(Agent):
       if lfn not in lfc_lfns:
         missing_lfns.append(lfn)
         gLogger.warn('LFN: %s not found in the LFC' % lfn)
-    if missing_lfns:    
+    if missing_lfns:
       # Mark this file in the transformation
       server = RPCClient('ProductionManagement/ProductionManager')
-      result = server.setFileStatusForTransformation(production,[('MissingLFC',missing_lfns)])    
+      result = server.setFileStatusForTransformation(production,[('MissingLFC',missing_lfns)])
       if not result['OK']:
         gLogger.warn(result['Message'])
 
     return S_OK(lfc_data)
+
+  def checkAncestors(self,lfn,ancestorDepth):
+    """ Check ancestor availablity on sites. Returns a list of SEs where all the ancestors
+        are present
+    """
+
+    result = getAncestorFiles(lfn,ancestorDepth)
+    if not result['OK']:
+      gLogger.warn(result['Message'])
+      return result
+
+    fileList = result['Value']
+    if not fileList:
+      return S_ERROR('No ancestors returned')
+
+    # Determine common SEs now
+    result = self.lfc.getReplicas(lfns,self.shifterDN)
+    if not result['OK']:
+      gLogger.warn(result['Message'])
+      return S_ERROR('Failed to get results from LFC: %s' % result['Message'])
+
+    replicas = result['Value']['Successful']
+    ancestorSEs = []
+    for lfn, replicaDict in replicas.items():
+      ancestorSEs = replicaDict.keys()
+      break
+
+    for lfn, replicaDict in replicas.items():
+      SEs = replicaDict.keys()
+      tmp_SEs = []
+      for se in SEs:
+        if se in ancestorSEs:
+          tmp_SEs.append(se)
+      ancestorSEs = tmp_SEs
+      if not ancestorSEs:
+        break
+
+    return S_OK(ancestorSEs)
