a62b185714bfc74dbbbddd0f6b25c83495766e32
roma <roma@4525493e-7705-40b1-a816-d608a930855b>
2008-10-10 16:31:37 +0000

    Monitoring of CE were added
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@12588 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/LHCbSystem/Agent/SAMAgent.py
+++ LHCbDIRAC/LHCbSystem/Agent/SAMAgent.py
@@ -5,6 +5,9 @@ from DIRAC.Core.Base.Agent import Agent
 from DIRAC.LHCbSystem.Testing.SAM.Client.DiracSAM import DiracSAM
 from DIRAC.Core.Utilities.Shifter import setupShifterProxyInEnv
 
+from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC  import gMonitor
+
 import os,time
 
 AGENT_NAME = "LHCb/SAMAgent"
@@ -26,21 +29,52 @@ class SAMAgent(Agent):
     self.proxyLocation = gConfig.getValue( self.section+'/ProxyLocation', '' )
     if not self.proxyLocation:
       self.proxyLocation = False
+
+    gMonitor.registerActivity("TotalSites","Total Sites","SAMAgent","Sites",gMonitor.OP_SUM,3600)
+    gMonitor.registerActivity("ActiveSites","Active Sites","SAMAgent","Sites",gMonitor.OP_SUM,3600)
+    gMonitor.registerActivity("BannedSites","Banned Sites","SAMAgent","Sites",gMonitor.OP_SUM,3600)
+
     return result
 
   def execute(self):
 
     gLogger.debug("Executing %s"%(self.name))
+
     if self.useProxies:
       result = setupShifterProxyInEnv( "SAMManager", self.proxyLocation )
       if not result[ 'OK' ]:
         self.log.error( "Can't get shifter's proxy: %s" % result[ 'Message' ] )
         return result
 
+    self._siteAccount()
+
     diracSAM = DiracSAM()
 
     result = diracSAM.submitAllSAMJobs()
-    if not result['OK']:
+     if not result['OK']:
       gLogger.error(result['Message'])
    
     return result
+
+  def _siteAccount(self):
+  
+    wmsAdmin = RPCClient('WorkloadManagement/WMSAdministrator')
+    result = wmsAdmin.getSiteMask()
+    if not result[ 'OK' ]:
+      self.log.error( "Can't get SiteMask: %s" % result[ 'Message' ] )
+      return result
+    sitesmask = result['Value']
+    numsitesmask = len(sitesmask)
+
+    result = gConfig.getSections('/Resources/Sites/LCG')
+    if not result[ 'OK' ]:
+      self.log.error( "Can't get Sites: %s" % result[ 'Message' ] )
+      return result
+    sites = result['Value']
+    numsites = len(sites)
+    
+    gMonitor.addMark("TotalSites", numsites)
+    gMonitor.addMark("ActiveSites", numsitesmask)
+    gMonitor.addMark("BannedSites", numsites - numsitesmask)
+   
+    return S_OK()
