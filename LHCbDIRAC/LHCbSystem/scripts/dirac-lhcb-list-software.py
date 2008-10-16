0f57e300591ff47571fabcf1f159c2f96a200c9a
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2008-10-16 09:51:49 +0000

    Added .py extension versions of existing scripts.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@12685 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/scripts/dirac-lhcb-list-software.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/scripts/dirac-lhcb-list-software.py,v 1.1 2008/10/16 09:51:47 paterson Exp $
+# File :   dirac-lhcb-list-software
+# Author : Stuart Paterson
+########################################################################
+__RCSID__   = "$Id: dirac-lhcb-list-software.py,v 1.1 2008/10/16 09:51:47 paterson Exp $"
+__VERSION__ = "$Revision: 1.1 $"
+import sys,string
+from DIRACEnvironment import DIRAC
+from DIRAC.Core.Base import Script
+from DIRAC import gConfig
+
+Script.parseCommandLine( ignoreErrors = True )
+args = Script.getPositionalArgs()
+
+def usage():
+  print 'Usage: %s [<Optional System Configuration To Query For>]' %(Script.scriptName)
+  DIRAC.exit(2)
+
+def printSoftware(package,packageArch):
+  adj = 30
+  print package.split('.')[0].ljust(adj)+package.split('.')[1].ljust(adj)+string.join(packageArch,',').ljust(adj)
+
+def printHeader(header):
+  print '=========> %s ' %header
+
+if len(args) > 1:
+  usage()
+
+softwareDistribution = gConfig.getOptionsDict('/Operations/SoftwareDistribution')
+if not softwareDistribution['OK']:
+  print 'ERROR: Could not get values for /Operations/SoftwareDistribution section with message:\n%s' %(result['Message'])
+  DIRAC.exit(2)
+
+software = softwareDistribution['Value']
+systemConfigs = software.keys()
+systemConfigs.remove('Active')
+systemConfigs.remove('Deprecated')
+
+warn = True
+if len(args)==1:
+  warn = False
+  systemConfigs = [args[0]]
+
+active = software['Active'].replace(' ','').split(',')
+active.sort()
+deprecated = software['Deprecated'].replace(' ','').split(',')
+deprecated.sort()
+
+if not warn:
+  printHeader('Active LHCb Software For System Configuration %s' %(args[0]))
+else:
+  printHeader('Active LHCb Software For All System Configurations')
+
+for package in active:
+  packageArch = []
+  for systemConfig in systemConfigs:
+    if package in software[systemConfig]:
+      packageArch.append(systemConfig)
+  if packageArch:
+    printSoftware(package,packageArch)
+  else:
+    if warn:
+      print 'WARNING: %s %s is not defined for any system configurations' %(package.split('.')[0],package.split('.')[1])
+
+if warn:
+  printHeader('Deprecated LHCb Software')
+  print '%s' %(string.join(deprecated,', '))
+
+DIRAC.exit(0)
---
+++ LHCbDIRAC/LHCbSystem/scripts/dirac-lhcb-manage-software.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+########################################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/scripts/dirac-lhcb-manage-software.py,v 1.1 2008/10/16 09:51:47 paterson Exp $
+# File :   dirac-lhcb-manage-software
+# Author : Stuart Paterson
+########################################################################
+__RCSID__   = "$Id: dirac-lhcb-manage-software.py,v 1.1 2008/10/16 09:51:47 paterson Exp $"
+__VERSION__ = "$Revision: 1.1 $"
+import sys
+from DIRACEnvironment import DIRAC
+from DIRAC.Core.Base import Script
+from DIRAC.ConfigurationSystem.Client.LocalConfiguration import LocalConfiguration
+from DIRAC.Core.Base.Agent import createAgent
+from DIRAC import gConfig
+
+Script.parseCommandLine( ignoreErrors = True )
+args = Script.getPositionalArgs()
+
+def usage():
+  print 'Usage: %s [<PATH TO SOFTWARE CACHE>]' %(Script.scriptName)
+  print 'If not specified, path to software cache taken from /LocalSite/SharedArea'
+  DIRAC.exit(2)
+
+if len(args) > 2:
+  usage()
+
+if not args:
+  sharedArea = gConfig.getValue('/LocalSite/SharedArea')
+else:
+  sharedArea = args[0]
+
+exitCode = 0
+agentName = 'LHCb/SoftwareManagementAgent'
+localCfg = LocalConfiguration()
+localCfg.addDefaultEntry('/LocalSite/SharedArea',sharedArea)
+localCfg.setConfigurationForAgent(agentName)
+resultDict = localCfg.loadUserData()
+if not resultDict[ 'OK' ]:
+  print "There were errors when loading configuration", resultDict['Message']
+  sys.exit(1)
+
+agent = createAgent(agentName)
+result = agent.run_once()
+
+if not result['OK']:
+  print 'ERROR %s' %result['Message']
+  exitCode = 2
+
+DIRAC.exit(exitCode)
