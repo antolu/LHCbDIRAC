e6056d6f2e1eae052f328cc3ad019a696c3c9147
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2008-10-17 13:15:47 +0000

    rename script with .py
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@12720 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/scripts/dirac-production-manager-cli.py
+++ LHCbDIRAC/ProductionManagementSystem/scripts/dirac-production-manager-cli.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/scripts/Attic/dirac-production-manager-cli,v 1.2 2008/04/17 15:26:54 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/scripts/dirac-production-manager-cli.py,v 1.24 2008/10/17 13:15:46 rgracian Exp $
 # File :   dirac-production-manager-cli
 # Author :  Gennady G. Kuznetsov
 ########################################################################
-__RCSID__   = "$Id: dirac-production-manager-cli,v 1.2 2008/04/17 15:26:54 rgracian Exp $"
-__VERSION__ = "$Revision: 1.2 $"
+__RCSID__   = "$Id: dirac-production-manager-cli.py,v 1.24 2008/10/17 13:15:46 rgracian Exp $"
+__VERSION__ = "$Revision: 1.24 $"
 
 from DIRACEnvironment import DIRAC
 from DIRAC.Core.Base import Script
