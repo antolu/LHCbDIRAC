5467faaa6dabdc7c3dd9a793bdc6e91e1d4af9ee
atsareg <atsareg@4525493e-7705-40b1-a816-d608a930855b>
2008-11-13 17:36:18 +0000

    New output format
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@13041 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/LHCbSystem/Agent/GridSiteWMSMonitoringAgent.py
+++ LHCbDIRAC/LHCbSystem/Agent/GridSiteWMSMonitoringAgent.py
@@ -1,4 +1,4 @@
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Agent/GridSiteWMSMonitoringAgent.py,v 1.1 2008/11/10 14:01:10 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Agent/GridSiteWMSMonitoringAgent.py,v 1.2 2008/11/13 17:36:18 atsareg Exp $
 
 
 '''
@@ -46,14 +46,51 @@ class GridSiteWMSMonitoringAgent(Agent):
     result = self._getWMSData()
     if not result[ 'OK' ]:
       return result
-    fileContents = 'site,vo,activity,parallel jobs(number),completed jobs(number),cpu time(seconds),'
-    fileContents += 'wall time(seconds),start time(epoch in seconds),end time(epoch in seconds)\n'
+
+    fileContents = ''
+    href = 'http://lhcbweb.pic.es/DIRAC/LHCb-Production/anonymous/systems/accountingPlots/WMSHistory#ds9:_plotNames12:NumberOfJobss13:_timeSelectors5:86400s7:_Statuss7:Runnings9:_typeNames10:WMSHistorys9:_groupings4:Sitee'
     for site,sDict in result['Value'].items():
+      parallel_jobs = 0
+      completed_jobs = 0
+      successfully_completed_jobs = 0
+      CPU_time = 0
+      wall_time = 0
       for activity,aDict in result['Value'][site].items():
-        fList = [site,'LHCb',activity,str(aDict['Running']),str(aDict['FinishedTotal']),
-                 str(aDict['FinishedSuccessful']),str(aDict['CPUTime']),str(aDict['WallClockTime']),
-                 str(int(time.time())-3600),str(int(time.time()))]
-        fileContents += ','.join(fList)+'\n' 
+        parallel_jobs += aDict['Running']
+        lTuple = (site,activity,aDict['Running'],int(time.time())-3600,int(time.time()),href)
+        line = '%s,%s,parallel_jobs,%d,-1,unknown,%d,%d,%s ' % lTuple
+        fileContents += line+'\n'
+        completed_jobs += aDict['FinishedTotal']
+        lTuple = (site,activity,aDict['FinishedTotal'],int(time.time())-3600,int(time.time()),href)
+        line = '%s,%s,completed_jobs,%d,-1,unknown,%d,%d,%s ' % lTuple
+        fileContents += line+'\n'
+        successfully_completed_jobs += aDict['FinishedSuccessful']
+        lTuple =  (site,activity,aDict['FinishedSuccessful'],int(time.time())-3600,int(time.time()),href)
+        line = '%s,%s,successfully_completed_jobs,%d,-1,unknown,%d,%d,%s ' % lTuple
+        fileContents += line+'\n'
+        CPU_time += aDict['CPUTime']
+        lTuple =  (site,activity,aDict['CPUTime'],int(time.time())-3600,int(time.time()),href)
+        line = '%s,%s,CPU_time,%d,-1,unknown,%d,%d,%s ' % lTuple
+        fileContents += line+'\n'
+        wall_time += aDict['WallClockTime']
+        lTuple =  (site,activity,aDict['WallClockTime'],int(time.time())-3600,int(time.time()),href)
+        line = '%s,%s,wall_time,%d,-1,unknown,%d,%d,%s ' % lTuple
+        fileContents += line+'\n'
+      lTuple = (site,'overall_activity',parallel_jobs,int(time.time())-3600,int(time.time()),href)
+      line = '%s,%s,parallel_jobs,%d,-1,unknown,%d,%d,%s ' % lTuple
+      fileContents += line+'\n'
+      lTuple = (site,'overall_activity',completed_jobs,int(time.time())-3600,int(time.time()),href)
+      line = '%s,%s,completed_jobs,%d,-1,unknown,%d,%d,%s ' % lTuple
+      fileContents += line+'\n'
+      lTuple = (site,'overall_activity',successfully_completed_jobs,int(time.time())-3600,int(time.time()),href)
+      line = '%s,%s,parallel_jobs,%d,-1,unknown,%d,%d,%s ' % lTuple
+      fileContents += line+'\n'
+      lTuple = (site,'overall_activity',CPU_time,int(time.time())-3600,int(time.time()),href)
+      line = '%s,%s,CPU_time,%d,-1,unknown,%d,%d,%s ' % lTuple
+      fileContents += line+'\n'
+      lTuple = (site,'overall_activity',wall_time,int(time.time())-3600,int(time.time()),href)
+      line = '%s,%s,wall_time,%d,-1,unknown,%d,%d,%s ' % lTuple
+      fileContents += line+'\n'
     self._lastUpdateTime = time.time()
     result = self._commitFileContents( fileContents )
     if result['OK']:
@@ -78,7 +115,7 @@ class GridSiteWMSMonitoringAgent(Agent):
 
   def _getSiteGOCNameMapping(self):
     """ Get DIRAC Site name to GOC Site name mapping
-    """ 
+    """
 
     result = gConfig.getSections('/Resources/Sites/LCG')
     if not result['OK']:
@@ -86,7 +123,7 @@ class GridSiteWMSMonitoringAgent(Agent):
 
     for site in result['Value']:
       gocName = gConfig.getValue('/Resources/Sites/LCG/%s/Name' % site,'Unknown')
-      self.siteGOCNameDict[site] = gocName      
+      self.siteGOCNameDict[site] = gocName
 
   def __getGOCName(self,site):
     """ Get GOC site name
@@ -107,7 +144,7 @@ class GridSiteWMSMonitoringAgent(Agent):
         return 'mc_production'
     for pattern in DATA_JOB_TYPES:
       if re.search(pattern,jType):
-        return 'data_reconstruction'  
+        return 'data_reconstruction'
     for pattern in USER_JOB_TYPES:
       if re.search(pattern,jType):
         return 'user_analysis'
@@ -115,26 +152,25 @@ class GridSiteWMSMonitoringAgent(Agent):
       if re.search(pattern,jType):
         return 'SAM_monitoring'
 
-    return mc_production         
+    return 'mc_production'
 
   def _getWMSData(self):
     """ Get the job data from the WMS
     """
 
-    self._getSiteGOCNameMapping() 
+    self._getSiteGOCNameMapping()
 
     monDict = {}
 
     # Currently running jobs
-    result = self.jobDB.getCounters(['Site','JobType'],{'Status':'Done'},None)
+    result = self.jobDB.getCounters(['Site','JobType'],{'Status':'Running'},None)
     if not result['OK']:
       return result
-
     for siteTuple in result['Value']:
       siteDict,count = siteTuple
       site = self.__getGOCName(siteDict['Site'])
       if site:
-        jobType = self.__getJobType(siteDict['JobType'])        
+        jobType = self.__getJobType(siteDict['JobType'])
         if site not in monDict.keys():
           monDict[site] = {}
         if jobType not in monDict[site].keys():
@@ -148,17 +184,16 @@ class GridSiteWMSMonitoringAgent(Agent):
           monDict[site][jobType]['Running'] = count
         else:
           monDict[site][jobType]['Running'] += count
- 
+
     # Jobs finished in the last hour
-    dt = Time.dateTime() - datetime.timedelta(seconds=36000000)
+    dt = Time.dateTime() - datetime.timedelta(seconds=3600)
     result = self.jobDB.getCounters(['Site','JobType'],{'Status':['Done','Completed','Failed','Killed']},str(dt),timeStamp='EndExecTime')
     if not result['OK']:
       return result
-
     for siteTuple in result['Value']:
       siteDict,count = siteTuple
       site = self.__getGOCName(siteDict['Site'])
-      if site: 
+      if site:
         jobType = self.__getJobType(siteDict['JobType'])
         if site not in monDict.keys():
           monDict[site] = {}
@@ -174,6 +209,7 @@ class GridSiteWMSMonitoringAgent(Agent):
         else:
           monDict[site][jobType]['FinishedTotal'] += count
         result = self.jobDB.getTimings(siteDict['Site'])
+        #result = S_ERROR()
         if result['OK']:
           if not monDict[site][jobType].has_key('CPUTime'):
             monDict[site][jobType]['CPUTime'] = result['Value']['CPUTime']
@@ -188,7 +224,6 @@ class GridSiteWMSMonitoringAgent(Agent):
     result = self.jobDB.getCounters(['Site','JobType'],{'Status':['Done','Completed']},str(dt),timeStamp='EndExecTime')
     if not result['OK']:
       return result
-
     for siteTuple in result['Value']:
       siteDict,count = siteTuple
       site = self.__getGOCName(siteDict['Site'])
