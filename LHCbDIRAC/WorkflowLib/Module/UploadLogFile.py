34bbcf6ac82d67286dee56973263d9e3bacdd2f5
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2009-02-05 17:05:00 +0000

    Added index reporting, now glob for files, debugged after testing.  Still to add failover for failed registrations.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@14079 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/WorkflowLib/Module/UploadLogFile.py
+++ LHCbDIRAC/WorkflowLib/Module/UploadLogFile.py
@@ -1,28 +1,34 @@
 ########################################################################
-# $Id: UploadLogFile.py,v 1.1 2008/11/08 11:56:43 acsmith Exp $
+# $Id: UploadLogFile.py,v 1.2 2009/02/05 17:05:00 paterson Exp $
 ########################################################################
-
-""" UploadLogFile module is used to upload the files present in the workingg directory
+""" UploadLogFile module is used to upload the files present in the working
+    directory.
 """
 
-__RCSID__ = "$Id: UploadLogFile.py,v 1.1 2008/11/08 11:56:43 acsmith Exp $"
+__RCSID__ = "$Id: UploadLogFile.py,v 1.2 2009/02/05 17:05:00 paterson Exp $"
 
 from DIRAC.RequestManagementSystem.Client.RequestContainer import RequestContainer
-from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
-from DIRAC.DataManagementSystem.Client.StorageElement import StorageElement
-from DIRAC import S_OK, S_ERROR, gLogger, gConfig
-from WorkflowLib.Module.ModuleBase import ModuleBase
-import os,shutil
+from DIRAC.DataManagementSystem.Client.ReplicaManager      import ReplicaManager
+from DIRAC.DataManagementSystem.Client.StorageElement      import StorageElement
+from DIRAC.Core.Utilities.Subprocess                       import shellCall
+from WorkflowLib.Module.ModuleBase                         import *
+from DIRAC                                                 import S_OK, S_ERROR, gLogger, gConfig
+
+try:
+  from DIRAC.LHCbSystem.Utilities.ProductionData  import getLogPath
+except Exception,x:
+  from LHCbSystem.Utilities.ProductionData  import getLogPath
+
+import os,shutil,glob,string,random
 
 class UploadLogFile(ModuleBase):
 
+  #############################################################################
   def __init__(self):
-
-    #####################################################
-    # Variables supposed to be set by the workflow
-
+    """Module initialization.
+    """
     self.version = __RCSID__
-
+    self.log = gLogger.getSubLogger('UploadLogFile')
     self.PRODUCTION_ID = None
     self.JOB_ID = None
     self.workflow_commons = None
@@ -31,31 +37,68 @@ class UploadLogFile(ModuleBase):
     self.logSE = gConfig.getValue('/Resources/StorageElements/ProductionLogSE','LogSE')
     self.root = gConfig.getValue('/LocalSite/Root',os.getcwd())
     self.site = gConfig.getValue('/LocalSite/Site','localSite')
-
+    self.logSizeLimit = gConfig.getValue('/Operations/LogFiles/SizeLimit',20*1024*1024)
+    self.logExtensions = gConfig.getValue('/Operations/LogFiles/Extensions',[])
+    self.diracLogo = gConfig.getValue('/Operations/SAM/LogoURL','https://lhcbweb.pic.es/DIRAC/images/logos/DIRAC-logo-transp.png')
     self.rm = ReplicaManager()
-    self.log = gLogger.getSubLogger('UploadLogFile')
-    self.log.setLevel('debug')
+
+    self.enable=True
+    self.failoverTest=False #flag to put log files to failover by default
+    self.jobID = ''
 
 ######################################################################
   def resolveInputVariables(self):
 
-    if self.workflow_commons.has_key('LogFilePath'):
-      self.logFilePath =  self.workflow_commons['LogFilePath']
+    if self.step_commons.has_key('Enable'):
+      self.enable=self.step_commons['Enable']
+      if not type(self.enable)==type(True):
+        self.log.warn('Enable flag set to non-boolean value %s, setting to False' %self.enable)
+        self.enable=False
+
+    if self.step_commons.has_key('TestFailover'):
+      self.enable=self.step_commons['TestFailover']
+      if not type(self.failoverTest)==type(True):
+        self.log.warn('Test failover flag set to non-boolean value %s, setting to False' %self.failoverTest)
+        self.failoverTest=False
+
+    if os.environ.has_key('JOBID'):
+      self.jobID = os.environ['JOBID']
+      self.log.verbose('Found WMS JobID = %s' %self.jobID)
+    else:
+      self.log.info('No WMS JobID found, disabling module via control flag')
+      self.enable=False
+
+    if self.workflow_commons.has_key('LogFilePath') and self.workflow_commons.has_key('LogTargetPath'):
+      self.logFilePath = self.workflow_commons['LogFilePath']
+      self.logLFNPath = self.workflow_commons['LogTargetPath']
+    else:
+      self.log.info('LogFilePath parameter not found, creating on the fly')
+      result = getLogPath(self.workflow_commons)
+      if not result['OK']:
+        self.log.error('Could not create LogFilePath',result['Message'])
+        return result
+      self.logFilePath=result['Value']['LogFilePath'][0]
+      self.logLFNPath = result['Value']['LogTargetPath'][0]
 
     if self.workflow_commons.has_key('Request'):
       self.request = self.workflow_commons['Request']
     else:
       self.request = RequestContainer()
+      self.request.setRequestName('job_%s_request.xml' % self.jobID)
+      self.request.setJobID(self.jobID)
+      self.request.setSourceComponent("Job_%s" % self.jobID)
+
+    return S_OK('Parameters resolved')
 
 ######################################################################
   def execute(self):
     """ Main executon method
     """
+    self.log.info('Initializing %s' % self.version)
     # Add global reporting tool
     self.resolveInputVariables()
 
     result = self.setApplicationStatus('Uploading Logs')
-    self.log.info('Initializing %s' % self.version)
 
     res = shellCall(0,'ls -al')
     if res['OK']:
@@ -65,104 +108,134 @@ class UploadLogFile(ModuleBase):
       self.log.error('Failed to list the log directory',str(res['Value'][2]))
 
     self.log.info('Job root is found to be %s' % (self.root))
-    self.log.info('PRODUTION_ID = %s, JOB_ID = %s '  % (self.PRODUCTION_ID, self.JOB_ID))
+    self.log.info('PRODUCTION_ID = %s, JOB_ID = %s '  % (self.PRODUCTION_ID, self.JOB_ID))
     self.logdir = os.path.realpath('./job/log/%s/%s' % (self.PRODUCTION_ID, self.JOB_ID))
     self.log.info('Selected log files will be temporarily stored in %s' % self.logdir)
 
     res = self.finalize()
+    self.workflow_commons['Request']=self.request
     return res
 
-##############################################################################
+  #############################################################################
   def finalize(self):
     """ finalize method performs final operations after all the job
         steps were executed. Only production jobs are treated.
     """
-
-    self.log.info('Starting UploadLogFile finalize')
+    self.log.verbose('Starting UploadLogFile finalize')
     ##########################################
     # First determine the files which should be saved
     self.log.info('Determining the files to be saved in the logs.')
-    res = determineRelevantFiles()
+    res = self.determineRelevantFiles()
     if not res['OK']:
       self.log.error('Completely failed to select relevant log files.',res['Message'])
       return S_OK()
     selectedFiles = res['Value']
-    self.log.info('The following %s files were selected to be saved:' % len(selectedFiles))
-    for file in selectedFiles:
-      self.log.info(file)
+    self.log.info('The following %s files were selected to be saved:\n%s' %(len(selectedFiles),string.join(selectedFiles,'\n')))
 
     #########################################
     # Create a temporary directory containing these files
     self.log.info('Populating a temporary directory for selected files.')
-    res = populateLogDirectory(selectedFiles)
+    res = self.populateLogDirectory(selectedFiles)
     if not res['OK']:
       self.log.error('Completely failed to populate temporary log file directory.',res['Message'])
       return S_OK()
     self.log.info('%s populated with log files.' % self.logdir)
 
     #########################################
+    # Create a tailored index page
+    self.log.info('Creating an index page for the logs')
+    result = self.__createLogIndex(selectedFiles)
+    if not result['OK']:
+      self.log.error('Failed to create index page for logs',res['Message'])
+
+    if not self.enable:
+      self.log.info('Module is disabled by control flag')
+      return S_OK('Module is disabled by control flag')
+
+    #########################################
     # Attempt to uplaod logs to the LogSE
     self.log.info('Transferring log files to the %s' % self.logSE)
-    self.log.info('PutDirectory %s %s %s' % (self.logFilePath, os.path.realpath(self.logdir),self.logSE))
-    res = self.rm.putDirectory(self.logFilePath,os.path.realpath(self.logdir),self.logSE)
-    if res['OK']:
-      self.log.info('Successfully upload log directory to %s' % self.logSE)
-      # TODO: The logURL should be constructed using the LogSE and StorageElement()
-      #storageElement = StorageElement(self.logSE)
-      #pfn = storageElement.getPfnForLfn(self.logFilePath)['Value']
-      #logURL = getPfnForProtocol(res['Value'],'http')['Value']
-      logURL = '<a href="http://lhcb-logs.cern.ch/storage%s">Log file directory</a>' % self.logFilePath
-      self.setJobParameter('Log URL',logURL)
-      self.log.info('Logs for this job may be retrieved from %s' % logURL)
-      return S_OK()
+    res = S_ERROR()
+    if not self.failoverTest:
+      self.log.info('PutDirectory %s %s %s' % (string.replace(self.logFilePath,'/%s' %self.JOB_ID,''), os.path.realpath(self.logdir),self.logSE))
+      res = self.rm.putDirectory(string.replace(self.logFilePath,'/%s' %self.JOB_ID,''),os.path.realpath(self.logdir),self.logSE)
+      self.log.verbose(res)
+      if res['OK']:
+        self.log.info('Successfully upload log directory to %s' % self.logSE)
+        # TODO: The logURL should be constructed using the LogSE and StorageElement()
+        #storageElement = StorageElement(self.logSE)
+        #pfn = storageElement.getPfnForLfn(self.logFilePath)['Value']
+        #logURL = getPfnForProtocol(res['Value'],'http')['Value']
+        logURL = '<a href="http://lhcb-logs.cern.ch/storage%s">Log file directory</a>' % self.logFilePath
+        self.setJobParameter('Log URL',logURL)
+        self.log.info('Logs for this job may be retrieved from %s' % logURL)
+        return S_OK()
 
     #########################################
     # Recover the logs to a failover storage element
-    self.log.error('Completely failed to upload log files to %s.' % self.logSE,res['Message'])
-    self.log.info('Uploading logs to failover Storage Element')
+    self.log.error('Completely failed to upload log files to %s, will attempt upload to failover SE' % self.logSE,res['Message'])
 
     ######### REALLY NASTY TEMPORARY CODE #######################
     # TODO: Use tar module if available
     tarFileDir = os.path.dirname(self.logdir)
-    tarFileName = '%s.tar.gz' % tarFileDir
+    self.logLFNPath = '%s.gz' %self.logLFNPath
+    tarFileName = os.path.basename(self.logLFNPath)
     comm = 'tar czvf %s %s' % (tarFileName,self.logdir)
     res = shellCall(0,comm)
     if not res['OK']:
       self.log.error('Failed to create tar file from directory','%s %s' % (self.logdir,res['Message']))
       return S_OK()
     ############################################################
+    logURL = '<a href="http://lhcb-logs.cern.ch/storage%s">Log file directory</a>' % self.logFilePath
+    self.setJobParameter('Log URL',logURL)
+    self.log.info('Logs for this job may be retrieved from %s' % logURL)
 
-    res = self.uploadFileToFailover(tarFileName,self.logFilePath)
+    res = self.uploadFileToFailover(tarFileName,self.logLFNPath)
     if not res['OK']:
       self.log.error('Failed to upload logs to all destinations')
       return S_OK()
-    res = createLogUploadRequest(self.logSE,logFileLFN)
+    res = self.createLogUploadRequest(self.logSE,self.logLFNPath)
     if not res['OK']:
       self.log.error('Failed to create failover request', res['Message'])
     else:
       self.log.info('Successfully created failover request')
     return S_OK()
 
-################################################################################
+  #############################################################################
   def determineRelevantFiles(self):
     """ The files which are below a configurable size will be stored in the logs.
         This will typically pick up everything in the working directory minus the output data files.
     """
-    # TODO: Get the file size from the CS
-    maximumFileSize = 5*(1024*1024) # 5MB
-    candidateFiles = os.listdir('.')
+    logFileExtensions = ['*.txt','*.log','*.root','*.out','*.output','*.xml','*.sh', '*.info', '*.err']
+    if self.logExtensions:
+      self.log.info('Using list of log extensions from CS:\n%s' %(string.join(self.logExtensions,', ')))
+      logFileExtensions = self.logExtensions
+    else:
+      self.log.info('Using default list of log extensions:\n%s' %(string.join(logFileExtensions,', ')))
+
+    candidateFiles=[]
+    for ext in logFileExtensions:
+      self.log.debug('Looking at log file wildcard: %s' %ext)
+      globList = glob.glob(ext)
+      for check in globList:
+        if os.path.isfile(check):
+          self.log.debug('Found locally existing log file: %s' %check)
+          candidateFiles.append(check)
+
     selectedFiles = []
     try:
       for candidate in candidateFiles:
         fileSize = os.stat(candidate)[6]
-        if fileSize < maximumFileSize:
+        if fileSize < self.logSizeLimit:
           selectedFiles.append(candidate)
+        else:
+          self.log.error('Log file found to be greater than maximum of %s bytes' %self.logSizeLimit,candidate)
       return S_OK(selectedFiles)
     except Exception,x:
       self.log.exception('Exception while determining files to save.','',str(x))
-      return S_ERROR()
+      return S_ERROR('Could not determine log files')
 
-################################################################################
+  #############################################################################
   def populateLogDirectory(self,selectedFiles):
     """ A temporary directory is created for all the selected files.
         These files are then copied into this directory before being uploaded
@@ -192,7 +265,7 @@ class UploadLogFile(ModuleBase):
       return S_OK()
 
 
-################################################################################
+  #############################################################################
   def uploadFileToFailover(self,localFile,logicalFileName,guid=None):
     """ This method will upload the localFile supplied to a failover storage element with the supplied logical file name
     """
@@ -207,7 +280,7 @@ class UploadLogFile(ModuleBase):
     self.log.error('Failed to upload file to all failover Storage Elements.')
     return S_ERROR()
 
-################################################################################
+  #############################################################################
   def uploadDataFileToSE(self,localFile,logicalFileName,storageElement,guid,failover=False):
     """ This method uploads the supplied local file to the supplied Storage Element with the supplied LFN.
     """
@@ -215,11 +288,12 @@ class UploadLogFile(ModuleBase):
     if failover:
       catalog = 'LcgFileCatalogCombined'
     res = self.rm.putAndRegister(logicalFileName,localFile,storageElement,guid=guid,catalog=catalog)
+    self.log.verbose(res)
     # In the event that the upload failed
     if not res['OK'] or not res['Value']['Successful'].has_key(logicalFileName):
       self.log.error('Failed to putAndRegisterFile','%s %s %s' % (localFile,logicalFileName,storageElement))
       return S_ERROR()
-    self.log.info('Successfully put file in %s seconds.' % diracSE,res['Value']['Successful'][lfn]['put'])
+    self.log.info('Successfully put file %s' %(logicalFileName))
     # In the event that there were no failed registrations
     if not res['Value']['Failed'].has_key(logicalFileName):
       self.log.info('Successfully registered in catalogs')
@@ -230,7 +304,7 @@ class UploadLogFile(ModuleBase):
       self.log.error('DO SOMETHING HERE!!!!!!!!!!!!!!!!!!!!!!!!')
     return S_OK()
 
-################################################################################
+  #############################################################################
   def createLogUploadRequest(self,targetSE,logFileLFN):
     """ Set a request to upload job log files from the output sandbox
     """
@@ -246,3 +320,68 @@ class UploadLogFile(ModuleBase):
     fileDict['LFN'] = logFileLFN
     result = self.request.setSubRequestFiles(index,'logupload',[fileDict])
     return S_OK()
+
+  #############################################################################
+  def __createLogIndex(self,selectedFiles):
+    """ Create a log index page for browsing the log files.
+    """
+    productionID = self.PRODUCTION_ID
+    prodJobID = self.JOB_ID
+    wmsJobID = self.jobID
+    logFilePath = self.logFilePath
+    site = self.site
+
+    targetFile = '%s/index.html' %(self.logdir)
+    fopen = open(targetFile,'w')
+    fopen.write( """
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>\n""")
+    fopen.write("<title>Logs for Job %s of Production %s (DIRAC WMS ID %s)</title>\n" %(prodJobID,productionID,wmsJobID))
+    fopen.write( """</head>
+<body text="#000000" bgcolor="#33ffff" link="#000099" vlink="#990099"
+ alink="#000099"> \n
+""")
+    fopen.write("""<IMG SRC="%s" ALT="DIRAC" WIDTH="300" HEIGHT="120" ALIGN="right" BORDER="0">
+<br>
+""" %self.diracLogo)
+    fopen.write("<h3>Log files for  Job %s_%s </h3> \n<br>"  %(productionID,prodJobID))
+    for fileName in selectedFiles:
+      fopen.write('<a href="%s">%s</a><br> \n' %(fileName,fileName))
+
+    fopen.write("<p>Job %s_%s corresponds to WMS JobID %s executed at %s.</p><br>" %(productionID,prodJobID,wmsJobID,site))
+    fopen.write("<h3>Parameter summary for job %s_%s</h3> \n"  %(prodJobID,productionID))
+    check = ['SystemConfig','SoftwarePackages','BannedSites','LogLevel','JobType','MaxCPUTime','ProductionOutputData','LogFilePath','InputData','InputSandbox']
+    params = {}
+    for n,v in self.workflow_commons.items():
+      for item in check:
+        if n==item and v:
+          params[n]=str(v)
+
+    finalKeys = params.keys()
+    finalKeys.sort()
+    rows = ''
+    for k in finalKeys:
+      rows += """
+
+<tr>
+<td> %s </td>
+<td> %s </td>
+</tr>
+      """ %(k,params[k])
+
+    table = """<table border="1" bordercolor="#000000" width="50%" bgcolor="#BCCDFE">
+<tr>
+<td>Parameter Name</td>
+<td>Parameter Value</td>
+</tr>"""+rows+"""
+</table>
+"""
+    fopen.write(table)
+    fopen.write("""</body>
+</html>""" )
+    fopen.close()
+    return S_OK()
+
+
+  #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
