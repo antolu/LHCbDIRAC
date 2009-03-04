bc10341f6fd4038a059a7ecf168671cce7629307
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2009-03-04 15:06:24 +0000

    correction
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@14518 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-get-file-descendants.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-get-file-descendants.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-get-file-descendants.py,v 1.2 2009/03/04 09:14:53 zmathe Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-get-file-descendants.py,v 1.3 2009/03/04 15:06:24 zmathe Exp $
 # File :   dirac-bookkeeping-get-file-descendants
 # Author : Zoltan Mathe
 ########################################################################
-__RCSID__   = "$Id: dirac-bookkeeping-get-file-descendants.py,v 1.2 2009/03/04 09:14:53 zmathe Exp $"
-__VERSION__ = "$Revision: 1.2 $"
+__RCSID__   = "$Id: dirac-bookkeeping-get-file-descendants.py,v 1.3 2009/03/04 15:06:24 zmathe Exp $"
+__VERSION__ = "$Revision: 1.3 $"
 
 from DIRACEnvironment import DIRAC
 from DIRAC.Core.Base import Script
@@ -41,5 +41,13 @@ if not result['OK']:
   print 'ERROR %s' %(result['Message'])
   exitCode = 2
 else:
-  print result['Value']
+  values = result['Value']
+  print 'Successful:'
+  files = values['Successful']
+  for i in files.keys():
+    print i+':'
+    for j in files[i]:
+      print '                 '+j
+  print 'Faild:',values['Failed']
+  
 DIRAC.exit(exitCode)
