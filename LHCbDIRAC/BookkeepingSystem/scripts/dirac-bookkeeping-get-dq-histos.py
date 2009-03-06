f683f86ed46f44c1e666961911486e90130cd2cb
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-03-06 08:14:36 +0000

    some documentation for patrick.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@14581 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-get-dq-histos.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-get-dq-histos.py
@@ -9,6 +9,11 @@ from DIRAC import gLogger
 
 
 def getExpressStreamRAW():
+  """ This performs a bookkeeping query to obtain the EXPRESS stream RAW files that are in the DataQuality status 'UNCHECKED'.
+      This will get you the files for the runs that have not yet been checked.
+
+      returns a list of RAW files.
+  """
   bkDict = {'ProcessingPass':'Real Data','FileType':'RAW', 'EventType':91000000,'ConfigName':'Fest', 'ConfigVersion':'Fest','DataQualityFlag':'UNCHECKED'}
   res = bkClient.getFilesWithGivenDataSets(bkDict)
   if not res['OK']:
@@ -21,6 +26,10 @@ def getExpressStreamRAW():
   return lfns
 
 def getRunRAWFiles(runID):
+  """ This queries the BKK for ALL the files (EXPRESS and FULL stream) that are assocaited with a supplied runID (which is an int).
+  
+      returns a list of RAW files.
+  """
   res = bkClient.getRunFiles(runID)
   if not res['OK']:
     gLogger.error(res['Message'])
@@ -32,6 +41,10 @@ def getRunRAWFiles(runID):
   return lfns.keys()
 
 def getRAWDescendants(rawLfns):
+  """ This queries the BKK to get the files produced with the supplied files as input. The depth is set to two because two steps are needed to perform Brunel then DaVinci. 
+   
+      returns a dictionary of format: {'rawFile':['decendant1','decendant2']}
+  """
   # Get the files with the RAW files as input
   depth = 2 # This is because RAW->RDST,BRUNELHIST->DAVINCIHIST
   res = bkClient.getDescendents(rawLfns, depth)
@@ -44,6 +57,11 @@ def getRAWDescendants(rawLfns):
   return res['Value']['Successful']
 
 def getFilesOfInterest(rawDescendants):
+  """ This will take the dictionary of file decendants {'rawFile':['decendant1','decendant2']} and check the DQ Status of each of the decendants.
+      It is assumed here that you are interested only in the decendants that are in UNCHECKED status.
+ 
+      returns a dictionary containing the RAW files and their decendants {'rawFile':['decendant1','decendant2']}
+  """
   raw2Reco = {}
   uncheckedRAW = []
   for lfn,produced in rawDescendants.items():
