2d99be38e48a973a236e8f8e8f9224278a0651c9
roma <roma@4525493e-7705-40b1-a816-d608a930855b>
2009-03-11 08:46:03 +0000

    Agent to Monitor RANK for all CEs
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@14687 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/Agent/RANKMonitoringAgent.py
@@ -0,0 +1,112 @@
+'''
+Queries BDII to pick out information about RANK.
+'''
+
+from DIRAC import gLogger, S_OK, S_ERROR, gConfig
+from DIRAC.Core.Base.Agent import Agent
+from DIRAC.Core.Utilities.ldapsearchBDII import ldapCEState
+from DIRAC  import gMonitor
+
+import sys, os
+
+AGENT_NAME = "LHCb/RANKMonitoringAgent"
+
+class RANKMonitoringAgent(Agent):
+
+  def __init__(self):
+    """ Standard constructor
+    """
+    Agent.__init__(self,AGENT_NAME)
+
+  def initialize( self ):
+
+    result = Agent.initialize(self)
+
+    self.pollingTime = gConfig.getValue(self.section+'/PollingTime',60*10) # Every 10 minuts
+    gLogger.info("PollingTime %d minuts" %(int(self.pollingTime)/60))
+
+    self.useProxies = gConfig.getValue(self.section+'/UseProxies','True').lower() in ( "y", "yes", "true" )
+    self.proxyLocation = gConfig.getValue( self.section+'/ProxyLocation', '' )
+    if not self.proxyLocation:
+      self.proxyLocation = False
+
+    cesqueues = self._getCEsQueues()
+    if not cesqueues:
+      return cesqueues
+    self.cesqueues = cesqueues['Value']
+    
+    cesqueues = self.cesqueues
+    for ce in cesqueues:
+      for queue in cesqueues[ce]:
+        queuename = "%s:2119/%s"%(ce,queue)
+        gMonitor.registerActivity(queuename,queuename,"RANK","",gMonitor.OP_MEAN,60*10)
+      
+    return S_OK()
+
+  def execute( self ):
+
+    cesqueues = self.cesqueues
+    for ce in cesqueues:
+      for queue in self.cesqueues[ce]:
+        queuename = "%s:2119/%s"%(ce,queue)
+        cestate = ldapCEState(queuename)
+        if not cestate['OK']:
+          gLogger.warn("cestate",cestate['Message'])
+          continue
+        cestate = cestate['Value']
+        if len(cestate) != 1:
+          gLogger.warn("cestate",cestate)
+          continue
+        cestate = cestate[0]
+        
+        gLogger.debug(queuename,cestate)
+        try:
+          waitingJobs = int(cestate['GlueCEStateWaitingJobs'])
+          runningJobs = int(cestate['GlueCEStateRunningJobs'])
+          gLogger.debug('(waitingJobs,runningJobs)',queuename+str((waitingJobs,runningJobs)))
+          if waitingJobs == 0:
+            totalCPUs = int(cestate['GlueCEInfoTotalCPUs'])
+            freeCPUs = int(cestate['GlueCEStateFreeCPUs'])
+            gLogger.debug('(totalCPUs,freeCPUs)',queuename+str((totalCPUs,freeCPUs)))
+            rank = freeCPUs * 10 / totalCPUs 
+          else:  
+            rank = -waitingJobs * 10 / ( runningJobs + 1 ) - 1
+        except Exception,x:
+          gLogger.warn(queuename,x)
+          continue
+        if rank>100:
+          rank = 100
+        if rank<-100:
+          rank = -100
+          
+        gLogger.debug(queuename,rank)
+        gMonitor.addMark(queuename,rank)
+           
+    return S_OK()
+
+ 
+  def _getCEsQueues(self):
+    sites = gConfig.getSections('/Resources/Sites/LCG')
+    if sites['OK']:
+      sites = sites['Value']
+    else:
+      return sites
+       
+    cesqueues = {}
+    for site in sites:
+#      if site[-2:]!='uk':
+#        continue        
+      gLogger.debug("Site",site)
+      ces = gConfig.getSections('/Resources/Sites/LCG/%s/CEs'%site)
+      if not ces['OK']:
+        continue
+      ces = ces['Value']
+      for ce in ces:
+        queues = gConfig.getSections('/Resources/Sites/LCG/%s/CEs/%s/Queues'%(site,ce))
+        if not queues['OK']:
+          continue
+        queues = queues['Value']
+        
+      cesqueues[ce] = queues
+
+    return S_OK(cesqueues)
