5d6ecc57d167f39085704ccc6e2d02bddc44c72d
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2009-03-18 14:21:15 +0000

    Updated to treat optional 'IgnoreMissing' flag (-i).
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@14807 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/LHCbSystem/scripts/dirac-lhcb-generate-catalog.py
+++ LHCbDIRAC/LHCbSystem/scripts/dirac-lhcb-generate-catalog.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/scripts/dirac-lhcb-generate-catalog.py,v 1.3 2008/12/02 18:05:48 paterson Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/scripts/dirac-lhcb-generate-catalog.py,v 1.4 2009/03/18 14:21:15 paterson Exp $
 # File :   dirac-lhcb-generate-catalog.py
 # Author : Stuart Paterson
 ########################################################################
-__RCSID__   = "$Id: dirac-lhcb-generate-catalog.py,v 1.3 2008/12/02 18:05:48 paterson Exp $"
-__VERSION__ = "$Revision: 1.3 $"
+__RCSID__   = "$Id: dirac-lhcb-generate-catalog.py,v 1.4 2009/03/18 14:21:15 paterson Exp $"
+__VERSION__ = "$Revision: 1.4 $"
 
 from DIRACEnvironment import DIRAC
 from DIRAC.Core.Base import Script
@@ -13,6 +13,7 @@ from DIRAC.Core.Base import Script
 Script.registerSwitch( "n:", "site=", "DIRAC Site Name" )
 Script.registerSwitch( "f:", "catalog=", "Catalogue File Name e.g. can be /path/to/catalog/file.xml, defaults to pool_xml_catalog.xml in PWD" )
 Script.registerSwitch( "d:", "depth=", "Optional ancestor depth to be queried from the Bookkeeping system" )
+Script.registerSwitch( "i", "ignore", "Optional flag to ignore missing files" )
 Script.parseCommandLine( ignoreErrors = True )
 
 from DIRAC.Interfaces.API.Dirac                              import Dirac
@@ -32,6 +33,7 @@ dirac=Dirac()
 exitCode = 0
 
 siteName = ''
+ignore = False
 catalogName = 'pool_xml_catalog.xml'
 lfns = args
 ancestorDepth = 0
@@ -43,6 +45,8 @@ for switch in Script.getUnprocessedSwitches():
     catalogName=switch[1]
   elif switch[0].lower() in ('d','depth'):
     ancestorDepth=switch[1]
+  elif switch[0].lower() in ('i','ignore'):
+    ignore = True
 
 if ancestorDepth:
   try:
@@ -59,7 +63,7 @@ if ancestorDepth:
     DIRAC.exit(2)
   lfns = result['Value']
 
-result = dirac.getInputDataCatalog(lfns,siteName,catalogName)
+result = dirac.getInputDataCatalog(lfns,siteName,catalogName,ignoreMissing=ignore)
 if not result['OK']:
   exitCode=2
   print 'ERROR: %s' %result['Message']
