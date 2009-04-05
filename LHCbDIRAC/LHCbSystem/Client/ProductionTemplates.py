792ac836d8391da8103f085765ca22f70d2c0ea2
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2009-04-05 15:20:41 +0000

    - Updated interface             - histogram files are not stored by default e.g. histograms = False             - for event types defaults to first step value unless explicitly specified             - API version stored in workflow             - production creation with BK pass publishing
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@14910 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/LHCbSystem/Client/ProductionTemplates.py
+++ LHCbDIRAC/LHCbSystem/Client/ProductionTemplates.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Client/ProductionTemplates.py,v 1.1 2009/03/31 22:39:32 paterson Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Client/ProductionTemplates.py,v 1.2 2009/04/05 15:20:41 paterson Exp $
 # File :   ProductionTemplates.py
 # Author : Stuart Paterson
 ########################################################################
@@ -8,9 +8,25 @@
     (at 00:30 ;) this should be copied to the directory of choice
     and executed as a python script.
 
+    MCSimulation and reconstruction workflows are currently supported.
+
+    Notes:
+
+     - A local testing script (XML) is written by the below Production objects
+     - To perform a test that writes files etc. must set JOBID env var to an integer
+
+    History:
+
+    4/4/9 - Updated interface
+            - histogram files are not stored by default e.g. histograms = False
+            - for event types defaults to first step value unless explicitly specified
+            - API version stored in workflow
+            - production creation with BK pass publishing
+
+    31/3/9 - Initial iteration supporting MC simulation workflows
 """
 
-__RCSID__ = "$Id: ProductionTemplates.py,v 1.1 2009/03/31 22:39:32 paterson Exp $"
+__RCSID__ = "$Id: ProductionTemplates.py,v 1.2 2009/04/05 15:20:41 paterson Exp $"
 
 import os
 
@@ -21,8 +37,12 @@ except Exception,x:
 
 from DIRAC import gLogger
 
+#############################################################################
+#Simulation workflow templates
+#############################################################################
 gLogger.info('Creating worklfows for simulation productions in:\n%s' %os.getcwd())
 
+#############################################################################
 gaussGun = Production()
 gaussGun.setProdType('MCSimulation')
 gaussGun.setWorkflowName('Production_GaussGun')
@@ -30,13 +50,14 @@ gaussGun.setWorkflowDescription('An example of Gauss particle guns.')
 gaussGun.setBKParameters('test','2008','MC-Test-v1','Beam7TeV-VeloClosed-MagDown')
 gaussGun.setDBTags('sim-20090112','head-20090112')
 gaussOpts = 'Gauss-2008.py;Beam7TeV-VeloClosed-MagDown.py;$DECFILESROOT/options/@{eventType}.opts'
-gaussGun.addGaussStep('v36r2','57000001','Pythia','2',gaussOpts,'AppConfig.v2r2')
+gaussGun.addGaussStep('v36r2','Pythia','2',gaussOpts,eventType='57000001',extraPackages='AppConfig.v2r2')
 gaussGun.addFinalizationStep(sendBookkeeping=True,uploadData=True,uploadLogs=True,sendFailover=True)
 gaussGun.banTier1s()
 gaussGun.setWorkflowLib('v9r9')
-gaussGun.setFileMask('sim;root')
+gaussGun.setFileMask('sim')
 gaussGun.createWorkflow()
 
+#############################################################################
 gaussBoole = Production()
 gaussBoole.setProdType('MCSimulation')
 gaussBoole.setWorkflowName('Production_GaussBoole')
@@ -44,14 +65,15 @@ gaussBoole.setWorkflowDescription('An example of Gauss + Boole, saving all outpu
 gaussBoole.setBKParameters('test','2008','MC-Test-v1','Beam7TeV-VeloClosed-MagDown')
 gaussBoole.setDBTags('sim-20090112','head-20090112')
 gaussOpts = 'Gauss-2008.py;Beam7TeV-VeloClosed-MagDown.py;$DECFILESROOT/options/@{eventType}.opts'
-gaussBoole.addGaussStep('v36r2','57000001','Pythia','2',gaussOpts,'AppConfig.v2r2')
-gaussBoole.addBooleStep('v17r2p1','digi','30000000','Boole-2008.py','AppConfig.v2r2')
+gaussBoole.addGaussStep('v36r2','Pythia','2',gaussOpts,eventType='57000001',extraPackages='AppConfig.v2r2')
+gaussBoole.addBooleStep('v17r2p1','digi','Boole-2008.py',extraPackages='AppConfig.v2r2')
 gaussBoole.addFinalizationStep(sendBookkeeping=True,uploadData=True,uploadLogs=True,sendFailover=True)
 gaussBoole.banTier1s()
 gaussBoole.setWorkflowLib('v9r9')
-gaussBoole.setFileMask('sim;digi;root')
+gaussBoole.setFileMask('sim;digi')
 gaussBoole.createWorkflow()
 
+#############################################################################
 gaussBooleBrunel = Production()
 gaussBooleBrunel.setProdType('MCSimulation')
 gaussBooleBrunel.setWorkflowName('Production_GaussBooleBrunel')
@@ -59,15 +81,16 @@ gaussBooleBrunel.setWorkflowDescription('An example of Gauss + Boole, saving all
 gaussBooleBrunel.setBKParameters('test','2008','MC-Test-v1','Beam7TeV-VeloClosed-MagDown')
 gaussBooleBrunel.setDBTags('sim-20090112','head-20090112')
 gaussOpts = 'Gauss-2008.py;Beam7TeV-VeloClosed-MagDown.py;$DECFILESROOT/options/@{eventType}.opts'
-gaussBooleBrunel.addGaussStep('v36r2','57000001','Pythia','2',gaussOpts,'AppConfig.v2r2')
-gaussBooleBrunel.addBooleStep('v17r2p1','digi','30000000','Boole-2008.py','AppConfig.v2r2')
-gaussBooleBrunel.addBrunelStep('v34r1p1','dst','57000001','Brunel-2008-MC.py','',inputDataType='digi')
+gaussBooleBrunel.addGaussStep('v36r2','Pythia','2',gaussOpts,eventType='57000001',extraPackages='AppConfig.v2r2')
+gaussBooleBrunel.addBooleStep('v17r2p1','digi','Boole-2008.py',extraPackages='AppConfig.v2r2')
+gaussBooleBrunel.addBrunelStep('v34r1p1','dst','Brunel-2008-MC.py',extraPackages='AppConfig.v2r2',inputDataType='digi')
 gaussBooleBrunel.addFinalizationStep(sendBookkeeping=True,uploadData=True,uploadLogs=True,sendFailover=True)
 gaussBooleBrunel.banTier1s()
 gaussBooleBrunel.setWorkflowLib('v9r9')
-gaussBooleBrunel.setFileMask('sim;digi;dst;root')
+gaussBooleBrunel.setFileMask('sim;digi;dst')
 gaussBooleBrunel.createWorkflow()
 
+#############################################################################
 gaussSpillover = Production()
 gaussSpillover.setProdType('MCSimulation')
 gaussSpillover.setWorkflowName('Production_GaussSpillover')
@@ -75,16 +98,65 @@ gaussSpillover.setWorkflowDescription('An example of Gauss + Boole, saving all o
 gaussSpillover.setBKParameters('test','2008','MC-Test-v1','Beam7TeV-VeloClosed-MagDown')
 gaussSpillover.setDBTags('sim-20090112','head-20090112')
 gaussOpts = 'Gauss-2008.py;Beam7TeV-VeloClosed-MagDown.py;$DECFILESROOT/options/@{eventType}.opts'
-gaussSpillover.addGaussStep('v36r2','57000001','Pythia','2',gaussOpts,'AppConfig.v2r2')
-gaussSpillover.addGaussStep('v36r2','30000000','Pythia','2',gaussOpts,'AppConfig.v2r2')
-gaussSpillover.addGaussStep('v36r2','30000000','Pythia','2',gaussOpts,'AppConfig.v2r2')
-gaussSpillover.addBooleStep('v17r2p1','digi','57000001',"Boole-2008.py",'AppConfig.v2r2',spillover=True,pileup=True)
-gaussSpillover.addBrunelStep('v34r1p1','dst','57000001','Brunel-2008-MC.py','AppConfig.v2r2',inputDataType='digi')
+gaussSpillover.addGaussStep('v36r2','Pythia','2',gaussOpts,eventType='57000001',extraPackages='AppConfig.v2r2')
+gaussSpillover.addGaussStep('v36r2','Pythia','2',gaussOpts,eventType='30000000',extraPackages='AppConfig.v2r2')
+gaussSpillover.addGaussStep('v36r2','Pythia','2',gaussOpts,eventType='30000000',extraPackages='AppConfig.v2r2')
+gaussSpillover.addBooleStep('v17r2p1','digi','Boole-2008.py',extraPackages='AppConfig.v2r2',spillover=True,pileup=True)
+gaussSpillover.addBrunelStep('v34r1p1','dst','Brunel-2008-MC.py',extraPackages='AppConfig.v2r2',inputDataType='digi')
 gaussSpillover.addFinalizationStep(sendBookkeeping=True,uploadData=True,uploadLogs=True,sendFailover=True)
 gaussSpillover.banTier1s()
 gaussSpillover.setWorkflowLib('v9r9')
-gaussSpillover.setFileMask('sim;digi;dst;root')
+gaussSpillover.setFileMask('sim;digi;dst')
 gaussSpillover.createWorkflow()
+
+#############################################################################
+#Data Reconstruction workflow templates (initially FULL FEST and EXPRESS Stream)
+#############################################################################
+
+gLogger.info('Creating worklfows for reconstruction productions in:\n%s' %os.getcwd())
+
+#############################################################################
+expressStream = Production()
+expressStream.setProdType('DataReconstruction')
+expressStream.setWorkflowName('Production_EXPRESS_FEST')
+expressStream.setWorkflowDescription('An example of the FEST EXPRESS stream production')
+expressStream.setBKParameters('Fest','Fest','FEST-Reco-v1','DataTaking6153')
+expressStream.setDBTags('head-20090112','head-20090112')
+brunelOpts = '$APPCONFIGOPTS/Brunel/FEST-200903.py' #;$APPCONFIGOPTS/UseOracle.py'
+brunelEventType = '91000000'
+brunelData='LFN:/lhcb/data/2009/RAW/EXPRESS/FEST/FEST/44878/044878_0000000002.raw'
+brunelSE='CERN-RDST'
+expressStream.addBrunelStep('v34r2','rdst',brunelOpts,extraPackages='AppConfig.v2r2',eventType=brunelEventType,inputData=brunelData,inputDataType='mdf',outputSE=brunelSE,histograms=True)
+dvOpts = '$APPCONFIGOPTS/DaVinci/DVMonitorDst.py'
+expressStream.addDaVinciStep('v22r1','dst',dvOpts,extraPackages='AppConfig.v2r2',histograms=True)
+expressStream.addFinalizationStep(sendBookkeeping=True,uploadData=True,uploadLogs=True,sendFailover=True)
+expressStream.setWorkflowLib('v9r9')
+expressStream.setFileMask('rdst;root')
+expressStream.setProdPriority('9')
+expressStream.createWorkflow()
+
+#############################################################################
+fullStream = Production()
+fullStream.setProdType('DataReconstruction')
+fullStream.setWorkflowName('Production_FULL_FEST')
+fullStream.setWorkflowDescription('An example of the FEST EXPRESS stream production')
+fullStream.setBKParameters('Fest','Fest','FEST-Reco-v1','DataTaking6153')
+fullStream.setDBTags('head-20090112','head-20090112')
+brunelOpts = '$APPCONFIGOPTS/Brunel/FEST-200903.py' #;$APPCONFIGOPTS/UseOracle.py'
+brunelEventType = '90000000'
+brunelData='LFN:/lhcb/data/2009/RAW/FULL/FEST/FEST/43041/043041_0000000002.raw'
+fullStream.addBrunelStep('v34r2','rdst',brunelOpts,extraPackages='AppConfig.v2r2',eventType=brunelEventType,inputData=brunelData,inputDataType='mdf',histograms=True)
+dvOpts = '$APPCONFIGOPTS/DaVinci/DVMonitorDst.py'
+fullStream.addDaVinciStep('v22r1','dst',dvOpts,extraPackages='AppConfig.v2r2',histograms=True)
+fullStream.addFinalizationStep(sendBookkeeping=True,uploadData=True,uploadLogs=True,sendFailover=True)
+fullStream.setWorkflowLib('v9r9')
+fullStream.setFileMask('rdst;root')
+fullStream.setProdPriority('8')
+fullStream.createWorkflow()
+
+#############################################################################
+#Local testing script
+#############################################################################
 
 gLogger.info('Creating test script for running XML files locally (in the DIRAC environment)')
 gLogger.info('Usage: python runLocalWorkflow.py <XML FILE NAME>')
@@ -98,3 +170,4 @@ j = LHCbJob(xmlfile)
 print d.submit(j,mode='local')
 """)
 
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
