abb9177456fafc7df134bb6c37a4ba8d1fb78522
atsareg <atsareg@4525493e-7705-40b1-a816-d608a930855b>
2009-04-20 17:05:22 +0000

    Skip add files to the ProductionDB if the number of files in the BK DB did not change since the last check
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@15173 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/BookkeepingWatchAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/BookkeepingWatchAgent.py
@@ -1,12 +1,12 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/BookkeepingWatchAgent.py,v 1.3 2009/03/05 15:11:59 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/BookkeepingWatchAgent.py,v 1.4 2009/04/20 17:05:22 atsareg Exp $
 ########################################################################
 
 """  The Transformation Agent prepares production jobs for processing data
      according to transformation definitions in the Production database.
 """
 
-__RCSID__ = "$Id: BookkeepingWatchAgent.py,v 1.3 2009/03/05 15:11:59 atsareg Exp $"
+__RCSID__ = "$Id: BookkeepingWatchAgent.py,v 1.4 2009/04/20 17:05:22 atsareg Exp $"
 
 from DIRAC.Core.Base.Agent    import Agent
 from DIRAC                    import S_OK, S_ERROR, gConfig, gLogger, gMonitor
@@ -24,6 +24,7 @@ class BookkeepingWatchAgent(Agent):
     """ Standard constructor for Agent
     """
     Agent.__init__(self,AGENT_NAME)
+    self.fileLog = {}
 
   #############################################################################
   def initialize(self):
@@ -57,7 +58,7 @@ class BookkeepingWatchAgent(Agent):
       transStatus = transDict['Status']
       bkQueryID = transDict['BkQueryID']
       
-      if transStatus in ["Active","Stopped","Completed"] and bkQueryID:
+      if transStatus in ["Active"] and bkQueryID:
         result = server.getBookkeepingQuery(bkQueryID)
         if not result['OK']:
           gLogger.warn("BookkeepingWatchAgent.execute: Failed to get BkQuery", result['Message'])
@@ -84,7 +85,17 @@ class BookkeepingWatchAgent(Agent):
           gLogger.error("BookkeepingWatchAgent.execute: Failed to get response from the Bookkeeping", result['Message'])
           continue
           
-        lfnList = result['Value']        
+        lfnList = result['Value']   
+        
+        # Check of the number of files has changed since the last cycle
+        nlfns = len(lfnList)
+        gLogger.verbose('%d files available for production %d in BK DB' % (nlfns,int(transID)) )
+        if self.fileLog.has_key(transID):
+          if nlfns == self.fileLog[transID]:
+            gLogger.verbose('No new files in BK DB since last check, skipping ...')
+            continue
+        self.fileLog[transID] = nlfns       
+             
         if lfnList:
           gLogger.verbose('Processing %d lfns for transformation %d' % (len(lfnList),transID) )
           fileList = []
@@ -94,17 +105,20 @@ class BookkeepingWatchAgent(Agent):
           result = server.addFile(fileList,True)
           if not result['OK']:
             gLogger.error("BookkeepingWatchAgent.execute: Failed to add files", result['Message'])
+            self.fileLog[transID] = 0
             continue  
 
           successfulList = result['Value']['Successful']
           failedList = result['Value']['Failed']
           if failedList:
             gLogger.warn("BookkeepingWatchAgent.execute: adding of %d files failed" % len(failedList), result['Message'])  
+            self.fileLog[transID] = 0
 
           lfns = successfulList.keys() 
           gLogger.verbose('Adding %d lfns for transformation %d' % (len(lfns),transID) )
           result = server.addLFNsToTransformation(lfns,transID)
           if not result['OK']:
             gLogger.warn("BookkeepingWatchAgent.execute: failed to add lfns to transformation", result['Message'])   
+            self.fileLog[transID] = 0
     
     return S_OK() 
