86d8fe8e0a5219f5fd1a05d2610b7f0c59454433
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-05-11 12:17:24 +0000

    do not change the status after a flush.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@15587 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
@@ -1,12 +1,12 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/TransformationAgent.py,v 1.26 2008/09/23 11:17:14 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/TransformationAgent.py,v 1.27 2009/05/11 12:17:24 acsmith Exp $
 ########################################################################
 
 """  The Transformation Agent prepares production jobs for processing data
      according to transformation definitions in the Production database.
 """
 
-__RCSID__ = "$Id: TransformationAgent.py,v 1.26 2008/09/23 11:17:14 atsareg Exp $"
+__RCSID__ = "$Id: TransformationAgent.py,v 1.27 2009/05/11 12:17:24 acsmith Exp $"
 
 from DIRAC.Core.Base.Agent      import Agent
 from DIRAC                      import S_OK, S_ERROR, gConfig, gLogger, gMonitor
@@ -95,11 +95,6 @@ class TransformationAgent(Agent):
             nJobs = result['Value']
             if nJobs > 0:
               gLogger.info('%d job(s) generated' % nJobs)
-            result = server.setTransformationStatus(transID, 'Stopped')
-            if not result['OK']:
-              gLogger.error(self.name+".execute: Failed to update transformation status to 'Stopped'.", res['Message'])
-            else:
-              gLogger.info(self.name+".execute: Updated transformation status to 'Stopped'.")
 
         # skip the transformations of other statuses
         else:
