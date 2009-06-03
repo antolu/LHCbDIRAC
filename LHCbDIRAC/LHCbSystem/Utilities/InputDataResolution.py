bb713c4097b909fe37253d796d645c0c2f86b57d
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2009-06-03 20:38:04 +0000

    Temporarily don't prepend root: for test at NIKHEF.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@15822 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/LHCbSystem/Utilities/InputDataResolution.py
+++ LHCbDIRAC/LHCbSystem/Utilities/InputDataResolution.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Id: InputDataResolution.py,v 1.5 2009/04/18 18:26:57 rgracian Exp $
+# $Id: InputDataResolution.py,v 1.6 2009/06/03 20:38:04 paterson Exp $
 # File :   InputDataResolution.py
 # Author : Stuart Paterson
 ########################################################################
@@ -14,7 +14,7 @@
 
 """
 
-__RCSID__ = "$Id: InputDataResolution.py,v 1.5 2009/04/18 18:26:57 rgracian Exp $"
+__RCSID__ = "$Id: InputDataResolution.py,v 1.6 2009/06/03 20:38:04 paterson Exp $"
 
 from DIRAC.Core.Utilities.ModuleFactory                             import ModuleFactory
 from DIRAC.WorkloadManagementSystem.Client.PoolXMLSlice             import PoolXMLSlice
@@ -83,8 +83,8 @@ class InputDataResolution:
       tmpDict[lfn]=mdata
       if re.search('.raw$',lfn):
         #correctedTURL = 'root:%s' %(val)
-        tmpDict[lfn].update({'turl':'root:%s' %(resolvedData[lfn]['turl'])})
-        self.log.verbose('Prepending root: to TURL for %s' %lfn)
+        #tmpDict[lfn].update({'turl':'root:%s' %(resolvedData[lfn]['turl'])})
+        self.log.verbose('Would have been prepending root: to TURL for %s (DISABLED)' %lfn)
 
     resolvedData = tmpDict
     catalogName = 'pool_xml_catalog.xml'
