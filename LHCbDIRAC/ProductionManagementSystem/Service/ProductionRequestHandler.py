e4f6ab75eb67620e8420f6c1f2d1e5ce0363f49a
azhelezo <azhelezo@4525493e-7705-40b1-a816-d608a930855b>
2009-06-15 11:43:23 +0000

    NEW: file based templates
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@16000 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Service/ProductionRequestHandler.py
+++ LHCbDIRAC/ProductionManagementSystem/Service/ProductionRequestHandler.py
@@ -1,10 +1,11 @@
-# $Id: ProductionRequestHandler.py,v 1.3 2009/03/06 17:01:34 azhelezo Exp $
+# $Id: ProductionRequestHandler.py,v 1.4 2009/06/15 11:43:23 azhelezo Exp $
 """
 ProductionRequestHandler is the implementation of
 the Production Request service
 """
-__RCSID__ = "$Revision: 1.3 $"
+__RCSID__ = "$Revision: 1.4 $"
 
+import os
 import re
 
 from types import *
@@ -13,6 +14,7 @@ from DIRAC.Core.DISET.RequestHandler import RequestHandler
 from DIRAC import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.ProductionManagementSystem.DB.ProductionRequestDB import ProductionRequestDB
 from DIRAC.Core.DISET.RPCClient import RPCClient
+from DIRAC.ConfigurationSystem.Client import PathFinder
 
 # This is a global instance of the ProductionRequestDB class
 productionRequestDB = False
@@ -138,3 +140,66 @@ class ProductionRequestHandler( RequestHandler ):
     """ Update tracked productions (used by Agent)
     """
     return self.database.updateTrackedProductions(update)
+
+  def __getTplFolder(self):
+    csS=PathFinder.getServiceSection( 'ProductionManagement/ProductionRequest' )
+    if not csS:
+      return S_ERROR("No ProductionRequest parameters in CS")
+    tplFolder = gConfig.getValue('%s/templateFolder' % csS,'')
+    if not tplFolder:
+      return S_ERROR("No templateFolder in ProductionRequest parameters in CS")
+    if not os.path.exists(tplFolder) or not os.path.isdir(tplFolder):
+      return S_ERROR("Template Folder %s doesn't exist" % tplFolder)
+    return S_OK(tplFolder)
+
+  def __getTemplate(self,name):
+    ret = self.__getTplFolder()
+    if not ret['OK']:
+      return ret
+    tplFolder = ret['Value']
+    if not os.path.exists(os.path.join(tplFolder,name)):
+      return S_ERROR("Template %s doesn't exist" % name)
+    try:
+      f = open(os.path.join(tplFolder,name))
+      body = f.read()
+      f.close()
+    except Exception, e:
+      return S_ERROR("Can't read template (%s)" % str(e))
+    return S_OK(body)
+
+  types_getProductionTemplateList = []
+  def export_getProductionTemplateList(self):
+    """ Return production template list (file based) """
+    ret = self.__getTplFolder()
+    if not ret['OK']:
+      return ret
+    tplFolder = ret['Value']
+    tpls = [x for x in os.listdir(tplFolder) \
+            if os.path.isfile(os.path.join(tplFolder,x))]
+    results = []
+    for t in tpls:
+      if t[-1] == '~':
+        continue
+      result = self.__getTemplate(t)
+      if not result['OK']:
+        return result
+      body = result['Value']
+      m = re.search("\$Id: ProductionRequestHandler.py,v 1.4 2009/06/15 11:43:23 azhelezo Exp $]*) \$",body)
+      ptime = ''
+      author = ''
+      ver = ''
+      if m:
+        m = re.match("[^ ]+ ([^ ]+) ([^ ]+ [^ ]+) ([^ ]+) Exp",m.group(1))
+        if m:
+          ptime = m.group(2)
+          author = m.group(3)
+          ver = m.group(1)
+          tpl = { "AuthorGroup": '', "Author": author, "PublishingTime": ptime,
+                  "LongDescription": '', "WFName": t, "AuthorDN": '',
+                  "WFParent": '', "Description": ver }
+          results.append(tpl)
+    return S_OK(results)
+
+  types_getProductionTemplate = [StringType]
+  def export_getProductionTemplate(self,name):
+    return self.__getTemplate(name)
