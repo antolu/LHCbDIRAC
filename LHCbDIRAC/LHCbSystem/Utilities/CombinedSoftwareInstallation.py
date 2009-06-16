687c4c64249f86fa64c8936410d51f146af913c5
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2009-06-16 11:31:36 +0000

    debugging
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@16039 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/LHCbSystem/Utilities/CombinedSoftwareInstallation.py
+++ LHCbDIRAC/LHCbSystem/Utilities/CombinedSoftwareInstallation.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Id: CombinedSoftwareInstallation.py,v 1.30 2009/06/16 11:22:33 rgracian Exp $
+# $Id: CombinedSoftwareInstallation.py,v 1.31 2009/06/16 11:31:36 rgracian Exp $
 # File :   CombinedSoftwareInstallation.py
 # Author : Ricardo Graciani
 ########################################################################
@@ -21,8 +21,8 @@
     on the Shared area
     If this is not possible it will do a local installation.
 """
-__RCSID__   = "$Id: CombinedSoftwareInstallation.py,v 1.30 2009/06/16 11:22:33 rgracian Exp $"
-__VERSION__ = "$Revision: 1.30 $"
+__RCSID__   = "$Id: CombinedSoftwareInstallation.py,v 1.31 2009/06/16 11:31:36 rgracian Exp $"
+__VERSION__ = "$Revision: 1.31 $"
 
 import os, shutil, sys, urllib, re, string
 import DIRAC
@@ -84,6 +84,8 @@ class CombinedSoftwareInstallation:
       DIRAC.gLogger.error( 'No architecture requested' )
       return DIRAC.S_ERROR( 'No architecture requested' )
       
+    print self.ce
+    sys.exit()
     if self.ce == 'DIRAC.ONLINE-FARM.ch':
       return onlineExecute( self.workflow_commons )
     
