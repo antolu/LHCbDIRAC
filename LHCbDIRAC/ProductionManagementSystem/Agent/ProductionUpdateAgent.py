fcb8ad903fc3b4bc523de613827934377a284c59
atsareg <atsareg@4525493e-7705-40b1-a816-d608a930855b>
2009-06-19 06:56:47 +0000

    Treat Stalled jobs as non-final status
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@16082 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/ProductionUpdateAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/ProductionUpdateAgent.py
@@ -1,12 +1,12 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/ProductionUpdateAgent.py,v 1.11 2009/01/28 17:26:46 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/ProductionUpdateAgent.py,v 1.12 2009/06/19 06:56:47 atsareg Exp $
 ########################################################################
 
 """  The Transformation Agent prepares production jobs for processing data
      according to transformation definitions in the Production database.
 """
 
-__RCSID__ = "$Id: ProductionUpdateAgent.py,v 1.11 2009/01/28 17:26:46 atsareg Exp $"
+__RCSID__ = "$Id: ProductionUpdateAgent.py,v 1.12 2009/06/19 06:56:47 atsareg Exp $"
 
 from DIRAC.Core.Base.Agent    import Agent
 from DIRAC                    import S_OK, S_ERROR, gConfig, gLogger, gMonitor
@@ -16,11 +16,11 @@ import os, time
 
 
 AGENT_NAME = 'ProductionManagement/ProductionUpdateAgent'
-UPDATE_STATUS = ['Created','Submitted','Received','Checking','Staging','Waiting','Matched','Running']
+UPDATE_STATUS = ['Created','Submitted','Received','Checking','Staging','Waiting','Matched','Running','Stalled']
 #UPDATE_STATUS = []
 WAITING_STATUS = ['Submitted','Received','Checking','Staging','Waiting']
-RUNNING_STATUS = ['Running']
-FINAL_STATUS = ['Done','Failed','Stalled','Completed']
+RUNNING_STATUS = ['Running','Stalled']
+FINAL_STATUS = ['Done','Failed','Completed']
 # Update job statuses no more frequently than NEWER minutes
 NEWER = 1
 
@@ -76,23 +76,32 @@ class ProductionUpdateAgent(Agent):
         if jobWMS in statusDict.keys():
           status = statusDict[jobWMS]['Status']    
         else:
-          status = "Removed"    
+          status = "Removed"   
+           
         if old_status != status:
-          gLogger.verbose('Setting job status for Production/Job %d/%d to %s' % (transID,jobID,status))
-          result = self.prodDB.setJobStatus(transID,jobID,status)
-          if not result['OK']:
-            gLogger.warn('Failed to set job status for jobID: '+str(jobID))
-        if old_status == "Submitted" and status in WAITING_STATUS:
-          result = self.prodDB.getJobInfo(transID,jobID)
-          if not result['OK']:
-            gLogger.warn('Failed to get job info for production %d job %d' % (int(transID),int(jobID)))    
-        elif old_status in WAITING_STATUS and status in RUNNING_STATUS:
-          result = self.prodDB.getJobInfo(transID,jobID)
-          if not result['OK']:
-            gLogger.warn('Failed to get job info for production %d job %d' % (int(transID),int(jobID)))
-        elif old_status in RUNNING_STATUS and status in FINAL_STATUS:
-          result = self.prodDB.getJobInfo(transID,jobID)
-          if not result['OK']:
-            gLogger.warn('Failed to get job info for production %d job %d' % (int(transID),int(jobID)))
+          if status == "Removed":
+            gLogger.verbose('Production/Job %d/%d removed from WMS while it is in %s status' % (transID,jobID,old_status))
+            gLogger.verbose('Setting Production/Job job to Failed')
+            result = self.prodDB.setJobStatus(transID,jobID,'Failed')
+            if not result['OK']:
+              gLogger.warn('Failed to set job status for jobID: '+str(jobID))
+          else:
+            gLogger.verbose('Setting job status for Production/Job %d/%d to %s' % (transID,jobID,status))
+            result = self.prodDB.setJobStatus(transID,jobID,status)
+            if not result['OK']:
+              gLogger.warn('Failed to set job status for jobID: '+str(jobID))
+            
+#        if old_status == "Submitted" and status in WAITING_STATUS:
+#          result = self.prodDB.getJobInfo(transID,jobID)
+#          if not result['OK']:
+#            gLogger.warn('Failed to get job info for production %d job %d' % (int(transID),int(jobID)))    
+#        elif old_status in WAITING_STATUS and status in RUNNING_STATUS:
+#          result = self.prodDB.getJobInfo(transID,jobID)
+#          if not result['OK']:
+#            gLogger.warn('Failed to get job info for production %d job %d' % (int(transID),int(jobID)))
+#        elif old_status in RUNNING_STATUS and status in FINAL_STATUS:
+#          result = self.prodDB.getJobInfo(transID,jobID)
+#          if not result['OK']:
+#            gLogger.warn('Failed to get job info for production %d job %d' % (int(transID),int(jobID)))
 
     return S_OK()
