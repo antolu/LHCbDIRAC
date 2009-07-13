c9d461a1f79c06f171e084804e554291330d64ce
atsareg <atsareg@4525493e-7705-40b1-a816-d608a930855b>
2009-07-13 23:55:34 +0000

    Check for Reserved jobs only for productions in Active or Stopped status
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@16589 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/ProductionJobAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/ProductionJobAgent.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/ProductionJobAgent.py,v 1.17 2009/07/12 12:46:17 atsareg Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/ProductionJobAgent.py,v 1.18 2009/07/13 23:55:34 atsareg Exp $
 ########################################################################
 
 """  The Production Job Agent automatically submits production jobs after
@@ -8,7 +8,7 @@
      Dirac Production interface to submit the jobs.
 """
 
-__RCSID__ = "$Id: ProductionJobAgent.py,v 1.17 2009/07/12 12:46:17 atsareg Exp $"
+__RCSID__ = "$Id: ProductionJobAgent.py,v 1.18 2009/07/13 23:55:34 atsareg Exp $"
 
 from DIRAC.Core.Base.AgentModule                          import AgentModule
 from DIRAC.Core.DISET.RPCClient                           import RPCClient
@@ -108,6 +108,9 @@ class ProductionJobAgent(AgentModule):
 
     for prod in result['Value']:
       production = int(prod['TransID'])
+      prodStatus = prod['Status']
+      if not prodStatus in ['Active','Stopped']:
+        continue
       time_stamp_older = str(datetime.datetime.utcnow() - datetime.timedelta(hours=1))
       time_stamp_newer = str(datetime.datetime.utcnow() - datetime.timedelta(days=7))
       # Get Reserved jobs - 100 at a time
