9c1840d375a139c875edacbb1b6fbd7495d1bb39
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2009-07-23 15:15:10 +0000

    add exception for LCG.CERN5.ch
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@16661 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/LHCbSystem/Testing/SAM/Modules/SoftwareInstallation.py
+++ LHCbDIRAC/LHCbSystem/Testing/SAM/Modules/SoftwareInstallation.py
@@ -1,5 +1,5 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Testing/SAM/Modules/SoftwareInstallation.py,v 1.38 2009/07/16 11:32:56 rgracian Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Testing/SAM/Modules/SoftwareInstallation.py,v 1.39 2009/07/23 15:15:10 joel Exp $
 # Author : Stuart Paterson
 ########################################################################
 
@@ -11,7 +11,7 @@
 
 """
 
-__RCSID__ = "$Id: SoftwareInstallation.py,v 1.38 2009/07/16 11:32:56 rgracian Exp $"
+__RCSID__ = "$Id: SoftwareInstallation.py,v 1.39 2009/07/23 15:15:10 joel Exp $"
 
 import DIRAC
 from DIRAC import S_OK, S_ERROR, gLogger, gConfig
@@ -142,7 +142,7 @@ class SoftwareInstallation(ModuleBaseSAM):
       isPoolAccount = True
 
     #nasty fix but only way to resolve writeable volume at CERN
-    if DIRAC.siteName()=='LCG.CERN.ch':
+    if DIRAC.siteName()=='LCG.CERN.ch' or self.site=='LCG.CERN5.ch':
       self.log.info('Changing shared area path to writeable volume at CERN')
       if re.search('.cern.ch',sharedArea):
         newSharedArea = sharedArea.replace('cern.ch','.cern.ch')
