a4fc1e33edcaca8be38aac9fc6dc0d996494d17f
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2009-08-03 13:28:15 +0000

    LHCbSystem.WorkflowTemplates
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@16785 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/WorkflowTemplates/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/WorkflowTemplates/__init__.py,v 1.1 2009/08/03 13:28:15 paterson Exp $
+############################################################
+
+"""
+   LHCbSystem.WorkflowTemplates package
+
+   This contains the workflow templates for the production request system.
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2009/08/03 13:28:15 paterson Exp $"
