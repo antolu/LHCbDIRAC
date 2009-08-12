4e3b37d33221604d683864fdaf7d2ababac58447
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2009-08-12 08:12:05 +0000

    New Version with arguments instead of having to edit the file
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@16977 4525493e-7705-40b1-a816-d608a930855b

--- scripts/install_dirac_site.sh
+++ scripts/install_dirac_site.sh
@@ -1,109 +1,182 @@
 #!/bin/bash
 ########################################################################
-# File :   install_base.sh
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/scripts/install_dirac_site.sh,v 1.8 2009/08/12 08:12:05 rgracian Exp $
+# File:    install_dirac_site.sh
+# Author : Florian Feldhaus, Ricardo Graciani
 ########################################################################
 #
-# User that is allowed to execute the script
-DIRACUSER=dirac
+# Some Default Variables that can be over written with proper arguments
 #
-# Host where it is allowed to run the script
-DIRACHOST=YOUR_HOSTNAME
+# User that is allowed to execute the script (default: dirac)
+DIRACUSER=dirac
 #
 # Location of the DIRAC site installation (default: /opt/dirac)
 DESTDIR=/opt/dirac
 #
-# Your queue for DIRAC jobs (batch is the default queue of Torque)
-Queue=batch
+# Your submission Queue for DIRAC jobs (batch is the default queue of Torque)
+Queue=default
+#
+# Log Level for installed Agents
+LOGLEVEL=VERBOSE
+#
+
+#
+# Some Variable with no default that must be set by proper arguments
+#
 # The name of your site given to you by the DIRAC admins
-SiteName=YOUR_SITE_NAME
-# The path to your shared area (default: /opt/shared)
-SharedArea=/opt/shared
+SiteName=""
+#
+# Your execution Queue for DIRAC jobs (in case there submission is a routing queue)
+ExecQueue=""
+#
+# Dirac version to install
+DIRACVERSION=""
+#
+# Dirac Architecture as determined with the platform.py script (default: DIRAC Native Platform)
+DIRACARCH=""
+#
 # If you have a proxy server which is able to handle big files up to 1GB config it here
 # example: HttpProxy=http://yourproxy.yourdomain.tld:3128
-HttpProxy=
-# Dirac Setup (e.g. LHCb-Production or LHCb-Development)
-DIRACSETUP=LHCb-Development
-# Dirac version to install (default: HEAD)
-DIRACVERSION=HEAD
-# Version of the external applications to install (default: HEAD)
-EXTVERSION=HEAD
-# Dirac Architecture as determined by the platform.py script
-DIRACARCH=Linux_x86_64_glibc-2.3.4
+HttpProxy=""
+#
+# The path to your shared area (default: /opt/shared)
+SharedArea=/opt/shared
+
+usage(){
+echo Usage: $0 
+echo "  -n --name SiteName      Set Site Name (mandatory)"
+echo "  -v --version Version    DIRAC Version to install (mandatory)"
+echo "  -L --LogLevel           LogLevel for installed Components"
+echo "  -P --path Path          Site Installation PATH (default: $DESTDIR)"
+echo "  -Q --Queue Queue        Batch System submit Queue (default: $Queue)"
+echo "  -E --ExecQueue Queue    Batch System executing Queue (default: same as Queue)"
+echo "  -U --User UserName      User executing the script (default: $DIRACUSER)"
+echo "  -p --platform Platform  Use Platform instead of local one"
+echo "  -h --help               Print this"
+exit 0
+}
+
+error_exit(){
+echo
+echo ERROR: $1
+echo
+exit 1
+}
+
+# 
 # Python Version to use
 DIRACPYTHON=24
 # Directories to create at DESTDIR
-DIRACDIRS="startup runit data work control requestDB"
+DIRACDIRS="startup runit data work control"
+#
+# Dirac Setup (e.g. LHCb-Production or LHCb-Development)
+DIRACSETUP=LHCb-Development
+DIRACINSTANCE=Development
+#
+# Needs to be exported for the install_agent.sh script to make use of it.
+export LOGLEVEL
+#
+while [ $1 ]
+do
+  case $1 in
 
-export DIRACINSTANCE=Development
-export LOGLEVEL=VERBOSE
+  -h | --help )
+    usage
+  ;;
+  -L | --LogLevel )
+    switch=$1
+    shift
+    [ $1 ] || error_exit "Switch $switch requires a argument"
+    LOGLEVEL=$1   
+  ;;
+  -n | --name )
+    switch=$1
+    shift
+    [ $1 ] || error_exit "Switch $switch requires a argument"
+    SiteName=$1   
+  ;;
+  -p | --platform )
+    switch=$1
+    shift
+    [ $1 ] || error_exit "Switch $switch requires a argument"
+    DIRACARCH=$1   
+  ;;
+  -P | --path )
+    switch=$1
+    shift
+    [ $1 ] || error_exit "Switch $switch requires a argument"
+    DESTDIR=$1   
+  ;;
+  -Q | --Queue )
+    switch=$1
+    shift
+    [ $1 ] || error_exit "Switch $switch requires a argument"
+    Queue=$1   
+  ;;
+  -E | --ExecQueue )
+    switch=$1
+    shift
+    [ $1 ] || error_exit "Switch $switch requires a argument"
+    ExecQueue=$1   
+  ;;
+  -U | --User )
+    switch=$1
+    shift
+    [ $1 ] || error_exit "Switch $switch requires a argument"
+    DIRACUSER=$1
+  ;;
+  -v | --version )
+    switch=$1
+    shift
+    [ $1 ] || error_exit "Switch $switch requires a argument"
+    DIRACVERSION=$1
+  ;;
+  * )
+    error_exit "Unkown argument switch $1"
+  ;;
+
+  esac
+  shift
+done
+
+[ -z "$SiteName" -o  -z "$DIRACVERSION" ] && error_exit "Missing mandatory argument, use -h for help"
+[ -z "$ExecQueue" ] && ExecQueue=$Queue
+
 #
-# check if we are called in the rigth host
-if [ "`hostname`" != "$DIRACHOST" ] ; then
-  echo $0 should be run at $DIRACHOST
-fi
-# check if we are the right user
-if [ $USER != $DIRACUSER ] ; then
-  echo $0 should be run by $DIRACUSER
-  exit
-fi
-# check if the mask is properly set
-if [ "`umask`" != "0002" ] && [ "`umask`" != "0022" ]; then
-  echo umask should be set to 0022 or 0002 at system level for users
-  exit
-fi
+# check we are the right user
+[ "$USER" == "$DIRACUSER" ] || error_exit "$0 should be run by user: $DIRACUSER"
 #
-# make sure $DESTDIR is available
-mkdir -p $DESTDIR || exit 1
+# check $DESTDIR is available
+mkdir -p $DESTDIR || error_exit "Install directory $DESTDIR not available"
 
 CURDIR=`dirname $0`
 CURDIR=`cd $CURDIR; pwd -P`
 
-ROOT=`dirname $DESTDIR`/DIRAC3
+ROOT=$DESTDIR/DIRAC
 
 echo
-echo "Installing under $ROOT"
+echo "Installing under $DESTDIR"
+echo
+echo "Installing DIRAC under $ROOT"
 echo
 [ -L $ROOT ] || ln -sf $DESTDIR/pro $ROOT || exit
 
-if [ ! -d $DESTDIR/etc ]; then
-  mkdir -p $DESTDIR/etc || exit 1
+if [ ! -d $DESTDIR/etc/grid-security/certificates ]; then
+  mkdir -p $DESTDIR/etc/grid-security/certificates || exit 1
 fi
 if [ ! -e $DESTDIR/etc/dirac.cfg ] ; then
   cat >> $DESTDIR/etc/dirac.cfg << EOF || exit
 DIRAC
 {
   Setup = $DIRACSETUP
-  Setups
-  {
-    LHCb-Development
-    {
-      WorkloadManagement = Development
-      LHCb = Development
-    }
-  }
-  Configuration
-  {
-    Servers = dips://volhcb04.cern.ch:9135/Configuration/Server
-    #Servers = dips://lhcbprod.pic.es:9135/Configuration/Server
-    #Servers += dips://lhcb-wms-dirac.cern.ch:9135/Configuration/Server
-    Name = LHCb-Devel
-  }
   Security
   {
     CertFile = $DESTDIR/etc/grid-security/hostcert.pem
     KeyFile = $DESTDIR/etc/grid-security/hostkey.pem
   }
 }
-LocalSite
-{
-  HttpProxy = $HttpProxy
-  Queue = $Queue
-  SharedArea = $SharedArea 
-  WaitingToRunningRatio = 0.5
-  MaxWaitingJobs = 50
-  MaxNumberJobs = 10000
-}
 EOF
+
 
 fi
 
@@ -125,28 +198,43 @@ done
 dir=`echo $DESTDIR/pro/$DIRACARCH/bin | sed 's/\//\\\\\//g'`
 PATH=`echo $PATH | sed "s/$dir://"`
 
-echo $CURDIR/dirac-install -S -P $VERDIR -v $DIRACVERSION -e $EXTVERSION -p $DIRACARCH -i $DIRACPYTHON -o /LocalSite/Root=$ROOT -o /LocalSite/Site=$SiteName 2>/dev/null || exit 1
-     $CURDIR/dirac-install -S -P $VERDIR -v $DIRACVERSION -e $EXTVERSION -p $DIRACARCH -i $DIRACPYTHON -o /LocalSite/Root=$ROOT -o /LocalSite/Site=$SiteName || exit 1
+Install_Options="-S -v $DIRACVERSION -P $VERDIR -i $DIRACPYTHON -o /LocalSite/Site=$SiteName"
+[ $EXTVERSION ] && Install_Options="$Install_Options -e $EXTVERSION"
+[ $DIRACARCH ] && Install_Options="$Install_Options -p $DIRACARCH"
+[ "$LOGLEVEL" == "DEBUG" ] && Install_Options="$Install_Options -d"
+Install_Options="$Install_Options -o /DIRAC/Security/SkipCAChecks=yes"
 
+python $CURDIR/dirac-install $Install_Options || exit 1
 #
 # Create pro and old links
 old=$DESTDIR/old
 pro=$DESTDIR/pro
 [ -L $old ] && rm $old; [ -e $old ] && exit 1; [ -L $pro ] && mv $pro $old; [ -e $pro ] && exit 1; ln -s $VERDIR $pro || exit 1
 
+[ $DIRACARCH ] || DIRACARCH=`$DESTDIR/pro/scripts/platform.py`
+
 #
 # Create bin link
 ln -sf pro/$DIRACARCH/bin $DESTDIR/bin
+
+chmod +x $DESTDIR/pro/scripts/install_bashrc.sh
+$DESTDIR/pro/scripts/install_bashrc.sh    $DESTDIR $DIRACVERSION $DIRACARCH python$DIRACPYTHON 2>&1 1>/dev/null|| exit 1
+
 ##
 ## Compile all python files .py -> .pyc, .pyo
 ##
 cmd="from compileall import compile_dir ; compile_dir('"$DESTDIR/pro"', force=1, quiet=True )"
 $DESTDIR/pro/$DIRACARCH/bin/python -c "$cmd" 1> /dev/null || exit 1
 $DESTDIR/pro/$DIRACARCH/bin/python -O -c "$cmd" 1> /dev/null  || exit 1
-
-chmod +x $DESTDIR/pro/scripts/install_bashrc.sh
-$DESTDIR/pro/scripts/install_bashrc.sh    $DESTDIR $DIRACVERSION $DIRACARCH python$DIRACPYTHON || exit 1
-
+#
+# Retrive last version of CA's
+#
+# To be done
+$DESTDIR/pro/scripts/dirac-configuration-dump-local-cache -f ttt.cfg
+#
+# Remove any reference to SkipCAChecks in dirac.cfg
+#
+sed -i 's/ *SkipCAChecks.*//' $DESTDIR/etc/dirac.cfg
 #
 # fix user .bashrc
 #
@@ -157,31 +245,7 @@ cp $CURDIR/dirac-install $DESTDIR/pro/scripts
 
 ##############################################################
 # INSTALL SERVICES
-# (modify SystemName and ServiceName)
 #
-
-#$DESTDIR/pro/scripts/install_service.sh SystemName ServiceName
-
-# If any special CS entried required modify and uncomment the following:
-
-#cat > $DESTDIR/etc/SystemName_ServiceName.cfg <<EOF
-#Systems
-#{
-#  SystemName
-#  {
-#    $DIRACINSTANCE
-#    {
-#      Services
-#      {
-#        ServiceName
-#        {
-#          Option = Value
-#        }
-#      }
-#    }
-#  }
-#}
-#EOF
 
 ##############################################################
 # INSTALL AGENTS
@@ -190,11 +254,21 @@ cp $CURDIR/dirac-install $DESTDIR/pro/scripts
 $DESTDIR/pro/scripts/install_agent.sh WorkloadManagement TaskQueueDirector
 
 cat > $DESTDIR/etc/WorkloadManagement_TaskQueueDirector.cfg <<EOF
+LocalSite
+{
+  HttpProxy = $HttpProxy
+  Queue = $Queue
+  ExecQueue = $ExecQueue
+  SharedArea = $SharedArea 
+  WaitingToRunningRatio = 0.5
+  MaxWaitingJobs = 50
+  MaxNumberJobs = 10000
+}
 Systems
 {
   WorkloadManagement
   {
-    Development
+    $DIRACINSTANCE
     {
       Agents
       {
