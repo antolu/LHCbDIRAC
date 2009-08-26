18247d1a83baba6da4b79b17760f6c38f25f6fe0
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2009-08-26 07:56:53 +0000

    typo availablity -> availability
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@17096 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
@@ -1,12 +1,12 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/TransformationAgent.py,v 1.34 2009/08/19 13:58:39 acsmith Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/TransformationAgent.py,v 1.35 2009/08/26 07:56:53 rgracian Exp $
 ########################################################################
 
 """  The Transformation Agent prepares production jobs for processing data
      according to transformation definitions in the Production database.
 """
 
-__RCSID__ = "$Id: TransformationAgent.py,v 1.34 2009/08/19 13:58:39 acsmith Exp $"
+__RCSID__ = "$Id: TransformationAgent.py,v 1.35 2009/08/26 07:56:53 rgracian Exp $"
 
 from DIRAC.Core.Base.Agent      import Agent
 from DIRAC                      import S_OK, S_ERROR, gConfig, gLogger, gMonitor
@@ -99,10 +99,10 @@ class TransformationAgent(Agent):
             nJobs = result['Value']
             if nJobs > 0:
               gLogger.info('%d job(s) generated' % nJobs)
-            result = server.setTransformationStatus(transID, 'Active') 	 
-            if not result['OK']: 	 
-              gLogger.error(self.name+".execute: Failed to update transformation status to 'Active'.", res['Message']) 	 
-            else: 	 
+            result = server.setTransformationStatus(transID, 'Active')
+            if not result['OK']:
+              gLogger.error(self.name+".execute: Failed to update transformation status to 'Active'.", res['Message'])
+            else:
               gLogger.info(self.name+".execute: Updated transformation status to 'Active'.")
 
         # skip the transformations of other statuses
@@ -503,7 +503,7 @@ class TransformationAgent(Agent):
     return S_OK(lfc_data)
 
   def checkAncestors(self,lfn,ancestorDepth):
-    """ Check ancestor availablity on sites. Returns a list of SEs where all the ancestors
+    """ Check ancestor availability on sites. Returns a list of SEs where all the ancestors
         are present
     """
 
