89243ab9dab735fd10e4b312d813dc232c281110
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-09-08 13:04:14 +0000

    added getParameters
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@17259 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Client/ProductionClient.py
+++ LHCbDIRAC/ProductionManagementSystem/Client/ProductionClient.py
@@ -1,7 +1,7 @@
 """ Client class to access the production manager service
 """
-# $Id: ProductionClient.py,v 1.1 2009/09/08 12:50:39 acsmith Exp $
-__RCSID__ = "$Revision: 1.1 $"
+# $Id: ProductionClient.py,v 1.2 2009/09/08 13:04:14 acsmith Exp $
+__RCSID__ = "$Revision: 1.2 $"
 
 from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.Core.Utilities.List import breakListIntoChunks
@@ -20,3 +20,13 @@ class ProductionClient:
   def getAllProductions(self):
     server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
     return server.getAllProductions()
+
+  def getParameters(self,prodID,pname=''):
+    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
+    res = server.getTransformation(int(prodID))
+    if not res['OK']: 
+      return res
+    paramDict = res['Value']
+    if paramDict.has_key('Additional'):
+      paramDict.update(paramDict.pop('Additional'))
+    return S_OK(paramDict)
