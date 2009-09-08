c097df95c9df5a5976b416cb62669d5ea153f36c
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-09-08 13:58:52 +0000

    added setProductionStatus
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@17260 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Client/ProductionClient.py
+++ LHCbDIRAC/ProductionManagementSystem/Client/ProductionClient.py
@@ -1,7 +1,7 @@
 """ Client class to access the production manager service
 """
-# $Id: ProductionClient.py,v 1.2 2009/09/08 13:04:14 acsmith Exp $
-__RCSID__ = "$Revision: 1.2 $"
+# $Id: ProductionClient.py,v 1.3 2009/09/08 13:58:52 acsmith Exp $
+__RCSID__ = "$Revision: 1.3 $"
 
 from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.Core.Utilities.List import breakListIntoChunks
@@ -29,4 +29,13 @@ class ProductionClient:
     paramDict = res['Value']
     if paramDict.has_key('Additional'):
       paramDict.update(paramDict.pop('Additional'))
+    if pname:
+      if paramDict.has_key(pname):
+        return S_OK(paramDict[pname])
+      else:
+        return S_ERROR('Parameter %s not found for production' % pname)
     return S_OK(paramDict)
+  
+  def setProductionStatus(self,prodID,status):
+    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
+    return server.setTransformationStatus(prodID, status)
