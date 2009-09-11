8f7de03fb14d65258e09c975640825b93564d7dc
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-09-11 15:24:25 +0000

    add check of status before performing check.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@17317 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/scripts/dirac-production-verify-outputdata.py
+++ LHCbDIRAC/ProductionManagementSystem/scripts/dirac-production-verify-outputdata.py
@@ -2,20 +2,33 @@
 from DIRAC.Core.Base.Script import parseCommandLine
 parseCommandLine()
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/scripts/dirac-production-verify-outputdata.py,v 1.9 2009/09/08 14:11:19 acsmith Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/scripts/dirac-production-verify-outputdata.py,v 1.10 2009/09/11 15:24:25 acsmith Exp $
 ########################################################################
-__RCSID__   = "$Id: dirac-production-verify-outputdata.py,v 1.9 2009/09/08 14:11:19 acsmith Exp $"
-__VERSION__ = "$Revision: 1.9 $"
+__RCSID__   = "$Id: dirac-production-verify-outputdata.py,v 1.10 2009/09/11 15:24:25 acsmith Exp $"
+__VERSION__ = "$Revision: 1.10 $"
 
 import sys
 if len(sys.argv) < 2:
-  print 'Usage: dirac-production-verify-outputdata.py prodID'
+  print 'Usage: dirac-production-verify-outputdata prodID'
   sys.exit()
 else:
   prodID = int(sys.argv[1])
 
+from DIRAC.ProductionManagementSystem.Agent.ValidateOutputData      import ValidateOutputData
+from DIRAC.ProductionManagementSystem.Client.ProductionClient       import ProductionClient
+from DIRAC import gLogger
+import DIRAC
 
-from DIRAC.ProductionManagementSystem.Agent.ValidateOutputData import ValidateOutputData
+client = ProductionClient()
+res = client.getProductionStatus(prodID)
+if not res['OK']:
+  gLogger.error("Failed to determine production status")
+  gLogger.error(res['Message'])
+  DIRAC.exit(-1)
+status = res['Value']
+if not status in ['ValidatingOutput','WaitingIntegrity']:
+  gLogger.error("The production is in %s status and can not be validated" % status)
+  DIRAC.exit(-1)
 agent = ValidateOutputData('ProductionManagement/ValidateOutputData','dirac-production-verify-outputdata')
 agent.initialize()
 agent.checkProductionIntegrity(prodID)
