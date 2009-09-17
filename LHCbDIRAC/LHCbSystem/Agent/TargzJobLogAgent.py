3cca94510dc290d28f79952f1fdeb3a6865e1b49
roma <roma@4525493e-7705-40b1-a816-d608a930855b>
2009-09-17 15:00:34 +0000

    Backup to CASTOR and clearing of production added
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@17473 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/LHCbSystem/Agent/TargzJobLogAgent.py
+++ LHCbDIRAC/LHCbSystem/Agent/TargzJobLogAgent.py
@@ -8,8 +8,13 @@ Compress Old Jobs
 
 from DIRAC import gLogger, S_OK, S_ERROR, gConfig
 from DIRAC.Core.Base.Agent import Agent
+from DIRAC.Core.Utilities.Subprocess import shellCall
+from DIRAC.Core.Utilities.Shifter import setupShifterProxyInEnv
+from DIRAC.DataManagementSystem.Client.StorageElement import StorageElement
+from DIRAC.Core.Utilities.Subprocess                import shellCall
 
-import sys, os
+
+import sys, os, shutil
 import time
 import types
 import glob
@@ -39,16 +44,33 @@ class TargzJobLogAgent(Agent):
 
     self.logPath = gConfig.getValue(self.section+'/LogPath', '/storage/lhcb/MC/MC09/LOG')
     gLogger.info("LogPath", self.logPath)
-    
+
+    self.useProxies = gConfig.getValue(self.section+'/UseProxies','True').lower() in ( "y", "yes", "true" )
+    self.proxyLocation = gConfig.getValue( self.section+'/ProxyLocation', '' )
+    if not self.proxyLocation:
+      self.proxyLocation = False
+
+    self.storageElement = StorageElement("CERN-tape")    
+    self.destDirectory="/lhcb/backup/log"
+
     return S_OK()
 
   def execute(self):
 
-    self.log.info( 'Starting Agent loop')
+    gLogger.info( 'Starting Agent loop')
+
+    if self.useProxies:
+      result = setupShifterProxyInEnv( "SAMManager", self.proxyLocation )
+      if not result[ 'OK' ]:
+        gLogger.error( "Can't get shifter's proxy: %s" % result[ 'Message' ] )
+        return result
+
     path = os.path.abspath(self.logPath)
 
-    age = gConfig.getValue(self.section+'/AgeDays', 7)
-    gLogger.info("AgeDays", age)
+    jobage = gConfig.getValue(self.section+'/JobAgeDays', 7)
+    gLogger.info("JobAgeDays", jobage)
+    prodage = gConfig.getValue(self.section+'/ProdAgeDays', 80)
+    gLogger.info("JobAgeDays", prodage)
 
     g1 = gConfig.getValue(self.section+'/ProductionGlob', '????????')
     gLogger.info("ProductionGlob", g1)
@@ -59,8 +81,24 @@ class TargzJobLogAgent(Agent):
     
     numberOfTared = 0
     numberOfFailed = 0
+    for subprodpath in self._iFindOldSubProd(path,g1,g2,prodage):
+      pathlist = subprodpath.split("/")
+      sub = pathlist[-1]
+      prod = pathlist[-2]
+      gLogger.info( "Found Old Log", "Production %s, subProduction %s" %(prod,sub) )
+      res = self._tarSubProdDir(path,prod,sub)
+      if res['OK']:
+        numberOfTared +=1
+      else:
+        numberOfFailed +=1
+            
+    gLogger.info( "Number of tared subproduction %d"%numberOfTared )
+    gLogger.info( "Number of failed subproduction %d"%numberOfFailed )
+      
+    numberOfTared = 0
+    numberOfFailed = 0
     
-    for jobpath in self._iFindOldJob(path,g1,g2,g3,age):
+    for jobpath in self._iFindOldJob(path,g1,g2,g3,jobage):
       pathlist = jobpath.split("/")
       job = pathlist[-1]
       prod = pathlist[-3]
@@ -132,3 +170,83 @@ class TargzJobLogAgent(Agent):
         os.remove(f)
     finally:
       os.chdir(oldpath)
+
+
+  def _iFindOldSubProd(self,path, g1, g2, agedays):  
+
+    p1 = '^\d{8}$'
+    c1 = re.compile(p1)
+    p2 = '^\d{4}$'
+    c2 = re.compile(p2)
+
+    def iFindDir(path,gl,reobject):
+
+      dirs = glob.glob(os.path.join(path,gl))
+      for d in dirs:
+        name = os.path.basename(d)
+        if reobject.match(name) and os.path.isdir(d):
+          yield d
+   
+    for d1 in iFindDir(path, g1, c1):
+      for d2 in iFindDir(d1, g2, c2):
+        mtime = os.stat(d2)[8]
+        modified = datetime.fromtimestamp(mtime)
+        if datetime.now()-modified>timedelta(days=agedays):
+          yield d2
+
+  def _tarSubProdDir(self,path,prod,sub):
+
+    oldpath = os.getcwd()
+
+    tarname = "/tmp/" + prod + "_" + sub + ".tgz"
+    destFile = self.destDirectory + "/" + prod + "_" + sub + ".tgz"
+
+    if not self._noStorageFile(destFile):
+      gLogger.info( "File exist ", destFile)
+      return S_OK()
+      
+    try:
+      os.chdir(path)
+      subprodpath = os.path.join( prod, sub)
+
+      tarFile = tarfile.open(tarname,"w:gz")
+      tarFile.add(subprodpath)
+      tarFile.close()
+    finally:
+      os.chdir(oldpath)
+
+    tared = False      
+    res = self.storageElement.getPfnForLfn(destFile)
+    if res['OK']:
+      pfn = res["Value"]
+      fileDict = {pfn:tarname}
+      gLogger.info("putFile",fileDict)
+      res = self.storageElement.putFile(fileDict)
+      if res['OK']:
+        if not res['Value']['Failed']:
+          subprodpath = os.path.join( path, prod, sub)
+          gLogger.info( "rmTree", subprodpath )
+	  shutil.rmtree(subprodpath)
+	  tared = True
+	else:
+	  gLogger.error("putFile", res['Value']['Failed'])
+      else:
+        gLogger.error("putFile",res['Message'])
+    else:
+      print res  
+    
+    gLogger.info( "remove ",tarname )
+    os.remove(tarname)
+    if tared:
+      return S_OK()
+    else:
+      return S_ERROR()
+        
+  def _noStorageFile(self,path):
+  
+    com = 'rfstat /castor/cern.ch/grid/%s'%path
+    res = shellCall(5,com)
+    if res['OK'] and res['Value'][0]==1:
+      return True
+    else:
+      return False
