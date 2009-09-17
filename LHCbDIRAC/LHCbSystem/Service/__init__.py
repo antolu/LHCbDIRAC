e60ccf2c35d1985394d0b9b903d1f632bdae2810
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2009-09-17 12:54:03 +0000

    LHCbSystem.Service
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@17442 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/Service/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/LHCbSystem/Service/__init__.py,v 1.1 2009/09/17 12:54:03 paterson Exp $
+############################################################
+
+"""
+   LHCbSystem.Service package
+
+   This contains the LHCb specific DIRAC services.
+"""
+
+__RCSID__ = "$Id: __init__.py,v 1.1 2009/09/17 12:54:03 paterson Exp $"
