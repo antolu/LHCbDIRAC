8ff18f6de9ad558ad0add3291866e13215b91dff
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2009-10-12 16:10:35 +0000

    bugfix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@17741 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-ls.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-ls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 ########################################################################
-# $Id: dirac-bookkeeping-ls.py,v 1.2 2008/10/17 13:03:02 rgracian Exp $
+# $Id: dirac-bookkeeping-ls.py,v 1.3 2009/10/12 16:10:35 zmathe Exp $
 ########################################################################
 
 from DIRACEnvironment import DIRAC
@@ -8,14 +8,19 @@ from DIRAC.Interfaces.API.Dirac                       import Dirac
 from DIRAC.BookkeepingSystem.Client.LHCB_BKKDBClient  import LHCB_BKKDBClient
 from DIRAC.Core.Base import Script
 
-__RCSID__ = "$Id: dirac-bookkeeping-ls.py,v 1.2 2008/10/17 13:03:02 rgracian Exp $"
+__RCSID__ = "$Id: dirac-bookkeeping-ls.py,v 1.3 2009/10/12 16:10:35 zmathe Exp $"
 
 Script.parseCommandLine( ignoreErrors = True )
 args = Script.getPositionalArgs()
 
 
 cl = LHCB_BKKDBClient()
-path = args[0]
+path = ''
+if len(args) > 0:
+  path = args[0]
+if len(args) > 1:
+  for i in range(1, len(args)):
+    path += args[i]
 
 print cl.list(path)
 
