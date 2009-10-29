6087110090f4a6b485ddcb523eba7785ca1a3b1b
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-10-29 19:18:23 +0000

    test script for obtaining root GUIDs.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@17945 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/LHCbSystem/scripts/dirac-lhcb-get-root-guid.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+from DIRAC.Core.Base                                      import Script
+Script.parseCommandLine(ignoreErrors = True)
+localFiles = Script.getPositionalArgs()
+import DIRAC
+from DIRAC                                                import gLogger
+from DIRAC.LHCbSystem.Utilities.ClientTools               import getRootFileGUID
+import os
+
+if not localFiles:
+  gLogger.info("No files suppied")
+  gLogger.info("Usage: dirac-lhcb-get-root-guid file1 [file2 ...]")
+  gLogger.info("Try dirac-lhcb-get-root-guid --help for options")
+  DIRAC.exit(0)
+
+for file in localFiles:
+  if not os.path.exists(file):
+    gLogger.info("The supplied file %s does not exist")
+    continue
+  res = getRootFileGUID(file)
+  if not res['OK']:
+    gLogger.info("Failed to obtain GUID for %s: %s" % (file,res['Message']))
+    continue
+  gLogger.info("%s GUID: %s" % (file,res['Value']))
+DIRAC.exit(0)
