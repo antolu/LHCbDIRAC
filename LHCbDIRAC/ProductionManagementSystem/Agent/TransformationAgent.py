2093fb990cd867012bb7beceaf17f0d9ff53c260
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-11-03 13:31:26 +0000

    import re module.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@17992 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
@@ -1,12 +1,12 @@
 ########################################################################
-# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/TransformationAgent.py,v 1.41 2009/10/27 17:17:47 acsmith Exp $
+# $Header: /tmp/libdirac/tmp.stZoy15380/dirac/DIRAC3/DIRAC/ProductionManagementSystem/Agent/TransformationAgent.py,v 1.42 2009/11/03 13:31:26 acsmith Exp $
 ########################################################################
 
 """  The Transformation Agent prepares production jobs for processing data
      according to transformation definitions in the Production database.
 """
 
-__RCSID__ = "$Id: TransformationAgent.py,v 1.41 2009/10/27 17:17:47 acsmith Exp $"
+__RCSID__ = "$Id: TransformationAgent.py,v 1.42 2009/11/03 13:31:26 acsmith Exp $"
 
 from DIRAC.Core.Base.Agent      import Agent
 from DIRAC                      import S_OK, S_ERROR, gConfig, gLogger, gMonitor
@@ -15,7 +15,7 @@ from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
 from DIRAC.LHCbSystem.Utilities.AncestorFiles import getAncestorFiles
 from DIRAC.Core.Utilities.SiteSEMapping       import getSitesForSE
 from DIRAC.Core.Utilities.Shifter import setupShifterProxyInEnv
-import os, time, random
+import os, time, random,re
 
 AGENT_NAME = 'ProductionManagement/TransformationAgent'
 
