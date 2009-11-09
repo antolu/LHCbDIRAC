d45de8c21ee5be846ede62888839fe001b84dabc
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2009-11-09 22:48:23 +0000

    LHCbDIRAC package
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@18131 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/WorkloadManagementSystem/__init__.py $
+############################################################
+
+"""
+   LHCbDIRAC package
+"""
+
+__RCSID__ = "$Id: __init__.py 18064 2009-11-05 19:40:01Z acasajus $"
