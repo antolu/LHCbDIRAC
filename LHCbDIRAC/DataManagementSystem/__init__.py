cbb77cafc2143a41e56c72e0ef8cf5b1d8ff2f1b
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-11-17 20:16:23 +0000

    preparing for move of LHCb DataManagement components.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@18317 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/DataManagementSystem/__init__.py
@@ -0,0 +1,7 @@
+############################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/WorkloadManagementSystem/__init__.py $
+############################################################
+__RCSID__ = "$Id: __init__.py 18064 2009-11-05 19:40:01Z acasajus $"
+
+""" LHCbDIRAC.DataManagementSystem package """
+
