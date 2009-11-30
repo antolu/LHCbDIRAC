7f2aa4bb5cdfad67911080e71a5b662280215223
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2009-11-30 14:07:43 +0000

    Major restructuring to reflect DIRAC internal structure.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@18703 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/Interfaces/__init__.py
@@ -0,0 +1,11 @@
+############################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk/LHCbDIRAC/Interfaces/API/__init__.py $
+############################################################
+
+"""
+   LHCbDIRAC.Interfaces package
+
+   This contains the LHCb specific API plugins for DIRAC.
+"""
+
+__RCSID__ = "$Id: __init__.py 18700 2009-11-30 13:48:50Z paterson $"
