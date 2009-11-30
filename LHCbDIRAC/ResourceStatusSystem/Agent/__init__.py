179784cbf9cdaeab6500d886b5b2f8852b084437
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2009-11-30 14:36:37 +0000

    Major restructuring to reflect DIRAC internal structure.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@18709 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/ResourceStatusSystem/Agent/__init__.py
@@ -0,0 +1,10 @@
+############################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk/LHCbDIRAC/LHCbSystem/Service/__init__.py $
+############################################################
+
+"""
+   LHCbDIRAC.ResourceStatusSystem.Agent package
+
+"""
+
+__RCSID__ = "$Id: __init__.py 18161 2009-11-11 12:07:09Z acasajus $"
