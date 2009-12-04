4747a432d8e2145b2881cbf9992212c70bd35fff
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-12-04 22:42:46 +0000

    use wherever possible the underlying transformation methods.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@19234 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Client/ProductionClient.py
+++ LHCbDIRAC/ProductionManagementSystem/Client/ProductionClient.py
@@ -1,197 +1,77 @@
-""" Client class to access the production manager service
-"""
+""" Client class to access the production manager service """
 # $Id$
 __RCSID__ = "$Revision: 1.6 $"
 
-from DIRAC  import gLogger, gConfig, S_OK, S_ERROR
-from DIRAC.Core.Utilities.List import breakListIntoChunks
-from DIRAC.Core.DISET.RPCClient import RPCClient
-import types
+from DIRAC                                                            import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC.Core.DISET.RPCClient                                       import RPCClient
+from LHCbDIRAC.TransformationSystem.Client.TransformationDBClient     import TransformationDBClient  
 
-class ProductionClient:
+class ProductionClient(TransformationDBClient):
 
-  #############################################################################
+  """ Exposes the functionality available in the LHCbDIRAC/ProductionManagerHandler
+
+      This inherits the DIRAC base Client for direct execution of server functionality.
+      The following methods are available (although not visible here).
+
+      Workflows (table) manipulation
+      
+          publishWorkflow(body,update=False)
+          getWorkflow(name)
+          getWorkflowFullDescription(name)
+          deleteWorkflow(name)
+          getListWorkflows()
+          getWorkflowInfo(name)
+          
+  """
   def __init__(self):
-    pass
+    TransformationDBClient.__init__()
+    self.setServer('ProductionManagement/ProductionManager')
 
   #############################################################################
-  def getProductionsWithStatus(self,status):
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getTransformationWithStatus(status)
-
-  #############################################################################
-  def getAllProductions(self):
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getAllProductions()
-
-  #############################################################################
-  def getParameters(self,prodID,pname=''):
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    res = server.getTransformation(int(prodID))
+  def getParameters(self,prodID,pname='',rpc=False,url='',timeout=120):
+    res = self.getTransformation(int(prodID),extraParams=True)
     if not res['OK']: 
       return res
     paramDict = res['Value']
-    if paramDict.has_key('Additional'):
-      paramDict.update(paramDict.pop('Additional'))
-    if pname:
-      if paramDict.has_key(pname):
-        return S_OK(paramDict[pname])
-      else:
-        return S_ERROR('Parameter %s not found for production' % pname)
-    return S_OK(paramDict)
+    if not pname:
+      return S_OK(paramDict)
+    if paramDict.has_key(pname):
+      return S_OK(paramDict[pname])
+    else:
+      return S_ERROR('Parameter %s not found for production' % pname)
   
-  #############################################################################  
-  def setProductionStatus(self,prodID,status):
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.setTransformationStatus(prodID, status)
-  
-  #############################################################################  
-  def setTransformationAgentType(self,prodID,status):
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.setTransformationAgentType(prodID, status)  
-
-  #############################################################################  
-  def deleteProduction(self,prodID):
-    """ Deletes a production from the production management system ONLY.
-    """    
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.deleteProduction(prodID)
-  
-  #############################################################################
-  def selectWMSJobs(self,prodID,statusList=[],newer=0):
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.selectWMSJobs(prodID,statusList,newer)
-  
-  #############################################################################
-  def cleanProduction(self,prodID):
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.cleanProduction(prodID)
-
-  #############################################################################
-  def getProduction(self,prodID):
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getTransformation(prodID)
-  
-  #############################################################################
-  def getProductionStatus(self,prodID):
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    res = server.getTransformation(prodID)
-    if not res['OK']:
-      return res
-    return S_OK(res['Value']['Status'])
-
-  #############################################################################
-  def getProductionLastUpdate(self,prodID):
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getTransformationLastUpdate(prodID)
-  
-  #############################################################################
-  def getProductionSummary(self):
-    """Returns a dictionary of production IDs and metadata.
-    """
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getProductionSummary()
-
-  #############################################################################
-  def getProductionInfo(self,prodID):
-    """Returns a dictionary of production information.
-    """
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getProductionInfo(prodID)
-  
-  #############################################################################
-  def getTransformationLogging(self,prodID):
-    """The logging information for the given production is returned.  This includes
-       the operation performed, any messages associated with the operation and the
-       DN of the responsible performing the action.
-    """  
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getTransformationLogging(prodID)  
-  
-  #############################################################################
-  def getProductionSummary(self): 
-    """Returns a detailed summary for the productions in the system.
-    """ 
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getProductionSummary()
-
-  #############################################################################
-  def getJobWMSStats(self,prodID):
-    """Retrieve job WMS statistics for a given production ID. 
-    """
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getJobWmsStats(prodID)
-  
-  #############################################################################  
-  def getFilesForTransformation(self,prodID,orderOutput=True):
-    """Retrieve transformation LFNs for given production ID. 
-    """
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getFilesForTransformation(prodID,orderOutput) 
-
-  #############################################################################    
-  def getFileSummary(self,lfns,prodID):
-    """Return the status of files for a given transformation.
-    """
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getFileSummary(lfns,prodID)   
-  
-  #############################################################################    
-  def setFileStatusForTransformation(self,prodID,status,lfns):
-    """ Update file status for a given transformation.
-    """  
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.setFileStatusForTransformation(prodID,status,lfns)   
-  
-  #############################################################################      
-  def getJobInfo(self,prodID,prodJobID):
-    """ Return job info.
-    """  
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getJobInfo(prodID,prodJobID)
-
-  #############################################################################      
-  def getJobsToSubmit(self,prodID,numberOfJobs,site):
-    """ Returns the information about jobs to submit. 
-    """      
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.getJobsToSubmit(prodID,numberOfJobs,site)
-    
-  #############################################################################      
-  def extendProduction(self,prodID,numberOfJobs):
-    """ Allows to extend a production by specified number of jobs. 
-    """         
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    return server.extendProduction(prodID,numberOfJobs)
-
-  #############################################################################      
-  def createProduction(self,fileName,fileMask='',groupSize=1,bkQuery={},plugin='',productionGroup='',productionType='',derivedProd='',maxJobs=0):
+  def createProduction(self,xmlString,fileMask='',groupSize=1,bkQuery={},plugin='',productionGroup='',productionType='',derivedProd='',maxJobs=0,rpc=False,url='',timeout=120):
     """ Create a production, based on the supplied parameters.
         Any input data can be specified by either fileMast or bkQuery. If both are specified then the BKQuery takes precedence.
-
-        Usage: createProduction <filename> <filemask> <groupsize> <bkquery> <plugin> <prodGroup> <prodType> <maxJobs>
-    """  
-    result=S_ERROR()
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)
-    if derivedProd:
-      result = server.publishDerivedProduction(derivedProd,xmlString,fileMask,groupSize,bkQuery,plugin,productionType,productionGroup,maxJobs,False)
-    else:
-      result = server.publishProduction(xmlString,fileMask,groupSize,False,bkQuery,plugin,productionGroup,productionType,maxJobs)
-
-    return result
-
-  #############################################################################        
-  def setJobStatus(self,prodID,jobNumber,status):
-    """ Update the status of a job for a given production. 
+        Usage: createProduction <xmlString> <filemask> <groupsize> <bkquery> <plugin> <prodGroup> <prodType> <maxJobs>
     """
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)    
-    return server.setJobStatus(prodID,jobNumber,status)
+    #TODO: Check where agentType comes from
+    wf = fromXMLString(xmlString)
+    transName = wf.getName()
+    description = wf.getDescrShort()
+    longDescription = wf.getDescription()
+    return self.addTransformation(transName,description,longDescription,productionType,plugin,agentType,fileMask,
+                                  transformationGroup = productionGroup,
+                                  groupSize           = groupSize,
+                                  inheritedFrom       = derivedProd,
+                                  body                = xmlString, 
+                                  maxJobs             = maxJobs,
+                                  eventsPerJob        = 0,
+                                  addFiles            = True,
+                                  bkQuery             = bkQuery)
 
-  #############################################################################        
-  def setJobStatusAndWmsID(self,prodID,jobNumber,status,wmsID):
-    """ Update the status of a job for a given production. 
-    """
-    server = RPCClient('ProductionManagement/ProductionManager',timeout=120)     
-    return server.setJobStatusAndWmsID(prodID,jobNumber,status,wmsID)
+  #############################################################################
+  #TODO: Update where used.
+  #############################################################################
+  
+  def setProductionStatus(self,prodID,status,rpc=False,url='',timeout=120):
+    return self.setTransformationStatus(prodID, status,rpc=rpc,url=url,timeout=timeout)
 
-                
+  def selectWMSJobs(self,prodID,statusList=[],newer=0,rpc=False,url='',timeout=120):
+    return self.selectWMSTasks(prodID,statusList,newer,rpc=rpc,url=url,timeout=timeout)
+
+  def getJobWMSStats(self,prodID,rpc=False,url='',timeout=120):
+    return self.getTransformationTaskStats(prodID,rpc=rpc,url=url,timeout=timeout)
+
+  def setJobStatusAndWmsID(self,prodID,jobNumber,status,wmsID,rpc=False,url='',timeout=120):
+    return self.setTaskStatusAndWmsID(prodID,jobNumber,status,wmsID,rpc=rpc,url=url,timeout=timeout)
