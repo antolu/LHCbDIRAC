650d8c8ce2c7a30e0b63793c0eeebd98a8083390
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-12-09 14:14:50 +0000

    polishing input statements.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@19398 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/BookkeepingWatchAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/BookkeepingWatchAgent.py
@@ -4,8 +4,8 @@
 
 __RCSID__ = "$Id$"
 
-from DIRAC  import S_OK, S_ERROR, gConfig, gLogger, gMonitor
-from LHCbDIRAC.TransformationSystem.Agent.BookkeepingWatchAgent import BookkeepingWatchAgent
+from DIRAC                                                        import S_OK, S_ERROR, gConfig, gLogger, gMonitor
+from LHCbDIRAC.TransformationSystem.Agent.BookkeepingWatchAgent   import BookkeepingWatchAgent
 
 AGENT_NAME = 'ProductionManagement/BookkeepingWatchAgent'
 
