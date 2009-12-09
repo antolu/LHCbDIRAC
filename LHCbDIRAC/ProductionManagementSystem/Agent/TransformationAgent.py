afc8e5b9db0675cc119ab2c75e69bf58d80819d3
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-12-09 19:19:36 +0000

    debugged.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@19414 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/TransformationAgent.py
@@ -4,7 +4,6 @@
 
 __RCSID__ = "$Id: TransformationAgent.py 19398 2009-12-09 14:14:50Z acsmith $"
 
-from DIRAC                                                        import S_OK, S_ERROR, gConfig, gLogger, gMonitor
 from DIRAC.TransformationSystem.Agent.TransformationAgent         import TransformationAgent as DIRACTransformationAgent
 
 AGENT_NAME = 'ProductionManagement/TransformationAgent'
