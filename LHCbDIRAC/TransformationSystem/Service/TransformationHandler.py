e8924da850256c576840afbf3fa4c28abc26303a
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-12-09 14:12:24 +0000

    removed methods that are private to the DB.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@19395 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/TransformationSystem/Service/TransformationHandler.py
+++ LHCbDIRAC/TransformationSystem/Service/TransformationHandler.py
@@ -15,34 +15,18 @@ class TransformationHandler(DIRACTransformationHandler):
   def __init__(self,*args,**kargs):
     DIRACTransformationHandler.__init__(self, *args,**kargs)
 
-  types_addBookkeepingQuery = [ DictType ]
-  def export_addBookkeepingQuery(self, queryDict):
-    res = self.database.addBookkeepingQuery(queryDict)
+  types_createTransformationQuery = [ [LongType, IntType, StringType], DictType ]
+  def export_createTransformationQuery(self,transName,queryDict):
+    authorDN = self._clientTransport.peerCredentials['DN']
+    res = self.database.createTransformationQuery(transName, queryDict,author=authorDN)
     return self.__parseRes(res)
 
-  types_getBookkeepingQueryForTransformation = [ [LongType, IntType, StringType] ]
-  def export_getBookkeepingQueryForTransformation(self, transName):
-    res = self.database.getBookkeepingQueryForTransformation(transName)
-    return self.__parseRes(res)
-    
-  types_getBookkeepingQuery = [ [LongType, IntType] ]
-  def export_getBookkeepingQuery(self, queryID):
-    res = self.database.getBookkeepingQuery(queryID)
-    return self.__parseRes(res)
-    
   types_deleteBookkeepingQuery = [ [LongType, IntType] ]
   def export_deleteBookkeepingQuery(self, queryID):
     res = self.database.deleteBookkeepingQuery(queryID)
     return self.__parseRes(res)
   
-  types_setTransformationQuery = [ [LongType, IntType, StringType], [LongType, IntType] ]
-  def export_setTransformationQuery(self,transName, queryID):
-    authorDN = self._clientTransport.peerCredentials['DN']
-    res = self.database.setTransformationQuery(transName, queryID,author=authorDN)
-    return self.__parseRes(res)
-  
-  types_createTransformationQuery = [ [LongType, IntType, StringType], DictType ]
-  def export_createTransformationQuery(self,transName,queryDict):
-    authorDN = self._clientTransport.peerCredentials['DN']
-    res = self.database.createTransformationQuery(transName, queryDict,author=authorDN)
+  types_getBookkeepingQueryForTransformation = [ [LongType, IntType, StringType] ]
+  def export_getBookkeepingQueryForTransformation(self, transName):
+    res = self.database.getBookkeepingQueryForTransformation(transName)
     return self.__parseRes(res)
