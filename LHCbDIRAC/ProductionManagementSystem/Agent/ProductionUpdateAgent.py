dc6a901d67e1ba9deaf1145b87be54cbda67f4b1
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2009-12-15 14:12:45 +0000

    updated to work.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@19494 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/ProductionUpdateAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/ProductionUpdateAgent.py
@@ -11,16 +11,13 @@ __RCSID__ = "$Id$"
 from DIRAC                                            import S_OK, S_ERROR, gConfig, gLogger, gMonitor
 from DIRAC.Core.DISET.RPCClient                       import RPCClient
 from DIRAC.Core.Base.AgentModule                      import AgentModule
-
+from DIRAC.Core.Utilities.List                        import sortList
 from LHCbDIRAC.ProductionManagementSystem.DB.ProductionDB import ProductionDB
 
-
-import os, time
-
+import os, time,datetime
 
 AGENT_NAME = 'ProductionManagement/ProductionUpdateAgent'
 UPDATE_STATUS = ['Created','Submitted','Received','Checking','Staging','Waiting','Matched','Running','Stalled']
-#UPDATE_STATUS = []
 WAITING_STATUS = ['Submitted','Received','Checking','Staging','Waiting']
 RUNNING_STATUS = ['Running','Stalled']
 FINAL_STATUS = ['Done','Failed','Completed']
@@ -35,8 +32,7 @@ class ProductionUpdateAgent(AgentModule):
     """
     self.pollingTime = self.am_getOption('PollingTime',120)
     self.prodDB = ProductionDB()
-    gMonitor.registerActivity("Iteration","Agent Loops",self.name,"Loops/min",gMonitor.OP_SUM)
-    return result
+    return S_OK()
 
   ##############################################################################
   def execute(self):
@@ -47,12 +43,18 @@ class ProductionUpdateAgent(AgentModule):
     result = self.prodDB.getTransformations()
     for transDict in result['Value']:
       transID = long(transDict['TransformationID'])
-      res = self.prodDB.getTransformationTasks(condDict={'TransformationID':transID,'WmsStatus':UPDATE_STATUS},newer=NEWER)
+      gLogger.info("Attempting to update transformation %d" % transID)
+      time_stamp_older = str(datetime.datetime.utcnow() - datetime.timedelta(minutes=NEWER))
+      res = self.prodDB.getTransformationTasks(condDict={'TransformationID':transID,'WmsStatus':UPDATE_STATUS},timeStamp='LastUpdateTime',older=time_stamp_older )
       if not res['OK']:
         gLogger.error("Failed to get WMS job IDs for production %d" % prodID,res['Message'])
         return res
+      jobDictList = res['Value']
+      gLogger.info("Selected %d tasks to be updated" % len(jobDictList))
+      if not jobDictList:
+        continue
       jobIDs = []
-      for jobDict in res['Value']:
+      for jobDict in jobDictList:
         jobIDs.append(jobDict['JobWmsID'])
       # Get the job statuses from WMS
       jobSvc = RPCClient('WorkloadManagement/JobMonitoring')
@@ -60,53 +62,28 @@ class ProductionUpdateAgent(AgentModule):
       if not result['OK']:
         gLogger.warn('Failed to get job status from the WMS system')
         continue
-
       statusDict = result['Value']
+
       updateDict = {}
-      for jobWMS in jobIDs:
-        jobID = jobDict[jobWMS][0]
-        old_status = jobDict[jobWMS][1]
+      for jobDict in jobDictList:
+        jobWMS = int(jobDict['JobWmsID'])
+        jobID = jobDict['JobID']
+        old_status = jobDict['WmsStatus']
+        status = "Removed"
         if jobWMS in statusDict.keys():
           status = statusDict[jobWMS]['Status']    
-        else:
-          status = "Removed"   
-           
         if old_status != status:
           if status == "Removed":
             gLogger.verbose('Production/Job %d/%d removed from WMS while it is in %s status' % (transID,jobID,old_status))
-            gLogger.verbose('Setting Production/Job job to Failed')
-            #result = self.prodDB.setTaskStatus(transID,jobID,'Failed')            
-            #if not result['OK']:
-            #  gLogger.warn('Failed to set job status for jobID: '+str(jobID))
-            if not updateDict.has_key('Failed'):
-              updateDict['Failed'] = []
-            updateDict['Failed'].append(jobID)    
-          else:
-            gLogger.verbose('Setting job status for Production/Job %d/%d to %s' % (transID,jobID,status))
-            #result = self.prodDB.setTaskStatus(transID,jobID,status)
-            #if not result['OK']:
-            #  gLogger.warn('Failed to set job status for jobID: '+str(jobID))
-            if not updateDict.has_key(status):
-              updateDict[status] = []
-            updateDict[status].append(jobID) 
+            status = "Failed"
+          gLogger.verbose('Setting job status for Production/Job %d/%d to %s' % (transID,jobID,status))
+          if not updateDict.has_key(status):
+            updateDict[status] = []
+          updateDict[status].append(jobID) 
             
-      for status in updateDict:
-        gLogger.verbose('Setting status %s for Production %d for jobs %s' % (status,transID,str(updateDict[status])))
-        result = self.prodDB.setTaskStatus(transID,updateDict[status],status)            
+      for status,taskIDs in updateDict.items():
+        gLogger.info('Setting status %s for Production %d for %d jobs' % (status,transID,len(taskIDs)))
+        result = self.prodDB.setTaskStatus(transID,taskIDs,status)            
         if not result['OK']:
-          gLogger.warn('Failed to set job status %s for jobs: %s' % (status,str(updateDict[status])))        
-            
-#        if old_status == "Submitted" and status in WAITING_STATUS:
-#          result = self.prodDB.getJobInfo(transID,jobID)
-#          if not result['OK']:
-#            gLogger.warn('Failed to get job info for production %d job %d' % (int(transID),int(jobID)))    
-#        elif old_status in WAITING_STATUS and status in RUNNING_STATUS:
-#          result = self.prodDB.getJobInfo(transID,jobID)
-#          if not result['OK']:
-#            gLogger.warn('Failed to get job info for production %d job %d' % (int(transID),int(jobID)))
-#        elif old_status in RUNNING_STATUS and status in FINAL_STATUS:
-#          result = self.prodDB.getJobInfo(transID,jobID)
-#          if not result['OK']:
-#            gLogger.warn('Failed to get job info for production %d job %d' % (int(transID),int(jobID)))
-
+          gLogger.warn('Failed to set job status %s for jobs: %s' % (status,str(taskIDs)))        
     return S_OK()
