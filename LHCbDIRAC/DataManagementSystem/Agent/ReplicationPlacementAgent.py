fc71a8b93e3e03152ebc496938c3fba409cb3ea9
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2010-01-19 09:35:14 +0000

    moved from LHCbDirac to LHCbDIRAC
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@19920 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/DataManagementSystem/Agent/ReplicationPlacementAgent.py
@@ -0,0 +1,11 @@
+########################################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk/LHCbDIRAC/DataManagementSystem/Agent/ReplicationPlacementAgent.py $
+########################################################################
+
+__RCSID__ = "$Id: ReplicationPlacementAgent.py 19398 2009-12-09 14:14:50Z acsmith $"
+
+from DIRAC.TransformationSystem.Agent.TransformationAgent         import TransformationAgent as DIRACTransformationAgent
+AGENT_NAME = 'DataManagement/ReplicationPlacementAgent'
+
+class ReplicationPlacementAgent(DIRACTransformationAgent):
+   pass
