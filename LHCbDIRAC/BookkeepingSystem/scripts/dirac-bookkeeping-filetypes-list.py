f30c034affd8e09c2a7bd6e4f1095bc4d9515178
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2010-02-10 09:35:46 +0000

    I also list files which are not in the CS!
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@21112 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-filetypes-list.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-filetypes-list.py
@@ -10,20 +10,28 @@ __VERSION__ = "$ $"
 
 import sys,string,re
 import DIRAC
-from DIRAC.Core.Base import Script
-
+from DIRAC.Core.Base                                               import Script
+from DIRAC.ConfigurationSystem.Client.Config                       import gConfig
 Script.parseCommandLine( ignoreErrors = True )
 
 from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient import BookkeepingClient
 bk = BookkeepingClient()
 exitCode = 0
 
+dataTypes = gConfig.getValue('/Operations/Bookkeeping/FileTypes',[])
+
+mfiletypes = []
 res=bk.getAvailableFileTypes()
 if res['OK']:
   dbresult = res['Value']
   print 'Filetypes:'
   for record in dbresult:
     print str(record[0]).ljust(10)
-    
+    if record[0] not in dataTypes:
+      mfiletypes += [record[0]]
+  if len(mfiletypes) > 0:
+    print 'File Types have to be add to CS:'
+    for i in mfiletypes:
+      print str(i).ljust(10)
 
 DIRAC.exit(exitCode)
