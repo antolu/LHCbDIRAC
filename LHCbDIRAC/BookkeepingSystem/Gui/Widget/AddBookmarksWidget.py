f853c58415d8a244a5be600825896a909290b770
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2010-02-16 14:59:04 +0000

    change rootPath to os.path.dirname(os.path.realpath(LHCbDIRAC.__path__[0]))
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@21488 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/Gui/Widget/AddBookmarksWidget.py
+++ LHCbDIRAC/BookkeepingSystem/Gui/Widget/AddBookmarksWidget.py
@@ -12,46 +12,46 @@ import LHCbDIRAC
 __RCSID__ = " $"
 
 
-#############################################################################  
+#############################################################################
 class AddBookmarksWidget(QDialog, Ui_AddBookmarks):
-  
-  #############################################################################  
+
+  #############################################################################
   def __init__(self, parent = None):
     """
     Constructor
-    
+
     @param parent parent widget (QWidget)
     """
     QWidget.__init__(self, parent)
     self.setupUi(self)
-    
-    picturesPath = LHCbDIRAC.rootPath+'/LHCbDIRAC/BookkeepingSystem/Gui/Widget'
+
+    picturesPath = os.path.dirname(os.path.realpath(LHCbDIRAC.__path__[0]))+'/LHCbDIRAC/BookkeepingSystem/Gui/Widget'
     #addIcon = QIcon(picturesPath+"/images/add.png")
     #self.addButton.setIcon(addIcon)
-    
+
     self.__controler = ControlerAddBookmarks(self, parent.getControler())
-      
+
     self.connect(self.okButton, SIGNAL("clicked()"), self.__controler.okButton)
     self.connect(self.cancelButton, SIGNAL("clicked()"), self.__controler.cancelButton)
     self.__model = None
-    
-    
-  #############################################################################  
+
+
+  #############################################################################
   def getControler(self):
     return self.__controler
-  
+
   #############################################################################
   def getTitle(self):
     return str(self.titlelineEdit.text())
-  
+
   #############################################################################
   def getPath(self):
     return str(self.pathlineEdit.text())
-  
+
   #############################################################################
   def setTitle(self, str):
     self.titlelineEdit.setText(str)
-  
+
   #############################################################################
   def setPath(self, str):
     self.pathlineEdit.setText(str)
