1a86d6a0fd617b2e7f820b54ecd56b58253d5737
atsareg <atsareg@4525493e-7705-40b1-a816-d608a930855b>
2010-03-12 10:30:06 +0000

    v5r1-pre5
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@22904 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -6,7 +6,7 @@ __path__ = extend_path(__path__, __name__)
 majorVersion = 5
 minorVersion = 1
 patchLevel   = 0
-preVersion   = 4
+preVersion   = 5
 
 version      = "v%sr%s" % ( majorVersion, minorVersion )
 buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
