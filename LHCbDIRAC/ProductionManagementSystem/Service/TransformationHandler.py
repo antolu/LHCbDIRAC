909a601f43ecd25f9a1187c48a7909670ff8e32b
acsmith <acsmith@4525493e-7705-40b1-a816-d608a930855b>
2010-05-25 13:03:08 +0000

    added methods for monitoring TransformationRuns table.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@25356 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Service/TransformationHandler.py
+++ LHCbDIRAC/ProductionManagementSystem/Service/TransformationHandler.py
@@ -52,6 +52,11 @@ class TransformationHandler(DIRACTransformationHandler):
     res = self.database.getTransformationRuns(condDict=condDict, orderAttribute=orderAttribute,limit=limit) 
     return self.__parseRes(res)
 
+  types_getTransformationRunStats = [ListType]
+  def export_getTransformationRunStats(self,transIDs):
+    res = self.database.getTransformationRunStats(transIDs)
+    return self.__parseRes(res)
+                          
   types_getTransformationRunsSummaryWeb = [DictType, ListType, IntType, IntType]
   def export_getTransformationRunsSummaryWeb(self,selectDict,sortList,startItem,maxItems):
     return self.__getTableSummaryWeb('TransformationRuns',selectDict,sortList,startItem,maxItems,selectColumns=['TransformationID','RunNumber','SelectedSite','Status'],timeStamp='LastUpdate',statusColumn='Status')  
@@ -64,3 +69,111 @@ class TransformationHandler(DIRACTransformationHandler):
   def export_setTransformationRunsSite(self,transID,runID,assignedSE):
     return self.database.setTransformationRunsSite(transID,runID,assignedSE)
 
+  types_getTransformationRunsSummaryWeb = [DictType, ListType, IntType, IntType]
+  def export_getTransformationRunsSummaryWeb(self, selectDict, sortList, startItem, maxItems):
+    """ Get the summary of the transformation run information for a given page in the generic format """
+
+    # Obtain the timing information from the selectDict
+    last_update = selectDict.get('LastUpdate',None)    
+    if last_update:
+      del selectDict['LastUpdate']
+    fromDate = selectDict.get('FromDate',None)    
+    if fromDate:
+      del selectDict['FromDate']
+    if not fromDate:
+      fromDate = last_update  
+    toDate = selectDict.get('ToDate',None)    
+    if toDate:
+      del selectDict['ToDate']  
+    # Sorting instructions. Only one for the moment.
+    if sortList:
+      orderAttribute = sortList[0][0]+":"+sortList[0][1]
+    else:
+      orderAttribute = None
+
+    # Get the transformations that match the selection
+    res = self.database.getTransformationRuns(condDict=selectDict,older=toDate, newer=fromDate, orderAttribute=orderAttribute)
+    if not res['OK']:
+      return self.__parseRes(res)
+
+    # Prepare the standard structure now within the resultDict dictionary
+    resultDict = {}
+    trList = res['Records']
+    # Create the total records entry
+    nTrans = len(trList)
+    resultDict['TotalRecords'] = nTrans
+    # Create the ParameterNames entry
+    paramNames = res['ParameterNames']
+    resultDict['ParameterNames'] = paramNames
+
+    # Add the job states to the ParameterNames entry
+    # TODO Removed for the moment
+    #taskStateNames   = ['Created','Running','Submitted','Failed','Waiting','Done','Stalled']
+    #resultDict['ParameterNames'] += ['Jobs_'+x for x in taskStateNames]
+    # Add the file states to the ParameterNames entry
+    fileStateNames  = ['PercentProcessed','Processed','Unused','Assigned','Total','Problematic']
+    resultDict['ParameterNames'] += ['Files_'+x for x in fileStateNames]
+
+    # Get the transformations which are within the selected window
+    if nTrans == 0:
+      return S_OK(resultDict)
+    ini = startItem
+    last = ini + maxItems
+    if ini >= nTrans:
+      return S_ERROR('Item number out of range')
+    if last > nTrans:
+      last = nTrans
+    transList = trList[ini:last]
+
+    # Obtain the run statistics for the requested transformations
+    transIDs = []
+    for transRun in transList:
+      transRunDict = dict(zip(paramNames,transRun))
+      transIDs.append(transRunDict['TransformationID'])
+    res = self.database.getTransformationRunStats(transIDs)
+    if not res['OK']:
+      return res
+    transRunStatusDict = res['Value']
+      
+    statusDict = {}
+    # Add specific information for each selected transformation/run
+    for transRun in transList:
+      transRunDict = dict(zip(paramNames,transRun))
+      transID = transRunDict['TransformationID']
+      runID = transRunDict['RunNumber']
+      # Update the status counters
+      status = transRunDict['Status']
+      if not statusDict.has_key(status):
+        statusDict[status] = 0
+      statusDict[status] += 1
+
+      # Populate the run file statistics
+      fileDict = transRunStatusDict[transID][runID]
+      if fileDict['Total'] == 0:
+        fileDict['PercentProcessed']  = 0
+      else:
+        processed = fileDict.get('Processed')
+        if not processed:
+          processed = 0
+        fileDict['PercentProcessed'] = "%.1f" % ((processed*100.0)/fileDict['Total'])
+      for state in fileStateNames:
+        if fileDict and fileDict.has_key(state):
+          trans.append(fileDict[state])
+        else:
+          trans.append(0)
+
+      # Get the statistics on the number of jobs for the transformation
+      # TODO Removed for the moment
+      #res = self.database.getTransformationTaskRunStats(transID)
+      #taskDict = {}
+      #if res['OK'] and res['Value']:
+      #  taskDict = res['Value']
+      #for state in taskStateNames:
+      #  if taskDict and taskDict.has_key(state):
+      #    trans.append(taskDict[state])
+      #  else:
+      #    trans.append(0)
+
+    resultDict['Records'] = transList
+    resultDict['Extras'] = statusDict
+    return S_OK(resultDict)
