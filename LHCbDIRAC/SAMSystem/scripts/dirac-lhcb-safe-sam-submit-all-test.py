6cefbd2bb1def6ce277edc814efbd4ab58a198d2
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2010-06-15 12:03:59 +0000

    submit all SAM jobs but do not publish the result to SAMDB
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@26243 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/SAMSystem/scripts/dirac-lhcb-safe-sam-submit-all-test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+########################################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk/LHCbDIRAC/SAMSystem/scripts/dirac-lhcb-safe-sam-submit-all.py $
+# File :   dirac-lhcb-safe-sam-submit-all
+# Author : Stuart Paterson
+########################################################################
+__RCSID__   = "$Id: dirac-lhcb-safe-sam-submit-all.py 18764 2009-12-01 13:19:40Z paterson $"
+__VERSION__ = "$Revision: 1.1 $"
+import sys
+import DIRAC
+from DIRAC.Core.Base import Script
+
+Script.registerSwitch( "", "script=", "Optional path to python script to execute in SAM jobs [Experts only]" )
+Script.parseCommandLine( ignoreErrors = True )
+args = Script.getPositionalArgs()
+
+from LHCbDIRAC.SAMSystem.Client.DiracSAM import DiracSAM
+
+script = ''
+
+def usage():
+  print 'Usage: %s [--script=<SCRIPT NAME>]' %(Script.scriptName)
+  print 'SAM test jobs will be submitted to all CEs defined in the DIRAC CS'
+  DIRAC.exit(2)
+
+if args:
+  usage()
+
+for switch in Script.getUnprocessedSwitches():
+  if switch[0].lower()=="script":
+    script=switch[1]
+
+exitCode = 0
+diracSAM = DiracSAM()
+result = diracSAM._promptUser('Are you sure you want to submit SAM jobs in safe mode (without software installation) for all CEs known to DIRAC?')
+if not result['OK']:
+  print 'Action cancelled.'
+  DIRAC.exit(2)
+
+result = diracSAM.submitAllSAMTestJobs(softwareEnableFlag=False,publishFlag=False,scriptName=script)
+if not result['OK']:
+  print 'ERROR %s' %result['Message']
+  exitCode = 2
+
+DIRAC.exit(exitCode)
