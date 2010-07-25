bdf0c9ed3bb259224dd97120fe3397c81683a801
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2010-07-25 14:33:28 +0000

    FIX: consider also /LocalSite/InstancePath as alternative to rootPath
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@27018 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/Agent/UserStorageUsageAgent.py
+++ LHCbDIRAC/DataManagementSystem/Agent/UserStorageUsageAgent.py
@@ -3,7 +3,7 @@
 # $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/DataManagementSystem/Agent/UserStorageUsageAgent.py $
 __RCSID__ = "$Id: UserStorageUsageAgent.py 18161 2009-11-11 12:07:09Z acasajus $"
 
-from DIRAC  import gLogger, gMonitor, S_OK, S_ERROR, rootPath
+from DIRAC  import gLogger, gMonitor, S_OK, S_ERROR, rootPath, gConfig
 from DIRAC.Core.Base.AgentModule import AgentModule
 
 from DIRAC.Core.Utilities.Shifter import setupShifterProxyInEnv
@@ -14,39 +14,39 @@ from DIRAC.DataManagementSystem.Client.ReplicaManager import CatalogDirectory
 
 from LHCbDIRAC.DataManagementSystem.Agent.StorageUsageAgent import StorageUsageAgent
 
-import time,os
+import time, os
 from types import *
 
 AGENT_NAME = 'DataManagement/UserStorageUsageAgent'
 
-class UserStorageUsageAgent(StorageUsageAgent):
+class UserStorageUsageAgent( StorageUsageAgent ):
 
-  def initialize(self):
+  def initialize( self ):
     self.catalog = CatalogDirectory()
-    if self.am_getOption('DirectDB',False):
+    if self.am_getOption( 'DirectDB', False ):
       from LHCbDIRAC.DataManagementSystem.DB.StorageUsageDB import StorageUsageDB
       self.StorageUsageDB = StorageUsageDB()
     else:
       from DIRAC.Core.DISET.RPCClient import RPCClient
-      self.StorageUsageDB = RPCClient('DataManagement/StorageUsage')
-    self.am_setModuleParam("shifterProxy", "DataManager")
-    self.am_setModuleParam("shifterProxyLocation","%s/runit/%s/proxy" % (rootPath,AGENT_NAME))
+      self.StorageUsageDB = RPCClient( 'DataManagement/StorageUsage' )
+    self.am_setModuleParam( "shifterProxy", "DataManager" )
+    self.am_setModuleParam( "shifterProxyLocation", "%s/runit/%s/proxy" % ( gConfig.getValue( '/LocalSite/InstancePath', rootPath ), AGENT_NAME ) )
     return S_OK()
 
-  def removeEmptyDir(self,directory):
-    gLogger.info("removeEmptyDir: Attempting to remove empty directory from Storage Usage database")
-    res = self.StorageUsageDB.publishEmptyDirectory(directory)
+  def removeEmptyDir( self, directory ):
+    gLogger.info( "removeEmptyDir: Attempting to remove empty directory from Storage Usage database" )
+    res = self.StorageUsageDB.publishEmptyDirectory( directory )
     if not res['OK']:
-      gLogger.error("removeEmptyDir: Failed to remove empty directory from Storage Usage database.",res['Message'])
+      gLogger.error( "removeEmptyDir: Failed to remove empty directory from Storage Usage database.", res['Message'] )
     else:
-      if len(directory.split('/')) > 5:
-        res = self.catalog.removeCatalogDirectory(directory)
+      if len( directory.split( '/' ) ) > 5:
+        res = self.catalog.removeCatalogDirectory( directory )
         if not res['OK']:
-          gLogger.error("removeEmptyDir: Failed to remove empty directory from File Catalog.",res['Message'])
-        elif res['Value']['Failed'].has_key(directory):   
-          gLogger.error("removeEmptyDir: Failed to remove empty directory from File Catalog.",res['Value']['Failed'][directory])
+          gLogger.error( "removeEmptyDir: Failed to remove empty directory from File Catalog.", res['Message'] )
+        elif res['Value']['Failed'].has_key( directory ):
+          gLogger.error( "removeEmptyDir: Failed to remove empty directory from File Catalog.", res['Value']['Failed'][directory] )
         else:
-          gLogger.info("removeEmptyDir: Successfully removed empty directory from File Catalog.")
+          gLogger.info( "removeEmptyDir: Successfully removed empty directory from File Catalog." )
       else:
-        gLogger.info("removeEmptyDir: Not removing user base directory.")
+        gLogger.info( "removeEmptyDir: Not removing user base directory." )
     return S_OK()
