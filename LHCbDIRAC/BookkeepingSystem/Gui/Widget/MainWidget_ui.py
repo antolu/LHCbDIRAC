eb19d1837a066a0af6252d8ef1a724ec6c1bf6f8
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2010-08-10 12:35:17 +0000

    bug fix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@27844 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/Gui/Widget/MainWidget_ui.py
+++ LHCbDIRAC/BookkeepingSystem/Gui/Widget/MainWidget_ui.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'MainWidget.ui'
 #
-# Created: Wed Jun  9 11:14:30 2010
-#      by: PyQt4 UI code generator 4.3.3
+# Created: Tue Aug 10 14:26:55 2010
+#      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -15,12 +15,10 @@ class Ui_MainWidget(object):
         MainWidget.setObjectName("MainWidget")
         MainWidget.setWindowModality(QtCore.Qt.ApplicationModal)
         MainWidget.setEnabled(True)
-        MainWidget.resize(QtCore.QSize(QtCore.QRect(0,0,400,500).size()).expandedTo(MainWidget.minimumSizeHint()))
+        MainWidget.resize(400, 500)
         MainWidget.setCursor(QtCore.Qt.ArrowCursor)
-
         self.centralwidget = QtGui.QWidget(MainWidget)
         self.centralwidget.setObjectName("centralwidget")
-
         self.vboxlayout = QtGui.QVBoxLayout(self.centralwidget)
         self.vboxlayout.setObjectName("vboxlayout")
 
@@ -29,28 +27,21 @@ class Ui_MainWidget(object):
         self.tree.setObjectName("tree")
         self.vboxlayout.addWidget(self.tree)
         MainWidget.setCentralWidget(self.centralwidget)
-
         self.menubar = QtGui.QMenuBar(MainWidget)
-        self.menubar.setGeometry(QtCore.QRect(0,0,400,25))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 400, 25))
         self.menubar.setObjectName("menubar")
-
         self.menuFile = QtGui.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
-
         self.menuSetings = QtGui.QMenu(self.menubar)
         self.menuSetings.setObjectName("menuSetings")
         MainWidget.setMenuBar(self.menubar)
-
         self.statusbar = QtGui.QStatusBar(MainWidget)
         self.statusbar.setObjectName("statusbar")
         MainWidget.setStatusBar(self.statusbar)
-
         self.actionExit = QtGui.QAction(MainWidget)
         self.actionExit.setObjectName("actionExit")
-
         self.actionFile_dialog_paging_size = QtGui.QAction(MainWidget)
         self.actionFile_dialog_paging_size.setObjectName("actionFile_dialog_paging_size")
-
         self.actionDataQuality = QtGui.QAction(MainWidget)
         self.actionDataQuality.setObjectName("actionDataQuality")
         self.menuFile.addAction(self.actionExit)
@@ -66,7 +57,7 @@ class Ui_MainWidget(object):
         #self.tree.headerItem().setText(0,QtGui.QApplication.translate("MainWidget", "Jaj", None, QtGui.QApplication.UnicodeUTF8))
         #self.tree.headerItem().setText(1,QtGui.QApplication.translate("MainWidget", "New Column", None, QtGui.QApplication.UnicodeUTF8))
         self.menuFile.setTitle(QtGui.QApplication.translate("MainWidget", "File", None, QtGui.QApplication.UnicodeUTF8))
-        self.menuSetings.setTitle(QtGui.QApplication.translate("MainWidget", "Setings", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuSetings.setTitle(QtGui.QApplication.translate("MainWidget", "Settings", None, QtGui.QApplication.UnicodeUTF8))
         self.actionExit.setText(QtGui.QApplication.translate("MainWidget", "Exit", None, QtGui.QApplication.UnicodeUTF8))
         self.actionFile_dialog_paging_size.setText(QtGui.QApplication.translate("MainWidget", "File dialog page size", None, QtGui.QApplication.UnicodeUTF8))
         self.actionDataQuality.setText(QtGui.QApplication.translate("MainWidget", "DataQuality", None, QtGui.QApplication.UnicodeUTF8))
