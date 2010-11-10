bf961e3353ebf8410939d0b891c50d68124fd1de
paterson <paterson@4525493e-7705-40b1-a816-d608a930855b>
2010-11-10 12:19:40 +0000

    Relocated scripts from ProductionManagementSystem that only depend on DiracProduction.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@30242 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/Interfaces/scripts/__init__.py
@@ -0,0 +1,9 @@
+############################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/Interfaces/scripts/__init__.py $
+############################################################
+
+"""
+   LHCbDIRAC.Interfaces.scripts package
+"""
+
+__RCSID__ = "$Id: __init__.py 18161 2009-11-11 12:07:09Z acasajus $"
