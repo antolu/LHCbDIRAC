e0cd43d1c5a3e9b3b1ef4b433bf52d8992284048
atsareg <atsareg@4525493e-7705-40b1-a816-d608a930855b>
2010-11-14 01:49:21 +0000

    Bug fix - correct order of arguments in setSimulationEvents call
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@30333 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Workflow/Templates/Certification_Testing_run.py
+++ LHCbDIRAC/Workflow/Templates/Certification_Testing_run.py
@@ -234,7 +234,7 @@ production.setBKParameters(configName,configVersion,prodGroup,simCond)
 production.setDBTags(conditionsDBTag,detDescDBTag)
 #This isn't associated to a request so is outside of the status machine
 #but fill the events total with a large number anyway to track progress.
-production.setSimulationEvents(events,eventNumberTotal)
+production.setSimulationEvents(eventNumberTotal,events)
 
 prodDescription = 'A four step workflow Gauss->Boole->Brunel + Merging'
 
