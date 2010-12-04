952745fc385fe5fc8a20090be4b1b9455d29ff26
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2010-12-04 12:03:50 +0000

    FIX: remove obsolete __VERSION__ = "$Revision:$"
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@31306 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Core/Utilities/OutputDataPolicy.py
+++ LHCbDIRAC/Core/Utilities/OutputDataPolicy.py
@@ -2,8 +2,7 @@
 
     DIRAC assumes an execute() method will exist during usage.
 """
-__RCSID__   = "$Id: OutputDataPolicy.py 19570 2010-01-07 08:42:02Z joel $"
-__VERSION__ = "$Revision: 1.40 $"
+__RCSID__ = "$Id: OutputDataPolicy.py 19570 2010-01-07 08:42:02Z joel $"
 
 import DIRAC
 from DIRAC                                          import gLogger
@@ -12,18 +11,18 @@ from LHCbDIRAC.Core.Utilities.ProductionData        import preSubmissionLFNs
 
 class OutputDataPolicy:
 
-  def __init__(self,paramDict):
+  def __init__( self, paramDict ):
     self.paramDict = paramDict
 
-  def execute(self):
+  def execute( self ):
     jobDescription = self.paramDict['Job']
     prodID = self.paramDict['TransformationID']
     jobID = self.paramDict['TaskID']
     inputData = self.paramDict['InputData']
-    
-    job = Job(jobDescription)    
-    result = preSubmissionLFNs(job._getParameters(),job.createCode(),
-                               productionID=prodID,jobID=jobID,inputData=inputData)
+
+    job = Job( jobDescription )
+    result = preSubmissionLFNs( job._getParameters(), job.createCode(),
+                               productionID = prodID, jobID = jobID, inputData = inputData )
     if not result['OK']:
-      gLogger.error(result)
+      gLogger.error( result )
     return result
