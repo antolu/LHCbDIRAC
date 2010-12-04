5587bcfbaa86c68dd22292e523c242ad230a4a20
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2010-12-04 12:23:17 +0000

    FIX: reformat
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@31316 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/Service/LcgFileCatalogProxyHandler.py
+++ LHCbDIRAC/DataManagementSystem/Service/LcgFileCatalogProxyHandler.py
@@ -9,26 +9,26 @@ from DIRAC.Resources.Catalog.FileCatalog            import FileCatalog
 from DIRAC.Core.Utilities.Shifter                   import setupShifterProxyInEnv
 from DIRAC.Core.Utilities.Subprocess                    import pythonCall
 from DIRAC.FrameworkSystem.Client.ProxyManagerClient    import gProxyManager
-import os,shutil
+import os, shutil
 from types import *
 
 
-def initializeLcgFileCatalogProxyHandler(serviceInfo):
+def initializeLcgFileCatalogProxyHandler( serviceInfo ):
   return S_OK()
 
-class LcgFileCatalogProxyHandler(RequestHandler):
+class LcgFileCatalogProxyHandler( RequestHandler ):
 
-  types_callProxyMethod = [StringType,TupleType,DictionaryType]
-  def export_callProxyMethod(self, name, args, kargs):
+  types_callProxyMethod = [StringType, TupleType, DictionaryType]
+  def export_callProxyMethod( self, name, args, kargs ):
     """ A generic method to call methods of the Storage Element.
     """
-    res = pythonCall(0,self.__proxyWrapper,name,args,kargs)
+    res = pythonCall( 0, self.__proxyWrapper, name, args, kargs )
     if res['OK']:
       return res['Value']
     else:
       return res
 
-  def __proxyWrapper(self,name,args,kargs):
+  def __proxyWrapper( self, name, args, kargs ):
     """ The wrapper will obtain the client proxy and set it up in the environment.
     
         The required functionality is then executed and returned to the client.
@@ -37,44 +37,44 @@ class LcgFileCatalogProxyHandler(RequestHandler):
     if not res['OK']:
       return res
     try:
-      fileCatalog = FileCatalog(['LcgFileCatalogCombined'])
-      method = getattr(fileCatalog,name)
+      fileCatalog = FileCatalog( ['LcgFileCatalogCombined'] )
+      method = getattr( fileCatalog, name )
     except AttributeError, x:
       errStr = "LcgFileCatalogProxyHandler.__proxyWrapper: No method named %s" % name
-      gLogger.exception(errStr,name,x)
-      return S_ERROR(error)
+      gLogger.exception( errStr, name, x )
+      return S_ERROR( error )
     try:
-      result = method(*args,**kargs)
+      result = method( *args, **kargs )
       return result
-    except Exception,x:
+    except Exception, x:
       errStr = "LcgFileCatalogProxyHandler.__proxyWrapper: Exception while performing %s" % name
-      gLogger.exception(errStr,name,x)
-      return S_ERROR(errStr)
+      gLogger.exception( errStr, name, x )
+      return S_ERROR( errStr )
 
-  def __prepareSecurityDetails(self):
+  def __prepareSecurityDetails( self ):
     """ Obtains the connection details for the client  
     """
     try:
       clientDN = self._clientTransport.peerCredentials['DN']
       clientUsername = self._clientTransport.peerCredentials['username']
       clientGroup = self._clientTransport.peerCredentials['group']
-      gLogger.debug( "Getting proxy for %s@%s (%s)" % (clientUsername,clientGroup,clientDN) )
-      res = gProxyManager.downloadVOMSProxy(clientDN, clientGroup)
+      gLogger.debug( "Getting proxy for %s@%s (%s)" % ( clientUsername, clientGroup, clientDN ) )
+      res = gProxyManager.downloadVOMSProxy( clientDN, clientGroup )
       if not res['OK']:
         return res
-      chain = res['Value']  
+      chain = res['Value']
       proxyBase = "/tmp/proxies"
-      if not os.path.exists(proxyBase):
-        os.makedirs(proxyBase)
-      proxyLocation = "%s/%s-%s" % (proxyBase,clientUsername,clientGroup)
-      gLogger.debug("Obtained proxy chain, dumping to %s." % proxyLocation)
-      res = gProxyManager.dumpProxyToFile(chain,proxyLocation)
+      if not os.path.exists( proxyBase ):
+        os.makedirs( proxyBase )
+      proxyLocation = "%s/%s-%s" % ( proxyBase, clientUsername, clientGroup )
+      gLogger.debug( "Obtained proxy chain, dumping to %s." % proxyLocation )
+      res = gProxyManager.dumpProxyToFile( chain, proxyLocation )
       if not res['OK']:
         return res
-      gLogger.debug("Updating environment.")
+      gLogger.debug( "Updating environment." )
       os.environ['X509_USER_PROXY'] = res['Value']
       return res
-    except Exception,x:
+    except Exception, x:
       exStr = "__getConnectionDetails: Failed to get client connection details."
-      gLogger.exception(exStr,'',x)
-      return S_ERROR(exStr)
+      gLogger.exception( exStr, '', x )
+      return S_ERROR( exStr )
