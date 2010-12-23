d6ca32456133ee4277d5a806c4bad3d220dcdb91
acasajus <acasajus@4525493e-7705-40b1-a816-d608a930855b>
2010-12-23 15:18:36 +0000

    NEW: Migrated to the new version
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@32365 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/DataManagementSystem/Agent/UserStorageUsageAgent.py
@@ -0,0 +1,19 @@
+"""  UserStorageUsageAgent simply inherits the StorageUsage agent and loops over the /lhcb/user directory
+"""
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/DataManagementSystem/Agent/UserStorageUsageAgent.py $
+__RCSID__ = "$Id: UserStorageUsageAgent.py 18161 2009-11-11 12:07:09Z acasajus $"
+
+from DIRAC import S_OK
+from LHCbDIRAC.DataManagementSystem.Agent.StorageUsageAgent import StorageUsageAgent
+from DIRAC.Core.Utilities import List
+import time, os
+from types import *
+
+class UserStorageUsageAgent( StorageUsageAgent ):
+
+  def removeEmptyDir( self, dirPath ):
+    #Do not remove user's home dir
+    if len( List.fromChar( dirPath, "/" ) ) > 4:
+      return StorageUsageAgent.removeEmptyDir( dirPath )
+    return S_OK()
+
