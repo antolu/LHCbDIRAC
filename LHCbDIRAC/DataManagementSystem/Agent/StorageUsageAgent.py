ed543e36b79e62e89ff28a27f9f147a7bbe923ad
acasajus <acasajus@4525493e-7705-40b1-a816-d608a930855b>
2010-12-23 14:17:16 +0000

    FIX: :)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@32360 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/Agent/StorageUsageAgent.py
+++ LHCbDIRAC/DataManagementSystem/Agent/StorageUsageAgent.py
@@ -8,7 +8,7 @@ from DIRAC.Core.Base.AgentModule import AgentModule
 
 from DIRAC.Core.Utilities.DirectoryExplorer import DirectoryExplorer
 from DIRAC.DataManagementSystem.Client.ReplicaManager import CatalogDirectory
-from DIRAC.Core.Utilities.List import sortList
+from DIRAC.Core.Utilities import List
 from DIRAC.Core.Utilities.Time import timeInterval, dateTime, week
 
 import time, os, re
@@ -31,7 +31,7 @@ class StorageUsageAgent( AgentModule ):
 
     # This sets the Default Proxy to used as that defined under 
     # /Operations/Shifter/DataManager
-    # the shifterProxy option in the Configuration can be used to change this default.
+    # the shifterProxy option in the Configsorteduration can be used to change this default.
     self.am_setOption( 'shifterProxy', 'DataManager' )
 
     self.activePeriod = self.am_getOption( 'ActivePeriod', 0 )
@@ -51,7 +51,8 @@ class StorageUsageAgent( AgentModule ):
     for lfn in dirData:
       for SEName in dirData[ lfn ]:
         if SEName not in filesOpened:
-          filePath = os.path.join( self.__replicaListFilesDir, "%s.replicas.%s" % ( self.__startExecutionTime, SEName ) )
+          filePath = os.path.join( self.__replicaListFilesDir, "replicas.%s.%s.filling" %  ( SEName,
+                                                                                             self.__baseDirLabel ) )
           #Check if file is opened and if not open it
           if SEName not in filesOpened:
             if SEName not in self.__replicaFilesUsed:
@@ -68,27 +69,44 @@ class StorageUsageAgent( AgentModule ):
   def __resetReplicaListFiles( self ):
     self.__replicaFilesUsed = set()
     self.__replicaListFilesDir = os.path.join( self.am_getOption( "WorkDirectory" ), "replicaLists" )
-    self.log.info( "Replica Lists directory is %s" % self.__replicaListFilesDir )
-    now = time.time()
-    maxReplicaListAge = self.am_getOption( "MaxReplicaListAge", 86400*5 )
     if not os.path.isdir( self.__replicaListFilesDir ):
       os.makedirs( self.__replicaListFilesDir )
-    repRE = re.compile( "^([0-9]+)\.replicas.(.*)$" )
+    self.log.info( "Replica Lists directory is %s" % self.__replicaListFilesDir )
+
+  def __replicaListFilesDone( self ):
+    old = re.compile( "^replicas\.([a-zA-Z0-9\-_]*)\.%s\.old$" % self.__baseDirLabel )
+    current = re.compile( "^replicas\.([a-zA-Z0-9\-_]*)\.%s$" % self.__baseDirLabel )
+    filling = re.compile( "^replicas\.([a-zA-Z0-9\-_]*)\.%s\.filling$" % self.__baseDirLabel )
+    #Delete old
     for fileName in os.listdir( self.__replicaListFilesDir ):
-      match = repRE.match( fileName )
+      match = old.match( fileName )
       if match:
-        try:
-          iterationTime = float( match.group( 1 ) )
-        except ValueError, e:
-          continue
-        fileAge = now - iterationTime
-        self.log.info( "Replica List file %s has %s seconds" % ( fileName, fileAge ) )
-        if fileAge >= maxReplicaListAge:
-          filePath = os.path.join( self.__replicaListFilesDir, fileName )
-          self.log.notice( "Deleting %s" % filePath )
-          os.unlink( filePath )
+        os.unlink( os.path.join( self.__replicaListFilesDir, fileName ) )
+    #Current -> old
+    for fileName in os.listdir( self.__replicaListFilesDir ):
+      match = current.match( fileName )
+      if match:
+        newFileName = "replicas.%s.%s.old" % ( match.group( 1 ), self.__baseDirLabel )
+        self.log.info( "Moving \n %s\n to \n %s" % ( os.path.join( self.__replicaListFilesDir, fileName ), 
+                                                     os.path.join( self.__replicaListFilesDir, newFileName ) ) )
+        os.rename( os.path.join( self.__replicaListFilesDir, fileName ),
+                   os.path.join( self.__replicaListFilesDir, newFileName ) )
+    # filling to current
+    for fileName in os.listdir( self.__replicaListFilesDir ):
+      match = filling.match( fileName )
+      if match:
+        newFileName = "replicas.%s.%s" % ( match.group( 1 ), self.__baseDirLabel )
+        self.log.info( "Moving \n %s\n to \n %s" % ( os.path.join( self.__replicaListFilesDir, fileName ), 
+                                                     os.path.join( self.__replicaListFilesDir, newFileName ) ) )
+        os.rename( os.path.join( self.__replicaListFilesDir, fileName ),
+                   os.path.join( self.__replicaListFilesDir, newFileName ) )
+
+    return S_OK()
 
   def execute( self ):
+    self.__baseDir = self.am_getOption( 'BaseDirectory', '/lhcb' )
+    self.__baseDirLabel = "_".join( List.fromChar( self.__baseDir, "/" ) )
+
     self.__startExecutionTime = long( time.time() )
     self.__resetReplicaListFiles()
     res = self.StorageUsageDB.getStorageSummary()
@@ -96,7 +114,7 @@ class StorageUsageAgent( AgentModule ):
       gLogger.notice( "Storage Usage Summary" )
       gLogger.notice( "============================================================" )
       gLogger.notice( "%s %s %s" % ( 'Storage Element'.ljust( 40 ), 'Number of files'.rjust( 20 ), 'Total size'.rjust( 20 ) ) )
-      for se in sortList( res['Value'].keys() ):
+      for se in sorted( res['Value'] ):
         usage = res['Value'][se]['Size']
         files = res['Value'][se]['Files']
         site = se.split( '_' )[0].split( '-' )[0]
@@ -107,11 +125,10 @@ class StorageUsageAgent( AgentModule ):
         gMonitor.addMark( "%s-files" % se, files )
 
 
-    baseDir = self.am_getOption( 'BaseDirectory', '/lhcb' )
     ignoreDirectories = self.am_getOption( 'Ignore', [] )
     dirExplorer = DirectoryExplorer( reverse = True )
-    dirExplorer.addDir( baseDir )
-    gLogger.notice( "Initiating with %s as base directory." % baseDir )
+    dirExplorer.addDir( self.__baseDir )
+    gLogger.notice( "Initiating with %s as base directory." % self.__baseDir )
 
     # Loop over all the directories and sub-directories
     directoriesToPublish = {}
@@ -147,13 +164,13 @@ class StorageUsageAgent( AgentModule ):
       if numberOfFiles > 0:
         directoriesToPublish[currentDir] = {'Files':numberOfFiles, 'TotalSize':totalSize, 'SEUsage':siteUsage}
         gLogger.verbose( "%s %s %s" % ( 'Storage Element'.ljust( 40 ), 'Number of files'.rjust( 20 ), 'Total size'.rjust( 20 ) ) )
-        for storageElement in sortList( siteUsage.keys() ):
+        for storageElement in sorted( siteUsage ):
           usageDict = siteUsage[storageElement]
           gLogger.verbose( "%s %s %s" % ( storageElement.ljust( 40 ), str( usageDict['Files'] ).rjust( 20 ), str( usageDict['Size'] ).rjust( 20 ) ) )
         if self.am_getOption( "DumpReplicasToFile", False ):
           self.__writeReplicasListFiles( currentDir )
       elif len( subDirs ) == 0 and len( closedDirs ) == 0:
-        if not currentDir == baseDir:
+        if not currentDir == self.__baseDir:
           self.removeEmptyDir( currentDir )
           continue
 
@@ -180,11 +197,14 @@ class StorageUsageAgent( AgentModule ):
     #Publish remaining directories
     self.publishDirectories( directoriesToPublish )
 
+    #Move replica list files
+    self.__replicaListFilesDone() 
+
     #Clean records older than 1 day
     gLogger.info( "Finished recursive directory search." )
 
     elapsedTime = time.time() - self.__startExecutionTime
-    result = self.StorageUsageDB.purgeOutdatedEntries( baseDir, long( self.am_getOption( "OutdatedSeconds", elapsedTime * 2  + 60) ) )
+    result = self.StorageUsageDB.purgeOutdatedEntries( self.__baseDir, long( self.am_getOption( "OutdatedSeconds", elapsedTime * 2 + 60 ) ) )
     if not result[ 'OK' ]:
       return result
     self.log.notice( "Purged %s outdated records" % result[ 'Value' ] )
@@ -211,4 +231,5 @@ class StorageUsageAgent( AgentModule ):
     if not res['OK']:
       gLogger.error( "Failed to publish directories", res['Message'] )
     return res
+
 
