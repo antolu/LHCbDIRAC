a5e4993f1542f22d25b547673dff49b4fb9dce46
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2011-01-12 17:43:33 +0000

    FIX: new SetUsageMessage
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@32429 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Core/scripts/dirac-lhcb-analyse-log-file.py
+++ LHCbDIRAC/Core/scripts/dirac-lhcb-analyse-log-file.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 ########################################################################
 # $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk/LHCbDIRAC/Core/scripts/dirac-lhcb-run-test-job.py $
-# File :   dirac-lhcb-analyse-log-file.py
-# Author : Stuart Paterson
+# File :    dirac-lhcb-analyse-log-file.py
+# Author :  Stuart Paterson
 ########################################################################
-__RCSID__   = "$Id:  dirac-lhcb-analyse-log-file.py 23248 2010-03-18 07:57:40Z paterson $"
+"""
+  Perform comprehensive checks on the supplied log file if it exists.
+"""
+__RCSID__ = "$Id:  dirac-lhcb-analyse-log-file.py 23248 2010-03-18 07:57:40Z paterson $"
 
-import sys,string,os,shutil
+import sys, string, os, shutil
 
 import DIRAC
 from DIRAC.Core.Base import Script
 
-Script.registerSwitch( "f:", "LogFile=", "Path to log file you wish to analyse" )
+Script.registerSwitch( "f:", "LogFile=", "Path to log file you wish to analyze (mandatory)" )
 Script.registerSwitch( "p:", "Project=", "Optional: project name (will be guessed if not specified)" )
 
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                     'Usage:',
+                                     '  %s [option|cfgfile] ... CE' % Script.scriptName ] ) )
 Script.parseCommandLine( ignoreErrors = True )
 
 from DIRAC import gConfig, gLogger, S_OK, S_ERROR
@@ -24,35 +30,30 @@ args = Script.getPositionalArgs()
 logFile = ''
 projectName = ''
 
-#Methods to help with the script execution
-def usage():
-  print 'Usage: %s -f <LOGFILE> -p <PROJECT NAME> [Try -h,--help for more information]' %(Script.scriptName)
-  DIRAC.exit(2)
-  
 #Start the script and perform checks
 if args or not Script.getUnprocessedSwitches():
-  usage()
+  Script.showHelp()
 
 for switch in Script.getUnprocessedSwitches():
-  if switch[0].lower() in ('p','project'):
-    projectName=switch[1]
-  elif switch[0].lower() in ('f','logfile'):
-    logFile=switch[1]
+  if switch[0].lower() in ( 'p', 'project' ):
+    projectName = switch[1]
+  elif switch[0].lower() in ( 'f', 'logfile' ):
+    logFile = switch[1]
 
 exitCode = 0
 try:
-  result = analyseLogFile(logFile,projectName)
-except Exception,x:
-  gLogger.exception('Log file analysis failed with exception: "%s"' %x)
-  exitCode=2
-  DIRAC.exit(exitCode)    
+  result = analyseLogFile( logFile, projectName )
+except Exception, x:
+  gLogger.exception( 'Log file analysis failed with exception: "%s"' % x )
+  exitCode = 2
+  DIRAC.exit( exitCode )
 
 if not result['OK']:
-  gLogger.warn(result)
-  gLogger.error('Problem found with log file %s: "%s"' %(logFile,result['Message']))
-  exitCode=2
+  gLogger.warn( result )
+  gLogger.error( 'Problem found with log file %s: "%s"' % ( logFile, result['Message'] ) )
+  exitCode = 2
 else:
-  gLogger.verbose(result)
-  gLogger.info('Log file %s, %s' %(logFile,result['Value']))
-  
-DIRAC.exit(exitCode)  
+  gLogger.verbose( result )
+  gLogger.info( 'Log file %s, %s' % ( logFile, result['Value'] ) )
+
+DIRAC.exit( exitCode )
