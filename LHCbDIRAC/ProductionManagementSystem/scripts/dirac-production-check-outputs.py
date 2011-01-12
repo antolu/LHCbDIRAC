0d4c507d1dbc2230ef018dee69cf4cff575bfe6c
rgracian <rgracian@4525493e-7705-40b1-a816-d608a930855b>
2011-01-12 17:26:36 +0000

    FIX_ reformat
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@32418 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/scripts/dirac-production-check-outputs.py
+++ LHCbDIRAC/ProductionManagementSystem/scripts/dirac-production-check-outputs.py
@@ -21,9 +21,9 @@ productionOutputData.txt: A list of all the production Output Data
 
 Script.registerSwitch( "q", "Sequential", "Get LFN's for all productions in the range [ProdIDLow,ProdIDHigh]." )
 Script.registerSwitch( "n", "NoFiles", "Do not write or create files (requires verbose to be useful)." )
-Script.registerSwitch( "f:","FromDate=", "Start date of query period, string format: 'YYYY-MM-DD'" )
-Script.registerSwitch( "p:","OutputDir=", "Output Directory for files. Default: $PWD" )
-Script.registerSwitch( "v", "Verbose", "Enable Verbose Output")
+Script.registerSwitch( "f:", "FromDate=", "Start date of query period, string format: 'YYYY-MM-DD'" )
+Script.registerSwitch( "p:", "OutputDir=", "Output Directory for files. Default: $PWD" )
+Script.registerSwitch( "v", "Verbose", "Enable Verbose Output" )
 #Script.registerSwitch( "", "", "" )
 
 Script.parseCommandLine ( ignoreErrors = True )
@@ -31,18 +31,18 @@ Script.parseCommandLine ( ignoreErrors = True )
 from DIRAC.Interfaces.API.Dirac import Dirac
 from LHCbDIRAC.Interfaces.API.DiracProduction            import DiracProduction
 
-def getBoolean(value):
-  if value.lower()=='true':
+def getBoolean( value ):
+  if value.lower() == 'true':
     return True
-  elif value.lower()=='false':
+  elif value.lower() == 'false':
     return False
   else:
     print 'ERROR: expected boolean'
-    DIRAC.exit(2)
+    DIRAC.exit( 2 )
 
 def usage():
-  print 'Usage: %s [<Production IDs>] -f <YYYY-MM-DD> -n <-p <OutputDir>> -v | -q <Production ID Low> <Production ID High>' %(Script.scriptName)
-  DIRAC.exit(2)
+  print 'Usage: %s [<Production IDs>] -f <YYYY-MM-DD> -n <-p <OutputDir>> -v | -q <Production ID Low> <Production ID High>' % ( Script.scriptName )
+  DIRAC.exit( 2 )
 
 
 args = Script.getPositionalArgs()
@@ -51,47 +51,47 @@ args = Script.getPositionalArgs()
 sequentialProds = False
 recordData = True
 fromDate = 'today'
-outputDir = os.environ.get('PWD')
+outputDir = os.environ.get( 'PWD' )
 verbose = False
 
-if len(args) < 1:
+if len( args ) < 1:
   usage()
 
 #print Script.getUnprocessedSwitches()
 for switch in Script.getUnprocessedSwitches():
-  if switch[0].lower() in ('q', 'sequential'):
+  if switch[0].lower() in ( 'q', 'sequential' ):
     sequentialProds = True
-  elif switch[0].lower() in ('n', 'nofiles'):
+  elif switch[0].lower() in ( 'n', 'nofiles' ):
     recordData = False
-  elif switch[0].lower() in ('f', 'fromdate'):
+  elif switch[0].lower() in ( 'f', 'fromdate' ):
     fromDate = switch[1]
-  elif switch[0].lower() in ('p', 'outputdir'):
-    if not os.path.isdir(switch[1]):
-      print "ERROR: %s is not a valid directory, using default value." %(switch[1])
+  elif switch[0].lower() in ( 'p', 'outputdir' ):
+    if not os.path.isdir( switch[1] ):
+      print "ERROR: %s is not a valid directory, using default value." % ( switch[1] )
     else:
       outputDir = switch[1]
-  elif switch[0].lower() in ('v', 'verbose'):
+  elif switch[0].lower() in ( 'v', 'verbose' ):
     verbose = True
 
 if verbose:
-  print "Using outputDir: %s" %(outputDir)
+  print "Using outputDir: %s" % ( outputDir )
 
 
 
-      
+
 dirac = Dirac()
 d = DiracProduction()
 
 if sequentialProds:
-    print "length of args: %s" %(len(args))
-    if len(args) <= 1:
-      print "ERROR: Option 'Sequential' selected but only one argument (%s) supplied." %(args[0])
-      DIRAC.exit(1)
+    print "length of args: %s" % ( len( args ) )
+    if len( args ) <= 1:
+      print "ERROR: Option 'Sequential' selected but only one argument (%s) supplied." % ( args[0] )
+      DIRAC.exit( 1 )
     else:
-      prodIDLow = int(args[0])
-      prodIDHigh = int(args[1])
-      print "prodHigh: %s      prodLow: %s" %(prodIDHigh, prodIDLow)
-      if (prodIDHigh - prodIDLow) <= 0:
+      prodIDLow = int( args[0] )
+      prodIDHigh = int( args[1] )
+      print "prodHigh: %s      prodLow: %s" % ( prodIDHigh, prodIDLow )
+      if ( prodIDHigh - prodIDLow ) <= 0:
         print "Error: ProdIDHigh is less than ProdIDLow, using appropriate range."
         temp = prodIDLow
         prodIDLow = prodIDHigh
@@ -104,38 +104,38 @@ podFileName = 'productionOutputData.txt'
 if recordData:
     if verbose:
         print "Opening files ..."
-    listProds = open('%s/%s' %(outputDir, prodFileName),'w')
-    listBook = open('%s/%s' %(outputDir, bkFileName),'w')
-    listOutput = open('%s/%s' %(outputDir, podFileName),'w')
+    listProds = open( '%s/%s' % ( outputDir, prodFileName ), 'w' )
+    listBook = open( '%s/%s' % ( outputDir, bkFileName ), 'w' )
+    listOutput = open( '%s/%s' % ( outputDir, podFileName ), 'w' )
 
 numProds = 0
 numBKFiles = 0
 numPODFiles = 0
 
-def loopOverProds(prodID,fromDate):
+def loopOverProds( prodID, fromDate ):
     global numProds, numBKFiles, numPODFiles, recordData, d, verbose
-    print "Processing Production: %s" %(prodID)
-    jobs = d.selectProductionJobs(long(prodID),Status='Done', Date=fromDate)
+    print "Processing Production: %s" % ( prodID )
+    jobs = d.selectProductionJobs( long( prodID ), Status = 'Done', Date = fromDate )
     if jobs['OK']:
         numProds += 1
-        result = d.getProductionSummary(prodID)
+        result = d.getProductionSummary( prodID )
         jobIDS = jobs['Value']
         if recordData:
-            listProds.write("%s %s%s" %(prodID, fromDate, '\n'))
+            listProds.write( "%s %s%s" % ( prodID, fromDate, '\n' ) )
         if jobIDS:
             for job in jobIDS:
-                jobJDL =  dirac.getJobJDL(job)
-                if jobJDL['Value'].has_key('BookkeeepingLFNs'):
-                    if not isinstance(jobJDL['Value']['BookkeeepingLFNs'], list ):
+                jobJDL = dirac.getJobJDL( job )
+                if jobJDL['Value'].has_key( 'BookkeeepingLFNs' ):
+                    if not isinstance( jobJDL['Value']['BookkeeepingLFNs'], list ):
                       myList = [jobJDL['Value']['BookkeeepingLFNs']]
                     else:
                       myList = jobJDL['Value']['BookkeeepingLFNs']
                     for poData in myList:
                       if verbose:
-                        print "%s%s%s%s%s     %s" %('ProdID: ', prodID, ' JobID: ', job, ' BookkeeepingLFNs:', poData)
+                        print "%s%s%s%s%s     %s" % ( 'ProdID: ', prodID, ' JobID: ', job, ' BookkeeepingLFNs:', poData )
                       numBKFiles += 1
                       if recordData:
-                        listOutput.write("%s%s" %(poData, '\n'))
+                        listOutput.write( "%s%s" % ( poData, '\n' ) )
 
 #                    print "%s%s%s%s%s      %s" %('ProdID: ', prodID, ' JobID: ', job, ' BookkeepingLFNs:', jobJDL['Value']['BookkeeepingLFNs'])
 #                    numFiles += 1
@@ -143,39 +143,39 @@ def loopOverProds(prodID,fromDate):
 #                        listBook.write("%s%s" %(jobJDL['Value']['BookkeeepingLFNs'], '\n'))
                 else:
                     if verbose:
-                        print "%s%s%s%s%s" %('ProdID: ', prodID, ' JobID: ', job, ' has no BookkeepingLFNs.')
-                if jobJDL['Value'].has_key('ProductionOutputData'):
-                    if not isinstance(jobJDL['Value']['ProductionOutputData'], list ):
+                        print "%s%s%s%s%s" % ( 'ProdID: ', prodID, ' JobID: ', job, ' has no BookkeepingLFNs.' )
+                if jobJDL['Value'].has_key( 'ProductionOutputData' ):
+                    if not isinstance( jobJDL['Value']['ProductionOutputData'], list ):
                       myList = [jobJDL['Value']['ProductionOutputData']]
                     else:
                       myList = jobJDL['Value']['ProductionOutputData']
                     for poData in myList:
                       if verbose:
-                        print "%s%s%s%s%s %s" %('ProdID: ', prodID, ' JobID: ', job, ' ProductionOutputData:', poData)
+                        print "%s%s%s%s%s %s" % ( 'ProdID: ', prodID, ' JobID: ', job, ' ProductionOutputData:', poData )
                       numPODFiles += 1
                       if recordData:
-                        listOutput.write("%s%s" %(poData, '\n'))
+                        listOutput.write( "%s%s" % ( poData, '\n' ) )
                 else:
                   if verbose:
-                    print "%s%s%s%s%s" %('ProdID: ', prodID, ' JobID: ', job, ' has no ProductionOutputData.')
+                    print "%s%s%s%s%s" % ( 'ProdID: ', prodID, ' JobID: ', job, ' has no ProductionOutputData.' )
     return
 
 if sequentialProds:
-    for prodID in range(int(prodIDLow), int(prodIDHigh) + 1):
-        loopOverProds(prodID,fromDate)
+    for prodID in range( int( prodIDLow ), int( prodIDHigh ) + 1 ):
+        loopOverProds( prodID, fromDate )
 else:
     for prodID in args:
-        loopOverProds(int(prodID),fromDate)
-    
+        loopOverProds( int( prodID ), fromDate )
 
-print "%s%s%s%s%s%s" %("Prods: ", numProds, "  BK Files: ", numBKFiles, "  ProductionOutputData Files: ", numPODFiles)
+
+print "%s%s%s%s%s%s" % ( "Prods: ", numProds, "  BK Files: ", numBKFiles, "  ProductionOutputData Files: ", numPODFiles )
 if recordData:
     listProds.close()
     listBook.close()
     listOutput.close()
 
 if recordData:
-  print "Data written to:\n%s\n%s\n%s" %(prodFileName, bkFileName, podFileName)
+  print "Data written to:\n%s\n%s\n%s" % ( prodFileName, bkFileName, podFileName )
 
 
-DIRAC.exit(1)
+DIRAC.exit( 1 )
