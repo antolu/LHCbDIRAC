6bb9b22dbdd0267b2505b15552cc5eab1a917744
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2011-04-21 09:35:19 +0000

    Release v6r5-pre2 : bug fix in DataManagement and Transformation
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@38570 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -4,9 +4,9 @@ __path__ = extend_path( __path__, __name__ )
 # Define Version
 
 majorVersion = 6
-minorVersion = 4
-patchLevel = 2
-preVersion = 0
+minorVersion = 5
+patchLevel = 0
+preVersion = 2
 
 version = "v%sr%s" % ( majorVersion, minorVersion )
 buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
