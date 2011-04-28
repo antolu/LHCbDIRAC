eb88c64db67a3272c0d5e0bd8b959cec41c7099d
vibernar <vibernar@4525493e-7705-40b1-a816-d608a930855b>
2011-04-28 14:04:43 +0000

    RSS: Reformating part 1/2 (LHCbDIRAC)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@38839 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Policy/Configurations.py
+++ LHCbDIRAC/ResourceStatusSystem/Policy/Configurations.py
@@ -9,7 +9,9 @@
 
 __RCSID__ = "$Id: "
 
-from DIRAC.ResourceStatusSystem.Utilities.Utils import *
+from DIRAC.ResourceStatusSystem.PolicySystem.Configurations import \
+    ValidRes, ValidStatus,  \
+    ValidSiteType, ValidResourceType, ValidServiceType
 
 #############################################################################
 # policies parameters
@@ -776,25 +778,27 @@ views_panels = {
 # Clients cache
 #############################################################################
 
-Commands_ClientsCache = [( 'ClientsCache_Command', 'JobsEffSimpleEveryOne_Command' ),
-                         ( 'ClientsCache_Command', 'PilotsEffSimpleEverySites_Command' ),
-#                         ('ClientsCache_Command', 'TransferQualityEverySEs_Command'),
-                         ( 'ClientsCache_Command', 'DTEverySites_Command' ),
-                         ( 'ClientsCache_Command', 'DTEveryResources_Command' )
-                         ]
+Commands_ClientsCache = [
+  ( 'ClientsCache_Command', 'JobsEffSimpleEveryOne_Command' ),
+  ( 'ClientsCache_Command', 'PilotsEffSimpleEverySites_Command' ),
+  #('ClientsCache_Command', 'TransferQualityEverySEs_Command'),
+  ( 'ClientsCache_Command', 'DTEverySites_Command' ),
+  ( 'ClientsCache_Command', 'DTEveryResources_Command' )
+  ]
 
-Commands_AccountingCache = [( 'AccountingCache_Command', 'TransferQualityByDestSplitted_Command', ( 2, ), 'Always' ),
-                            ( 'AccountingCache_Command', 'FailedTransfersBySourceSplitted_Command', ( 2, ), 'Always' ),
-                            ( 'AccountingCache_Command', 'TransferQualityByDestSplittedSite_Command', ( 24, ), 'Hourly' ),
-#                            ('AccountingCache_Command', 'TransferQualityBySourceSplittedSite_Command', (24, ), 'Hourly'),
-                            ( 'AccountingCache_Command', 'SuccessfullJobsBySiteSplitted_Command', ( 24, ), 'Hourly' ),
-                            ( 'AccountingCache_Command', 'FailedJobsBySiteSplitted_Command', ( 24, ), 'Hourly' ),
-                            ( 'AccountingCache_Command', 'SuccessfullPilotsBySiteSplitted_Command', ( 24, ), 'Hourly' ),
-                            ( 'AccountingCache_Command', 'FailedPilotsBySiteSplitted_Command', ( 24, ), 'Hourly' ),
-                            ( 'AccountingCache_Command', 'SuccessfullPilotsByCESplitted_Command', ( 24, ), 'Hourly' ),
-                            ( 'AccountingCache_Command', 'FailedPilotsByCESplitted_Command', ( 24, ), 'Hourly' ),
-                            ( 'AccountingCache_Command', 'RunningJobsBySiteSplitted_Command', ( 24, ), 'Hourly' ),
-                            ( 'AccountingCache_Command', 'RunningJobsBySiteSplitted_Command', ( 168, ), 'Hourly' ),
-                            ( 'AccountingCache_Command', 'RunningJobsBySiteSplitted_Command', ( 720, ), 'Daily' ),
-                            ( 'AccountingCache_Command', 'RunningJobsBySiteSplitted_Command', ( 8760, ), 'Daily' ),
-                            ]
+Commands_AccountingCache = [
+  ( 'AccountingCache_Command', 'TransferQualityByDestSplitted_Command', ( 2, ), 'Always' ),
+  ( 'AccountingCache_Command', 'FailedTransfersBySourceSplitted_Command', ( 2, ), 'Always' ),
+  ( 'AccountingCache_Command', 'TransferQualityByDestSplittedSite_Command', ( 24, ), 'Hourly' ),
+  #('AccountingCache_Command', 'TransferQualityBySourceSplittedSite_Command', (24, ), 'Hourly'),
+  ( 'AccountingCache_Command', 'SuccessfullJobsBySiteSplitted_Command', ( 24, ), 'Hourly' ),
+  ( 'AccountingCache_Command', 'FailedJobsBySiteSplitted_Command', ( 24, ), 'Hourly' ),
+  ( 'AccountingCache_Command', 'SuccessfullPilotsBySiteSplitted_Command', ( 24, ), 'Hourly' ),
+  ( 'AccountingCache_Command', 'FailedPilotsBySiteSplitted_Command', ( 24, ), 'Hourly' ),
+  ( 'AccountingCache_Command', 'SuccessfullPilotsByCESplitted_Command', ( 24, ), 'Hourly' ),
+  ( 'AccountingCache_Command', 'FailedPilotsByCESplitted_Command', ( 24, ), 'Hourly' ),
+  ( 'AccountingCache_Command', 'RunningJobsBySiteSplitted_Command', ( 24, ), 'Hourly' ),
+  ( 'AccountingCache_Command', 'RunningJobsBySiteSplitted_Command', ( 168, ), 'Hourly' ),
+  ( 'AccountingCache_Command', 'RunningJobsBySiteSplitted_Command', ( 720, ), 'Daily' ),
+  ( 'AccountingCache_Command', 'RunningJobsBySiteSplitted_Command', ( 8760, ), 'Daily' ),
+  ]
