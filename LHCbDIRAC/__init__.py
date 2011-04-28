80fe13f8bd42cdac441390905076059b861f4b92
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2011-04-28 13:57:17 +0000

    Release v6r4p7 : changes on Core and RSS, bugfixes on Transformation
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@38817 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -4,9 +4,9 @@ __path__ = extend_path( __path__, __name__ )
 # Define Version
 
 majorVersion = 6
-minorVersion = 5
-patchLevel = 6
-preVersion = 3
+minorVersion = 4
+patchLevel = 7
+preVersion = 0
 
 version = "v%sr%s" % ( majorVersion, minorVersion )
 buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
