bc60c5529b27e955358d8730bfdbda714ada3a82
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2011-05-03 17:15:57 +0000

    #CHANGE: removed SE->LFC checks, plus few cleanups
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@38939 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/TransformationSystem/Agent/ValidateOutputDataAgent.py
+++ LHCbDIRAC/TransformationSystem/Agent/ValidateOutputDataAgent.py
@@ -3,30 +3,23 @@
 ########################################################################
 __RCSID__ = "$Id$"
 
-from DIRAC                                                          import S_OK, S_ERROR, gConfig, gMonitor, gLogger, rootPath
+from DIRAC                                                          import S_OK, S_ERROR, gLogger
 from DIRAC.DataManagementSystem.Client.ReplicaManager               import ReplicaManager
 from DIRAC.Core.Utilities.List                                      import sortList
-from DIRAC.DataManagementSystem.Client.StorageUsageClient           import StorageUsageClient
-from DIRAC.Resources.Catalog.FileCatalogClient                      import FileCatalogClient
 from DIRAC.TransformationSystem.Agent.ValidateOutputDataAgent       import ValidateOutputDataAgent as DIRACValidateOutputDataAgent
 from LHCbDIRAC.DataManagementSystem.Client.DataIntegrityClient      import DataIntegrityClient
-from LHCbDIRAC.TransformationSystem.Client.TransformationClient     import TransformationClient
-
-import re, os
 
 AGENT_NAME = 'Transformation/ValidateOutputDataAgent'
 
 class ValidateOutputDataAgent( DIRACValidateOutputDataAgent ):
 
   #############################################################################
+
   def initialize( self ):
     """Sets defaults
     """
     self.integrityClient = DataIntegrityClient()
     self.replicaManager = ReplicaManager()
-    self.transClient = TransformationClient()
-    self.storageUsageClient = StorageUsageClient()
-    self.fileCatalogClient = FileCatalogClient()
 
     # This sets the Default Proxy to used as that defined under 
     # /Operations/Shifter/DataManager
@@ -37,13 +30,10 @@ class ValidateOutputDataAgent( DIRACValidateOutputDataAgent ):
     gLogger.info( "Will treat the following transformation types: %s" % str( self.transformationTypes ) )
     self.directoryLocations = sortList( self.am_getOption( 'DirectoryLocations', ['TransformationDB', 'StorageUsage'] ) )
     gLogger.info( "Will search for directories in the following locations: %s" % str( self.directoryLocations ) )
-    storageElements = gConfig.getValue( '/Resources/StorageElementGroups/Tier1_MC_M-DST', [] )
-    storageElements.extend( ['CNAF_MC-DST', 'CNAF-RAW'] )
-    self.activeStorages = sortList( self.am_getOption( 'ActiveSEs', storageElements ) )
-    gLogger.info( "Will check the following storage elements: %s" % str( self.activeStorages ) )
     return S_OK()
 
   #############################################################################
+
   def checkTransformationIntegrity( self, prodID ):
     """ This method contains the real work
     """
@@ -109,26 +99,5 @@ class ValidateOutputDataAgent( DIRACValidateOutputDataAgent ):
       if not res['OK']:
         gLogger.error( res['Message'] )
         return res
-
-    ###################################################### 
-    #
-    # This check performs SE->Catalog->BK for possible output directories
-    #
-    for storageElementName in sortList( self.activeStorages ):
-      res = self.integrityClient.storageDirectoryToCatalog( directories, storageElementName )
-      if not res['OK']:
-        gLogger.error( res['Message'] )
-        return res
-      catalogMetadata = res['Value']['CatalogMetadata']
-      storageMetadata = res['Value']['StorageMetadata']
-      lfnMissing = []
-      for lfn in storageMetadata.keys():
-        if not lfn in bk2catalogMetadata.keys():
-          lfnMissing.append( lfn )
-      if lfnMissing:
-        res = self.integrityClient.catalogFileToBK( lfnMissing )
-        if not res['OK']:
-          gLogger.error( res['Message'] )
-          return res
 
     return S_OK()
