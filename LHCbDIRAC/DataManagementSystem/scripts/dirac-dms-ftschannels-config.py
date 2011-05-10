1c946c83b5d807919a48d311b164441ad22300bd
cibak <cibak@4525493e-7705-40b1-a816-d608a930855b>
2011-05-10 08:27:53 +0000

    help and usage updated, reading VO name from DIRAC config added
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@39070 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-ftschannels-config.py
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-ftschannels-config.py
@@ -1,5 +1,21 @@
+#!/bin/env python
 # $HeadURL$
-"""List configuration of LHCbDIRAC T0-T1 FTS channels.
+"""Display FTS channels configuration used by DIRAC to transfer files between different Tiers.
+"""
+
+outputInfo = """
+Output:
+ [1] Name:      name used by VO (carefull, it's lhcb specific)
+ [2] OrigName:  name of channel set during channel creation
+ [3] Files:     number of concurrent file transfers (other will be enqueued)
+ [4] Streams:   number of concurrent streams in each transfer
+ [5] Share:     percentage of VO share
+ [6] State:     operation state of the channel (one of Active, Inactive, Drain, 
+                Halted, Stopped, Archived) 
+ [7] Bandwidth: nominal bandwidth of the channel (in Mb/s)
+
+ Please read man pages for glite-transfer-channel-add, glite-transfer-channel-list,
+ glite-transfer-channel-set and glite-transfer-channel-setvoshare for more information.
 """
 
 __RCSID__ = "$Id$"
@@ -8,33 +24,42 @@ import sys
 from DIRAC.Core.Base import Script
 from DIRAC.Core.Utilities.Grid import executeGridCommand
 
+## grid env settings
 gridEnv = "/afs/cern.ch/project/gd/LCG-share/3.2.8-0/etc/profile.d/grid-env"
+
+## Tier1 (lhcb specific!)
 Tier1s = [ "GRIDKA", "NIKHEF", "PIC", "CNAF", "IN2P3", "CERN", "RAL",
            "SARA", "INFN", "FZK", "CERNPROD", "RALLCG2", "FZKLCG2",
            "SARAMATRIX", "INFNT1", "IN2P3CC"  ]
+## T0-T1, T1-T1 channels
 Tier1sChannels = [ chA+'-'+chB for chA in Tier1s for chB in Tier1s ]
-translate = { "CERNPROD" : "CERN", "FZK" : "GRIDKA",
+
+## translation between grid and lhcb naming conventions
+transDict = { "CERNPROD" : "CERN", "FZK" : "GRIDKA",
               "INFNT1" : "CNAF", "FZKLCG2" : "GRIDKA",
               "IN2P3CC" : "IN2P3", "RALLCG2" : "RAL",
               "SARAMATRIX" : "SARA" }
 
-def getChannel( ch ):
-  """ translate channel name from grid to lhcb world
+def translate( ch ):
+  """Translate channel name from grid to lhcb world.
+
+  :param str ch: channel name
   """
   source, target = ch.split("-")
-  if source in translate: source = translate[source]
-  if target in translate: target = translate[target]
+  if source in transDict: source = transDict[source]
+  if target in transDict: target = transDict[target]
   return "%s-%s" % (source, target)
 
 def extract( stdout ):
-  """
-  extract info about perticular channel directly from command output
+  """Extract information about particular channel directly from command output.
+  
+  :param str stdout: stdout from glite-transfer-channel-list command
   """  
   name = origName = state = band = files = streams = "" 
   for line in stdout.split("\n"):
     if line.startswith("Channel:"):
       origName = line.split(":")[1].strip()
-      name = getChannel( origName )
+      name = translate( origName )
     elif line.startswith("State:"):
       state = line.split(":")[1].strip()
     elif line.startswith("Bandwidth:"):
@@ -48,24 +73,28 @@ def extract( stdout ):
   return name, origName, files, streams, share, state, band
   
 def execute():
-  """
-  main worker here
+  """main worker here, parses command line, executes a bunch of glite commands and prints the output 
   """
   usageStr = __doc__ + """
 Usage:
 	%s [option|cfgfile] 
-""" % Script.scriptName
+""" % Script.scriptName + outputInfo
   Script.setUsageMessage( usageStr )
   Script.parseCommandLine()
+
   import DIRAC
+
+  res = DIRAC.gConfig.getOption( "/DIRAC/VirtualOrganization" )
+  if not res["OK"]:
+    return res["Message"]
+  vo = res["Value"]
   
   res = DIRAC.gConfig.getOptionsDict( "/Resources/FTSEndpoints")
   if not res["OK"]:
     return res["Message"] 
-
   URL2FTS = dict( [ (k, v.replace("FileTransfer", "ChannelManagement") ) for (k, v) in res["Value"].items() ]  )
   
-  head = " %-18s %-18s %-10s %-10s %-10s %-10s %-10s" % ( "Name", "OrigName", "Files", "Streams", "Share", "State", "Band" )
+  head = " %-18s %-18s %-10s %-10s %-10s %-10s %-10s" % ( "Name", "OrigName", "Files", "Streams", "Share", "State", "Bandwidth" )
   hashLine = "#" * len(head)
     
   for ftsLCG, ftsURL in sorted(URL2FTS.items()):
@@ -89,7 +118,7 @@ Usage:
         return res["Message"]
       res = res["Value"]
       exitCode, stdout, stderr = res
-      if "VO 'lhcb'" in stdout:
+      if "VO '%s'" % vo in stdout:
         print " %-18s %-18s %-10s %-10s %-10s %-10s %-10s" % extract( stdout ) 
 
 ## entry point
