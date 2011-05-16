2be2e1c1a39f59ea5241679e8e8e093fa8010260
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2011-05-16 08:50:33 +0000

    CHANGE: fix the 100.0% to 99.9% when some files are still missing
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@39108 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/TransformationSystem/Service/TransformationManagerHandler.py
+++ LHCbDIRAC/TransformationSystem/Service/TransformationManagerHandler.py
@@ -14,116 +14,116 @@ from types import *
 database = False
 def initializeTransformationManagerHandler( serviceInfo ):
   global database
-  database = TransformationDB('TransformationDB', 'Transformation/TransformationDB')
+  database = TransformationDB( 'TransformationDB', 'Transformation/TransformationDB' )
   return S_OK()
 
-class TransformationManagerHandler(TransformationManagerHandlerBase):
+class TransformationManagerHandler( TransformationManagerHandlerBase ):
 
-  def __init__(self,*args,**kargs):
-    self.setDatabase(database)
-    TransformationManagerHandlerBase.__init__(self, *args,**kargs)
+  def __init__( self, *args, **kargs ):
+    self.setDatabase( database )
+    TransformationManagerHandlerBase.__init__( self, *args, **kargs )
 
   #############################################################################
   #
   # Managing the BkQueries table
   #
-  
+
   types_createTransformationQuery = [ [LongType, IntType, StringType], DictType ]
-  def export_createTransformationQuery(self,transName,queryDict):
+  def export_createTransformationQuery( self, transName, queryDict ):
     authorDN = self._clientTransport.peerCredentials['DN']
-    res = database.createTransformationQuery(transName, queryDict,author=authorDN)
-    return self._parseRes(res)
+    res = database.createTransformationQuery( transName, queryDict, author = authorDN )
+    return self._parseRes( res )
 
   types_deleteTransformationBookkeepingQuery = [ [LongType, IntType, StringType] ]
-  def export_deleteTransformationBookkeepingQuery(self, transName):
+  def export_deleteTransformationBookkeepingQuery( self, transName ):
     authorDN = self._clientTransport.peerCredentials['DN']
-    res = database.deleteTransformationBookkeepingQuery(transName,author=authorDN)
-    return self._parseRes(res)  
+    res = database.deleteTransformationBookkeepingQuery( transName, author = authorDN )
+    return self._parseRes( res )
 
   types_deleteBookkeepingQuery = [ [LongType, IntType] ]
-  def export_deleteBookkeepingQuery(self, queryID):
-    res = database.deleteBookkeepingQuery(queryID)
-    return self._parseRes(res)
-  
+  def export_deleteBookkeepingQuery( self, queryID ):
+    res = database.deleteBookkeepingQuery( queryID )
+    return self._parseRes( res )
+
   types_getBookkeepingQueryForTransformation = [ [LongType, IntType, StringType] ]
-  def export_getBookkeepingQueryForTransformation(self, transName):
-    res = database.getBookkeepingQueryForTransformation(transName)
-    return self._parseRes(res)
+  def export_getBookkeepingQueryForTransformation( self, transName ):
+    res = database.getBookkeepingQueryForTransformation( transName )
+    return self._parseRes( res )
 
   types_setBookkeepingQueryEndRunForTransformation = [ [LongType, IntType, StringType] , [LongType, IntType]]
-  def export_setBookkeepingQueryEndRunForTransformation(self, transName, runNumber):
-    res = database.setBookkeepingQueryEndRunForTransformation(transName, runNumber)
-    return self._parseRes(res)
+  def export_setBookkeepingQueryEndRunForTransformation( self, transName, runNumber ):
+    res = database.setBookkeepingQueryEndRunForTransformation( transName, runNumber )
+    return self._parseRes( res )
 
   types_setBookkeepingQueryStartRunForTransformation = [ [LongType, IntType, StringType] , [LongType, IntType]]
-  def export_setBookkeepingQueryStartRunForTransformation(self, transName, runNumber):
-    res = database.setBookkeepingQueryStartRunForTransformation(transName, runNumber)
-    return self._parseRes(res)
+  def export_setBookkeepingQueryStartRunForTransformation( self, transName, runNumber ):
+    res = database.setBookkeepingQueryStartRunForTransformation( transName, runNumber )
+    return self._parseRes( res )
 
 
   #############################################################################
   #
   # Managing the TransformationRuns table
   #
-  
+
   types_getTransformationRuns = []
-  def export_getTransformationRuns(self,condDict={}, orderAttribute=None, limit=None):
-    res = database.getTransformationRuns(condDict=condDict, orderAttribute=orderAttribute,limit=limit) 
-    return self._parseRes(res)
+  def export_getTransformationRuns( self, condDict = {}, orderAttribute = None, limit = None ):
+    res = database.getTransformationRuns( condDict = condDict, orderAttribute = orderAttribute, limit = limit )
+    return self._parseRes( res )
 
   types_getTransformationRunStats = [ListType]
-  def export_getTransformationRunStats(self,transIDs):
-    res = database.getTransformationRunStats(transIDs)
-    return self._parseRes(res)
-                          
+  def export_getTransformationRunStats( self, transIDs ):
+    res = database.getTransformationRunStats( transIDs )
+    return self._parseRes( res )
+
 #  types_getTransformationRunsSummaryWeb = [DictType, ListType, IntType, IntType]
 #  def export_getTransformationRunsSummaryWeb(self,selectDict,sortList,startItem,maxItems):
-#    return self.__getTableSummaryWeb('TransformationRuns',selectDict,sortList,startItem,maxItems,selectColumns=['TransformationID','RunNumber','SelectedSite','Status'],timeStamp='LastUpdate',statusColumn='Status')  
+#    return self.__getTableSummaryWeb('TransformationRuns',selectDict,sortList,startItem,maxItems,selectColumns=['TransformationID','RunNumber','SelectedSite','Status'],timeStamp='LastUpdate',statusColumn='Status')
 
-  types_addTransformationRunFiles = [[LongType, IntType, StringType], [LongType, IntType],ListType]
-  def export_addTransformationRunFiles(self,transName,runID,lfns):
-    return database.addTransformationRunFiles(transName,runID,lfns)
+  types_addTransformationRunFiles = [[LongType, IntType, StringType], [LongType, IntType], ListType]
+  def export_addTransformationRunFiles( self, transName, runID, lfns ):
+    return database.addTransformationRunFiles( transName, runID, lfns )
 
-  types_setTransformationRunStatus = [[LongType, IntType, StringType], [LongType, IntType, ListType],StringTypes]
-  def export_setTransformationRunStatus(self,transID,runID,status):
-    return database.setTransformationRunStatus(transID,runID,status)
+  types_setTransformationRunStatus = [[LongType, IntType, StringType], [LongType, IntType, ListType], StringTypes]
+  def export_setTransformationRunStatus( self, transID, runID, status ):
+    return database.setTransformationRunStatus( transID, runID, status )
 
-  types_setTransformationRunsSite = [[LongType, IntType, StringType], [LongType, IntType],StringTypes]
-  def export_setTransformationRunsSite(self,transID,runID,assignedSE):
-    return database.setTransformationRunsSite(transID,runID,assignedSE)
+  types_setTransformationRunsSite = [[LongType, IntType, StringType], [LongType, IntType], StringTypes]
+  def export_setTransformationRunsSite( self, transID, runID, assignedSE ):
+    return database.setTransformationRunsSite( transID, runID, assignedSE )
 
   types_getTransformationRunsSummaryWeb = [DictType, ListType, IntType, IntType]
-  def export_getTransformationRunsSummaryWeb(self, selectDict, sortList, startItem, maxItems):
+  def export_getTransformationRunsSummaryWeb( self, selectDict, sortList, startItem, maxItems ):
     """ Get the summary of the transformation run information for a given page in the generic format """
 
     # Obtain the timing information from the selectDict
-    last_update = selectDict.get('LastUpdate',None)    
+    last_update = selectDict.get( 'LastUpdate', None )
     if last_update:
       del selectDict['LastUpdate']
-    fromDate = selectDict.get('FromDate',None)    
+    fromDate = selectDict.get( 'FromDate', None )
     if fromDate:
       del selectDict['FromDate']
     if not fromDate:
-      fromDate = last_update  
-    toDate = selectDict.get('ToDate',None)    
+      fromDate = last_update
+    toDate = selectDict.get( 'ToDate', None )
     if toDate:
-      del selectDict['ToDate']  
+      del selectDict['ToDate']
     # Sorting instructions. Only one for the moment.
     if sortList:
-      orderAttribute = sortList[0][0]+":"+sortList[0][1]
+      orderAttribute = sortList[0][0] + ":" + sortList[0][1]
     else:
       orderAttribute = None
 
     # Get the transformations that match the selection
-    res = database.getTransformationRuns(condDict=selectDict,older=toDate, newer=fromDate, orderAttribute=orderAttribute)
+    res = database.getTransformationRuns( condDict = selectDict, older = toDate, newer = fromDate, orderAttribute = orderAttribute )
     if not res['OK']:
-      return self._parseRes(res)
+      return self._parseRes( res )
 
     # Prepare the standard structure now within the resultDict dictionary
     resultDict = {}
     trList = res['Records']
     # Create the total records entry
-    nTrans = len(trList)
+    nTrans = len( trList )
     resultDict['TotalRecords'] = nTrans
     # Create the ParameterNames entry
     paramNames = res['ParameterNames']
@@ -133,66 +133,66 @@ class TransformationManagerHandler(TransformationManagerHandlerBase):
     #taskStateNames   = ['Created','Running','Submitted','Failed','Waiting','Done','Stalled']
     #resultDict['ParameterNames'] += ['Jobs_'+x for x in taskStateNames]
     # Add the file states to the ParameterNames entry
-    fileStateNames  = ['PercentProcessed','Processed','Unused','Assigned','Total','Problematic']
-    resultDict['ParameterNames'] += ['Files_'+x for x in fileStateNames]
+    fileStateNames = ['PercentProcessed', 'Processed', 'Unused', 'Assigned', 'Total', 'Problematic']
+    resultDict['ParameterNames'] += ['Files_' + x for x in fileStateNames]
 
     # Get the transformations which are within the selected window
     if nTrans == 0:
-      return S_OK(resultDict)
+      return S_OK( resultDict )
     ini = startItem
     last = ini + maxItems
     if ini >= nTrans:
-      return S_ERROR('Item number out of range')
+      return S_ERROR( 'Item number out of range' )
     if last > nTrans:
       last = nTrans
     transList = trList[ini:last]
     if not transList:
-      return S_OK(resultDict)
+      return S_OK( resultDict )
 
     # Obtain the run statistics for the requested transformations
     transIDs = []
     for transRun in transList:
-      transRunDict = dict(zip(paramNames,transRun))
-      transID = int(transRunDict['TransformationID'])
+      transRunDict = dict( zip( paramNames, transRun ) )
+      transID = int( transRunDict['TransformationID'] )
       if not transID in transIDs:
-        transIDs.append(transID)
-    res = database.getTransformationRunStats(transIDs)
+        transIDs.append( transID )
+    res = database.getTransformationRunStats( transIDs )
     if not res['OK']:
       return res
     transRunStatusDict = res['Value']
-      
+
     statusDict = {}
     # Add specific information for each selected transformation/run
     for transRun in transList:
-      transRunDict = dict(zip(paramNames,transRun))
+      transRunDict = dict( zip( paramNames, transRun ) )
       transID = transRunDict['TransformationID']
       runID = transRunDict['RunNumber']
-      if not transRunStatusDict.has_key(transID):
-        transRun += [0.,0,0,0,0,0]
+      if not transRunStatusDict.has_key( transID ):
+        transRun += [0., 0, 0, 0, 0, 0]
         continue
-      if not transRunStatusDict[transID].has_key(runID):
-        transRun += [0.,0,0,0,0,0]
+      if not transRunStatusDict[transID].has_key( runID ):
+        transRun += [0., 0, 0, 0, 0, 0]
         continue
       # Update the status counters
       status = transRunDict['Status']
-      if not statusDict.has_key(status):
+      if not statusDict.has_key( status ):
         statusDict[status] = 0
       statusDict[status] += 1
 
       # Populate the run file statistics
       fileDict = transRunStatusDict[transID][runID]
       if fileDict['Total'] == 0:
-        fileDict['PercentProcessed']  = 0
+        fileDict['PercentProcessed'] = 0
       else:
-        processed = fileDict.get('Processed')
+        processed = fileDict.get( 'Processed' )
         if not processed:
           processed = 0
-        fileDict['PercentProcessed'] = "%.1f" % ((processed*100.0)/fileDict['Total'])
+        fileDict['PercentProcessed'] = "%.1f" % ( int( processed * 1000. / fileDict['Total'] ) / 10. )
       for state in fileStateNames:
-        if fileDict and fileDict.has_key(state):
-          transRun.append(fileDict[state])
+        if fileDict and fileDict.has_key( state ):
+          transRun.append( fileDict[state] )
         else:
-          transRun.append(0)
+          transRun.append( 0 )
 
       # Get the statistics on the number of jobs for the transformation
       #res = database.getTransformationTaskRunStats(transID)
@@ -207,4 +207,4 @@ class TransformationManagerHandler(TransformationManagerHandlerBase):
 
     resultDict['Records'] = transList
     resultDict['Extras'] = statusDict
-    return S_OK(resultDict)
+    return S_OK( resultDict )
