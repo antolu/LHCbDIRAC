41d2c4fcf141bbd84e8c867ca9aba14bc2e49dbd
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2011-06-01 09:03:37 +0000

    Minor changes on HCClient
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@40006 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Client/HCClient.py
+++ LHCbDIRAC/ResourceStatusSystem/Client/HCClient.py
@@ -2,34 +2,40 @@ import xmlrpclib
 
 from datetime import datetime
 
-def checkInt(name,value):
-  if not isinstance(value,int):
-    raise Exception('Wrong %s, expected int.'%name)
+def checkInt( name, value ):
+  if not isinstance( value, int ):
+    raise Exception( 'Wrong %s, expected int.' % name )
   return value
 
-def checkBool(name,value):
-  if not isinstance(value,bool):
-    raise Exception('Wrong %s, expected bool.'%name)
+def checkBool( name, value ):
+  if not isinstance( value, bool ):
+    raise Exception( 'Wrong %s, expected bool.' % name )
   return value
 
-def checkDatetime(name,value):
-  if not isinstance(value,datetime):
-    raise Exception('Wrong %s, expected datetime.'%name)
-  return value.strftime("%Y-%m-%d %H:%M:%S")
+def checkDatetime( name, value ):
+  if not isinstance( value, datetime ):
+    raise Exception( 'Wrong %s, expected datetime.' % name )
+  return value.strftime( "%Y-%m-%d %H:%M:%S" )
+
+def checkDict( name, value ):
+  if not isinstance( value, dict ):
+    raise Exception( 'Wrong %s, expected dict.' % name )
+  return value
+
 
 class HCClient:
   
 #############################################################################
   
-  def __init__(self):
+  def __init__( self ):
     """ 
       Initialize HCClient, creates ServerProxy to send XML-RPCs to the HC Server.
     """
-    self.rpc_srv = xmlrpclib.ServerProxy("http://hammercloud.cern.ch/hc/xmlrpc")
+    self.rpc_srv = xmlrpclib.ServerProxy( "http://hammercloud.cern.ch/hc/xmlrpc" )
 
 #############################################################################
    
-  def getTemplates(self,detailed=0,verbose=0):
+  def getTemplates( self, detailed = 0, verbose = 0 ):
     """
       Returns a list of templates with basic information, 
       of the given application - in this case lhcb.
@@ -44,16 +50,16 @@ class HCClient:
     """
     
     try:
-      res = self.rpc_srv.getTemplates({'app':'lhcb','detailed':detailed})
+      res = self.rpc_srv.getTemplates({ 'app':'lhcb', 'detailed':detailed })
       if verbose:
         print res
       return res
     except Exception, e:
-      return [False,{'type':'SUBMISSION','response':e}]
+      return [ False, { 'type':'SUBMISSION', 'response':e }]
    
 #############################################################################   
    
-  def getResults(self,test,detailed=0,verbose=0):
+  def getResults( self, test, detailed = 0, verbose = 0 ):
     """
       Returns a list of results with basic information, 
       of the given application -in this case lhcb, and a 
@@ -72,16 +78,16 @@ class HCClient:
     """
     
     try:
-      res = self.rpc_srv.getResults({'app':'lhcb','test':test,'detailed':detailed})
+      res = self.rpc_srv.getResults({ 'app':'lhcb', 'test':test, 'detailed':detailed })
       if verbose:  
         print res
       return res
     except Exception, e:
-      return [False, {'type':'SUBMISSION','response':e}]
+      return [ False, { 'type':'SUBMISSION', 'response':e }]
    
 #############################################################################   
    
-  def getSummarizedResults(self,test,detailed=0,verbose = 0):
+  def getSummarizedResults( self, test, detailed = 0, verbose = 0 ):
     """
       Returns a list of summarized results with basic information, 
       of the given application -in this case lhcb, and a 
@@ -100,16 +106,16 @@ class HCClient:
     """
     
     try:
-      res = self.rpc_srv.getSummarizedResults({'app':'lhcb','test':test,'detailed':detailed})
+      res = self.rpc_srv.getSummarizedResults({ 'app':'lhcb', 'test':test, 'detailed':detailed })
       if verbose:
         print res 
       return res
     except Exception, e:
-      return [False, {'type':'SUBMISSION','response':e}]
+      return [ False, { 'type':'SUBMISSION', 'response':e }]
     
 #############################################################################  
   
-  def getTest(self,test,verbose = 0):
+  def getTest( self, test, verbose = 0 ):
     """
       Returns test basic information.
       
@@ -122,16 +128,16 @@ class HCClient:
         
     """
     try:
-      res = self.rpc_srv.getTest({'app':'lhcb','test':test})
+      res = self.rpc_srv.getTest({ 'app':'lhcb', 'test':test })
       if verbose:   
         print res
       return res
     except Exception, e:
-      return [False, {'type':'SUBMISSION','response':e}]
+      return [ False, { 'type':'SUBMISSION', 'response':e }]
     
 #############################################################################   
    
-  def sendTest(self,site,*args,**kwargs):
+  def sendTest( self, site, *args, **kwargs ):
     """
       Sends a test to a single site.
             
@@ -147,17 +153,17 @@ class HCClient:
       
     """
     
-    site = HCSite(site)
+    site = HCSite( site )
     
-    hctest = HCTest(site,**kwargs)
+    hctest = HCTest( site, **kwargs )
           
-    print '  + sending %s'%hctest.toDict()
+    print '  + sending %s' % hctest.toDict()
     
     try:   
-      res = self.rpc_srv.createTest(hctest.toDict())
+      res = self.rpc_srv.createTest( hctest.toDict() )
       return res
     except Exception, e:
-      return [False,{'type':'SUBMISSION','response':e}]
+      return [ False, { 'type':'SUBMISSION', 'response':e }]
     
 #############################################################################
 
@@ -169,24 +175,24 @@ class HCSite:
   
 #############################################################################  
   
-  def __init__(self,
-               siteName,
-               resubmit_enabled=True,
-               resubmit_force=False,
-               num_datasets_per_bulk=1,
-               min_queue_depth=0,
-               max_running_jobs=1):
+  def __init__( self,
+                siteName,
+                resubmit_enabled      = True,
+                resubmit_force        = False,
+                num_datasets_per_bulk = 1,
+                min_queue_depth       = 0,
+                max_running_jobs      = 1 ):
     
-    self.name = siteName
-    self.resubmit_enabled = resubmit_enabled
-    self.resubmit_force = resubmit_force
+    self.name                  = siteName
+    self.resubmit_enabled      = resubmit_enabled
+    self.resubmit_force        = resubmit_force
     self.num_datasets_per_bulk = num_datasets_per_bulk
-    self.min_queue_depth = min_queue_depth
-    self.max_running_jobs = max_running_jobs
+    self.min_queue_depth       = min_queue_depth
+    self.max_running_jobs      = max_running_jobs
     
 #############################################################################
 
-  def toDict(self):
+  def toDict( self ):
     
     """
       Default values at server are:
@@ -206,23 +212,23 @@ class HCSite:
             'name'        : self.name
             }
     
-    re = checkBool('resubmit_enabled',self.resubmit_enabled)
+    re = checkBool( 'resubmit_enabled', self.resubmit_enabled )
     if re != True:
       dict['resubmit_enabled'] = re
 
-    rf = checkBool('resubmit_force',self.resubmit_force)
+    rf = checkBool( 'resubmit_force', self.resubmit_force )
     if rf != False:
       dict['resubmit_force'] = rf
       
-    ndpb = checkInt('num_datasets_per_bulk',self.num_datasets_per_bulk)  
+    ndpb = checkInt( 'num_datasets_per_bulk', self.num_datasets_per_bulk )  
     if ndpb != 1:
       dict['num_datasets_per_bulk'] = ndpb
       
-    mqd = checkInt('min_queue_depth',self.min_queue_depth)  
+    mqd = checkInt( 'min_queue_depth', self.min_queue_depth )  
     if mqd != 0:
       dict['min_queue_depth'] = mqd
       
-    mrj = checkInt('max_running_jobs',self.max_running_jobs)  
+    mrj = checkInt( 'max_running_jobs', self.max_running_jobs )  
     if mrj != 1:
       dict['max_running_jobs'] = mrj  
     
@@ -231,12 +237,12 @@ class HCSite:
 #############################################################################
 
 
-
 class HCTest:
 
 #############################################################################
   
-  def __init__(self, sites, template = 1, duration = 3600, description = 'RSS test', starttime = None):
+  def __init__( self, sites, template = 1, duration = 3600, description = 'RSS test', 
+                starttime = None, extraargs = '' ):
     
     """
       Initialize HC Test model
@@ -261,10 +267,11 @@ class HCTest:
     self.template       = template
     self.sites          = sites
     self.starttime      = starttime
+    self.extraargs      = extraargs
 
 #############################################################################
 
-  def toDict(self):
+  def toDict( self ):
     
     """
       Generate dictionary with all information needed in the
@@ -272,44 +279,43 @@ class HCTest:
     """
     
     dict = {
-            'app'        : self.__app,
-            'description': str(self.description),
-            'duration'   : checkInt('duration',self.duration),
-            'template'   : checkInt('template',self.template)
+            'app'          : self.__app,
+            'description'  : str( self.description ),
+            'duration'     : checkInt( 'duration',self.duration ),
+            'template'     : checkInt( 'template',self.template ),
+            'starttime'    : checkDatetime( 'starttime', self.starttime ),
+            'extraargs'    : self.extraargs
             }
     
-    if self.starttime:
-      dict['starttime'] = checkDatetime('starttime',self.starttime)
-    
-    sites = self.checkSitesList(self.sites)
+    sites = self.checkSitesList( self.sites )
     
     dict['sites'] = []
     for site in sites:
-      dict['sites'].append(site.toDict())
-    
+      dict['sites'].append( site.toDict() )
+        
     return dict
 
 #############################################################################
 
-  def checkSitesList(self,value):  
+  def checkSitesList( self, value ):  
     
     """
       Check if the sites is either a HCSite or a list of HCSites
     """
     
-    if not isinstance(value,list):
-      if not isinstance(value,HCSite):
-        raise Exception('Wrong sites, expected list or HCSite')
-      value = [value]
+    if not isinstance( value, list ):
+      if not isinstance( value, HCSite ):
+        raise Exception( 'Wrong sites, expected list or HCSite' )
+      value = [ value ]
     else:
-      if filter(lambda x: isinstance(x,HCSite) == False, value):  
-        raise Exception('Wrong sites, expected list of HCSites')
+      if filter( lambda x: isinstance( x, HCSite ) == False, value ):  
+        raise Exception( 'Wrong sites, expected list of HCSites' )
     
     return value
   
 #############################################################################
   
-  def rebootUniverse(self):
+  def rebootUniverse( self ):
     
     """
       Unless you know what are you doing, do not reboot the universe please.
