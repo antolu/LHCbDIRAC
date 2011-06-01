650f71f111bd82c2f4509f983d12ae87d9524bcc
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2011-06-01 08:49:26 +0000

    HCCommand simplified
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@40005 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Command/HC_Command.py
+++ LHCbDIRAC/ResourceStatusSystem/Command/HC_Command.py
@@ -1,3 +1,4 @@
+from DIRAC.ConfigurationSystem.Client.Config import gConfig
 from DIRAC.ResourceStatusSystem.Command.Command import *
 
 from LHCbDIRAC.ResourceStatusSystem.Client.HCClient import HCClient
@@ -17,52 +18,59 @@ class HC_Command(Command):
     """
     super(HC_Command, self).doCommand()
 
-    result = None
+    result = []
 
     '''
       This is a nice example of spaghetti code.
       Think how to configure the agent. Maybe in the CS. 
     '''
     
-    TEST_DURATION = 1.5
+#    LHCb_setup = gConfig.getValue( "DIRAC/Setup" )
+#    RSS_setup  = gConfig.getValue( "DIRAC/Setups/%s/ResourceStatus" % LHCb_setup )
+#    
+#    try:
+#      TEST_DURATION = float(gConfig.getValue( "Systems/ResourceStatus/RSS_setup/Agents/HCAgent/testDurationMin" )) / 60.
+#    except:
+#      gLogger.error( 'HC_command error getting testDurationMin ' )
+#      return None
 
-    rsa = ResourceStatusAgentClient()    
-    
+    rsa = ResourceStatusAgentClient()       
     res = rsa.getLastTest( self.args[1], 'HClastfinished' )
     
     if res:
       
       res = res[-1]
 
-      counter     = res[7]
-      counterTime = res[8]
-      now         = datetime.now()
+#      counter     = res[7]
+#      counterTime = res[8]
+#      now         = datetime.now()
     
-      influence   = 2 * counter * TEST_DURATION
+#      influence   = 2 * counter * TEST_DURATION
       
-      endInfluence = counterTime + timedelta( hours = influence )
+#      endInfluence = counterTime + timedelta( hours = influence )
         
-      if ( counterTime < now ) and ( now < endInfluence ):
+#      if ( counterTime < now ) and ( now < endInfluence ):
+#      if now < endInfluence: 
            
-        hc = HCClient()
+      hc = HCClient()
                     
-        sum = hc.getSummarizedResults( res[0], detailed = 1 )
-        if sum[0]:
-          gLogger.info( 'HC_command %s ' % str(sum) )
-          result = sum[1]['response']['summary'][0] 
-        else:
-          gLogger.info( 'Something went wrong' )
-          result = []
+      sum = hc.getSummarizedResults( res[0], detailed = 1 )
+      if sum[0]:
+        gLogger.debug( 'HC_command %s ' % str(sum) )
+        result = sum[1]['response']['summary'][0] 
+      else:
+        gLogger.info( 'Something went wrong' )
+        result = None
         
-        return {'Result':result}
+ #     return {'Result':result}
       
-    res = rsa.getTestListBySite( self.args[1], True )
+ #   res = rsa.getTestListBySite( self.args[1], True )
     
-    if res:
-      res = res[-1]
-      if ('HCongoing' in res[3]) or ('HCscheduled' in res[3]):
-        gLogger.info('HCCommand %s' % res[3])
-        result = []   
+ #   if res:
+ #     res = res[-1]
+ #     if ('HCongoing' in res[3]) or ('HCscheduled' in res[3]):
+ #       gLogger.info('HCCommand %s' % res[3])
+ #       result = []   
 
     return {'Result':result}
 
