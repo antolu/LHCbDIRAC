29bba418f6e0ce988efb8b7a95fc6769adbbbc31
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2011-08-03 14:05:01 +0000

    Improved docs
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@41878 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Client/ResourceManagementClient.py
+++ LHCbDIRAC/ResourceStatusSystem/Client/ResourceManagementClient.py
@@ -50,6 +50,9 @@ class ResourceManagementClient( DIRACResourceManagementClient ):
   def getHCTest( self, testID ):
     '''
     Returns the details of a specific HammerCloud test.
+    
+    :params:
+      :attr: `testID` : integer - (optional) assigned (by HC) test ID    
     '''
 
     return self.rsM.getHCTest( testID )    
@@ -60,6 +63,11 @@ class ResourceManagementClient( DIRACResourceManagementClient ):
     '''
     Returns the details of the tests on a specific site if any. If last
     parameter is used, only the last entry of the selection will be returned.
+    
+    :params:
+      :attr:`siteName`: string - site where the test is submitted
+      
+      :attr: `last` : bool - return last entry
     '''
         
     return self.rsM.getHCTestListBySite( siteName, last )
@@ -78,6 +86,21 @@ class ResourceManagementClient( DIRACResourceManagementClient ):
     Adds, if not already there a test with same metricName and serviceURI, a 
     new entry on the DB with all parameters above. If not, an exception will be
     returned.
+    
+    :params:
+      :attr:`metricName`: string - name of the metric
+
+      :attr:`serviceURI`: string - full name of the service
+           
+      :attr:`siteName`: string - site where the test was run
+      
+      :attr:`serviceFlavour`: string - type of service tested
+      
+      :attr:`metricStatus`: string - output of the test
+      
+      :attr:`summaryData`: string - detailed output
+      
+      :attr:`timestamp`: date time - test execution timestamp 
     '''
 
     return self.rsM.addMonitoringTest( metricName, serviceURI, siteName, 
@@ -92,6 +115,21 @@ class ResourceManagementClient( DIRACResourceManagementClient ):
     '''
     Updates the entry with metricName and serviceURI if any. If not, an
     exception will be returned.
+    
+    :params:
+      :attr:`metricName`: string - name of the metric
+
+      :attr:`serviceURI`: string - full name of the service
+           
+      :attr:`siteName`: string - (optional) site where the test was run
+      
+      :attr:`serviceFlavour`: string - (optional) type of service tested
+      
+      :attr:`metricStatus`: string - (optional) output of the test
+      
+      :attr:`summaryData`: string - (optional) detailed output
+      
+      :attr:`timestamp`: date time - (optional) test execution timestamp 
     '''
 
 
@@ -106,6 +144,21 @@ class ResourceManagementClient( DIRACResourceManagementClient ):
                             metricStatus = None, summaryData = None, timestamp = None ):
     '''
     Returns a list of Monitoring tests, using the parameters as filters. 
+    
+    :params:
+      :attr:`metricName`: string - (optional) name of the metric
+
+      :attr:`serviceURI`: string - (optional) full name of the service
+           
+      :attr:`siteName`: string - (optional) site where the test was run
+      
+      :attr:`serviceFlavour`: string - (optional) type of service tested
+      
+      :attr:`metricStatus`: string - (optional) output of the test
+      
+      :attr:`summaryData`: string - (optional) detailed output
+      
+      :attr:`timestamp`: date time - (optional) test execution timestamp     
     '''
 
     return self.rsM.getMonitoringTestList( metricName, serviceURI, siteName, 
@@ -120,6 +173,21 @@ class ResourceManagementClient( DIRACResourceManagementClient ):
     '''
     This method adds a monitoring test if there is no entry on the DB with
     metricName and serviceURI. If there is already one, it updates it.
+    
+    :params:
+      :attr:`metricName`: string - name of the metric
+
+      :attr:`serviceURI`: string - full name of the service
+           
+      :attr:`siteName`: string - site where the test was run
+      
+      :attr:`serviceFlavour`: string - type of service tested
+      
+      :attr:`metricStatus`: string - output of the test
+      
+      :attr:`summaryData`: string - detailed output
+      
+      :attr:`timestamp`: date time - test execution timestamp    
     '''
     
     res = self.rsM.getMonitoringTestList( metricName, serviceURI )
