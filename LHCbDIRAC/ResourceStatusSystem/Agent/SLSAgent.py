0d9d48130fd2628f2c00a0d3406669dfe2d964c0
vibernar <vibernar@4525493e-7705-40b1-a816-d608a930855b>
2011-08-03 16:47:56 +0000

    SLSAgent.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@41884 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Agent/SLSAgent.py
+++ LHCbDIRAC/ResourceStatusSystem/Agent/SLSAgent.py
@@ -447,6 +447,103 @@ class LFCReplicaTest(object):
       if type(self.cfg[site]) == dict:
         self.ro_mirrors.append(self.cfg[site]['ReadOnly'])
 
+    # For all the mirrors, do the unit test:
+    for mirror in self.ro_mirrors:
+      lfn =  '/lhcb/test/lfc-replication/%s/testFile.%s' % (mirror,time.time())
+      if not self.register_dummy(lfn):
+        print "Error: "+lfn+" is already in the master or can't be registered \
+there...check your voms role is prodution \n"
+        continue
+
+      # Try to open a session
+      if lfc.lfc_startsess(mirror, "DIRAC_test"): # rc != 0 means error
+        continue
+
+      # Measure time to create replica and write XML file
+      time_to_create = self.time_to_create_rep(lfn)
+      fd = open(self.path + mirror + ".timing", "w")
+      try:
+        fd.write("%s" % time_to_create)
+      finally:
+        fd.close()
+
+      # Measure time to find a replica
+      if time_to_create == self.timeout:
+        time_to_find = self.timeout
+      else:
+        time_to_find = self.time_to_find_rep(lfn)
+
+      lfc.lfc_endsess()
+
+      # Measure time to delete a replica
+      removed = self.remove_replica(lfn)
+      if removed:
+        # Try to open a session
+        if lfc.lfc_startsess(mirror, "DIRAC_test"): # rc != 0 means error
+          continue
+        time_to_remove = self.time_to_remove_rep(lfn)
+        lfc.lfc_endsess()
+        gLogger.always('%s %s %s %s' % (mirror, time_to_create, time_to_find, time_to_remove))
+
+  @staticmethod
+  def pfn_of_token(SE):
+    cfg = CS.getTypedDictRootedAt(
+      root="",
+      relpath="/Resources/StorageElements/" + SE + "/AccessProtocol.1")
+    return "srm://" + cfg['Host'] + cfg['Path']
+
+  def register_dummy(self, lfn, size=0, SE="CERN-USER", guid=makeGuid(), chksum=""):
+    pfn = self.pfn_of_token(SE) + lfn
+    res = self.master_lfc.addFile((lfn, pfn, size, SE, guid, chksum))
+    return res['OK'] and res['Value']['Successful'].has_key(lfn)
+
+  def get_replica(self, lfn):
+    reps = {}
+    rc, replica_objs = lfc.lfc_getreplica("/grid" + lfn, "", "")
+    if rc:
+      print lfc.sstrerror(lfc.cvar.serrno)
+    else:
+      for r in replica_objs:
+        SE = r.host
+        pfn = r.sfn.strip()
+      reps[SE] = pfn
+    return reps
+
+  def remove_replica(self, lfn, SE="CERN-USER"):
+    pfn = self.pfn_of_token(SE) + lfn
+    res = self.master_lfc.removeReplica((lfn, pfn, SE))
+    return res['OK'] and res['Value']['Successful'].has_key(lfn)
+
+  def remove_file(self, lfn):
+    res = self.master_lfc.removeFile(lfn)
+    return res['OK'] and res['Value']['Successful'].has_key(lfn)
+
+  def time_to_find_rep(self, lfn):
+    start_time = time.time()
+    while True:
+      reps = self.get_replica(lfn)
+      if reps.has_key('CERN-USER'):
+        return time.time() - start_time
+      else:
+        if (time.time() - start_time < self.timeout) : time.sleep(0.1)
+        else                                         : return self.timeout
+
+  def time_to_create_rep(self, lfn):
+    start_time = time.time()
+    while True:
+      if lfc.lfc_access("/grid" + lfn, 0) == 0:
+        return time.time() - start_time
+      else:
+        if (time.time() - start_time < self.timeout) : time.sleep(0.1)
+        else                                         : return self.timeout
+
+  def time_to_remove_rep(self, lfn):
+    start_time = time.time()
+    while not lfc.lfc_access("/grid" + lfn, 0): # rc = 0 if accessible
+        if (time.time() - start_time < self.timeout) : time.sleep(0.1)
+        else                                         : return self.timeout
+    return time.time() - start_time
+
 class SLSAgent(AgentModule):
 
   def execute(self):
