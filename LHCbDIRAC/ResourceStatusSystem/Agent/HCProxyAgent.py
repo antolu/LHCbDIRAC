967bcb030687d85cd83156529d8e0dab0179f4a0
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2011-08-04 06:59:47 +0000

    Improved docs
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@41888 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Agent/HCProxyAgent.py
+++ LHCbDIRAC/ResourceStatusSystem/Agent/HCProxyAgent.py
@@ -1,26 +1,46 @@
-########################################################################
-# $HeadURL:  $
-########################################################################
+"""
+LHCbDIRAC/ResourceStatusSystem/Agent/HCProxyAgent.py
+"""
 
-from DIRAC import gLogger, S_OK, S_ERROR
-from DIRAC.Core.Base.AgentModule import AgentModule
-from DIRAC.Interfaces.API.DiracAdmin import DiracAdmin
+__RCSID__ = "$Id:$"
 
-from DIRAC.ResourceStatusSystem.Utilities.Utils import where
-
+# First, pythonic stuff
 import os
 
-__RCSID__ = "$Id: $"
+# Second, DIRAC stuff
+from DIRAC                                      import gLogger, S_OK, S_ERROR
+from DIRAC.Core.Base.AgentModule                import AgentModule
+from DIRAC.ResourceStatusSystem.Utilities.Utils import where
+
+# Third, LHCbDIRAC stuff
+# ...
 
 AGENT_NAME = 'ResourceStatus/HCProxyAgent'
 
-class HCProxyAgent(AgentModule):
+class HCProxyAgent( AgentModule ):
+  '''
+  Class HCProxyAgent. This agent is in charge of getting a fresh proxy from
+  DIRAC. In this case, it sets the proxy as the one of the HammerCloudManager. 
+  See CS/Operations/Shift/HammerCloudManager
   
-#############################################################################
+  This is used by the HammerCloud framework, to get a new proxy every day,
+  without having to use any hardcoded password ( which means, this agent will
+  run on the same machine as HammerCloud ).
+  
+  The agent overwrites the parent methods:
+
+  - initialize
+  - execute
+  '''
+  
+################################################################################
 
   def initialize(self):
-    """ Standard constructor
-    """
+    ''' 
+    Method executed when the agent is launched.
+    It sets the proxy as the one of the HammerCloudManager, which is stored under
+      /opt/dirac/pro/work/ResourceStatus/HCProxyAgent
+    '''
     
     try:
 
@@ -29,14 +49,19 @@ class HCProxyAgent(AgentModule):
 
     except Exception:
       errorStr = "HCAgent initialization"
-      gLogger.exception(errorStr)
-      return S_ERROR(errorStr)
+      gLogger.exception( errorStr )
+      return S_ERROR( errorStr )
 
-#############################################################################
+################################################################################
 
   def execute(self):
     """
-      The main HCAgent execution method.
+    At every execution this method will try to print the environment variable
+    X509_USER_PROXY, which should contain the path to the proxy. If is empty,
+    something went wrong.
+    
+    This agent should get a new proxy whenever it is close to expire. 
+    Appart from that, which is done internally, it is a pretty dummy loop. 
     """
     
     try:
@@ -51,4 +76,6 @@ class HCProxyAgent(AgentModule):
       return S_ERROR(errorStr)
   
   
-#############################################################################
+################################################################################
+
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
