74d584564bba3edc9f20362e4a968a9ef13dba16
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2011-08-04 13:37:18 +0000

    Improved docs
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@41894 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Agent/__init__.py
+++ LHCbDIRAC/ResourceStatusSystem/Agent/__init__.py
@@ -1,10 +1,5 @@
-############################################################
-# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk/LHCbDIRAC/LHCbSystem/Service/__init__.py $
-############################################################
-
 """
-   LHCbDIRAC.ResourceStatusSystem.Agent package
-
+LHCbDIRAC/ResourceStatusSystem/Agent/__init__.py
 """
 
-__RCSID__ = "$Id: __init__.py 18161 2009-11-11 12:07:09Z acasajus $"
+__RCSID__ = "$Id:$"
