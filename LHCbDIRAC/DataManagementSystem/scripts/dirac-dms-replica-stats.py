dd27d64ba8e6d3f6059dbc809e0bd52654d1b91c
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2011-09-07 13:53:37 +0000

    CHANGE: Use new switches classes
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@42387 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-replica-stats.py
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-replica-stats.py
@@ -18,88 +18,80 @@ def orderSEs( listSEs ):
   orderedSEs += [se for se in listSEs if not se.endswith( "-ARCHIVE" )]
   return orderedSEs
 
-fileType = ''
-directories = []
-prods = []
-getSize = False
-bkQuery = ''
-runs = []
-Script.registerSwitch( "D:", "Directory=", "   Dir to search [ALL]" )
-Script.registerSwitch( "S:", "Size", "   Get the LFN size [No]" )
-Script.registerSwitch( "f:", "FileType=", "   File type to search [ALL]" )
-Script.registerSwitch( "p:", "Production=", "   Production ID to search [ALL] (space or comma separated list)" )
-Script.registerSwitch( "", "BKQuery=", "   Bookkeeping query" )
-Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
-                                     'Usage:',
-                                     '  %s [option|cfgfile] ...' % Script.scriptName, ] ) )
-Script.addDefaultOptionValue( 'LogLevel', 'error' )
-Script.parseCommandLine( ignoreErrors = False )
-
-for switch in Script.getUnprocessedSwitches():
-  if switch[0] == "D" or switch[0].lower() == "directory":
-    directories = switch[1].split( ',' )
-  if switch[0].lower() == "t" or switch[0].lower() == "filetype":
-    fileType = switch[1]
-  if switch[0].lower() == "p" or switch[0].lower() == "production":
-    prods = switch[1].split( ',' )
-  if switch[0].lower() == "bkquery":
-    bkQuery = switch[1]
-  if switch[0].lower() in ["s", "size"]:
-    getSize = True
-
-
 import DIRAC
-from DIRAC.Core.Utilities.List                        import sortList, breakListIntoChunks
-from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
-from DIRAC.Core.DISET.RPCClient import RPCClient
-from DIRAC.Core.Utilities.SiteSEMapping                                import getSitesForSE, getSEsForSite
-from LHCbDIRAC.DataManagementSystem.Client.StorageUsageClient  import StorageUsageClient
-from LHCbDIRAC.TransformationSystem.Client.Utilities   import buildBKQuery, getDirsForBKQuery
+from DIRAC.Core.Base import Script
+from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
 
-rm = ReplicaManager()
+if __name__ == "__main__":
 
-pr = []
-for p in prods:
-  if p.find( ":" ) > 0:
-    p = p.split( ":" )
-    for i in range( int( p[0] ), int( p[1] ) + 1 ):
-      pr.append( str( i ) )
+  dmScript = DMScript()
+  dmScript.registerBKSwitches()
+  dmScript.registerNamespaceSwitches()
+
+  Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                       'Usage:',
+                                       '  %s [option|cfgfile] [<LFN>] [<LFN>...]' % Script.scriptName, ] ) )
+
+  Script.registerSwitch( "S:", "Size", "   Get the LFN size [No]" )
+  Script.addDefaultOptionValue( 'LogLevel', 'error' )
+  Script.parseCommandLine( ignoreErrors = False )
+
+  getSize = False
+  for switch in Script.getUnprocessedSwitches():
+    if switch[0] in ( "S", "Size" ):
+      getSize = True
+
+  directories = dmScript.options.get( 'Directory' )
+
+
+  import DIRAC
+  from DIRAC.Core.Utilities.List                        import sortList, breakListIntoChunks
+  from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
+  from DIRAC.Core.DISET.RPCClient import RPCClient
+  from DIRAC.Core.Utilities.SiteSEMapping                                import getSitesForSE, getSEsForSite
+  from LHCbDIRAC.DataManagementSystem.Client.StorageUsageClient  import StorageUsageClient
+
+  rm = ReplicaManager()
+
+  lfnReplicas = {}
+  if directories:
+    for directory in directories:
+      res = rm.getReplicasFromDirectory( directory )
+      if not res['OK']:
+        print res['Message']
+        continue
+      lfnReplicas.update( res['Value'] )
   else:
-    pr.append( p )
-prods = pr
+    res = rm.getReplicas( dmScript.getLFNsForBKQuery( visible = True ) )
+    if not res['OK']:
+      print res['Message']
+      DIRAC.exit( 2 )
+    lfnReplicas = res['Value']['Successful']
 
-if not directories:
-  ( transBKQuery, reqID ) = buildBKQuery( bkQuery, prods, fileType, runs )
-  print transBKQuery
-  directories = getDirsForBKQuery( transBKQuery )
+  if not lfnReplicas:
+    print "No files found...."
+    DIRAC.exit( 2 )
 
-if len( directories ) == 0:
-  print "No directories to get statistics for"
-  DIRAC.exit( 2 )
-
-repStats = {}
-repSEs = {}
-repSites = {}
-maxRep = 0
-maxArch = 0
-nfiles = 0
-totSize = 0
-for directory in directories:
-  res = rm.getReplicasFromDirectory( directory )
-  if not res['OK']:
-    print res['Message']
-    continue
+  repStats = {}
+  repSEs = {}
+  repSites = {}
+  maxRep = 0
+  maxArch = 0
+  nfiles = 0
+  totSize = 0
   if getSize:
-    sys.stdout.write( "... getting size for %d LFNs, be patient...\r" % len( res['Value'] ) )
-    sys.stdout.flush()
     lfnSize = {}
-    for lfns in breakListIntoChunks( res['Value'].keys(), 100 ):
+    left = len( lfnReplicas )
+    for lfns in breakListIntoChunks( lfnReplicas.keys(), 500 ):
+      left -= len( lfns )
+      sys.stdout.write( "... getting size for %d LFNs (%d left), be patient...    \r" % ( len( lfns ), left ) )
+      sys.stdout.flush()
       r = rm.getCatalogFileSize( lfns )
       if r['OK']:
         lfnSize.update( r['Value']['Successful'] )
     for lfn, size in lfnSize.items():
       totSize += size
-  for lfn, replicas in res['Value'].items():
+  for lfn, replicas in lfnReplicas.items():
     SEs = replicas.keys()
     nrep = len( replicas )
     narchive = -1
@@ -131,45 +123,47 @@ for directory in directories:
     if - narchive > maxArch: maxArch = -narchive
     nfiles += 1
 
-GB = 1000. * 1000. * 1000.
-TB = GB * 1000.
-if totSize:
-  print "%d files found (%.3f GB) in" % ( nfiles, totSize / GB ), directories
-else:
-  print "%d files found in" % ( nfiles ), directories
-print "\nReplica statistics:"
-if repStats.has_key( -100 ):
-  print "Failover replicas:", repStats[-100], "files"
-if maxArch:
-  for nrep in range( 1, maxArch + 1 ):
-    if not repStats.has_key( -nrep ):
-      repStats[-nrep] = 0
-    print nrep - 1, "archives:", repStats[-nrep], "files"
-for nrep in range( maxRep + 1 ):
-  if not repStats.has_key( nrep ):
-    repStats[nrep] = 0
-  print nrep, "replicas:", repStats[nrep], "files"
+  GB = 1000. * 1000. * 1000.
+  TB = GB * 1000.
+  if totSize:
+    print "%d files found (%.3f GB) in" % ( nfiles, totSize / GB ), directories
+  else:
+    print "%d files found in" % ( nfiles ), directories
+  print "\nReplica statistics:"
+  if repStats.has_key( -100 ):
+    print "Failover replicas:", repStats[-100], "files"
+  if maxArch:
+    for nrep in range( 1, maxArch + 1 ):
+      if not repStats.has_key( -nrep ):
+        repStats[-nrep] = 0
+      print nrep - 1, "archives:", repStats[-nrep], "files"
+  for nrep in range( maxRep + 1 ):
+    if not repStats.has_key( nrep ):
+      repStats[nrep] = 0
+    print nrep, "replicas:", repStats[nrep], "files"
 
-print "\nSE statistics:"
-for se in orderSEs( repSEs.keys() ):
-  if se.endswith( "-FAILOVER" ): continue
-  if not se.endswith( "-ARCHIVE" ):
-    res = getSitesForSE( se, gridName = 'LCG' )
-    if res['OK']:
-      try:
-        site = res['Value'][0]
-      except:
-        continue
-      if not repSites.has_key( site ):
-        repSites[site] = [0, 0]
-      repSites[site][0] += repSEs[se][0]
-      repSites[site][1] += repSEs[se][1]
-  string = "%16s: %s files" % ( se, repSEs[se][0] )
-  if getSize: string += " - %.3f TB" % ( repSEs[se][1] / TB )
-  print string
+  print "\nSE statistics:"
+  for se in orderSEs( repSEs.keys() ):
+    if se.endswith( "-FAILOVER" ): continue
+    if not se.endswith( "-ARCHIVE" ):
+      res = getSitesForSE( se, gridName = 'LCG' )
+      if res['OK']:
+        try:
+          site = res['Value'][0]
+        except:
+          continue
+        if not repSites.has_key( site ):
+          repSites[site] = [0, 0]
+        repSites[site][0] += repSEs[se][0]
+        repSites[site][1] += repSEs[se][1]
+    string = "%16s: %s files" % ( se, repSEs[se][0] )
+    if getSize: string += " - %.3f TB" % ( repSEs[se][1] / TB )
+    print string
 
-print "\nSites statistics:"
-for site in sortList( repSites.keys() ):
-  string = "%16s: %d files" % ( site, repSites[site][0] )
-  if getSize: string += " - %.3f TB" % ( repSites[site][1] / TB )
-  print string
+  print "\nSites statistics:"
+  for site in sortList( repSites.keys() ):
+    string = "%16s: %d files" % ( site, repSites[site][0] )
+    if getSize: string += " - %.3f TB" % ( repSites[site][1] / TB )
+    print string
+
+
