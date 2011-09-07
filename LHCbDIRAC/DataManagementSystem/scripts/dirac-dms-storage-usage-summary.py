aaf44dbe28663d4da2abbfc984697db68de8943a
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2011-09-07 13:54:00 +0000

    CHANGE: Use new switches classes
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@42388 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-storage-usage-summary.py
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-storage-usage-summary.py
@@ -7,126 +7,150 @@
 """
 __RCSID__ = "$Id$"
 
-from DIRAC.Core.Base import Script
-unit = 'TB'
-dir = ''
-fileType = ''
-prods = []
-sites = []
-ses = []
-lcg = False
-full = False
-Script.registerSwitch( "u:", "Unit=", "   Unit to use [%s] (MB,GB,TB,PB)" % unit )
-Script.registerSwitch( "D:", "Dir=", "   Dir to search [ALL]" )
-Script.registerSwitch( "t:", "Type=", "   File type to search [ALL]" )
-Script.registerSwitch( "p:", "Prod=", "   Production ID to search [ALL] (space or comma separated list)" )
-Script.registerSwitch( "g:", "Sites=", "  Sites to consider [ALL] (space or comma separated list)" )
-Script.registerSwitch( "S:", "SEs=", "  SEs to consider [ALL] (space or comma separated list)" )
-Script.registerSwitch( "l", "LCG", "  Group results by tape and disk" )
-Script.registerSwitch( "f", "Full", "  Output the directories matching selection" )
-
-Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
-                                     'Usage:',
-                                     '  %s [option|cfgfile] ...' % Script.scriptName, ] ) )
-
-
-Script.parseCommandLine( ignoreErrors = False )
-
 import DIRAC
-from DIRAC import gConfig
-from DIRAC.Core.DISET.RPCClient import RPCClient
-from DIRAC.Core.Utilities.List import sortList
-import re
+from DIRAC.Core.Base import Script
+from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
 
-for switch in Script.getUnprocessedSwitches():
-  if switch[0].lower() == "u" or switch[0].lower() == "unit":
-    unit = switch[1]
-  if switch[0] == "D" or switch[0].lower() == "dir":
-    dir = switch[1]
-  if switch[0].lower() == "t" or switch[0].lower() == "type":
-    fileType = switch[1]
-  if switch[0].lower() == "p" or switch[0].lower() == "prod":
-    prods = switch[1].replace( ',', ' ' ).split()
-  if switch[0].lower() == "g" or switch[0].lower() == "sites":
-    sites = switch[1].replace( ',', ' ' ).split()
-  if switch[0] == "S" or switch[0].lower() == "ses":
-    ses = switch[1].replace( ',', ' ' ).split()
-  if switch[0].lower() == "l" or switch[0].lower() == "lcg":
-    lcg = True
-  if switch[0].lower() == "f" or switch[0].lower() == "full":
-    full = True
+def orderSEs( listSEs ):
+  listSEs = sortList( listSEs )
+  orderedSEs = [se for se in listSEs if se.endswith( "-ARCHIVE" )]
+  orderedSEs += [se for se in listSEs if not se.endswith( "-ARCHIVE" )]
+  return orderedSEs
 
-if not type( ses ) == type( [] ):
-  Script.showHelp()
-if not type( sites ) == type( [] ):
-  Script.showHelp()
-for site in sites:
-  res = gConfig.getOptionsDict( '/Resources/Sites/LCG/%s' % site )
-  if not res['OK']:
-    print 'Site %s not known' % site
-    Script.showHelp()
-  ses.extend( res['Value']['SE'].replace( ' ', '' ).split( ',' ) )
-scaleDict = { 'MB' : 1000 * 1000.0,
-              'GB' : 1000 * 1000 * 1000.0,
-              'TB' : 1000 * 1000 * 1000 * 1000.0,
-              'PB' : 1000 * 1000 * 1000 * 1000 * 1000.0}
-if not unit in scaleDict.keys():
-  Script.showHelp()
-scaleFactor = scaleDict[unit]
+if __name__ == "__main__":
 
-rpc = RPCClient( 'DataManagement/StorageUsage' )
+  dmScript = DMScript()
+  dmScript.registerBKSwitches()
+  dmScript.registerNamespaceSwitches()
+  dmScript.registerSiteSwitches()
 
-if not prods:
-  prods = ['']
+  unit = 'TB'
+  dirs = ['']
+  fileTypes = None
+  prods = []
+  sites = []
+  ses = []
+  lcg = False
+  full = False
+  Script.registerSwitch( "u:", "Unit=", "   Unit to use [%s] (MB,GB,TB,PB)" % unit )
+  Script.registerSwitch( "l", "LCG", "  Group results by tape and disk" )
+  Script.registerSwitch( '', "Full", "  Output the directories matching selection" )
 
-if full:
-  for prodID in sortList( prods ):
-    res = rpc.getStorageDirectoryData( dir, fileType, prodID, ses )
+  Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                       'Usage:',
+                                       '  %s [option|cfgfile] ...' % Script.scriptName, ] ) )
+
+
+  Script.parseCommandLine( ignoreErrors = False )
+
+  from DIRAC import gConfig
+  from DIRAC.Core.DISET.RPCClient import RPCClient
+  from DIRAC.Core.Utilities.List import sortList
+
+  for switch in Script.getUnprocessedSwitches():
+    if switch[0].lower() == "u" or switch[0].lower() == "unit":
+      unit = switch[1]
+    if switch[0].lower() == "l" or switch[0].lower() == "lcg":
+      lcg = True
+    if switch[0].lower() == "full":
+      full = True
+
+  sites = dmScript.getOption( 'Sites', [] )
+  ses = dmScript.getOption( 'SEs', [] )
+  prods = dmScript.getOption( 'Productions', [] )
+  dirs = dmScript.getOption( 'Directory', [''] )
+  bkQuery = dmScript.buildBKQuery()
+  if bkQuery and not prods:
+    prods = dmScript.getProductionsFromBKQuery( visible = False )
+    if not prods:
+      print 'No productions found for bkQuery %s' % str( bkQuery )
+      DIRAC.exit( 0 )
+    prods.sort()
+    print "Looking for %d productions:" % len( prods ), prods
+  fileTypes = dmScript.getOption( 'FileType' )
+  if bkQuery and not fileTypes:
+    fileTypes = bkQuery.get( 'FileType', None )
+    if type( fileTypes ) == type( '' ):
+      fileTypes = [fileTypes]
+    print 'FileTypes:', fileTypes
+  if not fileTypes:
+    fileTypes = ['']
+  for site in sites:
+    res = gConfig.getOptionsDict( '/Resources/Sites/LCG/%s' % site )
     if not res['OK']:
-      print 'Failed to get directories', res['Message']
-      DIRAC.exit( 2 )
-    dirData = res[ 'Value' ]
+      print 'Site %s not known' % site
+      Script.showHelp()
+    ses.extend( res['Value']['SE'].replace( ' ', '' ).split( ',' ) )
+  scaleDict = { 'MB' : 1000 * 1000.0,
+                'GB' : 1000 * 1000 * 1000.0,
+                'TB' : 1000 * 1000 * 1000 * 1000.0,
+                'PB' : 1000 * 1000 * 1000 * 1000 * 1000.0}
+  if not unit in scaleDict.keys():
+    Script.showHelp()
+  scaleFactor = scaleDict[unit]
+
+  rpc = RPCClient( 'DataManagement/StorageUsage' )
+  if not prods:
+    prods = ['']
+
+  if full:
+    dirData = {}
+    for prodID in sortList( prods ):
+      for fileType in fileTypes:
+        for dir in dirs:
+          res = rpc.getStorageDirectoryData( dir, fileType, prodID, ses )
+          if not res['OK']:
+            print 'Failed to get directories', res['Message']
+            DIRAC.exit( 2 )
+            dirData.update( res[ 'Value' ] )
     for resDir in sorted( dirData ):
       print resDir, " -> ", dirData[ resDir ]
 
-totalUsage = {}
-for prodID in prods:
-  res = rpc.getStorageSummary( dir, fileType, prodID, ses )
-  if res['OK']:
-    for se in sortList( res['Value'].keys() ):
-      if not totalUsage.has_key( se ):
-        totalUsage[se] = {'Files':0, 'Size':0}
-      totalUsage[se]['Files'] += res['Value'][se]['Files']
-      totalUsage[se]['Size'] += res['Value'][se]['Size']
+  totalUsage = {}
+  grandTotal = {'Files':0, 'Size':0}
+  for prodID in prods:
+    for fileType in fileTypes:
+      for dir in dirs:
+        res = rpc.getStorageSummary( dir, fileType, prodID, ses )
+        if res['OK']:
+          for se in res['Value']:
+            if se not in totalUsage:
+              totalUsage[se] = {'Files':0, 'Size':0}
+            totalUsage[se]['Files'] += res['Value'][se]['Files']
+            totalUsage[se]['Size'] += res['Value'][se]['Size']
+            if not se.endswith( '-ARCHIVE' ):
+              grandTotal['Files'] += res['Value'][se]['Files']
+              grandTotal['Size'] += res['Value'][se]['Size']
 
-if lcg:
-  from DIRAC.Resources.Storage.StorageElement                    import StorageElement
-  tapeTotalFiles = 0
-  diskTotalFiles = 0
-  tapeTotalSize = 0
-  diskTotalSize = 0
-  for se in sortList( totalUsage.keys() ):
-    storageElement = StorageElement( se )
-    files = totalUsage[se]['Files']
-    size = totalUsage[se]['Size']
-    seStatus = storageElement.getStatus()['Value']
-    if seStatus['TapeSE']:
-      tapeTotalFiles += files
-      tapeTotalSize += size
-    if seStatus['DiskSE']:
-      diskTotalFiles += files
-      diskTotalSize += size
-  print '%s %s %s' % ( 'Storage Type'.ljust( 20 ), ( 'Size (%s)' % unit ).ljust( 20 ), 'Files'.ljust( 20 ) )
+  if lcg:
+    from DIRAC.Resources.Storage.StorageElement                    import StorageElement
+    tapeTotalFiles = 0
+    diskTotalFiles = 0
+    tapeTotalSize = 0
+    diskTotalSize = 0
+    for se in sortList( totalUsage.keys() ):
+      storageElement = StorageElement( se )
+      files = totalUsage[se]['Files']
+      size = totalUsage[se]['Size']
+      seStatus = storageElement.getStatus()['Value']
+      if seStatus['TapeSE']:
+        tapeTotalFiles += files
+        tapeTotalSize += size
+      if seStatus['DiskSE']:
+        diskTotalFiles += files
+        diskTotalSize += size
+    print '%s %s %s' % ( 'Storage Type'.ljust( 20 ), ( 'Size (%s)' % unit ).ljust( 20 ), 'Files'.ljust( 20 ) )
+    print '-' * 50
+    print "%s %s %s" % ( 'T1D*'.ljust( 20 ), ( '%.1f' % ( tapeTotalSize / scaleFactor ) ).ljust( 20 ), str( tapeTotalFiles ).ljust( 20 ) )
+    print "%s %s %s" % ( 'T*D1'.ljust( 20 ), ( '%.1f' % ( diskTotalSize / scaleFactor ) ).ljust( 20 ), str( diskTotalFiles ).ljust( 20 ) )
+    DIRAC.exit( 0 )
+
+  print '%s %s %s' % ( 'DIRAC SE'.ljust( 20 ), ( 'Size (%s)' % unit ).ljust( 20 ), 'Files'.ljust( 20 ) )
   print '-' * 50
-  print "%s %s %s" % ( 'T1D*'.ljust( 20 ), ( '%.1f' % ( tapeTotalSize / scaleFactor ) ).ljust( 20 ), str( tapeTotalFiles ).ljust( 20 ) )
-  print "%s %s %s" % ( 'T*D1'.ljust( 20 ), ( '%.1f' % ( diskTotalSize / scaleFactor ) ).ljust( 20 ), str( diskTotalFiles ).ljust( 20 ) )
+  for se in orderSEs( totalUsage.keys() ):
+    dict = totalUsage[se]
+    files = dict['Files']
+    size = dict['Size']
+    print "%s %s %s" % ( se.ljust( 20 ), ( '%.1f' % ( size / scaleFactor ) ).ljust( 20 ), str( files ).ljust( 20 ) )
+  print "\n%s %s %s" % ( 'Total (disk)'.ljust( 20 ), ( '%.1f' % ( grandTotal['Size'] / scaleFactor ) ).ljust( 20 ), str( grandTotal['Files'] ).ljust( 20 ) )
   DIRAC.exit( 0 )
-
-print '%s %s %s' % ( 'DIRAC SE'.ljust( 20 ), ( 'Size (%s)' % unit ).ljust( 20 ), 'Files'.ljust( 20 ) )
-print '-' * 50
-for se in sortList( totalUsage.keys() ):
-  dict = totalUsage[se]
-  files = dict['Files']
-  size = dict['Size']
-  print "%s %s %s" % ( se.ljust( 20 ), ( '%.1f' % ( size / scaleFactor ) ).ljust( 20 ), str( files ).ljust( 20 ) )
-DIRAC.exit( 0 )
