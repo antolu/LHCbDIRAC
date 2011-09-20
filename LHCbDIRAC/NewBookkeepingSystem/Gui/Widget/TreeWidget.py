829439c35a462cf2aca85bae7e81bfe55c2cfbe8
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2011-09-20 14:44:51 +0000

    temporary fix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@42836 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/NewBookkeepingSystem/Gui/Widget/TreeWidget.py
+++ LHCbDIRAC/NewBookkeepingSystem/Gui/Widget/TreeWidget.py
@@ -55,7 +55,7 @@ class TreeWidget(QWidget, Ui_TreeWidget):
     self.__controler.addChild('InfoDialog', self.__infodialog.getControler())
 
     self.__fileDialog = FileDialog(self)
-    self.__fileDialog.setWindowFlags(Qt.Window)
+    #self.__fileDialog.setWindowFlags(Qt.Window)
     self.__controler.addChild('FileDialog',self.__fileDialog.getControler())
     self.Bookmarks.setupControler(self)
     self.__controler.addChild('Bookmarks', self.Bookmarks.getControler())
