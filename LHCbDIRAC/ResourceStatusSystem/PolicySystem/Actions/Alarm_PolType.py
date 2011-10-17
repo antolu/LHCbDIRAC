82f7cc8955a295a9789b2c82cc7e738e28b50e63
vibernar <vibernar@4525493e-7705-40b1-a816-d608a930855b>
2011-10-17 16:09:00 +0000

    Alarm_PolType inherit from DIRAC, __init__.py added
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@43787 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/PolicySystem/Actions/Alarm_PolType.py
+++ LHCbDIRAC/ResourceStatusSystem/PolicySystem/Actions/Alarm_PolType.py
@@ -2,11 +2,9 @@
 AlarmPolType Actions
 """
 import urllib
-
-from DIRAC.ResourceStatusSystem.Utilities import CS
-from DIRAC.ResourceStatusSystem.Utilities import Utils
-
+from DIRAC.ResourceStatusSystem.PolicySystem.Actions import Alarm_PolType
 from DIRAC.ResourceStatusSystem.DB.ResourceManagementDB import ResourceManagementDB
+
 rmDB = ResourceManagementDB()
 
 def getUsersToNotifyShiftDB():
@@ -19,71 +17,6 @@ def getUsersToNotifyShiftDB():
 
   return  [ rmDB.registryGetLoginFromName(e['Morning']) for e in lines ]
 
-def getUsersToNotify(setup, kwargs):
-  """Get a list of users to notify (helper function for AlarmPolTypeActions)
-  Optional keyword arguments:
-  - Granularity
-  - SiteType
-  - ServiceType
-  - ResourceType
-  """
-
-  notifications = []
-  groups = CS.getTypedDictRootedAt("AssigneeGroups/" + setup)
-
-  for k in groups:
-    if Utils.dictMatch(kwargs, groups[k]):
-      notifications.append({'Users':groups[k]['Users'],
-                            'Notifications':groups[k]['Notifications']})
-
-  # Adding people to notify from shiftdb
-  notifications += getUsersToNotifyShiftDB()
-
-  return notifications
-
-def AlarmPolTypeActions(name, res, nc, setup, rsDB, **kwargs):
-  """ Do actions required to notify users.
-  Mandatory keyword arguments:
-  - Granularity
-  Optional keyword arguments:
-  - SiteType
-  - ServiceType
-  - ResourceType
-  """
-  # raise alarms, right now makes a simple notification
-
-  if 'Granularity' not in kwargs.keys():
-    raise ValueError, "You have to provide a argument Granularity=<desired_granularity>"
-
-  granularity = kwargs['Granularity']
-
-  if res['Action']:
-
-    notif = "%s %s is perceived as" % (granularity, name)
-    notif = notif + " %s. Reason: %s." %(res['Status'], res['Reason'])
-
-    NOTIF_D = getUsersToNotify(setup, kwargs)
-
-    for notification in NOTIF_D:
-      for user in notification['Users']:
-        if 'Web' in notification['Notifications']:
-          nc.addNotificationForUser(user, notif)
-        if 'Mail' in notification['Notifications']:
-          was = rsDB.getMonitoredsHistory(granularity,
-                                          ['Status', 'Reason', 'DateEffective'],
-                                          name, False, 'DESC', 1)[0]
-
-          mailMessage = """Granularity = %s
-Name = %s
-New perceived status = %s
-Reason for status change = %s
-Was in status "%s", with reason "%s", since %s
-Setup = %s
-""" % granularity, name, res['Status'], res['Reason'], was[0], was[1], was[2], setup
-
-          nc.sendMail(rmDB.registryGetMailFromLogin(user),
-                      '%s: %s' % (name, res['Status']), mailMessage)
-
-
-if __name__ == "__main__":
-  print getUsersToNotifyShiftDB()
+class AlarmPolType(Alarm_PolType.AlarmPolType):
+  def getUsersToNotify(self):
+    return Alarm_PolType.AlarmPolType.getUsersToNotify(self) + getUsersToNotifyShiftDB()
