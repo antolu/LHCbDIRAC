ceef9ac5a5e63585a4f54a2165d8e7ea43d2b496
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2011-10-17 16:27:21 +0000

    NEW: Data processing progress script
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@43799 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/ProductionManagementSystem/scripts/dirac-production-stats.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+########################################################################
+# $HeadURL:  $
+########################################################################
+
+"""
+   Get statistics on productions related to a given processing pass
+"""
+
+from LHCbDIRAC.ProductionManagementSystem.Client.ProcessingProgress import ProcessingProgress, HTMLProgressTable
+
+if __name__ == "__main__":
+  import DIRAC
+  from DIRAC.Core.Base import Script
+  from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
+
+  dmScript = DMScript( useBKQuery = True )
+  dmScript.registerBKSwitches()
+  Script.registerSwitch( '', 'Conditions=', '   comma separated list of DataTakingConditions' )
+  Script.registerSwitch( '', 'HTML=', '   <file> : Output in html format to <file>' )
+  Script.registerSwitch( '', 'NoPrint', '   No printout' )
+
+  Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                       'Usage:',
+                                       '  %s [option|cfgfile] [<LFN>] [<LFN>...]' % Script.scriptName, ] ) )
+
+  Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                       'Usage:',
+                                       '  %s [option|cfgfile] ...' % Script.scriptName, ] ) )
+  Script.parseCommandLine( ignoreErrors = False )
+
+  switches = Script.getUnprocessedSwitches()
+  outputHTML = None
+  printResult = True
+  daqConditions = None
+  processingPass = None
+  for opt, val in switches:
+    if opt == "Conditions":
+      daqConditions = val.split( ',' )
+    elif opt == "ProcessingPass":
+      processingPass = val
+    elif opt == "HTML":
+      outputHTML = val
+    elif opt == "NoPrint":
+      printResult = False
+
+  bkQuery = dmScript.getBKQuery( visible = False )
+  if not bkQuery.getQueryDict():
+    Script.showHelp()
+    DIRAC.exit( 2 )
+  if daqConditions:
+      bkQueries = []
+      for cond in daqConditions:
+        bkQuery = BKQuery( bkQuery.setConditions( cond ) )
+        bkQueries.append( bkQuery )
+  else:
+    bkQueries = [bkQuery]
+
+  statCollector = ProcessingProgress()
+  printOutput = ''
+  htmlOutput = ''
+  summaryProdStats = []
+  if outputHTML:
+    htmlTable = HTMLProgressTable( bkQuery['ProcessingPass'] )
+  for bkQuery in bkQueries:
+    prodStats = statCollector.getFullStats( bkQuery.getQueryDict(), printResult = printResult )
+    summaryProdStats.append( prodStats )
+
+    if printResult:
+      printOutput += statCollector.outputResults( bkQuery.getConditions(), bkQuery.getProcessingPass(), prodStats )
+
+    if outputHTML:
+      htmlTable.writeHTML( bkQuery.getConditions(), prodStats )
+
+  if printResult:
+    print printOutput
+  if outputHTML:
+    htmlSummary = ''
+    if len( summaryProdStats ) > 1:
+      htmlTable.writeHTMLSummary( summaryProdStats )
+    try:
+      f = open( outputHTML, 'w' )
+      f.write( "<head>\n<title>Progress of %s</title>\n</title>\n" % bkQueries[0]['ProcessingPass'] )
+      f.write( str( htmlTable.getTable() ) )
+      f.close()
+      print "Successfully wrote HTML file", outputHTML
+    except:
+      print "Failed to write HTML file", outputHTML
