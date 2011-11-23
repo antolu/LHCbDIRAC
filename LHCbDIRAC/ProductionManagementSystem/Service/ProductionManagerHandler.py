567e4f1381aa6589714068bccfcca4f498bc55d1
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2011-11-23 18:37:36 +0000

    # FIX: compatibility with v6r0: RequestHandler._clientTransport -> getRemoteCredentials()
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@45272 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Service/ProductionManagerHandler.py
+++ LHCbDIRAC/ProductionManagementSystem/Service/ProductionManagerHandler.py
@@ -8,16 +8,16 @@ from LHCbDIRAC.ProductionManagementSystem.DB.ProductionDB                      i
 from DIRAC.Core.Workflow.Workflow                                              import *
 from types import *
 
-database=False
-def initializeProductionManagerHandler(serviceInfo):
+database = False
+def initializeProductionManagerHandler( serviceInfo ):
   global database
   database = ProductionDB()
   return S_OK()
 
-class ProductionManagerHandler(RequestHandler):
+class ProductionManagerHandler( RequestHandler ):
 
   types_publishWorkflow = [ StringType ]
-  def export_publishWorkflow( self, body, update=False):
+  def export_publishWorkflow( self, body, update = False ):
     """ Publish new workflow in the repository taking WFname from the workflow itself
     """
     errKey = "Publishing workflow failed:"
@@ -25,54 +25,55 @@ class ProductionManagerHandler(RequestHandler):
     parent = "Unknown"
     description = "empty description"
     descr_long = "empty long description"
-    authorDN = self._clientTransport.peerCredentials['DN']
-    authorGroup = self._clientTransport.peerCredentials['group']
+    credDict = self.getRemoteCredentials()
+    authorDN = credDict[ 'DN' ]
+    authorGroup = credDict['group']
     try:
-      wf = fromXMLString(body)
+      wf = fromXMLString( body )
       name = wf.getName()
       parent = wf.getType()
       description = wf.getDescrShort()
       descr_long = wf.getDescription()
-      result = database.publishWorkflow(name, parent, description, descr_long, body, authorDN, authorGroup, update)
+      result = database.publishWorkflow( name, parent, description, descr_long, body, authorDN, authorGroup, update )
       if not result['OK']:
-        errExpl = " name=%s because %s" % (name, result['Message'])
-        gLogger.error(errKey, errExpl)
+        errExpl = " name=%s because %s" % ( name, result['Message'] )
+        gLogger.error( errKey, errExpl )
       else:
         if update:
-          gLogger.verbose('Workflow %s is modified in the Production Repository by the %s'%(name, authorDN) )
+          gLogger.verbose( 'Workflow %s is modified in the Production Repository by the %s' % ( name, authorDN ) )
         else:
-          gLogger.verbose('Workflow %s is added to the Production Repository by the %s'%(name, authorDN) )
+          gLogger.verbose( 'Workflow %s is added to the Production Repository by the %s' % ( name, authorDN ) )
       return result
 
-    except Exception,x:
-      errExpl = " name=%s because %s" % (name, str(x))
-      gLogger.exception(errKey, errExpl)
-      return S_ERROR(errKey + str(x))
+    except Exception, x:
+      errExpl = " name=%s because %s" % ( name, str( x ) )
+      gLogger.exception( errKey, errExpl )
+      return S_ERROR( errKey + str( x ) )
 
   types_getWorkflow = [ StringType ]
-  def export_getWorkflow(self, name):
-    res = database.getWorkflow(name)
-    return self._parseRes(res)
+  def export_getWorkflow( self, name ):
+    res = database.getWorkflow( name )
+    return self._parseRes( res )
 
   types_getWorkflowFullDescription = [ StringType ]
-  def export_getWorkflowFullDescription(self,name):
-    res = database.getWorkflow(name)
+  def export_getWorkflowFullDescription( self, name ):
+    res = database.getWorkflow( name )
     if not res['OK']:
       return res
-    wf = fromXMLString(res['Value'])
-    return S_OK(wf.getDescription())
+    wf = fromXMLString( res['Value'] )
+    return S_OK( wf.getDescription() )
 
   types_deleteWorkflow = [ StringType ]
   def export_deleteWorkflow( self, name ):
-    res = database.deleteWorkflow(name)
-    return self._parseRes(res)
+    res = database.deleteWorkflow( name )
+    return self._parseRes( res )
 
   types_getListWorkflows = [ ]
-  def export_getListWorkflows(self):
+  def export_getListWorkflows( self ):
     res = database.getListWorkflows()
-    return self._parseRes(res)
+    return self._parseRes( res )
 
   types_getWorkflowInfo = [ StringType ]
   def export_getWorkflowInfo( self, name ):
-    res = database.getWorkflowInfo(name)
-    return self._parseRes(res)
+    res = database.getWorkflowInfo( name )
+    return self._parseRes( res )
