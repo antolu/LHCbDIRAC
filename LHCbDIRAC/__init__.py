df88c61840bbb5da8308212951057bd8bbe101f1
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2011-12-02 10:32:57 +0000

    Release v7r0 based on Dirac v6r0 and LHCbWebDirac v1r11p2
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@45638 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -6,7 +6,7 @@ __path__ = extend_path( __path__, __name__ )
 majorVersion = 7
 minorVersion = 0
 patchLevel = 0
-preVersion = 8
+preVersion = 0
 
 version = "v%sr%s" % ( majorVersion, minorVersion )
 buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
