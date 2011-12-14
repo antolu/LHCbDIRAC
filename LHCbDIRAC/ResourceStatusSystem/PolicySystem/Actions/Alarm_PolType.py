95adc1b3e08eef527ef6f26ec06696cccca23ff5
vibernar <vibernar@4525493e-7705-40b1-a816-d608a930855b>
2011-12-14 09:41:31 +0000

    Alarm_PolType reuses RSS Clients, fix #24919
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@45995 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/PolicySystem/Actions/Alarm_PolType.py
+++ LHCbDIRAC/ResourceStatusSystem/PolicySystem/Actions/Alarm_PolType.py
@@ -3,23 +3,20 @@ AlarmPolType Actions
 """
 import urllib
 from DIRAC.ResourceStatusSystem.PolicySystem.Actions import Alarm_PolType
-from DIRAC.ResourceStatusSystem.Client.ResourceManagementClient import ResourceManagementClient
-from DIRAC.ResourceStatusSystem.Utilities import Utils
-
-rmClient = ResourceManagementClient()
-
-def get_shiftdb_users():
-  url = urllib.urlopen("http://lbshiftdb.cern.ch/shiftdb_report.php")
-  lines = [l.split('|')[1:-1] for l in  url.readlines()]
-  lines = [ {'Date': e[0].strip(), 'Function': e[1].strip(), 'Phone':e[2].strip(), 'Morning':e[3].strip(),
-             'Evening':e[4].strip(), 'Night':e[5].strip() } for e in lines[1:]]
-
-  lines = [ e for e in lines if e['Function'] == "Grid Expert" or e['Function'] == "Production" ]
-  lines = [ (e["Morning"], e["Evening"], e["Night"]) for e in lines ]
-  lines = [ e for e in Utils.list_flatten(lines) if e]
-  lines = [ Utils.unpack(rmClient.getUserRegistryCache(name = e))[0][0] for e in lines ]
-  return [ {'Users': lines, 'Notifications': ["Mail"]} ] # Only mail notification since others are not working
+from DIRAC.ResourceStatusSystem.Utilities            import Utils
 
 class AlarmPolType(Alarm_PolType.AlarmPolType):
+  def get_shiftdb_users(self):
+    url = urllib.urlopen("http://lbshiftdb.cern.ch/shiftdb_report.php")
+    lines = [l.split('|')[1:-1] for l in  url.readlines()]
+    lines = [ {'Date': e[0].strip(), 'Function': e[1].strip(), 'Phone':e[2].strip(), 'Morning':e[3].strip(),
+               'Evening':e[4].strip(), 'Night':e[5].strip() } for e in lines[1:]]
+
+    lines = [ e for e in lines if e['Function'] == "Grid Expert" or e['Function'] == "Production" ]
+    lines = [ (e["Morning"], e["Evening"], e["Night"]) for e in lines ]
+    lines = [ e for e in Utils.list_flatten(lines) if e]
+    lines = [ Utils.unpack(self.rmAPI.getUserRegistryCache(name = e))[0][0] for e in lines ]
+    return [ {'Users': lines, 'Notifications': ["Mail"]} ] # Only mail notification since others are not working
+
   def _getUsersToNotify(self):
-    return Alarm_PolType.AlarmPolType._getUsersToNotify(self) + get_shiftdb_users()
+    return Alarm_PolType.AlarmPolType._getUsersToNotify(self) + self.get_shiftdb_users()
