53c2ad836d16f61078046d85a5f129e41e00bb06
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-01-16 16:24:42 +0000

    rmClient instead of rmAPI
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@46386 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Agent/SLSAgent.py
+++ LHCbDIRAC/ResourceStatusSystem/Agent/SLSAgent.py
@@ -9,6 +9,8 @@ from DIRAC.Core.Base.DB import DB
 from DIRAC.ResourceStatusSystem.Utilities                   import CS, Utils
 from DIRAC.ResourceStatusSystem.Utilities.Utils             import xml_append
 
+from DIRAC.Core.Utilities.File import makeGuid
+
 from LHCbDIRAC.Core.Utilities                               import ProductionEnvironment
 
 import xml.dom, xml.sax
@@ -660,6 +662,38 @@ LoadDDDB(Node = '/dd/Structure/LHCb')
     finally:
       xmlfile.close()
 
+################################################################################
+
+class LFCMirrorTest(TestBase):
+  def __init__(self, am):
+    super(LFCMirrorTest, self).__init__(am)
+
+    # Get ConDB infos
+#    self.CDB_infos = CS.getTypedDictRootedAt(root="", relpath="/Resources/CondDB")
+    self.xmlPath   = rootPath + "/" + self.getAgentValue("webRoot") + self.getTestValue("dir")
+    
+  def registerFile(self):
+
+    file={}
+    file['GUID'] = makeGuid()
+    file['PFN']  = 'srm://srm-lhcb.cern.ch/castor/cern.ch/grid%s' % lfn
+    file['Size'] = 0
+    file[''] = 'CERN-USER' 
+    checksum = ''
+    file['Checksum']=''
+    fileTuple = (lfn,pfn,size,se,guid,checksum)
+    res = masterLFC.addFile(fileTuple)
+#  res = masterLFC.addFile(file)
+    if not res['OK']:
+      print res
+    return False
+    if res['Value']['Successful'].has_key(lfn):
+      return True
+    return False
+  
+
+################################################################################
+
 class SLSAgent(AgentModule):
   def initialize(self):
     self.am_setOption( 'shifterProxy', 'DataManager' )
@@ -671,3 +705,6 @@ class SLSAgent(AgentModule):
     LOGSETest(self)
     CondDBTest(self)
     return S_OK()
+
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
