de6299fee243c6310a0e1faca139f29949f10c20
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2012-01-17 08:44:29 +0000

    fix, comments
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@46390 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Core/scripts/lhcb-proxy-init.py
+++ LHCbDIRAC/Core/scripts/lhcb-proxy-init.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+"""
+  Use dirac-proxy-init to get a proxy
+"""
+
+
 __RCSID__ = "$Id: "
 
-#!/usr/bin/env python
 import os, sys
 out = os.system( "dirac-proxy-init -o LogLevel=ALWAYS -t '%s'" % "' '".join( sys.argv[1:] ) )
 sys.exit( out / 256 )
