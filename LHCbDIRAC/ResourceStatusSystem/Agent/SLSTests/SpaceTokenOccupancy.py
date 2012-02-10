f9ead2449e5a4c75226280a31c672d2cfb1d5093
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-02-10 13:28:45 +0000

    XML generation for first test
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@47121 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Agent/SLSTests/SpaceTokenOccupancy.py
+++ LHCbDIRAC/ResourceStatusSystem/Agent/SLSTests/SpaceTokenOccupancy.py
@@ -7,7 +7,7 @@ from DIRAC                                                  import gLogger
 from DIRAC.ResourceStatusSystem.Utilities                   import CS
 from LHCbDIRAC.ResourceStatusSystem.Agent.SLSTests.TestBase import TestBase
 
-import lcg_util
+import lcg_util, time
 
 class TestModule( TestBase ):
   
@@ -28,33 +28,63 @@ class TestModule( TestBase ):
         
         except Exception, e:
           gLogger.exception( 'StorageTokenOccupancy for %s %s.\n %s' % ( site, spaceToken, e ) )     
-            
-#        print site, spaceToken
-
 
   def __launchTest( self, site, spaceToken ):
 
     total, guaranteed, free, availability = 0, 0, 0, 0
 
-    url          = self.SEs[ site ][ 'Endpoint' ]
-    validity     = 'PT0M'   
+    url              = self.SEs[ site ][ 'Endpoint' ]
+    validityduration = 'PT0M'   
 
     # get space tokens associated to a space token description, and their metadata
     answer = lcg_util.lcg_stmd( spaceToken, url, True, 0 )
 
-    print output
+    print answer
 
     if answer[ 0 ] == 0:
-      output       = answer[1][0]
-      total        = float( output[ 'totalsize' ] ) / 1e12 # Bytes to Terabytes
-      guaranteed   = float( output[ 'guaranteedsize' ] ) / 1e12
-      free         = float( output[ 'unusedsize' ] ) / 1e12
-      availability = 100 if free > 4 else ( free * 100 / total if total != 0 else 0 )
+      output           = answer[1][0]
+      total            = float( output[ 'totalsize' ] ) / 1e12 # Bytes to Terabytes
+      guaranteed       = float( output[ 'guaranteedsize' ] ) / 1e12
+      free             = float( output[ 'unusedsize' ] ) / 1e12
+      availability     = 100 if free > 4 else ( free * 100 / total if total != 0 else 0 )
       #validity     = self.getTestOption( "validity" )
-      validity     = self.testConfig[ 'validity' ]
+      validityduration = self.testConfig[ 'validityduration' ]
     else:
-      gLogger.info("StorageSpace: problew with lcg_util:\ lcg_util.lcg_stmd('%s', '%s', True, 0) = (%d, %s)" % (st, url, answer[0], answer[1]))
+      gLogger.info("StorageSpace: problem with lcg_util:\ lcg_util.lcg_stmd('%s', '%s', True, 0) = (%d, %s)" % (spaceToken, url, answer[0], answer[1]))
       gLogger.info( str( answer ) )
+
+    ## XML generation
+
+    xmlReport = []
+    xmlReport.append( { 'tag' : 'id', 'nodes' : '%s_%s' % ( site, st ) } )
+    xmlReport.append( { 'tag' : 'availability', 'nodes' : availability } )
+    
+    thresholdNodes = []
+    for t,v in self.testConfig[ 'thresholds' ].items():
+      thresholdNodes.append( { 'tag' : 'threshold', 'attr' : [ ( 'level', t ) ], 'nodes' : v } )
+    
+    xmlReport.append( { 'tag' : 'availabilitythresholds', 'nodes' : thresholdNodes } )
+    
+    xmlReport.append( { 'tag' : 'availabilityinfo' , 'nodes' : 'Free=%s Total=%s' % ( free, total ) } )
+    xmlReport.append( { 'tag' : 'availabilitydesc' , 'nodes' : self.testConfig[ 'availabilitydesc' ] } )
+    xmlReport.append( { 'tag' : 'refreshperiod'    , 'nodes' : self.testConfig[ 'refreshperiod' ] } )
+    xmlReport.append( { 'tag' : 'validityduration' , 'nodes' : validityduration } )
+
+    grpNodes = []
+    grpNodes.append( { 'tag' : 'numericvalue', 'attrs' : [ ( 'name', 'Consumed' ) ], 'nodes' : total - free } ) 
+    grpNodes.append( { 'tag' : 'numericvalue', 'attrs' : [ ( 'name', 'Capacity' ) ], 'nodes' : total } )
+    grp  = { 'tag' : 'grp', 'attrs' : [ ( 'name', 'Space occupancy' ) ], 'nodes' : grpNodes }
+    
+    data = [ grp ]
+    data.append( { 'tag' : 'numericvalue', 'attrs' : [ ( 'name', 'Free space' ) ], 'nodes' : free } )
+    data.append( { 'tag' : 'numericvalue', 'attrs' : [ ( 'name', 'Occupied space' ) ], 'nodes' : total - free } )
+    data.append( { 'tag' : 'numericvalue', 'attrs' : [ ( 'name', 'Total space' ) ], 'nodes' : total } )
+    
+    xmlReport.append( data )
+    
+    xmlReport.append( { 'tag' : 'timestamp', 'nodes' : time.strftime( "%Y-%m-%dT%H:%M:%S" ) })
+    
+    self.writeXml( xmlList = xmlReport )
 
 #    doc = gen_xml_stub()
 #    xml_append(doc, "id", site + "_" + st)
