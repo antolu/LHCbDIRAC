1e4ec8b31f539c5a42c0665b888fa32778dc141a
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-02-21 12:15:11 +0000

    Added LFCMaster test to SLS
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@47638 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/ResourceStatusSystem/Agent/SLSTests/LFCMaster.py
@@ -0,0 +1,73 @@
+################################################################################
+# $HeadURL:  $
+################################################################################
+__RCSID__  = "$Id:  $"
+
+from DIRAC import S_OK
+
+import os, time
+import lfc2
+
+def getProbeElements():
+  
+  # Hardcoded, to be fixed
+  master = 'lfc-lhcb.cern.ch'
+  
+  return S_OK( [ master ] )
+  
+
+def setupProbes( testConfig ):
+  
+  path = '%s/%s' % ( testConfig[ 'workdir' ], testConfig[ 'testName' ] )
+  
+  try:
+    os.makedirs( path )
+  except OSError:
+    pass # The dir exist already, or cannot be created: do nothin
+  
+  return S_OK()
+  
+
+def runProbe( probeInfo, testConfig ):
+  
+  master                   = probeInfo[ 0 ]
+  os.environ[ 'LFC_HOST' ] = master
+    
+  lfnDir  = '/lhcb/test/lfc_mirror_test/streams_propagation_test'
+  gridDir = '/grid' + lfnDir
+
+#  try:
+#    lfc2.lfc_rmdir( gridDir )
+#  except:
+#    pass
+    
+  _create, _remove = False, False  
+    
+  try:
+      
+    lfc2.lfc_mkdir( gridDir , 0777 )
+    _create  = True
+    gLogger.info( 'created %s' % gridDir )
+    lfc2.lfc_rmdir( gridDir )
+    _remove  = True
+    gLogger.info( 'removed %s' % gridDir )
+      
+  except ValueError:
+    _lfcMsg = 'Error manipulating directory, are you sure it does not exist ?'
+    gLogger.error( _lfcMsg )
+  except Exception, e:
+    gLogger.error( e )
+
+  availability = ( ( _create and _remove ) and 100 ) or 0
+  
+  xmlList = []
+  xmlList.append( { 'tag' : 'id', 'nodes' : 'LHCb_LFC_Master_%s' % master } )
+  xmlList.append( { 'tag' : 'availability', 'nodes' : availability } )
+  xmlList.append( { 'tag' : 'notes', 'nodes' : 'Either 0 or 100, 0 no basic operations performed, 100 all working.' } )
+  xmlList.append( { 'tag' : 'validityduration' , 'nodes' : 'PT2H' } )
+  xmlList.append( { 'tag' : 'timestamp', 'nodes' : time.strftime( "%Y-%m-%dT%H:%M:%S" ) }) 
+
+  return { 'xmlList' : xmlList, 'config' : testConfig, 'filename' : 'LHCb_LFC_Master_%s' % master }  
+
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
