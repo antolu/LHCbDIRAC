db9d07b288d75d85635e7c6972bead4e41496635
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-03-13 07:32:30 +0000

    FIX: remove catch Exception, e
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@48535 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Command/NagiosProbes_Command.py
+++ LHCbDIRAC/ResourceStatusSystem/Command/NagiosProbes_Command.py
@@ -21,39 +21,39 @@ class NagiosProbes_Command( Command ):
     
     '''
     super( NagiosProbes_Command, self ).doCommand()
-    self.APIs = initAPIs( self.__APIs__, self.APIs, force = True )  
+    self._APIs = initAPIs( self.__APIs__, self._APIs, force = True )  
     
-    try:
-    
+#    try:
+   
       #granularity = self.args[0]
-      name    = self.args[1]
-      flavour = self.args[2]
+    name    = self.args[1]
+    flavour = self.args[2]
     
-      #ServiceURI is a quite misleading name.. it is a Resource in the RSS DB in fact.
-      query = { 
-              'serviceURI'     : name, 
-              'serviceFlavour' : flavour,
-              'meta'           :
-                {
-                  'columns' : ['MetricStatus','SummaryData'],
-                  'count'   : True,
-                  'group'   : 'MetricStatus' 
-                 }
-            }
+    #ServiceURI is a quite misleading name.. it is a Resource in the RSS DB in fact.
+    query = { 
+            'serviceURI'     : name, 
+            'serviceFlavour' : flavour,
+            'meta'           :
+              {
+                'columns' : ['MetricStatus','SummaryData'],
+                'count'   : True,
+                'group'   : 'MetricStatus' 
+               }
+          }
     
-      res = self.APIs[ 'ResourceManagementClient'].getMonitoringTest( **query )
+    res = self._APIs[ 'ResourceManagementClient'].getMonitoringTest( **query )
       
-      if not res['OK']:
-      #  msg = "Error getting NagiosProbes for serviceURI '%s'\n %s" % ( name, res['Message'])
-      #  gLogger.error( msg )
-        return res
+    if not res['OK']:
+    #  msg = "Error getting NagiosProbes for serviceURI '%s'\n %s" % ( name, res['Message'])
+    #  gLogger.error( msg )
+      return { 'Result' : res }
     
-      res = S_OK( dict( [ ( r[0], r[1:] ) for r in res[ 'Value' ] ] ) )
+    res = S_OK( dict( [ ( r[0], r[1:] ) for r in res[ 'Value' ] ] ) )
 
-    except Exception, e:
-      _msg = '%s (%s): %s' % ( self.__class__.__name__, self.args, e )
-      gLogger.exception( _msg )
-      return { 'Result' : S_ERROR( _msg ) }
+#    except Exception, e:
+#      _msg = '%s (%s): %s' % ( self.__class__.__name__, self.args, e )
+#      gLogger.exception( _msg )
+#      return { 'Result' : S_ERROR( _msg ) }
 
     return { 'Result' : res }    
 
