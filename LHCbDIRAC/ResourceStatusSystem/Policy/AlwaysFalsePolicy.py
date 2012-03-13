8de7cff91241d0de9954619895b4cf9ed49376e9
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-03-13 12:13:49 +0000

    FIX: renamed AlwaysFalse_Policy to AlwaysFalsePolicy
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@48574 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/ResourceStatusSystem/Policy/AlwaysFalsePolicy.py
@@ -0,0 +1,30 @@
+# $HeadURL$
+''' AlwaysFalsePolicy
+
+  Class is a policy class that... checks nothing!
+  
+'''
+
+from DIRAC.ResourceStatusSystem.PolicySystem.PolicyBase import PolicyBase
+
+__RCSID__ = '$Id$'
+
+class AlwaysFalsePolicy( PolicyBase ):
+
+  def evaluate(self):
+    """
+    Does nothing.
+
+    Always returns:
+        {
+          'Status':'Active'
+          'Reason':None
+        }
+    """
+
+    return { 'Status' : 'Active', 'Reason' : 'This is the AlwasyFalse policy' }
+
+  evaluate.__doc__ = PolicyBase.evaluate.__doc__ + evaluate.__doc__
+  
+################################################################################  
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
--- LHCbDIRAC/ResourceStatusSystem/Policy/test/Test_RSS_Policy_AlwaysFalsePolicy.py
+++ LHCbDIRAC/ResourceStatusSystem/Policy/test/Test_RSS_Policy_AlwaysFalsePolicy.py
@@ -24,11 +24,11 @@ class AlwaysFalsePolicy_TestCase( unittest.TestCase ):
   def setUp( self ):
     
     # We need the proper software, and then we overwrite it.
-    import LHCbDIRAC.ResourceStatusSystem.Policy.AlwaysFalse_Policy as moduleTested
+    import LHCbDIRAC.ResourceStatusSystem.Policy.AlwaysFalsePolicy as moduleTested
     moduleTested.PolicyBase = PolicyBase   
-    moduleTested.AlwaysFalse_Policy.__bases__ = ( PolicyBase, ) 
+    moduleTested.AlwaysFalsePolicy.__bases__ = ( PolicyBase, ) 
 
-    self.policy = moduleTested.AlwaysFalse_Policy
+    self.policy = moduleTested.AlwaysFalsePolicy
 
   def tearDown( self ):
     
@@ -40,7 +40,7 @@ class AlwaysFalsePolicy_Success( AlwaysFalsePolicy_TestCase ):
     ''' tests that we can instantiate one object of the tested class
     '''  
     p = self.policy()
-    self.assertEqual( 'AlwaysFalse_Policy', p.__class__.__name__ )
+    self.assertEqual( 'AlwaysFalsePolicy', p.__class__.__name__ )
   
   def test_evaluate_none( self ):
     ''' tests that we can evaluate the policy
