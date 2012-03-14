8bda9d0e726763f5799032099e3e0e195e02406e
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-03-14 12:36:03 +0000

    FIX: removed except Exception e
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@48664 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Command/SAMResults_Command.py
+++ LHCbDIRAC/ResourceStatusSystem/Command/SAMResults_Command.py
@@ -8,12 +8,12 @@
 import httplib
 import urllib2
 
-from DIRAC                                           import gLogger, S_OK, S_ERROR
-from DIRAC.Core.Utilities.SitesDIRACGOCDBmapping     import getGOCSiteName
+from DIRAC                                        import gLogger, S_OK, S_ERROR
+from DIRAC.Core.Utilities.SitesDIRACGOCDBmapping  import getGOCSiteName
 
-from DIRAC.ResourceStatusSystem.Command.Command      import *
-from DIRAC.ResourceStatusSystem.Command.knownAPIs    import initAPIs
-from DIRAC.ResourceStatusSystem.Utilities.Utils      import where 
+from DIRAC.ResourceStatusSystem.Command.Command   import *
+from DIRAC.ResourceStatusSystem.Command.knownAPIs import initAPIs
+from DIRAC.ResourceStatusSystem.Utilities.Utils   import where 
 
 __RCSID__ = '$Id: $'
 
@@ -21,8 +21,8 @@ class SAMResults_Command( Command ):
   
   __APIs__ = [ 'SAMResultsClient', 'ResourceStatusClient' ]
   
-  def doCommand(self, rsClientIn=None):
-    """ 
+  def doCommand( self, rsClientIn = None ):
+    ''' 
     Return getStatus from SAM Results Client  
     
     :attr:`args`: 
@@ -33,52 +33,52 @@ class SAMResults_Command( Command ):
      - args[2]: string: optional - should be the (DIRAC) site name of the ValidRes
      
      - args[3]: list: list of tests
-    """
+    '''
 
-    super(SAMResults_Command, self).doCommand()
+    super( SAMResults_Command, self ).doCommand()
     self.APIs = initAPIs( self.__APIs__, self.APIs )
 
-    try:
-      granularity = self.args[0]
-      name        = self.args[1]
+#    try:
+    granularity = self.args[ 0 ]
+    name        = self.args[ 1 ]
       
-      if len( self.args ) > 2:  
-        siteName = self.args[2]
-      else:
-        siteName = None
+    if len( self.args ) > 2:  
+      siteName = self.args[ 2 ]
+    else:
+      siteName = None
 
-      if granularity == 'Site':
-        siteName = getGOCSiteName(name)
+    if granularity == 'Site':
+      siteName = getGOCSiteName( name )
+      if not siteName[ 'OK' ]:
+        return siteName
+      siteName = siteName['Value']
+        
+    elif granularity == 'Resource':
+      if siteName is None:
+        siteName = self.APIs[ 'ResourceStatusClient' ].getGridSiteName( granularity, name )
+        if not siteName['OK']:
+          return siteName    
+        siteName = siteName[ 'Value' ]
+      else:
+        siteName = getGOCSiteName(siteName)
         if not siteName['OK']:
           return siteName
         siteName = siteName['Value']
-        
-      elif granularity == 'Resource':
-        if siteName is None:
-          siteName = self.APIs[ 'ResourceStatusClient' ].getGridSiteName(granularity, name)
-          if not siteName['OK']:
-            return siteName    
-          siteName = siteName[ 'Value' ]
-        else:
-          siteName = getGOCSiteName(siteName)
-          if not siteName['OK']:
-            return siteName
-          siteName = siteName['Value']
-      else:
-        return S_ERROR( '%s is not a valid granularity' % self.args[ 0 ] ) 
+    else:
+      return S_ERROR( '%s is not a valid granularity' % self.args[ 0 ] ) 
     
-      if len( self.args ) > 3:
-        tests = self.args[ 3 ]
-      else:
-        tests = None
+    if len( self.args ) > 3:
+      tests = self.args[ 3 ]
+    else:
+      tests = None
 
-      res = self.APIs[ 'SAMResultsClient' ].getStatus(granularity, name, siteName, tests, 
-                                    timeout = self.timeout) 
+    res = self.APIs[ 'SAMResultsClient' ].getStatus( granularity, name, siteName, 
+                                                     tests, timeout = self.timeout ) 
   
-    except Exception, e:
-      _msg = '%s (%s): %s' % ( self.__class__.__name__, self.args, e )
-      gLogger.exception( _msg )
-      return { 'Result' : S_ERROR( _msg ) }
+#    except Exception, e:
+#      _msg = '%s (%s): %s' % ( self.__class__.__name__, self.args, e )
+#      gLogger.exception( _msg )
+#      return { 'Result' : S_ERROR( _msg ) }
 
     return { 'Result' : res }
   
--- LHCbDIRAC/ResourceStatusSystem/Command/SLS_Command.py
+++ LHCbDIRAC/ResourceStatusSystem/Command/SLS_Command.py
@@ -7,15 +7,18 @@
 
 import re
 
-from DIRAC                                           import gLogger, S_OK, S_ERROR
-from DIRAC.Core.LCG                                  import SLSClient
-from DIRAC.ResourceStatusSystem.Command.Command      import Command
-from DIRAC.ResourceStatusSystem.Utilities            import CS
+from DIRAC                                      import gLogger, S_OK, S_ERROR
+from DIRAC.Core.LCG                             import SLSClient
+from DIRAC.ResourceStatusSystem.Command.Command import Command
+from DIRAC.ResourceStatusSystem.Utilities       import CS
 
 __RCSID__ = '$Id: $'
 
-def slsid_of_service(granularity, name, type_ = None):
-  '''Return the SLS id of various services.'''
+def slsid_of_service( granularity, name, type_ = None ):
+  '''
+    Return the SLS id of various services.
+  '''
+  
   if type_ == 'CondDB': 
     return name.split( '@' )[ 1 ] + '_CondDB'
   if type_ == 'VO-BOX': 
@@ -32,101 +35,113 @@ def slsid_of_service(granularity, name, type_ = None):
   else: 
     return ''
 
-class SLSStatus_Command(Command):
-  def doCommand(self):
-    """
+class SLSStatus_Command( Command ):
+  '''
+    SLSStatus_Command
+  '''
+  
+  def doCommand( self ):
+    '''
     Return getAvailabilityStatus from SLS Client
 
     :attr:`args`:
      - args[0]: string: should be a granularity
      - args[1]: string: should be the (DIRAC) name of the granularity
      - args[2]: string: should be the granularity type (e.g. 'VO-BOX')
-    """
+    '''
 
-    super(SLSStatus_Command, self).doCommand()
+    super( SLSStatus_Command, self ).doCommand()
 
-    try:
+#    try:
       
-      res = SLSClient.getAvailabilityStatus(slsid_of_service(*self.args))
+    res = SLSClient.getAvailabilityStatus( slsid_of_service( *self.args ) )
 
-      if res['OK']:
-        res = S_OK( res['Value']['Availability'] )
+    if res[ 'OK' ]:
+      res = S_OK( res[ 'Value' ][ 'Availability' ] )
         
         #gLogger.error("No SLS sensors for " + self.args[0] + " " + self.args[1] )
         #return  { 'Result': None }
       #return { 'Result': res['Value']['Availability'] }
 
-    except Exception, e:
-      _msg = '%s (%s): %s' % ( self.__class__.__name__, self.args, e )
-      gLogger.exception( _msg )
-      return { 'Result' : S_ERROR( _msg ) }
+#    except Exception, e:
+#      _msg = '%s (%s): %s' % ( self.__class__.__name__, self.args, e )
+#      gLogger.exception( _msg )
+#      return { 'Result' : S_ERROR( _msg ) }
 
     return { 'Result' : res }    
 
   doCommand.__doc__ = Command.doCommand.__doc__ + doCommand.__doc__
 
-class SLSLink_Command(Command):
-  def doCommand(self):
-    """
+class SLSLink_Command( Command ):
+  '''
+    SLSLink_Command
+  '''
+  
+  def doCommand( self ):
+    '''
     Return getStatus from SLS Client
 
     :attr:`args`:
      - args[0]: string: should be a granularity
      - args[1]: string: should be the (DIRAC) name of the granularity
      - args[2]: string: should be the granularity type (e.g. 'VO-BOX')
-    """
+    '''
 
-    super(SLSLink_Command, self).doCommand()
+    super( SLSLink_Command, self ).doCommand()
 
-    try:
+#    try:
 
-      res = SLSClient.getAvailabilityStatus(slsid_of_service(*self.args))
+    res = SLSClient.getAvailabilityStatus( slsid_of_service( *self.args ) )
 
-      if res['OK']:
-        res = S_OK( res['Value']['Weblink'] )
+    if res[ 'OK' ]:
+      res = S_OK( res[ 'Value' ][ 'Weblink' ] )
       #gLogger.error("No SLS sensors for " + self.args[0] + " " + self.args[1] )
       #return  { 'Result': None }
     #return { 'Result': res['Value']['Weblink'] }
 
-    except Exception, e:
-      _msg = '%s (%s): %s' % ( self.__class__.__name__, self.args, e )
-      gLogger.exception( _msg )
-      return { 'Result' : S_ERROR( _msg ) }
+#    except Exception, e:
+#      _msg = '%s (%s): %s' % ( self.__class__.__name__, self.args, e )
+#      gLogger.exception( _msg )
+#      return { 'Result' : S_ERROR( _msg ) }
 
     return { 'Result' : res }    
 
   doCommand.__doc__ = Command.doCommand.__doc__ + doCommand.__doc__
 
-class SLSServiceInfo_Command(Command):
-  def doCommand(self):
-    """
+class SLSServiceInfo_Command( Command ):
+  '''
+    SLSServiceInfo_Command
+  '''
+
+  def doCommand( self ):
+    '''
     Return getServiceInfo from SLS Client
 
     :attr:`args`:
      - args[0]: string: should be a ValidRes
      - args[1]: string: should be the (DIRAC) name of the ValidRes
-    """
+    '''
 
-    super(SLSServiceInfo_Command, self).doCommand()
+    super( SLSServiceInfo_Command, self ).doCommand()
     
-    try:
+#    try:
     
-      if self.args[0] == 'StorageElement':
-        SLSName = slsid_of_service(self.args[0], self.args[1], "CASTOR")
-      else:
-        SLSName = slsid_of_service(*self.args)
+    if self.args[ 0 ] == 'StorageElement':
+      SLSName = slsid_of_service( self.args[ 0 ], self.args[ 1 ], 'CASTOR' )
+    else:
+      SLSName = slsid_of_service( *self.args )
 
-      res = SLSClient.getServiceInfo( SLSName )
+    res = SLSClient.getServiceInfo( SLSName )
       #if not res[ 'OK' ]:
       #  gLogger.error("No SLS sensors for " + self.args[0] + " " + self.args[1] )
       #  return { 'Result' : None }
       #else:
       #  return { 'Result' : res["Value"] }
 
-    except Exception, e:
-      _msg = '%s (%s): %s' % ( self.__class__.__name__, self.args, e )
-      gLogger.exception( _msg )
-      return { 'Result' : S_ERROR( _msg ) }
+#    except Exception, e:
+#      _msg = '%s (%s): %s' % ( self.__class__.__name__, self.args, e )
+#      gLogger.exception( _msg )
+#      return { 'Result' : S_ERROR( _msg ) }
 
     return { 'Result' : res }    
 
