4bb6b4532c8594fc474fecfb24b9715260c47250
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2012-03-19 08:50:33 +0000

    maintaining gaudi_extra_options if needed
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@48993 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Workflow/Modules/GaudiApplication.py
+++ LHCbDIRAC/Workflow/Modules/GaudiApplication.py
@@ -192,7 +192,10 @@ class GaudiApplication( ModuleBase ):
       gaudiRunFlags = gConfig.getValue( '/Operations/GaudiExecution/%s/gaudirunFlags' % ( setup ), 'gaudirun.py' )
 #      command = '%s %s %s' % ( gaudiRunFlags, self.optfile, generatedOpts )
       if self.optionsLine:
-        command = '%s %s %s' % ( gaudiRunFlags, self.optfile, prodConfFile, self.optionsLine )
+        fopen = open( 'gaudi_extra_options.py', 'w' )
+        fopen.write( self.optionsLine )
+        fopen.close()
+        command = '%s %s %s' % ( gaudiRunFlags, self.optfile, prodConfFile, 'gaudi_extra_options.py' )
       else:
         command = '%s %s %s' % ( gaudiRunFlags, self.optfile, prodConfFile )
       print 'Command = %s' % ( command )  #Really print here as this is useful to see
