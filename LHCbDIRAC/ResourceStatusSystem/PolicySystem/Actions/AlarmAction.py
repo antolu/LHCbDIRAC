cc0530446d1783d405b1dbbd6f49af70c48573bd
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-03-22 09:58:23 +0000

    BUGFIX: disabled temporarily sending alarms to shiftdb users. There is a paralell development on this topic.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@49278 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/PolicySystem/Actions/AlarmAction.py
+++ LHCbDIRAC/ResourceStatusSystem/PolicySystem/Actions/AlarmAction.py
@@ -3,7 +3,7 @@
 
 '''
 
-import urllib
+import urllib2
 
 from DIRAC.ResourceStatusSystem.PolicySystem.Actions.AlarmAction import AlarmAction as BaseAlarmAction
 from DIRAC.ResourceStatusSystem.Utilities import Utils
@@ -14,7 +14,11 @@ class AlarmAction( BaseAlarmAction ):
   '''
   
   def get_shiftdb_users( self ):
-    url = urllib.urlopen("http://lbshiftdb.cern.ch/shiftdb_report.php")
+    try: 
+      url = urllib2.urlopen("http://lbshiftdb.cern.ch/shiftdb_report.php", timeout = 60)
+    except urllib2.URLError:
+      print 'Error opening: http://lbshiftdb.cern.ch/shiftdb_report.php'
+      return []   
     lines = [l.split('|')[1:-1] for l in  url.readlines()]
     lines = [ {'Date': e[0].strip(), 'Function': e[1].strip(), 'Phone':e[2].strip(), 'Morning':e[3].strip(),
                'Evening':e[4].strip(), 'Night':e[5].strip() } for e in lines[1:]]
@@ -25,8 +29,9 @@ class AlarmAction( BaseAlarmAction ):
     lines = [ Utils.unpack(self.rmClient.getUserRegistryCache(name = e))[0][0] for e in lines ]
     return [ {'Users': lines, 'Notifications': ["Mail"]} ] # Only mail notification since others are not working
 
-  def _getUsersToNotify( self ):
-    return BaseAlarmAction._getUsersToNotify( self ) + self.get_shiftdb_users()
+# Disabled temporarily
+#  def _getUsersToNotify( self ):
+#    return BaseAlarmAction._getUsersToNotify( self ) + self.get_shiftdb_users()
 
 ################################################################################
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
