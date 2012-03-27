eef29bfa7fb4674d062d32af13e78408749e8c7a
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-03-27 12:57:35 +0000

    FIX: pyLint changes ( docstring )
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@49617 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Utilities/Utils.py
+++ LHCbDIRAC/ResourceStatusSystem/Utilities/Utils.py
@@ -1,7 +1,18 @@
+# $HeadURL: $
+''' Utils
+
+  This is a workaround to reuse the ResourceManagementClient on the SLSXML module.
+    
+'''
+
 from LHCbDIRAC.ResourceStatusSystem.Utilities                       import SLSXML
 from LHCbDIRAC.ResourceStatusSystem.Client.ResourceManagementClient import ResourceManagementClient
 
 _RMC = ResourceManagementClient()
 
 def writeSLSXml( task, taskResult ):
+  '''
+  Call the real SLSXML.writeSLSXml function, but adding the connection to the 
+  DB.
+  '''
   return SLSXML.writeSLSXml( task, taskResult, _RMC )
