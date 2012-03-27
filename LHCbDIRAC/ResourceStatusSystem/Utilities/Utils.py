32db70ad97d1a763252812e3d525bb98b45406a2
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-03-27 11:50:30 +0000

    FIX: pyLint changes
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@49596 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Utilities/Utils.py
+++ LHCbDIRAC/ResourceStatusSystem/Utilities/Utils.py
@@ -1,7 +1,7 @@
 from LHCbDIRAC.ResourceStatusSystem.Utilities                       import SLSXML
 from LHCbDIRAC.ResourceStatusSystem.Client.ResourceManagementClient import ResourceManagementClient
 
-rmc = ResourceManagementClient()
+_RMC = ResourceManagementClient()
 
 def writeSLSXml( task, taskResult ):
-  return SLSXML.writeSLSXml( task, taskResult, rmc )
+  return SLSXML.writeSLSXml( task, taskResult, _RMC )
