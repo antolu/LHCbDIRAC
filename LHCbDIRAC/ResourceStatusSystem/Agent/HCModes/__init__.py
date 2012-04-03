24e4edab712c3196a3f67ca439ad49eba6692be6
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-04-03 13:12:06 +0000

    CommentedOut HC modes until it is needed
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@50092 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Agent/HCModes/__init__.py
+++ LHCbDIRAC/ResourceStatusSystem/Agent/HCModes/__init__.py
@@ -1,8 +1,8 @@
-# $HeadURL $
-''' LHCbDIRAC.ResourceStatusSystem.Agent.HCModes package
-'''
-
-__RCSID__  = '$Id: $'
-
-################################################################################
-#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
+## $HeadURL $
+#''' LHCbDIRAC.ResourceStatusSystem.Agent.HCModes package
+#'''
+#
+#__RCSID__  = '$Id: $'
+#
+#################################################################################
+##EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
