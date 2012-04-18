c36d61e3560fc95b23e43ed96ecd692af580e56d
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-04-18 12:08:06 +0000

    NEW: dummy unittest to generate Jenkins reports
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@51016 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/BookkeepingSystem/Client/test/Test_BKK_Client_IEntitySystemStateless.py
@@ -0,0 +1,55 @@
+# $HeadURL: $
+''' Test_BKK_Client_IEntitySystemStateless
+
+'''
+
+import unittest
+
+__RCSID__ = '$Id: $'
+
+dummyResults = {}
+class DummyReturn():
+  
+  def __init__( self, *args, **kwargs ):
+    pass
+  def __getattr__( self, name ):
+    return self.dummyMethod
+  def dummyMethod( self, *args, **kwargs ):
+    return dummyResults[ self.__class__.__name__ ]
+  
+def dummyCallable( whatever ):
+  return whatever  
+  
+################################################################################  
+  
+class IEntitySystemStateless_TestCase( unittest.TestCase ):
+  
+  def setUp( self ):
+    '''
+    Setup
+    '''
+
+    # We need the proper software, and then we overwrite it.
+    import LHCbDIRAC.BookkeepingSystem.Client.IEntitySystemStateless as moduleTested
+    moduleTested.gLogger = dummyCallable    
+    
+    self.iEntity = moduleTested.IEntitySystemStateless
+    
+  def tearDown( self ):
+    '''
+    TearDown
+    '''
+    del self.iEntity  
+  
+################################################################################  
+  
+class IEntitySystemStateless_Success( IEntitySystemStateless_TestCase ):
+  
+  def test_instantiate( self ):
+    ''' tests that we can instantiate one object of the tested class
+    '''       
+    iEntity = self.iEntity()
+    self.assertEqual( 'IEntitySystemStateless', iEntity.__class__.__name__ )  
+  
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF  
