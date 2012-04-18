49e29f015f28fad79ba4ba6081d7bbddc6595c01
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-04-18 12:36:26 +0000

    NEW: dummy test to generate Jenkins reports
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@51018 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/ConfigurationSystem/Agent/test/Test_CS_Agent_DummyTest.py
@@ -0,0 +1,15 @@
+# $HeadURL: $
+''' Test_CS_Agent_DummyTest
+
+'''
+
+import unittest
+
+__RCSID__ = '$Id: $'
+
+class DummyTest_TestCase( unittest.TestCase ): pass
+
+class Dummy_Success( DummyTest_TestCase ):
+  
+  def test_dummy( self ):
+    self.assertEqual( True, True )
