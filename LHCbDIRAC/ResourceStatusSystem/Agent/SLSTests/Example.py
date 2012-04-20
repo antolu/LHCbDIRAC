6879532e1b1d27c81289c3a0a9517aa9129e1c3c
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-04-20 09:52:40 +0000

    NEW: commented out, SLSAgent2 will not be used.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@51367 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Agent/SLSTests/Example.py
+++ LHCbDIRAC/ResourceStatusSystem/Agent/SLSTests/Example.py
@@ -1,20 +1,20 @@
-# $HeadURL: $
-'''  Example
-
-  Module used as example and for tests.
-
-'''
-
-__RCSID__  = '$Id: $'
-   
-def getProbeElements():  
-  return { 'OK' : True, 'Value' : [ 1, 2, 3 ] }
-
-def setupProbes( testConfig ):
-  return { 'OK' : True, 'Value' : testConfig }
-
-def runProbe( probeInfo, testConfig ):
-  return { 'xmlDict' : {}, 'config' : testConfig }
-
-################################################################################
-#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
+## $HeadURL: $
+#'''  Example
+#
+#  Module used as example and for tests.
+#
+#'''
+#
+#__RCSID__  = '$Id: $'
+#   
+#def getProbeElements():  
+#  return { 'OK' : True, 'Value' : [ 1, 2, 3 ] }
+#
+#def setupProbes( testConfig ):
+#  return { 'OK' : True, 'Value' : testConfig }
+#
+#def runProbe( probeInfo, testConfig ):
+#  return { 'xmlDict' : {}, 'config' : testConfig }
+#
+#################################################################################
+##EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
