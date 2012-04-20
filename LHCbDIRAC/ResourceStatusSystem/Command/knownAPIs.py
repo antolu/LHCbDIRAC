c6408fcfecda999bed39b3048527d27d12f8d80d
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-04-20 14:08:18 +0000

    FIX: update dictionary inside function
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@51399 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Command/knownAPIs.py
+++ LHCbDIRAC/ResourceStatusSystem/Command/knownAPIs.py
@@ -14,13 +14,14 @@ __APIs__ = {
   'ResourceManagementClient' : 'LHCbDIRAC.ResourceStatusSystem.Client.ResourceManagementClient',
            }
 
-DIRACknownAPIs.__APIs__.update( __APIs__ )
-
 def initAPIs( desiredAPIs, knownAPIs, force = False ):
   '''
   Executes the function of DIRAC, with the only difference that __APIs__ has 
   been updated to use LHCbDIRAC clients instead of DIRAC ones.
   '''
+  
+  DIRACknownAPIs.__APIs__.update( __APIs__ )
+  
   return DIRACknownAPIs.initAPIs( desiredAPIs, knownAPIs, force )
 
 ################################################################################
