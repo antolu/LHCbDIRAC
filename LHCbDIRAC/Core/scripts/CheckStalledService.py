f52948f485d64b57be9eeaebf110de6b0adbaaa5
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2012-04-26 06:13:51 +0000

    remove unsued import
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@51598 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Core/scripts/CheckStalledService.py
+++ LHCbDIRAC/Core/scripts/CheckStalledService.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
 #
-#  check if a service or an agent is stalled
+"""  check if a service or an agent is stalled """
+
+__RCSID__ = "$Id"
+
 #
-import os, commands, sys
-import DIRAC
+import os, commands
+#import DIRAC
 from DIRAC.Core.Base import Script
 Script.addDefaultOptionValue( 'LogLevel', 'verbose' )
 Script.parseCommandLine( ignoreErrors = True )
 
-from DIRAC.Core.Utilities.Time import fromString, date, second, dateTime, day, timeInterval
+from DIRAC.Core.Utilities.Time import fromString, second, dateTime, timeInterval
 from DIRAC import gLogger
 
 
@@ -23,20 +26,20 @@ gLogger.notice( 'The script ' + Script.scriptName + ' is running at ' + str( now
 #print 'The script is runing at '+str(now)
 
 fd = os.listdir( runit_dir )
-for dir in fd:
+for dirname in fd:
   status, result = commands.getstatusoutput( 'runsvstat ' + runit_dir + '/' + dir )
-  filename = runit_dir + '/' + dir + '/' + logfile
+  filename = runit_dir + '/' + dirname + '/' + logfile
   fl = open( filename, "r" )
   listLines = fl.readlines()
   fl.close()
   lastLine = listLines[-1]
-  gLogger.info( 'Checking ----> ' + dir )
+  gLogger.info( 'Checking ----> ' + dirname )
   for line in listLines:
     if line.find( "WARN: Server is not who" ):
       if line.find( "Polling time" ) != -1:
         try:
           pollingtime = line.split( ':' )[4].split( ' ' )[1].split( '.' )[0]
-        except:
+        except TyepError:
           gLogger.error( "    wrong format for Polling Time : " + line )
           break
       else:
@@ -44,18 +47,18 @@ for dir in fd:
 
   lastLineList = lastLine.split( ' ' )
 #  print "    the PollingTime for "+dir+" is : "+str(pollingtime)+" s"
-  gLogger.debug( "    the PollingTime for " + dir + " is : " + str( pollingtime ) + " s" )
+  gLogger.debug( "    the PollingTime for " + dirname + " is : " + str( pollingtime ) + " s" )
   try:
     lastupdate = fromString( lastLineList[0] + ' ' + lastLineList[1] )
-  except:
-    gLogger.error( '    EXCEPT : ' + dir )
-    pass
+  except TypeError:
+    gLogger.error( '    EXCEPT : ' + dirname )
+
   if int( pollingtime ) < 59:
     pollingtime = 120
 
   interval = timeInterval( lastupdate, second * int( pollingtime ) )
   if not interval.includes( now ):
-    gLogger.info( '    last update for ' + dir + ' was : ' + str( lastupdate ) )
+    gLogger.info( '    last update for ' + dirname + ' was : ' + str( lastupdate ) )
     gLogger.info( '    Polling Time is ' + str( pollingtime ) + ' s' )
     gLogger.info( '    last known status' + result + '\n' )
     gLogger.debug( '   Last line is : \n' + lastLine )
