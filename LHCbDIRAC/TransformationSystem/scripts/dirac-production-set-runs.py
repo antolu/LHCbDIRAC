fb318264cb010ba1e9e3186cbda462049fdd4fff
afalabel <afalabel@4525493e-7705-40b1-a816-d608a930855b>
2012-04-27 09:13:42 +0000

    Remove dirac-production-set-startrun and dirac-production-set-endrun . Previous functionality now in dirac-production-set-runs
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@51820 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/TransformationSystem/scripts/dirac-production-set-runs.py
+++ LHCbDIRAC/TransformationSystem/scripts/dirac-production-set-runs.py
@@ -23,17 +23,17 @@ Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      '  RunNumber: New Start or End run',
                                      '  RunList: List of Runs to be added',
                                      'Examples:\n',
-                                     'dirac-production-set-run.py 92 -l                         (show the list of runs for production 92)\n',
-                                     'dirac-production-set-run.py 92 -p 98200,98201             (add some discrete run to production 92)\n',
-                                     'dirac-production-set-run.py 92 -p 98200,98201,99000:99100 (add some discrete run and a range of runs to production 92)\n',
-                                     'dirac-production-set-run.py 92 -a                         (change the start run for production 92)\n',
-                                     'dirac-production-set-run.py 92 -e                         (change the end run  for production 92)\n'
+                                     'dirac-production-set-run.py 92 --list                        (show the list of runs for production 92)\n',
+                                     'dirac-production-set-run.py 92 --add 98200,98201             (add some discrete run to production 92)\n',
+                                     'dirac-production-set-run.py 92 --add 98200,98201,99000:99100 (add some discrete run and a range of runs to production 92)\n',
+                                     'dirac-production-set-run.py 92 --start 98200                 (change the start run for production 92)\n',
+                                     'dirac-production-set-run.py 92 --end   98200                 (change the end run  for production 92)\n'
                                      ]) )
 
-Script.registerSwitch( 'e', 'end', "Specify endrun for the production" )
-Script.registerSwitch( 'a', 'start', "Specify startrun for the production" )
-Script.registerSwitch( 'p', 'add', "add List of runs to the production" )
-Script.registerSwitch( 'l', 'list', "List the runs for the production" )
+Script.registerSwitch('','end', "Specify endrun for the production" )
+Script.registerSwitch('','start', "Specify startrun for the production" )
+Script.registerSwitch('','add', "add List of runs to the production" )
+Script.registerSwitch('','list', "List the runs for the production" )
 Script.parseCommandLine ( ignoreErrors = True )
 
 args = Script.getPositionalArgs()
@@ -44,7 +44,8 @@ if (len( args )>2 or len( switches )!=1):
 
 from LHCbDIRAC.TransformationSystem.Client.TransformationClient import TransformationClient
 
-if not switches[0] in ( 'e', 'end' ):
+
+if not args[0]=='h':
   prodId = int( args[0] )
   client = TransformationClient()
   try:
@@ -54,79 +55,51 @@ if not switches[0] in ( 'e', 'end' ):
     print "Error retrieving bkquery for Production %s"%prodId
 
 
+
 for switch in switches[0]:
   opt = switch.lower()
-  if opt in ( 'e', 'end' ):
+  if opt=='end':
     if (len(args)!=2):
       Script.showHelp()
     else:  
-      try:
-        runId = int( args[1] )
-      except ValueError:
-        print "EndRun not integer"
-        DIRAC.exit(2)
       if (bkDict.has_key("EndRun") and bkDict.has_key("StartRun")):
         endRun = int(res['Value']['EndRun'])
-        res = client.convertBookkeepingQueryRunListTransformation(prodId)
+        runId = int( args[1] )
         res = client.setBookkeepingQueryEndRunForTransformation(prodId,runId)
         if res['OK']:
           print "End run of production %s is now %s" % ( str( prodId ), str( runId ) )
         else:
           print res['Message']
       else:
-        runList =bkDict['RunNumbers']
-        if str(runId) not in runList:
-          res = client.setBookkeepingQueryEndRunForTransformation(prodId,runId)          
-          if res['OK']:
-            print "End run of production %s is now %s" % ( str( prodId ), str( runId ) )
-          else:
-            print res['Message']
+        print "Production %s has RunNumbers key" %str( prodId )
   
-  if opt in ( 'a', 'start' ):
+  if opt=='start':
     if (len(args)!=2):
       Script.showHelp()
     else:  
-      try:
-        runId = int( args[1] )
-      except ValueError:
-        print "StartRun not integer"
-        DIRAC.exit(2)
       if (bkDict.has_key("EndRun") and bkDict.has_key("StartRun")):
-        res = client.convertBookkeepingQueryRunListTransformation(prodId)
+        runId = int( args[1] )
         res = client.setBookkeepingQueryStartRunForTransformation(prodId,runId)
         if res['OK']:
           print "Start run of production %s is now %s" % ( str( prodId ), str( runId ) )
         else:
           print res['Message']
       else:
-        runList =bkDict['RunNumbers']
-        if str(runId) not in runList:
-          res = client.setBookkeepingQueryStartRunForTransformation(prodId,runId)
-          print res          
-          if res['OK']:
-            print "Start run of production %s is now %s" % ( str( prodId ), str( runId ) )
-          else:
-            print res['Message']
-
-  if opt in ( 'p', 'add' ):
+        print "Production %s has RunNumbers key" %str( prodId )
+        
+  if opt=='add':
     if (len(args)!=2):
       Script.showHelp()
     else:  
       runId = str( args[1] )
-      if (bkDict.has_key("EndRun") and bkDict.has_key("StartRun")):
-        res = client.convertBookkeepingQueryRunListTransformation(prodId)
+      if (bkDict.has_key("RunNumbers")):
         res = client.addBookkeepingQueryRunListTransformation(prodId,runId)
         if res['OK']:
           print "Run list modified for production %s" % str( prodId )
         else:
           print res['Message']
       else:
-        runList =bkDict['RunNumbers']
-        res = client.addBookkeepingQueryRunListTransformation(prodId,runId)          
-        if res['OK']:
-          print "Run list modified for production %s" % str( prodId )
-        else:
-          print res['Message']
+        print "Production %s has start run and end run" % str( prodId )
 
   if opt in ( 'l', 'list' ):
     if (len(args)!=1):
