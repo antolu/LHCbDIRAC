652a2ac5656eabbd43df9c26b329b85ccb8c156f
joel <joel@4525493e-7705-40b1-a816-d608a930855b>
2012-05-04 11:52:09 +0000

    Check the Stalled Agent
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@52103 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Core/scripts/CheckStalledService.py
+++ LHCbDIRAC/Core/scripts/CheckStalledService.py
@@ -3,16 +3,16 @@
 #
 """  check if a service or an agent is stalled """
 
-__RCSID__ = "$Id"
+__RCSID__ = "$Id$"
 
 #
 import os, commands
-#import DIRAC
 from DIRAC.Core.Base import Script
 Script.addDefaultOptionValue( 'LogLevel', 'verbose' )
 Script.parseCommandLine( ignoreErrors = True )
 
 from DIRAC.Core.Utilities.Time import fromString, second, dateTime, timeInterval
+from DIRAC.Interfaces.API.DiracAdmin                         import DiracAdmin
 from DIRAC import gLogger
 
 
@@ -20,45 +20,58 @@ now = dateTime()
 runit_dir = '/opt/dirac/startup'
 logfile = 'log/current'
 pollingtime = 60
+diracAdmin = DiracAdmin()
+mailadress = 'lhcb-grid-shifter-oncall@cern.ch'
+subject = 'CheckStalled'
+msg = 'List of Services / agents which could be stalled \n\n'
+
+
+def write_log( mesg ):
+  """ create the log file """
+  global msg
+  gLogger.notice( mesg )
+  msg += mesg + '\n'
 
 gLogger.getSubLogger( "CheckStalledServices" )
-gLogger.notice( 'The script ' + Script.scriptName + ' is running at ' + str( now ) )
-#print 'The script is runing at '+str(now)
+write_log( 'The script ' + Script.scriptName + ' is running at ' + str( now ) )
 
 fd = os.listdir( runit_dir )
 for dirname in fd:
-  status, result = commands.getstatusoutput( 'runsvstat ' + runit_dir + '/' + dir )
+  status, result = commands.getstatusoutput( 'runsvstat ' + runit_dir + '/' + dirname )
   filename = runit_dir + '/' + dirname + '/' + logfile
   fl = open( filename, "r" )
   listLines = fl.readlines()
   fl.close()
   lastLine = listLines[-1]
-  gLogger.info( 'Checking ----> ' + dirname )
+  write_log( 'Checking ----> ' + dirname )
   for line in listLines:
     if line.find( "WARN: Server is not who" ):
       if line.find( "Polling time" ) != -1:
         try:
           pollingtime = line.split( ':' )[4].split( ' ' )[1].split( '.' )[0]
-        except TypeError:
-          gLogger.error( "    wrong format for Polling Time : " + line )
+        except:
+          write_log( "    wrong format for Polling Time : " + line )
           break
       else:
         lastLine = line
 
   lastLineList = lastLine.split( ' ' )
-#  print "    the PollingTime for "+dir+" is : "+str(pollingtime)+" s"
-  gLogger.debug( "    the PollingTime for " + dirname + " is : " + str( pollingtime ) + " s" )
   try:
     lastupdate = fromString( lastLineList[0] + ' ' + lastLineList[1] )
-  except TypeError:
-    gLogger.error( '    EXCEPT : ' + dirname )
+  except:
+    write_log( '    EXCEPT : ' + dirname )
 
   if int( pollingtime ) < 59:
     pollingtime = 120
 
   interval = timeInterval( lastupdate, second * int( pollingtime ) )
   if not interval.includes( now ):
-    gLogger.info( '    last update for ' + dirname + ' was : ' + str( lastupdate ) )
-    gLogger.info( '    Polling Time is ' + str( pollingtime ) + ' s' )
-    gLogger.info( '    last known status' + result + '\n' )
-    gLogger.debug( '   Last line is : \n' + lastLine )
+    write_log( "    the PollingTime is : " + str( pollingtime ) + " s" )
+    write_log( '    last update for ' + dirname + ' was : ' + str( lastupdate ) )
+    write_log( '    Polling Time is ' + str( pollingtime ) + ' s' )
+    write_log( '    last known status' + result + '\n' )
+    write_log( '   Last line is : \n' + lastLine )
+
+res = diracAdmin.sendMail( mailadress, subject, msg, fromAddress = 'joel.closier@cern.ch' )
+if not res[ 'OK' ]:
+  print 'The mail could not be sent'
