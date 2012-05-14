97c4d82471147a57999a92f88cd6bb6731a592aa
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-05-14 14:23:42 +0000

    FIX: pyLint compliant code
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@52504 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/AccountingSystem/Client/Types/DataStorage.py
+++ LHCbDIRAC/AccountingSystem/Client/Types/DataStorage.py
@@ -1,27 +1,41 @@
 # $HeadURL$
-__RCSID__ = "$Id$"
+''' DataStorage Type
+  
+'''
 
 from DIRAC.AccountingSystem.Client.Types.BaseAccountingType import BaseAccountingType
 
+__RCSID__ = "$Id$"
+
 class DataStorage( BaseAccountingType ):
+  '''
+    DataStorage as extension of BaseAccountingType
+  '''
 
   def __init__( self ):
+    
     BaseAccountingType.__init__( self )
-    self.definitionKeyFields = [ ( 'DataType' , "VARCHAR(64)" ),
-                                 ( 'Activity', "VARCHAR(64)" ),
-                                 ( 'FileType', "VARCHAR(32)" ),
-                                 ( 'Production', "VARCHAR(32)" ),
+    
+    self.definitionKeyFields = [ ( 'DataType',       "VARCHAR(64)" ),
+                                 ( 'Activity',       "VARCHAR(64)" ),
+                                 ( 'FileType',       "VARCHAR(32)" ),
+                                 ( 'Production',     "VARCHAR(32)" ),
                                  ( 'ProcessingPass', "VARCHAR(64)" ),
-                                 ( 'Conditions', "VARCHAR(64)" ),
-                                 ( 'EventType', "VARCHAR(64)" ),
+                                 ( 'Conditions',     "VARCHAR(64)" ),
+                                 ( 'EventType',      "VARCHAR(64)" ),
                                  ( 'StorageElement', "VARCHAR(32)" )
                                ]
-    self.definitionAccountingFields = [ ( 'LogicalSize', 'BIGINT UNSIGNED' ),
-                                        ( 'LogicalFiles', 'BIGINT UNSIGNED' ),
-                                        ( 'PhysicalSize', 'BIGINT UNSIGNED' ),
+    
+    self.definitionAccountingFields = [ ( 'LogicalSize',   'BIGINT UNSIGNED' ),
+                                        ( 'LogicalFiles',  'BIGINT UNSIGNED' ),
+                                        ( 'PhysicalSize',  'BIGINT UNSIGNED' ),
                                         ( 'PhysicalFiles', 'BIGINT UNSIGNED' )
                                       ]
+    
     self.bucketsLength = [ ( 86400 * 30 * 6, 86400 ), #<6m = 1d
-                           ( 31104000, 604800 ), #>6m = 1w
+                           ( 31104000,       604800 ), #>6m = 1w
                          ]
     self.checkType()
+
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
--- LHCbDIRAC/AccountingSystem/Client/Types/Storage.py
+++ LHCbDIRAC/AccountingSystem/Client/Types/Storage.py
@@ -1,21 +1,33 @@
 # $HeadURL$
-__RCSID__ = "$Id$"
+''' Storage Type
+  
+'''
 
 from DIRAC.AccountingSystem.Client.Types.BaseAccountingType import BaseAccountingType
 
+__RCSID__ = "$Id$"
+
 class Storage( BaseAccountingType ):
+  '''
+    Storage as extension of BaseAccountingType
+  '''
 
   def __init__( self ):
+    
     BaseAccountingType.__init__( self )
+    
     self.definitionKeyFields = [ ( 'StorageElement' , "VARCHAR(32)" ),
-                                 ( 'Directory', "VARCHAR(32)" )
+                                 ( 'Directory',       "VARCHAR(32)" )
                                ]
-    self.definitionAccountingFields = [ ( 'LogicalSize', 'BIGINT UNSIGNED' ),
-                                        ( 'LogicalFiles', 'BIGINT UNSIGNED' ),
-                                        ( 'PhysicalSize', 'BIGINT UNSIGNED' ),
+    self.definitionAccountingFields = [ ( 'LogicalSize',   'BIGINT UNSIGNED' ),
+                                        ( 'LogicalFiles',  'BIGINT UNSIGNED' ),
+                                        ( 'PhysicalSize',  'BIGINT UNSIGNED' ),
                                         ( 'PhysicalFiles', 'BIGINT UNSIGNED' )
                                       ]
     self.bucketsLength = [ ( 86400 * 30 * 6, 86400 ), #<6m = 1d
-                           ( 31104000, 604800 ), #>6m = 1w
+                           ( 31104000,       604800 ), #>6m = 1w
                          ]
     self.checkType()
+    
+################################################################################    
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
