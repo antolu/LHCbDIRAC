dce310c3881799e199ee2073cf1f98ecf0ac0bd4
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-07-05 09:47:09 +0000

    FIX: removed error and warnings cleanup
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@54201 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/DB/ProductionDB.py
+++ LHCbDIRAC/ProductionManagementSystem/DB/ProductionDB.py
@@ -1,18 +1,17 @@
-# $Id$
-
+# $HeadURL$
 """ DIRAC ProductionDB class is a front-end to the LHCb production specific database """
 
-__RCSID__ = "$Revision$"
-
-from DIRAC                                                          import gLogger, gConfig, S_OK, S_ERROR
+from DIRAC                                                          import gLogger, S_OK, S_ERROR
 from DIRAC.Core.Base.DB                                             import DB
+
+__RCSID__ = "$Id$"
 
 class ProductionDB(DB):
 
   def __init__( self, maxQueueSize=10 ):
     """ Constructor
     """
-    DB.__init__(self,'ProductionDB', 'ProductionManagement/ProductionDB', maxQueueSize)
+    DB.__init__( self, 'ProductionDB', 'ProductionManagement/ProductionDB', maxQueueSize )
 
 ################ WORKFLOW SECTION ####################################
 
@@ -20,12 +19,14 @@ class ProductionDB(DB):
 
     if not self._isWorkflowExists(wf_name):
       # workflow is not exists
-      result = self._insert('Workflows', [ 'WFName', 'WFParent', 'Description', 'LongDescription','AuthorDN', 'AuthorGroup', 'Body' ],
+      result = self._insert( 'Workflows', [ 'WFName', 'WFParent', 'Description', 'LongDescription',
+                                            'AuthorDN', 'AuthorGroup', 'Body' ],
                             [wf_name, wf_parent, wf_descr, wf_descr_long, authorDN, authorGroup, wf_body])
       if result['OK']:
         gLogger.verbose('Workflow "%s" published by DN="%s"' % (wf_name, authorDN))
       else:
-        error = 'Workflow "%s" FAILED to be published by DN="%s" with message "%s"' % (wf_name, authorDN, result['Message'])
+        _msgTuple = (wf_name, authorDN, result['Message'])
+        error = 'Workflow "%s" FAILED to be published by DN="%s" with message "%s"' % _msgTuple 
         gLogger.error(error)
         return S_ERROR( error )
     else: # workflow already exists
@@ -33,11 +34,15 @@ class ProductionDB(DB):
 
         # KGG tentative code !!!! have to check (seems working)
         result = self._escapeString( wf_body )
-        if not result['OK']: return result
-        wf_body_esc = result['Value'][1:len(result['Value'])-1] # we have to remove trailing " left by self._escapeString()
+        if not result['OK']: 
+          return result
+        # we have to remove trailing " left by self._escapeString()
+        wf_body_esc = result['Value'][1:len(result['Value'])-1] 
         result = self._escapeString( wf_descr_long )
-        if not result['OK']: return result
-        wf_descr_long_esc = result['Value'][1:len(result['Value'])-1] # we have to remove trailing " left by self._escapeString()
+        if not result['OK']: 
+          return result
+        # we have to remove trailing " left by self._escapeString()
+        wf_descr_long_esc = result['Value'][1:len(result['Value'])-1] 
         # end of tentative code
 
         cmd = "UPDATE Workflows Set WFParent='%s', Description='%s', LongDescription='%s', AuthorDN='%s', AuthorGroup='%s', Body='%s' WHERE WFName='%s' " \
@@ -60,7 +65,7 @@ class ProductionDB(DB):
     cmd = "SELECT  WFName, WFParent, Description, LongDescription, AuthorDN, AuthorGroup, PublishingTime from Workflows;"
     result = self._query(cmd)
     if result['OK']:
-      newres=[] # repacking
+      newres = [] # repacking
       for pr in result['Value']:
         newres.append({'WFName':pr[0], 'WFParent':pr[1], 'Description':pr[2], 'LongDescription':pr[3],
                        'AuthorDN':pr[4], 'AuthorGroup':pr[5], 'PublishingTime':pr[6].isoformat(' ')})
@@ -82,7 +87,7 @@ class ProductionDB(DB):
       gLogger.error("Failed to check if Workflow of name %s exists %s" % (name, result['Message']))
       return False
     elif result['Value'][0][0] > 0:
-        return True
+      return True
     return False
 
   def deleteWorkflow(self, name):
@@ -96,13 +101,25 @@ class ProductionDB(DB):
     return result
 
   def getWorkflowInfo(self, name):
+    
     cmd = "SELECT  WFName, WFParent, Description, LongDescription, AuthorDN, AuthorGroup, PublishingTime from Workflows WHERE WFname='%s'" % name
-    result = self._query(cmd)
+    result = self._query( cmd )
+    
     if result['OK']:
-      pr=result['Value'][0]
+      pr = result['Value'][0]
 
-      newres = {'WFName':pr[0], 'WFParent':pr[1], 'Description':pr[2], 'LongDescription':pr[3],
-                'AuthorDN':pr[4], 'AuthorGroup':pr[5], 'PublishingTime':pr[6].isoformat(' ')}
+      newres = {
+                 'WFName'          : pr[0], 
+                 'WFParent'        : pr[1], 
+                 'Description'     : pr[2], 
+                 'LongDescription' : pr[3],
+                 'AuthorDN'        : pr[4], 
+                 'AuthorGroup'     : pr[5], 
+                 'PublishingTime'  : pr[6].isoformat(' ')
+                 }
       return S_OK(newres)
     else:
-      return S_ERROR('Failed to retrive Workflow with the name=%s' % (name, result['Message']) )
+      return S_ERROR('Failed to retrive Workflow with the name=%s \n %s' % (name, result['Message']) )
+
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
