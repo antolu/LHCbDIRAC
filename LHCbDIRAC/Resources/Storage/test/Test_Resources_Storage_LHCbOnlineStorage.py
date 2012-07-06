cfd2404755a4a81b80f7757b211ab3cec20b106c
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-07-06 14:36:07 +0000

    NEW: more unit tests
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@54288 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Resources/Storage/test/Test_Resources_Storage_LHCbOnlineStorage.py
+++ LHCbDIRAC/Resources/Storage/test/Test_Resources_Storage_LHCbOnlineStorage.py
@@ -47,18 +47,38 @@ class LHCbOnlineStorage_Success( LHCbOnlineStorage_TestCase ):
     self.assertEqual( 'LHCbOnlineStorage', resource.__class__.__name__ )
   
   def test_init( self ):
-    ''' test that the init method does what it should do
+    ''' tests that the init method does what it should do
     '''
     
     resource = self.testClass( 'storageName', 'protocol', 'path', 'host', 'port', 'spaceToken', 'wspath' )  
       
     self.assertEqual( 'storageName', resource.name )
     self.assertEqual( 'LHCbOnline' , resource.protocolName )
+    self.assertEqual( 'protocol'   , resource.protocol )
     self.assertEqual( 'path'       , resource.path )
     self.assertEqual( 'host'       , resource.host )
     self.assertEqual( 'port'       , resource.port )
     self.assertEqual( 'spaceToken' , resource.spaceToken )
     self.assertEqual( 'wspath'     , resource.wspath )
+
+  def test_getParameters( self ):
+    ''' tests the output of getParameters method
+    '''
+
+    resource = self.testClass( 'storageName', 'protocol', 'path', 'host', 'port', 'spaceToken', 'wspath' )  
+
+    res = resource.getParameters()
+    self.assertEqual( True, res['OK'] )
+    res = res[ 'Value' ]
+      
+    self.assertEqual( 'storageName', res['StorageName'] )
+    self.assertEqual( 'LHCbOnline' , res['ProtocolName'] )
+    self.assertEqual( 'protocol'   , res['Protocol'] )
+    self.assertEqual( 'path'       , res['Path'] )
+    self.assertEqual( 'host'       , res['Host'] )
+    self.assertEqual( 'port'       , res['Port'] )
+    self.assertEqual( 'spaceToken' , res['SpaceToken'] )
+    self.assertEqual( 'wspath'     , res['WSUrl'] )
 
 ################################################################################
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
