b08b1a41c13268c146466fcb79e4004a82dba2cb
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-07-06 09:56:20 +0000

    FIX: reduced pyLint warnings, too picky with cosmetics..
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@54269 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Resources/Catalog/RAWIntegrityClient.py
+++ LHCbDIRAC/Resources/Catalog/RAWIntegrityClient.py
@@ -1,31 +1,36 @@
+# $HeadURL$
 """ Client plug-in for the RAWIntegrity catalogue.
     This exposes a single method to add files to the RAW IntegrityDB.
 """
+
 import DIRAC
-from DIRAC                                          import S_OK, S_ERROR, gLogger, gConfig
-from DIRAC.Core.DISET.RPCClient                     import RPCClient
-from DIRAC.ConfigurationSystem.Client               import PathFinder
-from DIRAC.Resources.Catalog.FileCatalogueBase      import FileCatalogueBase
 import types
 
-class RAWIntegrityClient(FileCatalogueBase):
+from DIRAC                                     import S_OK, S_ERROR, gLogger
+from DIRAC.Core.DISET.RPCClient                import RPCClient
+from DIRAC.ConfigurationSystem.Client          import PathFinder
+from DIRAC.Resources.Catalog.FileCatalogueBase import FileCatalogueBase
 
-  def __init__(self):
+__RCSID__  = '$Id$'
+
+class RAWIntegrityClient( FileCatalogueBase ):
+
+  def __init__( self ):
     try:
-      self.url = PathFinder.getServiceURL('DataManagement/RAWIntegrity')
+      self.url = PathFinder.getServiceURL( 'DataManagement/RAWIntegrity' )
       self.valid = True
-    except Exception,x:
+    except Exception, x:
       errStr = "RAWIntegrityClient.__init__: Exception while generating server url."
-      gLogger.exception(errStr,lException=x)
+      gLogger.exception( errStr, lException = x )
       self.valid = False
 
-  def isOK(self):
+  def isOK( self ):
     return self.valid
 
-  def exists(self,lfn):
+  def exists( self, lfn ):
     """ LFN may be a string or list of strings
     """
-    res = self.__checkArgumentFormat(lfn)
+    res = self.__checkArgumentFormat( lfn )
     if not res['OK']:
       return res
     lfns = res['Value']
@@ -33,31 +38,37 @@ class RAWIntegrityClient(FileCatalogueBase):
     failed = {}
     for lfn in lfns.keys():
       successful[lfn] = False
-    resDict = {'Failed':failed,'Successful':successful}
-    return S_OK(resDict)
+    resDict = { 
+               'Failed'     : failed,
+               'Successful' : successful
+               }
+    return S_OK( resDict )
 
-  def addFile(self, lfn):
-    res = self.__checkArgumentFormat(lfn)
+  def addFile( self, lfn ):
+    res = self.__checkArgumentFormat( lfn )
     if not res['OK']:
       return res
     failed = {}
     successful = {}
-    for lfn,info in res['Value'].items():
-      server = RPCClient(self.url,timeout=120)
-      pfn = str(info['PFN'])
-      size = int(info['Size'])
-      se = str(info['SE'])
-      guid = str(info['GUID'])
+    for lfn, info in res['Value'].items():
+      server = RPCClient( self.url, timeout=120 )
+      pfn      = str(info['PFN'])
+      size     = int(info['Size'])
+      se       = str(info['SE'])
+      guid     = str(info['GUID'])
       checksum = str(info['Checksum'])      
-      res = server.addFile(lfn,pfn,size,se,guid,checksum)
+      res      = server.addFile( lfn, pfn, size, se, guid, checksum )
       if not res['OK']:
         failed[lfn] = res['Message']
       else:
         successful[lfn] = True
-    resDict = {'Failed':failed,'Successful':successful}
+    resDict = {
+               'Failed'     : failed,
+               'Successful' : successful
+               }
     return S_OK(resDict)
 
-  def __checkArgumentFormat(self,path):
+  def __checkArgumentFormat( self, path ):
     if type(path) in types.StringTypes:
       urls = {path:False}
     elif type(path) == types.ListType:
@@ -83,3 +94,6 @@ class RAWIntegrityClient(FileCatalogueBase):
       successful[lfn] = {'Write':True}
     resDict = {'Failed':failed, 'Successful':successful}
     return S_OK( resDict )
+
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
