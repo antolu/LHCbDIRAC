ab23a26f9e21682c77284f7cf73901e894ed7272
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-07-27 15:48:32 +0000

    NEW: added PublisherHandler
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@54759 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/ResourceStatusSystem/Service/__init__.py
@@ -0,0 +1,8 @@
+# $HeadURL:  $
+'''  LHCbDIRAC.ResourceStatusSystem.Service package
+'''
+
+__RCSID__  = '$Id:  $'
+
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
