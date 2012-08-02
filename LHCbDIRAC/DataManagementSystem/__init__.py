a3692f03488b1fbc3f4c663964571519ed250f61
cibak <cibak@4525493e-7705-40b1-a816-d608a930855b>
2012-08-02 08:51:04 +0000

    code clean up to increase pylint score
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@54981 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/__init__.py
+++ LHCbDIRAC/DataManagementSystem/__init__.py
@@ -1,7 +1,7 @@
 ############################################################
 # $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/DIRAC/trunk/DIRAC/WorkloadManagementSystem/__init__.py $
 ############################################################
+""" LHCbDIRAC.DataManagementSystem package """
 __RCSID__ = "$Id: __init__.py 18064 2009-11-05 19:40:01Z acasajus $"
 
-""" LHCbDIRAC.DataManagementSystem package """
 
