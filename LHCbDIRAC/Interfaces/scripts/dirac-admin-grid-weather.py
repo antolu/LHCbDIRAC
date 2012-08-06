fc2728a6349a56d076b27cb66af393ae7c9c0fd2
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2012-08-06 10:37:10 +0000

    fix
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@55151 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Interfaces/scripts/dirac-admin-grid-weather.py
+++ LHCbDIRAC/Interfaces/scripts/dirac-admin-grid-weather.py
@@ -1,13 +1,12 @@
+#!/usr/bin/env python
+
 __RCSID__ = "$Id$"
 
 from DIRAC.Core.Base import Script
 
 Script.setUsageMessage( """
 Gives an overview of the grid resources status
-
-Usage:
-   %s
-""" % Script.scriptName )
+""" )
 Script.parseCommandLine()
 
 from LHCbDIRAC.Interfaces.API.DiracLHCb import DiracLHCb
