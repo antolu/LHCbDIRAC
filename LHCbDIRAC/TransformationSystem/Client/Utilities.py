c911770f29deeb92289cc1821e06267628ce4f78
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2012-08-09 12:24:09 +0000

    FIX: handle correctly NumberOfReplicas (string was wrong)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@55301 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/TransformationSystem/Client/Utilities.py
+++ LHCbDIRAC/TransformationSystem/Client/Utilities.py
@@ -2,6 +2,10 @@
   Utilities for scripts dealing with transformations
 """
 
+from DIRAC.Core.DISET.RPCClient                               import RPCClient
+from DIRAC.TransformationSystem.Client.TransformationClient   import TransformationClient
+from DIRAC.Core.Utilities.List                                import sortList
+
 __RCSID__ = "$Id$"
 
 import DIRAC
@@ -28,28 +32,19 @@ class PluginScript( DMScript ):
   def registerPluginSwitches( self ):
     self.registerBKSwitches()
     self.setSEs = {}
-    parameterSEs = ( "KeepSEs", "Archive1SEs", "Archive2SEs", "MandatorySEs",
-                     "SecondarySEs", "DestinationSEs", "FromSEs" )
+    parameterSEs = ( "KeepSEs", "Archive1SEs", "Archive2SEs", "MandatorySEs", "SecondarySEs", "DestinationSEs", "FromSEs" )
 
     Script.registerSwitch( "", "Plugin=", "   Plugin name (mandatory)", self.setPlugin )
-    Script.registerSwitch( "t:", "Type=",
-                           "   Transformation type [Replication] (Removal automatic)", self.setTransType )
-    Script.registerSwitch( "", "NumberOfReplicas=",
-                           "   Number of copies to create or to remove", self.setReplicas )
+    Script.registerSwitch( "t:", "Type=", "   Transformation type [Replication] (Removal automatic)", self.setTransType )
+    Script.registerSwitch( "", "NumberOfReplicas=", "   Number of copies to create or to remove", self.setReplicas )
     for param in parameterSEs:
       self.setSEs[param] = setter( self, param )
-      Script.registerSwitch( "", param + '=',
-                             "   List of SEs for the corresponding parameter of the plugin", self.setSEs[param].setOption )
-    Script.registerSwitch( "g:", "GroupSize=",
-                           "   GroupSize parameter for merging (GB) or nb of files" , self.setGroupSize )
-    Script.registerSwitch( "", "Parameters=",
-                           "   Additional plugin parameters ({<key>:<val>,[<key>:val>]}", self.setParameters )
-    Script.registerSwitch( "", "RequestID=",
-                           "   Sets the request ID (default 0)", self.setRequestID )
-    Script.registerSwitch( "", "ProcessingPasses=",
-                           "   List of processing passes for the DeleteReplicasWhenProcessed plugin", self.setProcessingPasses )
-    Script.registerSwitch( "", "Period=",
-                           "   minimal period at which a plugin is executed (if instrumented)", self.setPeriod )
+      Script.registerSwitch( "", param + '=', "   List of SEs for the corresponding parameter of the plugin", self.setSEs[param].setOption )
+    Script.registerSwitch( "g:", "GroupSize=", "   GroupSize parameter for merging (GB) or nb of files" , self.setGroupSize )
+    Script.registerSwitch( "", "Parameters=", "   Additional plugin parameters ({<key>:<val>,[<key>:val>]}", self.setParameters )
+    Script.registerSwitch( "", "RequestID=", "   Sets the request ID (default 0)", self.setRequestID )
+    Script.registerSwitch( "", "ProcessingPasses=", "   List of processing passes for the DeleteReplicasWhenProcessed plugin", self.setProcessingPasses )
+    Script.registerSwitch( "", "Period=", "   minimal period at which a plugin is executed (if instrumented)", self.setPeriod )
 
   def setPlugin( self, val ):
     self.options['Plugin'] = val
@@ -60,7 +55,7 @@ class PluginScript( DMScript ):
     return DIRAC.S_OK()
 
   def setReplicas ( self, val ):
-    self.options['Replicas'] = val
+    self.options['NumberOfReplicas'] = val
     return DIRAC.S_OK()
 
   def setGroupSize ( self, groupSize ):
@@ -113,8 +108,8 @@ class PluginScript( DMScript ):
       params[key] = seList
     return params
 
-def getRemovalPlugins( self ):
-  return ( "DestroyDataset", "DeleteDataset", "DeleteReplicas", 'DeleteReplicasWhenProcessed' )
-def getReplicationPlugins( self ):
-  return ( "LHCbDSTBroadcast", "LHCbMCDSTBroadcast", "LHCbMCDSTBroadcastRandom", "ArchiveDataset", "ReplicateDataset",
-           "RAWShares", 'FakeReplication', 'ReplicateToLocalSE' )
+  def getRemovalPlugins( self ):
+    return ( "DestroyDataset", "DeleteDataset", "DeleteReplicas", 'DeleteReplicasWhenProcessed' )
+  def getReplicationPlugins( self ):
+    return ( "LHCbDSTBroadcast", "LHCbMCDSTBroadcast", "LHCbMCDSTBroadcastRandom", "ArchiveDataset", "ReplicateDataset",
+             "RAWShares", 'FakeReplication', 'ReplicateToLocalSE' )
