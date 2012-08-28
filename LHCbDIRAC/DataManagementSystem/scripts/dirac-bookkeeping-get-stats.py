8ffaa85f27351098b1ff2fccc242db1a2444a3e3
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2012-08-28 11:21:41 +0000

    CHANGE: allow multiple file types and treat them correctly for average statistics
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@55676 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/scripts/dirac-bookkeeping-get-stats.py
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-bookkeeping-get-stats.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
 
   dmScript = DMScript()
   dmScript.registerBKSwitches()
+  dmScript.registerFileSwitches()
 
 
   Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
@@ -39,13 +40,14 @@ if __name__ == "__main__":
     pass
 
   bkQuery = dmScript.getBKQuery()
-  prodList = bkQuery.getQueryDict().get( 'Production', [None] )
-  bkQuery.setOption( 'Production', None )
+  prodList = bkQuery.getQueryDict().get( 'Production ', [None] )
+  bkQuery.setOption( 'ProductionID', None )
 
   from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient  import BookkeepingClient
   bk = BookkeepingClient()
   from LHCbDIRAC.TransformationSystem.Client.TransformationClient           import TransformationClient
   transClient = TransformationClient()
+  lfns = dmScript.getOption( 'LFNs' )
 
   for prod in prodList:
     if prod:
@@ -54,24 +56,57 @@ if __name__ == "__main__":
       prodName = res['Value']['TransformationName']
       bkQuery.setOption( 'Production', prod )
       print "For production %d, %s (query %s)" % ( prod, prodName, bkQuery )
+    elif lfns:
+      print "For LFNs:", lfns
     else:
       print "For BK query:", bkQuery
     queryDict = bkQuery.getQueryDict()
-    if not len( queryDict ):
+    if not lfns and ( not len( queryDict ) or queryDict.keys() == ['Visible'] ):
       print "Invalid query", queryDict
       DIRAC.exit( 1 )
-    if queryDict.get( 'Visible', 'No' ) == 'Yes':
+    if not lfns and queryDict.get( 'Visible', 'No' ) == 'Yes':
       if 'Visible' in queryDict: queryDict.pop( 'Visible' )
       query = queryDict.copy()
       if len( query ) <= 3:
         query.update( {'1':1, '2':2, '3':3 } )
-      res = bk.getFilesSummary( query )
-      if not res['OK']:
-        print "Error getting statistics from BK", res['Message']
-        DIRAC.exit( 0 )
-      nbRecords = res['Value']['TotalRecords']
-      records = res['Value']['Records']
-      paramNames = res['Value']['ParameterNames']
+      fileTypes = query.get( 'FileType' )
+      if type( fileTypes ) != type( [] ):
+        fileTypes = [fileTypes]
+      if 'FileType' in query:
+        query.pop( 'FileType' )
+      records = []
+      if fileTypes:
+        nDatasets = len( fileTypes )
+      else:
+        nDatasets = 1
+      for fileType in fileTypes:
+        if fileType:
+          query['FileType'] = fileType
+        res = bk.getFilesSummary( query )
+        if not res['OK']:
+          print "Error getting statistics from BK", res['Message']
+          DIRAC.exit( 0 )
+        paramNames = res['Value']['ParameterNames']
+        record = []
+        for paramValues in res['Value']['Records']:
+          if not record:
+            record = len( paramValues ) * [0]
+          record = [rec + val for rec, val in zip( record, paramValues )]
+        #print fileType, record
+        for name, value in zip( paramNames, record ):
+          if name == 'NumberOfEvents':
+            nevts = value
+          elif name == 'FileSize':
+            size = value
+          elif name == 'Luminosity':
+            lumi = value
+        record.append( nevts / float( lumi ) if lumi else 0. )
+        record.append( size / float( lumi ) if lumi else 0. )
+        if not records:
+          records = len( record ) * [0]
+        records = [rec1 + rec2 for rec1, rec2 in zip( record, records )]
+        #print fileType, records, 'Total'
+      paramNames += ['EvtsPerLumi', 'SizePerLumi' ]
     else:
       # To be replaced with getFilesWithMetadata when it allows invisible files
       paramNames = ['NbofFiles', 'NumberOfEvents', 'FileSize', 'Luminosity']
@@ -79,55 +114,69 @@ if __name__ == "__main__":
       nbEvents = 0
       fileSize = 0
       lumi = 0
-      res = bk.getFiles( queryDict )
-      if res['OK']:
-        res = bk.getFileMetadata( res['Value'] )
+      nDatasets = 1
+      if not lfns:
+        res = bk.getFilesWithGivenDataSets( queryDict )
         if res['OK']:
-          for lfn, metadata in res['Value'].items():
-            nbFiles += 1
-            nbEvents += metadata['EventStat']
-            fileSize += metadata['FileSize']
-            lumi += metadata['Luminosity']
-          records = [[nbFiles, nbEvents, fileSize, lumi]]
+          lfns = res['Value']
+        else:
+          print "Error getting files for %s", queryDict
+          continue
+      res = bk.getFileMetadata( lfns )
+      if res['OK']:
+        for lfn, metadata in res['Value'].items():
+          nbFiles += 1
+          nbEvents += metadata['EventStat']
+          fileSize += metadata['FileSize']
+          lumi += metadata['Luminosity']
+        records = [nbFiles, nbEvents, fileSize, lumi]
       if not res['OK']:
         records = []
         print "Error getting files information:", res['Message']
 
-    tab = 15
+    tab = 17
     sizeUnits = ( 'Bytes', 'kB', 'MB', 'GB', 'TB', 'PB' )
     lumiUnits = ( '/microBarn', '/nb', '/pb', '/fb', '/ab' )
-    for paramValues in records:
-      for name, value in zip( paramNames, paramValues ):
-        if name == 'NbofFiles':
-          nfiles = value
-          print '%s:%s' % ( 'Nb of Files'.ljust( tab ), intWithQuotes( value ) )
-        elif name == 'NumberOfEvents':
-          nevts = value
-          print '%s:%s' % ( 'Nb of Events'.ljust( tab ), intWithQuotes( value ) )
-        elif name == 'FileSize':
-          size = value
-          sizePerEvt = '(%.1f kB per evt)' % ( size / nevts / 1000. ) if nevts else ''
-          if size:
-            for unit in sizeUnits:
-              if size < 1000.:
-                break
-              size /= 1000.
-          else:
-            size = 0
-            unit = ''
-          print '%s:%.3f %s %s' % ( 'Total size'.ljust( tab ), size, unit, sizePerEvt )
-        elif name == 'Luminosity':
-          lumi = value
-          if lumi:
-            for unit in lumiUnits:
-              if lumi < 1000.:
-                break
-              lumi /= 1000.
-          else:
-            lumi = 0
-            unit = ''
-          print '%s:%.3f %s' % ( 'Luminosity'.ljust( tab ), lumi, unit )
+    for name, value in zip( paramNames, records ):
+      if name == 'NbofFiles':
+        nfiles = value
+        print '%s: %s' % ( 'Nb of Files'.ljust( tab ), intWithQuotes( value ) )
+      elif name == 'NumberOfEvents':
+        nevts = value
+        print '%s: %s' % ( 'Nb of Events'.ljust( tab ), intWithQuotes( value ) )
+      elif name == 'FileSize':
+        size = value
+        sizePerEvt = '(%.1f kB per evt)' % ( size / nevts / 1000. ) if nevts and nDatasets == 1 else ''
+        if size:
+          for sizeUnit in sizeUnits:
+            if size < 1000.:
+              break
+            size /= 1000.
+        else:
+          size = 0
+          sizeUnit = ''
+        print '%s: %.3f %s %s' % ( 'Total size'.ljust( tab ), size, sizeUnit, sizePerEvt )
+      elif name == 'Luminosity':
+        lumi = value / float( nDatasets )
+        totalLumi = value
+        if lumi:
+          for lumiUnit in lumiUnits:
+            if lumi < 1000.:
+              break
+            lumi /= 1000.
+        else:
+          lumi = 0
+          lumiUnit = ''
+        lumiString = 'Luminosity' if nDatasets == 1 else 'Avg luminosity'
+        print '%s: %.3f %s' % ( lumiString.ljust( tab ), lumi, lumiUnit )
+      elif name == 'SizePerLumi':
+        print "%s: %.1f GB" % ( ( 'Size  per %s' % '/pb' ).ljust( tab ), value * 1000000. / 1000000000. )
+        if nDatasets != 1:
+          sizePerEvt = value / evtsPerLumi / 1000. if evtsPerLumi else 0.
+          print '%s: %.1f kB' % ( 'Avg size per evt'.ljust( tab ), sizePerEvt )
+      elif name == 'EvtsPerLumi':
+        evtsPerLumi = value
     if lumi:
       filesPerLumi = nfiles / lumi
-      print "%s:%.1f" % ( ( 'Files per %s' % unit ).ljust( tab ), filesPerLumi )
+      print "%s: %.1f" % ( ( 'Files per %s' % lumiUnit ).ljust( tab ), filesPerLumi )
     print ""
