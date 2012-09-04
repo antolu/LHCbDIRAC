d64f4796a23dd8701c80363703b2c10e80fb44c6
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-09-04 11:47:19 +0000

    FIX: refactored to remove nasty parts of the code
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@55788 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/SAMSystem/Modules/TestApplications.py
+++ LHCbDIRAC/SAMSystem/Modules/TestApplications.py
@@ -11,8 +11,9 @@ import sys
 
 import DIRAC
 
-from DIRAC                      import S_OK, S_ERROR, gConfig
-from DIRAC.Interfaces.API.Dirac import Dirac
+from DIRAC                                               import S_OK, S_ERROR, gConfig
+from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
+from DIRAC.Interfaces.API.Dirac                          import Dirac
 
 from LHCbDIRAC.Core.Utilities.CombinedSoftwareInstallation import getSharedArea
 from LHCbDIRAC.Core.Utilities.DetectOS                     import NativeMachine
@@ -75,7 +76,8 @@ class TestApplications( ModuleBaseSAM ):
     '''
 
     if not self.testName or not self.appNameVersion or not self.logFile or not self.appNameOptions:
-      self.result = S_ERROR( 'No application name / version defined' )
+      return self.finalize( 'No application name / version defined', 'Unknown', 'error' )
+      #self.result = S_ERROR( 'No application name / version defined' )
 
     self.log.info( 'Checking local system configuration is suitable to run the application test' )
     localArch = gConfig.getValue( '/LocalSite/Architecture', '' )
@@ -100,29 +102,35 @@ class TestApplications( ModuleBaseSAM ):
         self.appSystemConfig = localPlatforms[0]
 
     options = self.__getOptions( self.appNameVersion.split( '.' )[0], 
-                                 self.appNameVersion.split( '.' )[1], self.appNameOptions )
-    if not options['OK']:
+                                 self.appNameVersion.split( '.' )[1] )
+    if not options[ 'OK' ] :
       return self.finalize( 'Inputs for %s %s could not be found' % ( self.appNameVersion.split( '.' )[0], 
                                                                       self.appNameVersion.split( '.' )[1] ), 
-                                                                      options['Message'], 'critical' )
+                                                                      options[ 'Message' ], 'critical' )
 
     sys.stdout.flush()
     result = self.__runApplication( self.appNameVersion.split( '.' )[0], 
-                                    self.appNameVersion.split( '.' )[1], options['Value'] )
+                                    self.appNameVersion.split( '.' )[1], 
+                                    options[ 'Value' ] )
     sys.stdout.flush()
-    if not result['OK']:
-      return self.finalize( 'Failure during %s %s execution' % ( self.appNameVersion.split( '.' )[0], 
-                                                                 self.appNameVersion.split( '.' )[1] ), 
-                                                                 result['Message'], 'error' )
+    if not result[ 'OK' ]:
+      return self.finalize( 'Failure during %s %s execution' % ( self.appNameVersion.split( '.' )[ 0 ], 
+                                                                 self.appNameVersion.split( '.' )[ 1 ] ), 
+                                                                 result[ 'Message' ], 'error' )
 
     self.log.info( 'Test %s completed successfully' % self.testName )
     self.setApplicationStatus( '%s Successful' % self.testName )
     return self.finalize( '%s Test Successful' % self.testName, 'Status OK (= 10)', 'ok' )
 
-  def __getOptions( self, appName, appVersion, appOptions ):
-    """Method to set the correct options for the LHCb project that will be executed.
+  ##############################################################################
+  # Protected methods
+
+  def __getOptions( self, appName, appVersion ):
+    '''
+       Method to set the correct options for the LHCb project that will be executed.
        By convention the inputs / outputs are the system configuration + file extension.
-    """
+    '''
+    
     sharedArea = getSharedArea()
     if not sharedArea or not os.path.exists( sharedArea ):
       self.log.info( 'Could not determine sharedArea for site %s:\n%s' % ( DIRAC.siteName(), sharedArea ) )
@@ -130,70 +138,91 @@ class TestApplications( ModuleBaseSAM ):
     else:
       self.log.info( 'Software shared area for site %s is %s' % ( DIRAC.siteName(), sharedArea ) )
 
-    localOpts = appOptions
-
-    #Nasty but works:
     extraOpts = ''
+    
     if appName == 'Gauss':
-      extraOpts = """ApplicationMgr().EvtMax = 2;
-OutputStream("GaussTape").Output = "DATAFILE='PFN:%s/%s.sim' TYP='POOL_ROOTTREE' OPT='RECREATE'";
-""" % ( os.getcwd(), self.appSystemConfig )
+      extraOpts = 'ApplicationMgr().EvtMax = 2;\n'
+      extraOpts += '''OutputStream("GaussTape").Output = "DATAFILE='PFN:%s/%s.sim' '''
+      extraOpts += '''TYP='POOL_ROOTTREE' OPT='RECREATE'";'''
+      extraOpts = extraOpts % ( os.getcwd(), self.appSystemConfig )
+
     elif appName == 'Boole':
       if self.enable:
         if not os.path.exists( '%s.sim' % self.appSystemConfig ):
           return S_ERROR( 'No input file %s.sim found for Boole' % ( self.appSystemConfig ) )
-      extraOpts = """#Boole().useSpillover=False;
-EventSelector().Input = ["DATAFILE='PFN:%s/%s.sim' TYP='POOL_ROOTTREE' OPT='READ'"];
-OutputStream("DigiWriter").Output = "DATAFILE='PFN:%s/%s.digi' TYP='POOL_ROOTTREE' OPT='REC'";
-""" % ( os.getcwd(), self.appSystemConfig, os.getcwd(), self.appSystemConfig )
+
+      extraOpts = '#Boole().useSpillover=False;\n'
+      extraOpts += '''EventSelector().Input = ["DATAFILE='PFN:%s/%s.sim' TYP='POOL_ROOTTREE' OPT='READ'"];\n'''
+      extraOpts += '''OutputStream("DigiWriter").Output = "DATAFILE='PFN:%s/%s.digi' TYP='POOL_ROOTTREE' OPT='REC';'''
+      extraOpts = extraOpts % ( os.getcwd(), self.appSystemConfig, os.getcwd(), self.appSystemConfig )
+
     elif appName == 'Brunel':
       if self.enable:
         if not os.path.exists( '%s.digi' % self.appSystemConfig ):
           return S_ERROR( 'No input file %s.digi found for Brunel' % ( self.appSystemConfig ) )
-      extraOpts = """EventSelector().Input = ["DATAFILE='PFN:%s/%s.digi' TYP='POOL_ROOTTREE' OPT='READ'"];
-OutputStream("DstWriter").Output = "DATAFILE='PFN:%s/%s.dst' TYP='POOL_ROOTTREE' OPT='REC'";
-""" % ( os.getcwd(), self.appSystemConfig, os.getcwd(), self.appSystemConfig )
+      
+      extraOpts = '''EventSelector().Input = ["DATAFILE='PFN:%s/%s.digi' TYP='POOL_ROOTTREE' OPT='READ'"];\n'''
+      extraOpts += '''OutputStream("DstWriter").Output = "DATAFILE='PFN:%s/%s.dst' TYP='POOL_ROOTTREE' OPT='REC'";'''
+      extraOpts = extraOpts % ( os.getcwd(), self.appSystemConfig, os.getcwd(), self.appSystemConfig )
+    
     elif appName == 'DaVinci':
       if self.enable:
         if not os.path.exists( '%s.dst' % self.appSystemConfig ):
           return S_ERROR( 'No input file %s.dst found for DaVinci' % ( self.appSystemConfig ) )
-      extraOpts = """EventSelector().Input = ["DATAFILE='PFN:%s/%s.dst' TYP='POOL_ROOTTREE' OPT='READ'"];
-""" % ( os.getcwd(), self.appSystemConfig )
+      
+      extraOpts = '''EventSelector().Input = ["DATAFILE='PFN:%s/%s.dst' TYP='POOL_ROOTTREE' OPT='READ'"];'''
+      extraOpts = extraOpts % ( os.getcwd(), self.appSystemConfig )
+    
+      # This was done on purpose, I simply do not know now why  
+      appName = 'Gaudi'
 
     newOpts = '%s-Extra.py' % ( appName )
     self.log.verbose( 'Adding extra options for %s %s:\n%s' % ( appName, appVersion, extraOpts ) )
     fopen = open( newOpts, 'w' )
-    if appName == 'DaVinci':
-      fopen.write( '#\n# Options added by TestApplications for DIRAC SAM test %s\n#\nfrom %s.Configuration import *\n' % ( self.testName, 'Gaudi' ) )
-    else:
-      fopen.write( '#\n# Options added by TestApplications for DIRAC SAM test %s\n#\nfrom %s.Configuration import *\n' % ( self.testName, appName ) )
+    
+    _msg = '#\n# Options added by TestApplications for DIRAC SAM test %s\n#\nfrom %s.Configuration import *\n' 
+
+    fopen.write( _msg % ( self.testName, appName ) )
     fopen.write( extraOpts )
     fopen.close()
-    return S_OK( [localOpts, newOpts] )
+    
+    return S_OK( newOpts )
 
   def __runApplication( self, appName, appVersion, options ):
-    """Method to run a test job locally.
-    """
+    '''
+       Method to run a test job locally.
+    '''
+    
     result = S_OK()
-    from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
+    dirac  = Dirac()
+    
+    options = [ self.appNameOptions, options ]
+    
     try:
-      j = LHCbJob( stdout = self.logFile.replace( 'log', 'stdout' ), stderr = self.logFile.replace( 'log', 'stderr' ) )
+    
+      j = LHCbJob( stdout = self.logFile.replace( 'log', 'stdout' ), 
+                   stderr = self.logFile.replace( 'log', 'stderr' ) )
       j.setSystemConfig( self.appSystemConfig )
       j.setApplication( appName, appVersion, options, logFile = self.logFile, events = 2 )
       j.setName( '%s%sSAMTest' % ( appName, appVersion ) )
       j.setLogLevel( Operations().getValue( 'SAM/LogLevel', 'verbose' ) )
       j.setPlatform( Operations().getValue( 'SAM/Platform', 'gLite' ) )
+      
       self.log.verbose( 'Job JDL is:\n%s' % ( j._toJDL() ) )
-      dirac = Dirac()
+      
       if self.enable:
         result = dirac.submit( j, mode = 'local' )
         self.log.info( '%s %s execution result: %s' % ( appName, appVersion, result ) )
+        
       #Correct the log file name since it will have Step1_ prepended.
       if os.path.exists( 'Step1_%s' % self.logFile ):
         shutil.move( 'Step1_%s' % self.logFile, self.logFile )
+        
     except SystemError, x:
+      
       self.log.warn( 'Problem during %s %s execution: "%s"' % ( appName, appVersion, x ) )
       return S_ERROR( str( x ) )
+    
     return result
 
 ################################################################################
