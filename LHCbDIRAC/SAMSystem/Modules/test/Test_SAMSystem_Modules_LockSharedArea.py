61e09568d193c813edccf7b67dc63e83170931b9
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-09-10 12:06:03 +0000

    NEW: first unittest for SAM modules
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@56023 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/SAMSystem/Modules/test/Test_SAMSystem_Modules_LockSharedArea.py
@@ -0,0 +1,44 @@
+# $HeadURL: $
+''' Test_SAMSystem_Modules_LockSharedArea
+'''
+
+import mock
+import unittest
+
+import LHCbDIRAC.SAMSystem.Modules.LockSharedArea as moduleTested 
+
+__RCSID__ = '$Id: $'
+
+################################################################################
+
+class LockSharedArea_TestCase( unittest.TestCase ):
+  
+  def setUp( self ):
+    '''
+    Setup
+    '''
+
+    self.moduleTested = moduleTested
+    self.testClass    = self.moduleTested.LockSharedArea
+
+  def tearDown( self ):
+    '''
+    Tear down
+    '''
+    
+    del self.moduleTested
+    del self.testClass
+
+################################################################################
+
+class LockSharedArea_Success( LockSharedArea_TestCase ):
+  
+  def test_instantiate( self ):
+    ''' tests that we can instantiate one object of the tested class
+    '''  
+    
+    module = self.testClass()
+    self.assertEqual( 'LockSharedArea', module.__class__.__name__ )
+
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
