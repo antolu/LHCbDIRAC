5007b93f01b3242ca876b3afe314eefdb69b82e3
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2012-11-20 16:42:22 +0000

    LHCBDIRAC-35: New regression tests
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@58709 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Workflow/test/RegressionTest/Test_Regression.py
+++ LHCbDIRAC/Workflow/test/RegressionTest/Test_Regression.py
@@ -1,20 +1,26 @@
 import unittest, os, shutil
 from DIRAC import gLogger
+from DIRAC.Core.Base.Script import parseCommandLine
 
 from LHCbDIRAC.Interfaces.API.LHCbJob import LHCbJob
 from LHCbDIRAC.Interfaces.API.DiracLHCb import DiracLHCb
 from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient import BookkeepingClient
+
 
 class RegressionTestCase( unittest.TestCase ):
   """ Base class for the Modules test cases
   """
   def setUp( self ):
 
-    gLogger.setLevel( 'INFO' )
+    parseCommandLine()
+
+    gLogger.setLevel( 'DEBUG' )
     self.diracLHCb = DiracLHCb()
     self.bkkClient = BookkeepingClient()
 
-    self.j_mc_201060 = LHCbJob( './RegressionTest/20160.xml' )
+    self.j_mc_20160 = LHCbJob( '20160.xml' )
+    self.j_mc_20194 = LHCbJob( '20194.xml' )
+    self.j_mc_20349 = LHCbJob( '20349.xml' )
 
 
   def tearDown( self ):
@@ -30,8 +36,19 @@ class RegressionTestCase( unittest.TestCase ):
 
 class MCSuccess( RegressionTestCase ):
   def test_execute( self ):
-    res = self.j_mc_201060.runLocal( self.diracLHCb, self.bkkClient )
+    res = self.j_mc_20160.runLocal( self.diracLHCb, self.bkkClient )
     self.assertTrue( res['OK'] )
+
+class RecoSuccess( RegressionTestCase ):
+  def test_execute( self):
+    res = self.j_mc_20194.runLocal( self.diracLHCb, self.bkkClient )
+    self.assertTrue( res['OK'] )
+
+class StrippSuccess( RegressionTestCase ):
+  def test_execute( self):
+    res = self.j_mc_20349.runLocal( self.diracLHCb, self.bkkClient )
+    self.assertTrue( res['OK'] )
+
 
 #############################################################################
 # Test Suite run
@@ -40,4 +57,6 @@ class MCSuccess( RegressionTestCase ):
 if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase( RegressionTestCase )
   suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( MCSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( RecoSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( StrippSuccess ) )
   testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
