43b3703a5bc851b30e576465e150338bcc1085b9
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2012-12-06 14:24:01 +0000

    FIX: Improve formatting (less spaces), add #events CHANGE: New switch --Full to get all metadata CHANGE: allow more than one LFN/File in arguments
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@59448 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-file-metadata.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-file-metadata.py
@@ -8,61 +8,87 @@
 """
 __RCSID__ = "$Id$"
 import  DIRAC.Core.Base.Script as Script
-Script.setUsageMessage('\n'.join([ __doc__.split('\n')[1],
+Script.registerSwitch( '', 'Full', '   Print out all metadata' )
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      'Usage:',
                                      '  %s [option|cfgfile] ... LFN|File' % Script.scriptName,
                                      'Arguments:',
                                      '  LFN:      Logical File Name',
-                                     '  File:     Name of the file with a list of LFNs' ]))
+                                     '  File:     Name of the file with a list of LFNs' ] ) )
 Script.parseCommandLine()
 
 import DIRAC
 from DIRAC.Core.DISET.RPCClient import RPCClient
 
+full = False
+switches = Script.getUnprocessedSwitches()
+for switch in switches:
+  if switch[0] == 'Full':
+    full = True
+
 args = Script.getPositionalArgs()
 
-if len(args) != 1:
+if len( args ) == 0:
   Script.showHelp()
 
-fileName = args[0]
+fileNames = args
 lfns = []
-try:
-  files = open(fileName)
-  for f in files:
-    lfns += [f.strip()]
-except Exception, ex:
-  lfns = [fileName]
+for fileName in fileNames:
+  try:
+    files = open( fileName )
+    for f in files:
+      lfns += [f.strip()]
+  except Exception, ex:
+    lfns.append( fileName )
 
-client = RPCClient('Bookkeeping/BookkeepingManager')
-res = client.getFileMetadata(lfns)
+client = RPCClient( 'Bookkeeping/BookkeepingManager' )
+res = client.getFileMetadata( lfns )
 if not res['OK']:
   print 'ERROR: Failed to get file metadata: %s' % res['Message']
-  DIRAC.exit(2)
+  DIRAC.exit( 2 )
 
 exitCode = 0
 
-print '%s %s %s %s %s %s' % ('FileName'.ljust(100),
-                             'Size'.ljust(10),
-                             'GUID'.ljust(40),
-                             'Replica'.ljust(10),
-                             'DataQuality'.ljust(10),
-                             'RunNumber'.ljust(10))
+lenName = 0
+for lfn in lfns:
+  lenName = max( lenName, len( lfn ) )
+lenName += 2
+lenGUID = 38
+lenItem = 15
+sep = ''
+
+if not full:
+  print '%s %s %s %s %s %s %s' % ( 'FileName'.ljust( lenName ),
+                                'Size'.ljust( 10 ),
+                                'GUID'.ljust( lenGUID ),
+                                'Replica'.ljust( 8 ),
+                                'DataQuality'.ljust( 12 ),
+                                'RunNumber'.ljust( 10 ),
+                                '#events'.ljust( 10 ) )
 for lfn in res['Value'].keys():
   dict = res['Value'][lfn]
-  size = dict['FileSize']
-  guid = dict['GUID']
-  gotReplica = dict['GotReplica']
-  dq = dict['DQFlag']
-  run = dict['RunNumber']
-  if not gotReplica:
-    gotReplica = 'No'
-  print  '%s %s %s %s %s %s' % (lfn.ljust(100),
-                                str(size).ljust(10),
-                                guid.ljust(40),
-                                gotReplica.ljust(10),
-                                dq.ljust(10),
-                                str(run).ljust(10))
-  lfns.remove(lfn)
+  if full:
+    print '%s%s %s' % ( sep, 'FileName'.ljust( lenItem ), lfn )
+    sep = '\n'
+    for item in sorted( dict ):
+      print '%s %s' % ( item.ljust( lenItem ), dict[item] )
+  else:
+    size = dict['FileSize']
+    guid = dict['GUID']
+    gotReplica = dict['GotReplica']
+    dq = dict['DQFlag']
+    run = dict['RunNumber']
+    evtStat = dict['EventStat']
+    if not gotReplica:
+      gotReplica = 'No'
+    print  '%s %s %s %s %s %s %s' % ( lfn.ljust( lenName ),
+                                   str( size ).ljust( 10 ),
+                                   guid.ljust( lenGUID ),
+                                   gotReplica.ljust( 8 ),
+                                   dq.ljust( 12 ),
+                                   str( run ).ljust( 10 ),
+                                   str( evtStat ).ljust( 10 ) )
+  lfns.remove( lfn )
 
 if lfns:
   print '\n'
@@ -71,5 +97,5 @@ for lfn in lfns:
     print '%s does not exist in the Bookkeeping.' % lfn
     exitCode = 2
 
-DIRAC.exit(exitCode)
+DIRAC.exit( exitCode )
 
