8b3e482ab66da783a90362d7a425ad6dcd57c4c9
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2012-12-06 16:42:07 +0000

    re-ordering
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@59454 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/RequestTrackingAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/RequestTrackingAgent.py
@@ -4,86 +4,46 @@
     productions and the number of Output Events for all productions.
 '''
 
-from DIRAC.Core.Base.AgentModule    import AgentModule
-from DIRAC.Core.DISET.RPCClient     import RPCClient
-from DIRAC                          import S_OK, S_ERROR, gLogger
+from DIRAC                                                import S_OK, S_ERROR, gLogger
+from DIRAC.Core.Base.AgentModule                          import AgentModule
+from DIRAC.Core.DISET.RPCClient                           import RPCClient
+from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient import BookkeepingClient
 
 __RCSID__ = "$Id$"
-
-def bkProductionProgress( iD, setup ):
-  RPC = RPCClient( 'Bookkeeping/BookkeepingManager', setup=setup )
-  return RPC.getProductionProcessedEvents( int( iD ) )
-
-def bkInputNumberOfEvents( r, setup ):
-  """ Extrim dirty way... But I DO NOT KNOW OTHER !!! """
-  try:
-    v = {
-      'ProcessingPass' : str( r['inProPass'] ),
-      'FileType' : str( r['inFileType'] ),
-      'EventType' : str( r['EventType'] ),
-      'ConfigName' : str( r['configName'] ),
-      'ConfigVersion' : str( r['configVersion'] ),
-      'DataQualityFlag' : str( r['inDataQualityFlag'] )
-      }
-    if r['condType'] == 'Run':
-      v['DataTakingConditions'] = str( r['SimCondition'] )
-    else:
-      v['SimulationConditions'] = str( r['SimCondition'] )
-    if str( r['inProductionID'] ) != '0':
-      v['Production'] = [int( x ) for x in str( r['inProductionID'] ).split( ',' )]
-    if 'inTCKs' in r and str( r['inTCKs'] ) != '':
-      v['TCK'] = [str( x ) for x in str( r['inTCKs'] ).split( ',' )]
-  except Exception, e:
-    return S_ERROR( "Can not parse the request: %s" % str( e ) )
-  RPC = RPCClient( 'Bookkeeping/BookkeepingManager', setup=setup )
-  v['NbOfEvents'] = True
-  result = RPC.getFiles( v )
-  if not result['OK']:
-    return result
-  if not result['Value'][0]:
-    return S_OK( 0 )
-  try:
-    sum_nr = long( result['Value'][0] )
-  except Exception, e:
-    return S_ERROR( "Can not convert result from BK call: %s" % str( e ) )
-  return S_OK( sum_nr )
 
 AGENT_NAME = 'ProductionManagement/RequestTrackingAgent'
 
 class RequestTrackingAgent( AgentModule ):
 
+  def __init__( self, agentName, loadName, baseAgentName = False, properties = dict() ):
+    ''' c'tor
+
+    :param self: self reference
+    :param str agentName: name of agent
+    :param bool baseAgentName: whatever
+    :param dict properties: whatever else
+    '''
+    AgentModule.__init__( self, agentName, loadName, baseAgentName, properties )
+
+    self.bkClient = BookkeepingClient()
+    self.prodReq = RPCClient( "ProductionManagement/ProductionRequest" )
+
   def initialize( self ):
-    """Sets defaults"""
+    '''Sets defaults
+    '''
     self.pollingTime = self.am_getOption( 'PollingTime', 1200 )
-    self.setup = self.am_getOption( 'Setup', '' )
     return S_OK()
 
-  def getTrackedProductions( self ):
-    RPC = RPCClient( "ProductionManagement/ProductionRequest", setup=self.setup )
-    return RPC.getTrackedProductions()
-
-  def updateTrackedProductions( self, update ):
-    RPC = RPCClient( "ProductionManagement/ProductionRequest", setup=self.setup )
-    return RPC.updateTrackedProductions( update )
-
-  def getTrackedInput( self ):
-    RPC = RPCClient( "ProductionManagement/ProductionRequest", setup=self.setup )
-    return RPC.getTrackedInput()
-
-  def updateTrackedInput( self, update ):
-    RPC = RPCClient( "ProductionManagement/ProductionRequest", setup=self.setup )
-    return RPC.updateTrackedInput( update )
-
   def execute( self ):
-    """The RequestTrackingAgent execution method.
-    """
+    '''The RequestTrackingAgent execution method.
+    '''
     gLogger.info( 'Request Tracking execute is started' )
 
-    result = self.getTrackedInput()
+    result = self.prodReq.getTrackedInput()
     update = []
     if result['OK']:
       for request in result['Value']:
-        result = bkInputNumberOfEvents( request, self.setup )
+        result = self.bkInputNumberOfEvents( request, self.bkClient )
         if result['OK']:
           update.append( {'RequestID':request['RequestID'],
                          'RealNumberOfEvents':result['Value']} )
@@ -93,15 +53,15 @@ class RequestTrackingAgent( AgentModule ):
     else:
       gLogger.error( 'Request service: %s' % result['Message'] )
     if update:
-      result = self.updateTrackedInput( update )
+      result = self.prodReq.updateTrackedInput( update )
       if not result['OK']:
         gLogger.error( result['Message'] )
 
-    result = self.getTrackedProductions()
+    result = self.self.prodReq.getTrackedProductions()
     update = []
     if result['OK']:
       for productionID in result['Value']:
-        result = bkProductionProgress( int( productionID ), self.setup )
+        result = self.bkClient.getProductionProcessedEvents( productionID )
         if result['OK']:
           if result['Value']:
             update.append( {'ProductionID':productionID, 'BkEvents':result['Value']} )
@@ -112,12 +72,47 @@ class RequestTrackingAgent( AgentModule ):
       gLogger.error( 'Request service: %s' % result['Message'] )
 
     if update:
-      result = self.updateTrackedProductions( update )
+      result = self.prodReq.updateTrackedProductions( update )
       if not result['OK']:
         gLogger.error( result['Message'] )
 
     gLogger.info( 'Request Tracking execute is ended' )
     return S_OK( 'Request Tracking information updated' )
+
+  def bkInputNumberOfEvents( self, r ):
+    ''' Extrim dirty way...
+    '''
+
+    try:
+      v = {
+        'ProcessingPass' : str( r['inProPass'] ),
+        'FileType' : str( r['inFileType'] ),
+        'EventType' : str( r['EventType'] ),
+        'ConfigName' : str( r['configName'] ),
+        'ConfigVersion' : str( r['configVersion'] ),
+        'DataQualityFlag' : str( r['inDataQualityFlag'] )
+        }
+      if r['condType'] == 'Run':
+        v['DataTakingConditions'] = str( r['SimCondition'] )
+      else:
+        v['SimulationConditions'] = str( r['SimCondition'] )
+      if str( r['inProductionID'] ) != '0':
+        v['Production'] = [int( x ) for x in str( r['inProductionID'] ).split( ',' )]
+      if 'inTCKs' in r and str( r['inTCKs'] ) != '':
+        v['TCK'] = [str( x ) for x in str( r['inTCKs'] ).split( ',' )]
+    except Exception, e:
+      return S_ERROR( "Can not parse the request: %s" % str( e ) )
+    v['NbOfEvents'] = True
+    result = self.bkClient.getFiles( v )
+    if not result['OK']:
+      return result
+    if not result['Value'][0]:
+      return S_OK( 0 )
+    try:
+      sum_nr = long( result['Value'][0] )
+    except Exception, e:
+      return S_ERROR( "Can not convert result from BK call: %s" % str( e ) )
+    return S_OK( sum_nr )
 
 ################################################################################
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
