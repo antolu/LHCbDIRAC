87d105f3dc3f67a7e5f50214a58247d0e547adec
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-12-17 10:05:06 +0000

    NEW: added first test for PopularityPlotter
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@59945 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/AccountingSystem/private/Plotters/test/Test_AS_private_Plotters_PopularityPlotter.py
@@ -0,0 +1,85 @@
+'''
+  Unittest for:
+    LHCbDIRAC.AccountingSystem.private.Plotters.PopularityPlotter
+    
+  PopularityPlotter.__bases__:
+    DIRAC.AccountingSystem.private.Plotters.BaseReporter  
+  
+  We are assuming there is a solid test of __bases__, we are not testing them
+  here and assuming they work fine.
+  
+  IMPORTANT: the test MUST be pylint compliant !  
+'''
+
+import mock
+import unittest
+
+class PopularityPlotterTestCase( unittest.TestCase ):
+  '''
+    PopularityPlotterTestCase
+  '''
+
+  moduleTested = None
+  classsTested = None
+  
+  def mockModuleTested( self, moduleTested ):
+    '''
+      Used to not redo the mocking done on the parent class ( if any )
+    '''
+    
+    # Tries to get the mocks of the parent TestCases ( if any )
+    for baseClass in self.__class__.__bases__:
+      try:
+        #pylint: disable=E1101
+        moduleTested = baseClass.mockModuleTested( moduleTested )
+      except TypeError:
+        continue  
+    
+    # And then makes its own mock
+    class MockPopularity:
+      #pylint: disable=C0111,R0903,W0232
+      definitionKeyFields = ( 'JobGroup', 'RunNumber', 'EventType', 'ProcessingType',
+                              'ProcessingStep', 'Site', 'FinalStepState' )
+    
+    moduleTested.Popularity = mock.Mock( return_value = MockPopularity() )
+    
+    return moduleTested
+    
+  def setUp( self ):
+    '''
+      Setup the test case
+    '''
+    
+    import LHCbDIRAC.AccountingSystem.private.Plotters.PopularityPlotter as moduleTested
+    
+    self.moduleTested = self.mockModuleTested( moduleTested )
+    self.classsTested = self.moduleTested.PopularityPlotter
+    
+  def tearDown( self ):
+    '''
+      Tear down the test case
+    '''
+    
+    del self.moduleTested
+    del self.classsTested  
+  
+#...............................................................................  
+
+class PopularityPlotterUnitTest( PopularityPlotterTestCase ):
+  '''
+    PopularityPlotterUnitTest
+    <constructor>
+     - test_instantiate
+    <class variables>
+    <methods> 
+  '''
+
+  def test_instantiate( self ):
+    ''' tests that we can instantiate one object of the tested class
+    '''     
+    obj = self.classsTested( None, None )
+    self.assertEqual( 'PopularityPlotter', obj.__class__.__name__,
+                      msg = 'Expected PopularityPlotter object' )
+    
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
