db868c4df2298efe34743b56d14127cd9fe0c1cd
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2012-12-17 09:48:45 +0000

    FIX: more readable PopularityPlotter
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@59941 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/AccountingSystem/private/Plotters/PopularityPlotter.py
+++ LHCbDIRAC/AccountingSystem/private/Plotters/PopularityPlotter.py
@@ -1,81 +1,151 @@
-__RCSID__ = "$Id: $"
+''' LHCbDIRAC.AccountingSystem.private.Plotters.PopularityPlotter
 
-from DIRAC import S_OK, S_ERROR, gLogger
-from LHCbDIRAC.AccountingSystem.Client.Types.Popularity import Popularity
+   PopularityPlotter.__bases__:
+     DIRAC.AccountingSystem.private.Plotters.BaseReporter.BaseReporter
+  
+'''
+
+from DIRAC                                                import S_OK
 from DIRAC.AccountingSystem.private.Plotters.BaseReporter import BaseReporter
-from DIRAC.Core.Utilities import Time
+
+from LHCbDIRAC.AccountingSystem.Client.Types.Popularity import Popularity
+
+__RCSID__ = '$Id$'
 
 class PopularityPlotter( BaseReporter ):
-
-  _typeName = "Popularity"
-  _typeKeyFields = [ dF[0] for dF in Popularity().definitionKeyFields ]
+  '''
+    PopularityPlotter as extension of BaseReporter
+  '''
+  
+  _typeName          = "Popularity"
+  _typeKeyFields     = [ dF[0] for dF in Popularity().definitionKeyFields ]
   _noSEtypeKeyFields = [ dF[0] for dF in Popularity().definitionKeyFields ]
-  _noSEGrouping = ( ", ".join( "%s" for f in _noSEtypeKeyFields ), _noSEtypeKeyFields )
+  _noSEGrouping      = ( ", ".join( "%s" for f in _noSEtypeKeyFields ), _noSEtypeKeyFields )
 
-  ###
+  #.............................................................................
+  # data Usage
+  
   _reportDataUsageName = "Data Usage"
   def _reportDataUsage( self, reportRequest ):
-    selectFields = ( self._getSelectStringForGrouping( reportRequest[ 'groupingFields' ] ) + ", %s, %s, SUM(%s)/SUM(%s)",
-                     reportRequest[ 'groupingFields' ][1] + [ 'startTime', 'bucketLength',
-                                    'Usage', 'entriesInBucket'
-                                   ]
-                   )
+    '''
+      Reports the data usage from the Accounting DB.
+    '''
+    
+    _selectString = self._getSelectStringForGrouping( reportRequest[ 'groupingFields' ] )
+    selectFields  = ( _selectString + ", %s, %s, SUM(%s)/SUM(%s)",
+                      reportRequest[ 'groupingFields' ][1] + [ 'startTime', 'bucketLength',
+                                                               'Usage', 'entriesInBucket'
+                                                             ]
+                    )
+    
     retVal = self._getTimedData( reportRequest[ 'startTime' ],
-                                reportRequest[ 'endTime' ],
-                                selectFields,
-                                reportRequest[ 'condDict' ],
-                                PopularityPlotter._noSEGrouping,
-                                { 'convertToGranularity' : 'sum', 'checkNone' : True } )
+                                 reportRequest[ 'endTime' ],
+                                 selectFields,
+                                 reportRequest[ 'condDict' ],
+                                 PopularityPlotter._noSEGrouping,
+                                 { 'convertToGranularity' : 'sum', 'checkNone' : True } )
     if not retVal[ 'OK' ]:
       return retVal
+    
     dataDict, granularity = retVal[ 'Value' ]
+    #FIXME: why is this used ?
     self.stripDataField( dataDict, 0 )
-    baseDataDict, graphDataDict, maxValue, unitName = self._findSuitableUnit( dataDict,
-                                                                              self._getAccumulationMaxValue( dataDict ),
-                                                                              "files" )
-    return S_OK( { 'data' : baseDataDict, 'graphDataDict' : graphDataDict,
-                   'granularity' : granularity, 'unit' : unitName } )
+    
+    accumMaxValue = self._getAccumulationMaxValue( dataDict )
+    suitableUnits = self._findSuitableUnit( dataDict, accumMaxValue, "files" )
+    
+    #3rd value, maxValue is not used
+    baseDataDict, graphDataDict, __, unitName = suitableUnits
+     
+    return S_OK( { 'data'          : baseDataDict, 
+                   'graphDataDict' : graphDataDict,
+                   'granularity'   : granularity, 
+                   'unit'          : unitName 
+                  } )
 
   def _plotDataUsage( self, reportRequest, plotInfo, filename ):
-    metadata = { 'title' : "Data Usage grouped by %s" % reportRequest[ 'grouping' ],
-                 'starttime' : reportRequest[ 'startTime' ],
-                 'endtime' : reportRequest[ 'endTime' ],
-                 'span' : plotInfo[ 'granularity' ],
-                 'ylabel' : plotInfo[ 'unit' ] }
-    plotInfo[ 'graphDataDict' ] = self._fillWithZero( plotInfo[ 'granularity' ], reportRequest[ 'startTime' ], reportRequest[ 'endTime' ], plotInfo[ 'graphDataDict' ] )
-    return self._generateStackedLinePlot( filename, plotInfo[ 'graphDataDict' ], metadata )
+    '''
+      Plots the data Usage statistics.
+    '''
+    
+    granularity = plotInfo[ 'granularity' ]
+    startEpoch  = reportRequest[ 'startTime' ]
+    endEpoch    = reportRequest[ 'endTime' ]
+    dataDict    = plotInfo[ 'graphDataDict' ]
+    
+    metadata = {
+                 'title'     : "Data Usage grouped by %s" % reportRequest[ 'grouping' ],
+                 'starttime' : startEpoch,
+                 'endtime'   : endEpoch,
+                 'span'      : granularity,
+                 'ylabel'    : plotInfo[ 'unit' ] 
+                }
+    
+    dataDict = self._fillWithZero( granularity, startEpoch, endEpoch, dataDict )
+    
+    return self._generateStackedLinePlot( filename, dataDict, metadata )
 
-  ###
+  #.............................................................................
+  # normalized data Usage
 
   _reportNormalizedDataUsageName = "Normalized Data Usage"
   def _reportNormalizedDataUsage( self, reportRequest ):
-    selectFields = ( self._getSelectStringForGrouping( reportRequest[ 'groupingFields' ] ) + ", %s, %s, SUM(%s)/SUM(%s)",
+    '''
+      Reports the normalized data usage from the Accounting DB.
+    '''
+    
+    _selectString = self._getSelectStringForGrouping( reportRequest[ 'groupingFields' ] )
+    selectFields = ( _selectString + ", %s, %s, SUM(%s)/SUM(%s)",
                      reportRequest[ 'groupingFields' ][1] + [ 'startTime', 'bucketLength',
-                                    'NormalizedUsage', 'entriesInBucket'
-                                   ]
+                                                              'NormalizedUsage', 'entriesInBucket'
+                                                            ]
                    )
     retVal = self._getTimedData( reportRequest[ 'startTime' ],
-                                reportRequest[ 'endTime' ],
-                                selectFields,
-                                reportRequest[ 'condDict' ],
-                                PopularityPlotter._noSEGrouping,
-                                { 'convertToGranularity' : 'sum', 'checkNone' : True } )
+                                 reportRequest[ 'endTime' ],
+                                 selectFields,
+                                 reportRequest[ 'condDict' ],
+                                 PopularityPlotter._noSEGrouping,
+                                 { 'convertToGranularity' : 'sum', 'checkNone' : True } )
     if not retVal[ 'OK' ]:
       return retVal
+    
     dataDict, granularity = retVal[ 'Value' ]
+    #FIXME: why is this used ?
     self.stripDataField( dataDict, 0 )
-    baseDataDict, graphDataDict, maxValue, unitName = self._findSuitableUnit( dataDict,
-                                                                              self._getAccumulationMaxValue( dataDict ),
-                                                                              "files" )
-    return S_OK( { 'data' : baseDataDict, 'graphDataDict' : graphDataDict,
-                   'granularity' : granularity, 'unit' : unitName } )
+    
+    accumMaxValue = self._getAccumulationMaxValue( dataDict )
+    suitableUnits = self._findSuitableUnit( dataDict, accumMaxValue, "files" )
+    
+    #3rd value, maxValue is not used
+    baseDataDict, graphDataDict, __, unitName = suitableUnits 
+    
+    return S_OK( { 'data'          : baseDataDict, 
+                   'graphDataDict' : graphDataDict,
+                   'granularity'   : granularity, 
+                   'unit'          : unitName 
+                  } )
 
   def _plotNormalizedDataUsage( self, reportRequest, plotInfo, filename ):
-    metadata = { 'title' : "Normalized Data Usage grouped by %s" % reportRequest[ 'grouping' ],
-                 'starttime' : reportRequest[ 'startTime' ],
-                 'endtime' : reportRequest[ 'endTime' ],
-                 'span' : plotInfo[ 'granularity' ],
-                 'ylabel' : plotInfo[ 'unit' ] }
-    plotInfo[ 'graphDataDict' ] = self._fillWithZero( plotInfo[ 'granularity' ], reportRequest[ 'startTime' ], reportRequest[ 'endTime' ], plotInfo[ 'graphDataDict' ] )
-    return self._generateStackedLinePlot( filename, plotInfo[ 'graphDataDict' ], metadata )
+    '''
+      Plots the normalized data usage.
+    '''
+    
+    granularity = plotInfo[ 'granularity' ]
+    startEpoch  = reportRequest[ 'startTime' ]
+    endEpoch    = reportRequest[ 'endTime' ]
+    dataDict    = plotInfo[ 'graphDataDict' ]
+    
+    metadata = { 
+                 'title'     : "Normalized Data Usage grouped by %s" % reportRequest[ 'grouping' ],
+                 'starttime' : startEpoch,
+                 'endtime'   : endEpoch,
+                 'span'      : granularity,
+                 'ylabel'    : plotInfo[ 'unit' ] 
+                }
+    
+    dataDict = self._fillWithZero( granularity, startEpoch, endEpoch, dataDict )
+    
+    return self._generateStackedLinePlot( filename, dataDict, metadata )
 
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
