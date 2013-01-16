505c19864bee5df3a431e99c248903b9954eb254
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-01-16 10:50:11 +0000

    NEW: merged Published wrote by Andrew
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@60554 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/ResourceStatusSystem/Service/__init__.py
@@ -0,0 +1,7 @@
+'''  LHCbDIRAC.ResourceStatusSystem.Service package
+'''
+
+__RCSID__  = '$Id:  $'
+
+################################################################################
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
