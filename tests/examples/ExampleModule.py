83d081bb3250c5244f7beb4db5c5646eed58127b
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-02-05 16:53:34 +0000

    NEW: added mock examples for agent base classes
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/TestLHCbDIRAC/trunk/TestLHCbDIRAC@61421 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/examples/ExampleModule.py
@@ -0,0 +1,11 @@
+''' ExampleModule 
+'''
+
+__RCSID__ = "$Id:$"
+
+from DIRAC.Core.Base.AgentModule import AgentModule
+
+class ExampleAgent( AgentModule ): pass
+
+#...............................................................................
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
