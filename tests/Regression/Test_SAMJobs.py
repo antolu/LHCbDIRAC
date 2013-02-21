a88497bab868b8cc207a32b5ffb9c177b2721ec7
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-02-21 11:55:31 +0000

    Added Test_SAMJobs
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@62019 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Regression/Test_SAMJobs.py
@@ -0,0 +1,34 @@
+from DIRAC.Core.Base.Script import parseCommandLine
+parseCommandLine()
+
+import unittest
+
+from TestLHCbDIRAC.Regression.utils import cleanTestDir
+
+from DIRAC import gLogger, gConfig
+
+from LHCbDIRAC.SAMSystem.Client.DiracSAM import DiracSAM
+
+class SAMJobTestCase( unittest.TestCase ):
+  ''' Base class for the SAMJob test cases
+  '''
+  def setUp( self ):
+    cleanTestDir()
+
+    gLogger.setLevel( 'DEBUG' )
+    self.diracSAM = DiracSAM()
+    self.subLogger = gLogger.getSubLogger( __file__ )
+
+    self.ce = gConfig.getValue( 'LocalSite/GridCE' )
+    self.subLogger.debug( self.ce )
+
+class SAMSuccess( SAMJobTestCase ):
+  def test_execute( self ):
+
+    result = self.diracSAM.submitSAMJob( ce = self.ce, mode = 'local' )
+    print result
+
+if __name__ == '__main__':
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase( SAMJobTestCase )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( SAMSuccess ) )
+  testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
