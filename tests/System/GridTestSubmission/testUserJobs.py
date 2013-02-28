59afddc594524bea7405e24658944a926c9a7a27
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-02-28 14:31:09 +0000

    added pConf
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@62533 4525493e-7705-40b1-a816-d608a930855b

--- tests/System/GridTestSubmission/testUserJobs.py
+++ tests/System/GridTestSubmission/testUserJobs.py
@@ -24,7 +24,7 @@ helloJ.setInputSandbox( [cwd + '/exe-script.py'] )
 
 helloJ.setExecutable( "exe-script.py", "", "helloWorld.log" )
 
-helloJ.setCPUTime( 172800 )
+helloJ.setCPUTime( 17800 )
 helloJ.setBannedSites( ['LCG.CERN.ch', 'LCG.CNAF.it', 'LCG.GRIDKA.de', 'LCG.IN2P3.fr',
                         'LCG.NIKHEF.nl', 'LCG.PIC.es', 'LCG.RAL.uk', 'LCG.SARA.nl'] )
 result = dirac.submit( helloJ )
@@ -39,27 +39,39 @@ dirac = DiracLHCb()
 
 helloJ.setName( "helloWorld-test-CERN" )
 helloJ.setInputSandbox( [cwd + '/exe-script.py'] )
-
 helloJ.setExecutable( "exe-script.py", "", "helloWorld.log" )
 
-helloJ.setCPUTime( 172800 )
+helloJ.setCPUTime( 17800 )
 helloJ.setDestination( 'LCG.CERN.ch' )
 result = dirac.submit( helloJ )
 gLogger.info( "Hello world job: ", result )
 
 ########################################################################################
 
-# gLogger.info( "Submitting gaudiRun job (Gauss only)" )
-#
-# gaudiRunJ = LHCbJob()
-#
-# gaudiRunJ.setName( "gauss GaudiRun Job" )
-# gaudiRunJ.setInputSandbox()
-# gaudiRunJ.setOutputSandbox()
-# gaudiRunJ.setApplicationScript()
-# gaudiRunJ.setAncestorDepth( 0 )
-# gaudiRunJ.setSystemConfig( 'x86_64-slc5-gcc43-opt' )
-# gaudiRunJ.setCPUTime( 172800 )
-#
-# result = dirac.submit( gaudiRunJ )
-# gLogger.info( 'Submission Result: ', result )
+gLogger.info( "Submitting gaudiRun job (Gauss only)" )
+
+gaudirunJob = LHCbJob()
+
+gaudirunJob.setName( "gaudirun-Gauss-test" )
+gaudirunJob.setInputSandbox( 'prodConf_Gauss_00012345_00067890_1.py' )
+gaudirunJob.setOutputSandbox( '00012345_00067890_1.sim' )
+
+optGauss = "$APPCONFIGOPTS/Gauss/Beam4000GeV-md100-JulSep2012-nu2.5.py;"
+optDec = "$DECFILESROOT/options/15512012.py;"
+optPythia = "$LBPYTHIAROOT/options/Pythia.py;"
+optOpts = " $APPCONFIGOPTS/Gauss/G4PL_LHEP_EmNoCuts.py;"
+optCompr = "$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py;"
+optPConf = "prodConf_Gauss_00012345_00067890_1.py"
+options = optGauss + optDec + optPythia + optOpts + optCompr + optPConf
+gaudirunJob.addPackage( 'AppConfig', 'v3r160' )
+gaudirunJob.addPackage( 'DecFiles', 'v26r24' )
+gaudirunJob.addPackage( 'ProdConf', 'v1r9' )
+gaudirunJob.setApplication( 'Gauss', 'v42r4', options, extraPackages = 'AppConfig.v3r160;DecFiles.v26r24;ProdConf.v1r9' )
+
+gaudirunJob.setSystemConfig( 'ANY' )
+gaudirunJob.setCPUTime( 172800 )
+
+result = dirac.submit( gaudirunJob )
+gLogger.info( 'Submission Result: ', result )
+
+########################################################################################
