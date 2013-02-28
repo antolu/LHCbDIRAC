58bce353add69f404946fa085614d1ac4e6744fb
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-02-28 13:33:03 +0000

    pConfGauss file
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@62528 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Regression/prodConf_Gauss_00012345_00067890_1.py
@@ -0,0 +1,15 @@
+from ProdConf import ProdConf
+
+ProdConf(
+  NOfEvents=2,
+  DDDBTag='dddb-20120831',
+  AppVersion='v42r4',
+  XMLSummaryFile='summaryGauss_00012345_00067890_1.xml',
+  Application='Gauss',
+  OutputFilePrefix='00012345_00067890_1',
+  RunNumber=2308595,
+  XMLFileCatalog='pool_xml_catalog.xml',
+  FirstEventNumber=518801,
+  CondDBTag='sim-20121025-vc-md100',
+  OutputFileTypes=['sim'],
+)
