1ec7d06329736ea8688d79820e35cd18127e9919
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-02-28 08:57:40 +0000

    added gaudirun job
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@62457 4525493e-7705-40b1-a816-d608a930855b

--- tests/Regression/Test_UserJobs.py
+++ tests/Regression/Test_UserJobs.py
@@ -27,14 +27,29 @@ class UserJobTestCase( unittest.TestCase ):
 class HelloWorldSuccess( UserJobTestCase ):
   def test_execute( self ):
 
-    helloJ = LHCbJob()
+    lhcbJob = LHCbJob()
 
-    helloJ.setName( "helloWorld-test" )
-    helloJ.setExecutable( "exe-script.py" )
-    res = helloJ.runLocal( self.dLHCb )
+    lhcbJob.setName( "helloWorld-test" )
+    lhcbJob.setExecutable( "exe-script.py" )
+    res = lhcbJob.runLocal( self.dLHCb )
     self.assertTrue( res['OK'] )
+
+class GaudirunSuccess( UserJobTestCase ):
+  def test_execute( self ):
+
+    lhcbJob = LHCbJob()
+
+    lhcbJob.setName( "gaudirun-test" )
+    lhcbJob.setApplication( 'Gauss', 'v41r4',
+                            "$APPCONFIGOPTS/Gauss/Beam3500GeV-md100-MC11-nu2.py;$DECFILESROOT/options/12133041.py;$LBPYTHIAROOT/options/Pythia.py;$APPCONFIGOPTS/Gauss/G4PL_LHEP_EmNoCuts.py",
+                            events = 2 )
+
+    res = lhcbJob.runLocal( self.dLHCb )
+    self.assertTrue( res['OK'] )
+
 
 if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase( UserJobTestCase )
   suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( HelloWorldSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( GaudirunSuccess ) )
   testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
