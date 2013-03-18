0d1572864d9a9de3dc3c9d4f7fff514fbceb28b9
chaen <chaen@4525493e-7705-40b1-a816-d608a930855b>
2013-03-18 09:13:18 +0000

    modify LHCbDIRAC/Core/scripts/dirac-lhcb-view-software.py
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@63479 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Core/scripts/dirac-lhcb-view-software.py
+++ LHCbDIRAC/Core/scripts/dirac-lhcb-view-software.py
@@ -7,7 +7,8 @@
 
     Usage:
       dirac-lhcb-view-software
-        --systemConfig        Optional System Configuration To Query For (by default it does not look for a specific configuration)
+        --systemConfig        Optional System Configuration To Query For
+                             (by default it does not look for a specific configuration)
         --name                Name of the LHCb software package
         --version             Version of the LHCb software package
 
@@ -20,6 +21,9 @@
 
 
 __RCSID__ = "$Id: $"
+
+import sys, string
+
 
 runMode = None
 subLogger = None
@@ -83,20 +87,37 @@ def parseSwitches():
 
 
 def printSoftware( package, packageArch ):
+  '''
+    Helper function to format the output
+    Prints the software name, the version, and the list of available architectures
+  '''
   adj = 30
-  gLogger.notice( package.split( '.' )[0].ljust( adj ) + package.split( '.' )[1].ljust( adj ) + ','.join( packageArch ).ljust( adj ) )
+  gLogger.notice( package.split( '.' )[0].ljust( adj ) \
+                  + package.split( '.' )[1].ljust( adj )\
+                  + ','.join( packageArch ).ljust( adj ) )
 
 def printHeader( header ):
+  '''
+    Helper function to format the output : prints a header
+  '''
   gLogger.notice( '=========> %s ' % header )
 
 
 
 
 def getSoftwareDistribution():
+  '''
+    Get information about software distribution and stores it into global variables :
+    software : a dictionary with keys being the system config, and the values the software list
+    systemConfig : the keys of software, without 'active' and 'deprecated'
+    active : list of active software
+    deprecated : list of deprecated software
+  '''
 
   softwareDistribution = gConfig.getOptionsDict( '/Operations/SoftwareDistribution' )
   if not softwareDistribution['OK']:
-    gLogger.notice( 'ERROR: Could not get values for /Operations/SoftwareDistribution section with message:\n%s' % ( softwareDistribution['Message'] ) )
+    gLogger.notice( 'ERROR: Could not get values for /Operations/SoftwareDistribution section with message:\n%s'\
+                     % ( softwareDistribution['Message'] ) )
     DIRACExit( 2 )
 
   global systemConfigs, active, deprecated, software
@@ -117,8 +138,11 @@ def getSoftwareDistribution():
   deprecated.sort()
 
 
-"""Check whether a given package version is installed"""
 def checkPackage( name, version ):
+  '''
+    Check whether a given package version is installed
+  '''
+
   packageChecked = name + '.' + version
   if not packageChecked in active:
     gLogger.notice( 'This package was not distributed on the GRID' )
@@ -126,33 +150,37 @@ def checkPackage( name, version ):
   else:
     gLogger.notice( 'This package is distributed on the GRID' )
 
-"""List the installed packages. If the 'configuration' argument is set, only this system configuration is listed"""
 def listConfig( configuration = None ):
-    if configuration:
-      printHeader( 'Active LHCb Software For System Configuration %s' % ( configuration ) )
-    else:
-      printHeader( 'Active LHCb Software For All System Configurations' )
+  '''
+    List the installed packages.
+    If the 'configuration' argument is set, only this system configuration is listed
+  '''
 
+  if configuration:
+    printHeader( 'Active LHCb Software For System Configuration %s' % ( configuration ) )
+  else:
+    printHeader( 'Active LHCb Software For All System Configurations' )
+
+  for systemConfig in systemConfigs:
+    try:
+      software[systemConfig] = software[systemConfig].replace( ' ', '' ).split( ',' )
+    except:
+      gLogger.error( "No system configuration called %s" % systemConfig )
+      DIRACExit( 3 )
+  for package in active:
+    packageArch = []
     for systemConfig in systemConfigs:
-      try:
-        software[systemConfig] = software[systemConfig].replace( ' ', '' ).split( ',' )
-      except:
-        gLogger.error( "No system configuration called %s" % systemConfig )
-        DIRACExit( 3 )
-    for package in active:
-      packageArch = []
-      for systemConfig in systemConfigs:
-        if package in software[systemConfig]:
-          packageArch.append( systemConfig )
-      if packageArch:
-        printSoftware( package, packageArch )
-      else:
-        if not configuration:
-          gLogger.notice( 'WARNING: %s %s is not defined for any system configurations' % ( package.split( '.' )[0], package.split( '.' )[1] ) )
+      if package in software[systemConfig]:
+        packageArch.append( systemConfig )
+    if packageArch:
+      printSoftware( package, packageArch )
+    else:
+      if not configuration:
+        gLogger.notice( 'WARNING: %s %s is not defined for any system configurations' % ( package.split( '.' )[0], package.split( '.' )[1] ) )
 
-    if not configuration:
-      printHeader( 'Deprecated LHCb Software' )
-      gLogger.notice( '%s' % ( ', '.join( deprecated ) ) )
+  if not configuration:
+    printHeader( 'Deprecated LHCb Software' )
+    gLogger.notice( '%s' % ( ', '.join( deprecated ) ) )
 
 
 
@@ -166,7 +194,6 @@ if __name__ == "__main__":
   registerSwitches()
   switchDict = parseSwitches()
 
-  import sys, string
   from DIRAC.Core.Utilities import List
   from DIRAC import gConfig
 
