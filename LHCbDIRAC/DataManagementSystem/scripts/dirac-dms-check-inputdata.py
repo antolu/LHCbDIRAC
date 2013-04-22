11377ac942f021edd2f147342880fe185fcf527d
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2013-04-22 15:27:46 +0000

    CHANGE: consider globally all jobs and all files CHANGE: allow --User and also all jobs FIX: if not in FC, check also BK
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@65180 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-check-inputdata.py
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-check-inputdata.py
@@ -15,7 +15,7 @@ def inaccessibleReplicas( lfn, se ):
   for se in seList:
     res = rm.getReplicaMetadata( lfn, se )
     if not res['OK']:
-      gLogger.always( 'Error getting metadata of %s at %s' % ( lfn, se ) )
+      gLogger.always( 'Error getting metadata of %s at %s' % ( lfn, se ), res['Message'] )
       continue
     if lfn in res['Value']['Failed']:
       failed[se] = res['Value']['Failed'][lfn]
@@ -35,6 +35,7 @@ if __name__ == "__main__":
 
   Script.registerSwitch( 'v', 'Verbose', '   Set verbose mode' )
   Script.registerSwitch( '', 'Production=', '   Select a production from which jobs in IDR will be used' )
+  Script.registerSwitch( '', 'User=', '  Select a user' )
 
   Script.parseCommandLine( ignoreErrors = True )
   import DIRAC
@@ -43,6 +44,7 @@ if __name__ == "__main__":
 
   verbose = False
   production = None
+  userName = None
   for opt, val in Script.getUnprocessedSwitches():
     if opt in ( 'v', 'Verbose' ):
       verbose = True
@@ -50,6 +52,8 @@ if __name__ == "__main__":
       val = val.split( ',' )
       production = ['%08d' % int( prod ) for prod in val if prod.isdigit()]
       production += [prod for prod in val if not prod.isdigit()]
+    if opt == 'User':
+      userName = val
 
   args = Script.getPositionalArgs()
   if args:
@@ -62,16 +66,26 @@ if __name__ == "__main__":
     jobs = []
 
   from DIRAC.DataManagementSystem.Client.ReplicaManager import ReplicaManager
+  from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient import BookkeepingClient
   from DIRAC.Core.Utilities.SiteSEMapping import getSEsForSite
   rm = ReplicaManager()
+  bk = BookkeepingClient()
 
   monitoring = RPCClient( 'WorkloadManagement/JobMonitoring' )
 
-  if production:
-    if len( production ) == 1:
-      production = production[0]
+  if not jobs:
     conditions = {'Status':'Failed', 'MinorStatus':'Maximum of reschedulings reached',
-                  'ApplicationStatus':'Failed Input Data Resolution ', 'JobGroup': production}
+                  'ApplicationStatus':'Failed Input Data Resolution '}
+    prStr = 'all jobs'
+    if production:
+      prStr = 'production %s' % ' '.join( productions )
+      if len( production ) == 1:
+        production = production[0]
+      conditions['JobGroup'] = production
+    if userName:
+      prStr = 'user %s' % userName
+      conditions['Owner'] = userName
+    gLogger.always( 'Obtaining IDR jobs for %s' % prStr )
     res = monitoring.getJobs( conditions )
     if not res['OK']:
       gLogger.always( 'Error selecting jobs for production %s' % str( production ), res['Message'] )
@@ -80,7 +94,8 @@ if __name__ == "__main__":
       gLogger.always( "No jobs found with IDR for production %s" % str( production ) )
     elif verbose:
       gLogger.always( 'Selected %d jobs from production %s' % ( len( res['Value'] ), str( production ) ) )
-    jobs += [int( job ) for job in res['Value']]
+    jobs = [int( job ) for job in res['Value']]
+    gLogger.always( "Obtained %d jobs... Now analyzing them" % len( jobs ) )
   if not jobs:
     gLogger.always( 'No jobs to check, exiting...' )
     DIRAC.exit( 0 )
@@ -91,95 +106,121 @@ if __name__ == "__main__":
     DIRAC.exit( 2 )
   gLogger.setLevel( 'FATAL' )
   jobSites = res['Value']
-  sep = ''
-  siteSEs = {}
+  filesAtSite = {}
+  chunkSize = 100
+  nj = 0
+  sys.stdout.write( 'Getting JDL for jobs (groups of %d): ' % chunkSize )
   for jobID in jobs:
-    try:
-      pbFound = False
-      gLogger.always( '%sJob %d' % ( sep, jobID ) )
-      sep = '=====================================\n'
-      res = monitoring.getJobJDL( jobID )
-      if not res['OK']:
-        gLogger.always( 'Error getting job %d JDL' % jobID, res['Message'] )
-        continue
-      jdl = res['Value'].splitlines()
-      ind = 0
-      found = 0
-      for l in jdl:
-        if 'InputData =' in l:
-          found = ind
-        if ind == found + 1:
-          if '{' in l:
-            found = ind + 1
-          else:
-            end = ind
-        if found and '}' in l:
+    if not nj % chunkSize:
+      sys.stdout.write( '.' )
+      sys.stdout.flush()
+    nj += 1
+    res = monitoring.getJobJDL( jobID )
+    if not res['OK']:
+      gLogger.always( 'Error getting job %d JDL' % jobID, res['Message'] )
+      continue
+    jdl = res['Value'].splitlines()
+    ind = 0
+    found = 0
+    for l in jdl:
+      if 'InputData =' in l:
+        found = ind
+      if ind == found + 1:
+        if '{' in l:
+          found = ind + 1
+        else:
           end = ind
-          break
-        ind += 1
-      if not found:
-        gLogger.always( 'No InputData field found in JDL for job %d' % jobID )
+      if found and '}' in l:
+        end = ind
+        break
+      ind += 1
+    if not found:
+      gLogger.always( 'No InputData field found in JDL for job %d' % jobID )
+      continue
+    if end == found + 1:
+      inputData = dmScript.getLFNsFromList( jdl[found].split( '"' )[1] )
+    else:
+      inputData = dmScript.getLFNsFromList( eval( '[' + ''.join( jdl[found:end] ) + ']' ) )
+    inputData.sort()
+    if verbose:
+      gLogger.always( 'Input Data for job %d\n%s' % ( jobID, '\n'.join( inputData ) ) )
+    site = jobSites.get( jobID, {} ).get( 'Site', 'Unknown' )
+    if verbose:
+      gLogger.always( 'Site: %s' % site )
+    for lfn in inputData:
+      filesAtSite.setdefault( site, {} ).setdefault( lfn, [] ).append( jobID )
+
+  sep = '\n'
+  for site in filesAtSite:
+    seUsed = ''
+    try:
+      jobs = []
+      for lfn in filesAtSite[site]:
+        for jobID in filesAtSite[site][lfn]:
+          if jobID not in jobs:
+            jobs.append( str( jobID ) )
+      res = getSEsForSite( site )
+      if not res['OK'] or not res['Value']:
+        gLogger.always( "Couldn't find SEs for site %s" % site )
         continue
-      if end == found + 1:
-        inputData = dmScript.getLFNsFromList( jdl[found].split( '"' )[1] )
+      seList = res['Value']
+      inputData = sorted( filesAtSite[site] )
+      if verbose:
+        gLogger.always( "%sSite: %s, jobs: %s, %d files" % ( sep, site, ','.join( jobs ), len( inputData ) ) )
       else:
-        inputData = dmScript.getLFNsFromList( eval( '[' + ''.join( jdl[found:end] ) + ']' ) )
-      inputData.sort()
+        gLogger.always( "%sSite: %s, %d jobs, %d files" % ( sep, site, len( jobs ), len( inputData ) ) )
+      sep = '=====================================\n'
       if verbose:
-        gLogger.always( 'Input Data for job %d\n%s' % ( jobID, '\n'.join( inputData ) ) )
-      site = jobSites.get( jobID, {} ).get( 'Site', 'Unknown' )
-      if verbose:
-        gLogger.always( 'Site: %s' % site )
-      seList = siteSEs.get( site )
-      if not seList:
-        res = getSEsForSite( site )
-        if not res['OK']:
-          gLogger.always( "Couldn't find SEs for site %s" % site )
-          continue
-        siteSEs[site] = seList = res['Value']
-      if verbose:
-        gLogger.always( 'SEs: %s' % str( seList ) )
+        gLogger.always( 'For %s, SEs: %s' % ( site, str( seList ) ) )
+      pbFound = False
 
       res = rm.getReplicas( inputData )
       if not res['OK']:
         gLogger.always( "Error getting replicas for %d files" % len( inputData ), res['Message'] )
         continue
+      replicas = res['Value']['Successful']
       notInFC = res['Value']['Failed']
       if notInFC:
-        pbFound = True
-        prettyMsg( 'not in the FC', notInFC )
-      replicas = res['Value']['Successful']
+        # Check if files has replica flag in the FC, If not ignore the problem
+        res = bk.getFileMetadata( notInFC.keys() )
+        if not res['OK']:
+          gLogger.always( 'Error getting BK metadata for %d files' % len( notInFC ), res['Message'] )
+          continue
+        metadata = res['Value'].get( 'Successful', res['Value'] )
+        notInFC = [lfn for lfn in notInFC if metadata.get( lfn, {} ).get( 'GotReplica' ) == 'Yes']
+        if notInFC:
+          pbFound = True
+          prettyMsg( 'not in the FC but in BK', notInFC )
       notFoundReplicas = replicas.keys()
       missingReplicas = []
       accessibleReplicas = []
       seUsed = []
       for lfn in [l for l in inputData if l in replicas]:
-        for se in replicas[lfn]:
-          if se in seList:
-            # Found a replica at the site
-            if se not in seUsed:
-              seUsed.append( se )
-            if lfn in notFoundReplicas:
-              notFoundReplicas.remove( lfn )
-            inaccessible = inaccessibleReplicas( lfn, se )
-            if se in inaccessible:
-              reason = inaccessible[se]
-              otherReplicas = [s for s in replicas[lfn] if s != se]
-              inaccessible = inaccessibleReplicas( lfn, otherReplicas )
-              accessible = [s for s in otherReplicas if s not in inaccessible]
-              missingReplicas.append( ( lfn, se, reason, 'Accessible at %s' % str( accessible ) if accessible else 'No other accessible replicas' ) )
-              prStr = 'not '
-            else:
-              prStr = ''
-            if verbose:
-              gLogger.always( '%s is %s accessible at %s' % ( lfn, prStr, se ) )
+        for se in [se for se in replicas[lfn] if se in seList]:
+          # Found a replica at the site
+          if se not in seUsed:
+            seUsed.append( se )
+          if lfn in notFoundReplicas:
+            notFoundReplicas.remove( lfn )
+          inaccessible = inaccessibleReplicas( lfn, se )
+          if se in inaccessible:
+            reason = inaccessible[se]
+            otherReplicas = [s for s in replicas[lfn] if s != se]
+            inaccessible = inaccessibleReplicas( lfn, otherReplicas )
+            accessible = [s for s in otherReplicas if s not in inaccessible]
+            missingReplicas.append( ( lfn, se, reason, 'Accessible at %s' % str( accessible ) if accessible else 'No other accessible replicas' ) )
+            prStr = 'not'
+          else:
+            prStr = ''
+          if verbose:
+            gLogger.always( '%s is %s accessible at %s' % ( lfn, prStr, se ) )
       if missingReplicas:
         pbFound = True
         msgList = ['%s at %s: %s\n\t%s' % x for x in missingReplicas]
         prettyMsg( 'not accessible', msgList )
       if notFoundReplicas:
         pbFound = True
-        prettyMsg( 'not found at %s' % site, notFoundReplicas )
+        gLogger.always( '%d files not found at SE close to %s, but have other replicas' % ( len( notFoundReplicas ), site ) )
     except Exception, e:
       gLogger.always( '%s' % e )
       pass
