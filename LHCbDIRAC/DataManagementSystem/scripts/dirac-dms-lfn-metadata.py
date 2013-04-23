1078949847e9f188e18a1280228a97a6f7d4b8fe
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2013-04-23 17:41:39 +0000

    NEW: new scripts for lfn/pfn-metadata, using DMScript (overwrites DIRAC ones)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@65240 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-lfn-metadata.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+########################################################################
+# File :    dirac-dms-lfn-metadata
+# Author :  Philippe Charpentier
+########################################################################
+"""
+  Get the metadata of a (list of) LFNs from the FC
+"""
+__RCSID__ = "$Id$"
+import DIRAC
+from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript, printDMResult
+from DIRAC.Core.Base import Script
+
+if __name__ == "__main__":
+
+  dmScript = DMScript()
+  dmScript.registerFileSwitches()
+  dmScript.registerSiteSwitches()
+  Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                       'Usage:',
+                                       '  %s [option|cfgfile] ... [LFN[,LFN2[,LFN3...]]]' % Script.scriptName,
+                                       'Arguments:',
+                                       '  LFN:      Logical File Name or file containing LFNs'] ) )
+  Script.parseCommandLine( ignoreErrors = True )
+
+
+  args = Script.getPositionalArgs()
+
+  for lfn in args:
+    dmScript.setLFNsFromFile( lfn )
+  lfnList = dmScript.getOption( 'LFNs', [] )
+  if not lfnList:
+    Script.showHelp()
+    DIRAC.exit( 0 )
+
+  from DIRAC.DataManagementSystem.Client.ReplicaManager    import ReplicaManager
+  from DIRAC import gLogger
+  rm = ReplicaManager()
+  gLogger.setLevel( "FATAL" )
+  res = rm.getCatalogFileMetadata( lfnList )
+  if res['OK']:
+    printDMResult( res, empty = "File not in FC" )
+  else:
+    gLogger.fatal( "Error getting metadata for %s" % ( lfnList ) )
+    printDMResult( res, empty = "File not in FC", script = "dirac-dms-lfn-metadata" )
+  DIRAC.exit( 0 )
---
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-pfn-metadata.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+########################################################################
+# File :    dirac-dms-pfn-metadata.py
+# Author :  Ph. Charpentier
+########################################################################
+"""
+  Gets the metadata of a (list of) LFNs/PFNs given a valid DIRAC SE.
+"""
+__RCSID__ = "$Id$"
+import DIRAC
+from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript, printDMResult
+from DIRAC.Core.Base import Script
+
+def __checkSEs( args ):
+  seList = []
+  res = gConfig.getSections( '/Resources/StorageElements' )
+  if res['OK']:
+    for ses in list( args ):
+      sel = [se for se in ses.split( ',' ) if se in res['Value']]
+      if sel :
+        seList.append( ','.join( sel ) )
+        args.remove( ses )
+  return seList, args
+
+if __name__ == "__main__":
+
+  dmScript = DMScript()
+  dmScript.registerFileSwitches()
+  dmScript.registerSiteSwitches()
+  Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                       'Usage:',
+                                       '  %s [option|cfgfile] ... [URL[,URL2[,URL3...]]] SE[,SE2...]' % Script.scriptName,
+                                       'Arguments:',
+                                       '  URL:      Logical/Physical File Name or file containing URLs',
+                                       '  SE:       Valid DIRAC SE' ] ) )
+  Script.parseCommandLine( ignoreErrors = True )
+
+  seList = dmScript.getOption( 'SEs', [] )
+
+  if not seList:
+    sites = dmScript.getOption( 'Sites', [] )
+    from DIRAC import gConfig
+    for site in sites:
+      res = gConfig.getOptionsDict( '/Resources/Sites/LCG/%s' % site )
+      if not res['OK']:
+        print 'Site %s not known' % site
+        Script.showHelp()
+      seList.extend( res['Value']['SE'].replace( ' ', '' ).split( ',' ) )
+  args = Script.getPositionalArgs()
+  if not seList:
+    seList, args = __checkSEs( args )
+  # This should be improved, with disk SEs first...
+  if not seList:
+    print "Give SE name as last argument or with --SE option"
+    Script.showHelp()
+  seList.sort()
+
+  for lfn in args:
+    dmScript.setLFNsFromFile( lfn )
+  urlList = dmScript.getOption( 'LFNs', [] )
+  if not urlList:
+    Script.showHelp()
+    DIRAC.exit( 0 )
+
+  from DIRAC.DataManagementSystem.Client.ReplicaManager    import ReplicaManager
+  from DIRAC import gLogger
+  if len( seList ) > 1:
+    gLogger.info( "Using the following list of SEs: %s" % str( seList ) )
+  rm = ReplicaManager()
+  gLogger.setLevel( "FATAL" )
+  result = {'OK':True, 'Value': {'Successful':{}, 'Failed':{}}}
+  for se in seList:
+    res = rm.getStorageFileMetadata( urlList, se )
+    if res['OK']:
+      result['Value']['Successful'].update( res['Value']['Successful'] )
+      result['Value']['Failed'].update( res['Value']['Failed'] )
+      for url in res['Value']['Successful']:
+        result['Value']['Failed'].pop( url, None )
+      for url in res['Value']['Failed']:
+        result['Value']['Failed'][url] += ' at %s' % ', '.join( seList )
+      urlList = res['Value']['Failed']
+      if not urlList: break
+    else:
+      result['OK'] = False
+
+  if result['OK']:
+    printDMResult( result, empty = "File not at SE" )
+  else:
+    gLogger.fatal( "Error getting metadata for %s at %s" % ( urlList, str( seList ) ) )
+    printDMResult( res, empty = "File not at SE", script = "dirac-dms-pfn-metadata" )
+  DIRAC.exit( 0 )
