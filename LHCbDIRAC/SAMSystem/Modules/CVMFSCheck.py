505843fd1020ecdd96b94433e5170b03d0ef6ca1
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-05-10 08:08:54 +0000

    NEW: CVMFSCheck SAM module
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@65655 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/SAMSystem/Modules/CVMFSCheck.py
@@ -0,0 +1,50 @@
+"""
+  CVMFSChech module for SAM jobs
+"""
+
+import os
+
+# DIRAC
+from DIRAC import gLogger, S_ERROR, S_OK
+
+# LHCbDIRAC
+from LHCbDIRAC.Workflow.Modules.ModuleBase import ModuleBase
+
+__RCSID__ = '$Id: $'
+
+class CVMFSCheck( ModuleBase ):
+  """
+  CVMFSCheck module extending more generic LHCb jobs ModuleBase.
+  """
+  
+  def __init__( self ):
+    """
+    Constructor
+    """
+    super( CVMFSCheck, self ).__init__()
+    
+    self.log = gLogger.getSubLogger( self.__class__.__name__ )
+
+  def execute( self ):
+    """
+    Main method. If ${VO_LHCB_SW_DIR}/lib/etc/cernvmfs is present, we continue
+    the SAM job.
+    """
+    
+    self.log.info( 'Checking presence of CVMFS' )
+    
+    if not 'VO_LHCB_SW_DIR' in os.environ:
+      return S_ERROR( 'Environment variable VO_LHCB_SW_DIR not found' )
+    
+    swDir = os.environ[ 'VO_LHCB_SW_DIR' ]
+    
+    cvmfsFilePath = os.path.join( swDir, '/lib/etc/cernvmfs' )
+    self.log.info( 'CVMFS file path "%s"' % cvmfsFilePath )
+    
+    if not os.path.exists( cvmfsFilePath ):
+      return S_ERROR( 'CVMFS file path "%s" does not exist' % cvmfsFilePath )
+    
+    return S_OK( 'CVMFS is present' )    
+
+#...............................................................................
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
