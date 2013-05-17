40ccc67c4f4611dc41a2393d6724dbba599b92d3
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2013-05-17 14:03:35 +0000

    fix the test using mock
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@66086 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/DB/test/Test_BKK_DB_OracleBookkeepingDB.py
+++ LHCbDIRAC/BookkeepingSystem/DB/test/Test_BKK_DB_OracleBookkeepingDB.py
@@ -4,9 +4,8 @@
 import mock
 import unittest
 
-from DIRAC.Core.Base import Script
-Script.parseCommandLine(ignoreErrors=True)
 import LHCbDIRAC.BookkeepingSystem.DB.OracleBookkeepingDB as moduleTested
+from DIRAC.ConfigurationSystem.Client.Config                import gConfig
 
 __RCSID__ = ''
 
@@ -19,9 +18,21 @@ class OracleBookkeepingDB_TestCase(unittest.TestCase):
     '''
     Setup
     '''
-
+    mock_OracleDB = mock.Mock()
     self.moduleTested = moduleTested
-    self.testClass = self.moduleTested.OracleBookkeepingDB()
+    self.testClass = self.moduleTested.OracleBookkeepingDB
+    moduleTested.OracleDB = mock_OracleDB
+
+    mock_getDatabaseSection = mock.Mock()
+    mock_getDatabaseSection.return_value =  '/Systems/Bookkeeping/Development/Databases/BookkeepingDB'
+    self.mock_getDatabaseSection         = mock_getDatabaseSection
+    self.moduleTested.getDatabaseSection = self.mock_getDatabaseSection
+
+    mock_gConfig = mock.Mock(spec=gConfig)
+    mock_gConfig.getOption.return_value = { 'OK' : True, 'Value' : 'exp1' }
+    self.mock_gConfig = mock_gConfig.getOption
+    self.moduleTested.gConfig.getOption = self.mock_gConfig
+
 
   def tearDown(self):
     '''
@@ -36,36 +47,40 @@ class OracleBookkeepingDB_TestCase(unittest.TestCase):
 class OracleBookkeepingDB_Success(OracleBookkeepingDB_TestCase):
   """
   """
+
   def test_instantiate(self):
     ''' tests that we can instantiate one object of the tested class
     '''
 
-    module = self.testClass
+    module = self.testClass()
     self.assertEqual('OracleBookkeepingDB', module.__class__.__name__)
 
   def test_buildRunNumbers(self):
     """It test the method which used to build the conditions when runnumbers is a
     list/number, and end run and start run is a number
     """
+    self.moduleTested.gConfig.getOption = self.mock_gConfig
+    self.moduleTested.getDatabaseSection = self.mock_getDatabaseSection
+    client = self.testClass()
     runnumbers = [1, 3, 4]
     startRunID = None
     endRunID = None
     condition = ''
     tables = ''
-    retVal = self.testClass._OracleBookkeepingDB__buildRunnumbers(runnumbers, startRunID, endRunID, condition, tables)
+    retVal = client._OracleBookkeepingDB__buildRunnumbers(runnumbers, startRunID, endRunID, condition, tables)
     self.assert_(retVal['OK'])
     self.assert_(retVal['Value'])
     self.assertEqual(retVal['Value'], (' and  (  j.runnumber=1 or  j.runnumber=3 or  j.runnumber=4 ) ', ''))
 
     startRunID = 1
-    retVal = self.testClass._OracleBookkeepingDB__buildRunnumbers(runnumbers, startRunID, endRunID, condition, tables)
+    retVal = client._OracleBookkeepingDB__buildRunnumbers(runnumbers, startRunID, endRunID, condition, tables)
     self.assert_(retVal['OK'])
     self.assert_(retVal['Value'])
     self.assertEqual((' and  (  j.runnumber=1 or  j.runnumber=3 or  j.runnumber=4 ) ', ''), retVal['Value'])
 
     startRunID = None
     endRunID = 1
-    retVal = self.testClass._OracleBookkeepingDB__buildRunnumbers(runnumbers, startRunID, endRunID, condition, tables)
+    retVal = client._OracleBookkeepingDB__buildRunnumbers(runnumbers, startRunID, endRunID, condition, tables)
     self.assert_(retVal['OK'])
     self.assert_(retVal['Value'])
     self.assertEqual((' and  (  j.runnumber=1 or  j.runnumber=3 or  j.runnumber=4 ) ', ''), retVal['Value'])
@@ -73,7 +88,7 @@ class OracleBookkeepingDB_Success(OracleBookkeepingDB_TestCase):
     startRunID = 1
     endRunID = 2
     runnumbers = [33, 44]
-    retVal = self.testClass._OracleBookkeepingDB__buildRunnumbers(runnumbers, startRunID, endRunID, condition, tables)
+    retVal = client._OracleBookkeepingDB__buildRunnumbers(runnumbers, startRunID, endRunID, condition, tables)
     self.assert_(retVal['OK'])
     self.assert_(retVal['Value'])
     self.assertEqual((' and (j.runnumber>=1 and j.runnumber<=2 or  (  j.runnumber=33 or  j.runnumber=44 ))', ''),
@@ -83,9 +98,12 @@ class OracleBookkeepingDB_Success(OracleBookkeepingDB_TestCase):
       runnumbers = i
       startRunID = 1
       endRunID = 2
-      retVal = self.testClass._OracleBookkeepingDB__buildRunnumbers(runnumbers, startRunID, endRunID, condition, tables)
+      retVal = client._OracleBookkeepingDB__buildRunnumbers(runnumbers, startRunID, endRunID, condition, tables)
       self.assert_(retVal['OK'])
       self.assert_(retVal['Value'])
       self.assertEqual((' and j.runnumber>=1 and j.runnumber<=2', ''), retVal['Value'])
+
+if __name__ == '__main__':
+  unittest.main()
 
 
