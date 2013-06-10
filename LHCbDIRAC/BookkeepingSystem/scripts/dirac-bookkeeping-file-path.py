b3bbc3d12b8fc06399c36766111c83237e287f72
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2013-06-10 17:21:00 +0000

    FIX: help
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@66863 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-file-path.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-file-path.py
@@ -6,7 +6,7 @@
 """
   Return the BK path for the directories of a (list of) files
 """
-__RCSID__ = "$Id: dirac-bookkeeping-file-metadata.py 65177 2013-04-22 15:24:07Z phicharp $"
+__RCSID__ = "$Id: dirac-bookkeeping-file-path.py 65177 2013-04-22 15:24:07Z phicharp $"
 import  DIRAC.Core.Base.Script as Script
 from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript, printDMResult
 import os
@@ -18,7 +18,7 @@ def __buildPath( bkDict ):
 if __name__ == "__main__":
   dmScript = DMScript()
   dmScript.registerFileSwitches()
-  Script.registerSwitch( '', 'Full', '   Print out all metadata' )
+  Script.registerSwitch( '', 'Full', '   Print out full BK dictionary (default: print out BK path)' )
   Script.registerSwitch( '', 'GroupByPath', '   Return a list of files per BK path' )
   Script.registerSwitch( '', 'GroupByProduction', '   Return a list of files per production' )
   Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
