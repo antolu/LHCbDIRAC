42c6ed3f5e7f5c73c50b3129a1a29393a38243bc
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2013-06-10 11:20:38 +0000

    NEW: script to add files to a transformation
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@66772 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/TransformationSystem/scripts/dirac-transformation-add-files.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+"""
+ Add files to a transformation
+"""
+
+__RCSID__ = "$Id: dirac-transformation-add-files.py 65086 2013-04-19 07:58:35Z phicharp $"
+
+def __getTransformations( args ):
+  if not len( args ):
+    print "Specify transformation number..."
+    Script.showHelp()
+  else:
+      ids = args[0].split( "," )
+      transList = []
+      for transID in ids:
+          r = transID.split( ':' )
+          if len( r ) > 1:
+              for i in range( int( r[0] ), int( r[1] ) + 1 ):
+                  transList.append( i )
+          else:
+              transList.append( int( r[0] ) )
+  return transList
+
+if __name__ == "__main__":
+
+  import DIRAC
+  from DIRAC import gLogger
+  from DIRAC.Core.Base import Script
+  from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
+  from LHCbDIRAC.TransformationSystem.Client.Utilities   import addFilesToTransformation
+  import time
+
+  dmScript = DMScript()
+  dmScript.registerFileSwitches()
+
+  Script.registerSwitch( '', 'NoRunInfo', '   Use if no run information is required' )
+
+  Script.parseCommandLine( ignoreErrors = True )
+
+  Script.setUsageMessage( '\n'.join( [ __doc__,
+                                       'Usage:',
+                                       '  %s [option|cfgfile] ...' % Script.scriptName, ] ) )
+
+
+  runInfo = True
+  switches = Script.getUnprocessedSwitches()
+  for switch in switches:
+    if switch[0] == 'NoRunInfo':
+      runInfo = False
+
+  args = Script.getPositionalArgs()
+
+  requestedLFNs = dmScript.getOption( 'LFNs' )
+  if not args:
+    gLogger.always( 'No transformation specified' )
+    Script.showHelp()
+    DIRAC.exit( 1 )
+  else:
+    try:
+      transID = int( args[0] )
+    except:
+      gLogger.always( 'Invalid transformation ID' )
+      DIRAC.exit( 1 )
+  if not requestedLFNs:
+    gLogger.always( 'A list of files must be provided' )
+    Script.showHelp()
+    DIRAC.exit( 1 )
+
+  res = addFilesToTransformation( transID, requestedLFNs, runInfo )
+  if res['OK']:
+    gLogger.always( 'Successfully added %d files to transformation %d' % ( len( res['Value'] ), transID ) )
+    rc = 0
+  else:
+    gLogger.always( 'Failed to add %d files to transformation %d' % ( len( requestedLFNs ), transID ), res['Message'] )
+    rc = 2
+  DIRAC.exit( rc )
