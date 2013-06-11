337178b044cca4d0fe5e0b4dbf6a9464da269cf4
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-06-11 17:12:00 +0000

    Current vs NEW implementation of the ProductionsStateMachine
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@66919 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Client/ProductionsClient.py
+++ LHCbDIRAC/ProductionManagementSystem/Client/ProductionsClient.py
@@ -1,20 +1,29 @@
 """ An extension of the LHCb TransformationClient to handle some specificities of productions
 """
 
-from LHCbDIRAC.TransformationSystem.Client.TransformationClient import TransformationClient
+from LHCbDIRAC.TransformationSystem.Client.TransformationClient   import TransformationClient
+from LHCbDIRAC.ProductionManagementSystem.Utilities.StateMachine  import ProductionsStateMachine
 
-class ProductionsClient(TransformationClient):
+class ProductionsClient( TransformationClient ):
   """ An extension of the LHCb TransformationClient to handle some specificities of productions
   """
-  
-  def setTransformationParameter(self, transName, paramName, paramValue):
+
+  def setTransformationParameter( self, transID, paramName, paramValue ):
     """ just invokes the state machine check when needed
     """
     if paramName.lower() == 'status':
-      self.setStatus( transName, paramValue )
+      return self.setStatus( transID, paramValue )
     else:
-      self.setTransformationParameter( transName, paramName, paramValue )
+      return TransformationClient.setTransformationParameter( self, transID, paramName, paramValue )
 
-  def setStatus( self, transName, status ):
+  def setStatus( self, transID, status, originalStatus = '' ):
     """ performs a state machine check for productions when asked to change the status
     """
+    if not originalStatus:
+      originalStatus = TransformationClient.getTransformationParameters( transID, 'Status' )
+      if not originalStatus['OK']:
+        return originalStatus
+      originalStatus = originalStatus['Value']
+
+    psm = ProductionsStateMachine( originalStatus )
+    return psm.setState( status )
--- LHCbDIRAC/ProductionManagementSystem/Utilities/StateMachine.py
+++ LHCbDIRAC/ProductionManagementSystem/Utilities/StateMachine.py
@@ -13,19 +13,39 @@ class ProductionsStateMachine( StateMachine ):
 
     super( ProductionsStateMachine, self ).__init__( state )
 
+    # Current states
     self.states = {
-                   'Deleted'    : State( 10 ),  # final state
-                   'Cleaning'   : State( 9, ['Deleted'] ),
-                   'Completing' : State( 8, ['Validating', 'Cleaning'], defState = 'Validating' ),
-                   'Stopped'    : State( 7, ['Active', 'Cleaning'], defState = 'Active' ),
-                   'Archived'   : State( 6 ),  # final state
-                   'Completed'  : State( 5, ['Archived'], defState = 'Archived' ),
-                   'Validating' : State( 4, ['Active', 'Completed', 'Cleaning'], defState = 'Completed' ),
-                   'Idle'       : State( 3, ['Active', 'Validating', 'Cleaning'], defState = 'Active' ),
-                   'Flush'      : State( 2, ['Active', 'Cleaning'], defState = 'Active' ),
-                   'Active'     : State( 1, ['Flush', 'Idle', 'Stopped', 'Completing', 'Validating', 'Cleaning'], defState = 'Flush' ),
-                   'New'        : State( 0, ['Active', 'Cleaning'], defState = 'Active' )  # initial state
+                   'Deleted'          : State( 14 ),  # final state
+                   'Cleaning'         : State( 13, ['Deleted'] ),
+                   'Completing'       : State( 12, ['Validating', 'Cleaning'], defState = 'Validating' ),
+                   'Stopped'          : State( 11, ['Active', 'Flush', 'Cleaning'], defState = 'Active' ),
+                   'Archived'         : State( 10 ),  # final state
+                   'Completed'        : State( 9, ['Archived'] ),
+                   'WaitingIntegrity' : State( 8, ['ValidatedOutput'] ),
+                   'ValidatedOutput'  : State( 6, ['Active', 'Completed', 'Cleaning'], defState = 'Completed' ),
+                   'ValidatingOutput' : State( 6, ['Active', 'ValidatedOutput', 'WaitingIntegrity'], defState = 'ValidatedOutput' ),
+                   'RemovedFiles'     : State( 5, ['Completed'] ),
+                   'RemovingFiles'    : State( 4, ['RemovedFiles'] ),
+                   'ValidatingInput'  : State( 3, ['Active', 'RemovingFiles', 'Cleaning'], defState = 'Active' ),
+                   'Flush'            : State( 2, ['Active', 'Cleaning'], defState = 'Active' ),
+                   'Active'           : State( 1, ['Flush', 'Idle', 'Stopped', 'Completing', 'ValidatingInput', 'ValidatingOutput', 'Cleaning'], defState = 'Flush' ),
+                   'New'              : State( 0, ['Active', 'Cleaning'], defState = 'Active' )  # initial state
                   }
+
+    # NEW states proposal
+#    self.states = {
+#                   'Deleted'    : State( 10 ),  # final state
+#                   'Cleaning'   : State( 9, ['Deleted'] ),
+#                   'Completing' : State( 8, ['Validating', 'Cleaning'], defState = 'Validating' ),
+#                   'Stopped'    : State( 7, ['Active', 'Flush', 'Cleaning'], defState = 'Active' ),
+#                   'Archived'   : State( 6 ),  # final state
+#                   'Completed'  : State( 5, ['Archived'], defState = 'Archived' ),
+#                   'Validating' : State( 4, ['Active', 'Completed', 'Cleaning'], defState = 'Completed' ),
+#                   'Idle'       : State( 3, ['Active', 'Validating', 'Cleaning'], defState = 'Active' ),
+#                   'Flush'      : State( 2, ['Active', 'Cleaning'], defState = 'Active' ),
+#                   'Active'     : State( 1, ['Flush', 'Idle', 'Stopped', 'Completing', 'Validating', 'Cleaning'], defState = 'Flush' ),
+#                   'New'        : State( 0, ['Active', 'Cleaning'], defState = 'Active' )  # initial state
+#                  }
 
   def setState( self, candidateState ):
     """ Makes sure the state is either None or known to the machine, and that it is a valid state to move into
