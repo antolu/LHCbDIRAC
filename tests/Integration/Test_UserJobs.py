96cc41565e2b8293a577cea6a576450d5868456e
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-06-21 13:25:09 +0000

    .
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@67551 4525493e-7705-40b1-a816-d608a930855b

--- tests/Integration/Test_UserJobs.py
+++ tests/Integration/Test_UserJobs.py
@@ -24,92 +24,92 @@ class HelloWorldSuccess( UserJobTestCase ):
     res = lhcbJob.runLocal( self.dLHCb )
     self.assertTrue( res['OK'] )
 
-class HelloWorldSuccessWithJobID( UserJobTestCase ):
-  def test_execute( self ):
-
-    os.environ['JOBID'] = '12345'
-
-    lhcbJob = LHCbJob()
-
-    lhcbJob.setName( "helloWorld-test" )
-    lhcbJob.setExecutable( "exe-script.py" )
-    res = lhcbJob.runLocal( self.dLHCb )
-    self.assertTrue( res['OK'] )  # There's nothing to upload, so it will complete happily
-
-    del os.environ['JOBID']
-
-class HelloWorldSuccessOutput( UserJobTestCase ):
-  def test_execute( self ):
-
-    lhcbJob = LHCbJob()
-
-    lhcbJob.setName( "helloWorld-test" )
-    lhcbJob.setExecutable( "exe-script.py" )
-    lhcbJob.setOutputData( "Script1_exe-script.py.log" )
-    res = lhcbJob.runLocal( self.dLHCb )
-    self.assertTrue( res['OK'] )
-
-class HelloWorldSuccessOutputWithJobID( UserJobTestCase ):
-  def test_execute( self ):
-
-    os.environ['JOBID'] = '12345'
-
-    lhcbJob = LHCbJob()
-
-    lhcbJob.setName( "helloWorld-test" )
-    lhcbJob.setExecutable( "exe-script.py" )
-    lhcbJob.setOutputData( "Script1_exe-script.py.log" )
-    res = lhcbJob.runLocal( self.dLHCb )  # Can't upload, so it will fail
-    self.assertFalse( res['OK'] )
-
-    del os.environ['JOBID']
-
-class GaudirunSuccess( UserJobTestCase ):
-  def test_mc( self ):
-
-    lhcbJob = LHCbJob()
-
-    lhcbJob.setName( "gaudirun-test" )
-    lhcbJob.setInputSandbox( 'prodConf_Gauss_00012345_00067890_1.py' )
-
-    optGauss = "$APPCONFIGOPTS/Gauss/Beam4000GeV-md100-JulSep2012-nu2.5.py;"
-    optDec = "$DECFILESROOT/options/15512012.py;"
-    optPythia = "$LBPYTHIAROOT/options/Pythia.py;"
-    optOpts = " $APPCONFIGOPTS/Gauss/G4PL_LHEP_EmNoCuts.py;"
-    optCompr = "$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py;"
-    optPConf = "prodConf_Gauss_00012345_00067890_1.py"
-    options = optGauss + optDec + optPythia + optOpts + optCompr + optPConf
-    lhcbJob.addPackage( 'AppConfig', 'v3r160' )
-    lhcbJob.addPackage( 'DecFiles', 'v26r24' )
-    lhcbJob.addPackage( 'ProdConf', 'v1r9' )
-
-    lhcbJob.setApplication( 'Gauss', 'v42r4', options,
-                            extraPackages = 'AppConfig.v3r160;DecFiles.v26r24;ProdConf.v1r9' )
-
-    res = lhcbJob.runLocal( self.dLHCb )
-    self.assertTrue( res['OK'] )
-
-  def test_boole( self ):
-
-    lhcbJob = LHCbJob()
-
-    lhcbJob.setName( "gaudirun-test-inputs" )
-    lhcbJob.setInputSandbox( 'prodConf_Boole_00012345_00067890_1.py' )
-
-    opts = "$APPCONFIGOPTS/Boole/Default.py;"
-    optDT = "$APPCONFIGOPTS/Boole/DataType-2012.py;"
-    optTCK = "$APPCONFIGOPTS/L0/L0TCK-0x0042.py;"
-    optComp = "$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py;"
-    optPConf = "prodConf_Boole_00012345_00067890_1.py"
-    options = opts + optDT + optTCK + optComp + optPConf
-    lhcbJob.addPackage( 'AppConfig', 'v3r155' )
-
-    lhcbJob.setApplication( 'Boole', 'v24r0', options,
-                            inputData = '/lhcb/user/f/fstagni/test/12345/12345678/00012345_00067890_1.sim',
-                            extraPackages = 'AppConfig.v3r155;ProdConf.v1r9' )
-
-    res = lhcbJob.runLocal( self.dLHCb )
-    self.assertTrue( res['OK'] )
+# class HelloWorldSuccessWithJobID( UserJobTestCase ):
+#  def test_execute( self ):
+#
+#    os.environ['JOBID'] = '12345'
+#
+#    lhcbJob = LHCbJob()
+#
+#    lhcbJob.setName( "helloWorld-test" )
+#    lhcbJob.setExecutable( "exe-script.py" )
+#    res = lhcbJob.runLocal( self.dLHCb )
+#    self.assertTrue( res['OK'] )  # There's nothing to upload, so it will complete happily
+#
+#    del os.environ['JOBID']
+#
+# class HelloWorldSuccessOutput( UserJobTestCase ):
+#  def test_execute( self ):
+#
+#    lhcbJob = LHCbJob()
+#
+#    lhcbJob.setName( "helloWorld-test" )
+#    lhcbJob.setExecutable( "exe-script.py" )
+#    lhcbJob.setOutputData( "Script1_exe-script.py.log" )
+#    res = lhcbJob.runLocal( self.dLHCb )
+#    self.assertTrue( res['OK'] )
+#
+# class HelloWorldSuccessOutputWithJobID( UserJobTestCase ):
+#  def test_execute( self ):
+#
+#    os.environ['JOBID'] = '12345'
+#
+#    lhcbJob = LHCbJob()
+#
+#    lhcbJob.setName( "helloWorld-test" )
+#    lhcbJob.setExecutable( "exe-script.py" )
+#    lhcbJob.setOutputData( "Script1_exe-script.py.log" )
+#    res = lhcbJob.runLocal( self.dLHCb )  # Can't upload, so it will fail
+#    self.assertFalse( res['OK'] )
+#
+#    del os.environ['JOBID']
+#
+# class GaudirunSuccess( UserJobTestCase ):
+#  def test_mc( self ):
+#
+#    lhcbJob = LHCbJob()
+#
+#    lhcbJob.setName( "gaudirun-test" )
+#    lhcbJob.setInputSandbox( 'prodConf_Gauss_00012345_00067890_1.py' )
+#
+#    optGauss = "$APPCONFIGOPTS/Gauss/Beam4000GeV-md100-JulSep2012-nu2.5.py;"
+#    optDec = "$DECFILESROOT/options/15512012.py;"
+#    optPythia = "$LBPYTHIAROOT/options/Pythia.py;"
+#    optOpts = " $APPCONFIGOPTS/Gauss/G4PL_LHEP_EmNoCuts.py;"
+#    optCompr = "$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py;"
+#    optPConf = "prodConf_Gauss_00012345_00067890_1.py"
+#    options = optGauss + optDec + optPythia + optOpts + optCompr + optPConf
+#    lhcbJob.addPackage( 'AppConfig', 'v3r160' )
+#    lhcbJob.addPackage( 'DecFiles', 'v26r24' )
+#    lhcbJob.addPackage( 'ProdConf', 'v1r9' )
+#
+#    lhcbJob.setApplication( 'Gauss', 'v42r4', options,
+#                            extraPackages = 'AppConfig.v3r160;DecFiles.v26r24;ProdConf.v1r9' )
+#
+#    res = lhcbJob.runLocal( self.dLHCb )
+#    self.assertTrue( res['OK'] )
+#
+#  def test_boole( self ):
+#
+#    lhcbJob = LHCbJob()
+#
+#    lhcbJob.setName( "gaudirun-test-inputs" )
+#    lhcbJob.setInputSandbox( 'prodConf_Boole_00012345_00067890_1.py' )
+#
+#    opts = "$APPCONFIGOPTS/Boole/Default.py;"
+#    optDT = "$APPCONFIGOPTS/Boole/DataType-2012.py;"
+#    optTCK = "$APPCONFIGOPTS/L0/L0TCK-0x0042.py;"
+#    optComp = "$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py;"
+#    optPConf = "prodConf_Boole_00012345_00067890_1.py"
+#    options = opts + optDT + optTCK + optComp + optPConf
+#    lhcbJob.addPackage( 'AppConfig', 'v3r155' )
+#
+#    lhcbJob.setApplication( 'Boole', 'v24r0', options,
+#                            inputData = '/lhcb/user/f/fstagni/test/12345/12345678/00012345_00067890_1.sim',
+#                            extraPackages = 'AppConfig.v3r155;ProdConf.v1r9' )
+#
+#    res = lhcbJob.runLocal( self.dLHCb )
+#    self.assertTrue( res['OK'] )
 
 # class GaudiScriptSuccess( UserJobTestCase ):
 #  def test_execute( self ):
