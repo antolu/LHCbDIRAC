573ad599b41ecdc58891b8ccfeb4bae2e7c8a1a0
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-06-24 16:35:09 +0000

    .
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@67665 4525493e-7705-40b1-a816-d608a930855b

--- tests/Integration/Test_ProductionJobs.py
+++ tests/Integration/Test_ProductionJobs.py
@@ -67,32 +67,6 @@ class MCSuccess( ProductionJobTestCase ):
     for found, expected in getOutput( 'MC' ):
       self.assertEqual( found, expected )
 
-class RecoSuccess( ProductionJobTestCase ):
-  def test_execute( self ):
-    lfns = ['/lhcb/data/2012/RAW/FULL/LHCb/COLLISION12/114753/114753_0000000296.raw']
-    # From request 8772
-    stepsInProd = [{'StepId': 38427, 'StepName': 'Reco14', 'ApplicationName': 'Brunel', 'ApplicationVersion': 'v43r2p2',
-                    'ExtraPackages': 'AppConfig.v3r149', 'ProcessingPass': 'Reco14', 'Visible': 'Y', 'Usable': 'Yes',
-                    'DDDB': 'dddb-20120831', 'CONDDB': 'cond-20120831', 'DQTag': '', 'OptionsFormat': '',
-                    'OptionFiles': '$APPCONFIGOPTS/Brunel/DataType-2012.py',
-                    'fileTypesIn':['RAW'],
-                    'fileTypesOut':['BRUNELHIST', 'FULL.DST']},
-                   {'StepId': 38510, 'StepName': 'DataQuality-FULL', 'ApplicationName': 'DaVinci', 'ApplicationVersion': 'v32r2',
-                    'ExtraPackages': 'AppConfig.v3r149', 'ProcessingPass': 'DataQuality-FULL', 'Visible': 'N', 'Usable': 'Yes',
-                    'DDDB': 'dddb-20120831', 'CONDDB': 'cond-20120831', 'DQTag': '', 'OptionsFormat': 'DQ',
-                    'OptionFiles': '$APPCONFIGOPTS/DaVinci/DVMonitor-RealData.py;$APPCONFIGOPTS/DaVinci/DataType-2012.py;$APPCONFIGOPTS/DaVinci/DaVinci-InputType-SDST.py',
-                    'fileTypesIn':['FULL.DST'],
-                    'fileTypesOut':['DAVINCIHIST']}
-                   ]
-
-    prod = self.pr._buildProduction( 'Reconstruction', stepsInProd, '', 'Tier1-BUFFER', 0, 100,
-                                     inputDataPolicy = 'protocol', inputDataList = lfns, events = 25 )
-    res = self.diracProduction.launchProduction( prod, False, True, 0 )
-    self.assertTrue( res['OK'] )
-
-    for found, expected in getOutput( 'Reco' ):
-      self.assertEqual( found, expected )
-
 class MCSuccess_new( ProductionJobTestCase ):
   def test_execute( self ):
 
@@ -128,90 +102,116 @@ class MCSuccess_new( ProductionJobTestCase ):
     for found, expected in getOutput( 'MC' ):
       self.assertEqual( found, expected )
 
-
-class StrippSuccess( ProductionJobTestCase ):
-  def test_execute( self ):
-    lfns = ['/lhcb/LHCb/Collision12/FULL.DST/00020330/0004/00020330_00047632_1.full.dst']
-    # From request 8891
-    stepsInProd = [{'StepId': 123715, 'StepName': 'Stripping20', 'ApplicationName': 'DaVinci', 'ApplicationVersion': 'v32r2p1',
-                    'ExtraPackages': 'AppConfig.v3r151', 'ProcessingPass': 'Stripping20', 'Visible': 'Y', 'Usable': 'Yes',
-                    'DDDB': 'dddb-20120831', 'CONDDB': 'cond-20120929', 'DQTag': '', 'OptionsFormat': 'Stripping',
-                    'OptionFiles': '$APPCONFIGOPTS/DaVinci/DV-Stripping20-Stripping.py;$APPCONFIGOPTS/DaVinci/DataType-2012.py;$APPCONFIGOPTS/DaVinci/InputType-DST.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py',
-                    'fileTypesIn':['SDST'],
-                    'fileTypesOut':['BHADRON.MDST', 'BHADRONCOMPLETEEVENT.DST', 'CALIBRATION.DST', 'CHARM.MDST', 'CHARMCOMPLETEEVENT.DST', 'CHARMCONTROL.DST', 'CHARMTOBESWUM.DST', 'DIMUON.DST', 'EW.DST', 'LEPTONIC.MDST', 'MINIBIAS.DST', 'PID.MDST', 'RADIATIVE.DST', 'SEMILEPTONIC.DST']},
-                   ]
-
-    prod = self.pr._buildProduction( 'Stripping', stepsInProd, '', 'Tier1-BUFFER', 0, 100,
-                                     inputDataPolicy = 'protocol', inputDataList = lfns, events = 500 )
-    res = self.diracProduction.launchProduction( prod, False, True, 0 )
-    self.assertTrue( res['OK'] )
-
-    for found, expected in getOutput( 'Stripp' ):
-      self.assertEqual( found, expected )
-
-class MergeSuccess( ProductionJobTestCase ):
-  def test_execute( self ):
-    lfns = ['/lhcb/LHCb/Collision12/FMDST/00020751/0000/00020751_00000280_1.fmdst',
-            '/lhcb/LHCb/Collision12/FMDST/00020751/0000/00020751_00000357_1.fmdst']
-    # From request 9085
-    stepsInProd = [{'StepId': 17420, 'StepName': 'MergeFMDST', 'ApplicationName': 'LHCb', 'ApplicationVersion': 'v34r2',
-                    'ExtraPackages': 'AppConfig.v3r134', 'ProcessingPass': 'MergeFMDST', 'Visible': 'N', 'Usable': 'Yes',
-                    'DDDB': '', 'CONDDB': '', 'DQTag': '', 'OptionsFormat': '',
-                    'OptionFiles': '$APPCONFIGOPTS/Merging/CopyDST.py',
-                    'fileTypesIn':['FMDST'],
-                    'fileTypesOut':['FMDST']},
-                   ]
-
-    prod = self.pr._buildProduction( 'Merge', stepsInProd, '', 'Tier1_MC-DST', 0, 100,
-                                     inputDataPolicy = 'protocol', inputDataList = lfns )
-    res = self.diracProduction.launchProduction( prod, False, True, 0 )
-    self.assertTrue( res['OK'] )
-
-    for found, expected in getOutput( 'Merge' ):
-      self.assertEqual( found, expected )
-
-class MergeMultStreamsSuccess( ProductionJobTestCase ):
-  def test_execute( self ):
-    lfns = ['/lhcb/LHCb/Collision12/LEPTONIC.MDST/00021210/0000/00021210_00002482_1.Leptonic.mdst',
-            '/lhcb/LHCb/Collision12/LEPTONIC.MDST/00021210/0000/00021210_00002481_1.Leptonic.mdst']
-    # From request 9085
-    stepsInProd = [{'StepId': 54132, 'StepName': 'Merging', 'ApplicationName': 'DaVinci', 'ApplicationVersion': 'v32r2p1',
-                    'ExtraPackages': 'AppConfig.v3r150', 'ProcessingPass': 'Merging', 'Visible': 'N', 'Usable': 'Yes',
-                    'DDDB': '', 'CONDDB': '', 'DQTag': '', 'OptionsFormat': 'Merge',
-                    'OptionFiles': '$APPCONFIGOPTS/Merging/DV-Stripping-Merging.py',
-                    'fileTypesIn':['BHADRON.MDST', 'BHADRONCOMPLETEEVENT.DST', 'CALIBRATION.DST', 'CHARM.MDST',
-                                   'CHARMCOMPLETEEVENT.DST', 'CHARMCONTROL.DST', 'DIMUON.DST', 'EW.DST',
-                                   'LEPTONIC.MDST', 'MINIBIAS.DST', 'PID.MDST', 'RADIATIVE.DST', 'SEMILEPTONIC.DST'],
-                    'fileTypesOut':['BHADRON.MDST', 'BHADRONCOMPLETEEVENT.DST', 'CALIBRATION.DST', 'CHARM.MDST',
-                                   'CHARMCOMPLETEEVENT.DST', 'CHARMCONTROL.DST', 'DIMUON.DST', 'EW.DST',
-                                   'LEPTONIC.MDST', 'MINIBIAS.DST', 'PID.MDST', 'RADIATIVE.DST', 'SEMILEPTONIC.DST']},
-                   ]
-
-    prod = self.pr._buildProduction( 'Merge', stepsInProd, '', 'Tier1_MC-DST', 0, 100,
-                                     inputDataPolicy = 'protocol', inputDataList = lfns )
-    res = self.diracProduction.launchProduction( prod, False, True, 0 )
-    self.assertTrue( res['OK'] )
-
-    for found, expected in getOutput( 'MergeM' ):
-      self.assertEqual( found, expected )
-
-class MergeMDFSuccess( ProductionJobTestCase ):
-  def test_execute( self ):
-    lfns = ['/lhcb/data/2011/RAW/EXPRESS/LHCb/COLLISION11/102360/102360_0000000031.raw',
-            '/lhcb/data/2011/RAW/EXPRESS/LHCb/COLLISION11/97887/097887_0000000013.raw']
-    # From request 9054
-    stepsInProd = [{'StepId': 123897, 'StepName': 'MergeMDF', 'ApplicationName': 'MergeMDF', 'ApplicationVersion': '',
-                    'ExtraPackages': '', 'ProcessingPass': 'Merging', 'Visible': 'Y', 'Usable': 'Yes',
-                    'DDDB': '', 'CONDDB': '', 'DQTag': '', 'OptionsFormat': '',
-                    'OptionFiles': '',
-                    'fileTypesIn':['RAW'],
-                    'fileTypesOut':['RAW']},
-                   ]
-    self.pr.modulesList = ['MergeMDF', 'BookkeepingReport']
-    prod = self.pr._buildProduction( 'Merge', stepsInProd, '', 'Tier1-BUFFER', 0, 100,
-                                     inputDataPolicy = 'download', inputDataList = lfns )
-    res = self.diracProduction.launchProduction( prod, False, True, 0 )
-    self.assertTrue( res['OK'] )
+#
+# class RecoSuccess( ProductionJobTestCase ):
+#  def test_execute( self ):
+#    lfns = ['/lhcb/data/2012/RAW/FULL/LHCb/COLLISION12/114753/114753_0000000296.raw']
+#    # From request 8772
+#    stepsInProd = [{'StepId': 38427, 'StepName': 'Reco14', 'ApplicationName': 'Brunel', 'ApplicationVersion': 'v43r2p2',
+#                    'ExtraPackages': 'AppConfig.v3r149', 'ProcessingPass': 'Reco14', 'Visible': 'Y', 'Usable': 'Yes',
+#                    'DDDB': 'dddb-20120831', 'CONDDB': 'cond-20120831', 'DQTag': '', 'OptionsFormat': '',
+#                    'OptionFiles': '$APPCONFIGOPTS/Brunel/DataType-2012.py',
+#                    'fileTypesIn':['RAW'],
+#                    'fileTypesOut':['BRUNELHIST', 'FULL.DST']},
+#                   {'StepId': 38510, 'StepName': 'DataQuality-FULL', 'ApplicationName': 'DaVinci', 'ApplicationVersion': 'v32r2',
+#                    'ExtraPackages': 'AppConfig.v3r149', 'ProcessingPass': 'DataQuality-FULL', 'Visible': 'N', 'Usable': 'Yes',
+#                    'DDDB': 'dddb-20120831', 'CONDDB': 'cond-20120831', 'DQTag': '', 'OptionsFormat': 'DQ',
+#                    'OptionFiles': '$APPCONFIGOPTS/DaVinci/DVMonitor-RealData.py;$APPCONFIGOPTS/DaVinci/DataType-2012.py;$APPCONFIGOPTS/DaVinci/DaVinci-InputType-SDST.py',
+#                    'fileTypesIn':['FULL.DST'],
+#                    'fileTypesOut':['DAVINCIHIST']}
+#                   ]
+#
+#    prod = self.pr._buildProduction( 'Reconstruction', stepsInProd, '', 'Tier1-BUFFER', 0, 100,
+#                                     inputDataPolicy = 'protocol', inputDataList = lfns, events = 25 )
+#    res = self.diracProduction.launchProduction( prod, False, True, 0 )
+#    self.assertTrue( res['OK'] )
+#
+#    for found, expected in getOutput( 'Reco' ):
+#      self.assertEqual( found, expected )
+#
+# class StrippSuccess( ProductionJobTestCase ):
+#  def test_execute( self ):
+#    lfns = ['/lhcb/LHCb/Collision12/FULL.DST/00020330/0004/00020330_00047632_1.full.dst']
+#    # From request 8891
+#    stepsInProd = [{'StepId': 123715, 'StepName': 'Stripping20', 'ApplicationName': 'DaVinci', 'ApplicationVersion': 'v32r2p1',
+#                    'ExtraPackages': 'AppConfig.v3r151', 'ProcessingPass': 'Stripping20', 'Visible': 'Y', 'Usable': 'Yes',
+#                    'DDDB': 'dddb-20120831', 'CONDDB': 'cond-20120929', 'DQTag': '', 'OptionsFormat': 'Stripping',
+#                    'OptionFiles': '$APPCONFIGOPTS/DaVinci/DV-Stripping20-Stripping.py;$APPCONFIGOPTS/DaVinci/DataType-2012.py;$APPCONFIGOPTS/DaVinci/InputType-DST.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py',
+#                    'fileTypesIn':['SDST'],
+#                    'fileTypesOut':['BHADRON.MDST', 'BHADRONCOMPLETEEVENT.DST', 'CALIBRATION.DST', 'CHARM.MDST', 'CHARMCOMPLETEEVENT.DST', 'CHARMCONTROL.DST', 'CHARMTOBESWUM.DST', 'DIMUON.DST', 'EW.DST', 'LEPTONIC.MDST', 'MINIBIAS.DST', 'PID.MDST', 'RADIATIVE.DST', 'SEMILEPTONIC.DST']},
+#                   ]
+#
+#    prod = self.pr._buildProduction( 'Stripping', stepsInProd, '', 'Tier1-BUFFER', 0, 100,
+#                                     inputDataPolicy = 'protocol', inputDataList = lfns, events = 500 )
+#    res = self.diracProduction.launchProduction( prod, False, True, 0 )
+#    self.assertTrue( res['OK'] )
+#
+#    for found, expected in getOutput( 'Stripp' ):
+#      self.assertEqual( found, expected )
+#
+# class MergeSuccess( ProductionJobTestCase ):
+#  def test_execute( self ):
+#    lfns = ['/lhcb/LHCb/Collision12/FMDST/00020751/0000/00020751_00000280_1.fmdst',
+#            '/lhcb/LHCb/Collision12/FMDST/00020751/0000/00020751_00000357_1.fmdst']
+#    # From request 9085
+#    stepsInProd = [{'StepId': 17420, 'StepName': 'MergeFMDST', 'ApplicationName': 'LHCb', 'ApplicationVersion': 'v34r2',
+#                    'ExtraPackages': 'AppConfig.v3r134', 'ProcessingPass': 'MergeFMDST', 'Visible': 'N', 'Usable': 'Yes',
+#                    'DDDB': '', 'CONDDB': '', 'DQTag': '', 'OptionsFormat': '',
+#                    'OptionFiles': '$APPCONFIGOPTS/Merging/CopyDST.py',
+#                    'fileTypesIn':['FMDST'],
+#                    'fileTypesOut':['FMDST']},
+#                   ]
+#
+#    prod = self.pr._buildProduction( 'Merge', stepsInProd, '', 'Tier1_MC-DST', 0, 100,
+#                                     inputDataPolicy = 'protocol', inputDataList = lfns )
+#    res = self.diracProduction.launchProduction( prod, False, True, 0 )
+#    self.assertTrue( res['OK'] )
+#
+#    for found, expected in getOutput( 'Merge' ):
+#      self.assertEqual( found, expected )
+#
+# class MergeMultStreamsSuccess( ProductionJobTestCase ):
+#  def test_execute( self ):
+#    lfns = ['/lhcb/LHCb/Collision12/LEPTONIC.MDST/00021210/0000/00021210_00002482_1.Leptonic.mdst',
+#            '/lhcb/LHCb/Collision12/LEPTONIC.MDST/00021210/0000/00021210_00002481_1.Leptonic.mdst']
+#    # From request 9085
+#    stepsInProd = [{'StepId': 54132, 'StepName': 'Merging', 'ApplicationName': 'DaVinci', 'ApplicationVersion': 'v32r2p1',
+#                    'ExtraPackages': 'AppConfig.v3r150', 'ProcessingPass': 'Merging', 'Visible': 'N', 'Usable': 'Yes',
+#                    'DDDB': '', 'CONDDB': '', 'DQTag': '', 'OptionsFormat': 'Merge',
+#                    'OptionFiles': '$APPCONFIGOPTS/Merging/DV-Stripping-Merging.py',
+#                    'fileTypesIn':['BHADRON.MDST', 'BHADRONCOMPLETEEVENT.DST', 'CALIBRATION.DST', 'CHARM.MDST',
+#                                   'CHARMCOMPLETEEVENT.DST', 'CHARMCONTROL.DST', 'DIMUON.DST', 'EW.DST',
+#                                   'LEPTONIC.MDST', 'MINIBIAS.DST', 'PID.MDST', 'RADIATIVE.DST', 'SEMILEPTONIC.DST'],
+#                    'fileTypesOut':['BHADRON.MDST', 'BHADRONCOMPLETEEVENT.DST', 'CALIBRATION.DST', 'CHARM.MDST',
+#                                   'CHARMCOMPLETEEVENT.DST', 'CHARMCONTROL.DST', 'DIMUON.DST', 'EW.DST',
+#                                   'LEPTONIC.MDST', 'MINIBIAS.DST', 'PID.MDST', 'RADIATIVE.DST', 'SEMILEPTONIC.DST']},
+#                   ]
+#
+#    prod = self.pr._buildProduction( 'Merge', stepsInProd, '', 'Tier1_MC-DST', 0, 100,
+#                                     inputDataPolicy = 'protocol', inputDataList = lfns )
+#    res = self.diracProduction.launchProduction( prod, False, True, 0 )
+#    self.assertTrue( res['OK'] )
+#
+#    for found, expected in getOutput( 'MergeM' ):
+#      self.assertEqual( found, expected )
+#
+# class MergeMDFSuccess( ProductionJobTestCase ):
+#  def test_execute( self ):
+#    lfns = ['/lhcb/data/2011/RAW/EXPRESS/LHCb/COLLISION11/102360/102360_0000000031.raw',
+#            '/lhcb/data/2011/RAW/EXPRESS/LHCb/COLLISION11/97887/097887_0000000013.raw']
+#    # From request 9054
+#    stepsInProd = [{'StepId': 123897, 'StepName': 'MergeMDF', 'ApplicationName': 'MergeMDF', 'ApplicationVersion': '',
+#                    'ExtraPackages': '', 'ProcessingPass': 'Merging', 'Visible': 'Y', 'Usable': 'Yes',
+#                    'DDDB': '', 'CONDDB': '', 'DQTag': '', 'OptionsFormat': '',
+#                    'OptionFiles': '',
+#                    'fileTypesIn':['RAW'],
+#                    'fileTypesOut':['RAW']},
+#                   ]
+#    self.pr.modulesList = ['MergeMDF', 'BookkeepingReport']
+#    prod = self.pr._buildProduction( 'Merge', stepsInProd, '', 'Tier1-BUFFER', 0, 100,
+#                                     inputDataPolicy = 'download', inputDataList = lfns )
+#    res = self.diracProduction.launchProduction( prod, False, True, 0 )
+#    self.assertTrue( res['OK'] )
 
 #############################################################################
 # Test Suite run
@@ -220,9 +220,10 @@ class MergeMDFSuccess( ProductionJobTestCase ):
 if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase( ProductionJobTestCase )
   suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( MCSuccess ) )
-  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( RecoSuccess ) )
-  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( StrippSuccess ) )
-  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( MergeSuccess ) )
-  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( MergeMultStreamsSuccess ) )
-  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( MergeMDFSuccess ) )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( MCSuccess_new ) )
+#  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( RecoSuccess ) )
+#  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( StrippSuccess ) )
+#  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( MergeSuccess ) )
+#  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( MergeMultStreamsSuccess ) )
+#  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( MergeMDFSuccess ) )
   testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
