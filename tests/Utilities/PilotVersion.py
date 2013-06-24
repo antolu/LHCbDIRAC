6289932f245a55c6bdb54e2960ff0ae2c6e9e49a
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-06-24 15:40:21 +0000

    Util to get PilotVersion
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@67660 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Utilities/PilotVersion.py
@@ -0,0 +1,19 @@
+import sys
+
+setup = sys.argv[ 1 ]
+    
+from DIRAC.Core.Base.Script import parseCommandLine
+parseCommandLine()
+
+from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
+pilotVersions = Operations( setup = setup ).getValue( 'Pilot/Version', [] )
+
+pilotVersions = [ LooseVersion( pV ) for pV in pilotVersions ]
+pilotVersions.sort()
+    
+print pilotVersions.pop().vstring
+
+sys.exit( 0 )
+
+#...............................................................................
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
