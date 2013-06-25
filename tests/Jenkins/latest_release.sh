6ed7cf4029aba4e6df577c22614a40eace948641
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-06-25 14:07:34 +0000

    NEW: Jenkins scripts
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@67731 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Jenkins/latest_release.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+#-------------------------------------------------------------------------------
+# latest_release
+#  
+# : gets releases.cfg and returns latest_release
+#  
+# ubeda@cern.ch  
+# 10/VI/2013
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# Parse Input Parameters
+#-------------------------------------------------------------------------------
+
+# Execute getopt
+ARGS=`getopt -o "p" -l "pre" -n "getopt.sh" -- "$@"`
+ 
+#Bad arguments
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+ 
+# A little magic
+eval set -- "$ARGS"
+
+PRE='p[[:digit:]]*' 
+ 
+# Now go through all the options
+while true;
+do
+  case "$1" in
+    -p|--pre) PRE='-pre'; shift 2 ;;
+    --) shift      ; break   ;;
+    *)  break ;;
+  esac
+done
+
+#-------------------------------------------------------------------------------
+# Get releases.cfg
+#-------------------------------------------------------------------------------
+
+tmp_dir=`mktemp -d -q`
+  
+cd $tmp_dir
+
+wget http://svn.cern.ch/guest/dirac/LHCbDIRAC/trunk/LHCbDIRAC/releases.cfg --quiet
+
+latest=`cat releases.cfg | grep [^:]v[[:digit:]]r[[:digit:]]*$PRE | head -1 | sed 's/ //g'`
+
+echo LHCbDIRAC Release: $latest
+
+s=`cat releases.cfg | grep -n $latest | cut -d ':' -f 1 | head -1`  
+s=$(($s+2))
+e=$(($s+3))
+versions=`sed -n "$s,$e p" releases.cfg`
+
+dirac=`echo $versions | tr ' ' '\n' | grep ^DIRAC:v*[^,] | sed 's/,//g' | cut -d ':' -f2`
+lhcbdirac=`echo $versions | tr ' ' '\n' | grep ^LHCbDIRAC:v* | sed 's/,//g' | cut -d ':' -f2`
+  
+cd - >> /dev/null 
+rm -r $tmp_dir
+
+echo DIRAC:$dirac && echo $dirac > dirac.version
+echo LHCbDIRAC:$lhcbdirac && echo $lhcbdirac > lhcbdirac.version
+
+#-------------------------------------------------------------------------------
+#EOF
