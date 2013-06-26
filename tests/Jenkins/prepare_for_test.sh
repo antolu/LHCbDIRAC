d4a2ebee2e85273e6ff750c320e45e22edd190db
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-06-26 13:26:00 +0000

    FIX: new script, prepare_for_test
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@67906 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/prepare_for_test.sh
+++ tests/Jenkins/prepare_for_test.sh
@@ -14,6 +14,59 @@
 set -o errexit
 
 #-------------------------------------------------------------------------------
+# findRelease:
+#
+#   If any parameter is passed, we assume we are on pre-release mode, otherwise, 
+#   we assume production. It reads from releases.cfg and picks the latest version
+#   which is written to {project,dirac,lhcbdirac}.version
+#
+#-------------------------------------------------------------------------------
+findRelease(){
+
+  [ $1 ] && PRE='-pre' || PRE='p[[:digit:]]*'  
+  
+  tmp_dir=`mktemp -d -q`
+  
+  cd $tmp_dir
+
+  wget http://svn.cern.ch/guest/dirac/LHCbDIRAC/trunk/LHCbDIRAC/releases.cfg --quiet
+
+  project=`cat releases.cfg | grep [^:]v[[:digit:]]r[[:digit:]]*$PRE | head -1 | sed 's/ //g'`
+
+  s=`cat releases.cfg | grep -n $project | cut -d ':' -f 1 | head -1`  
+  s=$(($s+2))
+  e=$(($s+3))
+  versions=`sed -n "$s,$e p" releases.cfg`
+
+  dirac=`echo $versions | tr ' ' '\n' | grep ^DIRAC:v*[^,] | sed 's/,//g' | cut -d ':' -f2`
+  lhcbdirac=`echo $versions | tr ' ' '\n' | grep ^LHCbDIRAC:v* | sed 's/,//g' | cut -d ':' -f2`
+  
+  cd - >> /dev/null 
+  rm -r $tmp_dir
+
+  echo PROJECT:$project && echo $project > project.version
+  echo DIRAC:$dirac && echo $dirac > dirac.version
+  echo LHCbDIRAC:$lhcbdirac && echo $lhcbdirac > lhcbdirac.version
+
+}
+
+#-------------------------------------------------------------------------------
+# findDatabases:
+#
+#   gets all database names from *DIRAC code and writes them to a file
+#   named databases.
+#
+#-------------------------------------------------------------------------------
+
+findDatabases(){
+
+  find *DIRAC -name *DB.sql | uniq | awk -F "/" '{print $2,$4}' > databases
+
+  echo found `wc -l databases`
+
+}
+
+#-------------------------------------------------------------------------------
 # diracInstall:
 #
 #   gets `project.version` code from the repository and copies certificates
