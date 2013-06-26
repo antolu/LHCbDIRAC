6a36c8ea0b39c346b354dafd6778eb491c4138f7
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-06-26 08:17:15 +0000

    NEW: add script to find all databases
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@67855 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Jenkins/find_databases.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#-------------------------------------------------------------------------------
+# find_databases
+#  
+# : gets from DIRAC and any of the installed Extensions the .sql files which
+# : correspond to a DB to be installed.
+#  
+# ubeda@cern.ch  
+# 26/VI/2013
+#-------------------------------------------------------------------------------
+
+find *DIRAC -name *DB.sql` | uniq | awk -F "/" '{print $2,$4} > databases
+
+cat databases
+
+#-------------------------------------------------------------------------------
+#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
