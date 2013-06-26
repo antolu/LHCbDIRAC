a0e42c63221bf26140fb248fa20e1f11b2a68051
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-06-26 13:55:27 +0000

    FIX: just database names
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@67909 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/prepare_for_test.sh
+++ tests/Jenkins/prepare_for_test.sh
@@ -60,7 +60,7 @@ findRelease(){
 
 findDatabases(){
 
-  find *DIRAC -name *DB.sql | uniq | awk -F "/" '{print $2,$4}' > databases
+  find *DIRAC -name *DB.sql | uniq | awk -F "/" '{print $4}' | cut -d '.' -f1 > databases
 
   echo found `wc -l databases`
 
