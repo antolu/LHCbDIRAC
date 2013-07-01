2444983e0e057253c738f4e792800b71598c7e8f
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-07-01 13:26:15 +0000

    FIX: use server certificate
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@68141 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/prepare_for_test.sh
+++ tests/Jenkins/prepare_for_test.sh
@@ -137,7 +137,7 @@ diracConfigure(){
   cp $WORKSPACE/LHCbTestDirac/Jenkins/install.cfg etc/
 
   #dirac-configure -o $exts -o $useCert -o $certFile -o $keyFile -A $arch -o $rootPass -o $userPass -o $hostPath $setups -o $hostDN -o $services -o $cMaster -o $cName -S Jenkins
-  dirac-configure etc/install.cfg -A $arch -o $rootPass -o $userPass -o $certFile -o $keyFile -d
+  dirac-configure etc/install.cfg -A $arch -o $rootPass -o $userPass -o $certFile -o $keyFile -o $useCert -d
   dirac-setup-site
   
 }  
