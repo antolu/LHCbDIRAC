27680ccf999a3d988b0f1c49d0a7c8c0651c06be
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-07-01 10:52:05 +0000

    Introduced TransformationCleaned status
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@68116 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Utilities/StateMachine.py
+++ LHCbDIRAC/ProductionManagementSystem/Utilities/StateMachine.py
@@ -15,22 +15,27 @@ class ProductionsStateMachine( StateMachine ):
 
     # Current states
     self.states = {
-                   'Cleaned'          : State( 15 ),  # final state
-                   'Cleaning'         : State( 14, ['Cleaned'] ),
-                   'Completing'       : State( 13, ['Validating', 'Cleaning'], defState = 'Validating' ),
-                   'Stopped'          : State( 12, ['Active', 'Flush', 'Cleaning'], defState = 'Active' ),
-                   'Archived'         : State( 11 ),  # final state
-                   'Completed'        : State( 10, ['Archived'] ),
-                   'WaitingIntegrity' : State( 9, ['ValidatedOutput'] ),
-                   'ValidatedOutput'  : State( 7, ['Active', 'Completed', 'Cleaning'], defState = 'Completed' ),
-                   'ValidatingOutput' : State( 7, ['Active', 'ValidatedOutput', 'WaitingIntegrity'], defState = 'ValidatedOutput' ),
-                   'RemovedFiles'     : State( 6, ['Completed'] ),
-                   'RemovingFiles'    : State( 5, ['RemovedFiles'] ),
-                   'ValidatingInput'  : State( 4, ['Active', 'RemovingFiles', 'Cleaning'], defState = 'Active' ),
-                   'Flush'            : State( 3, ['Active', 'Cleaning'], defState = 'Active' ),
-                   'Idle'             : State( 2, ['Active', 'ValidatingInput', 'ValidatingOutput'], defState = 'Active' ),
-                   'Active'           : State( 1, ['Flush', 'Idle', 'Stopped', 'Completing', 'ValidatingInput', 'ValidatingOutput', 'Cleaning'], defState = 'Flush' ),
-                   'New'              : State( 0, ['Active', 'Cleaning'], defState = 'Active' )  # initial state
+                   'Cleaned'              : State( 15 ),  # final state
+                   'Cleaning'             : State( 14, ['Cleaned'] ),
+                   'Completing'           : State( 13, ['Validating', 'Cleaning'], defState = 'Validating' ),
+                   'Stopped'              : State( 12, ['Active', 'Flush', 'Cleaning'], defState = 'Active' ),
+                   'TransformationCleaned': State( 16, ['Archived', 'Cleaned'], defState = 'Archived' ),
+                   'Archived'             : State( 11 ),  # final state
+                   'Completed'            : State( 10, ['Archived'] ),
+                   'WaitingIntegrity'     : State( 9, ['ValidatedOutput'] ),
+                   'ValidatedOutput'      : State( 7, ['Active', 'Completed', 'Cleaning'], defState = 'Completed' ),
+                   'ValidatingOutput'     : State( 7, ['Active', 'ValidatedOutput',
+                                                       'WaitingIntegrity'], defState = 'ValidatedOutput' ),
+                   'RemovedFiles'         : State( 6, ['Completed'] ),
+                   'RemovingFiles'        : State( 5, ['RemovedFiles'] ),
+                   'ValidatingInput'      : State( 4, ['Active', 'RemovingFiles', 'Cleaning'], defState = 'Active' ),
+                   'Flush'                : State( 3, ['Active', 'Cleaning'], defState = 'Active' ),
+                   'Idle'                 : State( 2, ['Active', 'ValidatingInput',
+                                                       'ValidatingOutput'], defState = 'Active' ),
+                   'Active'               : State( 1, ['Flush', 'Idle', 'Stopped', 'Completing', 'ValidatingInput',
+                                                       'ValidatingOutput', 'Cleaning', 'TransformationCleane'],
+                                                  defState = 'Flush' ),
+                   'New'                  : State( 0, ['Active', 'Cleaning'], defState = 'Active' )  # initial state
                   }
 
     # NEW states proposal
@@ -38,6 +43,7 @@ class ProductionsStateMachine( StateMachine ):
 #                   'Cleaned'    : State( 10 ), # final state
 #                   'Cleaning'   : State( 9, ['Cleaned'] ),
 #                   'Completing' : State( 8, ['Validating', 'Cleaning'], defState = 'Validating' ),
+#                   'TransformationCleaned': State( 16, ['Archived', 'Cleaned'], defState = 'Archived' ),
 #                   'Stopped'    : State( 7, ['Active', 'Flush', 'Cleaning'], defState = 'Active' ),
 #                   'Archived'   : State( 6 ),  # final state
 #                   'Completed'  : State( 5, ['Archived'], defState = 'Archived' ),
