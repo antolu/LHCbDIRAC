fcff4134dc821a73dc3ee9afc31aced42653493b
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-07-08 18:17:16 +0000

    FIX: remove TransferDB.sql
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@68417 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/prepare_for_test.sh
+++ tests/Jenkins/prepare_for_test.sh
@@ -60,7 +60,7 @@ findRelease(){
 
 findDatabases(){
 
-  find *DIRAC -name *DB.sql | awk -F "/" '{print $2,$4}' | sort | uniq > databases
+  find *DIRAC -name *DB.sql | grep -v TransferDB.sql | awk -F "/" '{print $2,$4}' | sort | uniq > databases
 
   echo found `wc -l databases`
 
@@ -229,7 +229,7 @@ finalCleanup(){
 
 diracDBs(){
 
-  dbs=`cat databases | grep -v TransferDB.sql | cut -d ' ' -f 2 | cut -d '.' -f 1`
+  dbs=`cat databases | cut -d ' ' -f 2 | cut -d '.' -f 1`
   for db in $dbs
   do
     dirac-install-db $db
