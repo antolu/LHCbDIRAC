66a9bc9b237957216b87b6e8b89ba24b036d7859
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-07-10 15:13:09 +0000

    FIX: added RequestDB to exceptions
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@68546 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci/test/test_db.py
+++ tests/Jenkins/lhcb_ci/test/test_db.py
@@ -104,7 +104,7 @@ class Installation_Test( lhcb_ci.basecase.DB_TestCase ):
     Tries to import the DB modules and create a class Object.
     """
     
-    _EXCEPTIONS = [ 'TransformationDB', 'RAWIntegrityDB' ]
+    _EXCEPTIONS = [ 'TransformationDB', 'RAWIntegrityDB', 'RequestDB' ]
 
     self.log.info( 'test_import_db_modules' )
     
