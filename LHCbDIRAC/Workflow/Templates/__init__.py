949f47e9629875ea8dbd3f7e59ed57af8be34791
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-07-17 13:50:52 +0000

    remove init (not importable)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@68799 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Workflow/Templates/__init__.py
+++
@@ -1,9 +0,0 @@
-############################################################
-
-"""
-   LHCbSystem.WorkflowTemplates package
-
-   This contains the workflow templates for the production request system.
-"""
-
-__RCSID__ = "$Id$"
