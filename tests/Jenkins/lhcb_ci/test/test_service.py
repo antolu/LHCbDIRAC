88ee92a258ca007e19b03680e47f79aa4a0344d6
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-07-17 09:08:49 +0000

    FIX: more verbose test_service
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@68782 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci/test/test_service.py
+++ tests/Jenkins/lhcb_ci/test/test_service.py
@@ -21,9 +21,14 @@ class Installation_Test( lhcb_ci.basecase.Service_TestCase ):
     
     for system, services in self.codeServices.iteritems():
       
+      if system not in self.installedServices:
+        self.log.exception( 'EXCEPTION: System %s not installed' % system )
+      
       for service in services:
         self.log.debug( "%s %s" % ( system, service ) )
-                        
+      
+        if not service in self.installedServices[ system ]:  
+          self.log.exception( 'EXCEPTION: Service %s/%s not installed' % ( system, service ) )                       
 
 #...............................................................................
 #EOF
