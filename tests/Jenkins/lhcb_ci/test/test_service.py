9f49a76641b47e1e7956e7528c9209e93af22173
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-07-18 15:04:47 +0000

    FIX: test_service, added RunDBInterface to exceptions
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@68877 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci/test/test_service.py
+++ tests/Jenkins/lhcb_ci/test/test_service.py
@@ -47,13 +47,15 @@ class Installation_Test( lhcb_ci.basecase.Service_TestCase ):
     self.logTestName( 'test_run_services' )
     
     _EXCEPTIONS = [ 'BookkeepingManager', 'RequestManager', 'StorageElement', 
-                    'TransferDBMonitoring', 'StorageElementProxy', 'DataUsage' ]
+                    'TransferDBMonitoring', 'StorageElementProxy', 'DataUsage',
+                    'RunDBInterface' ]
     # BookkeepingManager   : cx_Oracle
     # RequestManager       : RequestDB
     # StorageElement       : failed to get base path
     # TransferDBMonitoring : Can not connect to DB RequestDB
     # StorageElementProxy  : failed to get base path
     # DataUsage            : Can not connect to DB StorageUsageDB
+    # RunDBInterface       : from path import SQL_ALCHEMY_PATH
     
     for system, services in self.swServices.iteritems():
       
