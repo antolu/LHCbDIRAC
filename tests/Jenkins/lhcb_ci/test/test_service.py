ad3c198eb92d0bc647553f8bb0b11ca994b04d27
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-07-18 14:53:08 +0000

    FIX: test_service, added StorageElementProxy to exceptions
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@68875 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci/test/test_service.py
+++ tests/Jenkins/lhcb_ci/test/test_service.py
@@ -46,11 +46,13 @@ class Installation_Test( lhcb_ci.basecase.Service_TestCase ):
     
     self.logTestName( 'test_run_services' )
     
-    _EXCEPTIONS = [ 'BookkeepingManager', 'RequestManager', 'StorageElement', 'TransferDBMonitoring' ]
+    _EXCEPTIONS = [ 'BookkeepingManager', 'RequestManager', 'StorageElement', 
+                    'TransferDBMonitoring', 'StorageElementProxy' ]
     # BookkeepingManager   : cx_Oracle
     # RequestManager       : RequestDB
     # StorageElement       : failed to get base path
     # TransferDBMonitoring : Can not connect to DB RequestDB
+    # StorageElementProxy  : failed to get base path
     
     for system, services in self.swServices.iteritems():
       
