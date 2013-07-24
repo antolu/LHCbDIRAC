2696c46973cc92b8b89979d224f9d7476b56d35d
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-07-24 13:18:00 +0000

    FIX: added test_configure
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@69006 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Jenkins/lhcb_ci/test/test_configure.py
@@ -0,0 +1,46 @@
+""" lhcb_ci.test.test_configure
+
+  LHCb Continuous Integration system libraries.
+
+  @author: mario.ubeda.garcia@cern.ch
+  
+"""
+
+
+import lhcb_ci.basecase 
+import lhcb_ci.db
+
+
+class Configure_Test( lhcb_ci.basecase.DB_TestCase ):
+  
+  
+  def test_configure_db( self ):
+    """ test_configure_db
+    
+    Tests that we can configure databases on an "empty CS". This is kind of extremely
+    dirty, because we will carry on with the configured CS for the rest of the tests.
+    """
+    
+    self.logTestName( 'test_configure_db' )
+  
+    for systemName, systemDBs in self.databases.iteritems():   
+      
+      systemName = systemName.replace( 'System', '' )
+      
+      for dbName in systemDBs:
+        
+        res = lhcb_ci.db.configureDB( systemName, dbName )
+        self.assertDIRACEquals( res[ 'OK' ], True, res ) 
+
+
+  #.............................................................................
+  # Nosetests attrs
+  
+  
+  # test_configure_db
+  test_configure_db.configure = 1
+  test_configure_db.db        = 1  
+
+
+#...............................................................................
+#EOF
