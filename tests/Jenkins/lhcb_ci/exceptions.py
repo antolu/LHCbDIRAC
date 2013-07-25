1f7ca6c7fd95dba58f759dabf83d54f58f337e1e
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-07-25 15:07:31 +0000

    NEW: exceptions file
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@69077 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Jenkins/lhcb_ci/exceptions.py
@@ -0,0 +1,19 @@
+""" lhcb_ci.utils.exceptions
+
+  LHCb Continuous Integration system libraries.
+
+  @author: mario.ubeda.garcia@cern.ch
+  
+"""
+
+
+test_configure = {
+                  
+                  
+                  
+                  }
+
+
+
+#...............................................................................
+#EOF
