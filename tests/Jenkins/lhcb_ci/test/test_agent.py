8068567a241e568d04e712bb9f24ce12c676ed03
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-07-25 13:44:57 +0000

    NEW: test_agent
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@69061 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Jenkins/lhcb_ci/test/test_agent.py
@@ -0,0 +1,16 @@
+""" lhcb_ci.test.test_agent
+
+  LHCb Continuous Integration system libraries.
+
+  @author: mario.ubeda.garcia@cern.ch
+  
+"""
+
+class Installation_Test:#( lhcb_ci.basecase.DB_TestCase ):
+  """ Installation_Test
+  
+  Tests performing operations related with the Agents installation.
+  """
+
+#...............................................................................
+#EOF
