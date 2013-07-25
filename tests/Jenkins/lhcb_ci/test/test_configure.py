fbad682d0a3d419ad3bd909d8fd596b68f6d512a
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-07-25 09:46:58 +0000

    FIX: added RunDBInterface exception
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@69046 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci/test/test_configure.py
+++ tests/Jenkins/lhcb_ci/test/test_configure.py
@@ -104,7 +104,7 @@ class Configure_Test( lhcb_ci.basecase.Service_TestCase ):
     
     self.logTestName( 'test_configured_service_ports' )
     
-    _EXCEPTIONS = [ 'LcgFileCatalogProxy' ]
+    _EXCEPTIONS = [ 'LcgFileCatalogProxy', 'RunDBInterface' ]
     
     ports = {}
     
