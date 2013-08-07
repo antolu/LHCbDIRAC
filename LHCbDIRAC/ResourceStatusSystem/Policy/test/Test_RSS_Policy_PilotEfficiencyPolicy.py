e2414f393f5d0e63d449265a26e5c8db02036e67
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-08-07 14:41:30 +0000

    FIX: commented out old tests. Code has been ported to DIRAC.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@69329 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ResourceStatusSystem/Policy/test/Test_RSS_Policy_PilotEfficiencyPolicy.py
+++ LHCbDIRAC/ResourceStatusSystem/Policy/test/Test_RSS_Policy_PilotEfficiencyPolicy.py
@@ -1,92 +1,92 @@
-''' Test_RSS_Policy_PilotEfficiencyPolicy
-'''
-
-import mock
-import unittest
-
-import LHCbDIRAC.ResourceStatusSystem.Policy.PilotEfficiencyPolicy as moduleTested
-
-################################################################################
-
-class PilotEfficiencyPolicy_TestCase( unittest.TestCase ):
-  
-  def setUp( self ):
-    '''
-    Setup
-    '''
-                  
-    self.moduleTested = moduleTested
-    self.testClass    = self.moduleTested.PilotEfficiencyPolicy
-
-  def tearDown( self ):
-    '''
-    Tear down
-    '''
-   
-    del self.moduleTested
-    del self.testClass
-  
-
-################################################################################
-
-class PilotEfficiencyPolicy_Success( PilotEfficiencyPolicy_TestCase ):
-  
-  def test_instantiate( self ):
-    ''' tests that we can instantiate one object of the tested class
-    '''  
-   
-    module = self.testClass()
-    self.assertEqual( 'PilotEfficiencyPolicy', module.__class__.__name__ )
-
-  def test_evaluate( self ):
-    ''' tests the method _evaluate
-    '''  
-        
-    module = self.testClass()
-    
-    res = module._evaluate( { 'OK' : False, 'Message' : 'Bo!' } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Error', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Bo!', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : None } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'No values to take a decision', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'No values to take a decision', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [{}] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'No values to take a decision', res[ 'Value' ][ 'Reason' ] )
-    
-    res  = module._evaluate( { 'OK' : True, 'Value' : [{ 'Aborted' : 0, 'Deleted' : 0,
-                                                         'Done' : 0, 'Failed' : 0 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'No pilots to take a decision', res[ 'Value' ][ 'Reason' ] )    
-    
-    res  = module._evaluate( { 'OK' : True, 'Value' : [{ 'Aborted' : 1, 'Deleted' : 0,
-                                                         'Done' : 1, 'Failed' : 0 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Banned', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Pilots Efficiency of 0.50', res[ 'Value' ][ 'Reason' ] )    
-
-    res  = module._evaluate( { 'OK' : True, 'Value' : [{ 'Aborted' : 0, 'Deleted' : 0,
-                                                         'Done' : 3, 'Failed' : 1 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Degraded', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Pilots Efficiency of 0.75', res[ 'Value' ][ 'Reason' ] )
-
-    res  = module._evaluate( { 'OK' : True, 'Value' : [{ 'Aborted' : 0, 'Deleted' : 0,
-                                                         'Done' : 19, 'Failed' : 1 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Active', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Pilots Efficiency of 0.95', res[ 'Value' ][ 'Reason' ] )
-
-################################################################################    
-#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
+#''' Test_RSS_Policy_PilotEfficiencyPolicy
+#'''
+#
+#import mock
+#import unittest
+#
+#import LHCbDIRAC.ResourceStatusSystem.Policy.PilotEfficiencyPolicy as moduleTested
+#
+#################################################################################
+#
+#class PilotEfficiencyPolicy_TestCase( unittest.TestCase ):
+#  
+#  def setUp( self ):
+#    '''
+#    Setup
+#    '''
+#                  
+#    self.moduleTested = moduleTested
+#    self.testClass    = self.moduleTested.PilotEfficiencyPolicy
+#
+#  def tearDown( self ):
+#    '''
+#    Tear down
+#    '''
+#   
+#    del self.moduleTested
+#    del self.testClass
+#  
+#
+#################################################################################
+#
+#class PilotEfficiencyPolicy_Success( PilotEfficiencyPolicy_TestCase ):
+#  
+#  def test_instantiate( self ):
+#    ''' tests that we can instantiate one object of the tested class
+#    '''  
+#   
+#    module = self.testClass()
+#    self.assertEqual( 'PilotEfficiencyPolicy', module.__class__.__name__ )
+#
+#  def test_evaluate( self ):
+#    ''' tests the method _evaluate
+#    '''  
+#        
+#    module = self.testClass()
+#    
+#    res = module._evaluate( { 'OK' : False, 'Message' : 'Bo!' } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Error', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Bo!', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : None } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'No values to take a decision', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'No values to take a decision', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [{}] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'No values to take a decision', res[ 'Value' ][ 'Reason' ] )
+#    
+#    res  = module._evaluate( { 'OK' : True, 'Value' : [{ 'Aborted' : 0, 'Deleted' : 0,
+#                                                         'Done' : 0, 'Failed' : 0 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'No pilots to take a decision', res[ 'Value' ][ 'Reason' ] )    
+#    
+#    res  = module._evaluate( { 'OK' : True, 'Value' : [{ 'Aborted' : 1, 'Deleted' : 0,
+#                                                         'Done' : 1, 'Failed' : 0 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Banned', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Pilots Efficiency of 0.50', res[ 'Value' ][ 'Reason' ] )    
+#
+#    res  = module._evaluate( { 'OK' : True, 'Value' : [{ 'Aborted' : 0, 'Deleted' : 0,
+#                                                         'Done' : 3, 'Failed' : 1 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Degraded', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Pilots Efficiency of 0.75', res[ 'Value' ][ 'Reason' ] )
+#
+#    res  = module._evaluate( { 'OK' : True, 'Value' : [{ 'Aborted' : 0, 'Deleted' : 0,
+#                                                         'Done' : 19, 'Failed' : 1 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Active', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Pilots Efficiency of 0.95', res[ 'Value' ][ 'Reason' ] )
+#
+#################################################################################    
+##EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
--- LHCbDIRAC/ResourceStatusSystem/Policy/test/Test_RSS_Policy_PilotWebSummaryEfficiencyPolicy.py
+++ LHCbDIRAC/ResourceStatusSystem/Policy/test/Test_RSS_Policy_PilotWebSummaryEfficiencyPolicy.py
@@ -1,102 +1,102 @@
-''' Test_RSS_Policy_PilotWebSummaryEfficiencyPolicy
-'''
-
-import mock
-import unittest
-
-import LHCbDIRAC.ResourceStatusSystem.Policy.PilotWebSummaryEfficiencyPolicy as moduleTested
-
-################################################################################
-
-class PilotWebSummaryEfficiencyPolicy_TestCase( unittest.TestCase ):
-  
-  def setUp( self ):
-    '''
-    Setup
-    '''
-                  
-    self.moduleTested = moduleTested
-    self.testClass    = self.moduleTested.PilotWebSummaryEfficiencyPolicy
-
-  def tearDown( self ):
-    '''
-    Tear down
-    '''
-   
-    del self.moduleTested
-    del self.testClass
-
-################################################################################
-
-class PilotWebSummaryEfficiencyPolicy_Success( PilotWebSummaryEfficiencyPolicy_TestCase ):
-  
-  def test_instantiate( self ):
-    ''' tests that we can instantiate one object of the tested class
-    '''  
-   
-    module = self.testClass()
-    self.assertEqual( 'PilotWebSummaryEfficiencyPolicy', module.__class__.__name__ )
-
-  def test_evaluate( self ):
-    ''' tests the method _evaluate
-    '''
-    
-    module = self.testClass()
-    
-    res = module._evaluate( { 'OK' : False, 'Message' : 'Bo!' } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Error', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Bo!', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : None } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'No values to take a decision', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'No values to take a decision', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'A' : 1 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Error', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( '"Status" key missing', res[ 'Value' ][ 'Reason' ] )    
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 1 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Error', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( '"PilotJobEff" key missing', res[ 'Value' ][ 'Reason' ] )              
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 1, 'PilotJobEff' : 1 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Error', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Unknown status "1"', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 'Good', 'PilotJobEff' : 1 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Active', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Pilots Efficiency: 1 with status Good', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 'Fair', 'PilotJobEff' : 1 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Active', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Pilots Efficiency: 1 with status Fair', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 'Poor', 'PilotJobEff' : 1 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Degraded', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Pilots Efficiency: 1 with status Poor', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 'Idle', 'PilotJobEff' : 1 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Pilots Efficiency: 1 with status Idle', res[ 'Value' ][ 'Reason' ] )
-
-    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 'Bad', 'PilotJobEff' : 1 }] } )
-    self.assertEquals( True, res[ 'OK' ] )
-    self.assertEquals( 'Banned', res[ 'Value' ][ 'Status' ] )
-    self.assertEquals( 'Pilots Efficiency: 1 with status Bad', res[ 'Value' ][ 'Reason' ] )
-    
-################################################################################
-#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
+#''' Test_RSS_Policy_PilotWebSummaryEfficiencyPolicy
+#'''
+#
+#import mock
+#import unittest
+#
+#import LHCbDIRAC.ResourceStatusSystem.Policy.PilotWebSummaryEfficiencyPolicy as moduleTested
+#
+#################################################################################
+#
+#class PilotWebSummaryEfficiencyPolicy_TestCase( unittest.TestCase ):
+#  
+#  def setUp( self ):
+#    '''
+#    Setup
+#    '''
+#                  
+#    self.moduleTested = moduleTested
+#    self.testClass    = self.moduleTested.PilotWebSummaryEfficiencyPolicy
+#
+#  def tearDown( self ):
+#    '''
+#    Tear down
+#    '''
+#   
+#    del self.moduleTested
+#    del self.testClass
+#
+#################################################################################
+#
+#class PilotWebSummaryEfficiencyPolicy_Success( PilotWebSummaryEfficiencyPolicy_TestCase ):
+#  
+#  def test_instantiate( self ):
+#    ''' tests that we can instantiate one object of the tested class
+#    '''  
+#   
+#    module = self.testClass()
+#    self.assertEqual( 'PilotWebSummaryEfficiencyPolicy', module.__class__.__name__ )
+#
+#  def test_evaluate( self ):
+#    ''' tests the method _evaluate
+#    '''
+#    
+#    module = self.testClass()
+#    
+#    res = module._evaluate( { 'OK' : False, 'Message' : 'Bo!' } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Error', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Bo!', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : None } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'No values to take a decision', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'No values to take a decision', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'A' : 1 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Error', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( '"Status" key missing', res[ 'Value' ][ 'Reason' ] )    
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 1 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Error', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( '"PilotJobEff" key missing', res[ 'Value' ][ 'Reason' ] )              
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 1, 'PilotJobEff' : 1 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Error', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Unknown status "1"', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 'Good', 'PilotJobEff' : 1 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Active', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Pilots Efficiency: 1 with status Good', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 'Fair', 'PilotJobEff' : 1 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Active', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Pilots Efficiency: 1 with status Fair', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 'Poor', 'PilotJobEff' : 1 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Degraded', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Pilots Efficiency: 1 with status Poor', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 'Idle', 'PilotJobEff' : 1 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Unknown', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Pilots Efficiency: 1 with status Idle', res[ 'Value' ][ 'Reason' ] )
+#
+#    res = module._evaluate( { 'OK' : True, 'Value' : [{ 'Status' : 'Bad', 'PilotJobEff' : 1 }] } )
+#    self.assertEquals( True, res[ 'OK' ] )
+#    self.assertEquals( 'Banned', res[ 'Value' ][ 'Status' ] )
+#    self.assertEquals( 'Pilots Efficiency: 1 with status Bad', res[ 'Value' ][ 'Reason' ] )
+#    
+#################################################################################
+##EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
