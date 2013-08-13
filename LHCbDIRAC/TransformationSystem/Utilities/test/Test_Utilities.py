eca9b4c1075389cb911061647a9b37ad65e1ec95
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-08-13 10:33:28 +0000

    removed unused test
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@69452 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/TransformationSystem/Utilities/test/Test_Utilities.py
+++ LHCbDIRAC/TransformationSystem/Utilities/test/Test_Utilities.py
@@ -2,14 +2,11 @@ from mock import Mock
 import unittest
 
 from LHCbDIRAC.TransformationSystem.Utilities.StateMachine import TransformationFilesStateMachine
-# from LHCbDIRAC.TransformationSystem.Client.FileReport import FileReport
 
 class UtilitiesTestCase( unittest.TestCase ):
 
   def setUp( self ):
     self.transClient = Mock()
-#    self.transClient.setFileStatusForTransformation.return_value = {'OK':True}
-#    self.fr = FileReport()
 
   def tearDown( self ):
     pass
@@ -67,28 +64,6 @@ class tsfmFailure( UtilitiesTestCase ):
     res = tsfm.setState( 'Processed' )
     self.assert_( res['OK'] )
     self.assertEqual( res['Value'], 'MaxReset' )
-
-# class FileReportSuccess( UtilitiesTestCase ):
-#  def test_fr( self ):
-#    self.assertEqual( self.fr.statusDict, {} )
-#
-#    res = self.fr.setFileStatus( 1, '/aa.1.txt', 'Unused' )
-#    self.assert_( res['OK'] )
-#    self.assertEqual( self.fr.statusDict, {'/aa.1.txt': 'Unused'} )
-#
-#    res = self.fr.setFileStatus( 1, '/aa.1.txt', 'Assigned' )
-#    self.assert_( res['OK'] )
-#    self.assertEqual( self.fr.statusDict, {'/aa.1.txt': 'Assigned'} )
-#    res = self.fr.setFileStatus( 1, '/aa.2.txt', 'Assigned' )
-#    self.assert_( res['OK'] )
-#    self.assertEqual( self.fr.statusDict, {'/aa.1.txt': 'Assigned', '/aa.2.txt': 'Assigned'} )
-#
-#    res = self.fr.setFileStatus( 1, '/aa.1.txt', 'Processed', )
-#    res = self.fr.setFileStatus( 1, '/aa.1.txt', 'Moved' )
-#    self.assertFalse( res['OK'] )
-#
-#    res = self.fr.setFileStatus( 1, '/aa.1.txt', 'Unused' )
-#    self.assert_( res['OK'] )
 
 if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase( UtilitiesTestCase )
