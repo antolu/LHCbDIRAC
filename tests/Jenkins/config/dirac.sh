4a9c71e9a8c66790c7b50673fea648057fbb7661
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-08-20 12:07:03 +0000

    FIX: hacked dirac_externals to use v6r9 externals
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@69557 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/config/dirac.sh
+++ tests/Jenkins/config/dirac.sh
@@ -168,7 +168,8 @@ dirac_externals(){
   echo "Getting dirac externals"
   
   wget --no-check-certificate -O dirac-install 'https://github.com/DIRACGrid/DIRAC/raw/integration/Core/scripts/dirac-install.py' --quiet
-  python dirac-install -X -l DIRAC -r v6r9p10 -t server -p 'Linux_x86_64_glibc-2.5'
+  #python dirac-install -X -l DIRAC -r `cat $WORKSPACE/new_tag.txt` -t server -p 'Linux_x86_64_glibc-2.5'
+  python dirac-install -X -l DIRAC -r v6r9 -t server -p 'Linux_x86_64_glibc-2.5'
   ( 
     cd $WORKSPACE
     sed -i 's/`$DIRACSCRIPTS\/dirac-platform`/Linux_x86_64_glibc-2.5/g' bashrc
