a1caef5e3496c3a55f194d41e18bd94b7d872285
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-10-28 15:56:59 +0000

    re-use client
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@70748 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/TransformationSystem/Client/TransformationClient.py
+++ LHCbDIRAC/TransformationSystem/Client/TransformationClient.py
@@ -40,9 +40,10 @@ class TransformationClient( DIRACTransformationClient ):
                          url = '',
                          timeout = 120 ):
     rpcClient = self._getRPC( rpc = rpc, url = url, timeout = timeout )
-    res = rpcClient.addTransformation( transName, description, longDescription, transfType, plugin, agentType,
-                                       fileMask, transformationGroup, groupSize, inheritedFrom, body,
-                                       maxTasks, eventsPerTask, addFiles )
+    res = DIRACTransformationClient.addTransformation( transName, description, longDescription, transfType, plugin,
+                                                       agentType, fileMask, transformationGroup, groupSize,
+                                                       inheritedFrom, body, maxTasks, eventsPerTask, addFiles,
+                                                       rpc = rpcClient )
     if not res['OK']:
       return res
     transID = res['Value']
