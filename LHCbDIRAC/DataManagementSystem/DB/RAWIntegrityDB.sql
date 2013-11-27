3a09ac90617957c798738616e9906937fecb752e
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2013-11-27 15:26:55 +0000

    FIX DB creation
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@72045 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/DB/RAWIntegrityDB.sql
+++ LHCbDIRAC/DataManagementSystem/DB/RAWIntegrityDB.sql
@@ -2,16 +2,17 @@
 -- RAW Integrity DB definition
 -- -----------------------------------------------------------
 
---DROP DATABASE IF EXISTS RAWIntegrityDB;
+DROP DATABASE IF EXISTS RAWIntegrityDB;
 
---CREATE DATABASE RAWIntegrityDB;
+CREATE DATABASE RAWIntegrityDB;
+
 --
 -- Must set passwords for database user by replacing "must_be_set".
 --
 -- Create user DIRAC
---use mysql;
---GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON RAWIntegrityDB.* TO 'Dirac'@'localhost' IDENTIFIED BY 'must_be_set';
---GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON RAWIntegrityDB.* TO 'Dirac'@'%' IDENTIFIED BY 'must_be_set';
+use mysql;
+GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON RAWIntegrityDB.* TO 'Dirac'@'localhost' IDENTIFIED BY 'must_be_set';
+GRANT SELECT,INSERT,LOCK TABLES,UPDATE,DELETE,CREATE,DROP,ALTER ON RAWIntegrityDB.* TO 'Dirac'@'%' IDENTIFIED BY 'must_be_set';
 
 --FLUSH PRIVILEGES;
 
