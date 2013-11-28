1edce4c304ce02ffbf7c8b73f54b5bdee3dc6172
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-11-28 16:06:27 +0000

    FIX: added certificate creation
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@72116 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/prepare_for_test.sh
+++ tests/Jenkins/prepare_for_test.sh
@@ -112,7 +112,11 @@ diracInstall(){
   ./dirac-install -l LHCb -r `cat project.version` -e LHCb -t server $DEBUG
 
   mkdir -p etc/grid-security
-  ln -s ~/host{cert,key}.pem etc/grid-security
+  openssl genrsa -out hostkey.pem 2048
+  cp $WORKSPACE/LHCbTestDirac/Jenkins/openssl_config openssl_config
+  sed -i "s/#hostname#/$a/g" openssl_config
+  openssl req -new -x509 -key hostkey.pem -out hostcert.pem -days 1 -config openssl_config
+  #ln -s ~/host{cert,key}.pem etc/grid-security
   /etc/init.d/cvmfs probe
   ln -s /cvmfs/grid.cern.ch/etc/grid-security/certificates/ etc/grid-security/certificates
 
