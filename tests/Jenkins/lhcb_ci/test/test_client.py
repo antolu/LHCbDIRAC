a973df1d3687a222026ee5b2661dbadb47e6bc62
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-12-11 16:35:23 +0000

    Test Client: attempt 1
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@72547 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Jenkins/lhcb_ci/test/test_client.py
@@ -0,0 +1,28 @@
+""" lhcb_ci.test.test_client
+
+  LHCb Continuous Integration system libraries.
+
+  @author: mario.ubeda.garcia@cern.ch
+  
+"""
+
+# Run Federico's code
+
+# Think about SSHElement...
+
+import lhcb_ci.basecase
+
+
+class TransformationClientTest( lhcb_ci.basecase.Client_TestCase ):
+  
+  SUT = 'TransformationSystem.Client.TransformationClient'
+  
+  def test_demo( self ):
+    
+    raise NameError( 'DONE' )  
+  
+  test_demo.smoke  = 1
+  test_demo.client = 1
+  
+#...............................................................................
+#EOF
