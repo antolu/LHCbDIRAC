03eed52bcbeeb9ddb62c5b121effdd7935afcb3f
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2013-12-11 09:50:20 +0000

    change the datastorage accounting type fields
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@72479 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/AccountingSystem/Client/Types/DataStorage.py
+++ LHCbDIRAC/AccountingSystem/Client/Types/DataStorage.py
@@ -1,5 +1,5 @@
 ''' DataStorage Type
-  
+
 '''
 
 from DIRAC.AccountingSystem.Client.Types.BaseAccountingType import BaseAccountingType
@@ -12,25 +12,25 @@ class DataStorage( BaseAccountingType ):
   '''
 
   def __init__( self ):
-    
+
     BaseAccountingType.__init__( self )
-    
-    self.definitionKeyFields = [ ( 'DataType',       "VARCHAR(64)" ),
-                                 ( 'Activity',       "VARCHAR(64)" ),
-                                 ( 'FileType',       "VARCHAR(32)" ),
-                                 ( 'Production',     "VARCHAR(32)" ),
-                                 ( 'ProcessingPass', "VARCHAR(64)" ),
+
+    self.definitionKeyFields = [ ( 'StorageElement',       "VARCHAR(64)" ),
+                                 ( 'ProcessingPass',       "VARCHAR(64)" ),
+                                 ( 'Production',       "VARCHAR(32)" ),
+                                 ( 'DataType',     "VARCHAR(32)" ),
+                                 ( 'Activity', "VARCHAR(64)" ),
                                  ( 'Conditions',     "VARCHAR(64)" ),
                                  ( 'EventType',      "VARCHAR(64)" ),
-                                 ( 'StorageElement', "VARCHAR(32)" )
+                                 ( 'FileType', "VARCHAR(32)" )
                                ]
-    
+
     self.definitionAccountingFields = [ ( 'LogicalSize',   'BIGINT UNSIGNED' ),
                                         ( 'LogicalFiles',  'BIGINT UNSIGNED' ),
                                         ( 'PhysicalSize',  'BIGINT UNSIGNED' ),
                                         ( 'PhysicalFiles', 'BIGINT UNSIGNED' )
                                       ]
-    
+
     self.bucketsLength = [ ( 86400 * 30 * 6, 86400 ), #<6m = 1d
                            ( 31104000,       604800 ), #>6m = 1w
                          ]
