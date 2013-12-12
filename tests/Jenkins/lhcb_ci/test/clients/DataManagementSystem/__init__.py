8a72d89bdf69da2c004613635d8f26b29df371fc
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-12-12 17:33:25 +0000

    NEW: clients folder
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@72625 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Jenkins/lhcb_ci/test/clients/DataManagementSystem/__init__.py
@@ -0,0 +1,12 @@
+""" lhcb_ci.test.clients.DataManagementSystem
+
+  LHCb Continuous Integration system libraries.
+
+  @author: mario.ubeda.garcia@cern.ch
+
+  This file is here to pick up the test by pylint in Jenkins.
+  
+"""
+
+#...............................................................................
+#EOF
---
+++ tests/Jenkins/lhcb_ci/test/clients/DataManagementSystem/test_ftsclient.py
@@ -0,0 +1,32 @@
+""" lhcb_ci.test.clients.DataManagementSystem.test_ftsclient
+
+  LHCb Continuous Integration system libraries.
+
+  @author: mario.ubeda.garcia@cern.ch
+  
+"""
+
+# Run Federico's code
+
+# Think about SSHElement...
+
+import lhcb_ci.basecase
+
+
+class FTSClientTest( lhcb_ci.basecase.ClientTestCase ):
+
+  
+  SUT = 'DataManagementSystem.Client.FTSClient'
+
+
+  @lhcb_ci.basecase.timeDecorator
+  def test_demo( self ):    
+      
+    pass
+  
+  test_demo.smoke  = 1
+  test_demo.client = 1
+
+
+#...............................................................................
+#EOF
