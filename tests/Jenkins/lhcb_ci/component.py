45eb1dcf3cb9dc01f0a701c08459b39d4ac71cb3
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-12-12 12:02:40 +0000

    NEW: component module
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@72580 4525493e-7705-40b1-a816-d608a930855b

---
+++ tests/Jenkins/lhcb_ci/component.py
@@ -0,0 +1,93 @@
+""" lhcb_ci.component
+
+  LHCb Continuous Integration system libraries.
+
+  @author: mario.ubeda.garcia@cern.ch
+  
+"""
+
+import lhcb_ci.db
+import lhcb_ci.service
+
+#TODO: implement it. Implements an interface for all components
+
+class Component( object ):
+  
+  def __new__( cls, system, component, name ):
+    
+    if cls is Component:
+      if component == 'DB':
+        cls = DBComponent
+      elif component == 'Service':
+        cls = ServiceComponent
+      elif component == 'Agent':
+        cls = AgentComponent  
+      else:
+        raise NotImplementedError( 'No component implementation for %s' % component )
+    return super( Component, cls ).__new__( cls )
+  
+  def __init__( self, system, component, name ):
+    
+    self.system    = system
+    self.component = component
+    self.name      = name
+  
+  def configure( self ):
+    pass
+  
+  def install( self ):
+    pass
+  
+  def run( self ):
+    pass
+  
+  def stop( self ):
+    pass
+
+#...............................................................................
+
+class DBComponent( Component ):
+  
+  def run( self ):
+    
+    return lhcb_ci.db.installDB( self.name )
+  
+  def stop( self ):
+    
+    return lhcb_ci.db.dropDB( self.name )
+  
+#...............................................................................
+
+class ServiceComponent( Component ):
+  
+  def __init__( self, *args ):
+    
+    super( ServiceComponent, self ).__init__( *args )
+    self.server      = None
+    self.serviceName = None
+    self.service     = None
+  
+  def run( self ):
+    
+    systemName = self.system.replace( 'System', '' )
+    sReactor = lhcb_ci.service.initializeServiceReactor( systemName , self.name )
+    if not sReactor[ 'OK' ]:
+      return sReactor
+    sReactor = sReactor[ 'Value' ]
+    
+    # Extract the initialized ServiceReactor        
+    self.server, self.serviceName, self.service = lhcb_ci.service.serve( sReactor )
+    
+  def stop( self ):
+    
+    return lhcb_ci.service.unserve( self.server )  
+       
+
+#...............................................................................
+
+
+class AgentComponent( Component ):
+  pass
+
+#...............................................................................
+#EOF
