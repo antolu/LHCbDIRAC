cd6370a68ee2ebcca8e884e889dcc3c06b56faea
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-12-13 14:02:46 +0000

    ProxyDB in the machine
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@72790 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci/basecase.py
+++ tests/Jenkins/lhcb_ci/basecase.py
@@ -239,7 +239,8 @@ class DBTestCase( BaseTestCase ):
       self.log.error( 'setUp' )
       self.fail( res[ 'Message' ] )
       
-    if res[ 'Value' ]:
+    # We keep ProxyDB installed
+    if res[ 'Value' ] != [ 'ProxyDB' ]:
       self.log.error( 'setUp' )
       self.fail( 'DBs still installed: %s' % res[ 'Value' ] )  
       
