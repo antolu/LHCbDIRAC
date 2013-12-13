83d9b882f8f12f8cb23b150bcdcce7c8014b1c70
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-12-13 07:33:28 +0000

    ACtivate tests for long test
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@72661 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci/test/test_db.py
+++ tests/Jenkins/lhcb_ci/test/test_db.py
@@ -227,16 +227,16 @@ class InstallationTest( lhcb_ci.basecase.DBTestCase ):
   # Nosetests attrs
 
   # test_databases_install_drop
-  test_databases_install_drop.install = 1
-  test_databases_install_drop.db      = 1  
+  test_databases_install_drop.install = 0
+  test_databases_install_drop.db      = 0  
   
   # test_databases_reachable
-  test_databases_common_import.install = 1
-  test_databases_common_import.db      = 1
+  test_databases_common_import.install = 0
+  test_databases_common_import.db      = 0
   
   # test_install_tables
-  test_install_tables.install = 1
-  test_install_tables.db      = 1
+  test_install_tables.install = 0
+  test_install_tables.db      = 0
 
 
 class SmokeTest( lhcb_ci.basecase.DBTestCase ):
