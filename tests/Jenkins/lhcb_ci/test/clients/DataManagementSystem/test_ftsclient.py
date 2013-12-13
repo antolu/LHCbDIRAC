50d3bb885c0c7ad8219b5d1a81d55d4b19d29956
ubeda <ubeda@4525493e-7705-40b1-a816-d608a930855b>
2013-12-13 08:54:50 +0000

    FTS test
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@72702 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci/test/clients/DataManagementSystem/test_ftsclient.py
+++ tests/Jenkins/lhcb_ci/test/clients/DataManagementSystem/test_ftsclient.py
@@ -6,7 +6,12 @@
   
 """
 
-# Run Federico's code
+import mock
+import uuid
+
+from DIRAC.DataManagementSystem.Client.FTSFile import FTSFile
+from DIRAC.DataManagementSystem.Client.FTSJob  import FTSJob
+from DIRAC.DataManagementSystem.Client.FTSSite import FTSSite
 
 # Think about SSHElement...
 
@@ -19,8 +24,93 @@ class FTSClientTest( lhcb_ci.basecase.ClientTestCase ):
   SUT = 'DataManagementSystem.Client.FTSClient'
 
 
+  def setUp( self ):
+    
+    super( FTSClientTest, self ).setUp()
+    
+    self.ftsSites = [ FTSSite( ftsServer = 'https://fts22-t0-export.cern.ch:8443/glite-data-transfer-fts/services/FileTransfer', name = 'CERN.ch' ),
+                      FTSSite( ftsServer = 'https://fts.pic.es:8443/glite-data-transfer-fts/services/FileTransfer', name = 'PIC.es' ),
+                      FTSSite( ftsServer = 'https://lcgfts.gridpp.rl.ac.uk:8443/glite-data-transfer-fts/services/FileTransfer', name = 'RAL.uk' ),
+                    ]
+
+    self.ses      = [ 'CERN-USER', 'RAL-USER' ]
+    self.statuses = [ 'Submitted', 'Finished', 'FinishedDirty', 'Active', 'Ready' ]
+
+    self.submitted    = 0
+    self.numberOfJobs = 10
+    self.opIDs        = []
+
+    self.ftsJobs = []
+    for i in range( self.numberOfJobs ):
+
+      opID = i % 3
+      if opID not in self.opIDs:
+        self.opIDs.append( opID )
+
+      ftsJob = FTSJob()
+      ftsJob.FTSGUID = str( uuid.uuid4() )
+      ftsJob.FTSServer = self.ftsSites[0].FTSServer
+      ftsJob.Status = self.statuses[ i % len( self.statuses ) ]
+      ftsJob.OperationID = opID
+      if ftsJob.Status in FTSJob.FINALSTATES:
+        ftsJob.Completeness = 100
+      if ftsJob.Status == 'Active':
+        ftsJob.Completeness = 90
+      ftsJob.SourceSE = self.ses[ i % len( self.ses ) ]
+      ftsJob.TargetSE = 'PIC-USER'
+      ftsJob.RequestID = 12345
+
+      ftsFile = FTSFile()
+      ftsFile.FileID = i + 1
+      ftsFile.OperationID = i + 1
+      ftsFile.LFN = '/a/b/c/%d' % i
+      ftsFile.Size = 1000000
+      ftsFile.OperationID = opID
+      ftsFile.SourceSE = ftsJob.SourceSE
+      ftsFile.TargetSE = ftsJob.TargetSE
+      ftsFile.SourceSURL = 'foo://source.bar.baz/%s' % ftsFile.LFN
+      ftsFile.TargetSURL = 'foo://target.bar.baz/%s' % ftsFile.LFN
+      ftsFile.Status = 'Waiting' if ftsJob.Status != 'FinishedDirty' else 'Failed'
+      ftsFile.RequestID = 12345
+      ftsFile.Checksum = 'addler'
+      ftsFile.ChecksumType = 'adler32'
+
+      ftsFile.FTSGUID = ftsJob.FTSGUID
+      if ftsJob.Status == 'FinishedDirty':
+        ftsJob.FailedFiles = 1
+        ftsJob.FailedSize = ftsFile.Size
+
+      ftsJob.addFile( ftsFile )
+      self.ftsJobs.append( ftsJob )
+
+    self.submitted = len( [ i for i in self.ftsJobs if i.Status == 'Submitted' ] )
+
+    #self.ftsClient = FTSClient()
+    self.replicaManager = mock.Mock()
+    self.replicaManager.getActiveReplicas.return_value = { 'OK'    : True,
+                                                           'Value' : {
+                                                                       'Successful' : 
+                                                                          { '/a/b/c/1':
+                                                                            { 'CERN-USER':'/aa/a/b/c/1d',
+                                                                              'RAL-USER':'/bb/a/b/c/1d' },
+                                                                            '/a/b/c/2':
+                                                                            { 'CERN-USER':'/aa/a/b/c/2d',
+                                                                              'RAL-USER':'/bb/a/b/c/2d'},
+                                                                            '/a/b/c/3':
+                                                                            { 'CERN-USER':'/aa/a/b/c/3d',
+                                                                              'RAL-USER':'/bb/a/b/c/3d'}
+                                                                          },
+                                                                        'Failed': {'/a/b/c/4':'/aa/a/b/c/4d',
+                                                                                   '/a/b/c/5':'/aa/a/b/c/5d'}
+                                                                              }
+                                                                    }    
+
+
   @lhcb_ci.basecase.timeDecorator
   def test_demo( self ):    
+    
+    client = self.sutCls()
+    client.replicaManager = self.replicaManager
       
     pass
   
