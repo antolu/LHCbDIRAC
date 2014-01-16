88a60c981aea67b817b1ca8b536f31c1f3eade58
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2014-01-16 10:23:29 +0000

    Move script from DMS to BKS
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@73353 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-set-visibility.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-set-visibility.py
@@ -3,7 +3,7 @@
    Set the visibility flag to a dataset
 """
 
-__RCSID__ = "$Id$"
+__RCSID__ = "$Id: dirac-bookkeeping-set-visibility.py 69359 2013-08-08 13:57:13Z phicharp $"
 
 import DIRAC
 from DIRAC.Core.Base import Script
