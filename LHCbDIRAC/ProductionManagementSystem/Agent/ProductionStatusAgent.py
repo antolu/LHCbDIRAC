a2815ac0e3be26dac64b56be89f0b9e0b679ac9a
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-01-21 09:44:19 +0000

    Stricter Idle
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@73566 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ProductionManagementSystem/Agent/ProductionStatusAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/ProductionStatusAgent.py
@@ -210,19 +210,28 @@ class ProductionStatusAgent( AgentModule ):
       # only failed and done jobs
       # AND number of tasks created in total == number of tasks submitted
       prodStats = self._getTransformationTaskStats( prod )
-      self.log.debug( "Stats: %s" % str( prodStats ) )
-      isIdle = ( prodStats.get( 'TotalCreated', 0 ) == prodStats.get( 'Submitted', 0 ) ) \
-      and all( [prodStats.get( status, 0 ) == 0 for status in ['Checking', 'Completed', 'Created', 'Matched',
-                                                               'Received', 'Reserved', 'Rescheduled', 'Running',
-                                                               'Waiting' ]] )
+      self.log.debug( "Tasks Stats: %s" % str( prodStats ) )
+      isIdle = ( ( prodStats.get( 'TotalCreated', 0 ) == prodStats.get( 'Submitted', 0 ) ) \
+                  and \
+                  all( [prodStats.get( status, 0 ) == 0 for status in ['Checking', 'Completed', 'Created', 'Matched',
+                                                                     'Received', 'Reserved', 'Rescheduled', 'Running',
+                                                                     'Waiting' ]] ) )
     else:
       # other production type : go to Idle if
       # 0 unused, 0 assigned files
       # AND > 0 processed files
-      prodStats = self._getTransformationFilesStats( prod )
-      self.log.debug( "Stats: %s" % str( prodStats ) )
-      isIdle = ( prodStats.get( 'Processed', 0 ) > 0 ) \
-      and all( [prodStats.get( status, 0 ) == 0 for status in ['Unused', 'Assigned']] )
+      # AND only failed and done jobs
+      filesStats = self._getTransformationFilesStats( prod )
+      self.log.debug( "Files stats: %s" % str( filesStats ) )
+      prodStats = self._getTransformationTaskStats( prod )
+      self.log.debug( "Tasks Stats: %s" % str( prodStats ) )
+      isIdle = ( ( filesStats.get( 'Processed', 0 ) > 0 ) \
+                 and \
+                 all( [filesStats.get( status, 0 ) == 0 for status in ['Unused', 'Assigned']] ) \
+                 and \
+                 all( [prodStats.get( status, 0 ) == 0 for status in ['Checking', 'Completed', 'Created', 'Matched',
+                                                                      'Received', 'Reserved', 'Rescheduled', 'Running',
+                                                                      'Waiting' ]] ) )
 
     return isIdle
 
