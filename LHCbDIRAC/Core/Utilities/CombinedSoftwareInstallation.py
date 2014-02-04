979942ecaefc18a9c7b2473f5c4110fd82cacbd9
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-02-04 18:14:24 +0000

    Try to do SetupProject for all possible compatible platforms (ordered)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@73954 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Core/Utilities/CombinedSoftwareInstallation.py
+++ LHCbDIRAC/Core/Utilities/CombinedSoftwareInstallation.py
@@ -17,9 +17,10 @@
 __RCSID__ = "$Id$"
 
 import os, shutil, sys, urllib, re, copy
-import DIRAC
+from DIRAC import gLogger, gConfig, S_OK, S_ERROR
 from DIRAC.Core.Utilities.Subprocess import systemCall
 from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
+from DIRAC.ConfigurationSystem.Client.Helpers.Resources import getCompatiblePlatforms
 
 from LHCbDIRAC.Core.Utilities.DetectOS import NativeMachine
 from LHCbDIRAC.Core.Utilities.SoftwareArea import getSharedArea, getLocalArea
@@ -58,7 +59,7 @@ class CombinedSoftwareInstallation:
 
     self.apps = []
     for app in apps:
-      DIRAC.gLogger.verbose( 'Requested Package %s' % str( app ) )
+      gLogger.verbose( 'Requested Package %s' % str( app ) )
       app = tuple( app.split( '.' ) )
       self.apps.append( app )
 
@@ -87,65 +88,63 @@ class CombinedSoftwareInstallation:
     """
     if not self.apps:
       # There is nothing to do
-      return DIRAC.S_OK()
+      return S_OK()
     if not self.jobConfig:
-      DIRAC.gLogger.warn( 'No architecture requested' )
-      return DIRAC.S_ERROR( 'No architecture requested' )
+      gLogger.warn( 'No architecture requested' )
+      return S_ERROR( 'No architecture requested' )
 
     # The below only applies to local running since job agent will set to /LocalSite/Architecture
     # in case of 'ANY' in the job description.
     if self.jobConfig.lower() == 'any':
-      DIRAC.gLogger.info( 'Assume this is a locally running job with sys config set to "ANY"' )
-      self.jobConfig = DIRAC.gConfig.getValue( '/LocalSite/Architecture', '' )
+      gLogger.info( 'Assume this is a locally running job with sys config set to "ANY"' )
+      self.jobConfig = gConfig.getValue( '/LocalSite/Architecture', '' )
       self.ceConfigs = [self.jobConfig]
       if not self.jobConfig:
-        return DIRAC.S_ERROR( '/LocalSite/Architecture is missing and must be specified' )
+        return S_ERROR( '/LocalSite/Architecture is missing and must be specified' )
 
-    DIRAC.gLogger.info( 'CE supported system configurations are: %s' % ( ', '.join( self.ceConfigs ) ) )
+    gLogger.info( 'CE supported system configurations are: %s' % ( ', '.join( self.ceConfigs ) ) )
     if not self.ceConfigs:  # redundant check as this is done in the job agent and above
-      DIRAC.gLogger.info( 'Assume locally running job without CE configuration settings' )
-      return DIRAC.S_OK()
+      gLogger.info( 'Assume locally running job without CE configuration settings' )
+      return S_OK()
 
-    if self.jobConfig == DIRAC.gConfig.getValue( '/LocalSite/Architecture', '' ):  # as set by the job agent in case of 'ANY'
-      DIRAC.gLogger.info( 'Job SystemConfiguration is set to /LocalSite/Architecture, checking compatible platforms' )
-      compatibleArchs = DIRAC.gConfig.getValue( '/Resources/Computing/OSCompatibility/%s' % ( self.jobConfig ), [] )
-      if not compatibleArchs:
-        DIRAC.gLogger.error( 'Could not find matching section for %s \
-        in /Resources/Computing/OSCompatibility/' % ( self.jobConfig ) )
-        return DIRAC.S_ERROR( 'SystemConfig Not Found' )
-      self.jobConfig = compatibleArchs[0]
-      DIRAC.gLogger.info( 'Setting system config to compatible platform %s' % ( self.jobConfig ) )
+    if self.jobConfig == gConfig.getValue( '/LocalSite/Architecture', '' ):  # as set by the job agent in case of 'ANY'
+      gLogger.info( 'Job SystemConfiguration is set to /LocalSite/Architecture, checking compatible platforms' )
+      compatibleArchs = getCompatiblePlatforms( self.jobConfig )
+      if not compatibleArchs['OK']:
+        return compatibleArchs
+      self.jobConfig = compatibleArchs['Value'][0]
+      gLogger.info( 'Setting system config to compatible platform %s' % ( self.jobConfig ) )
       if not self.jobConfig in self.ceConfigs:
         self.ceConfigs.append( self.jobConfig )
 
     if not self.jobConfig in self.ceConfigs:
-      DIRAC.gLogger.error( 'Requested architecture not supported by CE' )
-      return DIRAC.S_ERROR( 'Requested architecture not supported by CE' )
+      gLogger.error( 'Requested architecture not supported by CE' )
+      return S_ERROR( 'Requested architecture not supported by CE' )
 
     for app in copy.deepcopy( self.apps ):
-      DIRAC.gLogger.info( 'Checking %s for %s with site root %s' % ( str( app ), self.jobConfig, self.mySiteRoot ) )
+      gLogger.info( 'Checking %s for %s with site root %s' % ( str( app ), self.jobConfig, self.mySiteRoot ) )
       result = checkApplication( app, self.jobConfig, self.mySiteRoot )
       if not result:
-        DIRAC.gLogger.info( 'Software was not found to be pre-installed in the shared area: %s' % str( app ) )
+        gLogger.info( 'Software was not found to be pre-installed in the shared area: %s' % str( app ) )
         if re.search( ':', self.mySiteRoot ):
           result = installApplication( app, self.jobConfig, self.mySiteRoot )
           if not result:
-            DIRAC.gLogger.error( 'Software failed to be installed!' )
-            return DIRAC.S_ERROR( 'Software Not Installed' )
+            gLogger.error( 'Software failed to be installed!' )
+            return S_ERROR( 'Software Not Installed' )
           else:
-            DIRAC.gLogger.info( 'Software was successfully installed in the local area' )
+            gLogger.info( 'Software was successfully installed in the local area' )
         else:
-          DIRAC.gLogger.error( 'No local area was found to install missing software!' )
-          return DIRAC.S_ERROR( 'No Local Area Found' )
+          gLogger.error( 'No local area was found to install missing software!' )
+          return S_ERROR( 'No Local Area Found' )
       else:
-        DIRAC.gLogger.info( '%s is installed for %s' % ( str( app ), self.jobConfig ) )
+        gLogger.info( '%s is installed for %s' % ( str( app ), self.jobConfig ) )
 
-    return DIRAC.S_OK()
+    return S_OK()
 
 #############################################################################
 
 def log( n, line ):
-  DIRAC.gLogger.verbose( line )
+  gLogger.verbose( line )
 
 #############################################################################
 
@@ -168,25 +167,25 @@ def checkApplication( app, config, area ):
   if not os.path.exists( installProject ):
     installProject = os.path.join( sharedArea, installProjectFile )
     if not os.path.exists( installProject ):
-      DIRAC.gLogger.warn( 'Failed to find:', installProjectFile )
+      gLogger.warn( 'Failed to find:', installProjectFile )
       return False
     else:
   # NOTE: must cd to LOCAL area directory (install_project requirement)
-      DIRAC.gLogger.info( ' change directory to %s ' % sharedArea )
+      gLogger.info( ' change directory to %s ' % sharedArea )
       os.chdir( sharedArea )
       area = sharedArea
   else:
     os.chdir( localArea )
 
-  DIRAC.gLogger.info( ' install_project is %s' % installProject )
+  gLogger.info( ' install_project is %s' % installProject )
   # Now run the installation
 
 
   cmtEnv = dict( os.environ )
   cmtEnv['MYSITEROOT'] = area
-  DIRAC.gLogger.info( 'Defining MYSITEROOT = %s' % area )
+  gLogger.info( 'Defining MYSITEROOT = %s' % area )
   cmtEnv['CMTCONFIG'] = config
-  DIRAC.gLogger.info( 'Defining CMTCONFIG = %s' % config )
+  gLogger.info( 'Defining CMTCONFIG = %s' % config )
 
   cmdTuple = [sys.executable]
   cmdTuple += [installProjectFile]
@@ -197,14 +196,14 @@ def checkApplication( app, config, area ):
   if appVersion:
     cmdTuple += [ appVersion ]
 
-  DIRAC.gLogger.info( 'Executing %s' % ' '.join( cmdTuple ) )
+  gLogger.info( 'Executing %s' % ' '.join( cmdTuple ) )
   ret = systemCall( 1800, cmdTuple, env = cmtEnv, callbackFunction = log )
   os.chdir( curDir )
   if not ret['OK']:
-    DIRAC.gLogger.error( 'Software checking failed', '%s %s' % ( appName, appVersion ) )
+    gLogger.error( 'Software checking failed', '%s %s' % ( appName, appVersion ) )
     return False
   if ret['Value'][0]:  # != 0
-    DIRAC.gLogger.error( 'Software checking failed with non-zero status', '%s %s' % ( appName, appVersion ) )
+    gLogger.error( 'Software checking failed with non-zero status', '%s %s' % ( appName, appVersion ) )
     return False
 
   return True
@@ -219,14 +218,14 @@ def installApplication( app, config, area ):
   """
   if not os.path.exists( '%s/%s' % ( os.getcwd(), installProjectFile ) ):
     try:
-      DIRAC.gLogger.debug( "Downloading %s from %s" % ( installProjectFile,
+      gLogger.debug( "Downloading %s from %s" % ( installProjectFile,
                                                         ( installProjectURL, installProjectFile ) ) )
       urllib.urlretrieve( '%s%s' % ( installProjectURL, installProjectFile ), installProjectFile )
     except urllib.ContentTooShortError, msg:
-      DIRAC.gLogger.exception( "Content too short ", msg )
+      gLogger.exception( "Content too short ", msg )
       return False
     if not os.path.exists( '%s/%s' % ( os.getcwd(), installProjectFile ) ):
-      DIRAC.gLogger.error( '%s/%s could not be downloaded' % ( installProjectURL, installProjectFile ) )
+      gLogger.error( '%s/%s could not be downloaded' % ( installProjectURL, installProjectFile ) )
       return False
 
   if not area:
@@ -243,7 +242,7 @@ def installApplication( app, config, area ):
     try:
       shutil.copy( installProjectFile, localArea )
     except shutil.Error, errorMsg:
-      DIRAC.gLogger.warn( 'Failed to create: %s %s' % ( installProject, errorMsg ) )
+      gLogger.warn( 'Failed to create: %s %s' % ( installProject, errorMsg ) )
       return False
 
   curDir = os.getcwd()
@@ -255,9 +254,9 @@ def installApplication( app, config, area ):
   if sharedArea:
     localmySiteRoot += ':%s' % sharedArea
   cmtEnv['MYSITEROOT'] = localmySiteRoot
-  DIRAC.gLogger.info( 'Defining MYSITEROOT = %s' % localmySiteRoot )
+  gLogger.info( 'Defining MYSITEROOT = %s' % localmySiteRoot )
   cmtEnv['CMTCONFIG'] = config
-  DIRAC.gLogger.info( 'Defining CMTCONFIG = %s' % config )
+  gLogger.info( 'Defining CMTCONFIG = %s' % config )
 
   cmdTuple = [sys.executable]
   cmdTuple += [installProjectFile]
@@ -268,17 +267,17 @@ def installApplication( app, config, area ):
   if appVersion:
     cmdTuple += [ appVersion ]
 
-  DIRAC.gLogger.info( 'Executing %s' % ' '.join( cmdTuple ) )
-  DIRAC.gLogger.info( ' at %s' % os.getcwd() )
+  gLogger.info( 'Executing %s' % ' '.join( cmdTuple ) )
+  gLogger.info( ' at %s' % os.getcwd() )
 
   ret = systemCall( 1800, cmdTuple, env = cmtEnv, callbackFunction = log )
   os.chdir( curDir )
   if not ret['OK']:
-    DIRAC.gLogger.warn( 'Failed to install software:', '_'.join( app ) )
-    DIRAC.gLogger.warn( ret['Message'] )
+    gLogger.warn( 'Failed to install software:', '_'.join( app ) )
+    gLogger.warn( ret['Message'] )
     return False
   if ret['Value'][0]:  # != 0
-    DIRAC.gLogger.warn( 'Failed to install software:', '_'.join( app ) )
+    gLogger.warn( 'Failed to install software:', '_'.join( app ) )
     return False
 
   return True
@@ -313,16 +312,16 @@ def _getApp( app ):
 #   if it does not exists
 #  """
 #  if not os.environ.has_key( 'VO_LHCB_SW_DIR' ):
-#    DIRAC.gLogger.info( 'VO_LHCB_SW_DIR not defined.' )
+#    gLogger.info( 'VO_LHCB_SW_DIR not defined.' )
 #    return False
 #
 #  sharedArea = os.environ['VO_LHCB_SW_DIR']
 #  if sharedArea == '.':
-#    DIRAC.gLogger.info( 'VO_LHCB_SW_DIR points to "."' )
+#    gLogger.info( 'VO_LHCB_SW_DIR points to "."' )
 #    return False
 #
 #  if not os.path.isdir( sharedArea ):
-#    DIRAC.gLogger.error( 'VO_LHCB_SW_DIR="%s" is not a directory' % sharedArea )
+#    gLogger.error( 'VO_LHCB_SW_DIR="%s" is not a directory' % sharedArea )
 #    return False
 #
 #  sharedArea = os.path.join( sharedArea, 'lib' )
@@ -336,6 +335,6 @@ def _getApp( app ):
 #    os.mkdir( sharedArea )
 #    return True
 #  except OSError, msg:
-#    DIRAC.gLogger.error( 'Problem trying to create shared area', str( msg ) )
+#    gLogger.error( 'Problem trying to create shared area', str( msg ) )
 #    return False
 
