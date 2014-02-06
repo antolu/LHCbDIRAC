1f6a85c5f28ac04fe0713e0c8adcb0e76ed3b69d
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2014-02-06 14:59:53 +0000

    FIX: handle large datasets
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@74032 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-pfn-metadata.py
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-pfn-metadata.py
@@ -83,27 +83,31 @@ if __name__ == "__main__":
   from DIRAC.DataManagementSystem.Client.ReplicaManager    import ReplicaManager
   from DIRAC import S_OK, S_ERROR
   from DIRAC.Core.Utilities.Adler import compareAdler
+  from DIRAC.Core.Utilities.List import breakListIntoChunks
   if len( seList ) > 1:
     gLogger.always( "Using the following list of SEs: %s" % str( seList ) )
+  if len( urlList ) > 100:
+    gLogger.always( "Getting metadata for %d files, be patient" % len( urlList ) )
   rm = ReplicaManager()
   gLogger.setLevel( "FATAL" )
   metadata = {'Successful':{}, 'Failed':{}}
   replicas = {}
   # restrict SEs to those where the replicas are
-  res = rm.getCatalogReplicas( urlList, allStatus = True )
-  if not res['OK']:
-    gLogger.fatal( 'Error getting replicas for %d files' % len( urlList ), res['Message'] )
-    DIRAC.exit( 2 )
-  else:
-    replicas = res['Value']['Successful']
-    for lfn in sorted( replicas ):
-      if seList and not [se for se in replicas[lfn] if se in seList]:
-        metadata['Failed'][lfn] = 'No such file at %s in FC' % ' '.join( seList )
-        replicas.pop( lfn )
-        urlList.remove( lfn )
+  for lfnChunk in breakListIntoChunks( urlList, 100 ):
+    res = rm.getCatalogReplicas( lfnChunk, allStatus = True )
+    if not res['OK']:
+      gLogger.fatal( 'Error getting replicas for %d files' % len( lfnChunk ), res['Message'] )
+      DIRAC.exit( 2 )
+    else:
+      replicas.update( res['Value']['Successful'] )
     for lfn in res['Value']['Failed']:
       metadata['Failed'][lfn] = 'FC: ' + res['Value']['Failed'][lfn]
-    metadata['Failed'].update( dict.fromkeys( [url for url in urlList if url not in replicas and url not in metadata['Failed']], 'FC: No active replicas' ) )
+  for lfn in sorted( replicas ):
+    if seList and not [se for se in replicas[lfn] if se in seList]:
+      metadata['Failed'][lfn] = 'No such file at %s in FC' % ' '.join( seList )
+      replicas.pop( lfn )
+      urlList.remove( lfn )
+  metadata['Failed'].update( dict.fromkeys( [url for url in urlList if url not in replicas and url not in metadata['Failed']], 'FC: No active replicas' ) )
   result = None
   if not seList:
     # take all SEs in replicas and add a fake '' to printout the SE name
@@ -113,39 +117,36 @@ if __name__ == "__main__":
       fileList = [url for url in urlList if se in replicas.get( url, [] )]
       if not fileList:
         continue
-      res = rm.getStorageFileMetadata( fileList, se )
-      if res['OK']:
-        seMetadata = res['Value']
-        for url in seMetadata['Successful']:
-          pfnMetadata = seMetadata['Successful'][url].copy()
-          metadata['Successful'].setdefault( url, {} )[se] = pfnMetadata if not exists else {'Exists': 'True (%sCached)' % ( '' if pfnMetadata.get( 'Cached' ) else 'Not ' )}
-          if exists and not pfnMetadata.get( 'Size' ):
-            metadata['Successful'][url][se].update( {'Exists':'Zero size'} )
-          if check:
-            res1 = rm.getCatalogFileMetadata( url )
-            if res1['OK']:
-              lfnMetadata = res1['Value']['Successful'][url]
-              ok = True
-              diff = 'False -'
-              for field in ( 'Checksum', 'Size' ):
-                if lfnMetadata[field] != pfnMetadata[field]:
-                  if field == 'Checksum' and compareAdler( lfnMetadata[field], pfnMetadata[field] ):
-                    continue
-                  ok = False
-                  diff += ' %s: (LFN %s, PFN %s)' % ( field, lfnMetadata[field], pfnMetadata[field] )
-              if len( seList ) > 1:
-                metadata['Successful'][url][se]['MatchLFN'] = ok if ok else diff
-              else:
-                metadata['Successful'][url]['MatchLFN'] = ok if ok else diff
-          # metadata['Failed'].pop( url, None )
-        for url in seMetadata['Failed']:
-           # if url not in metadata['Successful']:
-           metadata['Failed'].setdefault( url, {} )[se] = seMetadata['Failed'][url] if not exists else {'Exists': False}
-        # urlList = seMetadata['Failed']
-        # if not urlList: break
-      else:
-        for url in fileList:
-          metadata['Failed'][url] = res['Message'] + ' at %s' % se
+      for fileChunk in breakListIntoChunks( fileList, 100 ):
+        res = rm.getStorageFileMetadata( fileChunk, se )
+        if res['OK']:
+          seMetadata = res['Value']
+          for url in seMetadata['Successful']:
+            pfnMetadata = seMetadata['Successful'][url].copy()
+            metadata['Successful'].setdefault( url, {} )[se] = pfnMetadata if not exists else {'Exists': 'True (%sCached)' % ( '' if pfnMetadata.get( 'Cached' ) else 'Not ' )}
+            if exists and not pfnMetadata.get( 'Size' ):
+              metadata['Successful'][url][se].update( {'Exists':'Zero size'} )
+            if check:
+              res1 = rm.getCatalogFileMetadata( url )
+              if res1['OK']:
+                lfnMetadata = res1['Value']['Successful'][url]
+                ok = True
+                diff = 'False -'
+                for field in ( 'Checksum', 'Size' ):
+                  if lfnMetadata[field] != pfnMetadata[field]:
+                    if field == 'Checksum' and compareAdler( lfnMetadata[field], pfnMetadata[field] ):
+                      continue
+                    ok = False
+                    diff += ' %s: (LFN %s, PFN %s)' % ( field, lfnMetadata[field], pfnMetadata[field] )
+                if len( seList ) > 1:
+                  metadata['Successful'][url][se]['MatchLFN'] = ok if ok else diff
+                else:
+                  metadata['Successful'][url]['MatchLFN'] = ok if ok else diff
+          for url in seMetadata['Failed']:
+             metadata['Failed'].setdefault( url, {} )[se] = seMetadata['Failed'][url] if not exists else {'Exists': False}
+        else:
+          for url in fileChunk:
+            metadata['Failed'][url] = res['Message'] + ' at %s' % se
 
   if not summary:
     printDMResult( S_OK( metadata ), empty = "File not at SE" )
