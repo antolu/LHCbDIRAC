0a20b166f45a7519fb8da21d8393707f043774de
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2014-02-27 15:57:19 +0000

    NEW script for getting production numbers given a BK path
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@74457 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-prod4path.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+"""
+   Get production numbers given a dataset path
+"""
+
+__RCSID__ = "$Id: dirac-bookkeeping-get-stats.py 69357 2013-08-08 13:33:31Z phicharp $"
+
+import DIRAC
+from DIRAC.Core.Base import Script
+from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
+from DIRAC import gLogger
+
+def printProds( title, prods ):
+    types = list( set( prods.values() ) )
+    if len( types ) == 1:
+      gLogger.always( '%s (%s): %s' % ( title, types[0], ','.join( [str( prod ) for prod in sorted( prods )] ) ) )
+    else:
+      gLogger.always( '%s: %s' % ','.join( [' %s (%s)' % ( title, prod, prods[prod] ) for prod in sorted( prods )] ) )
+
+def execute():
+  from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient  import BookkeepingClient
+  from LHCbDIRAC.TransformationSystem.Client.TransformationClient           import TransformationClient
+  bk = BookkeepingClient()
+  tr = TransformationClient()
+
+  for switch in Script.getUnprocessedSwitches():
+    pass
+
+  bkQuery = dmScript.getBKQuery()
+
+  prods = bkQuery.getBKProductions( visible = 'All' )
+
+  parents = {}
+  productions = {}
+  for prod in prods:
+    type = tr.getTransformation( prod ).get( 'Value', {} ).get( 'Type', 'Unknown' )
+    productions[prod] = type
+    parent = tr.getBookkeepingQueryForTransformation( prod ).get( 'Value', {} ).get( 'ProductionID', '' )
+    if parent:
+      type = tr.getTransformation( parent ).get( 'Value', {} ).get( 'Type', 'Unknown' )
+      parents[parent] = type
+
+  gLogger.always( "For BK path %s:" % bkQuery.getPath() )
+  if not prods:
+    gLogger.always( 'No productions found!' )
+  else:
+    printProds( 'Productions found', productions )
+    if parents:
+      printProds( 'Parent productions', parents )
+
+if __name__ == "__main__":
+
+  dmScript = DMScript()
+  dmScript.registerBKSwitches()
+
+  Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+                                       'Usage:',
+                                       '  %s [option|cfgfile]' % Script.scriptName, ] ) )
+
+  Script.addDefaultOptionValue( 'LogLevel', 'error' )
+  Script.parseCommandLine( ignoreErrors = False )
+
+  execute()
