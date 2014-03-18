fad082920a2ddaf937ac7c56e6d9527ac8a14f0d
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-03-18 14:21:08 +0000

    sends an email when a new architecture is detected
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@75084 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/Core/scripts/dirac-architecture.py
+++ LHCbDIRAC/Core/scripts/dirac-architecture.py
@@ -21,10 +21,27 @@ if __name__ == "__main__" :
 
   OS = '_'.join( dist ).replace( ' ', '' )
 
+  error = False
   res = getDIRACPlatform( '_'.join( [platform.machine(), OS] ) )
   if not res['OK']:
-    print "ERROR: %s" % res['Message']
+    msg = "ERROR: %s" % res['Message']
+    error = True
   elif not res['Value']:
-    print "ERROR, %s not found" % '_'.join( [platform.machine(), OS] )
+    msg = "ERROR, %s not found" % '_'.join( [platform.machine(), OS] )
+    error = True
   else:
-    print res['Value'][0]
+    msg = res['Value'][0]
+    
+  print msg
+  
+  if error:
+    from DIRAC import gConfig
+    site = gConfig.getValue( 'LocalSite/Site' )
+    ce = gConfig.getValue( 'LocalSite/GridCE' )
+    queue = gConfig.getValue( 'LocalSite/CEQueue' )
+    body = "*** THIS IS AN AUTOMATED MESSAGE" + '\n\n' + msg + '\n\n'
+    body = body + "At site %s, CE = %s, queue = %s" % ( site, ce, queue )
+    from DIRAC.FrameworkSystem.Client.NotificationClient import NotificationClient
+    NotificationClient().sendMail('federico.stagni@cern.ch', 'Problem with DIRAC architecture', 
+                                  body, 'federico.stagni@cern.ch', localAttempt = False)
+    
