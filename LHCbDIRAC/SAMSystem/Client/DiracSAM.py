d7e0e97948535b2e1036d575ebb9fffba863beb7
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-03-27 10:28:53 +0000

    Moved to using API method for creating an application step
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@75290 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/SAMSystem/Client/DiracSAM.py
+++ LHCbDIRAC/SAMSystem/Client/DiracSAM.py
@@ -1,6 +1,6 @@
 """ LHCb SAM Dirac Class
 
-   The Dirac SAM class inherits generic VO functionality from the Dirac API base class.
+    The Dirac SAM class inherits generic VO functionality from the Dirac API base class.
 """
 
 from DIRAC.ConfigurationSystem.Client.Helpers.Operations import Operations
@@ -13,17 +13,15 @@ from LHCbDIRAC.Interfaces.API.LHCbJob      import LHCbJob
 COMPONENT_NAME = 'DiracSAMAPI'
 
 class DiracSAM( Dirac ):
-  """
-    DiracSAM: extension of Dirac Interface for SAM jobs
+  """ DiracSAM: extension of Dirac Interface for SAM jobs
     
-    It provides the following methods:
-    - getSuitableCEs
-    - submitSAMJob
+      It provides the following methods:
+      - getSuitableCEs
+      - submitSAMJob
   """
 
   def __init__( self ):
-    """
-       Instantiates the Workflow object and some default parameters.
+    """ Instantiates the Workflow object and some default parameters.
     """
     Dirac.__init__( self )
     
@@ -33,11 +31,10 @@ class DiracSAM( Dirac ):
     self.bannedSites = self.opsH.getValue( 'SAM/BannedSites', [] )
 
   def getSuitableCEs( self ):
-    """
-      Gets all CEs ( excluding the ones of banned sites )
+    """ Gets all CEs ( excluding the ones of banned sites )
     """
     
-    self.log.info( 'Banned SAM sites are: %s' % ( ', '.join( self.bannedSites ) ) )
+    self.log.info( "Banned SAM sites are: %s" % ( ', '.join( self.bannedSites ) ) )
     
     ceMapping = getCESiteMapping( self.gridType )
     if not ceMapping[ 'OK' ]:
@@ -53,28 +50,22 @@ class DiracSAM( Dirac ):
     return S_OK( validCEs )
 
   def defineSAMJob( self, ce ):
-    """
-      Defines an LHCbJob which is going to be submitted to a given ce, which the
-      following properties ( defaults ):
-      - logLevel      : verbose
-      - CPUTime       : 50000
-      - platform      : gLite-SAM
-      - outputSandbox : ['*.log']
-      - jobGroup      : SAM
-      - setType       : SAM
-      - priority      : 1
-      - submitPools   : SAM
+    """ Defines an LHCbJob which is going to be submitted to a given ce
       
-      Steps:
-      - GaudiApplication x 4 [ Gauss, Boole, Brunel, DaVinci ]
-      - UploadSAMLogs
+        Steps:
+        - CVMFSCheck
+        - GaudiApplication x 4 [ Gauss, Boole, Brunel, DaVinci ]
+        - UploadSAMLogs
     """
+
+    inputTypes = {'Boole'   : 'SIM',
+                  'Brunel'  : 'DIGI',
+                  'DaVinci' : 'DST'}
 
     #Get Job properties
     appTests = self.opsH.getValue( 'SAM/ApplicationTests', [] )
     samLogLevel = self.opsH.getValue( 'SAM/LogLevel', 'verbose' )
     samDefaultCPUTime = self.opsH.getValue( 'SAM/CPUTime', 50000 )
-#    samPlatform = self.opsH.getValue( 'SAM/Platform', 'gLite-SAM' )
     samOutputFiles = self.opsH.getValue( 'SAM/OutputSandbox', ['*.log'] )
     samGroup = self.opsH.getValue( 'SAM/JobGroup', 'Test' )
     samType = self.opsH.getValue( 'SAM/JobType', 'Test' )
@@ -86,31 +77,28 @@ class DiracSAM( Dirac ):
         return S_ERROR( 'Expected Integer for User priority' )
     
     #LHCbJob definition
-    samJob = LHCbJob( stdout = 'std.out', stderr =  'std.err' )
+    samJob = LHCbJob()
     res = samJob.setName( 'SAM-%s' % ce )
-    if not res[ 'OK' ]:
+    if not res['OK']:
       return res
 
     res = samJob.setDestinationCE( ce )
-    if not res[ 'OK' ]: 
+    if not res['OK']:
       return res    
     res = samJob.setLogLevel( samLogLevel )
-    if not res[ 'OK' ]: 
+    if not res['OK']:
       return res
     res = samJob.setCPUTime( samDefaultCPUTime )
-    if not res[ 'OK' ]: 
+    if not res['OK']:
       return res
-#     res = samJob.setPlatform( samPlatform )
-#     if not res[ 'OK' ]:
-#       return res
     res = samJob.setOutputSandbox( samOutputFiles )
-    if not res[ 'OK' ]:
+    if not res['OK']:
       return res
     res = samJob.setJobGroup( samGroup )
-    if not res[ 'OK' ]:
+    if not res['OK']:
       return res
     res = samJob.setType( samType )
-    if not res[ 'OK' ]:
+    if not res['OK']:
       return res
     
     samJob._addParameter( samJob.workflow, 'Priority', 'JDL', samPriority, 'User Job Priority' )
@@ -118,34 +106,77 @@ class DiracSAM( Dirac ):
     
     # CVMFS step definition
     stepName = 'CVMFSCheck'
-    step     = getStepDefinition( stepName, modulesNameList = [ 'CVMFSCheck' ],
-                                  importLine = "LHCbDIRAC.SAMSystem.Modules" )
+    step = getStepDefinition( stepName, modulesNameList = [stepName], importLine = "LHCbDIRAC.SAMSystem.Modules" )
     addStepToWorkflow( samJob.workflow, step, stepName )
    
+    gaudiSteps = []
     # Application Step definitions
     for appTest in appTests:
       
       appTestOptions = self.opsH.getOptionsDict( 'SAM/ApplicationTestOptions/%s' % appTest )
-      if not appTestOptions[ 'OK' ]:
-        return S_ERROR( '"SAM/ApplicationTestOptions/%s" is not defined or could not be retrieved' % appTest )
-      appTestOptions = appTestOptions[ 'Value' ]
-            
-      setSAMJobApplicationStep( samJob, appTest, appTestOptions )
-  
-    # Adding gaudiSteps
-    samJob._addParameter( samJob.workflow, 'gaudiSteps', 'list', appTests, 'list of Gaudi Steps' )
+      if not appTestOptions['OK']:
+        return S_ERROR( "'SAM/ApplicationTestOptions/%s' is not defined or could not be retrieved" % appTest )
+      appTestOptions = appTestOptions['Value']
+
+      inputDataType = inputTypes.get( appTest, '' )
+      if inputDataType:
+        inputData = 'previousStep'
+      else:
+        inputData = ''
+
+      applicationStep = samJob.setApplication( appName = appTest,
+                                               appVersion = appTestOptions.get( 'applicationVersion', '' ),
+                                               optionsFiles = appTestOptions.get( 'optionFiles', '' ),
+                                               inputData = inputData,
+                                               inputDataType = inputDataType,
+                                               events = 2,
+                                               extraPackages = 'ProdConf',
+                                               modulesNameList = ['GaudiApplication'],
+                                               parametersList = [
+                                                    ( 'applicationName', 'string', '', 'Application Name' ),
+                                                    ( 'applicationVersion', 'string', '', 'Application Version' ),
+                                                    ( 'applicationLog', 'string', '', 'Application output file' ),
+                                                    ( 'numberOfEvents', 'string', '', 'Events treated' ),
+                                                    ( 'outputFilePrefix', 'string', '', 'Data file name' ),
+                                                    ( 'XMLSummary', 'string', '', 'XMLSummaryFile name' ),
+                                                    ( 'extraPackages', 'string', '', 'extraPackages' ),
+                                                    ( 'optionsFile', 'string', '', 'optionsFile' ),
+                                                    ( 'listoutput', 'list', [], 'loutputs' ),
+                                                    ( 'DDDBTag', 'string', '', 'DDDBTag' ),
+                                                    ( 'CondDBTag', 'string', '', 'CondDBTag' ),
+                                                    ( 'inputData', 'string', '', 'iData' ),
+                                                    ( 'inputDataType', 'string', '', 'iDataType' ),
+                                                    ( 'SystemConfig', 'string', '', 'CMT Config' )
+                                                    ] )
+
+      if not applicationStep['OK']:
+        return applicationStep
+      filePrefix = appTest + appTestOptions.get( 'applicationVersion', '' )
+      outputFiles = self.__setOutputFile( appTest, filePrefix )
+      applicationStep['Value'].setValue( 'listoutput', [outputFiles] )
+      applicationStep['Value'].setValue( 'outputFilePrefix', filePrefix )
+
+      gaudiSteps.append( applicationStep['Value']['name'] )
+
+      if appTestOptions.get( 'DDDBTag', '' ):
+        applicationStep['Value'].setValue( 'DDDBTag', appTestOptions.get( 'DDDBTag', '' ) )
+      if appTestOptions.get( 'CondDBTag', '' ):
+        applicationStep['Value'].setValue( 'CondDBTag', appTestOptions.get( 'CondDBTag', '' ) )
+
+    samJob._addParameter( samJob.workflow, 'gaudiSteps', 'list', gaudiSteps, 'list of Gaudi Steps' )
     
     stepName = 'UploadSAMLogs' 
-    step     = getStepDefinition( stepName, modulesNameList = [ 'UploadSAMLogs' ], 
-                                  importLine = "LHCbDIRAC.SAMSystem.Modules" )
+    step = getStepDefinition( stepName, modulesNameList = [stepName],
+                              importLine = "LHCbDIRAC.SAMSystem.Modules" )
     addStepToWorkflow( samJob.workflow, step, stepName )
     
+    samJob.setDIRACPlatform()
+
     return S_OK( samJob )
   
   def submitNewSAMJob( self, ce, runLocal = False ):
-    """
-      Method that generates a NewStyle SAM Job and submits it to the given ce
-      if mode is wms. If mode is local, it will be run locally
+    """ Method that generates a NewStyle SAM Job and submits it to the given ce if mode is wms.
+        If mode is local, it will be run locally
     """
     
     mode = ( runLocal and 'local' ) or 'wms'
@@ -155,100 +186,21 @@ class DiracSAM( Dirac ):
       return samJob
     return self.submit( samJob[ 'Value' ], mode )
 
-# functions
-#...............................................................................  
-    
-def setInputFile( appName ):
-  """
-    Given an application, returns the extension of the files that the previous
-    step generated ( only applies to SAM jobs ).
-  """
-  
-  inputTypes = {
-                'Boole'   : 'SIM',
-                'Brunel'  : 'DIGI',
-                'DaVinci' : 'DST'
-               }
-    
-  return inputTypes.get( appName, '' )  
-  
-def setOutputFile( appName, outputFilePrefix ):
-  """
-    Given an application, returns a dictionary with its formatted output.
-    Note that outputDataSE is a dummy value ( this method only applies to
-    SAM Jobs ). 
-  """
+  @staticmethod
+  def __setOutputFile( appName, outputFilePrefix ):
+    """ Given an application, returns a dictionary with its formatted output.
+        Note that outputDataSE is a dummy value ( this method only applies to SAM Jobs ).
+    """
 
-  outputType = { 
-                'Gauss'  : 'sim', 
-                'Boole'  : 'digi',
-                'Brunel' : 'dst'
-               }
-  fileTypesOut = {}
-  if appName in outputType:
-    fileTypesOut     = { 'outputDataType' : outputType[ appName ], 
-                         'outputDataName' : '%s.%s' % ( outputFilePrefix,  outputType[ appName ] ),
-                         'outputDataSE'   : 'Spock' }
+    outputType = {'Gauss'  : 'sim',
+                  'Boole'  : 'digi',
+                  'Brunel' : 'dst',
+                  'DaVinci': 'AllStreams.dst' }
+    fileTypesOut = {}
+    if appName in outputType:
+      fileTypesOut = {'outputDataType': outputType[appName],
+                      'outputDataName': '%s.%s' % ( outputFilePrefix, outputType[appName] ),
+                      'outputBKType': outputType[appName].upper(),
+                      'outputDataSE': 'Spock' }
 
-  return fileTypesOut
-     
-def setSAMJobApplicationStep( samJob, appName, appOptions ):
-  """
-    Given a SAM job, sets an step ( GaudiApplication ) for the given app name.
-    It also adds to the step the options passed on the appOptions dictionary
-    if they apply. 
-  """
-
-  #StepDefinition
-  modules    = [ 'GaudiApplication' ]
-  paramsList = [ ( 'applicationName', 'string', '', 'Application Name' ), 
-                 ( 'applicationVersion', 'string', '', 'Application Version' ), 
-                 ( 'applicationLog', 'string', '', 'Application output file' ), 
-                 ( 'numberOfEvents', 'string', '', 'Events treated' ), 
-                 ( 'outputFilePrefix', 'string', '', 'Data file name' ),
-                 ( 'XMLSummary', 'string', '', 'XMLSummaryFile name' ),
-                 ( 'extraPackages', 'string', '', 'extraPackages' ), 
-                 ( 'optionsFile','string','','optionsFile' ),
-                 ( 'listoutput','list',[],'loutputs' ),
-                 ( 'DDDBTag','string','','DDDBTag' ),
-                 ( 'CondDBTag','string','','CondDBTag' ),
-                 ( 'inputData','string','','iData' ),
-                 ( 'inputDataType','string','','iDataType' )
-                ]
-    
-  step = getStepDefinition( appName, modulesNameList = modules, parametersList = paramsList )
-  stepInstance = addStepToWorkflow( samJob.workflow, step, appName )
-
-  #Step Parameters Passing  
-  appVersion = appOptions.get( 'applicationVersion', '' )
-  filePrefix = appName + appVersion
-
-  dddbtag = appOptions.get( 'DDDBTag', '' )
-  conddbtag = appOptions.get( 'CondDBTag', '' )
-
-  # Depends on the appName
-  inputDataType = setInputFile( appName )
-  outputFiles = setOutputFile( appName, filePrefix )
-    
-  stepInstance.setValue( 'XMLSummary', 'summary@{applicationName}_@{STEP_ID}.xml' )
-  stepInstance.setValue( 'applicationName', appName )
-  stepInstance.setValue( 'applicationVersion', appVersion )
-  stepInstance.setValue( 'applicationLog', '%s.log' % appName )
-  stepInstance.setValue( 'numberOfEvents', 2 )
-  stepInstance.setValue( 'outputFilePrefix', filePrefix )
-  stepInstance.setValue( 'optionsFile', appOptions.get( 'optionFiles', '' ) )
-  stepInstance.setValue( 'extraPackages', 'ProdConf' )
-  if dddbtag:
-    stepInstance.setValue( 'DDDBTag', dddbtag )
-  if conddbtag:
-    stepInstance.setValue( 'CondDBTag', conddbtag )
-  if inputDataType:
-    stepInstance.setValue( 'inputData', 'previousStep' )
-    stepInstance.setValue( 'inputDataType', inputDataType )
-  if outputFiles:
-    stepInstance.setValue( 'listoutput', [ outputFiles ] )
-
-  return stepInstance  
-
-################################################################################
-#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
+    return fileTypesOut
