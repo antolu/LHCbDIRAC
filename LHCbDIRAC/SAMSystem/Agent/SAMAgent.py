9ff847ed383d1d3dc6143c888450b141c48e994d
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-03-27 10:28:51 +0000

    bunch of minor fixes
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@75289 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/SAMSystem/Agent/SAMAgent.py
+++ LHCbDIRAC/SAMSystem/Agent/SAMAgent.py
@@ -1,5 +1,5 @@
-''' SAM Agent submits SAM jobs
-'''
+""" SAM Agent submits SAM jobs
+"""
 
 import time
 
@@ -18,14 +18,11 @@ AGENT_NAME = 'SAM/SAMAgent'
 
 class SAMAgent( AgentModule ):  
   
-  # Max lifetime of jobs before being cleaned
-  __days = 2
-  
   def __init__( self, *args, **kwargs ):
     
     AgentModule.__init__( self, *args, **kwargs )
     
-    self.days = self.__days
+    self.days = 2
     
   def initialize( self ):
     
@@ -33,9 +30,6 @@ class SAMAgent( AgentModule ):
 
     self.am_setOption( 'shifterProxy', 'TestManager' )
 
-#    _operation = self.monitor.OP_SUM
-#    _bucket    = 3600 * 2
-#
 #    self.monitor.registerActivity( 'TotalSites',  'Total Sites',  'SAMAgent', 'Sites', _operation, _bucket )
 #    self.monitor.registerActivity( 'ActiveSites', 'Active Sites', 'SAMAgent', 'Sites', _operation, _bucket )
 #    self.monitor.registerActivity( 'BannedSites', 'Banned Sites', 'SAMAgent', 'Sites', _operation, _bucket )
@@ -50,11 +44,6 @@ class SAMAgent( AgentModule ):
       self.log.error( res[ 'Message' ] )
       return res
 
-#    deletedJobs = res[ 'Value' ]     
-#    res = self._siteAccount( deletedJobs )
-#    if not res[ 'OK' ]:
-#      self.log.error( res[ 'Message' ] )
-
     diracSAM = DiracSAM()
    
     ces = diracSAM.getSuitableCEs()
@@ -64,52 +53,18 @@ class SAMAgent( AgentModule ):
     ces = ces[ 'Value' ]
   
     for ce in ces:
-      #result = diracSAM.submitSAMJob( ce, softwareEnable = False )
       result = diracSAM.submitNewSAMJob( ce )
       if not result[ 'OK' ]:
         self.log.error( 'Submission to CE %s failed with message %s' % ( ce, result[ 'Message' ] ) )
 
     return result
 
-#  IXME: is this really needed ? Is it used ?
-#  def _siteAccount( self, deletedJobs ):
-#    '''
-#      Adds marks to agent monitor regarding the number of sites
-#    '''
-#
-#    wmsAdmin = RPCClient( 'WorkloadManagement/WMSAdministrator' )
-#    result = wmsAdmin.getSiteMask()
-#    
-#    if not result[ 'OK' ]:
-#      result[ 'Message' ] = "Can't get SiteMask: %s" % result[ 'Message' ]      
-#      return result
-#    sitesmask = result[ 'Value' ]
-#
-#    result = gConfig.getSections( '/Resources/Sites/LCG' )
-#    
-#    if not result[ 'OK' ]:
-#      result[ 'Message' ] = "Can't get Sites: %s" % result[ 'Message' ] 
-#      return result
-#    sites = result[ 'Value' ]
-#
-#    numsites     = len( sites )
-#    numsitesmask = len( sitesmask )
-#    self.monitor.addMark( 'TotalSites', numsites )
-#    self.monitor.addMark( 'ActiveSites', numsitesmask )
-#    self.monitor.addMark( 'BannedSites', numsites - numsitesmask )
-#    self.monitor.addMark( 'DeletedJobs', deletedJobs )
-#    self.log.verbose( 'Site Account finished' )
-#    return S_OK()
-
   def _clearSAMjobs( self ):
-    '''
-       Clear SAM jobs older than <self.days> days
-    '''
+    """ Clear SAM jobs older than <self.days> days
+    """
 
     self.log.info( "Clear SAM jobs for last %d day(s)" % self.days )
     dirac = Dirac()
-
-    #testName = 'CE-lhcb-availability'
 
     allces     = []
     jobIDs     = []
@@ -121,7 +76,7 @@ class SAMAgent( AgentModule ):
     sites = sites[ 'Value' ]   
     
     monitoring = RPCClient( 'WorkloadManagement/JobMonitoring' )
-    cutDate    = ( date.today() - timedelta( days = 2 ) ).strftime( '%Y-%m-%d' )
+    cutDate = ( date.today() - timedelta( days = self.days ) ).strftime( '%Y-%m-%d' )
 
     for site in sites:
 
@@ -174,8 +129,6 @@ class SAMAgent( AgentModule ):
       dirac.delete( int( job ) )
     self.log.info( "%s:" % ( AGENT_NAME ), " %d SAM jobs were deleted" % ( len( oldWaitingJobs ) ) )
     
-#    deletedJobs = len( oldWaitingJobs )
-
     ceOldWaitingJobs = ceOldWaitingJobs.keys()
     ceNewStartedJobs = ceNewStartedJobs.keys()
     
@@ -193,7 +146,7 @@ class SAMAgent( AgentModule ):
 
       self.log.verbose( "CE status:", "%s %d" % ( ce, status ) )
 
-    return S_OK()#deletedJobs )
+    return S_OK()
 
 ################################################################################
 #EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
