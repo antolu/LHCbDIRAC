c6c3dc38888e583601660941c7dc47a6f0acfc04
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-06-26 10:15:32 +0000

    v6r11 (v6r12 not out yet)
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@76570 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/config/dirac.sh
+++ tests/Jenkins/config/dirac.sh
@@ -169,7 +169,7 @@ dirac_externals(){
   
   wget --no-check-certificate -O dirac-install 'https://github.com/DIRACGrid/DIRAC/raw/integration/Core/scripts/dirac-install.py' --quiet
   #python dirac-install -X -l DIRAC -r `cat $WORKSPACE/new_tag.txt` -t server -p 'Linux_x86_64_glibc-2.5'
-  python dirac-install -X -l DIRAC -r v6r12 -t server -p 'Linux_x86_64_glibc-2.5'
+  python dirac-install -X -l DIRAC -r v6r11 -t server -p 'Linux_x86_64_glibc-2.5'
   ( 
     cd $WORKSPACE
     sed -i 's/`$DIRACSCRIPTS\/dirac-platform`/Linux_x86_64_glibc-2.5/g' bashrc
