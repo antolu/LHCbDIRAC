8c1c9c01b49c14b49b4940c659b1b592411bbaa4
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2014-08-29 08:50:13 +0000

    Many fixes, identifying active datasets, using visibility flag
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@77693 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-scan-popularity.py
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-scan-popularity.py
@@ -20,17 +20,17 @@ def cacheDirectories( directories ):
   from DIRAC.Core.Utilities.List import breakListIntoChunks
 
   # Ignore cached directories
-  dirList = directories - set( bkPathForLfn )
-  if not dirList:
+  dirSet = directories - set( bkPathForLfn )
+  if not dirSet:
     return
   startTime = time.time()
-  gLogger.info( 'Caching %d directories, only %d not cached yet' % ( len( directories ), len( dirList ) ) )
+  gLogger.always( 'Caching %d directories, %d not cached yet' % ( len( directories ), len( dirSet ) ) )
 
   # # keep only the first directory when numbered
   dirLong2Short = {}
   dirShort2Long = {}
   # List must be sorted in order to get it from the cache!
-  for lfn in sorted( dirList ):
+  for lfn in sorted( dirSet ):
     splitDir = lfn.split( '/' )
     if len( splitDir[-2] ) == 4 and splitDir[-2].isdigit():
       short = '/'.join( splitDir[:-2] )
@@ -41,60 +41,82 @@ def cacheDirectories( directories ):
 
   # # Cache BK path for directories
   chunkSize = 100
-  gLogger.info( 'Getting cache metadata for %d directories' % len( dirShort2Long ) )
+  gLogger.always( 'Getting cache metadata for %d directories' % len( dirShort2Long ) )
   # First try cached information in Data Usage DB
+  invisible = set()
   res = duClient.getDirMetadata( dirShort2Long.values() )
   if not res['OK']:
     gLogger.fatal( "\nError getting metadata from DataUsage", res['Message'] )
     lfnsFromBK = set( dirShort2Long.values() )
   else:
     lfnsFromBK = set()
-    for lfn in dirList:
+    for lfn in dirSet:
       longDir = dirShort2Long[dirLong2Short[lfn]]
       metadata = res['Value'].get( longDir )
       if metadata:
-        bkPathForLfn[lfn] = os.path.join( '/', metadata[1], metadata[2], metadata[3], metadata[4][1:], metadata[5], metadata[6] )
+        if len( metadata ) < 9 or metadata[8] != "N":
+          bkPathForLfn[lfn] = os.path.join( '/', metadata[1], metadata[2], metadata[3], metadata[4][1:], metadata[5], metadata[6] )
+          processingPass[bkPathForLfn[lfn]] = metadata[4]
+          prodForBKPath.setdefault( bkPathForLfn[lfn], set() ).add( metadata[7] )
+        else:
+          invisible.add( lfn )
       else:
         lfnsFromBK.add( longDir )
   # For those not available ask the BK
   if lfnsFromBK:
+    gLogger.always( 'Getting BK metadata for %d directories' % len( lfnsFromBK ) )
     success = {}
     for lfns in breakListIntoChunks( list( lfnsFromBK ), chunkSize ):
-      res = bkClient.getDirectoryMetadata( lfns )
-      if not res['OK']:
-        gLogger.fatal( "\nError getting BK metadata", res['Message'] )
-        DIRAC.exit( 1 )
+      while True:
+        res = bkClient.getDirectoryMetadata( lfns )
+        if not res['OK']:
+          gLogger.fatal( "\nError getting BK metadata", res['Message'] )
+        else:
+          break
       success.update( res['Value'].get( 'Successful', {} ) )
     gLogger.always( '' )
-    for lfn in dirList - set( bkPathForLfn ):
+    for lfn in dirSet - set( bkPathForLfn ):
       longDir = dirShort2Long[dirLong2Short[lfn]]
-      if longDir in success:
-        metadata = success[longDir][0]
-        bkPathForLfn[lfn] = BKQuery( metadata ).makePath()
+      metadata = success.get( longDir, [{}] )[0]
+      if metadata:
+        if metadata.get( 'VisibilityFlag', 'Y' ) == 'Y':
+          bkPathForLfn[lfn] = BKQuery( metadata ).makePath()
+          processingPass[bkPathForLfn[lfn]] = metadata['ProcessingPass']
+          prodForBKPath.setdefault( bkPathForLfn[lfn], set() ).add( metadata['Production'] )
+        else:
+          invisible.add( lfn )
       else:
         bkPathForLfn[lfn] = "Unknown-" + lfn
-  gLogger.info( 'Obtained BK path' )
+        prodForBKPath.setdefault( bkPathForLfn[lfn], set() )
+  gLogger.always( 'Obtained BK path' )
+  if invisible:
+    gLogger.always( 'The following paths are ignored as invisible:\n', '\n'.join( sorted( invisible ) ) )
+  dirSet -= invisible
 
   # # Get the creation date
-  for lfns in breakListIntoChunks( list( dirList ), chunkSize ):
-    res = fcClient.getDirectoryMetadata( lfns )
-    if not res['OK']:
-      gLogger.fatal( 'Error getting directory metadata', res['Message'] )
-      DIRAC.exit( 1 )
+  for lfns in breakListIntoChunks( list( dirSet ), chunkSize ):
+    while True:
+      res = fcClient.getDirectoryMetadata( lfns )
+      if not res['OK']:
+        gLogger.fatal( 'Error getting directory metadata', res['Message'] )
+      else:
+        break
     success = res['Value']['Successful']
     for lfn in success:
       bkPath = bkPathForLfn[lfn]
       ct = creationTime.get( bkPath, datetime.now() )
       creationTime[bkPath] = min( success[lfn]['CreationDate'], ct )
 
-  missingSU = set( [dirLong2Short[lfn] for lfn in dirList] )
-  gLogger.info( 'Get LFN Storage Usage for %d directories' % len( missingSU ) )
+  missingSU = set( [dirLong2Short[lfn] for lfn in dirSet] )
+  gLogger.always( 'Get LFN Storage Usage for %d directories' % len( missingSU ) )
   for lfn in missingSU:
     # LFN usage
-    res = suClient.getSummary( lfn )
-    if not res['OK']:
-      gLogger.fatal( 'Error getting LFN storage usage %s' % lfn, res['Message'] )
-      DIRAC.exit( 1 )
+    while True:
+      res = suClient.getSummary( lfn )
+      if not res['OK']:
+        gLogger.fatal( 'Error getting LFN storage usage %s' % lfn, res['Message'] )
+      else:
+        break
     bkPath = bkPathForLfn[dirShort2Long[lfn]]
     type = 'LFN'
     bkPathUsage.setdefault( bkPath, {} ).setdefault( type, [0, 0] )
@@ -102,26 +124,29 @@ def cacheDirectories( directories ):
     bkPathUsage[bkPath][type][1] += sum( [val.get( 'Size', 0 ) for val in res['Value'].values()] )
 
   # # get the PFN usage per storage type
-  gLogger.info( 'Check storage type and PFN usage for %d directories' % len( dirList ) )
-  for lfn in dirList:
-    res = suClient.getDirectorySummaryPerSE( lfn )
-    if not res['OK']:
-      gLogger.fatal( 'Error getting storage usage per SE %s' % lfn, res['Message'] )
-      DIRAC.exit( 1 )
+  gLogger.always( 'Check storage type and PFN usage for %d directories' % len( dirSet ) )
+  for lfn in dirSet:
+    while True:
+      res = suClient.getDirectorySummaryPerSE( lfn )
+      if not res['OK']:
+        gLogger.fatal( 'Error getting storage usage per SE %s' % lfn, res['Message'] )
+      else:
+        break
     info = physicalDataUsage.setdefault( lfn, {} )
     for type in storageTypes:
+      # Active type will be recorded in Disk, just a special flag
       if type != 'LFN' and type not in info:
         nf = sum( [val['Files'] for se, val in res['Value'].items() if isType( se, type )] )
         size = sum( [val['Size'] for se, val in res['Value'].items() if isType( se, type )] )
         info[type] = {'Files':nf, 'Size':size}
     bkPath = bkPathForLfn[lfn]
-    datasetStorage[storageType( res['Value'] )].add( bkPath )
     for type in info:
       bkPathUsage.setdefault( bkPath, {} ).setdefault( type, [ 0, 0 ] )
       bkPathUsage[bkPath][type][0] += info[type].get( 'Files', 0 )
       bkPathUsage[bkPath][type][1] += info[type].get( 'Size', 0 )
+    datasetStorage[storageType( res['Value'] )].add( bkPath )
 
-  gLogger.always( 'Obtained BK path and storage usage of %d directories in %.1f seconds' % ( len( dirList ), time.time() - startTime ) )
+  gLogger.always( 'Obtained BK path and storage usage of %d directories in %.1f seconds' % ( len( dirSet ), time.time() - startTime ) )
 
 def isType( se, type ):
   if type == 'All':
@@ -153,34 +178,35 @@ def prSize( size ):
   return '???'
 
 def getPhysicalUsage( baseDir ):
-  res = suClient.getStorageDirectoryData( baseDir, None, None, None, timeout = 3600 )
-  if not res['OK']:
-    gLogger.fatal( "Error getting list of directories for %s" % baseDir, res['Message'] )
-    DIRAC.exit( 1 )
-  else:
-    # The returned value is a dictionary of all directory leaves
-    info = res['Value']
-    dirSet = set()
-    nf = 0
-    size = 0
-    gLogger.info( "Storage usage OK for %d directories in %s" % ( len( info ), baseDir ) )
-    for directory in info:
-      nodes = directory.split( '/' )
-      try:
-        prod = int( nodes[5] )
-        # FIXME
-        # Don't count the certification productions
-        if prod < 2000 and nodes[2] == 'validation':
-          continue
-      except:
-        pass
-      if len( nodes ) < 5 or nodes[4] not in ( 'LOG', 'HIST', 'BRUNELHIST', 'DAVINCIHIST', 'GAUSSHIST' ):
-        physicalDataUsage.setdefault( directory, {} ).setdefault( 'All', info[directory] )
-        dirSet.add( directory )
-        nf += info[directory]['Files']
-        size += info[directory]['Size']
-    # In case the base directory was not a leave, cache its physical storage data
-    physicalDataUsage.setdefault( baseDir, {} ).setdefault( 'All', {'Files':nf, 'Size':size} )
+  while True:
+    res = suClient.getStorageDirectoryData( baseDir, None, None, None, timeout = 3600 )
+    if not res['OK']:
+      gLogger.fatal( "Error getting list of directories for %s" % baseDir, res['Message'] )
+    else:
+      break
+  # The returned value is a dictionary of all directory leaves
+  info = res['Value']
+  dirSet = set()
+  nf = 0
+  size = 0
+  gLogger.info( "Storage usage OK for %d directories in %s" % ( len( info ), baseDir ) )
+  for directory in info:
+    nodes = directory.split( '/' )
+    try:
+      prod = int( nodes[5] )
+      # FIXME
+      # Don't count the certification productions
+      if prod < 2000 and nodes[2] == 'validation':
+        continue
+    except:
+      pass
+    if len( nodes ) < 5 or nodes[4] not in ( 'LOG', 'HIST', 'BRUNELHIST', 'DAVINCIHIST', 'GAUSSHIST' ):
+      physicalDataUsage.setdefault( directory, {} ).setdefault( 'All', info[directory] )
+      dirSet.add( directory )
+      nf += info[directory]['Files']
+      size += info[directory]['Size']
+  # In case the base directory was not a leave, cache its physical storage data
+  physicalDataUsage.setdefault( baseDir, {} ).setdefault( 'All', {'Files':nf, 'Size':size} )
   return dirSet
 
 def storageType( seList ):
@@ -216,18 +242,22 @@ if __name__ == '__main__':
       getAllDatasets = True
 
   bkPathForLfn = {}
+  prodForBKPath = {}
   bkPathUsage = {}
+  processingPass = {}
 
   from LHCbDIRAC.DataManagementSystem.Client.StorageUsageClient import StorageUsageClient
   from LHCbDIRAC.DataManagementSystem.Client.DataUsageClient import DataUsageClient
   from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient   import BookkeepingClient
   from LHCbDIRAC.BookkeepingSystem.Client.BKQuery   import BKQuery
+  from LHCbDIRAC.TransformationSystem.Client.TransformationClient import TransformationClient
   from DIRAC.Resources.Catalog.FileCatalog import FileCatalog
   from datetime import datetime, timedelta
   duClient = DataUsageClient()
   suClient = StorageUsageClient()
   bkClient = BookkeepingClient()
   fcClient = FileCatalog()
+  transClient = TransformationClient()
 
   # Dictionary with weekly/dayly usage for each BK path
   timeUsage = {}
@@ -282,10 +312,10 @@ if __name__ == '__main__':
       startTimeQuery = startTime.isoformat()
       status = 'Used'
       for _i in range( 10 ):
-        res = duClient.getDataUsageSummary( startTimeQuery, endTimeQuery, status, timeout = 3600 )
+        res = duClient.getDataUsageSummary( startTimeQuery, endTimeQuery, status, timeout = 7200 )
         if res['OK']:
           break
-        gLogger.warn( "Error getting popularity entries, retrying...", res['Message'] )
+        gLogger.error( "Error getting popularity entries, retrying...", res['Message'] )
       if not res['OK']:
         gLogger.fatal( "Error getting popularity entries", res['Message'] )
         DIRAC.exit( 1 )
@@ -318,13 +348,17 @@ if __name__ == '__main__':
     gLogger.always( "Retrieved %d entries from Popularity table in %.1f seconds" % ( entries, time.time() - stTime ) )
     gLogger.always( 'Found %d datasets used since %d days' % ( len( timeUsage ), since ) )
     counters = {}
+    strangeBKPaths = set( [bkPath for bkPath in timeUsage if not bkPathUsage[bkPath].get( 'LFN', ( 0, 0 ) )[0]] )
+    if strangeBKPaths:
+      gLogger.always( 'Some datasets do not have an LFN count:' )
+      gLogger.always( '\n'.join( ["%s : %s" % ( bkPath, str( bkPathUsage[bkPath] ) ) for bkPath in strangeBKPaths] ) )
     for type in ( 'All', 'LFN' ):
       for i in range( 2 ):
-        counters.setdefault( type, [] ).append( sum( [bkPathUsage[bkPath][type][i] for bkPath in timeUsage] ) )
+        counters.setdefault( type, [] ).append( sum( [bkPathUsage[bkPath].get( type, ( 0, 0 ) )[i] for bkPath in timeUsage] ) )
     for bkPath in sorted( timeUsage ):
       datasetStorage[storageType( usedSEs[bkPath] )].add( bkPath )
-      nLfns, lfnSize = bkPathUsage[bkPath]['LFN']
-      nPfns, pfnSize = bkPathUsage[bkPath]['All']
+      nLfns, lfnSize = bkPathUsage[bkPath].get( 'LFN', ( 0, 0 ) )
+      nPfns, pfnSize = bkPathUsage[bkPath].get( 'All', ( 0, 0 ) )
       gLogger.always( '%s (%d LFNs, %s), (%d PFNs, %s, %.1f replicas)' % ( bkPath, nLfns, prSize( lfnSize ), nPfns, prSize( pfnSize ), float( nPfns ) / float( nLfns ) if nLfns else 0. ) )
       bins = sorted( timeUsage[bkPath] )
       lastBin = bins[-1]
@@ -357,10 +391,10 @@ if __name__ == '__main__':
         counters = {}
         for t in ( 'All', 'LFN' ):
           for i in range( 2 ):
-            counters.setdefault( t, [] ).append( sum( [bkPathUsage[bkPath][t][i] for bkPath in unusedPaths] ) )
+            counters.setdefault( t, [] ).append( sum( [bkPathUsage[bkPath].get( t, ( 0, 0 ) )[i] for bkPath in unusedPaths] ) )
         for bkPath in sorted( unusedPaths ):
-          nLfns, lfnSize = bkPathUsage[bkPath]['LFN']
-          nPfns, pfnSize = bkPathUsage[bkPath]['All']
+          nLfns, lfnSize = bkPathUsage[bkPath].get( 'LFN', ( 0, 0 ) )
+          nPfns, pfnSize = bkPathUsage[bkPath].get( 'All', ( 0, 0 ) )
           gLogger.always( '\t%s (%d LFNs, %s), (%d PFNs, %s, %.1f replicas)' % ( bkPath, nLfns, prSize( lfnSize ), nPfns, prSize( pfnSize ), float( nPfns ) / float( nLfns ) ) )
         gLogger.always( "\nA total of %d %s LFNs (%s), %d PFNs (%s) were not used" % ( counters['LFN'][0], type, prSize( counters['LFN'][1] ), counters['All'][0], prSize( counters['All'][1] ) ) )
       noBKDirectories = sorted( unusedDirectories - set( bkPathForLfn ) )
@@ -380,25 +414,38 @@ if __name__ == '__main__':
   for bkPath in sorted( timeUsage ) + sorted( unusedBKPaths ):
     if bkPath.startswith( 'Unknown-' ):
       continue
-    processingPass = BKQuery( bkPath ).getProcessingPass()
     info = bkPathUsage[bkPath]
+    # If there are fewer Archive replicas than LFNs, check if the production is still active
+    lfns = info.get( 'LFN', ( 0, 0 ) )[0]
+    archives = info.get( 'Archived', ( 0, 0 ) )[0]
+    active = False
+    if archives and archives < lfns and prodForBKPath[bkPath]:
+      for prod in prodForBKPath.get( bkPath, () ):
+        res = transClient.getTransformation( prod )
+        if res.get( 'Value', {} ).get( 'Status', '' ) == 'Active':
+          # print 'Prod', prod, 'Active'
+          active = True
+          break
     if info['LFN'][0] == 0:
       continue
     for type in info:
       info[type][1] /= TB
     # Some BK paths contain a , to be replaces by a . for the CSV file!!
     config = '/'.join( bkPath.split( '/' )[0:3] )
-    row = '%s,%s,%s' % ( bkPath.replace( ',', '.' ).replace( 'Real Data', 'RealData' ), config, processingPass )
+    row = '%s,%s,%s' % ( bkPath.replace( ',', '.' ).replace( 'Real Data', 'RealData' ), config, processingPass.get( bkPath, 'Unknown' ) )
     row += ',0' if bkPath.startswith( '/MC' ) else ',1'
     row += ',%d' % ( getTimeBin( creationTime.get( bkPath, datetime.now() ) ) )
     for type in ( 'LFN', 'Disk', 'Tape', 'Archived' ):
       row += ',%d,%f' % tuple( info[type] )
     row += ',%f,%f' % ( float( info['Disk'][0] ) / float( info['LFN'][0] ), float( info['Archived'][0] ) / float( info['LFN'][0] ) )
-    dsType = 'Unknown'
-    for type in storageTypes[0:3]:
-      if bkPath in datasetStorage[type]:
-        dsType = type
-        break
+    if active:
+      dsType = 'Active'
+    else:
+      dsType = 'Unknown'
+      for type in storageTypes[0:3]:
+        if bkPath in datasetStorage[type]:
+          dsType = type
+          break
     row += ',%s' % dsType
     bins = sorted( timeUsage.get( bkPath, {} ) )
     if not bins:
