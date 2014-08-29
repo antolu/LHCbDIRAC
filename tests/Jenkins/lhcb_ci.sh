458cc0b270880a34efe722be944be92300b8be60
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-08-29 10:18:35 +0000

    added UseServerCertificate to DIRACPilot
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@77697 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci.sh
+++ tests/Jenkins/lhcb_ci.sh
@@ -709,6 +709,10 @@ function fullInstall(){
 
 function DIRACPilotInstall(){
 	
+	#certs and proxy first
+	generateCertificates
+	generateUserCredentials
+	
 	if [ ! -z "$DEBUG" ]
 	then
 		echo 'Running in DEBUG mode'
@@ -723,12 +727,12 @@ function DIRACPilotInstall(){
 	wget --no-check-certificate -O LHCbPilotCommands.py $LHCbDIRAC_PILOT_COMMANDS
 
 	#run the dirac-pilot script, only for installing
-	python dirac-pilot.py -S LHCb-Certification -l LHCb -M 5 -C dips://lhcb-conf-dirac.cern.ch:9135/Configuration/Server -e LHCb -T 50000 -N jenkins.cern.ch -Q cream-lsf-grid_2nh_lhcb -n DIRAC.Jenkins.ch -o '/LocalSite/CPUScalingFactor=4.0' -o '/LocalSite/CPUNormalizationFactor=4.0' -E LHCbPilot -X GetLHCbPilotVersion,InstallLHCbDIRAC $DEBUG
-	#this sould have been created
+python dirac-pilot.py -S LHCb-Certification -l LHCb -M 5 -C dips://lhcb-conf-dirac.cern.ch:9135/Configuration/Server -e LHCb -T 50000 -N jenkins.cern.ch -Q cream-lsf-grid_2nh_lhcb -n DIRAC.Jenkins.ch -o '/LocalSite/CPUScalingFactor=4.0' -o '/LocalSite/CPUNormalizationFactor=4.0' --UseServerCertificate -E LHCbPilot -X GetLHCbPilotVersion,InstallLHCbDIRAC,ConfigureDIRAC $DEBUG
+	#this should have been created
 	source bashrc
 	
 	#run the configuration, "by hand" because dirac-architecture would require a proxy...
-	python $WORKSPACE/DIRAC/Core/scripts/dirac-configure.py -n "DIRAC.Jenkins.ch" -N "jenkins.cern.ch" -S "LHCb-Certification" -C "dips://lhcb-conf-dirac.cern.ch:9135/Configuration/Server" -o /LocalSite/ReleaseProject=LHCb -o "/LocalSite/CPUScalingFactor=4.0" -o "/LocalSite/CPUNormalizationFactor=4.0" -o /LocalSite/GridMiddleware=DIRAC -N "jenkins.cern.ch" -o /LocalSite/GridCE=jenkins.cern.ch -o /LocalSite/ReleaseVersion=v8r0-pre9 -o /LocalSite/Architecture=x86_64-slc6 -I
+	#python $WORKSPACE/DIRAC/Core/scripts/dirac-configure.py -n "DIRAC.Jenkins.ch" -N "jenkins.cern.ch" -S "LHCb-Certification" -C "dips://lhcb-conf-dirac.cern.ch:9135/Configuration/Server" -o /LocalSite/ReleaseProject=LHCb -o "/LocalSite/CPUScalingFactor=4.0" -o "/LocalSite/CPUNormalizationFactor=4.0" -o /LocalSite/GridMiddleware=DIRAC -N "jenkins.cern.ch" -o /LocalSite/GridCE=jenkins.cern.ch -o /LocalSite/ReleaseVersion=v8r0-pre9 -o /LocalSite/Architecture=x86_64-slc6 -I
 
 }
 
