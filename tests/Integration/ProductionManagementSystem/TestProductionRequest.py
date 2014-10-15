1003f7c358bf04238f9f8b91d8234cfe336cdd61
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-10-15 08:13:24 +0000

    just username
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@78493 4525493e-7705-40b1-a816-d608a930855b

--- tests/Integration/ProductionManagementSystem/TestProductionRequest.py
+++ tests/Integration/ProductionManagementSystem/TestProductionRequest.py
@@ -31,23 +31,27 @@ class TestProductionRequestTestCaseChain( TestProductionRequestTestCase ):
     self.assertEqual( res['Value'], [] )
 
     # add
-    res = self.reqClient.createProductionRequest( {'ParentID':'', 'MasterID':'', 'RequestAuthor': 'anonymous',
-                    'RequestName': 'RequestName', 'RequestType': 'RequestType', 'RequestState': 'New',
-                    'RequestPriority': '2b', 'RequestPDG':'',
-                    'SimCondition':'Beam3500GeV-VeloClosed-MagUp', 'SimCondID':'429215', 'SimCondDetail':'BlahBlahBlah',
-                    'ProPath':'Blup', 'ProID':'', 'ProDetail':'BlaaaaaahBlahBlah',
-                    'EventType':'900000', 'NumberOfEvents':'-1', 'Description':'Description', 'Comments':'Comments',
-                    'Inform':'', 'RealNumberOfEvents':'', 'IsModel':0, 'Extra':''} )
+    res = self.reqClient.createProductionRequest( {'ParentID':'', 'MasterID':'', 'RequestAuthor': 'adminusername',
+                                                   'RequestName': 'RequestName', 'RequestType': 'RequestType',
+                                                   'RequestState': 'New', 'RequestPriority': '2b', 'RequestPDG':'',
+                                                   'SimCondition':'Beam3500GeV-VeloClosed-MagUp', 'SimCondID':'429215',
+                                                   'SimCondDetail':'BlahBlahBlah',
+                                                   'ProPath':'Blup', 'ProID':'', 'ProDetail':'BlaaaaaahBlahBlah',
+                                                   'EventType':'900000', 'NumberOfEvents':'-1',
+                                                   'Description':'Description', 'Comments':'Comments',
+                                                   'Inform':'', 'RealNumberOfEvents':'', 'IsModel':0, 'Extra':''} )
     self.assert_( res['OK'] )
     firstReq = res['Value']
   
-    res = self.reqClient.createProductionRequest( {'ParentID':'', 'MasterID':'', 'RequestAuthor': 'anonymous',
-                    'RequestName': 'RequestName - new', 'RequestType': 'RequestType', 'RequestState': 'New',
-                    'RequestPriority': '2b', 'RequestPDG':'',
-                    'SimCondition':'Beam3500GeV-VeloClosed-MagUp', 'SimCondID':'429215', 'SimCondDetail':'BlahBlahBlah',
-                    'ProPath':'Blup', 'ProID':'', 'ProDetail':'BlaaaaaahBlahBlah',
-                    'EventType':'900000', 'NumberOfEvents':'-1', 'Description':'Description', 'Comments':'Comments',
-                    'Inform':'', 'RealNumberOfEvents':'', 'IsModel':0, 'Extra':''} )
+    res = self.reqClient.createProductionRequest( {'ParentID':'', 'MasterID':'', 'RequestAuthor': 'adminusername',
+                                                   'RequestName': 'RequestName - new', 'RequestType': 'RequestType',
+                                                   'RequestState': 'New', 'RequestPriority': '2b', 'RequestPDG':'',
+                                                   'SimCondition':'Beam3500GeV-VeloClosed-MagUp', 'SimCondID':'429215',
+                                                   'SimCondDetail':'BlahBlahBlah',
+                                                   'ProPath':'Blup', 'ProID':'', 'ProDetail':'BlaaaaaahBlahBlah',
+                                                   'EventType':'900000', 'NumberOfEvents':'-1',
+                                                   'Description':'Description', 'Comments':'Comments',
+                                                   'Inform':'', 'RealNumberOfEvents':'', 'IsModel':0, 'Extra':''} )
     self.assert_( res['OK'] )
     self.assertEqual( res['Value'], firstReq + 1 )
 
@@ -69,7 +73,7 @@ class TestProductionRequestTestCaseChain( TestProductionRequestTestCase ):
 
     # delete
     res = self.reqClient.deleteProductionRequest( firstReq )
-    self.assert_( res['OK'] )
+    self.assertFalse( res['OK'] )
     res = self.reqClient.deleteProductionRequest( firstReq + 1 )
     self.assert_( res['OK'] )
 
