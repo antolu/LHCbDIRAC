098d56b9475b4c88ec56b1fcbe9abe05c3f99375
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-10-21 16:55:25 +0000

    Handling pilot.cfg everywhere
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@78635 4525493e-7705-40b1-a816-d608a930855b

--- tests/Regression/Test_RegressionProductionJobs.py
+++ tests/Regression/Test_RegressionProductionJobs.py
@@ -21,16 +21,45 @@ class RegressionTestCase( IntegrationTest ):
     self.diracLHCb = DiracLHCb()
     self.bkkClient = BookkeepingClient()
 
+    exeScriptLoc = find_all( 'exe-script.py', '.', 'Regression' )[0]
+    helloWorldLoc = find_all( 'helloWorld.py', '.', 'Regression' )[0]
+
+    shutil.copyfile( exeScriptLoc, './exe-script.py' )
+    shutil.copyfile( helloWorldLoc, './helloWorld.py' )
+
 #    self.j_mc_20160 = LHCbJob( '20160.xml' )
-    self.j_mc_31202 = LHCbJob( '31202.xml' )
-    self.j_reco_20194 = LHCbJob( '20194.xml' )
-    self.j_reco_20194_old = LHCbJob( '20194_old.xml' )
+    location31202 = find_all( '31202.xml', '.', 'Regression' )[0]
+    self.j_mc_31202 = LHCbJob( location31202 )
+    self.j_mc_31202.setConfigArgs( 'pilot.cfg' )
+
+    location20194 = find_all( '20194.xml', '.', 'Regression' )[0]
+    self.j_reco_20194 = LHCbJob()
+    self.j_reco_20194.setConfigArgs( 'pilot.cfg' )
+
+    location20194old = find_all( '20194_old.xml', '.', 'Regression' )[0]
+    self.j_reco_20194_old = LHCbJob( location20194old )
+    self.j_reco_20194_old.setConfigArgs( 'pilot.cfg' )
+
 #    self.j_stripp_20349 = LHCbJob( '20349.xml' )
-    self.j_stripp_31017 = LHCbJob( '31017.xml' )
-    self.j_merge_20752 = LHCbJob( '20752.xml' )
-    self.j_merge_21211 = LHCbJob( '21211.xml' )
-    self.j_mergeMDF_20657 = LHCbJob( '20657.xml' )
-    self.j_swimming_31057 = LHCbJob( '31057.xml' )
+    location31017 = find_all( '31017.xml', '.', 'Regression' )[0]
+    self.j_stripp_31017 = LHCbJob( location31017 )
+    self.j_stripp_31017.setConfigArgs( 'pilot.cfg' )
+
+    location20752 = find_all( '20752.xml', '.', 'Regression' )[0]
+    self.j_merge_20752 = LHCbJob( location20752 )
+    self.j_merge_20752.setConfigArgs( 'pilot.cfg' )
+
+    location21211 = find_all( '21211.xml', '.', 'Regression' )[0]
+    self.j_merge_21211 = LHCbJob( location21211 )
+    self.j_merge_21211.setConfigArgs( 'pilot.cfg' )
+
+    location20657 = find_all( '20657.xml', '.', 'Regression' )[0]
+    self.j_mergeMDF_20657 = LHCbJob( location20657 )
+    self.j_mergeMDF_20657.setConfigArgs( 'pilot.cfg' )
+
+    location31057 = find_all( '31057.xml', '.', 'Regression' )[0]
+    self.j_swimming_31057 = LHCbJob( location31057 )
+    self.j_swimming_31057.setConfigArgs( 'pilot.cfg' )
 
 class MCSuccess( RegressionTestCase ):
   def test_execute( self ):
