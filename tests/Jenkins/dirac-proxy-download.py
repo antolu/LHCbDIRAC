4a91ab9ed177dcef5f521d27004cbc8bbd41d706
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-10-21 13:49:18 +0000

    .
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@78618 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/dirac-proxy-download.py
+++ tests/Jenkins/dirac-proxy-download.py
@@ -11,5 +11,5 @@ from DIRAC.FrameworkSystem.Client.ProxyManagerClient        import gProxyManager
 
 gProxyManager.downloadProxyToFile( '/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=fstagni/CN=693025/CN=Federico Stagni',
                                     'lhcb_prmgr', limited = False, requiredTimeLeft = 1200,
-                                    cacheTime = 43200, filePath = 'x509up_u%s' % uid, proxyToConnect = False,
+                                    cacheTime = 43200, filePath = '/tmp/x509up_u%s' % uid, proxyToConnect = False,
                                     token = False )
