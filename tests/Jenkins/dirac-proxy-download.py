6cfb30a869040e93ebac9b34a31af74dea6f85cb
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-11-19 19:56:21 +0000

    added check of return value
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@79404 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/dirac-proxy-download.py
+++ tests/Jenkins/dirac-proxy-download.py
@@ -9,7 +9,11 @@ import os
 uid = os.getuid()
 from DIRAC.FrameworkSystem.Client.ProxyManagerClient        import gProxyManager
 
-gProxyManager.downloadProxyToFile( '/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=fstagni/CN=693025/CN=Federico Stagni',
-                                    'lhcb_prmgr', limited = False, requiredTimeLeft = 1200,
-                                    cacheTime = 43200, filePath = '/tmp/x509up_u%s' % uid, proxyToConnect = False,
-                                    token = False )
+res = gProxyManager.downloadProxyToFile( '/DC=ch/DC=cern/OU=Organic Units/OU=Users/CN=fstagni/CN=693025/CN=Federico Stagni',
+                                         'lhcb_prmgr', limited = False, requiredTimeLeft = 1200,
+                                         cacheTime = 43200, filePath = '/tmp/x509up_u%s' % uid, proxyToConnect = False,
+                                         token = False )
+
+if not res['OK']:
+  print "Error downloading proxy", res['Message']
+  exit( 1 )
