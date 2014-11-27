1df8b29e850329f35b935c750c8c8787125ab9cc
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-11-27 13:45:26 +0000

    Production.cfg file
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@79642 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/dirac-cfg-update-server.py
+++ tests/Jenkins/dirac-cfg-update-server.py
@@ -11,6 +11,9 @@ from DIRAC.Core.Utilities.CFG import CFG
 
 localCfg = CFG()
 
+localConfigFile = os.path.join( '.', 'etc', 'Production.cfg' )
+localCfg.loadFromFile( localConfigFile )
+
 os.makedirs( '/scratch/workspace/lhcbdirac_certification/sandboxes' )
 localCfg.setOption( 'WorkloadManagement/Production/Databases/SandboxStore/BasePath',
                     '/scratch/workspace/lhcbdirac_certification/sandboxes' )
