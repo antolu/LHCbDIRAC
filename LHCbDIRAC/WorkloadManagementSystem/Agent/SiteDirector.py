16fd4fe4ecab9f61c676999fadeb784c2ab156e1
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-11-28 11:42:11 +0000

    Script create in LHCb extension to avoid creating X509_CERT_DIR
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@79650 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/WorkloadManagementSystem/Agent/SiteDirector.py
+++ LHCbDIRAC/WorkloadManagementSystem/Agent/SiteDirector.py
@@ -4,8 +4,11 @@
 __RCSID__ = "$Id$"
 
 import os
+import base64
+import bz2
+import tempfile
 
-from DIRAC import S_OK, rootPath
+from DIRAC import S_OK, S_ERROR, rootPath
 from DIRAC.WorkloadManagementSystem.Agent.SiteDirector import SiteDirector as DIRACSiteDirector
 
 DIRAC_MODULES = [ os.path.join( rootPath, 'DIRAC', 'WorkloadManagementSystem', 'PilotAgent', 'pilotCommands.py' ),
@@ -27,6 +30,92 @@ class SiteDirector( DIRACSiteDirector ):
     self.extraModules = self.am_getOption( 'ExtraPilotModules', [] ) + DIRAC_MODULES
 
     return S_OK()
+
+  # FIXME: fully here ONLY because of removing the setting of X509_CERT_DIR, maybe better extensibility?
+  def _writePilotScript( self, workingDirectory, pilotOptions, proxy = None,
+                         httpProxy = '', pilotExecDir = '' ):
+    """ Bundle together and write out the pilot executable script, admix the proxy if given
+    """
+
+    try:
+      compressedAndEncodedProxy = ''
+      proxyFlag = 'False'
+      if proxy is not None:
+        compressedAndEncodedProxy = base64.encodestring( bz2.compress( proxy.dumpAllToString()['Value'] ) )
+        proxyFlag = 'True'
+      compressedAndEncodedPilot = base64.encodestring( bz2.compress( open( self.pilot, "rb" ).read(), 9 ) )
+      compressedAndEncodedInstall = base64.encodestring( bz2.compress( open( self.install, "rb" ).read(), 9 ) )
+      compressedAndEncodedExtra = {}
+      for module in self.extraModules:
+        moduleName = os.path.basename( module )
+        compressedAndEncodedExtra[moduleName] = base64.encodestring( bz2.compress( open( module, "rb" ).read(), 9 ) )
+    except:
+      self.log.exception( 'Exception during file compression of proxy, dirac-pilot or dirac-install' )
+      return S_ERROR( 'Exception during file compression of proxy, dirac-pilot or dirac-install' )
+
+    # Extra modules
+    mStringList = []
+    for moduleName in compressedAndEncodedExtra:
+      mString = """open( '%s', "w" ).write(bz2.decompress( base64.decodestring( \"\"\"%s\"\"\" ) ) )""" % \
+                ( moduleName, compressedAndEncodedExtra[moduleName] )
+      mStringList.append( mString )
+    extraModuleString = '\n  '.join( mStringList )
+
+    localPilot = """#!/bin/bash
+/usr/bin/env python << EOF
+#
+import os, tempfile, sys, shutil, base64, bz2
+try:
+  pilotExecDir = '%(pilotExecDir)s'
+  if not pilotExecDir:
+    pilotExecDir = None
+  pilotWorkingDirectory = tempfile.mkdtemp( suffix = 'pilot', prefix = 'DIRAC_', dir = pilotExecDir )
+  pilotWorkingDirectory = os.path.realpath( pilotWorkingDirectory )
+  os.chdir( pilotWorkingDirectory )
+  if %(proxyFlag)s:
+    open( 'proxy', "w" ).write(bz2.decompress( base64.decodestring( \"\"\"%(compressedAndEncodedProxy)s\"\"\" ) ) )
+    os.chmod("proxy",0600)
+    os.environ["X509_USER_PROXY"]=os.path.join(pilotWorkingDirectory, 'proxy')
+  open( '%(pilotScript)s', "w" ).write(bz2.decompress( base64.decodestring( \"\"\"%(compressedAndEncodedPilot)s\"\"\" ) ) )
+  open( '%(installScript)s', "w" ).write(bz2.decompress( base64.decodestring( \"\"\"%(compressedAndEncodedInstall)s\"\"\" ) ) )
+  os.chmod("%(pilotScript)s",0700)
+  os.chmod("%(installScript)s",0700)
+  %(extraModuleString)s
+  if "LD_LIBRARY_PATH" not in os.environ:
+    os.environ["LD_LIBRARY_PATH"]=""
+  # TODO: structure the output
+  print '==========================================================='
+  print 'Environment of execution host'
+  for key in os.environ.keys():
+    print key + '=' + os.environ[key]
+  print '==========================================================='
+except Exception, x:
+  print >> sys.stderr, x
+  sys.exit(-1)
+cmd = "python %(pilotScript)s %(pilotOptions)s"
+print 'Executing: ', cmd
+sys.stdout.flush()
+os.system( cmd )
+
+shutil.rmtree( pilotWorkingDirectory )
+
+EOF
+""" % { 'compressedAndEncodedProxy': compressedAndEncodedProxy,
+        'compressedAndEncodedPilot': compressedAndEncodedPilot,
+        'compressedAndEncodedInstall': compressedAndEncodedInstall,
+        'extraModuleString': extraModuleString,
+        'pilotExecDir': pilotExecDir,
+        'pilotScript': os.path.basename( self.pilot ),
+        'installScript': os.path.basename( self.install ),
+        'pilotOptions': ' '.join( pilotOptions ),
+        'proxyFlag': proxyFlag }
+
+    fd, name = tempfile.mkstemp( suffix = '_pilotwrapper.py', prefix = 'DIRAC_', dir = workingDirectory )
+    pilotWrapper = os.fdopen( fd, 'w' )
+    pilotWrapper.write( localPilot )
+    pilotWrapper.close()
+    return name
+
 
   def _getPilotOptions( self, queue, pilotsToSubmit ):
     """ Adding LHCb specific options
