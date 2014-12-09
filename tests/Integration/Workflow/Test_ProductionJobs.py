bf25cf0911665b09dfb84972882a96761953575b
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2014-12-09 10:22:47 +0000

    removed cpue, added events
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@80023 4525493e-7705-40b1-a816-d608a930855b

--- tests/Integration/Workflow/Test_ProductionJobs.py
+++ tests/Integration/Workflow/Test_ProductionJobs.py
@@ -26,49 +26,50 @@ class ProductionJobTestCase( IntegrationTest ):
 class MCSuccess( ProductionJobTestCase ):
   def test_Integration_Production( self ):
 
-    # From request 12789
-    stepsInProd = [{'StepId': 125080, 'StepName': 'Sim08a', 'ApplicationName': 'Gauss', 'ApplicationVersion': 'v45r3',
-                    'ExtraPackages': 'AppConfig.v3r171', 'ProcessingPass': 'Sim08a', 'Visible': 'Y', 'Usable': 'Yes',
-                    'DDDB': 'Sim08-20130503-1', 'CONDDB': 'Sim08-20130503-1-vc-mu100', 'DQTag': '', 'OptionsFormat': '',
-                    'OptionFiles': '$APPCONFIGOPTS/Gauss/Sim08-Beam4000GeV-mu100-2012-nu2.5.py;$DECFILESROOT/options/11102400.py;$LBPYTHIA8ROOT/options/Pythia8.py;$APPCONFIGOPTS/Gauss/G4PL_FTFP_BERT_EmNoCuts.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py',
-                    'isMulticore': 'N', 'SystemConfig': 'x86_64-slc5-gcc43-opt', 'mcTCK': '', 'ExtraOptions': '',
+    # From request 24950
+    stepsInProd = [{'StepId': 126921, 'StepName': 'Sim08f', 'ApplicationName': 'Gauss', 'ApplicationVersion': 'v47r0p1',
+                    'ExtraPackages': 'AppConfig.v3r200;DecFiles.v27r33', 'ProcessingPass': 'Sim08f', 'Visible': 'Y', 'Usable': 'Yes',
+                    'DDDB': 'dddb-20140729', 'CONDDB': 'sim-20140730-vc-mu100', 'DQTag': '', 'OptionsFormat': '',
+                    'OptionFiles': '$APPCONFIGOPTS/Gauss/Beam6500GeV-mu100-nu1.6.py;$APPCONFIGOPTS/Gauss/DataType-2015.py;$APPCONFIGOPTS/Gauss/RICHRandomHits.py;$DECFILESROOT/options/30000000.py;$LBPYTHIA8ROOT/options/Pythia8.py;$APPCONFIGOPTS/Gauss/G4PL_FTFP_BERT_EmNoCuts.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py',
+                    'isMulticore': 'N', 'SystemConfig': 'x86_64-slc6-gcc48-opt', 'mcTCK': '', 'ExtraOptions': '',
                     'fileTypesIn':[],
                     'fileTypesOut':['SIM']},
-                   {'StepId': 124620, 'StepName': 'Digi13', 'ApplicationName': 'Boole', 'ApplicationVersion': 'v26r3',
-                    'ExtraPackages': 'AppConfig.v3r164', 'ProcessingPass': 'Digi13', 'Visible': 'N', 'Usable': 'Yes',
-                    'DDDB': 'Sim08-20130503-1', 'CONDDB': 'Sim08-20130503-1-vc-mu100', 'DQTag': '', 'OptionsFormat': '',
-                    'isMulticore': 'N', 'SystemConfig': 'x86_64-slc5-gcc43-opt', 'mcTCK': '', 'ExtraOptions': '',
+                   {'StepId': 126995, 'StepName': 'Digi13', 'ApplicationName': 'Boole', 'ApplicationVersion': 'v29r1p1',
+                    'ExtraPackages': 'AppConfig.v3r203', 'ProcessingPass': 'Digi13', 'Visible': 'N', 'Usable': 'Yes',
+                    'DDDB': 'dddb-20140729', 'CONDDB': 'sim-20140730-vc-mu100', 'DQTag': '', 'OptionsFormat': '',
+                    'isMulticore': 'N', 'SystemConfig': '', 'mcTCK': '', 'ExtraOptions': '',
                     'OptionFiles': '$APPCONFIGOPTS/Boole/Default.py;$APPCONFIGOPTS/Boole/DataType-2012.py;$APPCONFIGOPTS/Boole/Boole-SiG4EnergyDeposit.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py',
                     'fileTypesIn':['SIM'],
                     'fileTypesOut':['DIGI']},
-                   {'StepId': 124632, 'StepName': 'Trig0x409f0045', 'ApplicationName': 'Moore', 'ApplicationVersion': 'v14r8p1',
-                    'ExtraPackages': 'AppConfig.v3r164', 'ProcessingPass': 'Trig0x40760037Flagged', 'Visible': 'N', 'Usable': 'Yes',
-                    'DDDB': 'Sim08-20130503-1', 'CONDDB': 'Sim08-20130503-1-vc-mu100', 'DQTag': '', 'OptionsFormat': '',
-                    'OptionFiles': '$APPCONFIGOPTS/Moore/MooreSimProductionWithL0Emulation.py;$APPCONFIGOPTS/Conditions/TCK-0x409f0045.py;$APPCONFIGOPTS/Moore/DataType-2012.py;$APPCONFIGOPTS/L0/L0TCK-0x0045.py',
-                    'isMulticore': 'N', 'SystemConfig': 'x86_64-slc5-gcc43-opt', 'mcTCK': '', 'ExtraOptions': '',
+                   {'StepId': 126926, 'StepName': 'L0Trig0x0033', 'ApplicationName': 'Moore', 'ApplicationVersion': 'v22r1p1',
+                    'ExtraPackages': 'AppConfig.v3r200', 'ProcessingPass': 'L0Trig0x0033', 'Visible': 'N', 'Usable': 'Yes',
+                    'DDDB': 'dddb-20140729', 'CONDDB': 'sim-20140730-vc-mu100', 'DQTag': '', 'OptionsFormat': 'l0App',
+                    'isMulticore': 'N', 'SystemConfig': '', 'mcTCK': '', 'ExtraOptions': '',
+                    'OptionFiles': '$APPCONFIGOPTS/L0App/L0AppSimProduction.py;$APPCONFIGOPTS/L0App/L0AppTCK-0x0033.py;$APPCONFIGOPTS/L0App/DataType-2012.py',
                     'fileTypesIn':['DIGI'],
                     'fileTypesOut':['DIGI']},
-                   {'StepId': 124834, 'StepName': 'Reco14a', 'ApplicationName': 'Brunel', 'ApplicationVersion': 'v43r2p7',
-                    'ExtraPackages': 'AppConfig.v3r164', 'ProcessingPass': 'Reco14a', 'Visible': 'N', 'Usable': 'Yes',
-                    'DDDB': 'Sim08-20130503-1', 'CONDDB': 'Sim08-20130503-1-vc-mu100', 'DQTag': '', 'OptionsFormat': '',
-                    'OptionFiles': '$APPCONFIGOPTS/Brunel/DataType-2012.py;$APPCONFIGOPTS/Brunel/MC-WithTruth.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py',
-                    'isMulticore': 'N', 'SystemConfig': 'x86_64-slc5-gcc43-opt', 'mcTCK': '', 'ExtraOptions': '',
+                   {'StepId': 126927, 'StepName': 'Trig0x40b10033', 'ApplicationName': 'Moore', 'ApplicationVersion': 'v22r1p1',
+                    'ExtraPackages': 'AppConfig.v3r201', 'ProcessingPass': 'Trig0x40b10033', 'Visible': 'N', 'Usable': 'Yes',
+                    'DDDB': 'dddb-20140729', 'CONDDB': 'sim-20140730-vc-mu100', 'DQTag': '', 'OptionsFormat': '',
+                    'OptionFiles': '$APPCONFIGOPTS/Moore/MooreSimProductionForSeparateL0AppStep2015.py;$APPCONFIGOPTS/Conditions/TCK-0x40b10033.py;$APPCONFIGOPTS/Moore/DataType-2012.py',
+                    'isMulticore': 'N', 'SystemConfig': '', 'mcTCK': '', 'ExtraOptions': '',
                     'fileTypesIn':['DIGI'],
-                    'fileTypesOut':['DST']},
-                   {'StepId': 124630, 'StepName': 'Stripping20NoPrescalingFlagged', 'ApplicationName': 'DaVinci', 'ApplicationVersion': 'v32r2p1',
-                    'ExtraPackages': 'AppConfig.v3r164', 'ProcessingPass': 'Stripping20NoPrescalingFlagged', 'Visible': 'N', 'Usable': 'Yes',
-                    'DDDB': 'Sim08-20130503-1', 'CONDDB': 'Sim08-20130503-1-vc-mu100', 'DQTag': '', 'OptionsFormat': '',
-                    'OptionFiles': '$APPCONFIGOPTS/DaVinci/DV-Stripping20-Stripping-MC-NoPrescaling.py;$APPCONFIGOPTS/DaVinci/DataType-2012.py;$APPCONFIGOPTS/DaVinci/InputType-DST.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py',
-                    'isMulticore': 'N', 'SystemConfig': 'x86_64-slc5-gcc43-opt', 'mcTCK': '', 'ExtraOptions': '',
-                    'fileTypesIn':['DST'],
-                    'fileTypesOut':['ALLSTREAMS.DST']},
+                    'fileTypesOut':['DIGI']},
+                   {'StepId': 127060, 'StepName': 'Reco15DEV', 'ApplicationName': 'Brunel', 'ApplicationVersion': 'v47r2p1',
+                    'ExtraPackages': 'AppConfig.v3r203', 'ProcessingPass': 'Reco15DEV', 'Visible': 'N', 'Usable': 'Yes',
+                    'DDDB': 'dddb-20140729', 'CONDDB': 'sim-20140730-vc-mu100', 'DQTag': '', 'OptionsFormat': '',
+                    'OptionFiles': '$APPCONFIGOPTS/Brunel/DataType-2015.py;$APPCONFIGOPTS/Brunel/MC-WithTruth.py;$APPCONFIGOPTS/Brunel/ldst.py;$APPCONFIGOPTS/Brunel/patchUpgrade1.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py',
+                    'isMulticore': 'N', 'SystemConfig': '', 'mcTCK': '', 'ExtraOptions': '',
+                    'fileTypesIn':['DIGI'],
+                    'fileTypesOut':['LDST']}
                    ]
 
     # First create the production object
-    prod = self.pr._buildProduction( 'MCSimulation', stepsInProd, {'ALLSTREAMS.DST': 'Tier1_MC-DST'}, 0, 100,
-                                     outputFileMask = 'ALLSTREAMS.DST', cpue = 1300 )
+    prod = self.pr._buildProduction( prodType = 'MCSimulation', stepsInProd = stepsInProd, outputSE = {'LDST': 'Tier1_MC-DST'},
+                                     priority = 0, cpu = 100, outputFileMask = 'LDST' )
     prod.LHCbJob.setInputSandbox( find_all( 'pilot.cfg', '.' )[0] )
     prod.LHCbJob.setConfigArgs( 'pilot.cfg' )
+    prod.setParameter( 'numberOfEvents', 'string', 2, 'Number of events to test' )
     # Then launch it
     res = self.diracProduction.launchProduction( prod, False, True, 0 )
 
@@ -120,9 +121,10 @@ class MCSuccessMultiCore( ProductionJobTestCase ):
 
     # First create the production object
     prod = self.pr._buildProduction( 'MCSimulation', stepsInProd, {'ALLSTREAMS.DST': 'Tier1_MC-DST'}, 0, 100,
-                                     outputFileMask = 'ALLSTREAMS.DST', cpue = 1300 )
+                                     outputFileMask = 'ALLSTREAMS.DST' )
     prod.LHCbJob.setInputSandbox( find_all( 'pilot.cfg', '.' )[0] )
     prod.LHCbJob.setConfigArgs( 'pilot.cfg' )
+    prod.setParameter( 'numberOfEvents', 'string', 6, 'Number of events to test' )
     # Then launch it
     res = self.diracProduction.launchProduction( prod, False, True, 0 )
 
@@ -160,9 +162,10 @@ class MCSuccess_new( ProductionJobTestCase ):
 
     # First create the production object
     prod = self.pr._buildProduction( 'MCSimulation', stepsInProd, {'DIGI':'Tier1_MC-DST'}, 0, 100,
-                                     outputFileStep = '3', cpue = 1300 )
+                                     outputFileStep = '3' )
     prod.LHCbJob.setInputSandbox( find_all( 'pilot.cfg', '.' )[0] )
     prod.LHCbJob.setConfigArgs( 'pilot.cfg' )
+    prod.setParameter( 'numberOfEvents', 'string', 2, 'Number of events to test' )
     # Then launch it
     res = self.diracProduction.launchProduction( prod, False, True, 0 )
 
