74ae940e533d4ca15b1e25eb3928b5bc86b7bda4
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2015-01-09 15:35:38 +0000

    Moved old functions
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@80549 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci.sh
+++ tests/Jenkins/lhcb_ci.sh
@@ -223,62 +223,6 @@ function diracInstall(){
 }
 
 
-#.............................................................................
-#
-# diracConfigure:
-# 
-#   in short, it writes dirac.cfg file. More on detail, it sets: 
-#
-#   o /LocalSite/Architecture
-#   o /LocalInstallation/Database/RootPwd
-#   o /LocalInstallation/Database/Password
-#   o /LocalInstallation/HostDN
-#   o /LocalInstallation/Host
-#
-#.............................................................................
-
-function diracConfigure(){
-	echo '[diracConfigure]'
-
-	cd $WORKSPACE
-
-	# Find architecture platform
-	arch=`dirac-architecture`
-
-	# Randomly generated passwords
-	# DB root
-	randomRoot=`tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1`
-	rootPass=/LocalInstallation/Database/RootPwd=$randomRoot
-	# DB user
-	randomUser=`tr -cd '[:alnum:]' < /dev/urandom | fold -w20 | head -n1`
-	userPass=/LocalInstallation/Database/Password=$randomUser
-
-	# Stores passwords on files for latter usage
-	echo $randomRoot > rootMySQL
-	echo $randomUser > userMySQL
-
-	# Sets all systems on Jenkins setup
-	setups=`cat systems | sed 's/System//' | sed 's/^/-o \/DIRAC\/Setups\/Jenkins\//' | sed 's/$/=Jenkins/'` 
-
-	cp $LHCb_CI_CONFIG/install.cfg etc/install.cfg
-
-	# Set HostDN in install.cfg
-	# We use colons instead of forward slashes in sed, otherwise we cannot scape
-	# the '/' characters in the DN
-	hostdn=`openssl x509 -noout -in etc/grid-security/hostcert.pem -subject | sed 's/subject= //g'` 
-	sed -i "s:#hostdn#:$hostdn:g" etc/install.cfg
-
-	# Set FQDN in install.cfg
-	fqdn=`hostname --fqdn`
-	sed -i "s/#hostname#/$fqdn/g" etc/install.cfg
-
-	dirac-configure etc/install.cfg -A $arch -o $rootPass -o $userPass $setups $DEBUG
-	dirac-setup-site $DEBUG
-
-}  
-  
-  
-  
 
   #.............................................................................
   #
@@ -419,34 +363,6 @@ function downloadProxy(){
 }
 
 
-  #.............................................................................
-  #
-  # diracMySQL:
-  #
-  #   installs MySQL. If it was running before, kills it before proceeding with
-  #   the installation. It seems the installation procedure is not complete,
-  #   which means we have to introduce a little hack on mysql.server file such
-  #   that the basedir points to our Linux external binaries.
-  #
-  #.............................................................................
-
-  function diracMySQL(){
-	echo '[diracMySQL]'
-
-    # Kills MySQL daemon if running
-    killMySQL
-    
-    # Hacks a bit mysql.server file
-    linuxDir=`ls $WORKSPACE | grep Linux`
-    basedir=$WORKSPACE/$linuxDir
-    sed -i "s:basedir=:basedir=$basedir:g" $WORKSPACE/mysql/share/mysql/mysql.server
-  
-    # Install MySQL using DIRAC scripts
-    dirac-install-mysql $DEBUG
-  
-  }
-
-
 #-------------------------------------------------------------------------------
 # Kill, Stop and Start scripts. Used to clean environment.
 #-------------------------------------------------------------------------------
@@ -542,70 +458,6 @@ function downloadProxy(){
     runsvstat $WORKSPACE/startup/*
    
   }
-
-  #.............................................................................
-  #
-  # killMySQL:
-  #
-  #   if MySQL is running, it stops it.
-  #
-  #.............................................................................
-
-  function killMySQL(){
-    echo '[killMySQL]'
-
-    # Bear in mind that we run with 'errexit' mode. This call, if finds nothing
-    # will return an error, which will make the whole script exit. However, if 
-    # finds nothing we are good, it means there are not leftover processes from
-    # other runs. So, we disable 'errexit' mode for this call.
-    
-    set +o errexit
-    mysql=`ps aux | grep mysql | grep -v grep`
-    set -o errexit
-   
-    if [ ! -z "$mysql" ]
-    then
-      killall mysqld
-    fi   
-   
-  }  
-
-  #.............................................................................
-  #
-  # stopMySQL:
-  #
-  #   if MySQL is running, it stops it.
-  #
-  #.............................................................................
-
-  function stopMySQL(){
-    echo '[stopMySQL]'
-
-    # Let's try to be a bit more delicated than the function above
-
-    $WORKSPACE/mysql/share/mysql/mysql.server stop
-    
-    # If does not work, we kill it
-    killMySQL
-        
-  }
-
-
-  #.............................................................................
-  #
-  # startMySQL:
-  #
-  #   if MySQL is not running, it starts it.
-  #
-  #.............................................................................
-
-  function startMySQL(){
-    echo '[startMySQL]'
-
-    $WORKSPACE/mysql/share/mysql/mysql.server start
-        
-  }
-
 
 #-------------------------------------------------------------------------------
 # finalCleanup:
@@ -885,8 +737,7 @@ function submitAndMatch(){
 	#Here, since I have CVMFS only, I can't use the "latest" pre-release, because won't be on CVMFS 
 	#FIXME!
 	SetupProject LHCbDIRAC v0r94
-	downloadProxy
-	
+	downloadProxy	
 	#submit the job: this job will go to the certification setup, so we suppose the JobManager there is accepting jobs
 	python $WORKSPACE/LHCbTestDirac/Jenkins/dirac-test-job.py
 
@@ -897,134 +748,6 @@ function submitAndMatch(){
 	#try running the job agent. The job should be matched and everything should be "ok"
 	dirac-agent WorkloadManagement/JobAgent -o MaxCycles=1 -s /Resources/Computing/CEDefaults -o WorkingDirectory=$PWD -o TotalCPUs=1 -o MaxCPUTime=47520 -o CPUTime=47520 -o MaxRunningJobs=1 -o MaxTotalJobs=10 -o /LocalSite/InstancePath=$PWD -o /AgentJobRequirements/ExtraOptions=pilot.cfg pilot.cfg $DEBUG
 }
-
-
-# Older functions
-#
-# o prepareDIRAC
-# o prepareTestExternals
-# o runTest
-# o mergeTests
-
-
-#...............................................................................
-#
-# prepareDIRAC:
-#
-#   installs DIRAC, MySQL, (DIRAC) Externals, creates credentials, etc...
-#   IT is THE function. 
-#
-#...............................................................................
-
-function prepareDIRAC(){
-  
-	if [ ! -z "$DEBUG" ]
-	then
-		echo 'Running in DEBUG mode'
-		export DEBUG='-ddd'
-	fi  
-  
-	killRunsv
-  
-	findRelease
-	diracInstall
-	generateCertificates
-  
-	findSystems
-	findDatabases
-
-	source $WORKSPACE/bashrc
-
-	diracConfigure
-  
-	generateUserCredentials
-	diracCredentials
-
-	diracMySQL
-  
-	dirac-install-db ProxyDB $DEBUG
-	dirac-install-service Framework/ProxyManager $DEBUG
-	ln -s $WORKSPACE/runit/Framework/ProxyManager $WORKSPACE/startup/Framework_ProxyManager
-  
-	# Give runit 10 secs to pick it up
-	sleep 10
-	diracProxies
-}
-
-
-#...............................................................................
-#
-# prepareTestExternals:
-#
-#   installs all externals we need to run the test:
-#   o nose 
-#
-#...............................................................................
-
-
-function prepareTestExternals(){
-
-	source $WORKSPACE/bashrc
-	python `which easy_install` nose
-
-}
-
-#...............................................................................
-#
-# runTest:
-#
-#   given a TEST_MODE ( configure, install ) runs all tests tagged with the 
-#   TEST_MODE and stores their results together.  
-#
-#...............................................................................
-
-function runTest(){
-
-  set +o errexit
-
-  if [ -z "$TEST_MODE" ]
-  then
-    exit "TEST_MODE not found"
-  fi
-  
-  mkdir -p $WORKSPACE/lhcb_ci
-
-  source $WORKSPACE/bashrc
-
-  export PYTHONPATH=$PYTHONPATH:$WORKSPACE/LHCbTestDirac/Jenkins
-  export LHCB_CI_DEBUG=$WORKSPACE/lhcb_ci/${TEST_MODE}.log
-
-  echo "########################################################"
-  echo "$TEST_MODE TESTS"
-  echo "log file: $LHCB_CI_DEBUG"
-  echo "########################################################"
-
-  nosetests -a $TEST_MODE --with-xunit LHCbTestDirac/Jenkins/lhcb_ci/test -v --xunit-file=nosetests_${TEST_MODE}.xml --with-coverage --cover-package=DIRAC,LHCbDIRAC
-  mv .coverage .coverage.${TEST_MODE}
-
-  set -o errexit
-
-}
-
-
-#...............................................................................
-#
-# mergeTests:
-#
-#   merges the individual results of all the tests run. 
-#     
-#
-#...............................................................................
-
-function mergeTests(){
-
-  source $WORKSPACE/bashrc
-
-  coverage combine
-  coverage xml --include="*DIRAC/*"
-
-}
-
 
 
 #-------------------------------------------------------------------------------
