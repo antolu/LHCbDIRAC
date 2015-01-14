a630493868d63ea37e641d18e800d11be419072c
fstagni <fstagni@4525493e-7705-40b1-a816-d608a930855b>
2015-01-14 11:13:39 +0000

    possibly passing a release to install for the pilot
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbTestDirac/trunk/LHCbTestDirac@80662 4525493e-7705-40b1-a816-d608a930855b

--- tests/Jenkins/lhcb_ci.sh
+++ tests/Jenkins/lhcb_ci.sh
@@ -164,8 +164,18 @@ function LHCbDIRACPilotInstall(){
 	
 	wget --no-check-certificate -O LHCbPilotCommands.py $LHCbDIRAC_PILOT_COMMANDS
 
+	if [ ! -z "$LHCBDIRAC_RELEASE" ]
+	then
+		echo 'Specified release'
+		echo $LHCBDIRAC_RELEASE
+		installVersion='-r'
+		installVersion+=" $LHCBDIRAC_RELEASE"
+	else
+		installVersion=''
+	fi
+
 	#run the dirac-pilot script, only for installing, do not run the JobAgent here
-	python dirac-pilot.py -S LHCb-Certification -l LHCb -C dips://lbvobox18.cern.ch:9135/Configuration/Server -N jenkins.cern.ch -Q jenkins-queue_not_important -n DIRAC.Jenkins.ch --cert --certLocation=/home/dirac/certs/ -E LHCbPilot -X LHCbGetPilotVersion,CheckWorkerNode,LHCbInstallDIRAC,LHCbConfigureBasics,LHCbConfigureSite,LHCbConfigureArchitecture,LHCbConfigureCPURequirements $DEBUG
+	python dirac-pilot.py -S LHCb-Certification -l LHCb $installVersion -C dips://lbvobox18.cern.ch:9135/Configuration/Server -N jenkins.cern.ch -Q jenkins-queue_not_important -n DIRAC.Jenkins.ch --cert --certLocation=/home/dirac/certs/ -E LHCbPilot -X LHCbGetPilotVersion,CheckWorkerNode,LHCbInstallDIRAC,LHCbConfigureBasics,LHCbConfigureSite,LHCbConfigureArchitecture,LHCbConfigureCPURequirements $DEBUG
 }
 
 
