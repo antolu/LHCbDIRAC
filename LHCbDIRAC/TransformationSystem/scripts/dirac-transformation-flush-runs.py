377e7fd40ff10ec878cf6f0a868e6ca093c6b292
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2015-02-10 12:49:05 +0000

    New option --Runs to give explicitly the list of runs to flush
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@81345 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/TransformationSystem/scripts/dirac-transformation-flush-runs.py
+++ LHCbDIRAC/TransformationSystem/scripts/dirac-transformation-flush-runs.py
@@ -1,26 +1,42 @@
 #! /usr/bin/env python
 """
-   In a transformation, flush runs that are flushed in the transformation used in BKQuery
+   In a transformation, flush a list of runs or runs that are flushed in the transformation used in BKQuery
 """
 
 import DIRAC
+from DIRAC import gLogger
 from DIRAC.Core.Base import Script
 
 if __name__ == "__main__":
+  Script.registerSwitch( '', 'Runs=', '   list of runs to flush (comma separated, ranges r1:r2)' )
   Script.registerSwitch( '', 'NoAction', '   No action taken, just give stats' )
-  Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
+  Script.setUsageMessage( '\n'.join( [ __doc__,
                                        'Usage:',
                                        '  %s [option|cfgfile] <TransID>[,<TransID2>...]' % Script.scriptName, ] ) )
   Script.parseCommandLine()
 
   args = Script.getPositionalArgs()
   action = True
+  runList = []
   for switch, val in Script.getUnprocessedSwitches():
     if switch == 'NoAction':
       action = False
+    elif switch == 'Runs':
+      try:
+        runs = val.split( ',' )
+        if len( runs ) > 1:
+          for run in runs:
+            if run.isdigit():
+              runList.append( int( run ) )
+            else:
+              runRange = run.split( ':' )
+              if len( runRange ) == 2 and runRange[0].isdigit() and runRange[1].isdigit():
+                runList += range( int( runRange[0] ), int( runRange[1] ) + 1 )
+      except Exception, x:
+        gLogger.exception( 'Bad run parameter', lException = x )
 
   if len( args ) != 1:
-    print "Specify transformation number..."
+    gLogger.fatal( "Specify transformation number..." )
     Script.showHelp()
     DIRAC.exit( 0 )
   else:
@@ -38,51 +54,55 @@ if __name__ == "__main__":
   transClient = TransformationClient()
 
   for transID in idList:
-    res = transClient.getBookkeepingQuery( transID )
-    if not res['OK']:
-      print "Error getting BK query for transformation %s" % transID, res['Message']
-      DIRAC.exit( 1 )
-    queryProd = res['Value'].get( 'ProductionID' )
-    if not queryProd:
-      print "Transformation is not based on another one"
-      DIRAC.exit( 0 )
-
     res = transClient.getTransformationRuns( {'TransformationID':transID} )
     if not res['OK']:
-      print "Error getting runs for transformation %s" % transID, res['Message']
+      gLogger.fatal( "Error getting runs for transformation %s" % transID, res['Message'] )
       DIRAC.exit( 1 )
     runs = res['Value']
     runs.sort( cmp = ( lambda r1, r2: int( r1['RunNumber'] - r2['RunNumber'] ) ) )
 
-    res = transClient.getTransformationRuns( {'TransformationID':queryProd} )
-    if not res['OK']:
-      print "Error getting runs for transformation %s" % queryProd, res['Message']
-      DIRAC.exit( 1 )
-    queryRuns = res['Value']
-    flushedRuns = [run['RunNumber'] for run in queryRuns if run['Status'] == 'Flush']
-
-    toBeFlushed = []
-    for run in [run['RunNumber'] for run in runs if run['Status'] != 'Flush' and run['RunNumber'] in flushedRuns]:
-      missing = -1
-      res = transClient.getTransformationFiles( {'TransformationID': queryProd, 'RunNumber': run} )
+    if not runList:
+      res = transClient.getBookkeepingQuery( transID )
       if not res['OK']:
-        print "Error getting files for run", run, res['Message']
-      else:
-        runFiles = res['Value']
-        missing = 0
-        for rFile in runFiles:
-          if rFile['Status'] in ( 'Unused', 'Assigned', 'MaxReset' ):
-            missing += 1
-        if not missing:
-          toBeFlushed.append( run )
+        gLogger.fatal( "Error getting BK query for transformation %s" % transID, res['Message'] )
+        DIRAC.exit( 1 )
+      queryProd = res['Value'].get( 'ProductionID' )
+      if not queryProd:
+        gLogger.fatal( "Transformation is not based on another one" )
+        DIRAC.exit( 0 )
+
+      res = transClient.getTransformationRuns( {'TransformationID':queryProd} )
+      if not res['OK']:
+        gLogger.fatal( "Error getting runs for transformation %s" % queryProd, res['Message'] )
+        DIRAC.exit( 1 )
+      queryRuns = res['Value']
+      flushedRuns = [run['RunNumber'] for run in queryRuns if run['Status'] == 'Flush']
+
+      toBeFlushed = []
+      for run in [run['RunNumber'] for run in runs if run['Status'] != 'Flush' and run['RunNumber'] in flushedRuns]:
+        missing = -1
+        res = transClient.getTransformationFiles( {'TransformationID': queryProd, 'RunNumber': run} )
+        if not res['OK']:
+          gLogger.fatal( "Error getting files for run %d" % run, res['Message'] )
+        else:
+          runFiles = res['Value']
+          missing = 0
+          for rFile in runFiles:
+            if rFile['Status'] in ( 'Unused', 'Assigned', 'MaxReset' ):
+              missing += 1
+          if not missing:
+            toBeFlushed.append( run )
+
+    else:
+      toBeFlushed = sorted( set( runList ) & set( [run['RunNumber'] for run in runs] ) )
 
     ok = 0
-    print "Runs %s flushed in transformation %s:" % ( 'being' if action else 'to be', transID ), toBeFlushed
+    gLogger.always( "Runs %s flushed in transformation %s:" % ( 'being' if action else 'to be', transID ), ','.join( [str( run ) for run in toBeFlushed] ) )
     for run in toBeFlushed:
       res = transClient.setTransformationRunStatus( transID, run, 'Flush' ) if action else {'OK':True}
       if not res['OK']:
-        print "Error setting run %s to Flush in transformation %s" % ( run, transID )
+        gLogger.fatal( "Error setting run %s to Flush in transformation %s" % ( run, transID ), res['Message'] )
       else:
         ok += 1
-    print ok, 'runs set to Flush in transformation', transID
+    gLogger.always( '%d runs set to Flush in transformation %d' % ( ok, transID ) )
 
