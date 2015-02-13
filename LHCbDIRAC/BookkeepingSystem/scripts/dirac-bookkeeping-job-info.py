55c57bbed660b589ae0dd34d5e2fc318942fc27e
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2015-02-13 08:42:09 +0000

    CHANGE: add options to print a summary of jobs
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@81392 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-job-info.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-job-info.py
@@ -12,11 +12,13 @@ __RCSID__ = "$Id:  $"
 from DIRAC.Core.Base import Script
 from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript, printDMResult
 import DIRAC
+from DIRAC import gLogger
 
 if __name__ == "__main__":
 
   bkScript = DMScript()
   bkScript.registerFileSwitches()
+  Script.registerSwitch( '', 'Summary', '   Only report job IDs' )
   Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                        'Usage:',
                                        '  %s [option|cfgfile] ... [LFN|File]' % Script.scriptName,
@@ -34,7 +36,35 @@ if __name__ == "__main__":
   if not lfnList:
     Script.showHelp()
     DIRAC.exit( 0 )
+  summary = False
+  for switch in Script.getUnprocessedSwitches():
+    if switch[0] == 'Summary':
+      summary = True
 
   from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient import BookkeepingClient
   retVal = BookkeepingClient().bulkJobInfo( lfnList )
+  if retVal['OK']:
+    success = retVal['Value']['Successful']
+    group = {}
+    jobs = {}
+    for lfn in success:
+      if type( success[lfn] ) == type( [] ) and len( success[lfn] ) == 1:
+        success[lfn] = success[lfn][0]
+      job = success[lfn]
+      jobID = 'Job %d' % job['DIRACJobId']
+      group.setdefault( jobID, [] ).append( lfn )
+      jobs.setdefault( jobID, job )
+    if summary:
+      retVal['Value']['Successful'] = group
+    else:
+      for jobID in group:
+        lfns = ','.join( group[jobID] )
+        for lfn in group[jobID]:
+          del success[lfn]
+        success[lfns] = jobs[jobID]
+
   printDMResult( retVal, empty = "File does not exists in the Bookkeeping" )
+
+  if summary and retVal['OK']:
+    gLogger.always( 'List of DIRAC jobs' )
+    gLogger.always( ','.join( sorted( [job.replace( 'Job ', '' ) for job in group] ) ) )
