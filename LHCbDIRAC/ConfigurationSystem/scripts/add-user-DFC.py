9e86b4dc30563a3896e2d075bf605f316403bc6b
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2015-04-24 08:12:54 +0000

    NEW: add --Recursive option
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@82257 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ConfigurationSystem/scripts/add-user-DFC.py
+++ LHCbDIRAC/ConfigurationSystem/scripts/add-user-DFC.py
@@ -5,17 +5,31 @@ from DIRAC import exit, gLogger
 import os, sys
 
 Script.registerSwitch( '', 'User=', '  User name' )
+Script.registerSwitch( '', 'Recursive', '  Set ownership recursively' )
 
 Script.parseCommandLine()
 
 user = None
+recursive = False
 for switch in Script.getUnprocessedSwitches():
   if switch[0] == 'User':
     user = switch[1].lower()
+  elif switch[0] == 'Recursive':
+    recursive = True
 
 if not user:
   Script.showHelp()
   exit( 1 )
+
+from DIRAC.Core.Security.ProxyInfo import getProxyInfo
+res = getProxyInfo()
+if not res['OK']:
+  gLogger.fatal( "Can't get proxy info", res['Message'] )
+  exit( 1 )
+properties = res['Value'].get( 'groupProperties', [] )
+if not 'FileCatalogManagement' in properties:
+  gLogger.error( "You need to use a proxy from a group with FileCatalogManagement" )
+  exit( 5 )
 
 from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
 exists = False
@@ -30,6 +44,9 @@ if dfc.isDirectory( baseDir ).get( 'Value', {} ).get( 'Successful', {} ).get( ba
     success = res['Value']['Successful'][baseDir]
     if success.get( 'SubDirs' ) or success.get( 'Files' ):
       gLogger.error( 'Directory is not empty:', ' %d files / %d subdirectories' % ( len( success['Files'] ), len( success['SubDirs'] ) ) )
+    elif recursive:
+      gLogger.always( "Empty directory, recursive is useless..." )
+      recursive = False
 if not exists:
   gLogger.always( 'Creating directory', baseDir )
   res = dfc.createDirectory( baseDir )
@@ -38,11 +55,11 @@ if not exists:
     exit( 2 )
 else:
   gLogger.always( 'Change ownership of directory', baseDir )
-res = dfc.changePathOwner( {baseDir: { 'Owner' : user}}, False )
+res = dfc.changePathOwner( {baseDir: { 'Owner' : user}}, recursive )
 if not res['OK']:
   gLogger.fatal( 'Error changing directory owner', res['Message'] )
   exit( 2 )
-res = dfc.changePathGroup( {baseDir: { 'Group' : 'lhcb_user'}}, False )
+res = dfc.changePathGroup( {baseDir: { 'Group' : 'lhcb_user'}}, recursive )
 if not res['OK']:
   gLogger.fatal( 'Error changing directory group', res['Message'] )
   exit( 2 )
