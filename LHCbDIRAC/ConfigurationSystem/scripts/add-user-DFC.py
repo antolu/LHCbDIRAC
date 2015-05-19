d16ac7469d9479246fe0acbac1d2169dff1110dd
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2015-05-19 15:43:08 +0000

    Adapt interface for changePathXxxx
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@82850 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/ConfigurationSystem/scripts/add-user-DFC.py
+++ LHCbDIRAC/ConfigurationSystem/scripts/add-user-DFC.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python
 
 from DIRAC.Core.Base import Script
-from DIRAC import exit, gLogger
+from DIRAC import exit, gLogger, S_OK
 import os, sys
+from time import time
+
+def chown( directories, user, recursive = False, ndirs = None ):
+  if isinstance( directories, basestring ):
+    directories = [directories]
+  if ndirs is None:
+    ndirs = 0
+  res = dfc.changePathOwner( dict.fromkeys( directories, user ) )
+  if not res['OK']:
+    return res
+  res = dfc.changePathGroup( dict.fromkeys( directories, 'lhcb_user' ) )
+  if not res['OK']:
+    return res
+  if recursive:
+    for subDir in directories:
+      if ndirs % 10 == 0:
+        sys.stdout.write( '.' )
+        sys.stdout.flush()
+      ndirs += 1
+      res = dfc.listDirectory( subDir )
+      if res['OK']:
+        subDirectories = res['Value']['Successful'][subDir]['SubDirs']
+        if subDirectories:
+          # print subDir, len( subDirectories ), ndirs
+          res = chown( subDirectories, user, recursive = True, ndirs = ndirs )
+          if not res['OK']:
+            return res
+          ndirs = res['Value']
+  return S_OK( ndirs )
+
 
 Script.registerSwitch( '', 'User=', '  User name' )
 Script.registerSwitch( '', 'Recursive', '  Set ownership recursively' )
@@ -36,14 +66,16 @@ exists = False
 initial = user[0]
 dfc = FileCatalogClient()
 baseDir = os.path.join( '/lhcb', 'user', initial, user )
+subDirectories = []
 if dfc.isDirectory( baseDir ).get( 'Value', {} ).get( 'Successful', {} ).get( baseDir ):
   gLogger.always( 'Directory already existing', baseDir )
   exists = True
   res = dfc.listDirectory( baseDir )
   if res['OK']:
     success = res['Value']['Successful'][baseDir]
+    subDirectories = success['SubDirs']
     if success.get( 'SubDirs' ) or success.get( 'Files' ):
-      gLogger.error( 'Directory is not empty:', ' %d files / %d subdirectories' % ( len( success['Files'] ), len( success['SubDirs'] ) ) )
+      gLogger.always( 'Directory is not empty:', ' %d files / %d subdirectories' % ( len( success['Files'] ), len( success['SubDirs'] ) ) )
     elif recursive:
       gLogger.always( "Empty directory, recursive is useless..." )
       recursive = False
@@ -54,15 +86,19 @@ if not exists:
     gLogger.fatal( 'Error creating directory', res['Message'] )
     exit( 2 )
 else:
-  gLogger.always( 'Change ownership of directory', baseDir )
-res = dfc.changePathOwner( {baseDir: { 'Owner' : user}}, recursive )
+  gLogger.always( 'Change%s ownership of directory' % ' recursively' if recursive else '', baseDir )
+res = chown( baseDir, user, recursive = False )
 if not res['OK']:
   gLogger.fatal( 'Error changing directory owner', res['Message'] )
   exit( 2 )
-res = dfc.changePathGroup( {baseDir: { 'Group' : 'lhcb_user'}}, recursive )
-if not res['OK']:
-  gLogger.fatal( 'Error changing directory group', res['Message'] )
-  exit( 2 )
+if recursive:
+  startTime = time()
+  res = chown( subDirectories, user, recursive = True, ndirs = 1 )
+  if not res['OK']:
+    gLogger.fatal( 'Error changing directory owner', res['Message'] )
+    exit( 2 )
+  gLogger.always( 'Successfully changed owner in %d directories in %.1f seconds' % ( res['Value'], time() - startTime ) )
+
 
 from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
 from LHCbDIRAC.DataManagementSystem.Client.ScriptExecutors import executeLfnMetadata
