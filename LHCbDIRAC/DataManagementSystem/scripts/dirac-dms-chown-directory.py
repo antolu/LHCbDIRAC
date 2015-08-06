efe17f2d725ec7efb94d99b1ee148f587fcfc6c3
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2015-08-06 13:12:39 +0000

    Allow a list of directories
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@84633 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-chown-directory.py
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-chown-directory.py
@@ -17,7 +17,7 @@ Script.parseCommandLine()
 user = None
 recursive = False
 group = None
-baseDir = None
+dirList = None
 mode = None
 create = False
 for switch in Script.getUnprocessedSwitches():
@@ -26,9 +26,7 @@ for switch in Script.getUnprocessedSwitches():
   elif switch[0] == 'Recursive':
     recursive = True
   elif switch[0] == 'Directory':
-    baseDir = switch[1]
-    if baseDir.endswith( '/' ):
-      baseDir = baseDir[:-1]
+    dirList = switch[1].split( ',' )
   elif switch[0] == 'Group':
     group = switch[1]
   elif switch[0] == 'Mode':
@@ -37,8 +35,8 @@ for switch in Script.getUnprocessedSwitches():
     create = True
 
 args = Script.getPositionalArgs()
-if baseDir is None and args:
-  baseDir = args[0]
+if dirList is None and args:
+  dirList = args
 
 from DIRAC.Core.Security.ProxyInfo import getProxyInfo
 from LHCbDIRAC.DataManagementSystem.Utilities.FCUtilities import chown
@@ -52,39 +50,71 @@ if not 'FileCatalogManagement' in properties:
   gLogger.error( "You need to use a proxy from a group with FileCatalogManagement" )
   exit( 5 )
 
-from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
-dfc = FileCatalogClient()
-if baseDir is None or ( user is None and group is None and mode is None ):
-  Script.showHelp()
-  exit( 1 )
-
-if not baseDir.startswith( '/lhcb' ):
-  gLogger.fatal( "Not a valid directory", baseDir )
-  exit ( 1 )
 if group and not group.startswith( 'lhcb_' ):
   gLogger.fatal( "This is not a valid group", group )
   exit( 1 )
-if not dfc.isDirectory( baseDir ).get( 'Value', {} ).get( 'Successful', {} ).get( baseDir ):
-  if create:
-    res = dfc.createDirectory( baseDir )
-    if not res['OK']:
-      gLogger.fatal( "Error creating directory", res['Message'] )
-      exit( 2 )
-  else:
-    gLogger.fatal( "Directory doesn't exist", baseDir )
-    exit( 1 )
-startTime = time()
-res = chown( baseDir, user, group = group, mode = mode, recursive = recursive, fcClient = dfc )
-if not res['OK']:
-  gLogger.fatal( 'Error changing directory owner', res['Message'] )
-  exit( 2 )
-gLogger.always( 'Successfully changed owner in %d directories in %.1f seconds' % ( res['Value'], time() - startTime ) )
 
-from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
-from LHCbDIRAC.DataManagementSystem.Client.ScriptExecutors import executeLfnMetadata
-dmScript = DMScript()
-dmScript.setLFNs( baseDir )
-sys.stdout.write( 'Directory metadata: ' )
-sys.stdout.flush()
-executeLfnMetadata( dmScript )
-exit( 0 )
+directories = set()
+for baseDir in dirList:
+  if os.path.exists( baseDir ):
+    f = open( baseDir )
+    directories.update( [os.path.dirname( line.split()[0] + '/' ) for line in f.read().splitlines()] )
+    f.close()
+  else:
+    directories.add( baseDir if not baseDir.endswith( '/' ) else baseDir[:-1] )
+
+if not directories or ( user is None and group is None and mode is None ):
+  Script.showHelp()
+  exit( 1 )
+
+from DIRAC.Resources.Catalog.FileCatalogClient import FileCatalogClient
+dfc = FileCatalogClient()
+
+error = 0
+success = 0
+startTime = time()
+if len( directories ) > 1:
+  sys.stdout.write( 'Changing ownership to %d directories :' % len( directories ) )
+  sys.stdout.flush()
+count = 0
+for baseDir in directories:
+  if not baseDir.startswith( '/lhcb' ):
+    gLogger.fatal( "\nNot a valid directory", baseDir )
+    error = 1
+    continue
+  if not dfc.isDirectory( baseDir ).get( 'Value', {} ).get( 'Successful', {} ).get( baseDir ):
+    if create:
+      res = dfc.createDirectory( baseDir )
+      if not res['OK']:
+        gLogger.fatal( "\nError creating directory", res['Message'] )
+        error = 1
+        continue
+    else:
+      gLogger.fatal( "\nDirectory doesn't exist", baseDir )
+      error = 1
+      continue
+  res = chown( baseDir, user, group = group, mode = mode, recursive = recursive, fcClient = dfc )
+  if not res['OK']:
+    gLogger.fatal( '\nError changing directory owner', res['Message'] )
+    error = 1
+    continue
+
+  if len( directories ) == 1:
+    gLogger.always( 'Successfully changed owner in %d directories in %.1f seconds' % ( res['Value'], time() - startTime ) )
+    from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
+    from LHCbDIRAC.DataManagementSystem.Client.ScriptExecutors import executeLfnMetadata
+    dmScript = DMScript()
+    dmScript.setLFNs( baseDir )
+    sys.stdout.write( 'Directory metadata: ' )
+    sys.stdout.flush()
+    executeLfnMetadata( dmScript )
+  elif count % 10 == 0:
+    sys.stdout.write( '.' )
+    sys.stdout.flush()
+  count += 1
+  success += res['Value']
+  error = 0
+
+if len( directories ) > 1:
+  gLogger.notice( '\nSuccessfully changed owner in %d directories in %.1f seconds' % ( success, time() - startTime ) )
+exit( error )
