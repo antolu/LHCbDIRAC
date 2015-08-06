901097042303aee5fbb1e4442016bb056ecaf1ca
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2015-08-06 13:23:58 +0000

    Improve functionality
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@84638 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/WorkloadManagementSystem/scripts/dirac-wms-get-wn.py
+++ LHCbDIRAC/WorkloadManagementSystem/scripts/dirac-wms-get-wn.py
@@ -7,21 +7,29 @@
   Get WNs for a selection of jobs
 """
 __RCSID__ = "$Id: dirac-bookkeeping-file-metadata.py 65177 2013-04-22 15:24:07Z phicharp $"
+import DIRAC
 import  DIRAC.Core.Base.Script as Script
 from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
+import datetime
 
 
 if __name__ == "__main__":
   site = 'BOINC.World.org'
   status = ["Running"]
-  workerNode = None
+  minorStatus = None
+  workerNodes = None
   since = None
   date = 'today'
   full = False
+  until = None
+  batchIDs = None
   Script.registerSwitch( '', 'Site=', '   Select site (default: %s)' % site )
   Script.registerSwitch( '', 'Status=', '   Select status (default: %s)' % status )
+  Script.registerSwitch( '', 'MinorStatus=', '   Select minor status' )
   Script.registerSwitch( '', 'WorkerNode=', '  Select WN' )
+  Script.registerSwitch( '', 'BatchID=', '  Select batch jobID' )
   Script.registerSwitch( '', 'Since=', '   Date since when to select jobs, or number of days (default: today)' )
+  Script.registerSwitch( '', 'Date', '   Speficy the date' )
   Script.registerSwitch( '', 'Full', '   Printout full list of job (default: False except if --WorkerNode)' )
   Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                        'Usage:',
@@ -31,17 +39,27 @@ if __name__ == "__main__":
   for switch in switches:
     if switch[0] == 'Site':
       site = switch[1]
+    elif switch[0] == 'MinorStatus':
+      minorStatus = switch[1]
     elif switch[0] == 'Status':
       if switch[1].lower() == 'all':
         status = [None]
       else:
         status = switch[1].split( ',' )
     elif switch[0] == 'WorkerNode':
-      workerNode = switch[1].split( ',' )
+      workerNodes = switch[1].split( ',' )
+    elif switch[0] == 'BatchID':
+      try:
+        batchIDs = [int( id ) for id in switch[1].split( ',' )]
+      except:
+        gLogger.error( 'Invalid jobID', switch[1] )
+        DIRAC.exit( 1 )
     elif switch[0] == 'Full':
       full = True
+    elif switch[0] == 'Date':
+      since = switch[1]
+      until = str( datetime.datetime.strptime( since, '%Y-%m-%d' ) + datetime.timedelta( days = 1 ) ).split()[0]
     elif switch[0] == 'Since':
-      import datetime
       date = switch[1].lower()
       if date == 'today':
         since = None
@@ -54,15 +72,14 @@ if __name__ == "__main__":
         date += ' days'
       else:
         since = date
-      if type( since ) == type( 0 ):
+      if isinstance( since, int ):
         since = str( datetime.datetime.now() - datetime.timedelta( days = since ) ).split()[0]
 
 
-  if workerNode:
-    status = [None]
+  if workerNodes or batchIDs:
+    # status = [None]
     full = True
 
-  import DIRAC
   from DIRAC import gLogger
   from DIRAC.Interfaces.API.Dirac import Dirac
   from DIRAC.Core.DISET.RPCClient                          import RPCClient
@@ -72,39 +89,53 @@ if __name__ == "__main__":
   # Get jobs according to selection
   jobs = []
   for stat in status:
-    res = dirac.selectJobs( site = site, date = since, status = stat )
+    res = dirac.selectJobs( site = site, date = since, status = stat, minorStatus = minorStatus )
     if not res['OK']:
       gLogger.error( 'Error selecting jobs', res['Message'] )
       DIRAC.exit( 1 )
-    jobs += [int( job ) for job in res['Value']]
+    allJobs = [int( job ) for job in res['Value']]
+    if until:
+      res = dirac.selectJobs( site = site, date = until, status = stat )
+      if not res['OK']:
+        gLogger.error( 'Error selecting jobs', res['Message'] )
+        DIRAC.exit( 1 )
+      allJobs = sorted( set( allJobs ) - set( [int( job ) for job in res['Value']] ) )
+    jobs += allJobs
   if not jobs:
     gLogger.always( 'No jobs found...' )
     DIRAC.exit( 0 )
   # res = monitoring.getJobsSummary( jobs )
   # print eval( res['Value'] )[jobs[0]]
 
-  allJobs = []
+  allJobs = set()
   result = {}
   wnJobs = {}
   gLogger.always( '%d jobs found' % len( jobs ) )
   # Get host name
   for job in jobs:
     res = monitoring.getJobParameter( job, 'HostName' )
-    if res['OK']:
-      node = res['Value'].get( 'HostName', 'Unknown' )
-      if workerNode:
-        if node not in workerNode:
-          continue
-        allJobs.append( job )
-      if full or status == [None]:
-        allJobs.append( job )
-      result.setdefault( job, {} )['Status'] = status
-      result[job]['Node'] = node
-      wnJobs[node] = wnJobs.setdefault( node, 0 ) + 1
+    node = res.get( 'Value', {} ).get( 'HostName', 'Unknown' )
+    res = monitoring.getJobParameter( job, 'LocalJobID' )
+    batchID = res.get( 'Value', {} ).get( 'LocalJobID', 'Unknown' )
+    if workerNodes:
+      if node.split( '.' )[0] not in [wn.split( '.' )[0] for wn in workerNodes]:
+        continue
+      allJobs.add( job )
+    if batchIDs:
+      if batchID not in batchIDs:
+        continue
+      allJobs.add( job )
+    if full or status == [None]:
+      allJobs.add( job )
+    result.setdefault( job, {} )['Status'] = status
+    result[job]['Node'] = node
+    result[job]['LocalJobID'] = batchID
+    wnJobs[node] = wnJobs.setdefault( node, 0 ) + 1
 
   # If necessary get jobs' status
   statusCounters = {}
   if allJobs:
+    allJobs = sorted( allJobs, reverse = True )
     res = monitoring.getJobsStatus( allJobs )
     if res['OK']:
       jobStatus = res['Value']
@@ -123,12 +154,12 @@ if __name__ == "__main__":
                  jobApplicationStatus.get( job, {} ).get( 'ApplicationStatus', 'Unknown' )
         result[job]['Status'] = stat
         statusCounters[stat] = statusCounters.setdefault( stat, 0 ) + 1
-  elif not workerNode:
-    allJobs = jobs
+  elif not workerNodes and not batchIDs:
+    allJobs = sorted( jobs, reverse = True )
 
   # Print out result
-  if workerNode:
-    gLogger.always( 'Found %d jobs at %s, WN %s (since %s):' % ( len( allJobs ), site, workerNode, date ) )
+  if workerNodes or batchIDs:
+    gLogger.always( 'Found %d jobs at %s, WN %s (since %s):' % ( len( allJobs ), site, workerNodes, date ) )
     gLogger.always( 'List of jobs:', ','.join( [str( job ) for job in allJobs] ) )
   else:
     if status == [None]:
@@ -141,10 +172,21 @@ if __name__ == "__main__":
                                                for node in sorted( wnJobs,
                                                                    cmp = ( lambda n1, n2: ( wnJobs[n2] - wnJobs[n1] ) ) )] ) )
   if full:
-    for job in [job for job in sorted( allJobs, reverse = True )]:
-      status = result[job]['Status']
-      node = result[job]['Node']
-      if not workerNode or len( workerNode ) > 1:
-        gLogger.always( '%s: %s - %s' % ( node, job, status ) )
-      else:
-        gLogger.always( '%s - %s' % ( job, status ) )
+    if workerNodes or batchIDs:
+      nodeJobs = {}
+      for job in allJobs:
+        status = result[job]['Status']
+        node = result[job]['Node'].split( '.' )[0]
+        jobID = result[job].get( 'LocalJobID' )
+        nodeJobs.setdefault( node, [] ).append( ( jobID, job, status ) )
+      if not workerNodes:
+        workerNodes = sorted( nodeJobs )
+      for node in workerNodes:
+        for job in nodeJobs.get( node.split( '.' )[0], [] ):
+          gLogger.always( '%s ' % node + '(%s): %s - %s' % job )
+    else:
+      for job in allJobs:
+        status = result[job]['Status']
+        node = result[job]['Node']
+        jobID = result[job].get( 'LocalJobID' )
+        gLogger.always( '%s (%s): %s - %s' % ( node, jobID, job, status ) )
