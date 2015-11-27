1505344c13e54e0753bf8306bb25a14fa09841ef
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2015-11-27 14:37:12 +0000

    fix the step metadata...
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@86400 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/DB/OracleBookkeepingDB.py
+++ LHCbDIRAC/BookkeepingSystem/DB/OracleBookkeepingDB.py
@@ -4399,9 +4399,9 @@ and files.qualityid= dataquality.qualityid'
               condition += retVal['Value']
             else:
               return retVal
-          if len( parentprod ) < 2:
-            parentprod = str( parentprod )[:-2] + ")"
-          command = "select distinct s.stepid,s.stepname,s.applicationname,s.applicationversion,\
+        
+        parentprod += production
+        command = "select distinct s.stepid,s.stepname,s.applicationname,s.applicationversion,\
        s.optionfiles,s.dddb, s.conddb,s.extrapackages,s.visible, cont.step  from  %s \
                      where cont.stepid=s.stepid and \
                       prod.production=cont.production and\
