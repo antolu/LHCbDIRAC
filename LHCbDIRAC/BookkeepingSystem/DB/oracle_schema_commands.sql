688cd3d0f7a862d749d9d393049f65e1ed320f6b
zmathe <zmathe@4525493e-7705-40b1-a816-d608a930855b>
2015-11-27 14:55:20 +0000

    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@86401 4525493e-7705-40b1-a816-d608a930855b

--- LHCbDIRAC/BookkeepingSystem/DB/oracle_schema_commands.sql
+++ LHCbDIRAC/BookkeepingSystem/DB/oracle_schema_commands.sql
@@ -65,3 +65,49 @@ insert into runstatus (runnumber, jobid, finished) select runnumber, jobid, 'Y' 
 alter table jobs add StepID number;
 alter table jobs add constraint fk_jobs_stepid FOREIGN KEY (StepId) references steps(stepid);
 
+CREATE OR REPLACE TRIGGER RUNQUALITY
+before update or insert on newrunquality
+referencing new as new old as old
+for each row  
+begin
+   update files set files.qualityid= :new.qualityid where jobid in (select j.jobid from jobs j where j.runnumber= :new.runnumber and j.production<0);
+   update files set files.qualityid= :new.qualityid where files.fileid in (select f.fileid from files f, jobs j where j.jobid=f.jobid and j.runnumber= :new.runnumber and f.gotreplica='Yes'and j.production in (select prod.production from productionscontainer prod where prod.processingid= :new.processingid));
+end;
+
+
+CREATE OR REPLACE TRIGGER RUNSTATUS
+before update on runstatus
+referencing new as new old as old
+for each row  
+begin
+   BOOKKEEPINGORACLEDB.updateLuminosity(:new.runnumber);
+end;
+/
+
+
+alter table jobs add WNMJFHS06 float;
+create or replace  type jobMetadata is object(lfn varchar2(256),
+  DiracJobId                  NUMBER,
+  DiracVersion                VARCHAR2(256),
+  EventInputStat              NUMBER,
+  ExecTime                    FLOAT,
+  FirstEventNumber            NUMBER,
+  Location                    VARCHAR2(256),
+  Name                        VARCHAR2(256),
+  NumberOfEvents              NUMBER,
+  StatisticsRequested         NUMBER,
+  WNCPUPower                  VARCHAR2(256),
+  CPUTime                     FLOAT,
+  WNCache                     VARCHAR2(256),
+  WNMemory                    VARCHAR2(256),
+  WNModel                     VARCHAR2(256),
+  WORKERNODE                  varchar2(256),
+  WNCPUHS06                   FLOAT,
+  jobid                       number,
+  totalLuminosity             NUMBER,
+  production                  NUMBER,
+  ProgramName                 VARCHAR2(256),
+  ProgramVersion              VARCHAR2(256),
+  WNMJFHS06                   FLOAT);
+ drop type bulk_collect_jobMetadata;
+create or replace type bulk_collect_jobMetadata is table of jobMetadata;
