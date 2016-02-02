e53c61e235c0b9f6e6c294508eaf528b3e7c28ae
cluzzi <cluzzi@4525493e-7705-40b1-a816-d608a930855b>
2016-02-02 09:46:03 +0000

    New request operation to verify the volunteers output.
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@87251 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/DataManagementSystem/Agent/RequestOperations/WMSSecureOutputData.py
@@ -0,0 +1,53 @@
+""" :mod: ValidateRequest
+    ==================
+
+    .. module: ValidateRequest
+    :synopsis: validateRequest operation handler
+"""
+
+__RCSID__ = "$Id $"
+
+
+# # imports
+from DIRAC import S_OK, S_ERROR
+from DIRAC.Core.Utilities import DEncode
+from DIRAC.FrameworkSystem.Client.MonitoringClient import gMonitor
+from DIRAC.RequestManagementSystem.private.OperationHandlerBase import OperationHandlerBase
+from DIRAC.RequestManagementSystem.Client.ReqClient import ReqClient
+
+########################################################################
+class WMSSecureOutputData( OperationHandlerBase ):
+  """
+  .. class:: ValidateRequest
+
+  Validate operation handler
+  """
+
+  def __init__( self, operation = None, csPath = None ):
+    """c'tor
+
+    :param self: self reference
+    :param Operation operation: Operation instance
+    :param str csPath: CS path for this handler
+    """
+    OperationHandlerBase.__init__( self, operation, csPath )
+
+
+  def __call__( self ):
+    """ It expects to find the reqID in operation.Arguments
+    """
+    try:
+      #requestID = DEncode.decode( self.operation.Arguments )[0]
+      decode = DEncode.decode( self.operation.Arguments )
+      self.log.debug (decode)
+      #self.log.debug( "decoded requestID=%s" % str( requestID ) )
+      print "in validate request;"
+    except ValueError, error:
+      self.log.exception( error )
+      self.operation.Error = str( error )
+      self.operation.Status = "Failed"
+      return S_ERROR( str( error ) )
+
+    self.operation.Status = "Done"
+    return S_OK()
+
