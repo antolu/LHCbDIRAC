229b5fc4770b3aa4d1049884f64f43507ddd06c8
phicharp <phicharp@4525493e-7705-40b1-a816-d608a930855b>
2016-02-11 10:15:37 +0000

    New script
    git-svn-id: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/trunk@87259 4525493e-7705-40b1-a816-d608a930855b

---
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-replicate-to-run-destination.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+########################################################################
+# $HeadURL: svn+ssh://svn.cern.ch/reps/dirac/LHCbDIRAC/branches/LHCbDIRAC_v8r1_branch/DataManagementSystem/scripts/dirac-dms-replicate-lfn.py $
+# File :    dirac-dms-replicate-to-run-destination
+# Author  : Philippe Charpentier
+########################################################################
+"""
+  Replicate a (list of) existing LFN(s) to Ses defined by the run destination
+"""
+__RCSID__ = "$Id: dirac-dms-replicate-lfn.py 79816 2014-12-03 09:11:02Z phicharp $"
+from DIRAC.Core.Base import Script
+from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
+
+if __name__ == "__main__":
+  dmScript = DMScript()
+  dmScript.registerFileSwitches()
+  dmScript.registerSiteSwitches()
+
+  Script.setUsageMessage( '\n'.join( [ __doc__,
+                                       'Usage:',
+                                       '  %s [option|cfgfile] ...  [LFN1[,LFN2,[...]]] Dest[,Dest2[,...]] [Source [Cache]]' % Script.scriptName,
+                                       'Arguments:',
+                                       '  Dest:     Valid DIRAC SE(s)'] ) )
+  Script.parseCommandLine( ignoreErrors = True )
+
+  from LHCbDIRAC.DataManagementSystem.Client.ScriptExecutors import executeReplicateToRunDestination
+  executeReplicateToRunDestination( dmScript )
+
