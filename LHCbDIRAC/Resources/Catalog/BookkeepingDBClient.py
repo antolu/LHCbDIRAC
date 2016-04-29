antolu/LHCbDIRAC

commit 8b4f6ffd95f1a11164b897941e454cdb163eb337

type -> isinstance

LHCbDIRAC/Resources/Catalog/BookkeepingDBClient.py
@@ -1,8 +1,6 @@
 """ Client for BookkeepingDB file catalog
 """
 
-import types
-
 from DIRAC                                                          import gLogger, S_OK, S_ERROR
 from DIRAC.Core.DISET.RPCClient                                     import RPCClient
 from DIRAC.Core.Utilities.List                                      import breakListIntoChunks
@@ -152,11 +150,11 @@ class BookkeepingDBClient( FileCatalogueBase ):
     '''
     if not self.valid:
       return S_ERROR( 'BKDBClient not initialised' )
-    if type( path ) in types.StringTypes:
+    if isinstance(path, basestring ):
       return S_OK( [path] )
-    elif type( path ) == types.ListType:
+    elif isinstance(path, list):
       return S_OK( path )
-    elif type( path ) == types.DictType:
+    elif isinstance(path, dict):
       return S_OK( path.keys() )
     else:
       errStr = "BookkeepingDBClient.__checkArgumentFormat: Supplied path is not of the correct format."
@@ -219,8 +217,8 @@ class BookkeepingDBClient( FileCatalogueBase ):
       else:
         success = res['Value'].get( 'Successful', res['Value'] )
         failed.update( dict.fromkeys( [lfn for lfn in lfnList if lfn not in success], 'File does not exist' ) )
-        failed.update( dict( [( lfn, val ) for lfn, val in success.items() if type( val ) in types.StringTypes ] ) )
-        successful.update( dict( [( lfn, val ) for lfn, val in success.items() if type( val ) not in types.StringTypes ] ) )
+        failed.update( dict( [( lfn, val ) for lfn, val in success.items() if isinstance( val, basestring ) ] ) )
+        successful.update( dict( [( lfn, val ) for lfn, val in success.items() if isinstance( val, basestring ) ] ) )
     return S_OK( {'Successful':successful, 'Failed':failed} )
 ################################################################################
 # EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF

commit bcd6232b09853122d82c05fbc2c843c4b020d011

Fix regression introduced by 8b4f6ffd95f1a11164b897941e454cdb163eb337

LHCbDIRAC/Resources/Catalog/BookkeepingDBClient.py
@@ -218,7 +218,7 @@ class BookkeepingDBClient( FileCatalogueBase ):
         success = res['Value'].get( 'Successful', res['Value'] )
         failed.update( dict.fromkeys( [lfn for lfn in lfnList if lfn not in success], 'File does not exist' ) )
         failed.update( dict( [( lfn, val ) for lfn, val in success.items() if isinstance( val, basestring ) ] ) )
-        successful.update( dict( [( lfn, val ) for lfn, val in success.items() if isinstance( val, basestring ) ] ) )
+        successful.update( dict( [( lfn, val ) for lfn, val in success.items() if not isinstance( val, basestring ) ] ) )
     return S_OK( {'Successful':successful, 'Failed':failed} )
 ################################################################################
 # EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF