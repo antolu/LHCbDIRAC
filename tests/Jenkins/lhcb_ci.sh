3bc1f595426eb5b20ba5844097df077d23e6b8a7
Federico Stagni <federico.stagni@cern.ch>
2016-05-02 15:22:14 +0200

    fix/hack

--- tests/Jenkins/lhcb_ci.sh
+++ tests/Jenkins/lhcb_ci.sh
@@ -164,6 +164,7 @@ function LHCbDIRACPilotInstall(){
 	fi
 
 	#run the dirac-pilot script, only for installing, do not run the JobAgent here
+	cp $TESTCODE/DIRAC/Core/scripts/dirac-install.py $PWD/
 	python $PILOTINSTALLDIR/dirac-pilot.py -S $DIRACSETUP -l LHCb $installVersion -C $CSURL -N $JENKINS_CE -Q $JENKINS_QUEUE -n $JENKINS_SITE --cert --certLocation=/home/dirac/certs/ -E LHCbPilot -X LHCbGetPilotVersion,CheckWorkerNode,LHCbInstallDIRAC,LHCbConfigureBasics,CheckCECapabilities,CheckWNCapabilities,LHCbConfigureSite,LHCbConfigureArchitecture,LHCbConfigureCPURequirements $DEBUG
 
 	echo '==> Done LHCbDIRACPilotInstall'
