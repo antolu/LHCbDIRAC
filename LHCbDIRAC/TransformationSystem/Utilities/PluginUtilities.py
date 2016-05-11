b9750f0f8e59f39e193da821d0b9586202f84185
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2016-05-11 16:54:17 +0200

    fix a bug and remove obsolete plugins

--- LHCbDIRAC/TransformationSystem/Utilities/PluginUtilities.py
+++ LHCbDIRAC/TransformationSystem/Utilities/PluginUtilities.py
@@ -120,8 +120,8 @@ class PluginUtilities( DIRACPluginUtilities ):
     Else:
     * Use the duration (in seconds) of runs as metrics for SE usage
     """
-    self.shareMetrics = self.getPluginParam( 'ShareMetrics', '' )
-    # self.shareMetrics = 'Time'
+    if self.shareMetrics is None:
+      self.shareMetrics = self.getPluginParam( 'ShareMetrics', '' )
     if section is None:
       # This is for reconstruction shares (CU)
       sharesSections = { 'DataReconstruction': 'CPUforRAW', 'DataReprocessing' : 'CPUforReprocessing'}
@@ -925,7 +925,6 @@ class PluginUtilities( DIRACPluginUtilities ):
     foundProds = {}
     for desc in descToCheck.keys():
       prod = descProd[desc]
-      # print desc, descToCheck[desc], prod
       # If we found an existing descendant in the list of productions, all OK
       if descMetadata[desc]['GotReplica'] == 'Yes' and prod in prodList:
         foundProds.setdefault( prod, set() ).update( descToCheck[desc] )
@@ -943,7 +942,7 @@ class PluginUtilities( DIRACPluginUtilities ):
       toCheckInProd = set()
       for desc in prodDesc[prod]:
         if not descToCheck[desc] & finalResult:
-          toCheckInProd.update( desc )
+          toCheckInProd.add( desc )
       if toCheckInProd:
         res = self.checkForDescendants( toCheckInProd, prodList, level = level + 1 )
         if not res['OK']:
@@ -989,12 +988,11 @@ class PluginUtilities( DIRACPluginUtilities ):
 
 def getRemovalPlugins():
   return ( "DestroyDataset", 'DestroyDatasetWhenProcessed' ,
-           "DeleteDataset", "DeleteReplicas", 'DeleteReplicasWhenProcessed',
            "RemoveDataset", "RemoveReplicas", 'RemoveReplicasWhenProcessed', 'ReduceReplicas' )
 def getReplicationPlugins():
-  return ( "LHCbDSTBroadcast", "LHCbMCDSTBroadcast", "LHCbMCDSTBroadcastRandom",
+  return ( "LHCbDSTBroadcast", "LHCbMCDSTBroadcastRandom",
            "ArchiveDataset", "ReplicateDataset",
-           "RAWShares", 'RAWReplication',
+           'RAWReplication',
            'FakeReplication', 'ReplicateToLocalSE', 'Healing' )
 
 def getShares( sType, normalise = False ):
