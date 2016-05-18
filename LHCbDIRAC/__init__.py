70d5a76e0f8694c33223b0653231ad512ffbf872
Christophe Haen <christophe.haen@cern.ch>
2016-05-18 16:29:01 +0200

    v8r2p43

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -5,7 +5,7 @@ __path__ = extend_path( __path__, __name__ )
 
 majorVersion = 8
 minorVersion = 2
-patchLevel = 42
+patchLevel = 43
 preVersion = 0
 
 version = "v%sr%s" % ( majorVersion, minorVersion )
