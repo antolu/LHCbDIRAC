df81918a2121ea984ccf66752452703364ec35f0
marcelovilaca <marcelovilaca@gmail.com>
2016-07-05 15:12:21 +0200

    Changed from /bin/csh to /bin/bash Changed all the chs sintax to bash Added line to get user directory (e.g. /m/msoares) Changed all exit status from 0 to dirac script exit status Changed lhcb_shifter lhcb_prmgr (my bad, I used shifter for testing before my prmgr status was working)

--- tests/System/Client/client_test.sh
+++ tests/System/Client/client_test.sh
@@ -1,87 +1,113 @@
-#!/bin/csh -f
+#!/bin/bash			
+
+
+dir=$( echo "$USER" |cut -c 1)/$USER
+ls>>hlta0102.txt
 
 echo "lhcb-proxy-init -g lhcb_prmgr"
-lhcb-proxy-init -g lhcb_shifter
-if $status != 0 then
-   exit 0
-endif
+lhcb-proxy-init -g lhcb_prmgr
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 echo "======  dirac-proxy-info"
 dirac-proxy-info
-if $status != 0 then
-   exit 0
-endif
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 echo "======  dirac-configuration-dump-local-cache"
 dirac-configuration-dump-local-cache
-if $status != 0 then
-   exit 0
-endif
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 echo "======  dirac-admin-get-banned-sites"
 dirac-admin-get-banned-sites
-if $status != 0 then
-   exit 0
-endif
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 echo "======   dirac-admin-site-info LCG.CERN.ch"
 dirac-admin-site-info LCG.CERN.ch
-if $status != 0 then
-   exit 0
-endif
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 echo "======  dirac-dms-show-se-status"
 dirac-dms-show-se-status
-if $status != 0 then
-   exit 0
-endif
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 echo "======  dirac-dms-storage-usage-summary"
 dirac-dms-storage-usage-summary -S CERN-USER -D /lhcb/
-if $status != 0 then
-   exit 0
-endif
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 echo "======  dirac-dms-lfn-replicas /lhcb/data/2010/RAW/FULL/LHCb/COLLISION10/81789/081789_0000000071.raw"
 dirac-dms-lfn-replicas /lhcb/data/2010/RAW/FULL/LHCb/COLLISION10/81789/081789_0000000071.raw
-if $status != 0 then
-   exit 0
-endif
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
-echo "======  dirac-dms-replicate-lfn /lhcb/user/m/msoares/hlta0102.txt CNAF-USER"
-dirac-dms-replicate-lfn /lhcb/user/m/msoares/hlta0102.txt CNAF-USER
-if $status != 0 then
-   exit 0
-endif
+echo "====== dirac-dms-add-file /lhcb/user/$dir/hlta0102.txt CNAF-USER"
+dirac-dms-add-file /lhcb/user/$dir/hlta0102.txt ./hlta0102.txt CNAF-USER
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
+echo " "
+
+echo "======  dirac-dms-replicate-lfn /lhcb/user/$dir/hlta0102.txt CNAF-USER"
+dirac-dms-replicate-lfn /lhcb/user/$dir/hlta0102.txt CNAF-USER
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 echo "======  dirac-dms-check-directory-integrity /lhcb/data/2010/RAW/FULL/LHCb/COLLISION10/81789"
 dirac-dms-check-directory-integrity /lhcb/data/2010/RAW/FULL/LHCb/COLLISION10/81789
-if $status != 0 then
-   exit 0
-endif
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 echo "====== dirac-dms-check-file-integrity /lhcb/data/2010/RAW/FULL/LHCb/COLLISION10/81789/081789_0000000044.raw"
 dirac-dms-check-file-integrity /lhcb/data/2010/RAW/FULL/LHCb/COLLISION10/81789/081789_0000000044.raw
-if $status != 0 then
-   exit 0
-endif
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 echo "======  dirac-monitoring-get-components-status"
 dirac-monitoring-get-components-status
-if $status != 0 then
-   exit 0
-endif
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
-echo "====== dirac-dms-remove-replicas /lhcb/user/m/msoares/hlta0102.txt CNAF-USER"
-dirac-dms-remove-replicas /lhcb/user/m/msoares/hlta0102.txt CNAF-USER
-if $status != 0 then
-   exit 0
-endif
+echo "====== dirac-dms-remove-replicas /lhcb/user/$dir/hlta0102.txt CNAF-USER"
+dirac-dms-remove-replicas /lhcb/user/$dir/hlta0102.txt CNAF-USER
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
-echo "====== dirac-dms-remove-files /lhcb/user/m/msoares/hlta0102.txt CNAF-USER"
-dirac-dms-remove-files /lhcb/user/m/msoares/hlta0102.txt CNAF-USER
-if $status != 0 then
-   exit 0
-endif
+echo "====== dirac-dms-remove-files /lhcb/user/$dir/hlta0102.txt CNAF-USER"
+dirac-dms-remove-files /lhcb/user/$dir/hlta0102.txt CNAF-USER
+if [ $? -ne 0 ]
+then
+   exit $?
+fi
 echo " "
 
