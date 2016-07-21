9a0e14a0f20a84649e152d9d021e488451448f3b
Federico Stagni <federico.stagni@cern.ch>
2016-07-21 10:02:50 +0200

    fix jenkins test

--- tests/Jenkins/dirac-test-job.py
+++ tests/Jenkins/dirac-test-job.py
@@ -42,7 +42,7 @@ gLogger.info( "\n Submitting hello world job, with input, targeting DIRAC.Jenkin
 inputJ = LHCbJob()
 inputJ.setName( "helloWorld-TEST-INPUT-TO-Jenkins" )
 inputJ.setInputSandbox( [find_all( 'exe-script-with-input.py', '..', 'GridTestSubmission' )[0]] )
-inputJ.setExecutable( "exe-script-with-input.py", "", "exeWithInput.log" )
+inputJ.setExecutable( "exe-script-with-input-jenkins.py", "", "exeWithInput.log" )
 inputJ.setInputData( '/lhcb/test/DIRAC/Jenkins/jenkinsInputTestFile.txt' )  # this file should be at CERN-SWTEST only
 inputJ.setInputDataPolicy( 'download' )
 inputJ.setCPUTime( 17800 )
