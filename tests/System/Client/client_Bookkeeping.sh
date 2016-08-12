1f010281c630d43e7dbcce4decac3453ca2a1819
Federico Stagni <federico.stagni@cern.ch>
2016-08-12 12:45:56 +0200

    using unique list of file names

--- tests/System/Client/client_Bookkeeping.sh
+++ tests/System/Client/client_Bookkeeping.sh
@@ -10,10 +10,17 @@ echo $tmpDir
 # Generate 10 files with random content
 # The names will be "random_content_X" and be between 1 and 10 Mb
 
+# array of fileNames
+fileNames=()
 for n in {1..10}
 do
-  random10=$(( (RANDOM % 10) +1 ))
-  dd if=/dev/urandom of=random_content_$( printf %03d "$n" ).init bs=1M count=$random10
+  fileNames+=(random_content_$(date +"20%y%m%d")_$(date +"%H%M%S")_$( printf %03d "$n" ))
+done
+
+for n in {1..10}
+do
+  random10=$(( (RANDOM % 10) +1 )) # a random value between 1 and 10
+  dd if=/dev/urandom of=${fileNames[$n-1]}.init bs=1M count=$random10
 done
 
 # Copy initXMLReport.xml template in tmpDir
@@ -23,7 +30,8 @@ cp $diracDir/tests/System/Client/BKReportsSamples/InitXMLReport.xml .
 for n in {1..10}
 do
   # Create the specific BK report
-  cp InitXMLReport.xml bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
+  xmlName=bookkeping_${fileNames[$n-1]}.xml
+  cp InitXMLReport.xml $xmlName
 
   # Getting the info
   fileName=random_content_$( printf %03d "$n" ).init
@@ -37,16 +45,16 @@ do
   end=$(date +"20%y-%m-%d %R")
 
   # Applying the info
-  sed -i s/VAR_Name/00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" )/g bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
-  sed -i s/VAR_Location/$location/g bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
-  sed -i s/VAR_ProgramVersion/$version/g bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
-  sed -i s/VAR_FileName/$fileName/g bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
-  sed -i s/VAR_FileSize/$size/g bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
-  sed -i "s/VAR_JobStart/$start/g" bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
-  sed -i "s/VAR_JobEnd/$end/g" bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
-  sed -i s/VAR_Date/$tdate/g bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
-  sed -i s/VAR_Time/$ttime/g bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
-  sed -i s/VAR_Guid/$guid/g bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml
+  sed -i s/VAR_Name/00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" )/g $xmlName
+  sed -i s/VAR_Location/$location/g $xmlName
+  sed -i s/VAR_ProgramVersion/$version/g $xmlName
+  sed -i s/VAR_FileName/$fileName/g $xmlName
+  sed -i s/VAR_FileSize/$size/g $xmlName
+  sed -i "s/VAR_JobStart/$start/g" $xmlName
+  sed -i "s/VAR_JobEnd/$end/g" $xmlName
+  sed -i s/VAR_Date/$tdate/g $xmlName
+  sed -i s/VAR_Time/$ttime/g $xmlName
+  sed -i s/VAR_Guid/$guid/g $xmlName
 done
 
 # Making sure the file types can be sent
