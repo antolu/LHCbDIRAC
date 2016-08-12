a5b7ac2448013a029b78c4ab2aeff831fe2c5ce0
Federico Stagni <federico.stagni@cern.ch>
2016-08-12 12:53:41 +0200

    using fixed names everywhere

--- tests/System/Client/client_Bookkeeping.sh
+++ tests/System/Client/client_Bookkeeping.sh
@@ -14,29 +14,36 @@ echo $tmpDir
 fileNames=()
 for n in {1..10}
 do
-  fileNames+=(random_content_$(date +"20%y%m%d")_$(date +"%H%M%S")_$( printf %03d "$n" ))
+  fileNames+=($(date +"20%y%m%d")_$(date +"%H%M%S")_$( printf %03d "$n" ))
 done
 
 for n in {1..10}
 do
   random10=$(( (RANDOM % 10) +1 )) # a random value between 1 and 10
-  dd if=/dev/urandom of=${fileNames[$n-1]}.init bs=1M count=$random10
+  dd if=/dev/urandom of=random_content_${fileNames[$n-1]}.init bs=1M count=$random10
 done
+
+# Making sure the file types can be sent
+python $diracDir/tests/System/Client/dirac-add-bkk-ft.py INIT "just a desc for a test file type (INIT)" 1
+python $diracDir/tests/System/Client/dirac-add-bkk-ft.py FOO "just a desc for a test file type (FOO)" 1
+python $diracDir/tests/System/Client/dirac-add-bkk-ft.py BAR "just a desc for a test file type (BAR)" 1
 
 # Copy initXMLReport.xml template in tmpDir
 cp $diracDir/tests/System/Client/BKReportsSamples/InitXMLReport.xml .
 
-# For each random_content_X files, create a BK report
+# For each random_content_X files, create a BK report, then send it
 for n in {1..10}
 do
-  # Create the specific BK report
+  # Names of files
+  fileName=random_content_${fileNames[$n-1]}.init
   xmlName=bookkeping_${fileNames[$n-1]}.xml
+
+  # Create the specific BK report
   cp InitXMLReport.xml $xmlName
 
   # Getting the info
-  fileName=random_content_$( printf %03d "$n" ).init
-  size=$(stat --printf="%s" random_content_$( printf %03d "$n" ).init)
-  guid=$(python $diracDir/tests/System/Client/dirac-get-guid.py $tmpDir/random_content_$( printf %03d "$n" ).init -o LogLevel=FATAL)
+  size=$(stat --printf="%s" $fileName)
+  guid=$(python $diracDir/tests/System/Client/dirac-get-guid.py $tmpDir/$fileName -o LogLevel=FATAL)
   location=$HOSTNAME
   version=$(dirac-version)
   tdate=$(date +"20%y-%m-%d")
@@ -45,7 +52,7 @@ do
   end=$(date +"20%y-%m-%d %R")
 
   # Applying the info
-  sed -i s/VAR_Name/00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" )/g $xmlName
+  sed -i s/VAR_Name/${fileNames[$n-1]}/g $xmlName
   sed -i s/VAR_Location/$location/g $xmlName
   sed -i s/VAR_ProgramVersion/$version/g $xmlName
   sed -i s/VAR_FileName/$fileName/g $xmlName
@@ -55,15 +62,7 @@ do
   sed -i s/VAR_Date/$tdate/g $xmlName
   sed -i s/VAR_Time/$ttime/g $xmlName
   sed -i s/VAR_Guid/$guid/g $xmlName
-done
 
-# Making sure the file types can be sent
-python ./tests/System/Client/dirac-add-bkk-ft.py INIT "just a desc for a test file type (INIT)" 1
-python ./tests/System/Client/dirac-add-bkk-ft.py FOO "just a desc for a test file type (FOO)" 1
-python ./tests/System/Client/dirac-add-bkk-ft.py BAR "just a desc for a test file type (BAR)" 1
+  python $diracDir/tests/System/Client/dirac-send-bk-report.py $xmlName -ddd
 
-# Now we send the bookkeping reports
-for n in {1..10}
-do
-  python $diracDir/tests/System/Client/dirac-send-bk-report.py bookkeping_00000001_0000000$( printf %03d "$n" )_$( printf %03d "$n" ).xml -ddd
 done
