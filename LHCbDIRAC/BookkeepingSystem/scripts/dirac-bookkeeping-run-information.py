a5ed6f2ce7bb2cf019a03c3d23d06e058b2a90fd
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2016-10-03 13:29:35 +0200

    allow better information formatting

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-run-information.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-run-information.py
@@ -83,91 +83,88 @@ if item:
         success.setdefault( finished, [] ).append( run )
       else:
         success[run] = finished
-    if byValue:
-      for val in success:
-        success[val] = ','.join( [str( run ) for run in success[val]] )
-    printDMResult( S_OK( result ), empty = 'None', script = 'dirac-bookkeeping-run-information' )
-    DIRAC.exit( 0 )
-
-  progressBar = ProgressBar( len( runSet ), title = "Getting %s for %d runs" % ( item, len( runSet ) ), step = 10 )
-
-for run in sorted( runSet ):
-  if item:
-    progressBar.loop()
-  res = bk.getRunStatus( [run] )
-  if res['OK']:
-    finished = {'N':'No', 'Y':'Yes'}.get( res['Value']['Successful'].get( run, {} ).get( 'Finished' ), 'Unknown' )
-    if item == 'Finished':
-      if byValue:
-        success.setdefault( finished, [] ).append( run )
-      else:
-        success[run] = finished
-      continue
-  elif item == 'Finished':
-    result['Failed'][run] = res['Message']
-    continue
   else:
-    finished = 'Unknown'
+    progressBar = ProgressBar( len( runSet ), title = "Getting %s for %d runs" % ( item, len( runSet ) ), step = 10 )
 
-  res = bk.getRunInformations( run )
-
-  if res['OK']:
-    val = res['Value']
+if item != 'Finished':
+  for run in sorted( runSet ):
     if item:
-      if item not in val:
-        gLogger.error( "Item not found", "\n Valid items: %s" % str( sorted( val ) ) )
-        DIRAC.exit( 3 )
-      itemValue = val.get( item, "Unknown" )
-      if byValue:
-        success.setdefault( itemValue, [] ).append( run )
-      else:
-        result['Successful'][run] = itemValue
-      continue
-    runstart = val.get( 'RunStart', 'Unknown' )
-    runend = val.get( 'RunEnd', 'Unknown' )
-    configname = val.get( 'Configuration Name', 'Unknown' )
-    configversion = val.get( 'Configuration Version', 'Unknown' )
-    fillnb = val.get( 'FillNumber', 'Unknown' )
-    datataking = val.get( 'DataTakingDescription', 'Unknown' )
-    processing = val.get( 'ProcessingPass', 'Unknown' )
-    stream = val.get( 'Stream', 'Unknown' )
-    fullstat = val.get( 'FullStat', 'Unknown' )
-    nbofe = val.get( 'Number of events', 'Unknown' )
-    nboff = val.get( 'Number of file', 'Unknown' )
-    fsize = val.get( 'File size', 'Unknown' )
-    totalLuminosity = val.get( "TotalLuminosity", 'Unknown' )
-    tck = val.get( 'Tck', 'Unknown' )
-
-    if sep:
-      print sep
-    print "Run  Informations for run %d: " % run
-    print "Run Start:".ljust( 30 ), str( runstart )
-    print "Run End:".ljust( 30 ), str( runend )
-    print "Total luminosity:".ljust( 30 ), str( totalLuminosity )
-    print "  Configuration Name:".ljust( 30 ), configname
-    print "  Configuration Version:".ljust( 30 ), configversion
-    print "  FillNumber:".ljust( 30 ), fillnb
-    print "  Finished:".ljust( 30 ), finished
-    print "  Data taking description:".ljust( 30 ), datataking
-    print "  Processing pass:".ljust( 30 ), processing
-    print "  TCK:".ljust( 30 ), tck
-    print "  Stream:".ljust( 30 ), stream
-    just = len( str( fsize ) ) + 3
-    print "  FullStat:".ljust( 30 ), str( fullstat ).ljust( just ), " Total: ".ljust( 10 ) + str( sum( fullstat ) )
-    print "  Number of events:".ljust( 30 ), str( nbofe ).ljust( just ), " Total:".ljust( 10 ) + str( sum( nbofe ) )
-    print "  Number of files:".ljust( 30 ), str( nboff ).ljust( just ), " Total: ".ljust( 10 ) + str( sum( nboff ) )
-    print "  File size:".ljust( 30 ), str( fsize ).ljust( just ), " Total: ".ljust( 10 ) + str( sum( fsize ) )
-    sep = 20 * '='
-  else:
-    if item:
+      progressBar.loop()
+    res = bk.getRunStatus( [run] )
+    if res['OK']:
+      finished = {'N':'No', 'Y':'Yes'}.get( res['Value']['Successful'].get( run, {} ).get( 'Finished' ), 'Unknown' )
+      if item == 'Finished':
+        if byValue:
+          success.setdefault( finished, [] ).append( run )
+        else:
+          success[run] = finished
+        continue
+    elif item == 'Finished':
       result['Failed'][run] = res['Message']
       continue
-    DIRAC.gLogger.fatal( "ERROR for run %s:" % run, res['Message'] )
-    DIRAC.exit( 1 )
+    else:
+      finished = 'Unknown'
+
+    res = bk.getRunInformations( run )
+
+    if res['OK']:
+      val = res['Value']
+      if item:
+        if item not in val:
+          gLogger.error( "Item not found", "\n Valid items: %s" % str( sorted( val ) ) )
+          DIRAC.exit( 3 )
+        itemValue = val.get( item, "Unknown" )
+        if byValue:
+          success.setdefault( itemValue, [] ).append( run )
+        else:
+          result['Successful'][run] = itemValue
+        continue
+      runstart = val.get( 'RunStart', 'Unknown' )
+      runend = val.get( 'RunEnd', 'Unknown' )
+      configname = val.get( 'Configuration Name', 'Unknown' )
+      configversion = val.get( 'Configuration Version', 'Unknown' )
+      fillnb = val.get( 'FillNumber', 'Unknown' )
+      datataking = val.get( 'DataTakingDescription', 'Unknown' )
+      processing = val.get( 'ProcessingPass', 'Unknown' )
+      stream = val.get( 'Stream', 'Unknown' )
+      fullstat = val.get( 'FullStat', 'Unknown' )
+      nbofe = val.get( 'Number of events', 'Unknown' )
+      nboff = val.get( 'Number of file', 'Unknown' )
+      fsize = val.get( 'File size', 'Unknown' )
+      totalLuminosity = val.get( "TotalLuminosity", 'Unknown' )
+      tck = val.get( 'Tck', 'Unknown' )
+
+      if sep:
+        print sep
+      print "Run  Informations for run %d: " % run
+      print "Run Start:".ljust( 30 ), str( runstart )
+      print "Run End:".ljust( 30 ), str( runend )
+      print "Total luminosity:".ljust( 30 ), str( totalLuminosity )
+      print "  Configuration Name:".ljust( 30 ), configname
+      print "  Configuration Version:".ljust( 30 ), configversion
+      print "  FillNumber:".ljust( 30 ), fillnb
+      print "  Finished:".ljust( 30 ), finished
+      print "  Data taking description:".ljust( 30 ), datataking
+      print "  Processing pass:".ljust( 30 ), processing
+      print "  TCK:".ljust( 30 ), tck
+      print "  Stream:".ljust( 30 ), stream
+      just = len( str( fsize ) ) + 3
+      print "  FullStat:".ljust( 30 ), str( fullstat ).ljust( just ), " Total: ".ljust( 10 ) + str( sum( fullstat ) )
+      print "  Number of events:".ljust( 30 ), str( nbofe ).ljust( just ), " Total:".ljust( 10 ) + str( sum( nbofe ) )
+      print "  Number of files:".ljust( 30 ), str( nboff ).ljust( just ), " Total: ".ljust( 10 ) + str( sum( nboff ) )
+      print "  File size:".ljust( 30 ), str( fsize ).ljust( just ), " Total: ".ljust( 10 ) + str( sum( fsize ) )
+      sep = 20 * '='
+    else:
+      if item:
+        result['Failed'][run] = res['Message']
+        continue
+      DIRAC.gLogger.fatal( "ERROR for run %s:" % run, res['Message'] )
+      DIRAC.exit( 1 )
+    if item:
+      progressBar.endLoop()
 
 if item:
-  progressBar.endLoop()
   if byValue:
     for val in success:
-      success[val] = ','.join( [str( run ) for run in success[val]] )
+      success[val] = ( '(%d runs) - ' % len( success[val] ) ) + ( ','.join( sorted( str( run ) for run in success[val] ) ) )
   printDMResult( S_OK( result ), empty = 'None', script = 'dirac-bookkeeping-run-information' )
