d21b3438ba3aaa6cc51e89fc325eef26e99e0027
Joel Closier <joel.closier@cern.ch>
2016-11-25 15:50:15 +0100

    remove lbvobox15 and lbvobox49

--- dist-tools/create_vobox_update.py
+++ dist-tools/create_vobox_update.py
@@ -16,16 +16,14 @@ HOME_DIR = os.path.join( os.environ['HOME'], 'DiracAdmin' )
 file_skel = os.path.join( HOME_DIR, 'skel_vobox_update' )
 file_T1 = os.path.join( HOME_DIR, 'vobox_update_T1' )
 T1_list = ['voboxlhcb.gridpp.rl.ac.uk', 'voboxlhcb.pic.es', 'voboxlhcb.nikhef.nl', 'voboxlhcb.cr.cnaf.infn.it', 'voboxlhcb.in2p3.fr', 'voboxlhcb.gridka.de']
-file_A = os.path.join( HOME_DIR, 'vobox_update_A' )
-A_list = ['lbvobox15.cern.ch', 'lbvobox18.cern.ch']
 file_B = os.path.join( HOME_DIR, 'vobox_update_B' )
-B_list = ['lbvobox27.cern.ch', 'lbvobox28.cern.ch']
+B_list = ['lbvobox27.cern.ch', 'lbvobox28.cern.ch','lbvobox18.cern.ch']
 file_C = os.path.join( HOME_DIR, 'vobox_update_C' )
 C_list = ['lbvobox30.cern.ch', 'lbvobox100.cern.ch','lbvobox101.cern.ch','lbvobox102.cern.ch','lbvobox103.cern.ch','lbvobox109.cern.ch','lbvobox200.cern.ch']
 file_D = os.path.join( HOME_DIR, 'vobox_update_D' )
 D_list = ['lbvobox06.cern.ch', 'lbvobox104.cern.ch','lbvobox105.cern.ch','lbvobox106.cern.ch','lbvobox107.cern.ch','lbvobox108.cern.ch']
 file_E = os.path.join( HOME_DIR, 'vobox_update_E' )
-E_list = ['lbvobox43.cern.ch', 'lbvobox46.cern.ch', 'lbvobox47.cern.ch', 'lbvobox49.cern.ch', 'volhcb04.cern.ch', 'volhcb05.cern.ch']
+E_list = ['lbvobox43.cern.ch', 'lbvobox46.cern.ch', 'lbvobox47.cern.ch', 'volhcb04.cern.ch', 'volhcb05.cern.ch']
 
 def generateTemplate(hosts, filename):
   fdr = open( file_skel )
@@ -47,7 +45,6 @@ def generateTemplate(hosts, filename):
 
 if __name__ == '__main__':
   generateTemplate(T1_list, file_T1)
-  generateTemplate(A_list, file_A)
   generateTemplate(B_list, file_B)
   generateTemplate(C_list, file_C)
   generateTemplate(D_list, file_D)
