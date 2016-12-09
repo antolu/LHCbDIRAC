afcfe287d52b8af72ca35043eb7c8c90877b822f
Zoltan Mathe <zoltan.mathe@cern.ch>
2016-12-09 15:27:38 +0100

    v8r5-pre3

--- LHCbDIRAC/__init__.py
+++ LHCbDIRAC/__init__.py
@@ -17,7 +17,7 @@ __path__ = extend_path( __path__, __name__ )
 majorVersion = 8
 minorVersion = 5
 patchLevel = 0
-preVersion = 2 
+preVersion = 3 
 
 version = "v%sr%s" % ( majorVersion, minorVersion )
 buildVersion = "v%dr%d" % ( majorVersion, minorVersion )
