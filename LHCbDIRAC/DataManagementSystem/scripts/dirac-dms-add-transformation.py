b8989bf0bce76e775595bd30fe906369355b75f0
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2017-01-06 09:30:42 +0100

    improve BK path wildcard

--- LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-add-transformation.py
+++ LHCbDIRAC/DataManagementSystem/scripts/dirac-dms-add-transformation.py
@@ -137,14 +137,14 @@ if __name__ == "__main__":
       DIRAC.exit( 2 )
     transBKQuery = bkQuery.getQueryDict()
     processingPass = transBKQuery.get( 'ProcessingPass', '' )
-    if processingPass.endswith( '...' ):
+    if '...' in processingPass:
       if listProcessingPasses:
         gLogger.notice( "List of processing passes for BK path", pluginScript.getOption( 'BKPath' ) )
       processingPasses = getProcessingPasses( bkQuery, depth = depth )
       if processingPasses:
         if not listProcessingPasses:
-          gLogger.notice( "Transformations will be launched for the following list of processing passes:\n" )
-        gLogger.notice( '\t'.join( [''] + processingPasses ) )
+          gLogger.notice( "Transformations will be launched for the following list of processing passes:" )
+        gLogger.notice( '\n'.join( [''] + processingPasses ) )
       else:
         gLogger.notice( "No processing passes matching the BK path" )
         DIRAC.exit( 0 )
