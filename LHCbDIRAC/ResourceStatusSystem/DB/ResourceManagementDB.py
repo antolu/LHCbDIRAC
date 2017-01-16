16edd77079e1d5d16638bfcfd080cf18c4735812
Stavros Moiras <stavros.miras@cern.ch>
2017-01-16 10:45:06 +0100

    Removed spaces

--- LHCbDIRAC/ResourceStatusSystem/DB/ResourceManagementDB.py
+++ LHCbDIRAC/ResourceStatusSystem/DB/ResourceManagementDB.py
@@ -5,13 +5,12 @@
 
 '''
 
-from DIRAC                                              import S_OK, S_ERROR
-from DIRAC.ResourceStatusSystem.DB.ResourceManagementDB import \
-  ResourceManagementDB as DIRACResourceManagementDB
+from DIRAC import S_OK, S_ERROR
+from DIRAC.ResourceStatusSystem.DB.ResourceManagementDB import ResourceManagementDB as DIRACResourceManagementDB
+from sqlalchemy.dialects.mysql import DOUBLE, INTEGER, TIMESTAMP, TINYINT, BIGINT
+from sqlalchemy import Table, Column, MetaData, String, DateTime, exc, Text, text, BLOB
 
-from sqlalchemy.dialects.mysql                     import DOUBLE, INTEGER, TIMESTAMP, TINYINT, BIGINT
-from sqlalchemy                                    import Table, Column, MetaData, String, \
-                                                          DateTime, exc, Text, text, BLOB
+__RCSID__ = "$Id$"
 
 class ResourceManagementDB( DIRACResourceManagementDB ):
   '''
@@ -27,7 +26,7 @@ class ResourceManagementDB( DIRACResourceManagementDB ):
 
   def createTables( self ):
 
-    EnvironmentCache         = Table( 'EnvironmentCache', self.metadata,
+    EnvironmentCache = Table( 'EnvironmentCache', self.metadata,
                               Column( 'DateEffective', DateTime, nullable = False ),
                               Column( 'LastCheckTime', DateTime, nullable = False ),
                               Column( 'SiteName', String( 64 ), nullable = False ),
@@ -36,7 +35,7 @@ class ResourceManagementDB( DIRACResourceManagementDB ):
                               Column( 'Arguments', String( 512 ), nullable = False ),
                               mysql_engine = 'InnoDB' )
 
-    HammerCloudTest         = Table( 'HammerCloudTest', self.metadata,
+    HammerCloudTest = Table( 'HammerCloudTest', self.metadata,
                               Column( 'Counter', INTEGER, nullable = False, server_default = '0' ),
                               Column( 'TestStatus', String( 16 ) ),
                               Column( 'TestID', INTEGER ),
@@ -50,7 +49,7 @@ class ResourceManagementDB( DIRACResourceManagementDB ):
                               Column( 'CounterTime', DateTime ),
                               mysql_engine = 'InnoDB' )
 
-    MonitoringTest         = Table( 'MonitoringTest', self.metadata,
+    MonitoringTest = Table( 'MonitoringTest', self.metadata,
                               Column( 'ServiceFlavour', String( 64 ), nullable = False ),
                               Column( 'ServiceURI', String( 128 ), nullable = False, primary_key = True ),
                               Column( 'LastCheckTime', DateTime, nullable = False ),
@@ -61,7 +60,7 @@ class ResourceManagementDB( DIRACResourceManagementDB ):
                               Column( 'MetricName', String( 128 ), nullable = False, primary_key = True ),
                               mysql_engine = 'InnoDB' )
 
-    JobAccountingCache      = Table( 'JobAccountingCache', self.metadata,
+    JobAccountingCache = Table( 'JobAccountingCache', self.metadata,
                               Column( 'Failed', DOUBLE, nullable = False, server_default = '0' ),
                               Column( 'Running', DOUBLE, nullable = False, server_default = '0' ),
                               Column( 'Done', DOUBLE, nullable = False, server_default = '0' ),
@@ -75,7 +74,7 @@ class ResourceManagementDB( DIRACResourceManagementDB ):
                               mysql_engine = 'InnoDB' )
 
 
-    PilotAccountingCache    = Table( 'PilotAccountingCache', self.metadata,
+    PilotAccountingCache = Table( 'PilotAccountingCache', self.metadata,
                               Column( 'Name', String( 64 ), nullable = False, primary_key = True ),
                               Column( 'LastCheckTime', DateTime, nullable = False ),
                               Column( 'Deleted', DOUBLE, nullable = False, server_default = '0' ),
@@ -87,7 +86,7 @@ class ResourceManagementDB( DIRACResourceManagementDB ):
 
     # TABLES THAT WILL EVENTUALLY BE DELETED
 
-    SLST1Service         = Table( 'SLST1Service', self.metadata,
+    SLST1Service = Table( 'SLST1Service', self.metadata,
                               Column( 'HostUptime', INTEGER ),
                               Column( 'Version', String( 32 ) ),
                               Column( 'ServiceUptime', INTEGER ),
@@ -98,7 +97,7 @@ class ResourceManagementDB( DIRACResourceManagementDB ):
                               Column( 'System', String( 32 ), nullable = False, primary_key = True ),
                               mysql_engine = 'InnoDB' )
 
-    SLSLogSE              = Table( 'SLSLogSE', self.metadata,
+    SLSLogSE = Table( 'SLSLogSE', self.metadata,
                               Column( 'DataPartitionTotal', BIGINT ),
                               Column( 'Name', String( 32 ), server_default = '0' ),
                               Column( 'TimeStamp', TIMESTAMP, server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')),
@@ -107,9 +106,7 @@ class ResourceManagementDB( DIRACResourceManagementDB ):
                               Column( 'Availability', TINYINT, nullable = False ),
                               mysql_engine = 'InnoDB' )
 
-
     # create tables
-
     try:
       self.metadata.create_all( self.engine )
     except exc.SQLAlchemyError as e:
