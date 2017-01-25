72aed10f5447a9994f99780441718add320e7a15
Federico Stagni <federico.stagni@cern.ch>
2017-01-25 17:51:22 +0100

    fix

--- tests/Jenkins/sourcingEnv.py
+++ tests/Jenkins/sourcingEnv.py
@@ -24,7 +24,7 @@ functsBahsrc = {}
 diracFunct = {}
 fp = open( 'bashrc', 'r' )
 for line in fp:
-  if line[0] == '#':
+  if line[0] == '#' or 'DIRACPLAT' in line:
     continue
   if '(' in line or '[' in line:
     try:
