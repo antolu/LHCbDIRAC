32f977f2dc00e3cf004d55013e197257a887edb0
Marco <marco.corvo@cern.ch>
2017-01-27 15:30:56 +0100

    More tests, added compression defaults to MC template

--- LHCbDIRAC/BookkeepingSystem/Client/test/mock_BookkeepingClient.py
+++ LHCbDIRAC/BookkeepingSystem/Client/test/mock_BookkeepingClient.py
@@ -113,6 +113,28 @@ class BookkeepingClientFake(object):
                           'ApplicationName', 'ApplicationVersion', 'Visible', 'Usable', 'ProcessingPass', 'ExtraPackages', 'DDDB', 'CONDDB', 'DQTag'],
                           'Records': [[ 998, '', '', 'x86_64-slc6-gcc49-opt', '', 'N',
                           'Stripping28', 'DaVinci', 'v41r3', 'Yes', 'Yes', 'Stripping28', 'AppConfig.v3r306', 'dddb-20150724', 'cond-20161011', '']]}}
+    elif stepID == {'StepId':997}:
+      return {'OK': True,
+             'Value' : { 'TotalRecords': 1,
+                          'ParameterNames' : ['StepId', 'OptionFiles', 'ExtraOptions', 'SystemConfig', 'mcTCK', 'isMulticore', 'StepName',
+                          'ApplicationName', 'ApplicationVersion', 'Visible', 'Usable', 'ProcessingPass', 'ExtraPackages', 'DDDB', 'CONDDB', 'DQTag'],
+                          'Records': [[ 997, '$APPCONFIGOPTS/Brunel/DataType-2016.py;$APPCONFIGOPTS/Brunel/PbPb-GECs.py', '', 'x86_64-slc6-gcc49-opt', '', 'N',
+                          'Stripping28', 'DaVinci', 'v41r3', 'Yes', 'Yes', 'Stripping28', 'AppConfig.v3r306', 'dddb-20150724', 'cond-20161011', '']]}}
+    elif stepID == {'StepId':996}:
+      return {'OK': True,
+             'Value' : { 'TotalRecords': 1,
+                          'ParameterNames' : ['StepId', 'OptionFiles', 'ExtraOptions', 'SystemConfig', 'mcTCK', 'isMulticore', 'StepName',
+                          'ApplicationName', 'ApplicationVersion', 'Visible', 'Usable', 'ProcessingPass', 'ExtraPackages', 'DDDB', 'CONDDB', 'DQTag'],
+                          'Records': [[ 996, '$APPCONFIGOPTS/Brunel/DataType-2016.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py;$APPCONFIGOPTS/Brunel/PbPb-GECs.py',
+                          '', 'x86_64-slc6-gcc49-opt', '', 'N', 'Stripping28', 'DaVinci', 'v41r3', 'Yes', 'Yes', 'Stripping28', 'AppConfig.v3r306', 'dddb-20150724', 'cond-20161011', '']]}}
+    elif stepID == {'StepId':1098}:
+      return {'OK': True,
+             'Value' : { 'TotalRecords': 1,
+                          'ParameterNames' : ['StepId', 'OptionFiles', 'ExtraOptions', 'SystemConfig', 'mcTCK', 'isMulticore', 'StepName',
+                          'ApplicationName', 'ApplicationVersion', 'Visible', 'Usable', 'ProcessingPass', 'ExtraPackages', 'DDDB', 'CONDDB', 'DQTag'],
+                          'Records': [[ 1098, '$APPCONFIGOPTS/Brunel/DataType-2016.py;$APPCONFIGOPTS/Persistency/Compression-ZLIB-1.py;$APPCONFIGOPTS/Persistency/Compression-LZMA-4.py',
+                          '', 'x86_64-slc6-gcc49-opt', '', 'N', 'Stripping28', 'DaVinci', 'v41r3', 'Yes', 'Yes', 'Stripping28', 'AppConfig.v3r306', 'dddb-20150724', 'cond-20161011', '']]}}
+
 
   def getStepInputFiles( self, stepID ):
     if stepID == 123:
@@ -141,7 +163,7 @@ class BookkeepingClientFake(object):
               'Value': {'TotalRecords': 7,
                         'ParameterNames': ['FileType', 'Visible'],
                         'Records': [['SIM', 'N']]}}
-    if stepID == 999 or stepID == 998:
+    if stepID == 999 or stepID == 998 or stepID == 997 or stepID == 996 or stepID == 1098:
       return {'OK': True,
               'Value': {'TotalRecords': 7,
                         'ParameterNames': ['FileType', 'Visible'],
@@ -174,7 +196,7 @@ class BookkeepingClientFake(object):
               'Value': {'TotalRecords': 7,
                         'ParameterNames': ['FileType', 'Visible'],
                         'Records': [['DIGI', 'N']]}}
-    if stepID == 999 or stepID == 998:
+    if stepID == 999 or stepID == 998 or stepID == 997 or stepID == 996 or stepID == 1098:
       return {'OK': True,
               'Value': {'TotalRecords': 7,
                         'ParameterNames': ['FileType', 'Visible'],
