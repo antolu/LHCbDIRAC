cd196dee2472b5932788bdb89b7ff07fb5e898de
Federico Stagni <federico.stagni@cern.ch>
2017-02-15 18:12:08 +0100

    minimal test

--- LHCbDIRAC/TransformationSystem/Agent/test/Test_TransformationCleaningAgent.py
+++ LHCbDIRAC/TransformationSystem/Agent/test/Test_TransformationCleaningAgent.py
@@ -15,15 +15,23 @@ class TransformationCleaningAgentTestCase( unittest.TestCase ):
     self.mockAM.am_getOption.return_value = S_OK()
     self.agent = importlib.import_module( 'LHCbDIRAC.TransformationSystem.Agent.TransformationCleaningAgent' )
     self.agent.AgentModule = self.mockAM
-    self.agent.DIRACMCExtensionAgent = self.mockAM
-    self.agent = TransformationCleaningAgent()
+    self.mockDTCAObj = MagicMock()
+    self.mockDTCAObj.getTransformationDirectories.return_value = {'OK':True, 'Value':['/this/is/SIM/dir',
+                                                                                      '/this/is/FOO/dir', '/this/is/BAR/dir']}
+    self.mockDTCA = MagicMock()
+    self.mockDTCA.return_value = self.mockDTCAObj
+    self.agent.DiracTCAgent = self.mockDTCA
+    self.agent = TransformationCleaningAgent('a', 'b')
     self.agent.log = gLogger
+    self.mockSUC = MagicMock()
+    self.mockSUC.getStorageDirectories.return_value = {'OK':True, 'Value':[]}
+    self.agent.storageUsageClient = self.mockSUC
 
   def test_getTransformationDirectories(self):
     res = self.agent.getTransformationDirectories(1)
-    print res
+    self.assertTrue(res['OK'])
 
 
 if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase( TransformationCleaningAgentTestCase )
-  testResult = unittest.TextTestResult(verbosity = 2).run(suite)
+  testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
