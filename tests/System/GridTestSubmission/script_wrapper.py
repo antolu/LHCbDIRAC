5e03b230f2ee87e6f517724404ec6a22783ee998
Federico Stagni <federico.stagni@cern.ch>
2017-02-17 15:52:09 +0100

    minor

--- tests/System/GridTestSubmission/script_wrapper.py
+++ tests/System/GridTestSubmission/script_wrapper.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 import os
 
-os.system('export DISPLAY="localhost:0.0" && root -l -q "runToys.C('')"')
+os.system('export DISPLAY="localhost:0.0" && root -l -q "runToys.C()"')
 ###INJECTEDCODE###
