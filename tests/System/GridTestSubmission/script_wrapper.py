1c4025b72677a138aab4274c141d33965a884852
Federico Stagni <federico.stagni@cern.ch>
2017-02-17 15:51:38 +0100

    minor

--- tests/System/GridTestSubmission/script_wrapper.py
+++ tests/System/GridTestSubmission/script_wrapper.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python
-import os, sys
-def formatVar(var):
-    try:
-        float(var)
-        return str(var)
-    except ValueError as v:
-        return '\"%s\"' % str(var)
+import os
 
-script_args = ''
-
-del sys.argv[sys.argv.index('script_wrapper.py')]
-script_args=[formatVar(v) for v in script_args]
-if script_args == []: script_args = ''
-os.system('export DISPLAY="localhoast:0.0" && root -l -q "runToys.C(%s)"' % script_args)
+os.system('export DISPLAY="localhost:0.0" && root -l -q "runToys.C('')"')
 ###INJECTEDCODE###
