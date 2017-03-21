2406da724ca022158c1a8807a3812172b36c1f0f
Federico Stagni <federico.stagni@cern.ch>
2017-03-21 18:43:09 +0100

    docs update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,7 +81,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'LHCbDIRAC'
-copyright = u'2013, LHCbDIRAC Project'
+copyright = u'%s, LHCbDIRAC Project' % datetime.datetime.utcnow().year
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
