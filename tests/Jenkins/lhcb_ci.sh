f4355dc7ee8d7bcea73632b9bbf664b6e2f92ea4
Zoltan Mathe <zoltan.mathe@cern.ch>
2017-05-05 11:09:46 +0200

    fixes

--- tests/Jenkins/lhcb_ci.sh
+++ tests/Jenkins/lhcb_ci.sh
@@ -378,8 +378,8 @@ function sourcingEnv(){
 }
 
 function setupBKKDB(){
-  echo -e "==> Seting up the Bookkeeping Database"
-  python $TESTCODE/LHCbDIRAC/tests/Jenkins/dirac-bkk-cfg-update.py -p VAR_DB_Password $DEBUG 
+  echo -e "==> Setting up the Bookkeeping Database"
+  python $TESTCODE/LHCbDIRAC/tests/Jenkins/dirac-bkk-cfg-update.py -p VAR_ORACLEDB_Password $DEBUG 
 }
 	
 #-------------------------------------------------------------------------------
