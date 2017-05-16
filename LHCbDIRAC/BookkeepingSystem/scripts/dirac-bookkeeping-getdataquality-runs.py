4f4b88dc7a25a78ef93b1c316b91c45dbb0423e3
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2017-05-16 17:37:32 +0200

    better output

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-getdataquality-runs.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-getdataquality-runs.py
@@ -11,27 +11,28 @@ import DIRAC
 from DIRAC.Core.Base import Script
 
 
-Script.setUsageMessage('\n'.join([ __doc__.split('\n')[1],
+Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
                                      'Usage:',
                                      '  %s [option|cfgfile] ... Run ...' % Script.scriptName,
                                      'Arguments:',
-                                     '  Run:      Run number' ]))
-Script.parseCommandLine(ignoreErrors=True)
-ids = Script.getPositionalArgs()
+                                     '  Run:      Run number' ] ) )
+Script.parseCommandLine( ignoreErrors = True )
+runSet = set( int( id ) for arg in Script.getPositionalArgs() for id in arg.split( ',' ) )
 
-if len(ids) < 1:
+if not runSet:
   Script.showHelp()
+  DIRAC.exit()
 
 from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient import BookkeepingClient
 cl = BookkeepingClient()
-for i in ids:
-  retVal = cl.getRunFilesDataQuality(int(i))
+print "-----------------------------------"
+print "Run Number".ljust( 15 ) + "Stream".ljust( 10 ) + "Flag".ljust( 10 )
+print "-----------------------------------"
+for runId in sorted( runSet ):
+  retVal = cl.getRunFilesDataQuality( runId )
   if retVal['OK']:
-    print "-----------------------------------"
-    print "Run Number".ljust(20) + "Flag".ljust(10)
-    print "-----------------------------------"
-    for i in  retVal["Value"]:
-      print str(i[0]).ljust(20) + str(i[1]).ljust(10)
+    for run, stream, flag in sorted( ( run, stream, flag ) for run, flag, stream in retVal["Value"] ):
+      print str( run ).ljust( 15 ) + str( stream ).ljust( 10 ) + str( flag ).ljust( 10 )
     print "-----------------------------------"
   else:
     print retVal["Message"]
