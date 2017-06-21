fad9d071822b1bc5c0371d38b4507dbbfc18231a
Federico Stagni <federico.stagni@cern.ch>
2017-06-21 11:11:05 +0200

    just updating the reco17 step

--- tests/Integration/Core/Test_RunApplication.py
+++ tests/Integration/Core/Test_RunApplication.py
@@ -268,24 +268,24 @@ class BrunelSuccess( RunApplicationTestCase ):
   def test_Brunel_new_gcc62( self ):
     """ Using ProdConf (production style). systemConfig = 'x86_64-slc6-gcc62-opt'
 
-        This is taken from step 130998 (Reco17-commissioning)
+        This is taken from step 131287 (Reco17)
     """
-    gLogger.always("**** Brunel v52r2")
+    gLogger.always("**** Brunel v52r4")
 
     ra = RunApplication()
     ra.applicationName = 'Brunel'
-    ra.applicationVersion = 'v52r2'
+    ra.applicationVersion = 'v52r4'
     ra.systemConfig = 'x86_64-slc6-gcc62-opt'
-    ra.commandOptions = ['$APPCONFIGOPTS/Brunel/DataType-2016.py',
+    ra.commandOptions = ['$APPCONFIGOPTS/Brunel/DataType-2017.py',
                          '$APPCONFIGOPTS/Brunel/rdst.py']
-    ra.extraPackages = [('AppConfig', 'v3r284'),
+    ra.extraPackages = [('AppConfig', 'v3r323'),
                         ('SQLDDDB', 'v7r10'),
                         ('ProdConf', '')
                        ]
     ra.step_Number = 1
-    ra.prodConfFileName = find_all('test_prodConf_brunel_v52r2.py', '..')[0]
-    ra.applicationLog = '0brunel_000v52r2_62_log.txt'
-    ra.stdError = '0brunel_000v52r2_62_err.txt'
+    ra.prodConfFileName = find_all('test_prodConf_brunel_v52r4.py', '..')[0]
+    ra.applicationLog = '0brunel_000v52r4_62_log.txt'
+    ra.stdError = '0brunel_000v52r4_62_err.txt'
 
     res = ra.run()
     self.assertFalse(res['OK']) #This will fail as there's no input file
@@ -294,23 +294,23 @@ class BrunelSuccess( RunApplicationTestCase ):
   def test_Brunel_new_bestCMT( self ):
     """ Using ProdConf (production style). systemConfig = 'ANY'
 
-        This is taken from step 130998 (Reco17-commissioning)
+        This is taken from step 131287 (Reco17)
     """
-    gLogger.always("**** Brunel v52r2")
+    gLogger.always("**** Brunel v52r4")
 
     ra = RunApplication()
     ra.applicationName = 'Brunel'
-    ra.applicationVersion = 'v52r2'
-    ra.commandOptions = ['$APPCONFIGOPTS/Brunel/DataType-2016.py',
+    ra.applicationVersion = 'v52r4'
+    ra.commandOptions = ['$APPCONFIGOPTS/Brunel/DataType-2017.py',
                          '$APPCONFIGOPTS/Brunel/rdst.py']
-    ra.extraPackages = [('AppConfig', 'v3r284'),
+    ra.extraPackages = [('AppConfig', 'v3r323'),
                         ('SQLDDDB', 'v7r10'),
                         ('ProdConf', '')
                        ]
     ra.step_Number = 1
-    ra.prodConfFileName = find_all('test_prodConf_brunel_v52r2.py', '..')[0]
-    ra.applicationLog = '0brunel_000v52r2_ANY_62_log.txt'
-    ra.stdError = '0brunel_000v52r2_ANY_62_err.txt'
+    ra.prodConfFileName = find_all('test_prodConf_brunel_v52r4.py', '..')[0]
+    ra.applicationLog = '0brunel_000v52r4_ANY_62_log.txt'
+    ra.stdError = '0brunel_000v52r4_ANY_62_err.txt'
 
     res = ra.run()
     self.assertFalse(res['OK']) #This will fail as there's no input file
