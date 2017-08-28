e326e0374165bdbf8b7ed8c62a5087819feea727
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2017-08-28 09:56:15 +0200

    Allow to flush all active runs in a production

--- LHCbDIRAC/TransformationSystem/scripts/dirac-transformation-flush-runs.py
+++ LHCbDIRAC/TransformationSystem/scripts/dirac-transformation-flush-runs.py
@@ -10,6 +10,7 @@ from DIRAC.Core.Base import Script
 if __name__ == "__main__":
   Script.registerSwitch( '', 'Runs=', '   list of runs to flush (comma separated, ranges r1:r2)' )
   Script.registerSwitch( '', 'NoAction', '   No action taken, just give stats' )
+  Script.registerSwitch( '', 'Active', '   If used, selects all active runs' )
   Script.setUsageMessage( '\n'.join( [ __doc__,
                                        'Usage:',
                                        '  %s [option|cfgfile] <TransID>[,<TransID2>...]' % Script.scriptName, ] ) )
@@ -18,9 +19,12 @@ if __name__ == "__main__":
   args = Script.getPositionalArgs()
   action = True
   runList = []
+  active = False
   for switch, val in Script.getUnprocessedSwitches():
     if switch == 'NoAction':
       action = False
+    elif switch == 'Active':
+      active = True
     elif switch == 'Runs':
       try:
         runs = val.split( ',' )
@@ -61,36 +65,43 @@ if __name__ == "__main__":
     runs.sort( cmp = ( lambda r1, r2: int( r1['RunNumber'] - r2['RunNumber'] ) ) )
 
     if not runList:
-      res = transClient.getBookkeepingQuery( transID )
-      if not res['OK']:
-        gLogger.fatal( "Error getting BK query for transformation %s" % transID, res['Message'] )
-        DIRAC.exit( 1 )
-      queryProd = res['Value'].get( 'ProductionID' )
-      if not queryProd:
-        gLogger.fatal( "Transformation is not based on another one" )
-        DIRAC.exit( 0 )
-
-      res = transClient.getTransformationRuns( {'TransformationID':queryProd} )
-      if not res['OK']:
-        gLogger.fatal( "Error getting runs for transformation %s" % queryProd, res['Message'] )
-        DIRAC.exit( 1 )
-      queryRuns = res['Value']
-      flushedRuns = [run['RunNumber'] for run in queryRuns if run['Status'] == 'Flush']
-
-      toBeFlushed = []
-      for run in [run['RunNumber'] for run in runs if run['Status'] != 'Flush' and run['RunNumber'] in flushedRuns]:
-        missing = -1
-        res = transClient.getTransformationFiles( {'TransformationID': queryProd, 'RunNumber': run} )
+      if active:
+        # Flush all Active runs in that transformation
+        toBeFlushed = [run['RunNumber'] for run in runs if run['Status'] != 'Flush']
+      else:
+        # Get transformationID used in the BK query, to find out which runs are flushed in there
+        res = transClient.getBookkeepingQuery( transID )
         if not res['OK']:
-          gLogger.fatal( "Error getting files for run %d" % run, res['Message'] )
-        else:
-          runFiles = res['Value']
-          missing = 0
-          for rFile in runFiles:
-            if rFile['Status'] in ( 'Unused', 'Assigned', 'MaxReset' ):
-              missing += 1
-          if not missing:
-            toBeFlushed.append( run )
+          gLogger.fatal( "Error getting BK query for transformation %s" % transID, res['Message'] )
+          DIRAC.exit( 1 )
+        queryProd = res['Value'].get( 'ProductionID' )
+        if not queryProd:
+          gLogger.fatal( "Transformation is not based on another one" )
+          DIRAC.exit( 0 )
+
+        res = transClient.getTransformationRuns( {'TransformationID':queryProd} )
+        if not res['OK']:
+          gLogger.fatal( "Error getting runs for transformation %s" % queryProd, res['Message'] )
+          DIRAC.exit( 1 )
+        queryRuns = res['Value']
+        # Get the list of runs flushed in the parent production
+        flushedRuns = [run['RunNumber'] for run in queryRuns if run['Status'] == 'Flush']
+
+        toBeFlushed = []
+        # Get runs not flushed in current production but flushed in the parent production
+        for run in [run['RunNumber'] for run in runs if run['Status'] != 'Flush' and run['RunNumber'] in flushedRuns]:
+          missing = -1
+          res = transClient.getTransformationFiles( {'TransformationID': queryProd, 'RunNumber': run} )
+          if not res['OK']:
+            gLogger.fatal( "Error getting files for run %d" % run, res['Message'] )
+          else:
+            runFiles = res['Value']
+            missing = 0
+            for rFile in runFiles:
+              if rFile['Status'] in ( 'Unused', 'Assigned', 'MaxReset' ):
+                missing += 1
+            if not missing:
+              toBeFlushed.append( run )
 
     else:
       toBeFlushed = sorted( set( runList ) & set( [run['RunNumber'] for run in runs] ) )
