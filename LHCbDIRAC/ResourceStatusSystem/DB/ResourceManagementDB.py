f5dfa8a509b7fac7b9697b18fb5e51c1a072a64c
Federico Stagni <federico.stagni@cern.ch>
2017-09-21 12:23:05 +0200

    moved some columns from DOUBLE to INTEGER

--- LHCbDIRAC/ResourceStatusSystem/DB/ResourceManagementDB.py
+++ LHCbDIRAC/ResourceStatusSystem/DB/ResourceManagementDB.py
@@ -15,7 +15,9 @@
 __RCSID__ = "$Id$"
 
 
-from sqlalchemy.dialects.mysql import DOUBLE, INTEGER, TIMESTAMP, TINYINT, BIGINT
+import datetime
+
+from sqlalchemy.dialects.mysql import INTEGER, TIMESTAMP, TINYINT, BIGINT
 from sqlalchemy import Column, String, DateTime, Text, text, BLOB
 
 from DIRAC.ResourceStatusSystem.DB.ResourceManagementDB import rmsBase, TABLESLIST
@@ -97,13 +99,15 @@ class MonitoringTest(rmsBase):
     Fill the fields of the MonitoringTest object from a dictionary
     """
 
+    utcnow = self.lastchecktime if self.lastchecktime else datetime.datetime.utcnow().replace(microsecond = 0)
+
     self.serviceuri = dictionary.get( 'ServiceURI', self.serviceuri )
     self.metricname = dictionary.get( 'MetricName', self.metricname )
     self.serviceflavour = dictionary.get( 'ServiceFlavour', self.serviceflavour )
     self.lastchecktime = dictionary.get( 'LastCheckTime', self.lastchecktime )
     self.metricstatus = dictionary.get( 'MetricStatus', self.metricstatus )
     self.sitename = dictionary.get( 'SiteName', self.sitename )
-    self.timestamp = dictionary.get( 'TimeStamp', self.timestamp )
+    self.timestamp = dictionary.get( 'TimeStamp', utcnow )
     self.summarydata = dictionary.get( 'SummaryData', self.summarydata )
 
   def toList(self):
@@ -123,20 +127,22 @@ class JobAccountingCache(rmsBase):
                     'mysql_charset': 'utf8'}
 
   name = Column( 'Name', String( 64 ), nullable = False, primary_key = True )
-  failed = Column( 'Failed', DOUBLE, nullable = False, server_default = '0' )
-  running = Column( 'Running', DOUBLE, nullable = False, server_default = '0' )
-  done = Column( 'Done', DOUBLE, nullable = False, server_default = '0' )
-  stalled = Column( 'Stalled', DOUBLE, nullable = False, server_default = '0' )
-  checking = Column( 'Checking', DOUBLE, nullable = False, server_default = '0' )
-  completed = Column( 'Completed', DOUBLE, nullable = False, server_default = '0' )
-  killed = Column( 'Killed', DOUBLE, nullable = False )
-  matched = Column( 'Matched', DOUBLE, nullable = False, server_default = '0' )
+  failed = Column( 'Failed', INTEGER, nullable = False, server_default = '0' )
+  running = Column( 'Running', INTEGER, nullable = False, server_default = '0' )
+  done = Column( 'Done', INTEGER, nullable = False, server_default = '0' )
+  stalled = Column( 'Stalled', INTEGER, nullable = False, server_default = '0' )
+  checking = Column( 'Checking', INTEGER, nullable = False, server_default = '0' )
+  completed = Column( 'Completed', INTEGER, nullable = False, server_default = '0' )
+  killed = Column( 'Killed', INTEGER, nullable = False, server_default = '0' )
+  matched = Column( 'Matched', INTEGER, nullable = False, server_default = '0' )
   lastchecktime = Column( 'LastCheckTime', DateTime, nullable = False )
 
   def fromDict( self, dictionary ):
     """
     Fill the fields of the JobAccountingCache object from a dictionary
     """
+
+    utcnow = self.lastchecktime if self.lastchecktime else datetime.datetime.utcnow().replace(microsecond = 0)
 
     self.name = dictionary.get( 'Name', self.name )
     self.failed = dictionary.get( 'Failed', self.failed )
@@ -147,7 +153,7 @@ class JobAccountingCache(rmsBase):
     self.completed = dictionary.get( 'Completed', self.completed )
     self.killed = dictionary.get( 'Killed', self.killed )
     self.matched = dictionary.get( 'Matched', self.matched )
-    self.lastchecktime = dictionary.get( 'LastCheckTime', self.lastchecktime )
+    self.lastchecktime = dictionary.get( 'LastCheckTime', utcnow )
 
 
   def toList(self):
@@ -166,10 +172,10 @@ class PilotAccountingCache(rmsBase):
                     'mysql_charset': 'utf8'}
 
   name = Column( 'Name', String( 64 ), nullable = False, primary_key = True )
-  failed = Column( 'Failed', DOUBLE, nullable = False, server_default = '0' )
-  deleted = Column( 'Deleted', DOUBLE, nullable = False, server_default = '0' )
-  done = Column( 'Done', DOUBLE, nullable = False, server_default = '0' )
-  aborted = Column( 'Aborted', DOUBLE, nullable = False, server_default = '0' )
+  failed = Column( 'Failed', INTEGER, nullable = False, server_default = '0' )
+  deleted = Column( 'Deleted', INTEGER, nullable = False, server_default = '0' )
+  done = Column( 'Done', INTEGER, nullable = False, server_default = '0' )
+  aborted = Column( 'Aborted', INTEGER, nullable = False, server_default = '0' )
   lastchecktime = Column( 'LastCheckTime', DateTime, nullable = False )
 
   def fromDict( self, dictionary ):
@@ -177,12 +183,14 @@ class PilotAccountingCache(rmsBase):
     Fill the fields of the PilotAccountingCache object from a dictionary
     """
 
+    utcnow = self.lastchecktime if self.lastchecktime else datetime.datetime.utcnow().replace(microsecond = 0)
+
     self.name = dictionary.get( 'Name', self.name )
     self.failed = dictionary.get( 'Failed', self.failed )
     self.deleted = dictionary.get( 'Deleted', self.deleted )
     self.done = dictionary.get( 'Done', self.done )
     self.aborted = dictionary.get( 'Aborted', self.aborted )
-    self.lastchecktime = dictionary.get( 'LastCheckTime', self.lastchecktime )
+    self.lastchecktime = dictionary.get( 'LastCheckTime', utcnow )
 
 
   def toList(self):
@@ -218,12 +226,14 @@ class SLST1Service(rmsBase):
     Fill the fields of the SLST1Service object from a dictionary
     """
 
+    utcnow = self.lastchecktime if self.lastchecktime else datetime.datetime.utcnow().replace(microsecond = 0)
+
     self.site = dictionary.get( 'Site', self.site )
     self.system = dictionary.get( 'System', self.system )
     self.hostuptime = dictionary.get( 'HostUpTime', self.hostuptime )
     self.version = dictionary.get( 'Version', self.version )
     self.serviceuptime = dictionary.get( 'ServiceUpTime', self.serviceuptime )
-    self.timestamp = dictionary.get( 'TimeStamp', self.timestamp )
+    self.timestamp = dictionary.get( 'TimeStamp', utcnow )
     self.message = dictionary.get( 'Message', self.message )
     self.availability = dictionary.get( 'Availability', self.availability )
 
--- tests/Integration/ResourceStatusSystem/Test_ResourceManagement.py
+++ tests/Integration/ResourceStatusSystem/Test_ResourceManagement.py
@@ -2,99 +2,115 @@
     ResourceManagementClient -> ResourceManagementHandler -> ResourceManagementDB
     It supposes that the DB is present, and that the service is running
 
-    this is pytest!
 '''
 
+import unittest
 import datetime
+
 from DIRAC.Core.Base.Script import parseCommandLine
 parseCommandLine()
 
 from DIRAC import gLogger
-from DIRAC.tests.Integration.ResourceStatusSystem.Test_ResourceManagement import test_addAndRemove as ResourceManagementClientChain
+from DIRAC.tests.Integration.ResourceStatusSystem.Test_ResourceManagement import ResourceManagementClientChain as DIRACResourceManagementClientChain
 from LHCbDIRAC.ResourceStatusSystem.Client.ResourceManagementClient import ResourceManagementClient
 
 gLogger.setLevel('DEBUG')
 
-ResourceManagementClientChain()
+# DIRACResourceManagementClientChain()
 
 rsClient = ResourceManagementClient()
 
 Datetime = datetime.datetime.now()
 
-def test_addAndRemove():
+
+class LHCbResourceManagementTestCase( unittest.TestCase ):
+
+  def setUp( self ):
+    self.rmClient = ResourceManagementClient()
+
+  def tearDown( self ):
+    pass
+
+
+class LHCbResourceManagementClientChain( LHCbResourceManagementTestCase, DIRACResourceManagementClientChain ):
+
+  def test_addAndRemove(self):
+
+
+    # TEST addOrModifyMonitoringTest
+    # ...............................................................................
+
+    res = rsClient.addOrModifyMonitoringTest('TestName1234', 'serviceURI', 'siteName', 'serviceFlavour',
+                                             'metricStatus', 'summaryData', Datetime, Datetime)
+    self.assertTrue(res['OK'])
+
+    res = rsClient.selectMonitoringTest('TestName1234')
+    self.assertTrue(res['OK'])
+    #check if the name that we got is equal to the previously added 'TestName1234'
+    self.assertEqual(res['Value'][0][1],'TestName1234')
+
+
+    # TEST deleteMonitoringTest
+    # ...............................................................................
+
+    res = rsClient.deleteMonitoringTest('TestName1234')
+    self.assertTrue(res['OK'])
+
+    res = rsClient.selectMonitoringTest('TestName1234')
+    self.assertTrue(res['OK'])
+    self.assertFalse(res['Value'])
+
+
+    # TEST addOrModifyJobAccountingCache
+    # ...............................................................................
+
+    res = rsClient.addOrModifyJobAccountingCache('TestName1234')
+    self.assertTrue(res['OK'])
+
+    res = rsClient.selectJobAccountingCache('TestName1234')
+    self.assertTrue(res['OK'])
+    #check if the name that we got is equal to the previously added 'TestName1234'
+    self.assertEqual(res['Value'][0][0],'TestName1234')
+
+
+    # TEST deleteJobAccountingCache
+    # ...............................................................................
+
+    res = rsClient.deleteJobAccountingCache('TestName1234')
+    self.assertTrue(res['OK'])
+
+    res = rsClient.selectJobAccountingCache('TestName1234')
+    self.assertTrue(res['OK'])
+    self.assertFalse(res['Value'])
 
 
 
-  # TEST addOrModifyMonitoringTest
-  # ...............................................................................
+    # TEST addOrModifyPilotAccountingCache
+    # ...............................................................................
 
-  res = rsClient.addOrModifyMonitoringTest('TestName1234', 'serviceURI', 'siteName', 'serviceFlavour',
-                                         'metricStatus', 'summaryData', Datetime, Datetime)
-  assert res['OK'] == True
+    res = rsClient.addOrModifyPilotAccountingCache('TestName1234')
+    self.assertTrue(res['OK'])
 
-  res = rsClient.selectMonitoringTest('TestName1234')
-  assert res['OK'] == True
-  #check if the name that we got is equal to the previously added 'TestName1234'
-  assert res['Value'][0][7] == 'TestName1234'
-
-
-  # TEST deleteMonitoringTest
-  # ...............................................................................
-
-  res = rsClient.deleteMonitoringTest('TestName1234')
-  assert res['OK'] == True
-
-  res = rsClient.selectMonitoringTest('TestName1234')
-  assert res['OK'] == True
-  assert not res['Value']
+    res = rsClient.selectPilotAccountingCache('TestName1234')
+    self.assertTrue(res['OK'])
+    #check if the name that we got is equal to the previously added 'TestName1234'
+    self.assertEqual(res['Value'][0][0],'TestName1234')
 
 
-  # TEST addOrModifyJobAccountingCache
-  # ...............................................................................
+    # TEST deletePilotAccountingCache
+    # ...............................................................................
 
-  res = rsClient.addOrModifyJobAccountingCache('TestName1234')
+    res = rsClient.deletePilotAccountingCache('TestName1234')
+    self.assertTrue(res['OK'])
 
-  assert res['OK'] == True
+    res = rsClient.selectPilotAccountingCache('TestName1234')
+    self.assertTrue(res['OK'])
+    self.assertFalse(res['Value'])
 
-  res = rsClient.selectJobAccountingCache('TestName1234')
-  assert res['OK'] == True
-  #check if the name that we got is equal to the previously added 'TestName1234'
-  assert res['Value'][0][3] == 'TestName1234'
-
-
-  # TEST deleteJobAccountingCache
-  # ...............................................................................
-
-  res = rsClient.deleteJobAccountingCache('TestName1234')
-  assert res['OK'] == True
-
-  res = rsClient.selectJobAccountingCache('TestName1234')
-  assert res['OK'] == True
-  assert not res['Value']
-
-
-
-  # TEST addOrModifyPilotAccountingCache
-  # ...............................................................................
-
-  res = rsClient.addOrModifyPilotAccountingCache('TestName1234')
-  assert res['OK'] == True
-
-  res = rsClient.selectPilotAccountingCache('TestName1234')
-  assert res['OK'] == True
-  #check if the name that we got is equal to the previously added 'TestName1234'
-  assert res['Value'][0][0] == 'TestName1234'
-
-
-  # TEST deletePilotAccountingCache
-  # ...............................................................................
-
-  res = rsClient.deletePilotAccountingCache('TestName1234')
-  assert res['OK'] == True
-
-  res = rsClient.selectPilotAccountingCache('TestName1234')
-  assert res['OK'] == True
-  assert not res['Value']
+if __name__ == '__main__':
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase( LHCbResourceManagementTestCase )
+  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( LHCbResourceManagementClientChain ) )
+  testResult = unittest.TextTestRunner( verbosity = 2 ).run( suite )
 
 
 # EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF
