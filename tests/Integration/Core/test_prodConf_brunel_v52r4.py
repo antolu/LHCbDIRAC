47ae5d6e2ba8fdcdcb766a066baf67165adf8ef4
Federico Stagni <federico.stagni@cern.ch>
2017-10-03 19:09:32 +0200

    brunel v52r4

--- tests/Integration/Core/test_prodConf_brunel_v52r4.py
+++ tests/Integration/Core/test_prodConf_brunel_v52r4.py
@@ -7,9 +7,9 @@ ProdConf(
   NOfEvents=-1,
   DDDBTag='dddb-20150724',
   CondDBTag='cond-20161004',
-  AppVersion='v52r2',
-  XMLSummaryFile='summaryDaVinci_0brunel_000v52r2_1.xml',
+  AppVersion='v52r4',
+  XMLSummaryFile='summaryDaVinci_0brunel_000v52r4_1.xml',
   Application='Brunel',
-  OutputFilePrefix='0brunel_000v52r2',
+  OutputFilePrefix='0brunel_000v52r4',
   XMLFileCatalog='pool_xml_catalog.xml',
 )
