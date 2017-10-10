f457fa9ac4ae28d3d08ac578be81362a2e867b86
Federico Stagni <federico.stagni@cern.ch>
2017-10-10 15:50:18 +0200

    fix

--- tests/Jenkins/lhcb_ci.sh
+++ tests/Jenkins/lhcb_ci.sh
@@ -156,7 +156,7 @@ diracServices(){
     then
       # start BKK DB setup
       setupBKKDB
-      source $SERVERINSTALLDIR/scripts/install_oracle-client.sh
+      . $SERVERINSTALLDIR/scripts/install_oracle-client.sh
       # end BKK DB setup
     fi
 
