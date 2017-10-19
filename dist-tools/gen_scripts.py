903518d825d6b7b0b81a162cee04e6c735f4d9df
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2017-10-19 10:31:23 +0200

    make scripts symlinks relative to the scripts directory

--- dist-tools/gen_scripts.py
+++ dist-tools/gen_scripts.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
+"""
+Create symlinks to all teh scripts found in the LHCbDIRAC tree into a single directory
+"""
 
 import os
-import shutil
 import logging
 
 log = logging.getLogger()
 
-prjroot='.'
+prjroot = '.'
 scripts_dir = os.path.join( prjroot, 'scripts' )
 
 
@@ -15,15 +17,19 @@ if not os.path.isdir( scripts_dir ):
 
 for root, dirs, files in os.walk( os.path.join( prjroot, 'LHCbDIRAC' ) ):
   if 'scripts' in dirs:
+    # Print directory as it contains the script directory
     log.info( '  - %s', root )
-        # we are only interested in the content of the scripts directories
-    dirs[:] = ['scripts']
   elif os.path.basename( root ) == 'scripts':
-    dirs[:] = []  # avoid further recursion (it should not be needed)
+    # Loop over the files in the scripts directory
     for f in files:
-      if f.endswith( '.py' ):
-        dst = os.path.join( scripts_dir, f[:-3] )
-      else:
-        dst = os.path.join( scripts_dir, f )
-      shutil.copyfile( os.path.join( root, f ), dst )
+      # Remove extension if it exists and replace underscore with dash
+      f = os.path.splitext( f )[0].replace( '_', '-' )
+      dst = os.path.join( scripts_dir, f )
+      # Only create a symlink if it doesn't exist or it is broken
+      if not os.path.exists( dst ):
+        # If the link however exists but is broken, remove it
+        if os.path.lexists( dst ):
+          os.remove( dst )
+        # Make a link relative to scripts_dir in order to be portable
+        os.symlink( os.path.relpath( os.path.join( root, f ), scripts_dir ), dst )
       os.chmod( dst, 0755 )  # ensure that the new file is executable
