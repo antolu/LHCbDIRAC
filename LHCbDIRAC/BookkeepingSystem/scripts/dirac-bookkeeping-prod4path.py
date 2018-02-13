f77bc7a865568918e182f018aab4422d552701fe
Philippe Charpentier <Philippe.Charpentier@cern.ch>
2018-02-13 09:56:09 +0100

    Remove addLevelDefaultValue

--- LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-prod4path.py
+++ LHCbDIRAC/BookkeepingSystem/scripts/dirac-bookkeeping-prod4path.py
@@ -11,13 +11,15 @@ from DIRAC import gLogger, exit
 from LHCbDIRAC.DataManagementSystem.Client.DMScript import DMScript
 from LHCbDIRAC.TransformationSystem.Client.TransformationClient import TransformationClient
 
-def printProds( title, prods ):
+
+def printProds(title, prods):
   typeDict = {}
   for prod, prodType in prods.iteritems():
-    typeDict.setdefault( prodType, [] ).append( prod )
-  gLogger.notice( title )
+    typeDict.setdefault(prodType, []).append(prod)
+  gLogger.notice(title)
   for prodType, prodList in typeDict.iteritems():
-    gLogger.notice( '(%s): %s' % ( prodType, ','.join( [str( prod ) for prod in sorted( prodList )] ) ) )
+    gLogger.notice('(%s): %s' % (prodType, ','.join([str(prod) for prod in sorted(prodList)])))
+
 
 def execute():
   tr = TransformationClient()
@@ -27,8 +29,8 @@ def execute():
 
   bkQuery = dmScript.getBKQuery()
   if not bkQuery:
-    gLogger.notice( "No BKQuery given..." )
-    exit( 1 )
+    gLogger.notice("No BKQuery given...")
+    exit(1)
 
   startTime = time.time()
   prods = bkQuery.getBKProductions()  # visible = 'All' )
@@ -36,32 +38,33 @@ def execute():
   parents = {}
   productions = {}
   for prod in prods:
-    type = tr.getTransformation( prod ).get( 'Value', {} ).get( 'Type', 'Unknown' )
+    type = tr.getTransformation(prod).get('Value', {}).get('Type', 'Unknown')
     productions[prod] = type
-    parent = tr.getBookkeepingQuery( prod ).get( 'Value', {} ).get( 'ProductionID', '' )
+    parent = tr.getBookkeepingQuery(prod).get('Value', {}).get('ProductionID', '')
     if parent:
-      type = tr.getTransformation( parent ).get( 'Value', {} ).get( 'Type', 'Unknown' )
+      type = tr.getTransformation(parent).get('Value', {}).get('Type', 'Unknown')
       parents[parent] = type
 
-  gLogger.notice( "For BK path %s:" % bkQuery.getPath() )
+  gLogger.notice("For BK path %s:" % bkQuery.getPath())
   if not prods:
-    gLogger.notice( 'No productions found!' )
+    gLogger.notice('No productions found!')
   else:
-    printProds( 'Productions found', productions )
+    printProds('Productions found', productions)
     if parents:
-      printProds( 'Parent productions', parents )
+      printProds('Parent productions', parents)
 
-  gLogger.notice( 'Completed in %.1f seconds' % ( time.time() - startTime ) )
+  gLogger.notice('Completed in %.1f seconds' % (time.time() - startTime))
+
+
 if __name__ == "__main__":
 
   dmScript = DMScript()
   dmScript.registerBKSwitches()
 
-  Script.setUsageMessage( '\n'.join( [ __doc__.split( '\n' )[1],
-                                       'Usage:',
-                                       '  %s [option|cfgfile]' % Script.scriptName, ] ) )
+  Script.setUsageMessage('\n'.join([__doc__.split('\n')[1],
+                                    'Usage:',
+                                    '  %s [option|cfgfile]' % Script.scriptName, ]))
 
-  Script.addDefaultOptionValue( 'LogLevel', 'error' )
-  Script.parseCommandLine( ignoreErrors = False )
+  Script.parseCommandLine(ignoreErrors=False)
 
   execute()
