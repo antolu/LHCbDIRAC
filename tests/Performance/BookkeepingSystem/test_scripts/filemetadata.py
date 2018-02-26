aa87fa862361cab92280e9531e3fb973af06ca48
Zoltan Mathe <zoltan.mathe@cern.ch>
2018-02-26 15:29:59 +0100

    add more tests

---
+++ tests/Performance/BookkeepingSystem/test_scripts/filemetadata.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import time
+import random
+
+from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient import BookkeepingClient
+from DIRAC.Core.Utilities.List import sortList
+from DIRAC.Core.Base import Script
+Script.parseCommandLine(ignoreErrors=True)
+
+bk = BookkeepingClient()
+
+lfns = []
+with open('testfiles.txt') as f:
+  for i in f:
+    lfns += [i.strip()]
+
+
+class Transaction(object):
+
+  def __init__(self):
+    self.custom_timers = {}
+
+  def run(self):
+    nb = random.randint(0, len(lfns) - 1)
+    start_time = time.time()
+    retVal = bk.getFileMetadata(lfns[:nb])
+    if not retVal['OK']:
+      print retVal['Message']
+    end_time = time.time()
+
+    self.custom_timers['Bkk_ResponseTime'] = end_time - start_time
+
+
+if __name__ == '__main__':
+  trans = Transaction()
+  trans.run()
+  print trans.custom_timers
