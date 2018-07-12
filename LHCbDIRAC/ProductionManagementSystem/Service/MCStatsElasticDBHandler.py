5673ad456166abbdf5ca800cc940d26c6b5d3e92
lsheikmo <lara.sheik.mohammadi@cern.ch>
2018-07-12 10:08:34 +0200

    Add Handler to MCStatsElasticDB

---
+++ LHCbDIRAC/ProductionManagementSystem/Service/MCStatsElasticDBHandler.py
@@ -0,0 +1,36 @@
+import threading # DO i need this??
+
+from DIRAC import gLogger, S_OK, S_ERROR
+from DIRAC.Core.DISET.RequestHandler import RequestHandler
+from LHCbDIRAC.ProductionManagementSystem.DB.MCStatsElasticDB import MCStatsElasticDB
+
+__RCSID__ = "$Id$"
+
+MCStatsElasticDB = False
+
+def initializeMCStatsHandler(self):
+	global MCStatsElasticDB
+	MCStatsElasticDB = MCStatsElasticDB()
+	return S_OK()
+
+class MCStatsElasticDBHandler(RequestHandler):
+	def __init__(self, *args, **kargs):
+		RequestHandler.__init__(self, *args, **kargs)
+
+		self.DB = MCStatsElasticDB
+		self.lock = threading.lock() # What is this??
+
+	def export_set(self, typeName, data):
+
+		gLogger.notice('Called set() with typeName = %s' % typeName)
+		return self.DB.set(typeName, data)
+
+	def export_get(self):
+
+		gLogger.notice('Called get()' )
+		return self.DB.get()
+
+	def export_remove(self):
+
+		gLogger.notice('Called remove()' )
+		return self.DB.remove()
