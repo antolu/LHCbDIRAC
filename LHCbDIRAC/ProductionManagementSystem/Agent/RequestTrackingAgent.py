3c9cc7fb00412a750a7ab6fa23ea27b7d0c84cb0
Federico Stagni <federico.stagni@cern.ch>
2018-07-12 15:59:59 +0200

    autopep8

--- LHCbDIRAC/ProductionManagementSystem/Agent/RequestTrackingAgent.py
+++ LHCbDIRAC/ProductionManagementSystem/Agent/RequestTrackingAgent.py
@@ -3,94 +3,95 @@
     productions and the number of Output Events for all productions.
 """
 
-from DIRAC                                                import S_OK, S_ERROR, gLogger
-from DIRAC.Core.Base.AgentModule                          import AgentModule
-from DIRAC.Core.DISET.RPCClient                           import RPCClient
+from DIRAC import S_OK, S_ERROR, gLogger
+from DIRAC.Core.Base.AgentModule import AgentModule
+from DIRAC.Core.DISET.RPCClient import RPCClient
 from LHCbDIRAC.BookkeepingSystem.Client.BookkeepingClient import BookkeepingClient
 
 __RCSID__ = "$Id$"
 
 AGENT_NAME = 'ProductionManagement/RequestTrackingAgent'
 
-class RequestTrackingAgent( AgentModule ):
 
-  def __init__( self, *args, **kwargs ):
+class RequestTrackingAgent(AgentModule):
+
+  def __init__(self, *args, **kwargs):
     """ c'tor
     """
-    AgentModule.__init__( self, *args, **kwargs )
+    AgentModule.__init__(self, *args, **kwargs)
 
     self.bkClient = None
     self.prodReq = None
 
-  def initialize( self ):
+  def initialize(self):
     """ Just initializing the clients
     """
     self.bkClient = BookkeepingClient()
-    self.prodReq = RPCClient( "ProductionManagement/ProductionRequest" )
+    self.prodReq = RPCClient("ProductionManagement/ProductionRequest")
 
     return S_OK()
 
-  def execute( self ):
+  def execute(self):
     """The RequestTrackingAgent execution method.
     """
     result = self.prodReq.getTrackedInput()
     update = []
     if result['OK']:
-      gLogger.verbose( "Requests tracked: %s" % ( ','.join( [str( req['RequestID'] ) for req in result['Value'] ] ) ) )
+      gLogger.verbose("Requests tracked: %s" % (','.join([str(req['RequestID']) for req in result['Value']])))
       for request in result['Value']:
-        result = self.bkInputNumberOfEvents( request )
+        result = self.bkInputNumberOfEvents(request)
         if result['OK']:
-          update.append( {'RequestID':request['RequestID'],
-                          'RealNumberOfEvents':result['Value']} )
+          update.append({'RequestID': request['RequestID'],
+                         'RealNumberOfEvents': result['Value']})
         else:
-          gLogger.error( 'Input of %s is not updated: %s' %
-                         ( str( request['RequestID'] ), result['Message'] ) )
+          gLogger.error('Input of %s is not updated: %s' %
+                        (str(request['RequestID']), result['Message']))
     else:
-      gLogger.error( 'Request service: %s' % result['Message'] )
+      gLogger.error('Request service: %s' % result['Message'])
     if update:
-      result = self.prodReq.updateTrackedInput( update )
+      result = self.prodReq.updateTrackedInput(update)
       if not result['OK']:
-        gLogger.error( result['Message'] )
+        gLogger.error(result['Message'])
 
-    return S_OK( 'Request Tracking information updated' )
+    return S_OK('Request Tracking information updated')
 
-  def bkInputNumberOfEvents( self, request ):
+  def bkInputNumberOfEvents(self, request):
     """ Extremely dirty way...
     """
     try:
-      condition = {'ProcessingPass'  : str( request['inProPass'] ).strip(),
-                   'FileType'        : [str(ift) for ift in request['inFileType'].replace(' ', '').split(',')],
-                   'EventType'       : str( request['EventType'] ).replace( ' ', '' ),
-                  'ConfigName'      : str( request['configName'] ).replace( ' ', '' ),
-                  'ConfigVersion'   : str( request['configVersion'] ).replace( ' ', '' ),
-                  'DataQualityFlag' : [str(idq) for idq in request['inDataQualityFlag'].replace(' ', '').split(',')]
-                  }
+      condition = {'ProcessingPass': str(request['inProPass']).strip(),
+                   'FileType': [str(ift) for ift in request['inFileType'].replace(' ', '').split(',')],
+                   'EventType': str(request['EventType']).replace(' ', ''),
+                   'ConfigName': str(request['configName']).replace(' ', ''),
+                   'ConfigVersion': str(request['configVersion']).replace(' ', ''),
+                   'DataQualityFlag': [str(idq) for idq in request['inDataQualityFlag'].replace(' ', '').split(',')]
+                   }
     except KeyError as ke:
-      gLogger.error("%s is incomplete: %s" %(request['RequestID'], repr(ke)))
+      gLogger.error("%s is incomplete: %s" % (request['RequestID'], repr(ke)))
       return S_ERROR()
 
     if 'condType' in request and request['condType'] == 'Run':
-      condition['DataTakingConditions'] = str( request['SimCondition'] )
+      condition['DataTakingConditions'] = str(request['SimCondition'])
     else:
-      condition['SimulationConditions'] = str( request['SimCondition'] )
-    if str( request['inProductionID'] ) not in ('0', 'ALL'):
-      condition['Production'] = [int( x ) for x in str( request['inProductionID'] ).split( ',' )]
-    if 'inTCKs' in request and str( request['inTCKs'] ) != '':
-      condition['TCK'] = [str( x ) for x in str( request['inTCKs'] ).split( ',' )]
+      condition['SimulationConditions'] = str(request['SimCondition'])
+    if str(request['inProductionID']) not in ('0', 'ALL'):
+      condition['Production'] = [int(x) for x in str(request['inProductionID']).split(',')]
+    if 'inTCKs' in request and str(request['inTCKs']) != '':
+      condition['TCK'] = [str(x) for x in str(request['inTCKs']).split(',')]
     condition['NbOfEvents'] = True
 
-    gLogger.verbose( "Requesting: ", str( condition ) )
-    result = self.bkClient.getFiles( condition )
+    gLogger.verbose("Requesting: ", str(condition))
+    result = self.bkClient.getFiles(condition)
     if not result['OK']:
       gLogger.error("Error requesting files from BK", result['Message'])
       return result
     if not result['Value'][0]:
       return S_OK(0)
     try:
-      sum_nr = long( result['Value'][0] )
+      sum_nr = long(result['Value'][0])
     except ValueError as e:
-      return S_ERROR( "Can not convert result from BK call: %s" % str( e ) )
-    return S_OK( sum_nr )
+      return S_ERROR("Can not convert result from BK call: %s" % repr(e))
+    return S_OK(sum_nr)
 
 #...............................................................................
-#EOF
+# EOF
