cd3c96e695a757cb44c760ac93db61a3e404d892
lsheikmo <lara.sheik.mohammadi@cern.ch>
2018-07-17 14:50:02 +0200

    style

--- tests/Integration/ProductionManagementSystem/Test_MCStatsElasticDB.py
+++ tests/Integration/ProductionManagementSystem/Test_MCStatsElasticDB.py
@@ -1,12 +1,10 @@
 import unittest
-import datetime
-import time
-
 
 from DIRAC.Core.Base.Script import parseCommandLine
 parseCommandLine()
 from DIRAC import gLogger
 from LHCbDIRAC.ProductionManagementSystem.DB.MCStatsElasticDB import MCStatsElasticDB
+
 
 class MCStatsElasticDBTestCase(unittest.TestCase):
   def __init__(self, *args, **kwargs):
@@ -14,46 +12,48 @@ class MCStatsElasticDBTestCase(unittest.TestCase):
     super(MCStatsElasticDBTestCase, self).__init__(*args, **kwargs)
 
     self.data = {
-              "Errors": {
-                      "Counter": 2, 
-                      "Error type": "Test error 1", 
-                      "Events": [
-                          {
-                              "runnr": "1", 
-                              "eventnr": "1"
-                          }, 
-                          {
-                              "runnr": "2", 
-                              "eventnr": "2"
-                          }
-                      ]
-              }            
-          }
+        "Errors": {
+            "Counter": 2,
+            "Error type": "Test error 1",
+                          "Events": [
+                              {
+                                  "runnr": "1",
+                                  "eventnr": "1"
+                              },
+                              {
+                                  "runnr": "2",
+                                  "eventnr": "2"
+                              }
+                          ]
+        }
+    }
 
     self.moreData = {
-                "Errors": {
-                  "Counter" : 4,
-                      "Error type": "Test error 2", 
-                      "Events": [
-                          {
-                              "runnr": "Run 1", 
-                              "eventnr": "Evt 1"
-                          }, 
-                          {
-                              "runnr": "Run 2", 
-                              "eventnr": "Evt 2"
-                          }, 
-                          {
-                              "runnr": "Run 3", 
-                              "eventnr": "Evt 3"
-                          }, 
-                          {
-                              "runnr": "Run 4", 
-                              "eventnr": "Evt 4"
-                          }
-                    ]
+        "Errors":
+        {
+            "Counter": 4,
+            "Error type": "Test error 2",
+            "Events":
+            [
+                {
+                    "runnr": "Run 1",
+                    "eventnr": "Evt 1"
+                },
+                {
+                    "runnr": "Run 2",
+                    "eventnr": "Evt 2"
+                },
+                {
+                    "runnr": "Run 3",
+                    "eventnr": "Evt 3"
+                },
+                {
+                    "runnr": "Run 4",
+                    "eventnr": "Evt 4"
                 }
-            }
+            ]
+        }
+    }
 
     self.typeName = 'test'
     self.indexName = 'mcstatsdb'
@@ -65,15 +65,17 @@ class MCStatsElasticDBTestCase(unittest.TestCase):
   def tearDown(self):
     self.MCStatsElasticDB = False
 
+
 class TestMCStatsElasticDB(MCStatsElasticDBTestCase):
 
   def test_set(self):
-    result = self.DB.set( self.typeName, self.data)
+    result = self.DB.set(self.typeName, self.data)
     self.assertTrue(result['OK'])
-    result = self.DB.set( self.typeName, self.moreData)
+    result = self.DB.set(self.typeName, self.moreData)
     self.assertTrue(result['OK'])
+
 
 if __name__ == '__main__':
   suite = unittest.defaultTestLoader.loadTestsFromTestCase(MCStatsElasticDBTestCase)
-  suite.addTest( unittest.defaultTestLoader.loadTestsFromTestCase( TestMCStatsElasticDB ) )
-  testResult = unittest.TextTestRunner(verbosity = 2).run(suite)
+  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestMCStatsElasticDB))
+  testResult = unittest.TextTestRunner(verbosity=2).run(suite)
