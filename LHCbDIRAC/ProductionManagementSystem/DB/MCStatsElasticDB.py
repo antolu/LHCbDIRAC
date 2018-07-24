11f0577183c7d080c4b7c13ac9bd5819f85a3c4d
lsheikmo <lara.sheik.mohammadi@cern.ch>
2018-07-24 15:55:33 +0200

    update get()-query

--- LHCbDIRAC/ProductionManagementSystem/DB/MCStatsElasticDB.py
+++ LHCbDIRAC/ProductionManagementSystem/DB/MCStatsElasticDB.py
@@ -3,36 +3,46 @@ A database wrapper for ElasticDB
 author: Lara Sheik
 version: 17/07/18
 """
-from DIRAC.Core.Base.ElasticDB import ElasticDB as DB
+from elasticsearch import Elasticsearch
 from DIRAC import gLogger, S_OK, S_ERROR
+from DIRAC.Core.Base.ElasticDB import ElasticDB as DB
+import json
+ES = Elasticsearch()
 # import os
 
 
 class MCStatsElasticDB(DB):
-    def __init__(self):
-        DB.__init__(self, 'MCStatsDB', 'WorkloadManagement/MCStatsDB')
-        self.indexName = 'mcstatsdb'
-        # self.typeName = 'LogErr'    # We assume the type of the data is from LogErr
-        self.mapping = {
-            'Errors': {
-                'properties': {
-                    'Counter': {'type': 'integer'},
-                    'Error type': {'type': 'text'},
-                    'Events': {
-                        'properties': {
-                            'runnr': {'type': 'text'},
-                            'eventnr': {'type': 'text'}
-                        }
+  def __init__(self):
+    DB.__init__(self, 'MCStatsDB', 'ProductionManagement/MCStatsDB')
+    self.indexName = 'mcstatsdb'
+    # self.typeName = 'LogErr'    # We assume the type of the data is from LogErr
+    self.mapping = {
+        "Errors": {
+            "properties": {
+                "ID": {
+                    "properties": {
+                        "JobID": {"type": "integer"},
+                        "TransformationID": {"type": "text"},
+                        "ProductionID": {"type": "text"}
+                    }
+                },
+                "Counter": {"type": "integer"},
+                "Error type": {"type": "text"},
+                "Events": {
+                    "properties": {
+                        "runnr": {"type": "text"},
+                        "eventnr": {"type": "text"}
                     }
                 }
             }
         }
+    }
 
-        createIndex = self.createIndex(self.indexName, self.mapping, None)
-        if not createIndex['OK']:
-            gLogger.error("ERROR: Couldn't create index")
-        else:
-            gLogger.notice("Creating index: ", self.indexName)
+    createIndex = self.createIndex(self.indexName, self.mapping, None)
+    if not createIndex['OK']:
+      gLogger.error("ERROR: Couldn't create index")
+    else:
+      gLogger.notice("Creating index: ", self.indexName)
 
 #############################################################################
 
@@ -58,29 +68,69 @@ class MCStatsElasticDB(DB):
 
     #   self.index(self.indexName, typeName, data)
 
-    def set(self, typeName, data):
-        """
-        Inserts data into specified index using data given in argument'
+  def set(self, typeName, data):
+    """
+    Inserts data into specified index using data given in argument'
 
-        :param str typeName: The type in the index in ElasticSearch
-        :param dict data: The data to be inserted in ElasticSearch in JSON format
-        """
-        result = self.index(self.indexName, typeName, data)
-        if self.exists(self.indexName) and result['OK']:
-            gLogger.notice('Inserting data in index', self.indexName)
-        else:
-            gLogger.error("ERROR: Couldn't find file")
-        return result
-
-#############################################################################
-
-#  def get(self, indexName):
-
-#    pass
-
+    :param str typeName: The type in the index in ElasticSearch
+    :param dict data: The data to be inserted in ElasticSearch in JSON format
+    """
+    result = self.index(self.indexName, typeName, data)
+    if self.exists(self.indexName) and result['OK']:
+      gLogger.notice('Inserting data in index', self.indexName)
+    else:
+      gLogger.error("ERROR: Couldn't find file")
+    return result
 
 #############################################################################
 
-#  def remove(self, indexName):
+  def get(self, ID):
+    # write code that performs query on id
+    query = {
+      "query": {
+        "bool": {
+          "must": {
+            "match": {
+              "Errors.ID.JobID": ID
+            }
+          }
+        }
+      }
+    }
+    
+    gLogger.notice('Getting results for JobID: ', ID)
+    result = self.query('mcstatsdb*', query)
 
-#    pass
+    resultDict = {}
+    if not result['OK']:
+      return S_ERROR(result)
+
+    sources = result['Value']['hits']['hits']
+    for source in sources:
+        data = source['_source']
+        resultDict.update(data)
+    resultDict = json.dumps(resultDict)
+    return S_OK(resultDict)
+  
+#############################################################################
+
+  def remove(self, ID):
+    query = {
+      "query": {
+        "bool": {
+          "must": {
+            "match": {
+              "Errors.ID.JobID": ID
+            }
+          }
+        }
+      }
+    }
+
+    gLogger.notice('Attempting to delete data with JobID ', ID)
+    try:
+      ES.delete_by_query(index = self.indexName, body = query)
+    except Exception as inst:
+      return S_ERROR(inst)
+    print 'hi'
+    return S_OK('Successfully deleted data with JobID: %s' %ID)
