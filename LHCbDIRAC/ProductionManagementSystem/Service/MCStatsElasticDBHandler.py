e5055af7bd5dff52e3ecd8affd7089e6f06dcd8c
lsheikmo <lara.sheik.mohammadi@cern.ch>
2018-07-30 16:17:36 +0200

    fix types

--- LHCbDIRAC/ProductionManagementSystem/Service/MCStatsElasticDBHandler.py
+++ LHCbDIRAC/ProductionManagementSystem/Service/MCStatsElasticDBHandler.py
@@ -1,36 +1,39 @@
-from DIRAC import gLogger, S_OK, S_ERROR
+"""
+Handler for MCStatsElasticDB
+"""
+
+from DIRAC import gLogger, S_OK
 from DIRAC.Core.DISET.RequestHandler import RequestHandler
 from LHCbDIRAC.ProductionManagementSystem.DB.MCStatsElasticDB import MCStatsElasticDB
 
 __RCSID__ = "$Id$"
 
-MCStatsElasticDB = False
+#global mcStatsDB
+mcStatsDB = False
 
-def initializeMCStatsHandler(self):
-  global MCStatsElasticDB
-  MCStatsElasticDB = MCStatsElasticDB()
+def initializeMCStatsElasticDBHandler(_serviceinfo):
+  global mcStatsDB
+  mcStatsDB = MCStatsElasticDB()
   return S_OK()
 
 class MCStatsElasticDBHandler(RequestHandler):
   def __init__(self, *args, **kargs):
     RequestHandler.__init__(self, *args, **kargs)
 
-    self.DB = MCStatsElasticDB
-
-  types_set = [ dict ]
+  types_set = [ basestring, basestring ]
   def export_set(self, typeName, data):
 
-    gLogger.notice('Called set() with typeName = %s' % typeName)
-    return self.DB.set(typeName, data)
+    gLogger.notice('Called set() with typeName = %s' %typeName)
+    return mcStatsDB.set(typeName, data)
 
-  # types_get = [ dict ]
-  # def export_get(self):
+  types_get = [ int ]
+  def export_get(self, jobID):
 
-  #   gLogger.notice('Called get()' )
-  #   return self.DB.get()
+    gLogger.notice('Called get() with jobID = %s' %jobID)
+    return mcStatsDB.get(jobID)
 
-  # types_remove = [ dict ]
-  # def export_remove(self):
+  types_remove = [ int ]
+  def export_remove(self, jobID):
 
-  #   gLogger.notice('Called remove()' )
-  #   return self.DB.remove()
+    gLogger.notice('Called remove() with jobID = %s' %jobID)
+    return mcStatsDB.remove(jobID)
