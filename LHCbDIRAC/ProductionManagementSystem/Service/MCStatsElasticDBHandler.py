b99e6990519be7050c9eb843fd1e01ba472c8ef9
lsheikmo <lara.sheik.mohammadi@cern.ch>
2018-08-07 14:10:44 +0200

    autopep8

--- LHCbDIRAC/ProductionManagementSystem/Service/MCStatsElasticDBHandler.py
+++ LHCbDIRAC/ProductionManagementSystem/Service/MCStatsElasticDBHandler.py
@@ -12,29 +12,34 @@ __RCSID__ = "$Id$"
 global mcStatsDB
 mcStatsDB = False
 
+
 def initializeMCStatsElasticDBHandler(_serviceinfo):
   global mcStatsDB
   mcStatsDB = MCStatsElasticDB()
   return S_OK()
 
+
 class MCStatsElasticDBHandler(RequestHandler):
   def __init__(self, *args, **kargs):
     RequestHandler.__init__(self, *args, **kargs)
 
-  types_set = [ basestring, basestring ]
+  types_set = [basestring, basestring]
+
   def export_set(self, typeName, data):
 
-    gLogger.notice('Called set() with typeName = %s' %typeName)
+    gLogger.notice('Called set() with typeName = %s' % typeName)
     return mcStatsDB.set(typeName, data)
 
-  types_get = [ int ]
+  types_get = [int]
+
   def export_get(self, jobID):
 
-    gLogger.notice('Called get() with jobID = %s' %jobID)
+    gLogger.notice('Called get() with jobID = %s' % jobID)
     return mcStatsDB.get(jobID)
 
-  types_remove = [ int ]
+  types_remove = [int]
+
   def export_remove(self, jobID):
 
-    gLogger.notice('Called remove() with jobID = %s' %jobID)
+    gLogger.notice('Called remove() with jobID = %s' % jobID)
     return mcStatsDB.remove(jobID)
