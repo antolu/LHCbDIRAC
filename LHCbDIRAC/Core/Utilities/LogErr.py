6e9d19bcc6027a891a7737e082d131b20b92ae1d
lsheikmo <lara.sheik.mohammadi@cern.ch>
2018-08-14 12:19:45 +0200

    Fix to camelCase

--- LHCbDIRAC/Core/Utilities/LogErr.py
+++ LHCbDIRAC/Core/Utilities/LogErr.py
@@ -6,48 +6,48 @@ import json
 from DIRAC import gLogger, S_ERROR, S_OK
 
 
-def readLogFile(log_file, project, version, jobID, prodID, wmsID, name='errors.json'):
+def readLogFile(logFile, project, version, jobID, prodID, wmsID, name='errors.json'):
 
   fileOK = False
   logString = ''
   stringFile = ''
-  stringFile = pick_string_file(project, version, stringFile)
-  dict_total = []
-  dict_G4_errors = dict()
-  dict_G4_errors_count = dict()
+  stringFile = pickStringFile(project, version, stringFile)
+  dictTotal = []
+  dictG4Errors = dict()
+  dictG4ErrorsCount = dict()
 
   if stringFile is None or os.stat(stringFile)[6] == 0:
     gLogger.warn('WARNING: STRINGFILE %s is empty' % stringFile)
 
-  read_error_dict(stringFile, dict_G4_errors)
+  readErrorDict(stringFile, dictG4Errors)
 
-  fileOK = get_log_string(log_file, logString, fileOK)
+  fileOK = getLogString(logFile, logString, fileOK)
   if not fileOK:
-    gLogger.warn('WARNING: Problems in reading %s' % log_file)
+    gLogger.warn('WARNING: Problems in reading %s' % logFile)
     return S_ERROR()
 
-  reversed_keys = sorted(dict_G4_errors.keys(), reverse=True)
+  reversedKeys = sorted(dictG4Errors.keys(), reverse=True)
 
-  for error_string in reversed_keys:
-    dict_count_dump_error_string = dict()
-    dict_test = dict()
-    ctest = logString.count(error_string)
-    test = logString.find(error_string)
+  for errorString in reversedKeys:
+    dictCountDumpErrorString = dict()
+    dictTemp = dict()
+    ctest = logString.count(errorString)
+    test = logString.find(errorString)
     array = []
     for i in range(0, ctest):
 
       start = test
-      test = logString.find(error_string, start)
-      already_found = False
-      for error in reversed_keys:
-        if error == error_string:
+      test = logString.find(errorString, start)
+      alreadyFound = False
+      for error in reversedKeys:
+        if error == errorString:
           break
         checke = logString[test:test + 100].find(error)
         if checke != -1:
-          already_found = True
+          alreadyFound = True
           test = test + len(error)
           break
-      if already_found:
+      if alreadyFound:
         continue
 
       if test != -1:
@@ -59,38 +59,38 @@ def readLogFile(log_file, project, version, jobID, prodID, wmsID, name='errors.j
           eventnr = 'Evt ' + logString[eventnr_point:test].split('INFO Evt')[1].strip().split(',')[0]
           runnr = logString[eventnr_point:].split('INFO Evt')[1].strip().split(',')[1]
 
-        if error_string.find('G4') != -1:
+        if errorString.find('G4') != -1:
           check = logString[test:test + 250].find('***')
           if check != -1:
-            error_base = logString[test:test + 250].split('***')[0]
-            dict_count_dump_error_string[i] = eventnr + "  " + runnr + "  -->" + error_base
+            errorBase = logString[test:test + 250].split('***')[0]
+            dictCountDumpErrorString[i] = eventnr + "  " + runnr + "  -->" + errorBase
 
             array.append(dict())
             array[-1]['eventnr'] = eventnr
             array[-1]['runnr'] = runnr
 
-            length_dump = len(error_base)
-            test = test + length_dump
+            lengthDump = len(errorBase)
+            test = test + lengthDump
         else:
-          error_base = logString[test:test + 250].split('\n')[0]
-          dict_count_dump_error_string[i] = eventnr + "  " + runnr + "  -->" + error_base
+          errorBase = logString[test:test + 250].split('\n')[0]
+          dictCountDumpErrorString[i] = eventnr + "  " + runnr + "  -->" + errorBase
 
           array.append(dict())
           array[-1]['eventnr'] = eventnr
           array[-1]['runnr'] = runnr
 
-          length_dump = len(error_base)
-          test = test + length_dump
+          lengthDump = len(errorBase)
+          test = test + lengthDump
 
         if array[-1] is not None:
-          dict_test[error_string] = dict()
-          dict_test[error_string] = array
+          dictTemp[errorString] = dict()
+          dictTemp[errorString] = array
 
-    if dict_test != {}:
-      dict_total.append(dict_test)
-    dict_G4_errors_count[error_string] = dict_count_dump_error_string
-  create_json_table(dict_total, name, jobID, prodID, wmsID)
-  create_HTML_table(dict_G4_errors_count, "errors.html")
+    if dictTemp != {}:
+      dictTotal.append(dictTemp)
+    dictG4ErrorsCount[errorString] = dictCountDumpErrorString
+  createJSONtable(dictTotal, name, jobID, prodID, wmsID)
+  createHTMLtable(dictG4ErrorsCount, "errors.html")
 
 ################################################
 
@@ -98,7 +98,7 @@ def readLogFile(log_file, project, version, jobID, prodID, wmsID, name='errors.j
 #   (which is more clear than the one below) couldn't be used.
 #   # I have still saved the function here.
 
-# def create_json_table(dict_total, name):
+# def createJSONtable(dictTotal, name):
 
 #   ids = {}
 #   ids['JobID'] = JOB_ID
@@ -108,7 +108,7 @@ def readLogFile(log_file, project, version, jobID, prodID, wmsID, name='errors.j
 #   errors = []
 
 #   with open(name, 'w') as output:
-#     for error in dict_total:
+#     for error in dictTotal:
 #       newrow = {}
 #       for key, value in error.items():
 #         newrow['Error type'] = key
@@ -129,7 +129,7 @@ def readLogFile(log_file, project, version, jobID, prodID, wmsID, name='errors.j
 #####################################################
 
 
-def create_json_table(dict_total, name, jobID, prodID, wmsID):
+def createJSONtable(dictTotal, name, jobID, prodID, wmsID):
 
   result = {}
   temp = {}
@@ -139,7 +139,7 @@ def create_json_table(dict_total, name, jobID, prodID, wmsID):
   ids['wmsID'] = wmsID
 
   with open(name, 'w') as output:
-    for error in dict_total:
+    for error in dictTotal:
       for key, value in error.items():
         temp[key] = len(value)
     temp['ID'] = ids
@@ -150,7 +150,7 @@ def create_json_table(dict_total, name, jobID, prodID, wmsID):
 #####################################################
 
 
-def create_HTML_table(dict_G4_errors_count, name):
+def createHTMLtable(dictG4ErrorsCount, name):
 
   f = open(name, 'w')
   f.write("<HTML>\n")
@@ -162,17 +162,17 @@ def create_HTML_table(dict_G4_errors_count, name):
   f.write("<td>DUMP OF ERROR MESSAGES</td>")
   f.write("</tr>")
 
-  orderedKeys = sorted(dict_G4_errors_count.keys())
+  orderedKeys = sorted(dictG4ErrorsCount.keys())
   for errString in orderedKeys:
-    if dict_G4_errors_count[errString] != {}:
+    if dictG4ErrorsCount[errString] != {}:
       f.write("<tr>")
       f.write("<td>" + errString + "</td>")
-      f.write("<td>" + str(len(dict_G4_errors_count[errString].keys())) + "</td>")
+      f.write("<td>" + str(len(dictG4ErrorsCount[errString].keys())) + "</td>")
       f.write("<td>")
       f.write("<lu>")
-      for y in dict_G4_errors_count[errString].keys():
+      for y in dictG4ErrorsCount[errString].keys():
         f.write("<li>")
-        f.write(" " + dict_G4_errors_count[errString][y] + " ")
+        f.write(" " + dictG4ErrorsCount[errString][y] + " ")
       f.write("</lu>")
       f.write("</td>")
       f.write("</tr>")
@@ -184,56 +184,56 @@ def create_HTML_table(dict_G4_errors_count, name):
 #######################################################
 
 
-def read_error_dict(string_file, dict_name):
+def readErrorDict(stringFile, dict_name):
   'Reads errors in a stringfile and puts them in dict_name'
-  file_lines = get_lines(string_file)
-  for line in file_lines:
-    error_string = line.split(',')[0]
+  fileLines = getLines(stringFile)
+  for line in fileLines:
+    errorString = line.split(',')[0]
     description = line.split(',')[1]
-    dict_name[error_string] = description
+    dict_name[errorString] = description
   return
 
 ################################################
 
 
-def get_lines(string_file):
+def getLines(stringFile):
   'Reads lines from string file'
-  gLogger.notice('>>> Processed STRINGFILE -> ', string_file)
-  with open(string_file, 'r') as f:
+  gLogger.notice('>>> Processed STRINGFILE -> ', stringFile)
+  with open(stringFile, 'r') as f:
     lines = f.readlines()
   return lines
 
 ################################################
 
 
-def get_log_string(log_file, logString, fileOK):
-  gLogger.notice('Attempting to open %s' % log_file)
-  if not os.path.exists(log_file):
-    gLogger.error('%s could not be found' % log_file)
+def getLogString(logFile, logString, fileOK):
+  gLogger.notice('Attempting to open %s' % logFile)
+  if not os.path.exists(logFile):
+    gLogger.error('%s could not be found' % logFile)
     return S_ERROR()
-  if os.stat(log_file)[6] == 0:
-    gLogger.error('%s is empty' % log_file)
+  if os.stat(logFile)[6] == 0:
+    gLogger.error('%s is empty' % logFile)
     return S_ERROR()
-  with open(log_file, 'r') as f:
+  with open(logFile, 'r') as f:
     logString = f.read()
   fileOK = True
-  gLogger.notice("Successfully read %s" % log_file)
+  gLogger.notice("Successfully read %s" % logFile)
   return fileOK
 
 ################################################
 
 
-def pick_string_file(project, version, stringFile):
-  # source_dir = commands.getoutput('echo $PWD') + '/errstrings'
-  source_dir = os.getcwd()
-  file_string = project + '_' + version + '_errors.txt'
-  stringFile = os.path.join(source_dir, os.path.basename(file_string))
+def pickStringFile(project, version, stringFile):
+  # sourceDir = commands.getoutput('echo $PWD') + '/errstrings'
+  sourceDir = os.getcwd()
+  fileString = project + '_' + version + '_errors.txt'
+  stringFile = os.path.join(sourceDir, os.path.basename(fileString))
   if not os.path.exists(stringFile):
     gLogger.notice('string file %s does not exist, attempting to take the most recent file ...' % stringFile)
-    file_list = [os.path.join(source_dir, f) for f in os.listdir(source_dir) if f.find(project) != -1]
-    if len(file_list) != 0:
-      file_list.sort()
-      stringFile = file_list[len(file_list) - 1]
+    fileList = [os.path.join(sourceDir, f) for f in os.listdir(sourceDir) if f.find(project) != -1]
+    if len(fileList) != 0:
+      fileList.sort()
+      stringFile = fileList[len(fileList) - 1]
     else:
       gLogger.warn('WARNING: no string files for this project')
       return None
@@ -262,9 +262,9 @@ def pick_string_file(project, version, stringFile):
 
 # #LOG_STRING = ''
 # #FILE_OK = ''
-# dict_G4_errors = dict()
-# dict_G4_errors_count = dict()
-# STRING_FILE = pick_string_file(PROJECT, VERSION)
+# dictG4Errors = dict()
+# dictG4ErrorsCount = dict()
+# STRING_FILE = pickStringFile(PROJECT, VERSION)
 
 # if STRING_FILE is not None:
 #   if os.stat(STRING_FILE)[6] != 0:
