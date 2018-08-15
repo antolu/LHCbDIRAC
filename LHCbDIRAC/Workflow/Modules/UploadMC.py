67f6a39134da8ed50c3c70d93d67da6a8f7a008d
lsheikmo <lara.sheik.mohammadi@cern.ch>
2018-08-15 10:41:08 +0200

    add comma

--- LHCbDIRAC/Workflow/Modules/UploadMC.py
+++ LHCbDIRAC/Workflow/Modules/UploadMC.py
@@ -55,7 +55,7 @@ class UploadMC(ModuleBase):
                                                          self.applicationName,
                                                          self.applicationVersion,
                                                          self.step_number))
-      MCStatsClient().set('LogErr', 'json' jsonData)
+      MCStatsClient().set('LogErr', 'json', jsonData)
 
     except Exception as e:  # pylint:disable=broad-except
       self.log.exception("Failure in UploadMC execute module", lException=e)
