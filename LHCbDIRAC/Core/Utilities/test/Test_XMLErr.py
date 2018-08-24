7da36561026ceec0bb9464acdac0db4794842e5c
lsheikmo <lara.sheik.mohammadi@cern.ch>
2018-08-24 15:47:04 +0200

    fix name change

--- LHCbDIRAC/Core/Utilities/test/Test_XMLErr.py
+++ LHCbDIRAC/Core/Utilities/test/Test_XMLErr.py
@@ -1,5 +1,5 @@
 """
-Test LogErrXML.py
+Test XMLErr.py
 """
 
 import json
@@ -8,13 +8,13 @@ import ast
 import xml.etree.ElementTree as ET
 
 from DIRAC import gLogger
-import LHCbDIRAC.Core.Utilities.LogErrXML as LogErrXML
+import LHCbDIRAC.Core.Utilities.XMLErr as XMLErr
 
 
-class LogErrXMLTestCase(unittest.TestCase):
+class XMLErrTestCase(unittest.TestCase):
 
   def __init__(self, *args, **kwargs):
-    super(LogErrXMLTestCase, self).__init__(*args, **kwargs)
+    super(XMLErrTestCase, self).__init__(*args, **kwargs)
 
   def setUp(self):
     self.jobID = 1
@@ -25,7 +25,7 @@ class LogErrXMLTestCase(unittest.TestCase):
     pass
 
 
-class TestLogErrXML(LogErrXMLTestCase):
+class TestXMLErr(XMLErrTestCase):
 
   #########################################
   # Test extractData()
@@ -33,33 +33,33 @@ class TestLogErrXML(LogErrXMLTestCase):
   def test_extractDataWithNoCounter(self):
     rootNoCounter = ET.fromstring("<counters></counters>")
     expected = dict({"Counters": {"ID": {"JobID": 1, "ProductionID": 2, "wmsID": 3}}})
-    result = LogErrXML.extractData(rootNoCounter, self.jobID, self.prodID, self.wmsID)
+    result = XMLErr.extractData(rootNoCounter, self.jobID, self.prodID, self.wmsID)
 
     self.assertTrue(result['OK'])
     self.assertEqual(ast.literal_eval(result['Value']), expected)
 
   def test_extractDataWithStringValue(self):
     rootStringValue = ET.fromstring('<counters><counter name = "test">"string"</counter></counters>')
-    result = LogErrXML.extractData(rootStringValue, self.jobID, self.prodID, self.wmsID)
+    result = XMLErr.extractData(rootStringValue, self.jobID, self.prodID, self.wmsID)
 
     self.assertFalse(result['OK'])
 
   def test_extractDataWithFloatValue(self):
     rootFloatValues = ET.fromstring('<counters><counter name = "test">1.5</counter></counters>')
-    result = LogErrXML.extractData(rootFloatValues, self.jobID, self.prodID, self.wmsID)
+    result = XMLErr.extractData(rootFloatValues, self.jobID, self.prodID, self.wmsID)
 
     self.assertFalse(result['OK'])
 
   def test_extractDataNormal(self):
     root = ET.fromstring('<counters><counter name = "test1">1</counter><counter name = "test2">-1</counter></counters>')
     expected = dict({"Counters": {"test1": 1, "test2": -1, "ID": {"JobID": 1, "ProductionID": 2, "wmsID": 3}}})
-    result = LogErrXML.extractData(root, self.jobID, self.prodID, self.wmsID)
+    result = XMLErr.extractData(root, self.jobID, self.prodID, self.wmsID)
 
     self.assertTrue(result['OK'])
     self.assertEqual(ast.literal_eval(result['Value']), expected)
 
 
 if __name__ == '__main__':
-  suite = unittest.defaultTestLoader.loadTestsFromTestCase(LogErrXMLTestCase)
-  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestLogErrXML))
+  suite = unittest.defaultTestLoader.loadTestsFromTestCase(XMLErrTestCase)
+  suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestXMLErr))
   testResult = unittest.TextTestRunner(verbosity=2).run(suite)
