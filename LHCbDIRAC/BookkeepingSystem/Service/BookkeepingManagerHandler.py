3d414a06f88a05f735cb5e47dcc57073fa8cc35e
Zoltan Mathe <zoltan.mathe@cern.ch>
2018-09-14 15:08:51 +0200

    accept integers as well

--- LHCbDIRAC/BookkeepingSystem/Service/BookkeepingManagerHandler.py
+++ LHCbDIRAC/BookkeepingSystem/Service/BookkeepingManagerHandler.py
@@ -2394,7 +2394,7 @@ class BookkeepingManagerHandler(RequestHandler):
     return dataMGMT_.fixRunLuminosity(runnumbers)
 
   #############################################################################
-  types_getProductionProducedEvents = [long]
+  types_getProductionProducedEvents = [(long, int)]
 
   @staticmethod
   def export_getProductionProducedEvents(prodid):
