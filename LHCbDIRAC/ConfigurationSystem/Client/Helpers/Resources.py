6efae583c55315e0febc316a1d4ab45d91fc6fd2
Federico Stagni <federico.stagni@cern.ch>
2018-11-21 15:08:46 +0100

    added function for getting the DIRAC job platform (requirement) from the list of CMT configs (binary tags)

--- LHCbDIRAC/ConfigurationSystem/Client/Helpers/Resources.py
+++ LHCbDIRAC/ConfigurationSystem/Client/Helpers/Resources.py
@@ -3,6 +3,8 @@
 
 import LbPlatformUtils
 
+from DIRAC import gLogger
+
 
 def getDIRACPlatform(platform):
   """ Returns list of compatible platforms.
@@ -37,3 +39,116 @@ def getDIRACPlatform(platform):
       compatiblePlatforms.append('x86_64-' + co + '.' + cm)
 
   return compatiblePlatforms
+
+
+def getPlatformForJob(workflow):
+  """ Looks inside the steps definition to find all requested CMTConfigs ("binary tag"),
+      then translates it in a DIRAC platform.
+
+      A binary tag is in the form of
+        arch+microarch-osversion-gccversion-opt
+      e.g.: x86_64+avx2+fma-centos7-gcc7-opt, x86_64-slc6-gcc49-opt
+
+      We want to know what the worklow (the job) requires, so we need to "compose" the requested config
+      and then get the minimum DIRAC platform that can run it.
+      If, for example, the step1 and step2 respectively requires
+        x86_64+avx2+fma-slc6-gcc7-opt
+        x86_64-centos7-gcc62-opt
+      then we conclude that to run this job we need
+        x86_64+avx2+fma-centos7-gcc7-opt
+      and so the DIRAC platform x86_64-centos7.avx2+fma
+
+      :returns: a DIRAC platform (a string) or None
+  """
+
+  archSet = set()
+  microarchSet = set()
+  osVersionSet = set()
+  gccVersionSet = set()
+  binaryTags = _findBinaryTags(workflow)
+  if not binaryTags:
+    gLogger.debug("Resources.getPlatformForJob: this job has no specific binary tag requested in any of its steps")
+    return None
+  for bt in binaryTags:
+    # arch+microarch-osversion-gccversion-opt
+    fulllarch, osversion, gccversion = bt.split('-')[0:3]
+    try:
+      arch, microarch = fulllarch.split('+', 1)
+      archSet.add(arch)
+      microarchSet.add(microarch)
+    except ValueError:
+      archSet.add(fulllarch)
+    osVersionSet.add(osversion)
+    gccVersionSet.add(gccversion)
+
+  archCompatibilitiesInAList = []
+  archCompatibilities = dict(LbPlatformUtils.ARCH_COMPATIBILITY)
+  while True:
+    archsCompatible = set()
+    for archCompatibleList in archCompatibilities.itervalues():
+      for archCompatible in archCompatibleList:
+        archsCompatible.add(archCompatible)
+    for o in archCompatibilities.iterkeys():
+      if o not in archsCompatible:
+        highestArch = o
+    try:
+      archCompatibilities.pop(highestArch)
+    except KeyError:
+      break
+    archCompatibilitiesInAList.append(highestArch)
+
+  for ar in archCompatibilitiesInAList:
+    if ar in archSet:
+      break
+
+  ma = ''
+  for mal in LbPlatformUtils.MICROARCH_LEVELS:
+    if mal in microarchSet:
+      ma = '+' + mal
+      break
+
+  osCompatibilityInAList = []
+  osCompatibilities = dict(LbPlatformUtils.OS_COMPATIBILITY)
+  while True:
+    ossCompatible = set()
+    for osCompatibleList in osCompatibilities.itervalues():
+      for osCompatible in osCompatibleList:
+        ossCompatible.add(osCompatible)
+    for o in osCompatibilities.iterkeys():
+      if o not in ossCompatible:
+        highestOS = o
+    try:
+      osCompatibilities.pop(highestOS)
+    except KeyError:
+      break
+    osCompatibilityInAList.append(highestOS)
+
+  for osV in osCompatibilityInAList:
+    if osV in osVersionSet:
+      break
+
+  gccV = sorted(list(gccVersionSet))[-1]
+
+  # Now, we compose the binary tag
+  composedBinaryTag = ar + ma + '-' + osV + '-' + gccV + '-opt'
+  gLogger.verbose("Resources.getPlatformForJob: binary tag composed: %s" % composedBinaryTag)
+
+  try:
+    platform = LbPlatformUtils.requires(composedBinaryTag)
+  except BaseException as error:
+    gLogger.exception("Exception while getting platform, don't set it", lException=error)
+    return None
+  return platform
+
+
+def _findBinaryTags(wf):
+  """ developer function
+      :returns: set of binary tags found in the workflow
+  """
+  binaryTags = set()
+  for step_instance in wf.step_instances:
+    for parameter in step_instance.parameters:
+      if parameter.name.lower() == 'systemconfig':  # this is where the CMTConfigs ("binary tags") are stored
+        if parameter.value and parameter.value.lower() != 'any':  # 'ANY' is a wildcard
+          binaryTags.add(parameter.value)
+  return binaryTags
---
+++ LHCbDIRAC/ConfigurationSystem/Client/Helpers/test/Test_Helpers.py
@@ -0,0 +1,39 @@
+""" unit tests for Configuration Helpers
+"""
+
+import mock
+import pytest
+
+from DIRAC import gLogger
+
+import LHCbDIRAC.ConfigurationSystem.Client.Helpers.Resources as moduleTested
+
+gLogger.setLevel('DEBUG')
+
+
+@pytest.mark.parametrize("binaryTagsSet, expected", [
+    (set(), None),
+    ({'x86_64-slc6-gcc48-opt'}, 'x86_64-slc6'),
+    ({'x86_64-slc6-gcc62-opt'}, 'x86_64-slc6.sse4_2'),
+    ({'x86_64+avx2-slc6-gcc62-opt'}, 'x86_64-slc6.avx2'),
+    ({'x86_64+avx2+fma-centos7-gcc7-opt'}, 'x86_64-centos7.avx2+fma'),
+    ({'x86_64-slc6-gcc48-opt', 'x86_64-slc6-gcc62-opt'}, 'x86_64-slc6.sse4_2'),
+    ({'x86_64-centos7-gcc49-opt', 'x86_64-slc6-gcc62-opt'}, 'x86_64-centos7.sse4_2'),
+    ({'x86_64-centos7-gcc49-opt', 'x86_64-slc6-gcc62-opt', 'x86_64+avx2-centos7-gcc62-opt'}, 'x86_64-centos7.avx2'),
+    ({'x86_64-centos7-gcc49-opt', 'x86_64+avx-slc6-gcc62-opt', 'x86_64+avx2-centos7-gcc62-opt'}, 'x86_64-centos7.avx2'),
+    ({'x86_64+fma-centos7-gcc49-opt', 'x86_64+avx-slc6-gcc62-opt', 'x86_64+avx2-centos7-gcc62-opt'},
+     'x86_64-centos7.avx2'),
+    ({'x86_64+avx2+fma-centos7-gcc49-opt', 'x86_64+avx-slc6-gcc62-opt', 'x86_64+avx2+fma-centos7-gcc62-opt'},
+     'x86_64-centos7.avx2+fma'),
+    ({'x86_64+avx2+fma-centos7-gcc49-opt', 'x86_64+avx-slc6-gcc7-opt', 'x86_64+avx2+fma-centos7-gcc62-opt'},
+     'x86_64-centos7.avx2+fma'),
+    ({'x86_64+fma-centos7-gcc7-opt', 'x86_64+avx-slc6-gcc62-opt'},
+     'x86_64-centos7.avx'),
+])
+def test_getPlatformForJob(binaryTagsSet, expected):
+  fbtMock = mock.MagicMock()
+  fbtMock.return_value = binaryTagsSet
+  moduleTested._findBinaryTags = fbtMock
+
+  res = moduleTested.getPlatformForJob(mock.MagicMock())
+  assert res == expected
