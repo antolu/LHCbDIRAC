445a2deca5ec721691b40231987644e3c9c8ac90
Federico Stagni <federico.stagni@cern.ch>
2019-01-29 11:53:14 +0100

    removed BkInputDataAgent optimizer (disabled since long time)

--- LHCbDIRAC/Core/Utilities/JobPathResolution.py
+++ LHCbDIRAC/Core/Utilities/JobPathResolution.py
@@ -9,7 +9,6 @@
     of JDL parameters.
 """
 
-from DIRAC.WorkloadManagementSystem.DB.JobDB import JobDB
 from DIRAC import S_OK, S_ERROR, gConfig, gLogger
 
 COMPONENT_NAME = 'LHCbJobPathResolution'
@@ -58,25 +57,6 @@ class JobPathResolution:
       ancestors = gConfig.getValue('%s/AncestorFiles' % section, 'AncestorFiles')
       path.append(ancestors)
 
-    inputData = jobManifest.getOption("InputData", '').replace('Unknown', '')
-    if inputData:
-      if not jobManifest.getOption('DisableDataScheduling', False):
-        self.log.info('Job %s has input data requirement' % (jid))
-        bkInputData = gConfig.getValue('%s/BKInputData' % section, 'BKInputData')
-        path.append('InputData')
-        if bkInputData not in path:
-          path.append(bkInputData)
-      else:
-        self.log.info('Job %s has input data requirement but scheduling via input data is disabled' % (jid))
-        result = JobDB().setInputData(jid, [])
-        if not result['OK']:
-          self.log.error(result)
-          return S_ERROR('Could not reset input data to null')
-
-    if jobManifest.getOption('CondDBTags', ""):
-      condDB = gConfig.getValue('%s/CondDB' % section, 'CondDB')
-      path.append(condDB)
-
     if not path:
       self.log.info('No LHCb specific optimizers to be added')
 
@@ -90,27 +70,7 @@ class JobPathResolution:
       ancestors = gConfig.getValue(section + '/AncestorFiles', 'AncestorFiles')
       lhcbPath += ancestors + ','
 
-    inputData = classadJob.get_expression('InputData').replace('"', '').replace('Unknown', '')
-    if inputData and not classadJob.lookupAttribute('DisableDataScheduling'):
-      self.log.info('Job %s has input data requirement' % (job))
-      bkInputData = gConfig.getValue(section + '/BKInputData', 'BKInputData')
-      lhcbPath += 'InputData,'
-      lhcbPath += bkInputData + ','
-
-    if inputData and classadJob.lookupAttribute('DisableDataScheduling'):
-      self.log.info('Job %s has input data requirement but scheduling via input data is disabled' % (job))
-      result = JobDB().setInputData(job, [])
-      if not result['OK']:
-        self.log.error(result)
-        return S_ERROR('Could not reset input data to null')
-
-    if classadJob.lookupAttribute('CondDBTags'):
-      condDB = gConfig.getValue(section + '/CondDB', 'CondDB')
-      lhcbPath += condDB + ','
-
     if not lhcbPath:
       self.log.info('No LHCb specific optimizers to be added')
 
     return S_OK(lhcbPath)
-
-#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#EOF#
