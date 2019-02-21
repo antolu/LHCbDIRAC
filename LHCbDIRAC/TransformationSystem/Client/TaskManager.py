6e2400f757161d6ae2e141f742896ff0b076e925
Zoltan Mathe <zoltan.mathe@cern.ch>
2019-02-21 11:20:51 +0100

    fix typo and pylint

--- LHCbDIRAC/TransformationSystem/Client/TaskManager.py
+++ LHCbDIRAC/TransformationSystem/Client/TaskManager.py
@@ -7,41 +7,42 @@ __RCSID__ = "$Id$"
 
 COMPONENT_NAME = 'LHCbTaskManager'
 
-class LHCbWorkflowTasks( WorkflowTasks ):
+
+class LHCbWorkflowTasks(WorkflowTasks):
   """ A simple LHCb extension to the task manager,
       for now only used to set the runNumber and runMetadata
   """
 
-  def _handleInputs( self, oJob, paramsDict ):
+  def _handleInputs(self, oJob, paramsDict):
     """ set job inputs (+ metadata)
     """
     try:
       if paramsDict['InputData']:
-        self.log.verbose( 'Setting input data to %s' % paramsDict['InputData'] )
-        self.log.verbose( 'Setting run number to %s' % str( paramsDict['RunNumber'] ) )
-        oJob.setInputData( paramsDict['InputData'], runNumber = paramsDict['RunNumber'] )
+        self.log.verbose('Setting input data to %s' % paramsDict['InputData'])
+        self.log.verbose('Setting run number to %s' % str(paramsDict['RunNumber']))
+        oJob.setInputData(paramsDict['InputData'], runNumber=paramsDict['RunNumber'])
 
         try:
           runMetadata = paramsDict['RunMetadata']
-          self.log.verbose( 'Setting run metadata information to %s' % str( runMetadata ) )
-          oJob.setRunMetadata( runMetadata )
+          self.log.verbose('Setting run metadata information to %s' % str(runMetadata))
+          oJob.setRunMetadata(runMetadata)
         except KeyError:
           pass
 
     except KeyError:
-      self.log.error( 'Could not found an input data or a run number' )
-      raise KeyError( 'Could not found an input data or a run number' )
+      self.log.error('Could not found an input data or a run number')
+      raise KeyError('Could not found an input data or a run number')
 
   #############################################################################
 
-  def _handleRest( self, oJob, paramsDict ):
+  def _handleRest(self, oJob, paramsDict):
     """ add as JDL parameters all the other parameters that are not for inputs or destination
     """
 
-    for paramName, paramValue in paramsDict.itertems():
-      if paramName not in ( 'InputData', 'RunNumber', 'RunMetadata', 'Site', 'TargetSE' ):
+    for paramName, paramValue in paramsDict.iteritems():
+      if paramName not in ('InputData', 'RunNumber', 'RunMetadata', 'Site', 'TargetSE'):
         if paramValue:
-          self.log.verbose( 'Setting %s to %s' % ( paramName, paramValue ) )
-          oJob._addJDLParameter( paramName, paramValue )
+          self.log.verbose('Setting %s to %s' % (paramName, paramValue))
+          oJob._addJDLParameter(paramName, paramValue)
 
   #############################################################################
