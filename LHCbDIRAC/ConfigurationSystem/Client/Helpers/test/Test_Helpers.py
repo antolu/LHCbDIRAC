6c198f0cba6ab2cb64c8623067e75a22b26e2958
Chris Burr <christopher.burr@cern.ch>
2019-05-07 10:55:13 +0200

    Support LbPlatformUtils 3.1

--- LHCbDIRAC/ConfigurationSystem/Client/Helpers/test/Test_Helpers.py
+++ LHCbDIRAC/ConfigurationSystem/Client/Helpers/test/Test_Helpers.py
@@ -10,25 +10,24 @@ import LHCbDIRAC.ConfigurationSystem.Client.Helpers.Resources as moduleTested
 
 gLogger.setLevel('DEBUG')
 
-
 @pytest.mark.parametrize("binaryTagsSet, expected", [
     (set(), None),
     ({'x86_64-slc6-gcc48-opt'}, 'x86_64-slc6'),
-    ({'x86_64-slc6-gcc62-opt'}, 'x86_64-slc6.sse4_2'),
-    ({'x86_64+avx2-slc6-gcc62-opt'}, 'x86_64-slc6.avx2'),
-    ({'x86_64+avx2+fma-centos7-gcc7-opt'}, 'x86_64-centos7.avx2+fma'),
-    ({'x86_64-slc6-gcc48-opt', 'x86_64-slc6-gcc62-opt'}, 'x86_64-slc6.sse4_2'),
-    ({'x86_64-centos7-gcc49-opt', 'x86_64-slc6-gcc62-opt'}, 'x86_64-centos7.sse4_2'),
-    ({'x86_64-centos7-gcc49-opt', 'x86_64-slc6-gcc62-opt', 'x86_64+avx2-centos7-gcc62-opt'}, 'x86_64-centos7.avx2'),
-    ({'x86_64-centos7-gcc49-opt', 'x86_64+avx-slc6-gcc62-opt', 'x86_64+avx2-centos7-gcc62-opt'}, 'x86_64-centos7.avx2'),
+    ({'x86_64-slc6-gcc62-opt'}, 'nehalem-slc6'),
+    ({'x86_64+avx2-slc6-gcc62-opt'}, 'haswell-slc6'),
+    ({'x86_64+avx2+fma-centos7-gcc7-opt'}, 'haswell-centos7'),
+    ({'x86_64-slc6-gcc48-opt', 'x86_64-slc6-gcc62-opt'}, 'nehalem-slc6'),
+    ({'x86_64-centos7-gcc49-opt', 'x86_64-slc6-gcc62-opt'}, 'nehalem-centos7'),
+    ({'x86_64-centos7-gcc49-opt', 'x86_64-slc6-gcc62-opt', 'x86_64+avx2-centos7-gcc62-opt'}, 'haswell-centos7'),
+    ({'x86_64-centos7-gcc49-opt', 'x86_64+avx-slc6-gcc62-opt', 'x86_64+avx2-centos7-gcc62-opt'}, 'haswell-centos7'),
     ({'x86_64+fma-centos7-gcc49-opt', 'x86_64+avx-slc6-gcc62-opt', 'x86_64+avx2-centos7-gcc62-opt'},
-     'x86_64-centos7.avx2'),
+     'haswell-centos7'),
     ({'x86_64+avx2+fma-centos7-gcc49-opt', 'x86_64+avx-slc6-gcc62-opt', 'x86_64+avx2+fma-centos7-gcc62-opt'},
-     'x86_64-centos7.avx2+fma'),
+     'haswell-centos7'),
     ({'x86_64+avx2+fma-centos7-gcc49-opt', 'x86_64+avx-slc6-gcc7-opt', 'x86_64+avx2+fma-centos7-gcc62-opt'},
-     'x86_64-centos7.avx2+fma'),
+     'haswell-centos7'),
     ({'x86_64+fma-centos7-gcc7-opt', 'x86_64+avx-slc6-gcc62-opt'},
-     'x86_64-centos7.avx'),
+     'haswell-centos7'),
 ])
 def test_getPlatformForJob(binaryTagsSet, expected):
   fbtMock = mock.MagicMock()
@@ -45,11 +44,11 @@ def test_getPlatformForJob(binaryTagsSet, expected):
     (['ANY'], True, []),
     ([None], True, []),
     (['bih', 'boh'], False, []),
-    ('x86_64-slc6', True, ['x86_64-slc6', 'x86_64-centos7', 'x86_64-slc6.avx2']),
-    ('x86_64-centos7', True, ['x86_64-centos7', 'x86_64-centos7.avx2+fma']),
-    ('x86_64-slc5', True, ['x86_64-slc5', 'x86_64-slc6', 'x86_64-slc5.avx2+fma', 'x86_64-slc5.sse4_2']),
-    ('x86_64-slc5.avx2', True, ['x86_64-slc5.avx2+fma', 'x86_64-slc6.avx2+fma', 'x86_64-slc6.avx2']),
-    ('x86_64-slc6.avx2+fma', True, ['x86_64-slc6.avx2+fma', 'x86_64-centos7.avx2+fma']),
+    ('x86_64-slc6', True, ['cannonlake-centos7', 'skylake_avx512-centos7', 'skylake-centos7', 'broadwell-centos7', 'haswell-centos7', 'ivybridge-centos7', 'sandybridge-centos7', 'westmere-centos7', 'nehalem-centos7', 'core2-centos7', 'x86_64-centos7', 'cannonlake-slc6', 'skylake_avx512-slc6', 'skylake-slc6', 'broadwell-slc6', 'haswell-slc6', 'ivybridge-slc6', 'sandybridge-slc6', 'westmere-slc6', 'nehalem-slc6', 'core2-slc6', 'x86_64-slc6']),
+    ('x86_64-centos7', True, ['cannonlake-centos7', 'skylake_avx512-centos7', 'skylake-centos7', 'broadwell-centos7', 'haswell-centos7', 'ivybridge-centos7', 'sandybridge-centos7', 'westmere-centos7', 'nehalem-centos7', 'core2-centos7', 'x86_64-centos7']),
+    ('x86_64-slc5', True, ['cannonlake-slc6', 'skylake_avx512-slc6', 'skylake-slc6', 'broadwell-slc6', 'haswell-slc6', 'ivybridge-slc6', 'sandybridge-slc6', 'westmere-slc6', 'nehalem-slc6', 'core2-slc6', 'x86_64-slc6', 'cannonlake-slc5', 'skylake_avx512-slc5', 'skylake-slc5', 'broadwell-slc5', 'haswell-slc5', 'ivybridge-slc5', 'sandybridge-slc5', 'westmere-slc5', 'nehalem-slc5', 'core2-slc5', 'x86_64-slc5']),
+    ('x86_64-slc5.avx2', True, ['cannonlake-slc6', 'skylake_avx512-slc6', 'skylake-slc6', 'broadwell-slc6', 'haswell-slc6', 'cannonlake-slc5', 'skylake_avx512-slc5', 'skylake-slc5', 'broadwell-slc5', 'haswell-slc5']),
+    ('x86_64-slc6.avx2+fma', True, ['cannonlake-centos7', 'skylake_avx512-centos7', 'skylake-centos7', 'broadwell-centos7', 'haswell-centos7', 'cannonlake-slc6', 'skylake_avx512-slc6', 'skylake-slc6', 'broadwell-slc6', 'haswell-slc6']),
 ])
 def test_getDIRACPlatform(platform, expectedRes, expectedValue):
   res = moduleTested.getDIRACPlatform(platform)
