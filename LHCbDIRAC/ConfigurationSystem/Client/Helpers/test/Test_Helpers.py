3815dd5427bad3bf1bc40019c0bd9b7a9f55e226
Chris Burr <christopher.burr@cern.ch>
2019-07-16 07:36:54 +0000

    Reduce indentation to 2

--- LHCbDIRAC/ConfigurationSystem/Client/Helpers/test/Test_Helpers.py
+++ LHCbDIRAC/ConfigurationSystem/Client/Helpers/test/Test_Helpers.py
@@ -116,14 +116,14 @@ def test_getDIRACPlatform(platform, expectedRes, expectedValue):
 ])
 def test_listPlatforms(applicationName, applicationVersion, expected):
   if not os.path.isdir('/cvmfs/lhcb.cern.ch'):
-      pytest.skip('CVMFS is required')
+    pytest.skip('CVMFS is required')
 
   from six.moves import xmlrpc_client
   import ssl
   try:
-      xmlrpc_client.ServerProxy('https://lbsoftdb.cern.ch/read/', allow_none=True).listApplications()
+    xmlrpc_client.ServerProxy('https://lbsoftdb.cern.ch/read/', allow_none=True).listApplications()
   except ssl.SSLError:
-      pytest.skip('CERN certificate authority must be trusted')
+    pytest.skip('CERN certificate authority must be trusted')
 
   # Good RPC and good fallback
   result = moduleTested._listPlatforms(
